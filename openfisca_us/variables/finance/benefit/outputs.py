49fc1a7ad62535dd06cf3d055faf0a020a1ba77d
nmrodelo <75631529+nmrodelo@users.noreply.github.com>
2021-08-15 14:13:35 -0400

    placeholder for expected food contribution, removed benefit unit references

--- openfisca_us/variables/finance/benefit/outputs.py
+++ openfisca_us/variables/finance/benefit/outputs.py
@@ -5,11 +5,35 @@ from openfisca_us.tools.general import *
 
 class snap(Variable):
     value_type = float
-    entity = BenefitUnit
+    entity = SPMUnit
     label = u"Supplemental Nutrition Assistance Program, benefit amount"
     definition_period = YEAR
 
-    def formula(benefitunit, period, parameters):
+    def formula(spm_unit, period, parameters):
+
+        # Params:
+        # Income Threshold
+        # Max Benefit
+        # Phase-out Rate
+        # for later, Employment Rules
+
+        # Get vars:
+        # net income
+        # gross income
+        # number of individuals
+
+        # for later, Employment Status
+
+        return standard
+
+
+class expected_food_contribution(Variable):
+    value_type = float
+    entity = SPMUnit
+    label = u"Supplemental Nutrition Assistance Program, spm unit's expected food contribution"
+    definition_period = YEAR
+
+    def formula(spm_unit, period, parameters):
 
         # Params:
         # Income Threshold
