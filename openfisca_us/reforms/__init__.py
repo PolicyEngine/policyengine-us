ae22fe1e8ed2a5264264f0ff18cbae86b905c203
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2021-08-21 15:31:07 +0100

    Add UK reform tools

--- openfisca_us/reforms/__init__.py
+++ openfisca_us/reforms/__init__.py
@@ -6,3 +6,4 @@ A reform is a set of modifications to be applied to a reference tax and benefit 
 See https://openfisca.org/doc/key-concepts/reforms.html
 """
 from openfisca_us.reforms.parametric import parametric_reform, reform_from_file
+from openfisca_us.reforms.tools import parametric, structural
