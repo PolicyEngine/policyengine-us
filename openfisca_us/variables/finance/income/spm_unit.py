988d3f38ae23c098f635e6e9054f6064e9a06a1a
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2021-09-20 22:01:14 +0100

    Add poverty threshold

--- openfisca_us/variables/finance/income/spm_unit.py
+++ openfisca_us/variables/finance/income/spm_unit.py
@@ -8,3 +8,22 @@ class SPM_unit_net_income(Variable):
     entity = SPMUnit
     label = u"SPM unit net income"
     definition_period = YEAR
+
+
+class poverty_threshold(Variable):
+    value_type = float
+    entity = SPMUnit
+    label = u"Poverty threshold"
+    definition_period = YEAR
+
+
+class in_poverty(Variable):
+    value_type = bool
+    entity = SPMUnit
+    label = u"In poverty"
+    definition_period = YEAR
+
+    def formula(spm_unit, period, parameters):
+        income = spm_unit("SPM_unit_net_income", period)
+        poverty_threshold = spm_unit("poverty_threshold", period)
+        return income < poverty_threshold
