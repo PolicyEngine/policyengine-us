e36f14df3ad980a78f3a5872e143e287cc07303d
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2021-09-23 19:51:10 +0100

    Fix tuple-ised MARS type

--- openfisca_us/variables/demographic/tax_unit.py
+++ openfisca_us/variables/demographic/tax_unit.py
@@ -27,7 +27,7 @@ class MARSType(Enum):
     SINGLE = "Single"
     JOINT = "Joint"
     SEPARATE = "Separate"
-    HOUSEHOLD_HEAD = ("Head of household",)
+    HOUSEHOLD_HEAD = "Head of household"
     WIDOW = "Widow(er)"
 
 
