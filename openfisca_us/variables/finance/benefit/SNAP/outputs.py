9403f226b697da2cf0b03f4450bb86949a9b449e
nmrodelo <75631529+nmrodelo@users.noreply.github.com>
2021-10-09 10:49:06 -0400

    added intermediate and output .py files, added earnings deduction, snap_standard_deduction, snap_net_income_pre_shelter, and snap_shelter_deduction variables

---
+++ openfisca_us/variables/finance/benefit/SNAP/outputs.py
@@ -0,0 +1,4 @@
+from openfisca_core.model_api import *
+from openfisca_us.entities import *
+from openfisca_us.tools.general import *
+from openfisca_us.variables.entity.household import *  # not sure what I need here
