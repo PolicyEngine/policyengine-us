88c8681ec9279f175e62433e68cc786888df8df2
nmrodelo <75631529+nmrodelo@users.noreply.github.com>
2021-10-09 10:54:55 -0400

    added snap_net_income and snap_expected_contribution_towards_food variables

--- openfisca_us/variables/finance/benefit/SNAP/intermediate.py
+++ openfisca_us/variables/finance/benefit/SNAP/intermediate.py
@@ -1,16 +1,127 @@
 from openfisca_core.model_api import *
 from openfisca_us.entities import *
 from openfisca_us.tools.general import *
-from openfisca_us.variables.entity.household import *  # not sure what I need here
+from openfisca_us.variables.entity.household import *
+from variables.finance.benefit.SNAP.inputs import (
+    gross_income,
+    housing_cost,
+)  # not sure what I need here
 
 
-class shelter_deduction(Variable):
-    
-
-
-class net_income(Variable):
+class snap_earnings_deduction(Variable):
     value_type = int
     entity = household
     definition_period = YEAR
-    documentation
+    documentation = ""
 
+    def formula(spm_unit, period, parameters):
+
+        snap_earnings_deduction = parameters(
+            period
+        ).benefits.SNAP.earnings_deduction
+
+        return spm_unit(gross_income) * snap_earnings_deduction
+
+
+class snap_standard_deduction(Variable):
+    value_type = int
+    entity = household
+    definition_period = YEAR
+    documentation = ""
+
+    def formula(spm_unit, period, parameters):
+
+        standard_deductions = parameters(
+            period
+        ).benefits.SNAP.standard_deduction
+
+        state_group = spm_unit("state_group")
+
+        household_size = spm_unit("household_size")
+
+        return standard_deductions[state_group][household_size]
+
+
+class snap_net_income_pre_shelter(Variable):
+    value_type = int
+    entity = household
+    definition_period = YEAR
+    documentation = ""
+
+    def formula(spm_unit, period, parameters):
+
+        return (
+            spm_unit(gross_income)
+            - spm_unit(snap_standard_deduction)
+            - spm_unit(snap_earnings_deduction)
+        )
+
+
+class snap_shelter_deduction(Variable):
+    value_type = int
+    entity = household
+    definition_period = YEAR
+    documentation = ""
+
+    def formula(spm_unit, period, parameters):
+
+        # check for member of household with disability/elderly status
+
+        max_shelter_deductions = parameters(
+            period
+        ).benefits.SNAP.shelter_deduction
+
+        if elderly_or_disabled == True:
+
+            max_shelter_deductions = (
+                max_shelter_deductions.elderly_or_disabled_exempt
+            )
+
+            return spm_unit(housing_cost) - (
+                0.5 * spm_unit(snap_net_income_pre_shelter)
+            )
+
+        else:
+
+            state_group = spm_unit("state_group")
+
+            # note that the income used below must be SNAP net income - the other adjustments must come first
+
+            if spm_unit(housing_cost) > (
+                0.5 * spm_unit(snap_net_income_pre_shelter)
+            ):
+
+                return min(
+                    (
+                        (
+                            spm_unit(housing_cost)
+                            - (0.5 * spm_unit(snap_net_income_pre_shelter))
+                        ),
+                        max_shelter_deductions[state_group],
+                    )
+                )
+
+
+class snap_net_income(Variable):
+    value_type = int
+    entity = household
+    definition_period = YEAR
+    documentation = ""
+
+    def formula(spm_unit, period, parameters):
+
+        return spm_unit(snap_net_income_pre_shelter) - spm_unit(
+            snap_shelter_deduction
+        )
+
+
+class snap_expected_contribution_towards_food(Variable):
+
+    value_type = int
+    entity = household
+    definition_period = YEAR
+    documentation = ""
+
+    def formula(spm_unit, period, parameters):
+
+        return spm_unit(snap_net_income) * 0.3
