4c46dcb5ac3ce3d5b57dfe3377e1c6b9e9fc3a96
Nikhil <nikhil@policyengine.org>
2021-10-14 19:42:29 +0100

    Get working

--- openfisca_us/variables/finance/benefit/school_meals.py
+++ openfisca_us/variables/finance/benefit/school_meals.py
@@ -1,28 +1,29 @@
 from openfisca_core.model_api import *
 from openfisca_us.entities import *
 from openfisca_us.tools.general import *
-from openfisca_us.variables.entity.spm_unit import *
+from openfisca_us.variables.demographic.spm_unit import *
 
+# add new variable for enum tier
 
 class school_meal_subsidy(Variable):
     value_type = float
-    entity = spm_unit
+    entity = SPMUnit
     definition_period = YEAR
     documentation = ""
 
     def formula(spm_unit, period, parameters):
         # Get state and poverty ratio for SPM unit.
-        state_group = spm_unit("state_group")
-        poverty_ratio = spm_unit("poverty_ratio")
+        state_group = spm_unit("spm_unit_state_group", period)
+        poverty_ratio = spm_unit("poverty_ratio", period)
         # Get parameters.
-        p_school_meals = parameters(period).benefits.school_meals
+        p_school_meals = parameters(period).benefit.school_meals
         p_income_limit = p_school_meals.income_limit
         # Look up the free/reduced/full subsidy tier for each SPM unit by
         # poverty ratio.
-        tier = where_(
+        tier = where(
             poverty_ratio < p_income_limit["free"],
             "free",
-            where_(
+            where(
                 poverty_ratio < p_income_limit["reduced_price"],
                 "reduced_price",
                 "paid",
@@ -30,13 +31,13 @@ class school_meal_subsidy(Variable):
         )
         p_amount = p_school_meals.amount
         # Get NSLP and SBP per child for each SPM unit.
-        nslp_per_child = p_amount["NSLP"][state_group][price_level]
-        sbp_per_child = p_amount["SBP"][state_group][price_level]
+        nslp_per_child = p_amount.NSLP[state_group][tier]
+        sbp_per_child = p_amount.SBP[state_group][tier]
         # Add NSLP and SBP.
         school_meal_subsidy_per_child = nslp_per_child + sbp_per_child
         # Multiply by number of school days in the year and number of children.
         return (
             school_meal_subsidy_per_child
-            * spm_unit("children")
+            * spm_unit("children", period)
             * p_school_meals.school_days
         )
