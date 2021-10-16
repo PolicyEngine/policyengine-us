4c50bc45dc9f39e83dcbd7779c4f14d1e00ab01a
Max Ghenis <mghenis@gmail.com>
2021-10-16 11:54:43 -0700

    Fix poverty parameter

--- openfisca_us/variables/finance/income/benefit/poverty.py
+++ openfisca_us/variables/finance/income/benefit/poverty.py
@@ -12,7 +12,7 @@ class spm_unit_poverty_guideline(Variable):
     def formula(spm_unit, period, parameters):
         n = spm_unit.nb_persons()
         state_group = spm_unit("spm_unit_state_group", period)
-        p_fpg = parameters(period).benefit.poverty.fpg
+        p_fpg = parameters(period).poverty.fpg
         p1 = p_fpg.first_person[state_group]
         pn = p_fpg.additional_person[state_group]
         return p1 + pn * (n - 1)
