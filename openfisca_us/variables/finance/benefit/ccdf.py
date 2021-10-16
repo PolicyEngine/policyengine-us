6d23fc39981075868bbeed9d6f3c8a28c8d68421
Tola Ouk <tolaouk@gmail.com>
2021-10-16 15:05:28 -0500

    add spm_unit asset variable

--- openfisca_us/variables/finance/benefit/ccdf.py
+++ openfisca_us/variables/finance/benefit/ccdf.py
@@ -16,12 +16,12 @@ class ccdf_initial_eligible(Variable):
         # Get state and poverty ratio for SPM unit.
         state = spm_unit("spm_unit_state", period)
         poverty_ratio = spm_unit("poverty_ratio", period)
-        assets = spm_unit("assets", period)
+        assets = spm_unit("spm_unit_assets", period)
         # Get parameters.
         p_ccdf_elig = parameters(period).benefit.CCDF.eligibility
         p_income_limit = p_ccdf_elig.initial_income[state]
         p_asset_limit = p_ccdf_elig.assets[state]
-
+        # Determine eligibility by income (% poverty) and assets.
         income_elig = poverty_ratio <= p_income_limit
         assets_elig = assets <= p_asset_limit
         return income_elig & assets_elig
