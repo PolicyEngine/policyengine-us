001d46165bc2790a32636aa30742ad076f93401e
Tola Ouk <tolaouk@gmail.com>
2021-11-13 16:47:28 -0600

    update test and formula

--- openfisca_us/variables/finance/benefit/ccdf.py
+++ openfisca_us/variables/finance/benefit/ccdf.py
@@ -24,11 +24,11 @@ class ccdf_market_rate(Variable):
     definition_period = ETERNITY
 
     def formula(person, period, parameters):
-        county_cluster = person(ccdf_county_cluster(county, period))
-        provider_type_group = person(provider_type_group, period)
-        child_age_group = person(ccdf_age_group, period)
-        duration_of_care = person(duration_of_care, period)
-        market_rate_mapping = parameters(period).benifit.ccdf.amount
+        county_cluster = person("ccdf_county_cluster", period)
+        provider_type_group = person("provider_type_group", period)
+        child_age_group = person("ccdf_age_group", period)
+        duration_of_care = person("duration_of_care", period)
+        market_rate_mapping = parameters(period).benefit.ccdf.amount
         return market_rate_mapping[county_cluster][provider_type_group][
             child_age_group
         ][duration_of_care]
