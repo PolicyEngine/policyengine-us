e0357ec14f04aea9eb4e93fb36ad60d4299ec662
Nikhil <nikhil@policyengine.org>
2021-11-20 20:07:23 +0000

    Add income definitions

--- openfisca_us/variables/finance/income/person.py
+++ openfisca_us/variables/finance/income/person.py
@@ -3,13 +3,46 @@ from openfisca_us.entities import *
 from openfisca_us.tools.general import *
 
 
+class market_income(Variable):
+    value_type = float
+    entity = Person
+    label = "Market income"
+    unit = "currency-USD"
+    documentation = "Income from non-benefit sources"
+    definition_period = YEAR
+
+
+class gross_income(Variable):
+    value_type = float
+    entity = Person
+    label = "Gross income"
+    unit = "currency-USD"
+    documentation = "Combined market and benefit income"
+    definition_period = YEAR
+
+
+class tax(Variable):
+    value_type = float
+    entity = Person
+    label = "Tax"
+    unit = "currency-USD"
+    documentation = "Total tax liability"
+    definition_period = YEAR
+
+
+class benefits(Variable):
+    value_type = float
+    entity = Person
+    label = "Benefits"
+    unit = "currency-USD"
+    documentation = "Total benefit entitlement"
+    definition_period = YEAR
+
+
 class net_income(Variable):
     value_type = float
     entity = Person
-    label = u"Net income"
+    label = "Net income"
+    unit = "currency-USD"
+    documentation = "Disposable income after taxes and transfers"
     definition_period = YEAR
-
-    def formula(person, period, parameters):
-        return person("is_spm_unit_head", period) * person.spm_unit(
-            "spm_unit_net_income", period
-        )
