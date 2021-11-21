6ab179a858980aa1852eaceb25a3973e6c27b55f
Yv V <yvvijay121@gmail.com>
2021-11-21 12:26:16 -0600

    made changes to tanf.py, person to spm_unit

--- openfisca_us/variables/finance/benefit/tanf.py
+++ openfisca_us/variables/finance/benefit/tanf.py
@@ -1,35 +1,35 @@
 from openfisca_core.model_api import *
 from openfisca_us.entities import *
 from openfisca_us.tools.general import *
-from openfisca_us.variables.demographic.person import *
+from openfisca_us.variables.demographic.spm_unit import *
 
 
 class tanf(Variable):
     value_type = float
-    entity = Family
+    entity = SPMUnit
     definition_period = MONTH
     label = "Temporary Assistance for Needy Families"
     documentation = "Amount of Temporary Assistance for Needy Families benefit received."
     unit = "currency-USD"
 
-    def formula(person, period, parameters):
+    def formula(spm_unit, period, parameters):
         # Obtain eligibility.
-        eligible = person("is_tanf_eligible", period)
+        eligible = spm_unit("is_tanf_eligible", period)
         # Obtain amount they would receive if they were eligible.
-        amount_if_eligible = person("tanf_amount_if_eligible", period)
+        amount_if_eligible = spm_unit("tanf_amount_if_eligible", period)
         return where(eligible, amount_if_eligible, 0)
 
 
 class is_tanf_eligible(Variable):
     value_type = bool
-    entity = Family
+    entity = SPMUnit
     definition_period = MONTH
     label = "Eligibility for Temporary Assistance for Needy Families"
     documentation = "Whether the family is eligible for Temporary Assistance for Needy Families benefit."
 
 class tanf_amount_if_eligible(Variable):
     value_type = float
-    entity = Family
+    entity = SPMUnit
     definition_period = MONTH
     label = "Temporary Assistance for Needy Families amount if family is eligible"
     documentation = "How much a family would receive if they were eligible for Temporary Assistance for Needy Families benefit."
