2992b1dcb6ae5fb72454d1dcd1ac58bb3cf886c0
Max Ghenis <mghenis@gmail.com>
2021-11-23 11:23:07 -0800

    ditto

--- openfisca_us/variables/finance/benefit/spm_unit.py
+++ openfisca_us/variables/finance/benefit/spm_unit.py
@@ -3,12 +3,12 @@ from openfisca_us.entities import *
 from openfisca_us.tools.general import *
 
 
-class poverty_ratio(Variable):
+class spm_unit_fpg_ratio(Variable):
     value_type = float
     entity = SPMUnit
-    label = u"poverty_ratio"
+    label = u"SPM unit FPG ratio"
+    description = "SPM unit's "
     definition_period = YEAR
-
     def formula(spm_unit, period):
         return spm_unit("spm_unit_net_income", period) / spm_unit(
             "poverty_threshold", period
