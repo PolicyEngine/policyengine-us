08a47e88bd06471127662d66517a98d45d46b522
Nikhil <nikhil@policyengine.org>
2021-11-24 11:38:05 +0000

    Add str fix (for now)

--- openfisca_us/variables/demographic/household.py
+++ openfisca_us/variables/demographic/household.py
@@ -137,7 +137,21 @@ class state_group(Variable):
         )
 
 
+class state_group_str(Variable):
+    value_type = str
+    entity = Household
+    label = "State group (string)"
+    unit = "currency-USD"
+    documentation = "State group variable, stored as a string"
+    definition_period = YEAR
+
+    def formula(household, period, parameters):
+        return household("state_group", period).decode_to_str()
+
+
 # Counties in NY state
+
+
 class County(Enum):
     ALBANY_NY = "Albany, NY"
     ALLEGANY_NY = "Allegany, NY"
