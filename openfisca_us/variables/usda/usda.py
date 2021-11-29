d3d5b4bafb566f41fc8cb0e685b150d208d473cd
nmrodelo <nmrodelo@bu.edu>
2021-11-29 15:01:52 -0500

    update variable name in usda.py

--- openfisca_us/variables/usda/usda.py
+++ openfisca_us/variables/usda/usda.py
@@ -3,7 +3,7 @@ from openfisca_us.entities import *
 from openfisca_us.tools.general import *
 
 
-class is_usda_elderly(Variable):
+class is_usda_disabled(Variable):
     value_type = bool
     entity = Person
     definition_period = YEAR
