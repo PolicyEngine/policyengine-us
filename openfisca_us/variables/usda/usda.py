157d9610f1f20925ba177da4c171a4eaa52826f1
nmrodelo <nmrodelo@bu.edu>
2021-11-29 21:11:04 -0500

    replace elderly_age with elderly.age

--- openfisca_us/variables/usda/usda.py
+++ openfisca_us/variables/usda/usda.py
@@ -11,5 +11,5 @@ class is_usda_elderly(Variable):
     label = ""
 
     def formula(person, period, parameters):
-        min_age = parameters(period).usda.elderly_age
+        min_age = parameters(period).usda.elderly.age
         return person("age", period) >= min_age
