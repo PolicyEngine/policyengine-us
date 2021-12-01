a85d9a559c974dff46b2d0f6fb0c217569f68bfb
nmrodelo <75631529+nmrodelo@users.noreply.github.com>
2021-12-01 12:14:36 -0500

    Update openfisca_us/variables/demographic/household.py
    Co-authored-by: Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>

--- openfisca_us/variables/demographic/household.py
+++ openfisca_us/variables/demographic/household.py
@@ -231,5 +231,5 @@ class is_homeless(Variable):
     value_type = bool
     entity = Household
     definition_period = YEAR
-    documentation = ""
+    documentation = "Whether all members are homeless individuals and are not receiving free shelter throughout the month"
     label = ""
