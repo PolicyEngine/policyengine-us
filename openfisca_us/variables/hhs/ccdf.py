37822946822d20d44e5cfb0786bd2f7d846bb9c6
Tola Ouk <tolaouk@gmail.com>
2021-12-02 18:53:37 -0600

    update ccdf.py

--- openfisca_us/variables/hhs/ccdf.py
+++ openfisca_us/variables/hhs/ccdf.py
@@ -174,37 +174,6 @@ class ProviderTypeGroup(Enum):
     LE_ENH = "Informal child care enhanced rate"
 
 
-class provider_type_group(Variable):
-    value_type = Enum
-    possible_values = ProviderTypeGroup
-    # DCC_SACC is most common among provider types
-    default_value = ProviderTypeGroup.DCC_SACC
-    entity = Person
-    label = u"CCDF provider type group"
-    definition_period = YEAR
-
-
-class childcare_hours_per_week(Variable):
-    value_type = float
-    entity = Person
-    label = u"Child care hours per week"
-    definition_period = YEAR
-
-
-class childcare_hours_per_day(Variable):
-    value_type = float
-    entity = Person
-    label = u"Child care hours per day"
-    definition_period = YEAR
-
-
-class childcare_days_per_week(Variable):
-    value_type = float
-    entity = Person
-    label = u"Child care days per week"
-    definition_period = YEAR
-
-
 class DurationOfCare(Enum):
     WEEKLY = "Weekly"
     DAILY = "Daily"
