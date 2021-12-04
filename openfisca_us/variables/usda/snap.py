298dfcd5696d93413b5bc621c04e42b4e063e5b6
Max Ghenis <mghenis@gmail.com>
2021-12-04 13:14:45 -0800

    Add comments to snap_minimum_benefit function

--- openfisca_us/variables/usda/snap.py
+++ openfisca_us/variables/usda/snap.py
@@ -12,13 +12,17 @@ class snap_minimum_benefit(Variable):
     label = "Minimum benefit for SNAP"
 
     def formula(spm_unit, period, parameters):
+        # Parameters for the minimum benefit.
         min_benefit = parameters(period).usda.snap.minimum_benefit
-        household_size = spm_unit.nb_persons()
+        # Calculate the relevant maximum benefit, defined as the maximum
+        # benefit for a household of a certain size in their state.
         snap_max_benefits = parameters(period).usda.snap.amount.main
         state_group = spm_unit.household("state_group_str", period)
         relevant_max_benefit = snap_max_benefits[state_group][
             min_benefit.relevant_max_benefit_household_size
         ]
+        # Minimum benefits only apply to households up to a certain size.
+        household_size = spm_unit.nb_persons()
         min_share_of_max = where(
             household_size <= min_benefit.maximum_household_size,
             min_benefit.rate,
