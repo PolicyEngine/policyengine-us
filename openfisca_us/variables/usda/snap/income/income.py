f8ed79f49a7130fa5731cab1221057e58edf2a89
Nikhil <nikhil@policyengine.org>
2021-12-13 01:39:14 +0000

    Add reference and round down as per law

--- openfisca_us/variables/usda/snap/income/income.py
+++ openfisca_us/variables/usda/snap/income/income.py
@@ -21,8 +21,9 @@ class snap_net_income(Variable):
     documentation = "Final net income, after all deductions"
     label = "SNAP net income"
     unit = "currency-USD"
+    reference = "https://www.law.cornell.edu/uscode/text/7/2014"
 
     def formula(spm_unit, period):
         gross_income = spm_unit("snap_gross_income", period)
         deductions = spm_unit("snap_deductions", period)
-        return max_(0, gross_income - deductions)
+        return np.floor(max_(0, gross_income - deductions))
