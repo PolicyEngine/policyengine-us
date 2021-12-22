2d9be0e65eed03c79eff15c4c07536121d840f77
Max Ghenis <mghenis@gmail.com>
2021-12-21 22:17:06 -0800

    Add ebb variable

---
+++ openfisca_us/variables/fcc/ebb/ebb.py
@@ -0,0 +1,15 @@
+from openfisca_us.model_api import *
+
+class ebb(Variable):
+    value_type = float
+    entity = SPMUnit
+    label = "Emergency Broadband Benefit amount"
+    description = "Emergency Broadband Benefit amount"
+    definition_period = YEAR
+
+    def formula(spm_unit, period, parameters):
+        eligible = spm_unit("is_ebb_eligible", period)
+        broadband_cost = spm_unit("broadband_cost", period)
+        tribal = spm_unit.household("is_on_tribal_land", period)
+        amount = parameters(period).fcc.ebb.amount
+        return where(eligible, min_(broadband_cost, amount[tribal]), 0) * 12
