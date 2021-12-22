380aaf3b03b8e47edf04724cc8135d9ef8315738
Max Ghenis <mghenis@gmail.com>
2021-12-22 15:39:15 -0800

    Fix some test issues

--- openfisca_us/variables/fcc/lifeline/is_lifeline_eligible.py
+++ openfisca_us/variables/fcc/lifeline/is_lifeline_eligible.py
@@ -14,7 +14,7 @@ class is_lifeline_eligible(Variable):
         categorically_eligible = np.any(
             [spm_unit(program, period) for program in programs]
         )
-        fpg_ratio = spm_unit("fcc_fpg_ratio")
-        fpg_threshold = parameters(period).fcc.lifeline.fpg_threshold
-        fpg_eligible = fpg_ratio <= fpg_threshold
+        fpg_ratio = spm_unit("fcc_fpg_ratio", period)
+        fpg_limit = parameters(period).fcc.lifeline.fpg_limit
+        fpg_eligible = fpg_ratio <= fpg_limit
         return categorically_eligible | fpg_eligible
