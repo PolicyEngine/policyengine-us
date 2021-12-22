e163eb07018d5bbd1579afb0535c42f133a64b9d
Max Ghenis <mghenis@gmail.com>
2021-12-21 22:13:11 -0800

    Add is_ebb_eligible variable

---
+++ openfisca_us/variables/fcc/ebb/is_ebb_eligible.py
@@ -0,0 +1,13 @@
+from openfisca_us.model_api import *
+
+class is_ebb_eligible(Variable):
+    value_type = bool
+    entity = SPMUnit
+    label = "Eligible for Emergency Broadband Benefit"
+    description = "Eligible for Emergency Broadband Benefit"
+    definition_period = YEAR
+
+    def formula(spm_unit, period, parameters):
+        programs = parameters(period).fcc.ebb.categorical_eligibility
+
+        return np.any([spm_unit(program, period) for program in programs])
