c8b72d2378bf29c1a78ddb6138b624448a9367e9
nmrodelo <nmrodelo@bu.edu>
2021-12-26 19:30:19 -0500

    add is_snap_eligible variable formula

--- openfisca_us/variables/usda/snap/is_snap_eligible.py
+++ openfisca_us/variables/usda/snap/is_snap_eligible.py
@@ -10,3 +10,27 @@ class is_snap_eligible(Variable):
     definition_period = YEAR
     reference = "https://www.law.cornell.edu/uscode/text/7/2017#a"
     default_value = True
+
+    def formula(spm_unit, period, parameters):
+
+        # per the law, this test is applied before any of the deductions are applied to snap gross income
+        # we are currently not modeling the possibility of permanently disabled individuals forming a second
+        # sub-household
+        gross_limit = parameters(period).usda.snap.income_limits.gross.standard
+        net_limit = parameters(period).usda.snap.income_limits.net
+
+        gross_income = spm_unit("snap_gross_income", period)
+        net_income = spm_unit("snap_net_income", period)
+
+        has_elderly_disabled = spm_unit.household(
+            "has_elderly_disabled", period
+        )
+
+        meets_net_income_limit = net_income < net_limit
+        meets_gross_income_limit = gross_income < gross_limit
+
+        exempt_from_gross_income_limit = has_elderly_disabled
+
+        return meets_net_income_limit & (
+            meets_gross_income_limit | exempt_from_gross_income_limit
+        )
