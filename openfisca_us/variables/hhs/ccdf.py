1900fc72f4dbf97b53834f9b6017d24d91171b22
Tola Ouk <tolaouk@gmail.com>
2021-12-30 19:54:31 -0600

    Add ccdf_subsidy variables

--- openfisca_us/variables/hhs/ccdf.py
+++ openfisca_us/variables/hhs/ccdf.py
@@ -245,3 +245,31 @@ class duration_of_care(Variable):
                 DurationOfCare.HOURLY,
             ],
         )
+
+
+class spm_unit_total_childcare_market_rate(Variable):
+    value_type = float
+    entity = SPMUnit
+    definition_period = YEAR
+    label = u"SPM unit total childcare market rate"
+
+
+class spm_unit_total_ccdf_copay(Variable):
+    value_type = float
+    entity = SPMUnit
+    definition_period = YEAR
+    label = u"SPM unit total ccdf copay"
+
+
+class spm_unit_ccdf_subsidy(Variable):
+    value_type = float
+    entity = SPMUnit
+    definition_period = YEAR
+    label = u"SPM unit ccdf subsidy"
+
+    def formula(spm_unit, period, parameters):
+        total_market_rate = spm_unit(
+            "spm_unit_total_childcare_market_rate", period
+        )
+        total_copay = spm_unit("spm_unit_total_ccdf_copay", period)
+        return total_market_rate - total_copay
