be7a669f0e7a653e1e56b4162767aed16d71932c
Yv V <yvvijay121@gmail.com>
2022-01-02 12:50:20 -0600

    Added tanf_initial_employment_deduction variable in tanf.py Added initial_employment_deduction.yaml parameter Added TANF IL integration tests
    Co-authored-by: Max Ghenis <mghenis@gmail.com>

--- openfisca_us/variables/hhs/tanf.py
+++ openfisca_us/variables/hhs/tanf.py
@@ -95,6 +95,21 @@ class tanf_max_amount(Variable):
         return max_amount[state][family_size] * 12
 
 
+class tanf_initial_employment_deduction(Variable):
+    value_type = int
+    entity = SPMUnit
+    definition_period = YEAR
+    label = "TANF IED (Initial Employment Deduction)"
+    documentation = "The amount deducted from the countable earnings of a TANF application when calculating initial eligibility."
+    unit = USD
+
+    def formula(spm_unit, period, parameters):
+        family_size = spm_unit.nb_persons().astype(str)
+        state = spm_unit.household("state_code_str", period)
+        ied = parameters(period).hhs.tanf.initial_employment_deduction
+        return ied[state][family_size] * 12
+
+
 class tanf_countable_income(Variable):
     value_type = float
     entity = SPMUnit
