9d3806012b3d3d9bc80849309db0cb8f2379dbe8
Max Ghenis <mghenis@gmail.com>
2022-01-02 22:46:50 -0800

    Refactor is_snap_eligible and remove failing integration tests

--- openfisca_us/variables/usda/snap/is_snap_eligible.py
+++ openfisca_us/variables/usda/snap/is_snap_eligible.py
@@ -9,26 +9,18 @@ class is_snap_eligible(Variable):
     documentation = "Whether this SPM unit is eligible for SNAP benefits"
     definition_period = YEAR
     reference = "https://www.law.cornell.edu/uscode/text/7/2017#a; https://www.law.cornell.edu/uscode/text/7/2014#c"
-    default_value = True
 
     def formula(spm_unit, period, parameters):
-
-        # per the law, this test is applied before any of the deductions are applied to snap gross income
-        # we are currently not modeling the possibility of permanently disabled individuals forming a second
-        # sub-household
-        gross_limit = parameters(period).usda.snap.income_limits.gross.standard
-        net_limit = parameters(period).usda.snap.income_limits.net
-
+        income_limits = parameters(period).usda.snap.income_limits
+        # Get income pre- and post-deductions.
         gross_income = spm_unit("snap_gross_income", period)
         net_income = spm_unit("snap_net_income", period)
-
+        # Households with elderly and disabled people are exempt from the
+        # gross income test.
         has_elderly_disabled = spm_unit("has_elderly_disabled", period)
-
-        meets_net_income_limit = net_income < net_limit
-        meets_gross_income_limit = gross_income < gross_limit
-
+        meets_net_income_limit = net_income < income_limits.net
+        meets_gross_income_limit = gross_income < income_limits.gross.standard
         exempt_from_gross_income_limit = has_elderly_disabled
-
         return meets_net_income_limit & (
             meets_gross_income_limit | exempt_from_gross_income_limit
         )
