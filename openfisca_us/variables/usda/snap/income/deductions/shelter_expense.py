ec650d1490cc7c1eeb1c5b39101eb53efa19f3a9
Max Ghenis <mghenis@gmail.com>
2022-01-02 22:10:30 -0800

    Format shelter_expense.py

--- openfisca_us/variables/usda/snap/income/deductions/shelter_expense.py
+++ openfisca_us/variables/usda/snap/income/deductions/shelter_expense.py
@@ -32,11 +32,14 @@ class snap_shelter_deduction(Variable):
 
         has_elderly_disabled = spm_unit("has_elderly_disabled", period)
         # Cap for all but elderly/disabled people.
-        non_homeless_shelter_deduction = where(
-            has_elderly_disabled,
-            uncapped_ded,
-            min_(uncapped_ded * 12, ded_cap * 12),
-        ) + spm_unit("snap_utility_allowance", period)
+        non_homeless_shelter_deduction = (
+            where(
+                has_elderly_disabled,
+                uncapped_ded,
+                12 * min_(uncapped_ded, ded_cap),
+            )
+            + spm_unit("snap_utility_allowance", period)
+        )
         homeless_shelter_deduction = (
             spm_unit("snap_homeless_shelter_deduction", period)
         ) * 12
