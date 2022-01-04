e78d48376fc2e8655345cb4848e01ef21636967e
Max Ghenis <mghenis@gmail.com>
2022-01-03 23:02:00 -0800

    Bump all SNAP tests from 2021 to 2022

--- openfisca_us/variables/hhs/tanf/non_cash/meets_tanf_non_cash_income_test.py
+++ openfisca_us/variables/hhs/tanf/non_cash/meets_tanf_non_cash_income_test.py
@@ -5,12 +5,12 @@ class meets_tanf_non_cash_income_test(Variable):
     value_type = bool
     entity = SPMUnit
     label = "Meets income test for TANF non-cash benefit"
-    documentation = "Income eligibility (as a percent of the poverty line) for TANF non-cash benefit for SNAP BBCE"
+    documentation = "Income eligibility (gross income as a percent of the poverty line) for TANF non-cash benefit for SNAP BBCE"
     definition_period = YEAR
 
     def formula(spm_unit, period, parameters):
         state = spm_unit.household("state_code_str", period)
         limits = parameters(period).hhs.tanf.non_cash
         income_limit_fpg = limits.gross_income_limit_fpg[state]
-        income_fpg = spm_unit("snap_net_income_fpg_ratio", period)
+        income_fpg = spm_unit("snap_gross_income_fpg_ratio", period)
         return income_fpg <= income_limit_fpg
