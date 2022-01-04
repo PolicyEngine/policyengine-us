803cada49fdc8ebdf0cf0750ad775bc5b5f1a552
Max Ghenis <mghenis@gmail.com>
2022-01-03 16:21:15 -0800

    Add variables for SNAP gross/net income : FPG ratios and TANF non-cash asset and income eligibility

--- openfisca_us/variables/hhs/tanf/non_cash/is_tanf_non_cash_eligible.py
+++ openfisca_us/variables/hhs/tanf/non_cash/is_tanf_non_cash_eligible.py
@@ -9,9 +9,6 @@ class is_tanf_non_cash_eligible(Variable):
     definition_period = YEAR
 
     def formula(spm_unit, period, parameters):
-        assets = spm_unit("snap_assets", period)
-        state = spm_unit.household("state_code_str", period)
-        limits = parameters(period).hhs.tanf.non_cash
-        asset_limit = limits.asset_limit[state]
-        income_limit_fpg = limits.gross_income_limit_fpg[state]
-        return assets <= asset_limit
+        income_eligible = spm_unit("meets_tanf_non_cash_income_test", period)
+        asset_eligible = spm_unit("meets_tanf_non_cash_asset_test", period)
+        return income_eligible & asset_eligible
