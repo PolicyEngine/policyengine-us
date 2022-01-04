bb4ce77512f27010f25913804bdbad1db7675d1b
Max Ghenis <mghenis@gmail.com>
2022-01-03 22:24:07 -0800

    Use new meets_snap*_test in is_snap_eligible formula

--- openfisca_us/variables/usda/snap/eligibility/is_snap_eligible.py
+++ openfisca_us/variables/usda/snap/eligibility/is_snap_eligible.py
@@ -13,23 +13,11 @@ class is_snap_eligible(Variable):
     )
 
     def formula(spm_unit, period, parameters):
-        income_limits = parameters(period).usda.snap.income_limits
-        fpg = spm_unit("spm_unit_fpg", period)
-        net_income_limit = income_limits.net * fpg
-        gross_income_limit = income_limits.gross.standard * fpg
-        # Get income pre- and post-deductions.
-        gross_income = spm_unit("snap_gross_income", period)
-        net_income = spm_unit("snap_net_income", period)
-        # Households with elderly and disabled people are exempt from the
-        # gross income test.
-        has_elderly_disabled = spm_unit("has_elderly_disabled", period)
-        meets_net_income_test = net_income < net_income_limit
-        meets_gross_income_test = gross_income < gross_income_limit
-        exempt_from_gross_income_limit = has_elderly_disabled
-        meets_asset_test = spm_unit("meets_snap_asset_test", period)
-        meets_gross_income_test = (
-            meets_gross_income_test | exempt_from_gross_income_limit
+        meets_net_income_test = spm_unit("meets_snap_net_income_test", period)
+        meets_gross_income_test = spm_unit(
+            "meets_snap_gross_income_test", period
         )
+        meets_asset_test = spm_unit("meets_snap_asset_test", period)
         normal_eligibility = (
             meets_net_income_test & meets_asset_test & meets_gross_income_test
         )
