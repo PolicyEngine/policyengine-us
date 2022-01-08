c21c27a1aa860551ebaa95cdc2bf8d5773567ffc
Max Ghenis <mghenis@gmail.com>
2022-01-07 21:28:49 -0800

    Add formula from market income to tanf gross income

--- openfisca_us/variables/hhs/tanf/cash/tanf_total_gross_income.py
+++ openfisca_us/variables/hhs/tanf/cash/tanf_total_gross_income.py
@@ -9,3 +9,6 @@ class tanf_total_gross_income(Variable):
     documentation = "Gross income for calculating Temporary Assistance for Needy Families benefit. Includes both gross earned and unearned income."
     unit = USD
     reference = "https://www.dhs.state.il.us/page.aspx?item=15814"
+
+    def formula(spm_unit, period, parameters):
+        return spm_unit.sum(spm_unit.members("market_income", period))
