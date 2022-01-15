cb6a90e3275e08efcdb932179d24cfddc982a12b
Max Ghenis <mghenis@gmail.com>
2022-01-14 22:35:37 -0800

    fix spm_unit in ssi

--- openfisca_us/variables/ssa/ssi/ssi.py
+++ openfisca_us/variables/ssa/ssi/ssi.py
@@ -9,9 +9,9 @@ class ssi(Variable):
     label = "Supplemental Security Income"
     unit = USD
 
-    def formula(person, period, parameters):
+    def formula(spm_unit, period, parameters):
         # Obtain eligibility.
-        eligible = person("is_ssi_eligible", period)
+        eligible = spm_unit("is_ssi_eligible", period)
         # Obtain amount they would receive if they were eligible.
-        amount_if_eligible = person("ssi_amount_if_eligible", period)
+        amount_if_eligible = spm_unit("ssi_amount_if_eligible", period)
         return where(eligible, amount_if_eligible, 0)
