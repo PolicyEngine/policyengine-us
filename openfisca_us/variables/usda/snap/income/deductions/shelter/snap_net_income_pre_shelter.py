2e4ae8ef1a5b246b4343e3a68b4e5c9a71bcfb88
Max Ghenis <mghenis@gmail.com>
2022-01-15 20:15:23 -0800

    Rename snap_earnings_deduction to snap_earned_income_deduction for alignment with legislation

--- openfisca_us/variables/usda/snap/income/deductions/shelter/snap_net_income_pre_shelter.py
+++ openfisca_us/variables/usda/snap/income/deductions/shelter/snap_net_income_pre_shelter.py
@@ -12,6 +12,9 @@ class snap_net_income_pre_shelter(Variable):
 
     def formula(spm_unit, period):
         income = spm_unit("snap_gross_income", period)
-        DEDUCTIONS = ("snap_standard_deduction", "snap_earnings_deduction")
+        DEDUCTIONS = (
+            "snap_standard_deduction",
+            "snap_earned_income_deduction",
+        )
         deductions = add(spm_unit, period, *DEDUCTIONS)
         return max_(income - deductions, 0)
--- openfisca_us/variables/usda/snap/income/deductions/snap_earned_income_deduction.py
+++ openfisca_us/variables/usda/snap/income/deductions/snap_earned_income_deduction.py
@@ -1,12 +1,14 @@
 from openfisca_us.model_api import *
 
 
-class snap_earnings_deduction(Variable):
+class snap_earned_income_deduction(Variable):
     value_type = float
     entity = SPMUnit
     definition_period = YEAR
-    documentation = "Earnings deduction for calculating SNAP benefit amount"
-    label = "SNAP earnings deduction"
+    documentation = (
+        "Earned income deduction for calculating SNAP benefit amount"
+    )
+    label = "SNAP earned income deduction"
     unit = USD
     reference = "https://www.law.cornell.edu/uscode/text/7/2014#e_2"
 
