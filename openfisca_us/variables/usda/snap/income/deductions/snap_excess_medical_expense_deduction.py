bc1b88f42dd15c691fe1c96cd8ccfcdeee641059
Max Ghenis <mghenis@gmail.com>
2022-01-15 16:10:11 -0800

    Add initial logic for moop deduction, no SMD

--- openfisca_us/variables/usda/snap/income/deductions/snap_excess_medical_expense_deduction.py
+++ openfisca_us/variables/usda/snap/income/deductions/snap_excess_medical_expense_deduction.py
@@ -11,3 +11,14 @@ class snap_excess_medical_expense_deduction(Variable):
     )
     definition_period = YEAR
     reference = "https://www.law.cornell.edu/uscode/text/7/2014#e_5"
+
+    def formula(spm_unit, period, parameters):
+        # Deduction applies to medical expenses incurred by elderly or disabled
+        # members only.
+        person = spm_unit.members
+        elderly = person("is_usda_elderly", period)
+        disabled = person("is_usda_disabled", period)
+        moop = person("medical_out_of_pocket_expenses", period)
+        elderly_disabled_moop = spm_unit.sum(moop * (elderly | disabled))
+        p = parameters(period).usda.snap.deductions.excess_medical_expense
+        return max_(elderly_disabled_moop - p.threshold, 0)
