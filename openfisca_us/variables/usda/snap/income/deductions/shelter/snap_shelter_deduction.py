71e15ecb133ec6cb44e8b17e4281736ad08be0d8
Max Ghenis <mghenis@gmail.com>
2022-01-15 20:53:21 -0800

    Refactor snap_shelter_deduction and remove snap_homeless_shelter_deduction variable

--- openfisca_us/variables/usda/snap/income/deductions/shelter/snap_shelter_deduction.py
+++ openfisca_us/variables/usda/snap/income/deductions/shelter/snap_shelter_deduction.py
@@ -6,45 +6,34 @@ class snap_shelter_deduction(Variable):
     entity = SPMUnit
     definition_period = YEAR
     documentation = (
-        "Excess shelter deduction for calculating SNAP benefit amount"
+        "Excess shelter expense deduction for calculating SNAP benefit amount"
     )
     label = "SNAP shelter deduction"
     reference = ("United States Code, Title 7, Section 2014(e)(6)",)
     unit = USD
 
     def formula(spm_unit, period, parameters):
-        shelter_deduction = parameters(period).usda.snap.deductions.shelter
-
+        p = parameters(period).usda.snap.deductions.shelter
         # Calculate uncapped shelter deduction as housing costs in excess of
-        # income threshold
-        uncapped_ded = max_(
-            spm_unit("housing_cost", period)
-            - (
-                shelter_deduction.income_share_threshold
-                * spm_unit("snap_net_income_pre_shelter", period)
-            ),
-            0,
+        # income threshold.
+        net_income_pre_shelter = spm_unit(
+            "snap_net_income_pre_shelter", period
         )
-
-        # Index maximum shelter deduction by state group.
+        subtracted_income = p.income_share_threshold * net_income_pre_shelter
+        housing_cost = spm_unit("snap_housing_cost", period)
+        uncapped_ded = max_(housing_cost - subtracted_income, 0)
+        # Calculate capped deduction based on state group parameter.
         state_group = spm_unit.household("state_group_str", period)
-        ded_cap = shelter_deduction.cap[state_group]
-
+        ded_cap = p.cap[state_group]
+        capped_ded = min_(uncapped_ded, ded_cap)
         has_elderly_disabled = spm_unit("has_usda_elderly_disabled", period)
-        # Cap for all but elderly/disabled people.
-        non_homeless_shelter_deduction = (
-            where(
-                has_elderly_disabled,
-                uncapped_ded,
-                12 * min_(uncapped_ded, ded_cap),
-            )
-            + spm_unit("snap_utility_allowance", period)
-        )
-        homeless_shelter_deduction = (
-            spm_unit("snap_homeless_shelter_deduction", period)
-        ) * 12
-        return where(
+        # Cap for all but elderly/disabled people and add utility allowance.
+        non_homeless_shelter_deduction = where(
+            has_elderly_disabled, uncapped_ded, capped_ded
+        ) + spm_unit("snap_utility_allowance", period)
+        # Homeless shelter deduction is flat and has no utility component.
+        return 12 * where(
             spm_unit.household("is_homeless", period),
-            homeless_shelter_deduction,
+            p.homeless,
             non_homeless_shelter_deduction,
         )
