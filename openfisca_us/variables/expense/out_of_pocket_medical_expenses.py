6bd94729bf8344d018b4398f235c150cc1417fbc
Max Ghenis <mghenis@gmail.com>
2022-01-15 16:21:53 -0800

    Fix variable naming bugs

--- openfisca_us/variables/expense/out_of_pocket_medical_expenses.py
+++ openfisca_us/variables/expense/out_of_pocket_medical_expenses.py
@@ -1,10 +1,10 @@
 from openfisca_us.model_api import *
 
 
-class out_of_pocket_medical_expenses(Variable):
+class medical_out_of_pocket_expenses(Variable):
     value_type = float
     entity = Person
-    label = "Out of pocket medical expenses"
-    documentation = "Person's out of pocket medical expenses"
+    label = "Medical out of pocket expenses"
+    documentation = "Person's medical out of pocket expenses"
     unit = USD
     definition_period = YEAR
