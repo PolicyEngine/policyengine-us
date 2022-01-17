5495890b8a6939dac509e607d8853fc7146c233b
Max Ghenis <mghenis@gmail.com>
2022-01-17 15:03:50 -0800

    Add documentation to childcare_expenses

--- openfisca_us/variables/expense/childcare/childcare_expenses.py
+++ openfisca_us/variables/expense/childcare/childcare_expenses.py
@@ -5,5 +5,6 @@ class childcare_expenses(Variable):
     value_type = float
     entity = SPMUnit
     label = "Child care expenses"
+    documentation = "Out of pocket spending on childcare"
     definition_period = YEAR
     unit = USD
