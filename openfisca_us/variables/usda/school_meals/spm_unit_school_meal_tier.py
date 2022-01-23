21b9a5db9f8c1c9618ba0dde05edf4792277be38
Max Ghenis <mghenis@gmail.com>
2022-01-22 22:24:11 -0800

    Add categorical eligibility criterion to school meal tier variable

--- openfisca_us/variables/usda/school_meals/spm_unit_school_meal_tier.py
+++ openfisca_us/variables/usda/school_meals/spm_unit_school_meal_tier.py
@@ -18,9 +18,13 @@ class spm_unit_school_meal_tier(Variable):
     def formula(spm_unit, period, parameters):
         fpg_ratio = spm_unit("spm_unit_school_meal_fpg_ratio", period)
         p_income_limit = parameters(period).usda.school_meals.income_limit
+        # Categorical eligibility provides free school meals.
+        categorical_eligibility = spm_unit(
+            "meets_school_meal_categorical_eligibility", period
+        )
         return select(
             [
-                fpg_ratio <= p_income_limit.FREE,
+                (fpg_ratio <= p_income_limit.FREE) | categorical_eligibility,
                 fpg_ratio <= p_income_limit.REDUCED,
                 True,
             ],
