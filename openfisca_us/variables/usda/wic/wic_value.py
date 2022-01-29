9975edf0a27662ac1852366bcb254819e0f85dc1
nmrodelo <nmrodelo@bu.edu>
2022-01-28 19:14:56 -0500

    add variable, test, and params for wic value Co-authored-by: Max Ghenis <mghenis@gmail.com>

---
+++ openfisca_us/variables/usda/wic/wic_value.py
@@ -0,0 +1,15 @@
+from openfisca_us.model_api import *
+
+
+class wic_value(Variable):
+    value_type = float
+    entity = Person
+    definition_period = YEAR
+    documentation = "Average benefit value for the Special Supplemental Nutrition Program for Women, Infants and Children (WIC)"
+    label = "WIC value"
+    reference = "https://fns-prod.azureedge.net/sites/default/files/resource-files/WICPC2018FoodPackage-Summary.pdf#page=2"
+
+    def formula(person, period, parameters):
+        values = parameters(period).usda.wic.value
+        category = person("wic_category", period)
+        return values[category]
