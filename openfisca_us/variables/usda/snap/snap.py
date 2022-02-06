a097173607a6eb9f9bc376090127e83255f1c343
Max Ghenis <mghenis@gmail.com>
2022-02-05 22:20:45 -0800

    Fix tests

--- openfisca_us/variables/usda/snap/snap.py
+++ openfisca_us/variables/usda/snap/snap.py
@@ -23,7 +23,7 @@ class snap(Variable):
         ea = parameters(period).usda.snap.emergency_allotment
         state = spm_unit.household("state_code_str", period)
         ea_in_effect = ea.in_effect[state]
-        ea_minimum = ea.minimum[state]
+        ea_minimum = ea.minimum[state] * 12
         ea_amount_if_in_effect = max_(
             ea_minimum, max_allotment - allotment_if_eligible
         )
