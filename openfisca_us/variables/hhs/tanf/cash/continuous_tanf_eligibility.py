e58a75562bc8133ee2112e8f037f59935a365ea4
Yv V <yvvijay121@gmail.com>
2022-02-06 11:50:13 -0600

    continuous_tanf_eligibility formulas & one more integration test

--- openfisca_us/variables/hhs/tanf/cash/continuous_tanf_eligibility.py
+++ openfisca_us/variables/hhs/tanf/cash/continuous_tanf_eligibility.py
@@ -7,3 +7,10 @@ class continuous_tanf_eligibility(Variable):
     definition_period = YEAR
     label = "Continued Economic Eligibility for TANF"
     documentation = "Whether the familiy meets the economic requirements for the Temporary Assistance for Needy Families program after being approved."
+
+    def formula(spm_unit, period, parameters):
+        earned_income = spm_unit("tanf_gross_earned_income", period)
+        unearned_income = spm_unit("tanf_gross_unearned_income", period)
+        net_income = earned_income + unearned_income
+        payment_level = spm_unit("tanf_max_amount", period)
+        return net_income <= payment_level
