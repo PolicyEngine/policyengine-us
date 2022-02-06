fb788abe8232056cc0cfc86868a03c53f261af1b
Max Ghenis <mghenis@gmail.com>
2022-02-05 22:30:15 -0800

    Separate normal and emergency allotments into separate variables

--- openfisca_us/variables/usda/snap/snap.py
+++ openfisca_us/variables/usda/snap/snap.py
@@ -11,21 +11,5 @@ class snap(Variable):
     unit = USD
 
     def formula(spm_unit, period, parameters):
-        # Federal SNAP rules are defined in U.S.C Title 7, Chapter 51, which also
-        # defines state powers to modify the rules.
-        eligible = spm_unit("is_snap_eligible", period)
-        expected_contribution = spm_unit("snap_expected_contribution", period)
-        max_allotment = spm_unit("snap_max_allotment", period)
-        normal_allotment = max_allotment - expected_contribution
-        min_allotment = spm_unit("snap_min_allotment", period)
-        allotment_if_eligible = max_(min_allotment, normal_allotment)
-        # Calculate emergency allotment, which provides all eligible households the maximum.
-        ea = parameters(period).usda.snap.emergency_allotment
-        state = spm_unit.household("state_code_str", period)
-        ea_in_effect = ea.in_effect[state]
-        ea_minimum = ea.minimum[state] * 12
-        ea_amount_if_in_effect = max_(
-            ea_minimum, max_allotment - allotment_if_eligible
-        )
-        ea_amount = ea_in_effect * ea_amount_if_in_effect
-        return eligible * (allotment_if_eligible + ea_amount)
+        ALLOTMENTS = ["snap_normal_allotment", "snap_emergency_allotment"]
+        return add(spm_unit, period, ALLOTMENTS)
