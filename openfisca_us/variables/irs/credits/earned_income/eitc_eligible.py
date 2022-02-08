a6734fc7bf8a7a2390e281bc4d94a376a725067b
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-02-08 16:49:25 +0000

    Formalise EITC and add missing parameters

--- openfisca_us/variables/irs/credits/earned_income/eitc_eligible.py
+++ openfisca_us/variables/irs/credits/earned_income/eitc_eligible.py
@@ -1,18 +1,19 @@
 from openfisca_us.model_api import *
 
+
 class eitc_eligible(Variable):
     value_type = bool
     entity = TaxUnit
     label = "Eligible for EITC"
     definition_period = YEAR
+    reference = "https://www.law.cornell.edu/uscode/text/26/32#c_1_A"
 
     def formula(tax_unit, period, parameters):
         person = tax_unit.members
         has_child = tax_unit.any(person("is_child", period))
         age = person("age", period)
         eitc = parameters(period).irs.credits.eitc
-        meets_age_requirements = (
-            (age >= eitc.eligibility.age.min)
-            & (age <= eitc.eligibility.age.max)
+        meets_age_requirements = (age >= eitc.eligibility.age.min) & (
+            age <= eitc.eligibility.age.max
         )
         return has_child | tax_unit.any(meets_age_requirements)
