f98ec812cde202f5149247ba9a75e1d9641a47eb
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-02-08 14:04:56 +0000

    Add EITC eligibility and test

---
+++ openfisca_us/variables/irs/credits/earned_income/eitc_eligible.py
@@ -0,0 +1,18 @@
+from openfisca_us.model_api import *
+
+class eitc_eligible(Variable):
+    value_type = bool
+    entity = TaxUnit
+    label = "Eligible for EITC"
+    definition_period = YEAR
+
+    def formula(tax_unit, period, parameters):
+        person = tax_unit.members
+        has_child = tax_unit.any(person("is_child", period))
+        age = person("age", period)
+        eitc = parameters(period).irs.credits.eitc
+        meets_age_requirements = (
+            (age >= eitc.eligibility.age.min)
+            & (age <= eitc.eligibility.age.max)
+        )
+        return has_child | tax_unit.any(meets_age_requirements)
