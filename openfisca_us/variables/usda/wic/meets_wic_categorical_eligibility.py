419ef72f01ecab6ab1368abb532f9d5b07dd8e13
Max Ghenis <mghenis@gmail.com>
2022-02-08 22:21:30 -0800

    Add WIC categorical eligibility

--- openfisca_us/variables/usda/wic/meets_wic_categorical_eligibility.py
+++ openfisca_us/variables/usda/wic/meets_wic_categorical_eligibility.py
@@ -9,4 +9,10 @@ class meets_wic_categorical_eligibility(Variable):
         "Meets the program participation eligibility criteria for WIC"
     )
     label = "Meets WIC categorical (program participation) eligibility"
-    reference = "https://www.law.cornell.edu/uscode/text/42/1786#d_2_A_ii"
+    reference = "https://www.law.cornell.edu/uscode/text/42/1786#d_2_A"
+
+    def formula(spm_unit, period, parameters):
+        programs = parameters(period).usda.wic.categorical_eligibility
+        return np.any(
+            [spm_unit(program, period) for program in programs], axis=0
+        )
