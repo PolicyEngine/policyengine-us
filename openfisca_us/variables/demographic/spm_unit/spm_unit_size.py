5fec495c7e7173244b50b02cd7eb402f19bb5bf2
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-02-10 17:20:51 +0000

    Remove numrical enums

--- openfisca_us/variables/demographic/spm_unit/spm_unit_size.py
+++ openfisca_us/variables/demographic/spm_unit/spm_unit_size.py
@@ -9,37 +9,3 @@ class spm_unit_size(Variable):
 
     def formula(spm_unit, period, parameters):
         return spm_unit.nb_persons()
-
-
-class HouseholdSize(Enum):
-    ONE = 1
-    TWO = 2
-    THREE = 3
-    FOUR = 4
-    FIVE = 5
-    SIX = 6
-    SEVEN = 7
-    EIGHT = 8
-    NINE = 9
-    TEN = 10
-    ELEVEN = 11
-    TWELVE = 12
-    THIRTEEN = 13
-    FOURTEEN = 14
-    FIFTEEN = 15
-    SIXTEEN = 16
-    SEVENTEEN = 17
-    EIGHTEEN = 18
-    NINETEEN = 19
-    TWENTY = 20
-
-
-class household_size(Variable):
-    value_type = Enum
-    possible_values = HouseholdSize
-    default_value = HouseholdSize.ONE
-    entity = Household
-    label = "Household size"
-    unit = USD
-    documentation = "Dummy variable, used for parameter nesting"
-    definition_period = ETERNITY
