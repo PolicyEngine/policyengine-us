84eda0b348a66eff43c6d1e1cedce0f52bbbc785
Max Ghenis <mghenis@gmail.com>
2022-02-26 18:23:52 -0800

    Add SSI income variables

--- openfisca_us/variables/ssa/ssi/income/ssi_deemed_income.py
+++ openfisca_us/variables/ssa/ssi/income/ssi_deemed_income.py
@@ -1,1 +1,10 @@
+from openfisca_us.model_api import *
 
+
+class ssi_deemed_income(Variable):
+    value_type = float
+    entity = Person
+    definition_period = YEAR
+    documentation = "Supplemental Security Income deemed income"
+    label = "SSI deemed income"
+    unit = USD
--- openfisca_us/variables/ssa/ssi/income/ssi_earned_income.py
+++ openfisca_us/variables/ssa/ssi/income/ssi_earned_income.py
@@ -1,1 +1,10 @@
+from openfisca_us.model_api import *
 
+
+class ssi_earned_income(Variable):
+    value_type = float
+    entity = Person
+    definition_period = YEAR
+    documentation = "Supplemental Security Income earned income"
+    label = "SSI earned income"
+    unit = USD
