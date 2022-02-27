4a1d7d6d60918b1c29311081e2479102de206a04
Max Ghenis <mghenis@gmail.com>
2022-02-26 21:44:18 -0800

    Add ssi_countable_income formula and test (pass)

--- openfisca_us/variables/ssa/ssi/income/ssi_countable_income.py
+++ openfisca_us/variables/ssa/ssi/income/ssi_countable_income.py
@@ -10,9 +10,19 @@ class ssi_countable_income(Variable):
     unit = USD
 
     def formula(person, period, parameters):
-        # TODO: Add deductions.
-        return add(
-            person,
-            period,
-            ["ssi_earned_income", "ssi_unearned_income", "ssi_deemed_income"],
-        )
+        earned = person("ssi_earned_income", period) / 12
+        unearned = person("ssi_unearned_income", period) / 12
+        deemed = person("ssi_deemed_income", period) / 12
+        # Deduct exclusions.
+        exclusions = parameters(period).ssa.ssi.income.exclusions
+        # Subtract from unearned income first.
+        unearned_exclusion = min_(exclusions.general, unearned)
+        countable_unearned = unearned - unearned_exclusion
+        # Remaining general exclusion is treated as an earned income exclusion.
+        remaining_general_exclusion = exclusions.general - unearned_exclusion
+        earned_exclusion = exclusions.earned + remaining_general_exclusion
+        # Subtract the percentage of earned income above the flat exclusion.
+        earned_after_flat_exclusion = max_(earned - earned_exclusion, 0)
+        countable_earned_share = 1 - exclusions.earned_share
+        countable_earned = earned_after_flat_exclusion * countable_earned_share
+        return (countable_unearned + countable_earned + deemed) * 12
