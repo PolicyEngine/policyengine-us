22d0327cc23fc53331b7d78f657db9e248752ea8
Yv V <yvvijay121@gmail.com>
2022-03-06 08:29:30 -0600

    Added CA income parameters - CA earned income deductions - initial employment deductions

--- openfisca_us/variables/hhs/tanf/cash/tanf_countable_income.py
+++ openfisca_us/variables/hhs/tanf/cash/tanf_countable_income.py
@@ -12,10 +12,15 @@ class tanf_countable_income(Variable):
     def formula(spm_unit, period, parameters):
         earned_income = spm_unit("tanf_gross_earned_income", period)
         state = spm_unit.household("state_code_str", period)
-        earned_income_deduction = parameters(
+        earnings_deductions = parameters(
             period
-        ).hhs.tanf.cash.earned_income_deduction[state]
-        countable_earned_income = earned_income * (1 - earned_income_deduction)
+        ).hhs.tanf.cash.income.deductions.earnings
+        flat_earnings_deduction = earnings_deductions.flat[state]
+        percent_earnings_deduction = earnings_deductions.percent[state]
+        countable_earned_income -= flat_earnings_deduction
+        countable_earned_income -= (
+            countable_earned_income * percent_earnings_deduction
+        )
         # No deduction for unearned income.
         unearned_income = spm_unit("tanf_gross_unearned_income", period)
         return countable_earned_income + unearned_income
