90e5f8a738777cfbd6456a68952d8f7c0c698b04
Max Ghenis <mghenis@gmail.com>
2022-03-25 21:44:55 -0400

    Add gross and net income limits

---
+++ openfisca_us/variables/hhs/tanf/non_cash/meets_tanf_non_cash_net_income_test.py
@@ -0,0 +1,24 @@
+from openfisca_us.model_api import *
+
+
+class meets_tanf_non_cash_net_income_test(Variable):
+    value_type = bool
+    entity = SPMUnit
+    label = "Meets net income test for TANF non-cash benefit"
+    documentation = "Income eligibility (net income as a percent of the poverty line) for TANF non-cash benefit for SNAP BBCE"
+    definition_period = YEAR
+
+    def formula(spm_unit, period, parameters):
+        state = spm_unit.household("state_code_str", period)
+        # All limits and incomes here expressed as % of FPG.
+        limits = parameters(period).hhs.tanf.non_cash.income_limit
+        hheod = spm_unit("is_tanf_non_cash_hheod", period)
+        hheod_net_limit_applies = limits.net_applies.hheod[state]
+        non_hheod_net_limit_applies = limits.net_applies.non_hheod[state]
+        net_limit_applies = where(
+            hheod, hheod_net_limit_applies, non_hheod_net_limit_applies
+        )
+        net_income = spm_unit("snap_net_income_fpg_ratio", period)
+        net_limit = parameters(period).usda.snap.income.limit.net
+        # Either the net limit doesn't apply or they pass it.
+        return ~net_limit_applies | (net_income <= net_limit)
--- openfisca_us/variables/usda/has_all_usda_elderly_disabled.py
+++ openfisca_us/variables/usda/has_all_usda_elderly_disabled.py
@@ -7,6 +7,7 @@ class has_all_usda_elderly_disabled(Variable):
     definition_period = YEAR
     documentation = "Whether the SPM unit's members all meet USDA definitions of elderly or disabled"
     label = "Has all USDA elderly or disabled people"
+    # NB: This isn't used in SNAP directly, but it is used for BBCE (TANF).
 
     def formula(spm_unit, period, parameters):
         person = spm_unit.members
