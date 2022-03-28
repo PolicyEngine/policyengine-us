d0a6a31b159180dc0f1635fd903ca9ddd28592e2
Max Ghenis <mghenis@gmail.com>
2022-03-27 22:11:18 -0700

    Add parameters

---
+++ openfisca_us/variables/demographic/person/is_permanently_and_totally_disabled.py
@@ -0,0 +1,8 @@
+from openfisca_us.model_api import *
+
+
+class is_permanently_and_totally_disabled(Variable):
+    value_type = bool
+    entity = Person
+    label = "Is permanently and totally disabled"
+    definition_period = YEAR
