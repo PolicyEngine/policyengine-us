9f6ab70aeb92a09de1bb8337d3ace760868ba39d
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-03-30 19:45:16 +0100

    Fix Add `is_full_time_college_student` and point `is_full_time_student` to it #644

--- openfisca_us/variables/demographic/person/is_full_time_student.py
+++ openfisca_us/variables/demographic/person/is_full_time_student.py
@@ -6,3 +6,8 @@ class is_full_time_student(Variable):
     entity = Person
     label = "Is a full time student"
     definition_period = YEAR
+
+    def formula(person, period, parameters):
+        in_college = person("is_full_time_college_student", period)
+        in_k12 = person("is_in_k12_school", period)
+        return in_college | in_k12
