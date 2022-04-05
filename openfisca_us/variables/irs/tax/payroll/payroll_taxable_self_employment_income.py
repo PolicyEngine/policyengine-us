53667cf9678173975653db0987e02dc7ac424310
Max Ghenis <mghenis@gmail.com>
2022-04-04 23:22:54 -0700

    Split out taxable self-employment income with a test

--- openfisca_us/variables/irs/tax/payroll/payroll_taxable_self_employment_income.py
+++ openfisca_us/variables/irs/tax/payroll/payroll_taxable_self_employment_income.py
@@ -7,9 +7,14 @@ class payroll_taxable_self_employment_income(Variable):
     label = "Taxable self-employment income"
     definition_period = YEAR
     unit = USD
+    reference = "https://www.law.cornell.edu/uscode/text/26/1402#a"
 
     def formula(person, period, parameters):
-        return max_(
-            0.0,
-            person("sey", period) * person.tax_unit("sey_frac", period),
+        irs = parameters(period).irs
+        combined_rate = (
+            irs.payroll.social_security.rate.self_employment
+            + irs.payroll.medicare.rate.self_employment
         )
+        deduction_rate = irs.ald.misc.employer_share * combined_rate
+        base = max_(person("sey", period), 0)
+        return base * (1 - deduction_rate)
