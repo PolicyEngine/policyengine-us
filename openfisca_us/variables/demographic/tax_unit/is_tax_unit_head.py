9e2ae5c3e83ad68916f042b3751187d0e45a8a07
Max Ghenis <mghenis@gmail.com>
2022-04-15 10:38:27 -0700

    Fix test

--- openfisca_us/variables/demographic/tax_unit/is_tax_unit_head.py
+++ openfisca_us/variables/demographic/tax_unit/is_tax_unit_head.py
@@ -8,11 +8,11 @@ class is_tax_unit_head(Variable):
     definition_period = ETERNITY
 
     def formula(person, period, parameters):
-        # Establish basic conditions
+        # Establish basic conditions.
         eligible = person("age", period) >= 18
-
-        # Of those who meet the criteria, select the first person defined
+        # Of those who meet the criteria, select the first person defined.
         tax_unit = person.tax_unit
+        # NB: tax_unit.members_position is actually person-level.
         rank = where(eligible, tax_unit.members_position, inf)
         first_rank = tax_unit.min(rank)
         return eligible & (rank == first_rank)
--- openfisca_us/variables/demographic/tax_unit/is_tax_unit_spouse.py
+++ openfisca_us/variables/demographic/tax_unit/is_tax_unit_spouse.py
@@ -8,12 +8,13 @@ class is_tax_unit_spouse(Variable):
     definition_period = YEAR
 
     def formula(person, period, parameters):
-        # Establish basic conditions
+        # Establish basic conditions.
         is_adult = person("age", period) >= 18
         is_not_head = ~person("is_tax_unit_head", period)
         eligible = is_adult * is_not_head
-        # Of those who meet the criteria, select the first person defined
+        # Of those who meet the criteria, select the first person defined.
         tax_unit = person.tax_unit
+        # NB: tax_unit.members_position is actually person-level.
         rank = where(eligible, tax_unit.members_position, inf)
         first_rank = tax_unit.min(rank)
         return eligible & (rank == first_rank)
