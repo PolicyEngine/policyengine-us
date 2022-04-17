1cd86eddc7a476c7535cff6d93c00f94d7fbd135
Yv V <yvvijay121@gmail.com>
2022-04-17 12:55:10 -0500

    Fixes: Apply TANF percent earnings deduction to gross earned income #677
    Co-authored-by: Max Ghenis <mghenis@gmail.com>

--- openfisca_us/variables/hhs/tanf/cash/amount/tanf_countable_income.py
+++ openfisca_us/variables/hhs/tanf/cash/amount/tanf_countable_income.py
@@ -10,17 +10,15 @@ class tanf_countable_income(Variable):
     unit = USD
 
     def formula(spm_unit, period, parameters):
-        countable_earned_income = spm_unit("tanf_gross_earned_income", period)
+        gross_earned_income = spm_unit("tanf_gross_earned_income", period)
         state = spm_unit.household("state_code_str", period)
         earnings_deductions = parameters(
             period
         ).hhs.tanf.cash.amount.countable_income.deductions.earnings
         household_earnings_deduction = earnings_deductions.household[state]
         percent_earnings_deduction = earnings_deductions.percent[state]
-        countable_earned_income -= household_earnings_deduction
-        countable_earned_income -= (
-            countable_earned_income * percent_earnings_deduction
-        )
+        total_earnings_deductions = household_earnings_deduction + (gross_earned_income * percent_earnings_deduction)
+        countable_earned_income = gross_earned_income - total_earnings_deductions
         # No deduction for unearned income.
         unearned_income = spm_unit("tanf_gross_unearned_income", period)
         return max_(countable_earned_income + unearned_income, 0)
