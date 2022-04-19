9f2f99f18fc90a940ee451beca56413601744d17
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-04-19 09:49:27 +0100

    Fix dep

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
             "coverage",
             "plotly",
             "yaml-changelog>=0.1.6",
-            "python-us",
+            "us",
         ],
     },
     python_requires=">=3.7,<3.8",
