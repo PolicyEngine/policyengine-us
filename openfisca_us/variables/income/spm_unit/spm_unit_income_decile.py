a36065f4c821bf3f3d08ea042f936bea1347f89c
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-04-21 14:18:24 +0100

    Add person-weighting to decile formula

--- openfisca_us/variables/income/spm_unit/spm_unit_income_decile.py
+++ openfisca_us/variables/income/spm_unit/spm_unit_income_decile.py
@@ -2,15 +2,14 @@ from openfisca_us.model_api import *
 
 
 class spm_unit_income_decile(Variable):
-    value_type = float
+    value_type = int
     entity = SPMUnit
     label = "Income decile"
-    unit = int
-    documentation = "The income decile of the SPM unit, using OECD-equivalised net income"
+    documentation = "The income decile of the SPM unit, person-weighted and using OECD-equivalised net income"
     definition_period = YEAR
 
     def formula(spm_unit, period, parameters):
         from microdf import MicroSeries
         income = spm_unit("spm_unit_oecd_equiv_net_income", period)
-        weights = spm_unit("spm_unit_weight", period)
+        weights = spm_unit("spm_unit_weight", period) * spm_unit.nb_persons()
         return MicroSeries(income, weights=weights).decile_rank()
