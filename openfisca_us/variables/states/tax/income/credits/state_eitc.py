d543eb248b3e92d1316c48f399644eab5795dbc2
Max Ghenis <mghenis@gmail.com>
2022-04-29 13:56:53 -0700

    Add MA EITC

---
+++ openfisca_us/variables/states/tax/income/credits/state_eitc.py
@@ -0,0 +1,15 @@
+from openfisca_us.model_api import *
+
+
+class state_eitc(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "State EITC"
+    unit = USD
+    documentation = "State-level Earned Income Tax Credit"
+    definition_period = YEAR
+
+    def formula(tax_unit, period, parameters):
+        match = parameters(period).states.tax.income.credits.eitc.percent_match
+        state = tax_unit.household("state_code", period)
+        return tax_unit("eitc", period) * match[state]
