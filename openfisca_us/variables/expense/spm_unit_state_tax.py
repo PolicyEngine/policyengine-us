3aee46d30fc3861e95a29df2c386cc762909fa1c
Max Ghenis <mghenis@gmail.com>
2022-04-30 13:19:46 -0700

    add formula for spm state tax

--- openfisca_us/variables/expense/spm_unit_state_tax.py
+++ openfisca_us/variables/expense/spm_unit_state_tax.py
@@ -9,4 +9,10 @@ class spm_unit_state_tax(Variable):
     unit = USD
 
     # state_income_tax is at the tax unit level.
-    # formula = sum_of_variables("state_income_tax")
+    # formula = sum_of_variables(["state_income_tax"])
+
+    def formula(spm_unit, period, parameters):
+        person = spm_unit.members
+        head = person("is_tax_unit_head", period)
+        tax = person.tax_unit("state_income_tax", period)
+        return spm_unit.sum(head * tax)
