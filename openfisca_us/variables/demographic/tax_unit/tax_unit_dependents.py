9fcdafa449b320f31966a4c47cc0c2d673b7bd49
Max Ghenis <mghenis@gmail.com>
2022-04-29 20:28:44 -0700

    Simplify formula

--- openfisca_us/variables/demographic/tax_unit/tax_unit_dependents.py
+++ openfisca_us/variables/demographic/tax_unit/tax_unit_dependents.py
@@ -7,6 +7,4 @@ class tax_unit_dependents(Variable):
     label = "Number of dependents in the tax unit"
     definition_period = YEAR
 
-    def formula(tax_unit, period, parameters):
-        dependent = tax_unit.members("is_tax_unit_dependent", period)
-        return tax_unit.sum(where(dependent, 1, 0))
+    formula = sum_of_variables(["is_tax_unit_dependent"])
