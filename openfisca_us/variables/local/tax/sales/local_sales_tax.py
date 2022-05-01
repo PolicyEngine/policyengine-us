8b131d3e3a30de61b06a08bb2e71edb8f6aaa5de
Max Ghenis <mghenis@gmail.com>
2022-04-30 20:12:59 -0700

    Tie state income tax to SALT deduction #722

---
+++ openfisca_us/variables/local/tax/sales/local_sales_tax.py
@@ -0,0 +1,9 @@
+from openfisca_us.model_api import *
+
+
+class local_sales_tax(Variable):
+    value_type = float
+    entity = TaxUnit
+    definition_period = YEAR
+    label = "Local sales tax"
+    unit = USD
