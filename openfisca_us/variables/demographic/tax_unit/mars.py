a9196c1746e66611ddfbb7227e7ccd8dded23e05
Max Ghenis <mghenis@gmail.com>
2022-05-01 10:14:07 -0700

    Add head of household

--- openfisca_us/variables/demographic/tax_unit/mars.py
+++ openfisca_us/variables/demographic/tax_unit/mars.py
@@ -22,7 +22,11 @@ class mars(Variable):
         spouse_with_age = person("is_tax_unit_spouse", period)
         has_age = person("age", period) > 0
         has_spouse_with_age = tax_unit.any(spouse_with_age & has_age)
-        return where(has_spouse_with_age, MARSType.JOINT, MARSType.SINGLE)
+        has_dependents = add(tax_unit, period, ["is_tax_unit_dependent"]) > 0
+        return select(
+            [has_spouse_with_age, has_dependents, True],
+            [MARSType.JOINT, MARSType.HEAD_OF_HOUSEHOLD, MARSType.SINGLE],
+        )
 
 
 marital_status = variable_alias("marital_status", mars)
