d756d173afc26819b5b83be91f552dba874d99ff
Max Ghenis <mghenis@gmail.com>
2022-05-01 12:36:36 -0700

    fix tests

--- openfisca_us/variables/demographic/tax_unit/mars.py
+++ openfisca_us/variables/demographic/tax_unit/mars.py
@@ -19,12 +19,10 @@ class mars(Variable):
 
     def formula(tax_unit, period, parameters):
         person = tax_unit.members
-        spouse_with_age = person("is_tax_unit_spouse", period)
-        has_age = person("age", period) > 0
-        has_spouse_with_age = tax_unit.any(spouse_with_age & has_age)
+        has_spouse = add(tax_unit, period, ["is_tax_unit_spouse"]) > 0
         has_dependents = tax_unit("tax_unit_dependents", period) > 0
         return select(
-            [has_spouse_with_age, has_dependents, True],
+            [has_spouse, has_dependents, True],
             [MARSType.JOINT, MARSType.HEAD_OF_HOUSEHOLD, MARSType.SINGLE],
         )
 
