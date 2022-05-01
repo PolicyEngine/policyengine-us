a0ef4525fdb5cc9aecd4b73a86e0aeec24c39d7e
Max Ghenis <mghenis@gmail.com>
2022-05-01 11:03:44 -0700

    fix num

--- openfisca_us/variables/demographic/tax_unit/num.py
+++ openfisca_us/variables/demographic/tax_unit/num.py
@@ -5,9 +5,9 @@ class num(Variable):
     value_type = int
     entity = TaxUnit
     definition_period = YEAR
-    documentation = "2 when MARS is 2 (married filing jointly); otherwise 1"
+    documentation = "2 when MARS is married filing jointly; otherwise 1"
     unit = USD
 
     def formula(tax_unit, period, parameters):
         mars = tax_unit("mars", period)
-        return where(mars == mars.possible_values.MARRIED_FILING_JOINTLY, 2, 1)
+        return where(mars == mars.possible_values.JOINT, 2, 1)
