866622a2855a73d9e54dd91c765fd9ed2d360004
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-05-02 23:40:08 +0100

    Add child support deduction

---
+++ openfisca_us/variables/expense/child_support.py
@@ -0,0 +1,11 @@
+from openfisca_us.model_api import *
+
+
+class child_support_expense(Variable):
+    value_type = float
+    entity = Person
+    label = "Child support expense"
+    unit = "currency-USD"
+    documentation = "Legally mandated child support expenses."
+    definition_period = YEAR
+
