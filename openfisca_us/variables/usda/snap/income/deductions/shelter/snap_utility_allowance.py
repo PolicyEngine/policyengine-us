56cdb330407485e353098063e647187294b52091
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-05-02 22:16:19 +0100

    Finish utility allowance reimplementation

--- openfisca_us/variables/usda/snap/income/deductions/shelter/snap_utility_allowance.py
+++ openfisca_us/variables/usda/snap/income/deductions/shelter/snap_utility_allowance.py
@@ -13,13 +13,10 @@ class snap_utility_allowance(Variable):
         utility = parameters(period).usda.snap.income.deductions.utility
         allowance_type = spm_unit("snap_utility_allowance_type", period)
         allowance_types = allowance_type.possible_values
-        state = spm_unit.household("state_code_str", period)
-        return 12 * select(
-            [
-                allowance_type == allowance_types.SUA,
-                allowance_type == allowance_types.LUA,
-                allowance_type == allowance_types.TUA,
-                True,
-            ],
-            [utility.sua[state], utility.lua[state], utility.tua[state], 0],
-        )
+        region = spm_unit("snap_utility_region", period)
+        sua_due = where(allowance_type == allowance_types.SUA, utility.standard[region], 0)
+        lua_due = where(allowance_type == allowance_types.LUA, utility.limited[region], 0)
+        expense_types = utility.utility_types
+        sum_of_individual_allowances = sum([utility[expense.replace("_expense", "")][region] for expense in expense_types])
+        iua_due = where(allowance_type == allowance_types.IUA, sum_of_individual_allowances, 0)
+        return sua_due + lua_due + iua_due
