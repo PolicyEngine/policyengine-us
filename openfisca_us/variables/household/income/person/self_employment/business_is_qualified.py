fc64f66487e6a9a8dc65cd0f5f0cb22ae6257033
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-05-16 15:43:14 +0100

    Fix bug

--- openfisca_us/variables/household/income/person/self_employment/business_is_qualified.py
+++ openfisca_us/variables/household/income/person/self_employment/business_is_qualified.py
@@ -8,5 +8,4 @@ class business_is_qualified(Variable):
     unit = USD
     documentation = "Whether all income from self-employment, partnerships and S-corporations is from qualified businesses. A qualified trade or business is any trade or business other than a specified service trade or business, or employment. The list of specified service trades can be found at https://www.law.cornell.edu/uscode/text/26/1202#e_3_A."
     definition_period = YEAR
-    default_value = True
     reference = "https://www.law.cornell.edu/uscode/text/26/199A#d_1"
