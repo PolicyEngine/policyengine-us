8a94beff89901b4eb0aea69210d79005313e0fed
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-05-24 16:39:59 +0100

    Update openfisca_us/variables/household/demographic/geographic/zip_code.py
    Co-authored-by: Max Ghenis <mghenis@gmail.com>

--- openfisca_us/variables/household/demographic/geographic/zip_code.py
+++ openfisca_us/variables/household/demographic/geographic/zip_code.py
@@ -4,6 +4,6 @@ from openfisca_us.model_api import *
 class zip_code(Variable):
     value_type = str
     entity = Household
-    label = "Zip code"
+    label = "ZIP code"
     definition_period = YEAR
     default_value = "UNKNOWN"
