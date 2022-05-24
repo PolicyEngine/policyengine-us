fd696076c4ca94270f8cde419e02fab77b1a4d1a
Max Ghenis <mghenis@gmail.com>
2022-05-23 21:30:51 -0700

    Error with person-level childcare expenses #816

--- openfisca_us/variables/household/expense/childcare/tax_unit_childcare_expenses.py
+++ openfisca_us/variables/household/expense/childcare/tax_unit_childcare_expenses.py
@@ -8,4 +8,10 @@ class tax_unit_childcare_expenses(Variable):
     unit = USD
     definition_period = YEAR
 
-    formula = sum_of_variables(["childcare_expenses"])
+    def formula(tax_unit, period, parameters):
+        # Distribute the childcare expenses evenly across the SPM unit's members.
+        spmu = tax_unit.spm_unit
+        spmu_childcare = spmu("childcare_expenses", period)
+        spmu_size = spmu("spm_unit_size", period)
+        tu_size = tax_unit("tax_unit_size", period)
+        return spmu_childcare * (tu_size / spmu_size)
