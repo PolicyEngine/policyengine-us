3899bf82b7428288540ae1dbc3b6170becff2106
Max Ghenis <mghenis@gmail.com>
2022-05-23 21:43:26 -0700

    Remove more extraneous variable documentation fields

--- openfisca_us/variables/gov/ssa/ss/social_security_dependents.py
+++ openfisca_us/variables/gov/ssa/ss/social_security_dependents.py
@@ -5,6 +5,5 @@ class social_security_dependents(Variable):
     value_type = float
     entity = Person
     definition_period = YEAR
-    documentation = "Social Security dependents benefits"
     label = "Social Security dependents benefits"
     unit = USD
--- openfisca_us/variables/gov/ssa/ss/social_security_retirement.py
+++ openfisca_us/variables/gov/ssa/ss/social_security_retirement.py
@@ -5,6 +5,5 @@ class social_security_retirement(Variable):
     value_type = float
     entity = Person
     definition_period = YEAR
-    documentation = "Social Security retirement benefits"
     label = "Social Security retirement benefits"
     unit = USD
