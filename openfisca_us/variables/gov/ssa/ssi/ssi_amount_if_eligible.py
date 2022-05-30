463f4bd63959685aa60dc34ecd6443615206b4b4
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-05-30 16:37:30 +0100

    Repair some SSI changes

--- openfisca_us/variables/gov/ssa/ssi/ssi_amount_if_eligible.py
+++ openfisca_us/variables/gov/ssa/ssi/ssi_amount_if_eligible.py
@@ -10,11 +10,9 @@ class ssi_amount_if_eligible(Variable):
     reference = "https://www.law.cornell.edu/uscode/text/42/1382#b"
 
     def formula(person, period, parameters):
-        has_eligible_spouse = spouse(person, period, "is_ssi_eligible_spouse")
-        has_income_deemed_from_ineligible_spouse = person("ssi_income_deemed_from_ineligible_spouse", period) > 0
         ssi = parameters(period).ssa.ssi.amount
         return where(
-            has_eligible_spouse & has_income_deemed_from_ineligible_spouse,
+            person("ssi_claim_is_joint", period),
             ssi.couple,
             ssi.individual,
         )
--- openfisca_us/variables/gov/ssa/state_supplement/state_supplement.py
+++ openfisca_us/variables/gov/ssa/state_supplement/state_supplement.py
@@ -8,11 +8,10 @@ class state_supplement(Variable):
     definition_period = YEAR
 
     def formula(person, period, parameters):
-        marital_unit = person.marital_unit
-        uncapped_ssi = marital_unit("uncapped_ssi", period)
+        uncapped_ssi = person("uncapped_ssi", period)
         reduction_after_ssi = max_(0, -uncapped_ssi)
-        maximum_ss = add(marital_unit, period, ["maximum_state_supplement"])
+        maximum_ss = person("maximum_state_supplement", period)
         marital_unit_ss = max_(0, maximum_ss - reduction_after_ssi)
         eligible = person("is_ssi_aged_blind_disabled", period)
-        num_eligible = marital_unit.sum(eligible)
-        return eligible * marital_unit_ss * where(num_eligible > 1, 1 / 2, 1)
+        joint_claim = person("ssi_claim_is_joint", period)
+        return eligible * marital_unit_ss * where(joint_claim, 1 / 2, 1)
