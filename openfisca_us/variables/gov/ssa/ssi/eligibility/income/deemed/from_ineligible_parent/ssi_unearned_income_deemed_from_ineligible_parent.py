f7d4c0a8189d006f5e24df66ad9a124032f6c1bd
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-05-31 15:08:02 +0100

    Tweak order of operations slightly to exactly match example

--- openfisca_us/variables/gov/ssa/ssi/eligibility/income/deemed/from_ineligible_parent/ssi_unearned_income_deemed_from_ineligible_parent.py
+++ openfisca_us/variables/gov/ssa/ssi/eligibility/income/deemed/from_ineligible_parent/ssi_unearned_income_deemed_from_ineligible_parent.py
@@ -19,30 +19,41 @@ class ssi_unearned_income_deemed_from_ineligible_parent(Variable):
         ineligible_parent = person("is_ssi_ineligible_parent", period)
         tax_unit = person.tax_unit
 
-        ineligible_parents_income = eligible_child * _apply_ssi_exclusions(
-            eligible_child
-            * tax_unit.sum(
-                person("ssi_earned_income", period) * ineligible_parent
-            ),
-            eligible_child
-            * tax_unit.sum(
-                person("ssi_unearned_income", period) * ineligible_parent
-            ),
+        parental_earned_income = eligible_child * tax_unit.sum(
+            person("ssi_earned_income", period) * ineligible_parent
+        )
+        parental_unearned_income = eligible_child * tax_unit.sum(
+            person("ssi_unearned_income", period) * ineligible_parent
+        )
+
+        child_allocations = eligible_child * add(
+            tax_unit, period, ["ssi_ineligible_child_allocation"]
+        )
+        parental_allocations = eligible_child * add(
+            tax_unit, period, ["ssi_ineligible_parent_allocation"]
+        )
+
+        # To understand this ordering of operations, see Example 4 of
+        # https://www.law.cornell.edu/cfr/text/20/416.1165#h.
+
+        parental_unearned_income -= child_allocations
+        remaining_child_allocations = max_(0, -parental_unearned_income)
+        parental_unearned_income = max_(0, parental_unearned_income)
+        parental_earned_income = max_(
+            0, parental_earned_income - remaining_child_allocations
+        )
+
+        net_parental_deemed_income = _apply_ssi_exclusions(
+            parental_earned_income,
+            parental_unearned_income,
             parameters,
             period,
         )
 
-        child_allocations = add(
-            tax_unit, period, ["ssi_ineligible_child_allocation"]
-        )
-        parental_allocations = add(
-            tax_unit, period, ["ssi_ineligible_parent_allocation"]
-        )
-        total_allocations = child_allocations + parental_allocations
-
         net_parental_deemed_income = max_(
-            0, ineligible_parents_income - total_allocations
+            0, net_parental_deemed_income - parental_allocations
         )
+
         count_eligible_children = tax_unit.sum(eligible_child)
         return where(
             count_eligible_children > 0,
