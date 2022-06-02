493d39f5cd7a55b3e9336d41ab12daa37465850b
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-06-02 12:57:07 +0100

    Bump tools dep

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         "h5py",
         "microdf_python",
         "OpenFisca-Core[web-api] >= 35.0.0",
-        "OpenFisca-Tools>=0.8.0,<1.0.0",
+        "OpenFisca-Tools>=0.11.0,<1.0.0",
         "pandas",
         "pathlib",
         "pytest",
