bacdf7869b1754c3849c8925ffd1dd3196915a4f
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-06-06 13:53:02 +0100

    Format

--- openfisca_us/variables/gov/states/ma/tax/income/gross_income/ma_part_c_gross_income.py
+++ openfisca_us/variables/gov/states/ma/tax/income/gross_income/ma_part_c_gross_income.py
@@ -7,7 +7,7 @@ class ma_part_c_gross_income(Variable):
     label = "MA Part C gross income"
     unit = USD
     definition_period = YEAR
-    reference = "https://www.mass.gov/info-details/mass-general-laws-c62-ss-2" # (b)(3)
+    reference = "https://www.mass.gov/info-details/mass-general-laws-c62-ss-2"  # (b)(3)
 
     def formula(tax_unit, period, parameters):
         return max_(0, add(tax_unit, period, ["long_term_capital_gains"]))
