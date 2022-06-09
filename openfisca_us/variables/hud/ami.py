c0ecb577b85b58d79dacc6ce182f811e70f3ecf4
Katarina van Alebeek <katarinavanalebeek@gmail.com>
2022-06-08 18:41:56 -0700

    HUD total tenant payment Fixes #659

--- openfisca_us/variables/hud/ami.py
+++ openfisca_us/variables/hud/ami.py
@@ -2,7 +2,7 @@ from openfisca_us.model_api import *
 
 
 class ami(Variable):
-    value_type = bool
+    value_type = float
     entity = SPMUnit
     label = "Area median income"
     documentation = "Area median income"
