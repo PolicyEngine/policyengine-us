da5dd54c337de815d534a5238284de4d4ea296a1
Max Ghenis <mghenis@gmail.com>
2022-06-11 17:31:49 -0700

    Add NYS household credit for single filers

--- openfisca_us/variables/gov/states/ny/tax/income/credits/nonrefundable/ny_household_credit.py
+++ openfisca_us/variables/gov/states/ny/tax/income/credits/nonrefundable/ny_household_credit.py
@@ -8,3 +8,15 @@ class ny_household_credit(Variable):
     unit = USD
     definition_period = YEAR
     reference = "https://www.nysenate.gov/legislation/laws/TAX/606"  # (b)
+
+    def formula(tax_unit, period, parameters):
+        p = parameters(
+            period
+        ).states.ny.tax.income.credits.nonrefundable.household_credit
+        filing_status = tax_unit("filing_status", period)
+        filing_statuses = filing_status.possible_values
+        agi = tax_unit("adjusted_gross_income", period)
+        single = filing_status == filing_statuses.SINGLE
+        amount_single = p.single.calc(agi, right=True)  # "over...but not over"
+        # TODO: Other filing statuses.
+        return amount_single * single
