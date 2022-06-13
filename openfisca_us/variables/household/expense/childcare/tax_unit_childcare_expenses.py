0faf5beb21fb673237f90a56108691b9883b23bf
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-06-13 19:17:59 +0100

    Fix tax unit formula

--- openfisca_us/variables/household/expense/childcare/tax_unit_childcare_expenses.py
+++ openfisca_us/variables/household/expense/childcare/tax_unit_childcare_expenses.py
@@ -12,6 +12,6 @@ class tax_unit_childcare_expenses(Variable):
         # Distribute the childcare expenses evenly across the SPM unit's members.
         spm_unit = tax_unit.spm_unit
         spm_unit_childcare = spm_unit("childcare_expenses", period)
-        spm_unit_size = spm_unit("spm_unit_size", period)
-        tax_unit_size = tax_unit("tax_unit_size", period)
-        return spm_unit_childcare * (tax_unit_size / spm_unit_size)
+        spm_unit_count_children = add(spm_unit, period, ["is_child"])
+        tax_unit_count_children = add(tax_unit, period, ["is_child"])
+        return spm_unit_childcare * (tax_unit_count_children / spm_unit_count_children)
