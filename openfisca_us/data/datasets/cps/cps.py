57ce13ab4502acb223838fea7aa816137f01f1ca
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-06-13 19:13:29 +0100

    Add childcare

--- openfisca_us/data/datasets/cps/cps.py
+++ openfisca_us/data/datasets/cps/cps.py
@@ -221,6 +221,7 @@ def add_spm_variables(cps: h5py.File, spm_unit: DataFrame) -> None:
         spm_unit_medical_expenses="SPM_MEDXPNS",
         spm_unit_spm_threshold="SPM_POVTHRESHOLD",
         spm_unit_net_income_reported="SPM_RESOURCES",
+        childcare_expenses="SPM_CHILDCAREXPNS",
     )
 
     for openfisca_variable, asec_variable in SPM_RENAMES.items():
