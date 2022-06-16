d90af5792b0634e68a97423d73a1ab98e38ced20
Max Ghenis <mghenis@gmail.com>
2022-06-16 08:31:04 -0700

    Reorganize tax variables

---
+++ openfisca_us/variables/gov/irs/tax/federal_income/income_tax.py
@@ -0,0 +1,20 @@
+from openfisca_us.model_api import *
+
+
+class iitax(Variable):
+    value_type = float
+    entity = TaxUnit
+    definition_period = YEAR
+    unit = USD
+    label = "Federal income tax"
+    documentation = "Total federal individual income tax liability."
+
+    def formula(tax_unit, period, parameters):
+        before_refundable_credits = tax_unit(
+            "income_tax_before_refundable_credits", period
+        )
+        refundable_credits = tax_unit("income_tax_refundable_credits", period)
+        return before_refundable_credits - refundable_credits
+
+
+income_tax = variable_alias("income_tax", iitax)
--- openfisca_us/variables/gov/irs/tax/federal_income/income_tax_before_refundable_credits.py
+++ openfisca_us/variables/gov/irs/tax/federal_income/income_tax_before_refundable_credits.py
@@ -20,22 +20,3 @@ class c09200(Variable):
 income_tax_before_refundable_credits = variable_alias(
     "income_tax_before_refundable_credits", c09200
 )
-
-
-class iitax(Variable):
-    value_type = float
-    entity = TaxUnit
-    definition_period = YEAR
-    unit = USD
-    label = "Federal income tax"
-    documentation = "Total federal individual income tax liability."
-
-    def formula(tax_unit, period, parameters):
-        before_refundable_credits = tax_unit(
-            "income_tax_before_refundable_credits", period
-        )
-        refundable_credits = tax_unit("income_tax_refundable_credits", period)
-        return before_refundable_credits - refundable_credits
-
-
-income_tax = variable_alias("income_tax", iitax)
