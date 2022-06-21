dc78867cb62d764e4f6865af66bd61f67e4d536e
andersonfrailey <andersonfrailey@gmail.com>
2022-06-21 16:32:46 -0600

    Apply suggestions from code review
    Co-authored-by: Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>

--- openfisca_us/variables/gov/states/ut/tax/income/taxable_income/ut_taxable_income.py
+++ openfisca_us/variables/gov/states/ut/tax/income/taxable_income/ut_taxable_income.py
@@ -11,6 +11,6 @@ class ut_taxable_income(Variable):
     def formula(tax_unit, period, parameters):
         salt_refund = tax_unit("salt_refund_last_year", period)
         ut_subtractions = tax_unit("ut_subtractions_from_income", period)
-        ut_total_income = tax_unit("ut_total_income")
+        ut_total_income = tax_unit("ut_total_income", period)
 
         return ut_total_income - salt_refund - ut_subtractions
