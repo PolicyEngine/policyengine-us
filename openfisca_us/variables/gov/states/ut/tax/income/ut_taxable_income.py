a8034f45cfa3f90e65b34b7be8674807e19066d6
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-06-21 17:33:36 -0600

    Remove placeholder

--- openfisca_us/variables/gov/states/ut/tax/income/ut_taxable_income.py
+++
@@ -1,9 +0,0 @@
-from openfisca_us.model_api import *
-
-
-class ut_taxable_income(Variable):
-    value_type = float
-    entity = TaxUnit
-    label = "Utah Taxable Income"
-    unit = USD
-    definition_period = YEAR
