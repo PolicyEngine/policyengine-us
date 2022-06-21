17a5e72497e371820c2ad9558ac07a79dc28d596
Richard Evans <rickecon@gmail.com>
2022-06-21 17:07:23 -0600

    Updated ut_taxpayer_credit.py

--- openfisca_us/variables/gov/states/ut/tax/income/credits/taxpayer_credit/ut_taxpayer_credit.py
+++ openfisca_us/variables/gov/states/ut/tax/income/credits/taxpayer_credit/ut_taxpayer_credit.py
@@ -9,10 +9,10 @@ class ut_taxpayer_credit(Variable):
     definition_period = YEAR
 
     def formula(tax_unit, period, parameters):
-        ut_init_cred_before_phaseout = tax_unit(
+        ut_taxpayer_credit_max = tax_unit(
             "ut_taxpayer_credit_max", period
         )
         ut_taxpayer_credit_reduction = tax_unit(
             "ut_taxpayer_credit_reduction", period
         )
-        return ut_init_cred_before_phaseout - ut_taxpayer_credit_reduction
+        return ut_taxpayer_credit_max - ut_taxpayer_credit_reduction
