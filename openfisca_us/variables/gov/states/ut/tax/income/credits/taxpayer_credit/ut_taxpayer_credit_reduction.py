9ddbda48afdadf95d513c39a8d0e02620bded178
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-06-21 17:02:29 -0600

    Fix filing status call

--- openfisca_us/variables/gov/states/ut/tax/income/credits/taxpayer_credit/ut_taxpayer_credit_reduction.py
+++ openfisca_us/variables/gov/states/ut/tax/income/credits/taxpayer_credit/ut_taxpayer_credit_reduction.py
@@ -13,8 +13,13 @@ class ut_taxpayer_credit_reduction(Variable):
         ut_taxpayer_credit_params_path = (
             gov.states.ut.tax.income.credits.taxpayer_credit
         )
-        phase_out_start = ut_taxpayer_credit_params_path.phase_out.start
+        filing_status = tax_unit("filing_status", period)
+        phase_out_start = ut_taxpayer_credit_params_path.phase_out.start[
+            filing_status
+        ]
         phase_out_rate = ut_taxpayer_credit_params_path.phase_out.rate
         ut_taxable_income = tax_unit("ut_taxable_income", period)
-        income_subject_to_phase_out = max(ut_taxable_income - phase_out_start, 0)
+        income_subject_to_phase_out = max(
+            ut_taxable_income - phase_out_start, 0
+        )
         return income_subject_to_phase_out * phase_out_rate
