b9e6f20d337da44fbbe8b36bad3583df7dda86c7
matthjensen <matthewjensen@Matthews-MacBook-Pro.local>
2022-06-21 14:00:45 -0600

    ut taxpayer credit reduction

---
+++ openfisca_us/variables/gov/states/ut/tax/income/credits/taxpayer_credit/ut_taxpayer_credit_reduction.py
@@ -0,0 +1,18 @@
+from openfisca_us.model_api import *
+
+
+class ut_taxpayer_credit_reduction(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "UT taxpayer credit reduction"
+    unit = USD
+    definition_period = YEAR
+
+    def formula(tax_unit, period, parameters):
+        gov = parameters(period).gov
+        ut_taxpayer_credit_params_path = gov.states.ut.tax.income.credits.taxpayer_credit
+        phaseout_start = ut_taxpayer_credit_params_path.phase_out.start
+        phaseout_rate = ut_taxpayer_credit_params_path.phase_out.rate
+        ut_taxable_income = tax_unit("ut_taxable_income", period)
+        income_subject_to_phaseout = max(ut_taxable_income - phaseout_start, 0)
+        return income_subject_to_phaseout * phaseout_rate
