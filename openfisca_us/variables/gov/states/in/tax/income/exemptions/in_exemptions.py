ed9e15e5b3bb3f156cead5275ae7019a813f2099
Matt Jensen <matthjensen@gmail.com>
2022-06-28 08:00:55 -0400

    cleaning up

--- openfisca_us/variables/gov/states/in/tax/income/exemptions/in_exemptions.py
+++ openfisca_us/variables/gov/states/in/tax/income/exemptions/in_exemptions.py
@@ -1,6 +1,3 @@
-from gov.states.
-from gov.states.
-from gov.states. in .tax.income.exemptions.in_aged_blind_exemptions import in_aged_blind_exemptionsin.tax.income.exemptions.in_additional_exemptions import in_additional_exemptionsin.tax.income.exemptions.in_base_exemptions import in_base_exemptions
 from openfisca_us.model_api import *
 
 
@@ -15,10 +12,12 @@ class in_exemptions(Variable):
         in_base_exemptions = tax_unit("in_base_exemptions", period)
         in_additional_exemptions = tax_unit("in_additional_exemptions", period)
         in_aged_blind_exemptions = tax_unit("in_aged_blind_exemptions", period)
-        in_aged_low_agi_exemptions = tax_unit("in_aged_low_agi_exemptions", period)
+        in_aged_low_agi_exemptions = tax_unit(
+            "in_aged_low_agi_exemptions", period
+        )
         return (
             in_base_exemptions
             + in_additional_exemptions
             + in_aged_blind_exemptions
             + in_aged_low_agi_exemptions
-            )
+        )
