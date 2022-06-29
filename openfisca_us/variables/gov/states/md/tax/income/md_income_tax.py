4d1e275e18d6808e7d4b94953f02835028160e81
William Fedder <fedderw@gmail.com>
2022-06-29 16:36:40 -0400

    add md exemption test

--- openfisca_us/variables/gov/states/md/tax/income/md_income_tax.py
+++ openfisca_us/variables/gov/states/md/tax/income/md_income_tax.py
@@ -7,6 +7,3 @@ class md_income_tax(Variable):
     label = "MD income tax"
     unit = USD
     definition_period = YEAR
-
-    def formula(tax_unit, period, parameters):
-        return tax_unit("md_income_tax", period)
