6a2ff8cea19c906e0701f8b8df2072d07567554c
Matt Jensen <matthjensen@gmail.com>
2022-07-01 13:05:55 -0400

    cleanup

--- openfisca_us/variables/gov/irs/income/taxable_income/deductions/standard_deduction/aged_blind_count.py
+++ openfisca_us/variables/gov/irs/income/taxable_income/deductions/standard_deduction/aged_blind_count.py
@@ -1,10 +1,10 @@
 from openfisca_us.model_api import *
 
 
-class in_aged_blind_count(Variable):
+class aged_blind_count(Variable):
     value_type = float
     entity = TaxUnit
-    label = "IN aged and or blind head and spouse count"
+    label = "Aged and or blind head and spouse count"
     unit = USD
     definition_period = YEAR
     reference = "http://iga.in.gov/legislative/laws/2021/ic/titles/006#6-3-1-3.5"  # (a)(4)(B)
