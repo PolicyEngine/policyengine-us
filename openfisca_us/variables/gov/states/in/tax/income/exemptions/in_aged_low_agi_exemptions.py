480e02e598563d4bae4557eedd2b46dd92e509bb
Matt Jensen <matthjensen@gmail.com>
2022-07-01 13:35:27 -0400

    new aged_head and aged_spouse vars

--- openfisca_us/variables/gov/states/in/tax/income/exemptions/in_aged_low_agi_exemptions.py
+++ openfisca_us/variables/gov/states/in/tax/income/exemptions/in_aged_low_agi_exemptions.py
@@ -15,11 +15,8 @@ class in_aged_low_agi_exemptions(Variable):
         threshold = p.aged_low_agi.threshold[filing_status]
         aged_low_agi_exemption = p.aged_low_agi.amount
         federal_agi = tax_unit("adjusted_gross_income", period)
-        age_threshold = parameters(
-            period
-        ).gov.irs.deductions.standard.aged_or_blind.age_threshold
-        aged_head = (tax_unit("age_head", period) >= age_threshold) * 1
-        aged_spouse = (tax_unit("age_spouse", period) >= age_threshold) * 1
+        aged_head = tax_unit("aged_head", period)
+        aged_spouse = tax_unit("aged_spouse", period) 
         income_eligible = where(federal_agi < threshold, 1, 0)
         return (
             income_eligible
