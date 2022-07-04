d0d321403fd4b13001bfe5584ad523a254b6df76
nmrodelo <nmrodelo@bu.edu>
2022-07-04 17:54:09 -0400

    comment out tax credits

--- openfisca_us/variables/gov/states/mo/tax/income/mo_income_tax.py
+++ openfisca_us/variables/gov/states/mo/tax/income/mo_income_tax.py
@@ -18,9 +18,9 @@ class mo_income_tax(Variable):
         )
 
         CREDITS = [
-            "mo_resident_credit",  # this seems to relate to paying taxes in other states
-            "mo_misc_tax_credits",  # sum of credits found here - https://dor.mo.gov/forms/MO-TC_2021.pdf
-            "mo_property_tax_credit",
+            # "mo_resident_credit",  # this seems to relate to paying taxes in other states
+            # "mo_misc_tax_credits",  # sum of credits found here - https://dor.mo.gov/forms/MO-TC_2021.pdf
+            # "mo_property_tax_credit",
         ]
 
         credit_value = add(tax_unit, period, CREDITS)
