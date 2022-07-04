124d8e26b94a76951b1132b46ebc434c484f767b
nmrodelo <nmrodelo@bu.edu>
2022-07-04 17:18:32 -0400

    update agi to adjusted_gross_income in formulas

--- openfisca_us/variables/gov/states/mo/tax/income/mo_income_tax_before_credits.py
+++ openfisca_us/variables/gov/states/mo/tax/income/mo_income_tax_before_credits.py
@@ -10,7 +10,7 @@ class mo_income_tax_before_credits(Variable):
     reference = "https://dor.mo.gov/forms/MO-1040%20Print%20Only_2021.pdf"
 
     def formula(tax_unit, period, parameters):
-        agi = tax_unit("agi", period)
+        agi = tax_unit("adjusted_gross_income", period)
         rates = parameters(period).gov.states.mo.tax.income.rates
         tax_due = rates.calc(agi, right=True)
         return tax_due
