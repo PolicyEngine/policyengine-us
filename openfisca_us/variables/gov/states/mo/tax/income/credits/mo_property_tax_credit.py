bf53774f6c8650c1a8e815b0c1ae02e97e0d21a5
nmrodelo <nmrodelo@bu.edu>
2022-07-04 17:51:15 -0400

    update nesting of rates.yaml, remove age info from tests to create true unit tests, create placeholder variables for MO tax credits

---
+++ openfisca_us/variables/gov/states/mo/tax/income/credits/mo_property_tax_credit.py
@@ -0,0 +1,14 @@
+from openfisca_us.model_api import *
+
+
+class mo_property_tax_credit(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "MO property tax credit"
+    unit = USD
+    definition_period = YEAR
+    reference = (
+    )
+
+    def formula(tax_unit, period, parameters):
+        
