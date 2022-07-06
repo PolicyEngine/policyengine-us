ff8a7bb38dc7217c260ce565957e499263476d5a
nmrodelo <nmrodelo@bu.edu>
2022-07-06 14:11:36 -0400

    update name of mo_taxable income

--- openfisca_us/variables/gov/states/mo/tax/income/mo_taxable_income.py
+++ openfisca_us/variables/gov/states/mo/tax/income/mo_taxable_income.py
@@ -1,10 +1,10 @@
 from openfisca_us.model_api import *
 
 
-class mo_part_a_taxable_income(Variable):
+class mo_taxable_income(Variable):
     value_type = float
     entity = TaxUnit
-    label = "MO Part A taxable income"
+    label = "MO taxable income"
     unit = USD
     definition_period = YEAR
-    reference = 'https://revisor.mo.gov/main/OneSection.aspx?section=143.111&bid=7201&hl='
+    reference = "https://revisor.mo.gov/main/OneSection.aspx?section=143.111&bid=7201&hl="
