d88ffa55103a156553e961362a6cce1db4e41725
Max Ghenis <mghenis@gmail.com>
2022-07-07 18:48:38 -0700

    Add tests

--- openfisca_us/variables/gov/states/ma/tax/income/credits/ma_covid_19_essential_employee_premium_pay_program/ma_covid_19_essential_employee_premium_pay_program.py
+++ openfisca_us/variables/gov/states/ma/tax/income/credits/ma_covid_19_essential_employee_premium_pay_program/ma_covid_19_essential_employee_premium_pay_program.py
@@ -10,7 +10,7 @@ class ma_covid_19_essential_employee_premium_pay_program(Variable):
     reference = "https://www.mass.gov/info-details/covid-19-essential-employee-premium-pay-program"
 
     def formula(person, period, parameters):
-        earnings = person("earned", period)
+        earnings = person("earned_income", period)
         tax_unit = person.tax_unit
         agi = tax_unit("adjusted_gross_income", period)
         fpg = tax_unit("tax_unit_fpg", period)
@@ -21,7 +21,11 @@ class ma_covid_19_essential_employee_premium_pay_program(Variable):
         meets_earnings_test = earnings >= p.min_earnings
         meets_poverty_ratio_test = poverty_ratio <= p.max_poverty_ratio
         meets_program_test = add(person, period, p.disqualifying_programs) == 0
+        in_ma = person.household("state_code_str", period) == "MA"
         eligible = (
-            meets_earnings_test & meets_poverty_ratio_test & meets_program_test
+            meets_earnings_test
+            & meets_poverty_ratio_test
+            & meets_program_test
+            & in_ma
         )
         return eligible * p.amount
