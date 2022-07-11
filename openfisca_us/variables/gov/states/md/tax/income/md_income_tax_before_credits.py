febdc00c49244131c8df085e6bb0a913a5a6ad03
William Fedder <fedderw@gmail.com>
2022-07-11 11:43:01 -0400

    fixx income tax params for rate schedule selector

--- openfisca_us/variables/gov/states/md/tax/income/md_income_tax_before_credits.py
+++ openfisca_us/variables/gov/states/md/tax/income/md_income_tax_before_credits.py
@@ -15,7 +15,7 @@ class md_income_tax_before_credits(Variable):
 
         taxable_income = tax_unit("md_taxable_income", period)
 
-        p = parameters(period).gov.states.md.tax.income
+        p = parameters(period).gov.states.md.tax.income.rate_schedule
 
         single_separate = p.single_separate.calc(taxable_income)
 
