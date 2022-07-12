50c49bb205272b7db88c008d30526020d9e725ef
Matt Jensen <matthjensen@gmail.com>
2022-07-12 18:23:14 -0400

    cleanup

--- openfisca_us/variables/gov/states/in/tax/income/deductions/in_num_children_nonpublic_school.py
+++ openfisca_us/variables/gov/states/in/tax/income/deductions/in_num_children_nonpublic_school.py
@@ -5,9 +5,9 @@ class in_num_children_nonpublic_school(Variable):
     value_type = float
     entity = TaxUnit
     label = "IN rent"
-    unit = USD
+    unit = /1
     definition_period = YEAR
     documentation = "Number of dependent children who attended a nonpublic school in Indiana for 180 days or more and for whom non-reimbursed education expenditures were made."
     reference = (
-        "http://iga.in.gov/legislative/laws/2021/ic/titles/006#6-3-2-4"(d)(1)
+        "http://iga.in.gov/legislative/laws/2021/ic/titles/006#6-3-2-4" #(d)(1)
     )
