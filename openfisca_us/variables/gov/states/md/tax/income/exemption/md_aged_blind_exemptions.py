889eaffe09ec0fd3da17c72f0c4630d01a1ab114
William Fedder <fedderw@gmail.com>
2022-07-12 18:08:56 -0400

    empty lines

--- openfisca_us/variables/gov/states/md/tax/income/exemption/md_aged_blind_exemptions.py
+++ openfisca_us/variables/gov/states/md/tax/income/exemption/md_aged_blind_exemptions.py
@@ -12,7 +12,6 @@ class md_aged_blind_exemptions(Variable):
     def formula(tax_unit, period, parameters):
         filing_status = tax_unit("filing_status", period)
         filing_status_type = filing_status.possible_values
-
         blind = parameters(period).gov.states.md.tax.income.exemptions.blind
         # Count number of is_blind from tax_unit
         blind_head = tax_unit("blind_head", period) * 1
@@ -20,35 +19,23 @@ class md_aged_blind_exemptions(Variable):
             tax_unit("blind_spouse", period)
             & (filing_status == filing_status_type.JOINT)
         ) * 1
-
         blind_exemption_total = (blind_head + blind_spouse) * blind
-
         p = parameters(period).gov.states.md.tax.income.exemptions.aged
-
         age_for_exemption = p.age
-
         aged_amount = p.amount
-
         aged_head = (tax_unit("age_head", period) >= age_for_exemption) * 1
-
         aged_spouse = (
             tax_unit("age_spouse", period) >= age_for_exemption * 1
         ) & (filing_status == filing_status_type.JOINT)
-
         aged_exemption_total = (
             aged_head * aged_amount + aged_spouse * aged_amount
         )
-
         # These apply to dependents over the age of 65
         person = tax_unit.members
         dependent = person("is_tax_unit_dependent", period)
-
         age = person("age", period)
-
         elderly = age >= age_for_exemption
-
         aged_dependents = tax_unit.sum(dependent & elderly)
-
         aged_dependent_exemption = aged_dependents * p.aged_dependent
 
         return (
