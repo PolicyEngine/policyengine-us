7f5f053264fc9d0e73d247d4c64b95d2d7152441
Matt Jensen <matthjensen@gmail.com>
2022-07-13 10:26:59 -0400

    cleanup

--- openfisca_us/variables/gov/states/in/tax/income/deductions/in_military_service_deduction.py
+++ openfisca_us/variables/gov/states/in/tax/income/deductions/in_military_service_deduction.py
@@ -10,4 +10,4 @@ class in_military_service_deduction(Variable):
     reference = "http://iga.in.gov/legislative/laws/2021/ic/titles/006#6-3-2-4"  # (a)(1)
 
     def formula(tax_unit, period, parameters):
-        return aggr(tax_unit, period, [in_person_military_service_deduction])
+        return aggr(tax_unit, period, ["in_person_military_service_deduction"])
