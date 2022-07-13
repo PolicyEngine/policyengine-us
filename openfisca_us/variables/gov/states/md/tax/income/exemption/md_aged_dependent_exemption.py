1ea40e8a0b4e74fd2e91506819df59640bdf61bf
Will Fedder <fedderw@gmail.com>
2022-07-12 20:22:44 -0400

    Update openfisca_us/variables/gov/states/md/tax/income/exemption/md_aged_dependent_exemption.py
    Co-authored-by: Max Ghenis <mghenis@gmail.com>

--- openfisca_us/variables/gov/states/md/tax/income/exemption/md_aged_dependent_exemption.py
+++ openfisca_us/variables/gov/states/md/tax/income/exemption/md_aged_dependent_exemption.py
@@ -19,6 +19,6 @@ class md_aged_dependent_exemption(Variable):
         age = person("age", period)
         elderly = age >= age_for_exemption
         aged_dependents = tax_unit.sum(dependent & elderly)
-        aged_dependent_exemption = aged_dependents * p.aged_dependent
+        return aged_dependents * p.aged_dependent
 
         return aged_dependent_exemption
