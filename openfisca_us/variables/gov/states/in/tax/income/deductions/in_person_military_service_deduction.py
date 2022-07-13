50ae3596d0a8081f58065c2b19ff20e243050047
Matt Jensen <matthjensen@gmail.com>
2022-07-13 10:03:18 -0400

    cleanup

--- openfisca_us/variables/gov/states/in/tax/income/deductions/in_person_military_service_deduction.py
+++ openfisca_us/variables/gov/states/in/tax/income/deductions/in_person_military_service_deduction.py
@@ -15,4 +15,4 @@ class in_person_military_service_deduction(Variable):
             .gov.states["in"]
             .tax.income.deductions.military_service.max
         )
-        return min(person("military_service_income", period), cap)
+        return min_(person("military_service_income", period), cap)
