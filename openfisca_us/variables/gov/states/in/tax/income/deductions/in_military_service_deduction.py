8314c1a458d1758a948cd3a9b1b512a3d1a615d0
Matt Jensen <matthjensen@gmail.com>
2022-07-13 12:21:27 -0400

    try new approach to summing person info to tax unit level

--- openfisca_us/variables/gov/states/in/tax/income/deductions/in_military_service_deduction.py
+++ openfisca_us/variables/gov/states/in/tax/income/deductions/in_military_service_deduction.py
@@ -10,4 +10,6 @@ class in_military_service_deduction(Variable):
     reference = "http://iga.in.gov/legislative/laws/2021/ic/titles/006#6-3-2-4"  # (a)(1)
 
     def formula(tax_unit, period, parameters):
-        return aggr(tax_unit, period, ["in_person_military_service_deduction"])
+        deductions = tax_unit.members("in_person_military_service_deduction", period)
+        sum_deductions = tax_unit.sum(deductions)
+        return sum(deductions)
