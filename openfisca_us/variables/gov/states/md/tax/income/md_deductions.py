0616af043dc975c13f1aa51f3d742fd3c6cfd04f
William Fedder <fedderw@gmail.com>
2022-07-14 17:02:13 -0400

    change some tests

--- openfisca_us/variables/gov/states/md/tax/income/md_deductions.py
+++ openfisca_us/variables/gov/states/md/tax/income/md_deductions.py
@@ -1,6 +1,5 @@
 from openfisca_us.model_api import *
-from openfisca_us.variables.gov.irs.income.taxable_income.deductions.standard_deduction.standard_deduction import (
-    standard_deduction)
+
 
 
 class md_deductions(Variable):
@@ -14,7 +13,7 @@ class md_deductions(Variable):
         # Check if the tax_unit itemized on their federal returns:
         tax_unit_itemizes = tax_unit("tax_unit_itemizes", period)
         standard_deduction = tax_unit("md_standard_deduction", period)
-        deductions_if_itemizing = tax_unit_itemizes * parameters(period).gov.states.md.tax.income.deductions_if_itemizing
+        deductions_if_itemizing = tax_unit_itemizes * tax_unit("taxable_income_deductions_if_itemizing", period)
         return where(
             tax_unit_itemizes,
             deductions_if_itemizing,
