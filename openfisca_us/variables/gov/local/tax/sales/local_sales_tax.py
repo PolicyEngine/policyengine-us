69beba557f909b7cd6161e16a11ea066cb07eb42
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-07-15 21:14:17 +0100

    Add county tax rates

--- openfisca_us/variables/gov/local/tax/sales/local_sales_tax.py
+++
@@ -1,9 +0,0 @@
-from openfisca_us.model_api import *
-
-
-class local_sales_tax(Variable):
-    value_type = float
-    entity = TaxUnit
-    definition_period = YEAR
-    label = "Local sales tax"
-    unit = USD
