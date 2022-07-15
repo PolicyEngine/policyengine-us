5a71d8d59ee0264425b6f6be0320275c98ec5173
Will Fedder <fedderw@gmail.com>
2022-07-14 22:30:30 -0400

    Apply suggestions from code review
    Co-authored-by: Max Ghenis <mghenis@gmail.com>

--- openfisca_us/variables/gov/states/md/tax/income/md_deductions.py
+++ openfisca_us/variables/gov/states/md/tax/income/md_deductions.py
@@ -13,7 +13,7 @@ class md_deductions(Variable):
         # Check if the tax_unit itemized on their federal returns:
         tax_unit_itemizes = tax_unit("tax_unit_itemizes", period)
         standard_deduction = tax_unit("md_standard_deduction", period)
-        deductions_if_itemizing = tax_unit_itemizes * tax_unit("taxable_income_deductions_if_itemizing", period)
+        deductions_if_itemizing = tax_unit("taxable_income_deductions_if_itemizing", period)
         return where(
             tax_unit_itemizes,
             deductions_if_itemizing,
