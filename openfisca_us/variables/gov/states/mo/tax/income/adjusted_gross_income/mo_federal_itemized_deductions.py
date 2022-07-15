5cec6d0b37db039edc118233e820cf4b116e6e1d
nmrodelo <nmrodelo@bu.edu>
2022-07-14 20:15:35 -0400

    updates to variables/parameter names, formatting, added outer return statement to mo_taxable_income

--- openfisca_us/variables/gov/states/mo/tax/income/adjusted_gross_income/mo_federal_itemized_deductions.py
+++ openfisca_us/variables/gov/states/mo/tax/income/adjusted_gross_income/mo_federal_itemized_deductions.py
@@ -1,7 +1,7 @@
 from openfisca_us.model_api import *
 
 
-class federal_itemized_deductions(Variable):
+class mo_federal_itemized_deductions(Variable):
     value_type = float
     entity = TaxUnit
     label = "Sum of Federal itemized deductions applicable to MO taxable income calculation"
@@ -13,13 +13,8 @@ class federal_itemized_deductions(Variable):
         casualty_loss_deduction = tax_unit("casualty_loss_deduction", period)
         charitable_deduction = tax_unit("charitable_deduction", period)
         interest_deduction = tax_unit("interest_deduction", period)
-        itemized_taxable_income_deduction = tax_unit("itemized_taxable_income_deduction", period)
+        itemized_taxable_income_deductions = tax_unit("itemized_taxable_income_deductions", period)
         medical_expense_deduction = tax_unit("medical_expense_deduction", period)
         misc_deduction = tax_unit("misc_deduction", period)
 
-        return sum(casualty_loss_deduction,
-                    charitable_deduction,
-                    interest_deduction,
-                    itemized_taxable_income_deduction,
-                    medical_expense_deduction,
-                    misc_deduction,)
+        return (casualty_loss_deduction + charitable_deduction + interest_deduction + itemized_taxable_income_deductions + medical_expense_deduction + misc_deduction) 
--- openfisca_us/variables/gov/states/mo/tax/income/adjusted_gross_income/mo_taxable_income.py
+++ openfisca_us/variables/gov/states/mo/tax/income/adjusted_gross_income/mo_taxable_income.py
@@ -11,9 +11,10 @@ class mo_taxable_income(Variable):
 
     def formula(tax_unit, period, parameters):
         filing_status = tax_unit("filing_status", period)
-        threshold = parameters(period).gov.states.mo.tax.income.itemization_threshold[filing_status]
+        threshold = parameters(period).gov.states.mo.tax.income.mo_taxable_income.mo_federal_itemization_threshold[filing_status]
 
         #any itemizations 
+        agi = tax_unit("adjusted_gross_income", period)
         total_itemized_federal_deductions = tax_unit("mo_federal_itemized_deductions", period)
         if total_itemized_federal_deductions > 0:
             
@@ -30,10 +31,13 @@ class mo_taxable_income(Variable):
             #itemizations from federal return w/ > $10,000 or $5,000 (MFS) in state and local taxes
             if state_and_local_sales_or_income_tax > threshold:
                 real_estate_tax = tax_unit("real_estate_taxes", period)
-                base_amount = parameters(period).gov.states.mo.tax.income.max_salt[filing_status]
+                base_amount = parameters(period).gov.states.mo.tax.income.mo_taxable_income.mo_salt_cap[filing_status]
                 #overwrites the net_state_income_taxes variable assigned above
                 adjustment = base_amount * (real_estate_tax / (state_and_local_sales_or_income_tax - self_employment_tax))
                 net_state_income_taxes = state_and_local_sales_or_income_tax - adjustment
-                return tax_unit("adjusted_gross_income", period) - (items - net_state_income_taxes)
+                return agi - (items - net_state_income_taxes)
             else:
-                return tax_unit("adjusted_gross_income", period) - (items - net_state_income_taxes)
+                return agi - (items - net_state_income_taxes)
+        else:
+            return agi
+            
