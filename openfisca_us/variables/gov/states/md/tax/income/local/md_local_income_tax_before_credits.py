637a976552d99ff01c1cfef66d7c82a51b9b5e57
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-07-16 10:23:44 +0100

    Add suggested edits

--- openfisca_us/variables/gov/states/md/tax/income/local/md_local_income_tax_before_credits.py
+++ openfisca_us/variables/gov/states/md/tax/income/local/md_local_income_tax_before_credits.py
@@ -11,9 +11,9 @@ class md_local_income_tax_before_credits(Variable):
     def formula(tax_unit, period, parameters):
         county = tax_unit.household("county_str", period)
         in_md = tax_unit.household("state_code_str", period) == "MD"
-        print(in_md)
         rate = np.zeros_like(county, dtype=float)
         rates = parameters(period).gov.local.tax.income.flat_tax_rate
         if in_md.sum() > 0:
+            # Only compute Maryland local taxes if the simulation includes Maryland households.
             rate[in_md] = rates[county[in_md]]
         return rate * tax_unit("md_taxable_income", period)
