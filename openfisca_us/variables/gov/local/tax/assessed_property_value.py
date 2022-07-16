f8b19f4a3c41d5a89051b5efa55163c78b7255be
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-07-16 11:41:32 +0100

    Re-add deleted files

---
+++ openfisca_us/variables/gov/local/tax/assessed_property_value.py
@@ -0,0 +1,10 @@
+from openfisca_us.model_api import *
+
+
+class assessed_property_value(Variable):
+    value_type = float
+    entity = Person
+    label = "Assessed property value"
+    unit = USD
+    documentation = "Total assessed value of property owned by this person."
+    definition_period = YEAR
