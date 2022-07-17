3a22f9c3f72bae87481e326a262e9d5ee8d43a9a
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-07-17 22:37:15 +0100

    Formatting and versioning

--- openfisca_us/variables/household/income/person/dividends/ordinary_dividend_income.py
+++ openfisca_us/variables/household/income/person/dividends/ordinary_dividend_income.py
@@ -8,4 +8,6 @@ class ordinary_dividend_income(Variable):
     unit = USD
     definition_period = YEAR
 
-    formula = sum_of_variables(["qualified_dividend_income", "non_qualified_dividend_income"])
+    formula = sum_of_variables(
+        ["qualified_dividend_income", "non_qualified_dividend_income"]
+    )
