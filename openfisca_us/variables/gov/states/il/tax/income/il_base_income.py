b5ee467ede23aad79ad6b83c66f018066ad8e3c3
Yash Vijay <yvvijay121@gmail.com>
2022-07-17 17:47:05 -0500

    quick fix 2

--- openfisca_us/variables/gov/states/il/tax/income/il_base_income.py
+++ openfisca_us/variables/gov/states/il/tax/income/il_base_income.py
@@ -13,24 +13,3 @@ class il_base_income(Variable):
         ADDITIONS = ["adjusted_gross_income", "tax_exempt_interest_income", "dividend_income", "il_schedule_m_additions"]
         SUBTRACTIONS = ["social_security_benefits", "schedule_m_subtractions"]
         return add(tax_unit, period, ADDITIONS) - add(tax_unit, period, SUBTRACTIONS)
-
-        federal_agi = tax_unit("adjusted_gross_income", period)
-        federal_tax_exempt_interest = sum(
-            person("tax_exempt_interest_income", period)
-        )
-        dividend_income = sum(person("dividend_income", period))
-        social_security_benefits = sum(person("social_security", period))
-
-        schedule_m_additions = tax_unit("il_schedule_m_additions", period)
-        schedule_m_subtractions = tax_unit("il_schedule_m_subtractions", period)
-
-        # TODO: add Illinois Income Tax overpayment, and other adjustments
-
-        return (
-            federal_agi
-            + federal_tax_exempt_interest
-            + dividend_income
-            + schedule_m_additions
-            - social_security_benefits
-            - schedule_m_subtractions
-        )
