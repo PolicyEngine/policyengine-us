4ef4c60d375d8c7694983db9dc08c19771f7a42f
Yash Vijay <yvvijay121@gmail.com>
2022-07-18 07:40:23 -0500

    adding recapture of investment tax credits

--- openfisca_us/variables/gov/states/il/tax/income/il_income_tax_before_credits.py
+++ openfisca_us/variables/gov/states/il/tax/income/il_income_tax_before_credits.py
@@ -11,6 +11,7 @@ class il_income_tax_before_credits(Variable):
     def formula(tax_unit, period, parameters):
         exemption_allowance = tax_unit("il_exemption_allowance", period)
         base_income = tax_unit("il_base_income", period)
+        recapture_of_investment_credit = tax_unit("recapture_of_investment_credit", period)
         rate = parameters(period).gov.states.il.tax.income.rate
 
-        return max(base_income - exemption_allowance, 0) * rate
+        return max(base_income - exemption_allowance, 0) * rate + recapture_of_investment_credit
