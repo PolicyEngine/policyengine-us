5d40697a8b0c14e6b0266858b0ed2c11ffb17132
Matt Jensen <matthjensen@gmail.com>
2022-07-18 09:33:35 -0400

    cleanup

--- openfisca_us/variables/gov/states/in/tax/income/deductions/in_nonpublic_school_deduction.py
+++ openfisca_us/variables/gov/states/in/tax/income/deductions/in_nonpublic_school_deduction.py
@@ -14,12 +14,10 @@ class in_nonpublic_school_deduction(Variable):
             parameters(period)
             .gov.states["in"]
             .tax.income.deductions.nonpublic_school
-        )
-        person = tax_unit.members
-        child = person("is_in_k12_nonpublic_school", period) * 1
-        children = tax_unit.sum(child)
+            )
+        children = add(tax_unit, period, ["is_in_k12_nonpublic_school"])
         # Law specifices dependent children who attended a nonpublic school in Indiana
         # for 180 days or more and for whom non-reimbursed education expenditures were made;
         # using a national var here to save mem
 
-        return children * p.amount
+        return (children * p.amount)
