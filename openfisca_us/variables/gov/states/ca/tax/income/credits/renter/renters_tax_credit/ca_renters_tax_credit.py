6ff29c274439ed9b362b41c8f1422d509b550060
Josh Marsh <josh.j.marsh@gmail.com>
2022-07-18 11:32:55 +1000

    fix bug in cal rent py files

--- openfisca_us/variables/gov/states/ca/tax/income/credits/renter/renters_tax_credit/ca_renters_tax_credit.py
+++ openfisca_us/variables/gov/states/ca/tax/income/credits/renter/renters_tax_credit/ca_renters_tax_credit.py
@@ -13,12 +13,11 @@ class ca_renters_tax_credit(Variable):
         # Check eligibility based on state, rent, filing status, and income.
         p = parameters(period).gov.states.ca.tax.income.credits.renter
         agi = tax_unit("adjusted_gross_income", period)
-        rent = tax_unit("rent", period)
+        has_rent = add(tax_unit, period, ["rent"]) > 0
         filing_status = tax_unit("filing_status", period)
         in_ca = tax_unit.household("state_code_str", period) == "CA"
         income_cap = p.income_cap[filing_status]
         income_eligible = agi <= income_cap
-        has_rent = rent > 0
         eligible = in_ca & income_eligible & has_rent
         # Determine amount if eligible based on filing status.
         amount_if_eligible = p.amount[filing_status]
