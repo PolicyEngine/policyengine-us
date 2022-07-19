ffd08f4725908e3db545e3068add82bc1e1c4b79
Max Ghenis <mghenis@gmail.com>
2022-07-18 17:00:45 -0700

    Fix bugs

--- openfisca_us/variables/gov/states/md/tax/income/credits/eitc/md_refundable_eitc.py
+++ openfisca_us/variables/gov/states/md/tax/income/credits/eitc/md_refundable_eitc.py
@@ -11,7 +11,7 @@ class md_refundable_eitc(Variable):
     reference = "https://casetext.com/statute/code-of-maryland/article-tax-general/title-10-income-tax/subtitle-7-income-tax-credits/section-10-704-effective-until-6302023-for-earned-income"
 
     def formula(tax_unit, period, parameters):
-        eligible = tax_unit("md_qualifies_for_single_childless_eitc", period)
+        eligible = ~tax_unit("md_qualifies_for_single_childless_eitc", period)
         non_refundable_eitc = tax_unit("md_non_refundable_eitc", period)
         p = parameters(period).gov.states.md.tax.income.credits.eitc
         income_tax = tax_unit("md_income_tax_before_credits", period)
