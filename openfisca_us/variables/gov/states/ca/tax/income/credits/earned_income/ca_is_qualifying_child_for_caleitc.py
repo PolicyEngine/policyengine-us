1378921227685e0d5d08e5d2284a419fad64f9b8
Josh <josh.j.marsh@gmail.com>
2022-07-20 12:08:30 +1000

    fixed young child tax credit Co-authored-by: Max Ghenis <mghenis@gmail.com>

--- openfisca_us/variables/gov/states/ca/tax/income/credits/earned_income/ca_is_qualifying_child_for_caleitc.py
+++ openfisca_us/variables/gov/states/ca/tax/income/credits/earned_income/ca_is_qualifying_child_for_caleitc.py
@@ -1,9 +1,8 @@
 from openfisca_us.model_api import *
 
-class ca_young_child(Variable):
-    value_type = float
-    entity = TaxUnit
-    label = "CA earned income tax credit"
-    unit = USD
+class ca_is_qualifying_child_for_caleitc(Variable):
+    value_type = bool
+    entity = Person
+    label = "Child qualifies for CalEITC"
     definition_period = YEAR
     reference = "https://www.ftb.ca.gov/file/personal/credits/california-earned-income-tax-credit.html#What-you-ll-get"
--- openfisca_us/variables/gov/states/ca/tax/income/credits/young_child/ca_young_child_tax_credit.py
+++ openfisca_us/variables/gov/states/ca/tax/income/credits/young_child/ca_young_child_tax_credit.py
@@ -6,29 +6,42 @@
 # https://github.com/PolicyEngine/openfisca-us/blob/master/openfisca_us/variables/gov/irs/credits/cdcc/cdcc_rate.py
 
 from openfisca_us.model_api import *
+import numpy as np
 
 
 class ca_young_child(Variable):
     value_type = float
     entity = TaxUnit
-    label = "CA young child tax credit"
+    label = "CA Young Child Tax Credit"
     unit = USD
     definition_period = YEAR
-    reference = ("https://www.ftb.ca.gov/forms/2021/2021-3514.pdf#page=3")
+    reference = ("https://www.ftb.ca.gov/forms/2021/2021-3514.pdf#page=3", "https://leginfo.legislature.ca.gov/faces/codes_displaySection.xhtml?lawCode=RTC&sectionNum=17052.1", "https://www.ftb.ca.gov/forms/2021/2021-3514-instructions.html")
 
-    def formula(tax_unit, period, parameters, ca_is_eligible_for_caleitc):
+    def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states.ca.tax.income.credits.young_child
-        agi = tax_unit("adjusted_gross_income", period)
+        earnings = tax_unit("tax_unit_earned_income", period)
 
-        # check if eligible
-        has_eligible_child = False
+        # Eligibility requires
+        # a) tax unit receives CalEITC
+        # b) tax unit has at least one CalEITC-qualifying child under six
+        person = tax_unit.members
+        meets_age_limit = person("age", period) < p.max_age
+        is_qualifying_child_for_caleitc = person("ca_is_qualifying_child_for_caleitc", period)
+        is_eligible_child = meets_age_limit & is_qualifying_child_for_caleitc
+        has_eligible_child = tax_unit.sum(is_eligible_child) > 0
+        receives_ca_eitc = tax_unit("ca_eitc", period) > 0
         in_ca = tax_unit.household("state_code_str", period) == "CA"
-        eligible = ca_is_eligible_for_caleitc & in_ca & has_eligible_child
-
-        # First phase-out
-        excess_agi = max_(0, agi - p.phase_out.start)
-        increments = excess_agi / p.phase_out.increment
-        reduction = increments * p.phase_out.amount
+        eligible = has_eligible_child & receives_ca_eitc & in_ca
+        # Phase out the amount.
+        # NB: Law and instructions say $20 per $100, but the tax form
+        # instructs the filer to round.
+        excess_earnings = max_(0, earnings - p.phase_out.start)
+        # Round increments to two decimal places.
+        increments = np.round_(excess_earnings / p.phase_out.increment, 2)
+        # Round reduction to two decimal places.
+        reduction = np.round_(increments * p.phase_out.amount, 2)
+        # Round final result to nearest dollar.
+        # In reality, <$1 goes to $1.
         return eligible * np.rint(p.amount -  reduction)
         
 
