391020c813f4d25660912164cc0863b755e3c49c
Matt Jensen <matthjensen@gmail.com>
2022-07-24 14:17:56 -0400

    cleanup

---
+++ openfisca_us/variables/gov/states/in/tax/income/taxes/in_county_tax.py
@@ -0,0 +1,17 @@
+from openfisca_us.model_api import *
+
+
+class in_county_tax(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "IN county tax"
+    definition_period = YEAR
+    unit = USD
+    reference = "http://iga.in.gov/legislative/laws/2021/ic/titles/006#6-3-2-1"  # (a)(3)
+
+    def formula(tax_unit, period, parameters):
+        p = parameters(period).gov.states["in"].tax.income.taxes.county
+        in_agi = tax_unit("in_agi", period)
+        # county calculations are at the person level for each taxpayer in the law
+        county = tax_unit.household("county_enum", period)
+        return in_agi * p.rates[county]
