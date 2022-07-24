590f87ee8d1066e5996fb5798256ba6bb3e8304b
Matt Jensen <matthjensen@gmail.com>
2022-07-24 12:43:21 -0400

    Format and changelog

--- openfisca_us/variables/gov/states/in/tax/income/deductions/Taxes/in_agi_tax.py
+++ openfisca_us/variables/gov/states/in/tax/income/deductions/Taxes/in_agi_tax.py
@@ -10,10 +10,6 @@ class in_agi_tax(Variable):
     reference = "http://iga.in.gov/legislative/laws/2021/ic/titles/006#6-3-2-1"  # (a)(3)
 
     def formula(tax_unit, period, parameters):
-        p = (
-            parameters(period)
-            .gov.states["in"]
-            .tax.income.taxes.in_agi_tax
-        )
+        p = parameters(period).gov.states["in"].tax.income.taxes.in_agi_tax
         in_agi = tax_unit("in_agi", period)
         return in_agi * p.rate
