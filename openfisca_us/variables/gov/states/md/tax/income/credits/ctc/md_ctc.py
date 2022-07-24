2e51021675901119c9ac246c6399328c7ad6360f
William Fedder <fedderw@gmail.com>
2022-07-24 19:45:05 -0400

    add leg text details to md ctc

--- openfisca_us/variables/gov/states/md/tax/income/credits/ctc/md_ctc.py
+++ openfisca_us/variables/gov/states/md/tax/income/credits/ctc/md_ctc.py
@@ -12,6 +12,7 @@ class md_ctc(Variable):
     defined_for = StateCode.MD
 
     def formula(tax_unit, period, parameters):
+        "Md. Code, Tax-Gen. § 10-751 (c) The amount of the credit allowed under subsection (b) of this section for a qualified child shall be reduced, but not below zero, by the amount of any federal child tax credit claimed against the federal income tax for the qualified child under § 24 of the Internal Revenue Code. (d) If the credit allowed under this section in any taxable year exceeds the State income tax for that taxable year, the taxpayer may claim a refund in the amount of the excess."
         md_ctc = tax_unit("md_ctc_without_federal", period)
         federal_ctc = tax_unit("ctc", period)
         return max_(md_ctc - federal_ctc, 0)
