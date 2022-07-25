9754ea20123645263c308a8e0ed5ad5f5eaa419e
Matt Jensen <matthjensen@gmail.com>
2022-07-25 07:34:26 -0400

    cleanup

--- openfisca_us/variables/gov/states/in/tax/income/taxes/in_county_tax.py
+++ openfisca_us/variables/gov/states/in/tax/income/taxes/in_county_tax.py
@@ -8,7 +8,8 @@ class in_county_tax(Variable):
     definition_period = YEAR
     unit = USD
     reference = "http://iga.in.gov/legislative/laws/2021/ic/titles/006#6-3-2-1"  # (a)(3)
-defined_for = StateCode.IN
+    defined_for = StateCode.IN
+
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states["in"].tax.income.taxes.county
         in_agi = tax_unit("in_agi", period)
