f207c1c0cebd2ab95c79cf7eb3bf79f7a20be44c
Yash Vijay <yvvijay121@gmail.com>
2022-07-28 12:20:35 -0500

    changed, since the IL base income only considers taxable social security benefits

--- openfisca_us/variables/gov/states/il/tax/income/il_base_income.py
+++ openfisca_us/variables/gov/states/il/tax/income/il_base_income.py
@@ -11,5 +11,5 @@ class il_base_income(Variable):
 
     def formula(tax_unit, period, parameters):
         ADDITIONS = ["adjusted_gross_income", "tax_exempt_interest_income", "dividend_income", "il_schedule_m_additions"]
-        SUBTRACTIONS = ["social_security_benefits", "schedule_m_subtractions"]
+        SUBTRACTIONS = ["taxable_ss", "schedule_m_subtractions"]
         return add(tax_unit, period, ADDITIONS) - add(tax_unit, period, SUBTRACTIONS)
