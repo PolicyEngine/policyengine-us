b15651788cf3b0ef21a171dca7ff443b86fdb25d
Max Ghenis <mghenis@gmail.com>
2022-08-01 13:00:41 -0700

    Tests

--- openfisca_us/variables/contrib/congress/senate/democrats/inflation_reduction_act/eligible_used_electric_vehicle_credit.py
+++ openfisca_us/variables/contrib/congress/senate/democrats/inflation_reduction_act/eligible_used_electric_vehicle_credit.py
@@ -9,12 +9,16 @@ class eligible_used_electric_vehicle_credit(Variable):
     documentation = "Eligible for nonrefundable credit for the purchase of a used electric vehicle"
     unit = USD
     reference = "https://www.democrats.senate.gov/imo/media/doc/inflation_reduction_act_of_2022.pdf#page=370"
-    defined_for = "purchased_used_electric_vehicle"
+    defined_for = "purchased_qualifying_used_electric_vehicle"
 
     def formula(tax_unit, period, parameters):
-        p = parameters(
+        ira = p = parameters(
             period
-        ).contrib.congress.senate.democrats.inflation_reduction_act.electric_vehicle_credit.used
+        ).contrib.congress.senate.democrats.inflation_reduction_act
+        # Current law does not allow a used EV credit.
+        if not ira.in_effect:
+            return False
+        p = ira.electric_vehicle_credit.used
         # Income eligibility based on lesser of MAGI in current and prior year.
         # Assume AGI in current year for now.
         agi = tax_unit("adjusted_gross_income", period)
