06906f0753a34079a5039555c2c09d850593c2a3
Max Ghenis <mghenis@gmail.com>
2022-08-01 12:33:58 -0700

    Add EV variables

--- openfisca_us/variables/contrib/congress/senate/democrats/inflation_reduction_act/used_electric_vehicle_credit.py
+++ openfisca_us/variables/contrib/congress/senate/democrats/inflation_reduction_act/used_electric_vehicle_credit.py
@@ -11,21 +11,13 @@ class used_electric_vehicle_credit(Variable):
     )
     unit = USD
     reference = "https://www.democrats.senate.gov/imo/media/doc/inflation_reduction_act_of_2022.pdf#page=370"
+    defined_for = "eligible_used_electric_vehicle_credit"
 
     def formula(tax_unit, period, parameters):
         p = parameters(
             period
         ).contrib.congress.senate.democrats.inflation_reduction_act.electric_vehicle_credit.used
-        # Income eligibility based on lesser of MAGI in current and prior year.
-        # Assume AGI in current year for now.
-        agi = tax_unit("adjusted_gross_income", period)
-        filing_status = tax_unit("filing_status", period)
-        income_limit = p.eligibility.income_limit[filing_status]
-        income_eligible = agi <= income_limit
-        # Purchase price limit.
         sale_price = tax_unit("used_electric_vehicle_sale_price", period)
-        price_eligible = price <= p.eligibility.sale_price_limit
         # Amount is lesser of $4,000 and 30% of sale price.
         uncapped_amount = sale_price * p.amount.percent_of_sale_price
-        amount_if_eligible = min_(uncapped_amount, p.amount.max)
-        return income_eligible * price_eligible * amount_if_eligible
+        return min_(uncapped_amount, p.amount.max)
--- openfisca_us/variables/gov/irs/credits/electric_vehicle/new_electric_vehicle_credit_eligible.py
+++ openfisca_us/variables/gov/irs/credits/electric_vehicle/new_electric_vehicle_credit_eligible.py
@@ -12,4 +12,18 @@ class new_electric_vehicle_credit_eligible(Variable):
     defined_for = "purchased_qualifying_new_electric_vehicle"
 
     def formula(tax_unit, period, parameters):
-        return True
+        ira = parameters(
+            period
+        ).contrib.congress.senate.democrats.inflation_reduction_act
+        # The Inflation Reduction Act introduces income and MSRP limits.
+        if not ira.in_effect:
+            return True
+        p = ira.electric_vehicle_credit.new.eligibility
+        agi = tax_unit("adjusted_gross_income", period)
+        meets_income_limit = agi <= p.income_limit
+        msrp = tax_unit("new_electric_vehicle_msrp", period)
+        classification = tax_unit(
+            "new_electric_vehicle_classification", period
+        )
+        meets_msrp_limit = msrp <= p.msrp_limit[classification]
+        return meets_income_limit & meets_msrp_limit
