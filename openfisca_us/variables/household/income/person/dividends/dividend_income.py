2ca538f2388c73f47b86e2495ea25a762e7562bc
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-08-02 11:04:49 +0100

    Fix logic

--- openfisca_us/variables/household/income/person/dividends/dividend_income.py
+++ openfisca_us/variables/household/income/person/dividends/dividend_income.py
@@ -15,16 +15,13 @@ class dividend_income(Variable):
             "qualified_dividend_income", period
         )
         non_qualified_dividends = simulation.get_array(
-            "non_qualified_dividends", period
+            "non_qualified_dividend_income", period
         )
+        zeros = np.zeros(person.count)
         if qualified_dividends is None and non_qualified_dividends is None:
-            raise ValueError(
-                f"You did not provide an input for qualified dividends (`qualified_dividend_income`), non-qualified dividends (`non_qualified_dividends`), or total dividends (`dividend_income`). Please provide at least one of these inputs: if you provide total dividends, the model will impute qualified and ordinary dividends from that by splitting according to the percentage of dividends which is qualified from IRS statistics; if you provide qualified dividends, ordinary dividends, or both (if you provide one, we assume the other to be zero), the model calculates total dividends as the sum of those variables."
-            )
+            return zeros
         if qualified_dividends is None:
-            qualified_dividends = dividend_income.default_array(person.count)
+            qualified_dividends = zeros
         if non_qualified_dividends is None:
-            non_qualified_dividends = dividend_income.default_array(
-                person.count
-            )
+            non_qualified_dividends = zeros
         return qualified_dividends + non_qualified_dividends
