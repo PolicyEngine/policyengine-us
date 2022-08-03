ea48399794ac91524fa7d7acbec9166389d340a1
Max Ghenis <mghenis@gmail.com>
2022-08-03 12:34:13 -0700

    Add missing PolicyEngine metadata for electric vehicle variables and parameters.

--- openfisca_us/variables/household/expense/vehicle/new_electric_vehicle_battery_capacity.py
+++ openfisca_us/variables/household/expense/vehicle/new_electric_vehicle_battery_capacity.py
@@ -5,6 +5,7 @@ class new_electric_vehicle_battery_capacity(Variable):
     value_type = float
     entity = TaxUnit
     label = "Battery capacity of a purchased new electric vehicle"
+    documentation = "In kilowatt-hours (kWh)"
     unit = "kWh"
     definition_period = YEAR
     quantity_type = STOCK
