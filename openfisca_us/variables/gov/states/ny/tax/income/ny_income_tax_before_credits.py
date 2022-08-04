d734285cf08fcf0be4f2bd5d3e0c587ad72e6357
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-08-04 14:27:45 +0100

    Add formula handling for statuses

--- openfisca_us/variables/gov/states/ny/tax/income/ny_income_tax_before_credits.py
+++ openfisca_us/variables/gov/states/ny/tax/income/ny_income_tax_before_credits.py
@@ -11,10 +11,28 @@ class ny_income_tax_before_credits(Variable):
     def formula(tax_unit, period, parameters):
         taxable_income = tax_unit("ny_taxable_income", period)
         filing_status = tax_unit("filing_status", period)
+        status = filing_status.possible_values
 
         rates = parameters(period).gov.states.ny.tax.income.rates
+        single = rates.single
+        joint = rates.joint
+        hoh = rates.head_of_household
+        widow = rates.widow
+        separate = rates.separate
 
-        return rates.single.calc(taxable_income)
-
-
-
+        return select(
+            [
+                filing_status == status.SINGLE,
+                filing_status == status.JOINT,
+                filing_status == status.HEAD_OF_HOUSEHOLD,
+                filing_status == status.WIDOW,
+                filing_status == status.SEPARATE,
+            ],
+            [
+                single.calc(taxable_income),
+                joint.calc(taxable_income),
+                hoh.calc(taxable_income),
+                widow.calc(taxable_income),
+                separate.calc(taxable_income),
+            ],
+        )
