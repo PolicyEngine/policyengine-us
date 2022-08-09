469c0ae4ee0e64f58d1ece39ba39d43e4dc023c5
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-08-09 19:06:27 +0100

    Add phaseout start

--- openfisca_us/variables/gov/states/ny/tax/income/taxable_income/ny_itemized_deductions_reduction.py
+++ openfisca_us/variables/gov/states/ny/tax/income/taxable_income/ny_itemized_deductions_reduction.py
@@ -1,7 +1,7 @@
 from openfisca_us.model_api import *
 
 
-class ny_itemized_deductions(Variable):
+class ny_itemized_deductions_reduction(Variable):
     value_type = float
     entity = TaxUnit
     label = "NY uncapped itemized deductions"
@@ -11,19 +11,5 @@ class ny_itemized_deductions(Variable):
     defined_for = StateCode.NY
 
     def formula(tax_unit, period, parameters):
-        gov = parameters(period).gov
-        federal_deductions_if_itemizing = (
-            gov.irs.deductions.deductions_if_itemizing
-        )
-        federal_deductions_if_itemizing = [
-            deduction
-            for deduction in federal_deductions_if_itemizing
-            if deduction
-            not in [
-                "salt_deduction",
-                "qualified_business_income_deduction",
-            ]
-        ]
-        return add(
-            tax_unit, period, federal_deductions_if_itemizing
-        )
+        federal_agi = tax_unit("adjusted_gross_income", period)
+        
