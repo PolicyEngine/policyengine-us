8357e8d1406a6a3ed74ea2dcb4553e83df5d1157
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-08-11 11:20:10 +0100

    Add NY CDCC

---
+++ openfisca_us/variables/gov/states/ny/tax/income/credits/cdcc/ny_cdcc_rate.py
@@ -0,0 +1,49 @@
+from openfisca_us.model_api import *
+
+
+class ny_cdcc_rate(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "NY CDCC rate"
+    unit = USD
+    definition_period = YEAR
+    reference = "https://www.nysenate.gov/legislation/laws/TAX/606"  # (c)
+    defined_for = StateCode.NY
+
+    def formula(tax_unit, period, parameters):
+        ny_agi = tax_unit("ny_agi", period)
+        federal_rate = tax_unit("cdcc_rate", period)
+
+        ny_cdcc = parameters(period).gov.states.ny.tax.income.credits.cdcc
+        first_threshold = ny_cdcc.multiplier.phase_out.first.start
+        second_threshold = ny_cdcc.multiplier.phase_out.first.end
+        below_main_phase_out = ny_agi <= first_threshold
+        in_main_phase_out = ny_agi > first_threshold
+        in_second_phase_out = ny_agi > second_threshold
+        percent_along_first_phase_out = (ny_agi - first_threshold) / (
+            second_threshold - first_threshold
+        )
+        multiplier_max = ny_cdcc.multiplier.max
+        # Interpolate between the max and 1
+        interpolated_first_phase_out_rate = multiplier_max + (
+            percent_along_first_phase_out * (1 - multiplier_max)
+        )
+        second_phase_out_rate = ny_cdcc.phase_out.second.rate
+        multiplier_floor = ny_cdcc.multiplier.addition
+        additional_multiplier = ny_cdcc.additional_multiplier.calc(ny_agi)
+
+        return select(
+            [
+                below_main_phase_out,
+                in_main_phase_out,
+                in_second_phase_out,
+            ],
+            [
+                multiplier_max,
+                interpolated_first_phase_out_rate,
+                min_(
+                    multiplier_floor,
+                    1 - second_phase_out_rate * ny_agi / 1_000,
+                ),
+            ],
+        )
