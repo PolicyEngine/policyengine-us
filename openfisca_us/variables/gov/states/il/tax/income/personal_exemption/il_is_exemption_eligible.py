08a3e308b07a794b0bd60c68632790d9cf619c42
Yash Vijay <yvvijay121@gmail.com>
2022-08-14 13:48:58 -0500

    joint/nonjoint > filing statuses in cap.yaml

--- openfisca_us/variables/gov/states/il/tax/income/personal_exemption/il_is_exemption_eligible.py
+++ openfisca_us/variables/gov/states/il/tax/income/personal_exemption/il_is_exemption_eligible.py
@@ -13,10 +13,5 @@ class il_is_exemption_eligible(Variable):
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states.il.tax.income.exemption.cap
         filing_status = tax_unit("filing_status", period)
-        joint = where(
-            filing_status == filing_status.possible_values.JOINT,
-            "joint",
-            "nonjoint",
-        )
 
-        return tax_unit("adjusted_gross_income", period) < p[joint]
+        return tax_unit("adjusted_gross_income", period) < p[filing_status]
