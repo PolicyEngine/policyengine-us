0689a09fd23f1ddf2d9c07e6bc520af5b227160e
Yash Vijay <yvvijay121@gmail.com>
2022-08-14 14:26:43 -0500

    accidental github commit suggestion mistake

--- openfisca_us/variables/gov/states/il/tax/income/personal_exemption/il_total_exemption.py
+++ openfisca_us/variables/gov/states/il/tax/income/personal_exemption/il_total_exemption.py
@@ -11,14 +11,3 @@ class il_total_exemption(Variable):
     defined_for = "il_is_exemption_eligible"
     
     formula = sum_of_variables(["il_personal_exemption","il_aged_blind_exemption", "il_dependent_exemption"])
-
-    def formula(tax_unit, period, parameters):
-        total_amount = (
-            tax_unit("il_personal_exemption", period)
-            + tax_unit("il_aged_blind_exemption", period)
-            + tax_unit("il_dependent_exemption", period)
-        )
-
-        return where(
-            tax_unit("il_is_exemption_eligible", period), total_amount, 0
-        )
