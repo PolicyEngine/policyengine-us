d070104192a938a50bcc5da232072faa63264bf1
Max Ghenis <mghenis@gmail.com>
2022-08-18 01:41:46 -0700

    Change date to 2023

--- openfisca_us/variables/gov/irs/credits/residential_energy/nonbusiness/improvements/qualified_energy_efficiency_improvements_expenditures.py
+++ openfisca_us/variables/gov/irs/credits/residential_energy/nonbusiness/improvements/qualified_energy_efficiency_improvements_expenditures.py
@@ -7,13 +7,11 @@ class qualified_energy_efficiency_improvements_expenditures(Variable):
     definition_period = YEAR
     documentation = "Expenditures on qualified energy efficiency improvements"
     unit = USD
-    reference = "https://www.law.cornell.edu/uscode/text/26/25C#c"
+    reference = (
+        "https://www.law.cornell.edu/uscode/text/26/25C#c",
+        "https://www.democrats.senate.gov/imo/media/doc/inflation_reduction_act_of_2022.pdf#page=340",
+    )
 
     formula = sum_of_variables(
-        [
-            "capped_energy_efficient_door_expenditures",
-            "energy_efficient_insulation_expenditures",
-            "energy_efficient_roof_expenditures",
-            "capped_energy_efficient_window_expenditures",
-        ]
+        "gov.irs.credits.residential_energy.nonbusiness.qualified_expenditures.energy_efficiency_improvements"
     )
---
+++ openfisca_us/variables/gov/irs/credits/residential_energy/nonbusiness/property/capped_heat_pump_water_heater_expenditures.py
@@ -0,0 +1,17 @@
+from openfisca_us.model_api import *
+
+
+class capped_heat_pump_water_heater_expenditures(Variable):
+    value_type = float
+    entity = TaxUnit
+    definition_period = YEAR
+    documentation = (
+        "Capped electric or natural gas heat pump water heater expenditures"
+    )
+    unit = USD
+    reference = "https://www.democrats.senate.gov/imo/media/doc/inflation_reduction_act_of_2022.pdf#page=340"
+
+    def formula(tax_unit, period, parameters):
+        uncapped = tax_unit("heat_pump_water_heater_expenditures", period)
+        p = parameters(period).gov.irs.credits.residential_energy.nonbusiness
+        return min_(uncapped, p.cap.annual.heat_pumps_stoves_boilers)
