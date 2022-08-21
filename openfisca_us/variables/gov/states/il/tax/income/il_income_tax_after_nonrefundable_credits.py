b59df68242ce10daa17372524212ba05317dc142
Yash Vijay <yvvijay121@gmail.com>
2022-08-21 11:00:02 -0500

    added income tax var after nonrefundable credits

---
+++ openfisca_us/variables/gov/states/il/tax/income/il_income_tax_after_nonrefundable_credits.py
@@ -0,0 +1,16 @@
+from openfisca_us.model_api import *
+
+class il_income_tax_after_nonrefundable_credits(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "IL income tax after nonrefundable credits"
+    unit = USD
+    definition_period = YEAR
+    reference = ""
+    defined_for = StateCode.IL
+
+    def formula(tax_unit, period, parameters):
+        income_tax_before_credits = tax_unit("il_income_tax_before_nonrefundable_credits", period)
+        nonrefundable_credits = tax_unit("il_nonrefundable_credits", period)
+
+        return income_tax_before_credits - nonrefundable_credits
