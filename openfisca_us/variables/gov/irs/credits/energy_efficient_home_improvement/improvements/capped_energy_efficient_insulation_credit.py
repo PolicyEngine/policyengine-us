8ca2cad1d81bc6f1dc3d04d5370f078899997c3c
Max Ghenis <mghenis@gmail.com>
2022-08-21 07:50:13 -0700

    Cap energy efficient improvements by credit value

--- openfisca_us/variables/gov/irs/credits/energy_efficient_home_improvement/improvements/capped_energy_efficient_insulation_credit.py
+++ openfisca_us/variables/gov/irs/credits/energy_efficient_home_improvement/improvements/capped_energy_efficient_insulation_credit.py
@@ -1,21 +1,21 @@
 from openfisca_us.model_api import *
 
 
-class capped_energy_efficient_window_credit(Variable):
+class capped_energy_efficient_insulation_credit(Variable):
     value_type = float
     entity = TaxUnit
     definition_period = YEAR
-    documentation = (
-        "Capped credit on energy-efficient exterior window and skylights"
-    )
+    documentation = "Capped credit on energy-efficient insulation material"
     unit = USD
     reference = "https://www.democrats.senate.gov/imo/media/doc/inflation_reduction_act_of_2022.pdf#page=339"
 
     def formula(tax_unit, period, parameters):
-        expenditure = tax_unit("energy_efficient_window_expenditures", period)
+        expenditure = tax_unit(
+            "energy_efficient_insulation_expenditures", period
+        )
         p = parameters(
             period
         ).gov.irs.credits.energy_efficient_home_improvement
         rate = p.rates.improvements
         uncapped = expenditure * rate
-        return min_(uncapped, p.cap.annual.window)
+        return min_(uncapped, p.cap.annual.insulation)
