be10b1e64a87a494c4055f02a52090a5464a6049
Yash Vijay <yvvijay121@gmail.com>
2022-08-21 09:01:59 -0500

    changed structure of var for naming standards

--- openfisca_us/variables/gov/states/il/tax/income/personal_exemption/il_is_exemption_eligible.py
+++ openfisca_us/variables/gov/states/il/tax/income/personal_exemption/il_is_exemption_eligible.py
@@ -11,7 +11,7 @@ class il_is_exemption_eligible(Variable):
     defined_for = StateCode.IL
 
     def formula(tax_unit, period, parameters):
-        p = parameters(period).gov.states.il.tax.income.exemption.cap
         filing_status = tax_unit("filing_status", period)
-
-        return tax_unit("adjusted_gross_income", period) < p[filing_status]
+        cap = parameters(period).gov.states.il.tax.income.exemption.cap[filing_status]
+        
+        return tax_unit("adjusted_gross_income", period) < cap
