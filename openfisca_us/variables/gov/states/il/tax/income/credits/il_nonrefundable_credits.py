780cf46dc738339d09e4b39268f11af922e9178d
Yash Vijay <yvvijay121@gmail.com>
2022-08-21 10:55:53 -0500

    new nonrefundable credits variable

---
+++ openfisca_us/variables/gov/states/il/tax/income/credits/il_nonrefundable_credits.py
@@ -0,0 +1,12 @@
+from openfisca_us.model_api import *
+
+
+class il_nonrefundable_credits(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "IL nonrefundable credits"
+    unit = USD
+    definition_period = YEAR
+    reference = ""
+
+    formula = sum_of_variables("gov.states.il.tax.income.credits.list_of_nonrefundable_credits")
