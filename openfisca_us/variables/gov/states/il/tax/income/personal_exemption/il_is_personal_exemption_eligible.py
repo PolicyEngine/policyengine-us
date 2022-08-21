ff6d163f90dd7ede66482be4164a07acb393296c
Yash Vijay <yvvijay121@gmail.com>
2022-08-21 12:36:43 -0500

    formatting?

--- openfisca_us/variables/gov/states/il/tax/income/personal_exemption/il_is_personal_exemption_eligible.py
+++ openfisca_us/variables/gov/states/il/tax/income/personal_exemption/il_is_personal_exemption_eligible.py
@@ -11,26 +11,47 @@ class il_is_personal_exemption_eligible(Variable):
     value_type = Enum
     possible_values = EligibilityStatus
     entity = TaxUnit
-    label = "Whether this tax unit is eligible for any exemptions"
+    label = "Whether The Tax Unit Is Eligible For The Illinois Personal Exemption"
     definition_period = YEAR
     reference = ""
 
     def formula(tax_unit, period, parameters):
-        p = parameters(period).gov.states.il.tax.income.exemption.personal
+        personal_eligiblity_amounts = parameters(
+            period
+        ).gov.states.il.tax.income.exemption.personal
 
         filing_status = tax_unit("filing_status", period)
-        joint = (filing_status == filing_status.possible_values.JOINT,)
+        joint = (filing_status == filing_status.possible_values.JOINT)
         claimable_count = add(tax_unit, period, ["dsi_spouse", "dsi"])
         il_base_income = tax_unit("il_base_income", period)
 
         ineligible = (
             (not joint)
             & (claimable_count > 0)
-            & (il_base_income > p["nonjoint"])
-        ) | (joint & (claimable_count > 1) & (il_base_income > p["joint"]))
+            & (
+                il_base_income
+                > personal_eligiblity_amounts[
+                    EligibilityStatus.PARTNER_INELIGIBLE
+                ]
+            )
+        ) | (
+            joint
+            & (claimable_count > 1)
+            & (
+                il_base_income
+                > personal_eligiblity_amounts[EligibilityStatus.ELIGIBLE]
+            )
+        )
 
         partner_ineligible = (
-            joint & (claimable_count == 1) & (il_base_income > p["nonjoint"])
+            joint
+            & (claimable_count == 1)
+            & (
+                il_base_income
+                > personal_eligiblity_amounts[
+                    EligibilityStatus.PARTNER_INELIGIBLE
+                ]
+            )
         )
 
         return select(
