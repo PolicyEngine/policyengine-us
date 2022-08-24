03c4ed8d70983c822ce2aa4a48b88b23c8a9df53
Pavel Makarchuk <pavel.ma99@gmail.com>
2022-08-23 20:16:41 -0400

    minimal changes

--- openfisca_us/variables/gov/doe/high_efficiency_electric_home_rebate/capped_heat_pump_rebate.py
+++ openfisca_us/variables/gov/doe/high_efficiency_electric_home_rebate/capped_heat_pump_rebate.py
@@ -1,16 +1,16 @@
 from openfisca_us.model_api import *
 
 
-class capped_electric_stove_cooktop_range_or_oven_rebate(Variable):
+class capped_heat_pump_rebate(Variable):
     value_type = float
     entity = Household
-    label = "Capped electric stove cooktop range or oven rebate"
+    label = "Capped heat pump rebate"
     documentation = "Before total high efficiency electric home rebate cap"
     definition_period = YEAR
     unit = USD
 
     def formula(household, period, parameters):
         percent_covered = household("high_efficiency_electric_home_rebate_percent_covered", period)
-        expenditures = household("electric_stove_cooktop_range_or_oven_expenditures", period)
-        cap = parameters(period).gov.doe.high_efficiency_electric_home_rebate.cap.electric_stove_cooktop_range_or_oven
+        expenditures = household("heat_pump_expenditures", period)
+        cap = parameters(period).gov.doe.high_efficiency_electric_home_rebate.cap.heat_pump
         return min_(expenditures * percent_covered, cap)
