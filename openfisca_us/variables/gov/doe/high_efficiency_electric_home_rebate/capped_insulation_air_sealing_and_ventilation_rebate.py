0e865901dfe667e83a9bff42f46097bc23bab99c
Max Ghenis <mghenis@gmail.com>
2022-08-27 12:38:46 -0700

    fix typo

--- openfisca_us/variables/gov/doe/high_efficiency_electric_home_rebate/capped_insulation_air_sealing_and_ventilation_rebate.py
+++ openfisca_us/variables/gov/doe/high_efficiency_electric_home_rebate/capped_insulation_air_sealing_and_ventilation_rebate.py
@@ -18,7 +18,7 @@ class capped_insulation_air_sealing_and_ventilation_rebate(Variable):
             period,
             # NB: Separated because insulation also receives a tax credit.
             [
-                "energy_efficiency_insulation_expenditures",
+                "energy_efficient_insulation_expenditures",
                 "air_sealing_and_ventilation_expenditures",
             ],
         )
