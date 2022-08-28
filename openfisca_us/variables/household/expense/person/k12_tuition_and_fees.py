5b3c599bd64ba0314a7e41ef0b82971b57eb5b14
Yash Vijay <yvvijay121@gmail.com>
2022-08-28 11:39:33 -0500

    agh v2

---
+++ openfisca_us/variables/household/expense/person/k12_tuition_and_fees.py
@@ -0,0 +1,11 @@
+from openfisca_us.model_api import *
+
+
+class k12_tuition_and_fees(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "K-12 Tuition and fees (from Form 8917)"
+    unit = USD
+    definition_period = YEAR
+
+    formula = sum_of_variables("tuition_and_fees")
