c46aed5953ad4bc403a78f3c6066317323903ea1
Yash Vijay <yvvijay121@gmail.com>
2022-08-28 11:07:28 -0500

    maybe this fixes the recursion error?

--- openfisca_us/variables/household/expense/tax/property_tax_primary_residence.py
+++ openfisca_us/variables/household/expense/tax/property_tax_primary_residence.py
@@ -3,9 +3,11 @@ from openfisca_us.model_api import *
 
 class property_tax_primary_residence(Variable):
     value_type = float
-    entity = Person
+    entity = TaxUnit
     label = "Property tax for the real estate that includes the principal residence"
     unit = USD
     definition_period = YEAR
 
-    formula = sum_of_variables(["property_tax_primary_residence"])
+    def formula(tax_unit, period, parameters):
+        person = tax_unit("person", period)
+        return tax_unit.sum(person("property_tax", period))
