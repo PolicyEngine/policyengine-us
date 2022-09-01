406c1800e9f39c170c9921acc32f256c1e5d123b
nmrodelo <nmrodelo@bu.edu>
2022-08-31 22:16:28 -0400

    test federal tax

--- openfisca_us/variables/gov/states/mo/tax/income/deductions/mo_federal_income_tax_deduction.py
+++ openfisca_us/variables/gov/states/mo/tax/income/deductions/mo_federal_income_tax_deduction.py
@@ -21,4 +21,5 @@ class mo_federal_income_tax_deduction(Variable):
         federal_income_tax_deduction_amount = federal_tax * rate
 
         federal_income_tax_deduction_cap = parameters(period).gov.states.mo.tax.income.deductions.mo_federal_income_tax_deduction_caps[filing_status]        
-        return min_(federal_income_tax_deduction_amount, federal_income_tax_deduction_cap)
+        #min_(federal_income_tax_deduction_amount, federal_income_tax_deduction_cap)
+        return federal_tax
