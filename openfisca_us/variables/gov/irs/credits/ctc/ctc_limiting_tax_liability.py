63f0ee4ff29f32fd069c1135b90426a613a3fe0f
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-09-03 12:59:37 +0100

    Ignore SALT when computing CTC-limiting liability

--- openfisca_us/variables/gov/irs/credits/ctc/ctc_limiting_tax_liability.py
+++ openfisca_us/variables/gov/irs/credits/ctc/ctc_limiting_tax_liability.py
@@ -1,12 +1,12 @@
 from openfisca_us.model_api import *
-
+from openfisca_core.taxbenefitsystems import TaxBenefitSystem
 
 class ctc_limiting_tax_liability(Variable):
     value_type = float
     entity = TaxUnit
     label = "CTC-limiting tax liability"
     unit = USD
-    documentation = "The tax liability used to determine the maximum amount of the non-refundable CTC."
+    documentation = "The tax liability used to determine the maximum amount of the non-refundable CTC. Excludes SALT from all calculations (this is an inaccuracy required to avoid circular dependencies)."
     definition_period = YEAR
 
     def formula(tax_unit, period, parameters):
@@ -20,4 +20,7 @@ class ctc_limiting_tax_liability(Variable):
                 if credit != "non_refundable_ctc"
             ]
         )
-        return tax_unit("income_tax_before_credits", period) - total_credits
+        simulation = tax_unit.simulation.clone()
+        simulation.tax_benefit_system.neutralize_variable("salt_deduction")
+        tax_liability = simulation.calculate("income_tax_before_credits", period)
+        return max_(0, tax_liability - total_credits)
