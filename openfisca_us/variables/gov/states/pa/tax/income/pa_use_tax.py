64db8ac4a394d4079a680976b2f94f2ff63bd1c1
Pavel Makarchuk <pavel.ma99@gmail.com>
2022-09-08 14:31:14 -0400

    test fixes

--- openfisca_us/variables/gov/states/pa/tax/income/pa_use_tax.py
+++ openfisca_us/variables/gov/states/pa/tax/income/pa_use_tax.py
@@ -16,23 +16,25 @@ class pa_use_tax(Variable):
         # - Philadelphia (county==city)
         # - Allegheny County
         # - Rest of PA
-        county = tax_unit.household("county", period)
-        counties = county.possible_values
-        philadelphia = county == counties.PHILADELPHIA_COUNTY_PA
-        allegheny = county == counties.ALLEGHENY_COUNTY_PA
+        county = tax_unit.household("county_str", period)
+        # counties = county.possible_values
+
+        philadelphia = county == "PHILADELPHIA_COUNTY_PA"
+        allegheny = county == "ALLEGHENY_COUNTY_PA"
         geo_list = [philadelphia, allegheny]
         p = parameters(period).gov.states.pa.tax.use_tax
         # Compute main amount.
         main_amount = select(
             geo_list,
-            [p.main.philadelphia.calc(income), p.main.allegheny.calc(income)], p.main.rest_of_pa.calc(income)
+            [p.main.philadelphia_county.calc(income), p.main.allegheny_county.calc(income)], p.main.rest_of_pa.calc(income)
         )
         # Compute the uncapped amount based on the higher threshold.
         excess_over_higher_threshold = max_(income - p.higher.threshold, 0)
-        higher_rate = select(geo_list, [p.higher.rate.philadelphia, p.higher.rate.allegheny], p.higher.rate.rest_of_pa)
+        higher_rate = select(geo_list, [p.higher.rate.philadelphia_county, p.higher.rate.allegheny_county], p.higher.rate.rest_of_pa)
         uncapped_higher_amount = excess_over_higher_threshold * higher_rate
         # Cap that amount.
-        higher_cap = select(geo_list, [p.higher.cap.philadelphia, p.higher.cap.allegheny], p.higher.cap.rest_of_pa)
+        higher_cap = select(geo_list, [p.higher.cap.philadelphia_county, p.higher.cap.allegheny_county], p.higher.cap.rest_of_pa)
         higher_amount = min_(uncapped_higher_amount, higher_cap)
         # Return main or higher amount depending on if income exceeds the higher threshold.
         return main_amount + higher_amount
+    
