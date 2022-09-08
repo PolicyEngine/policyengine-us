8bc4097489b407b52b76eb6c860ad90078167811
nmrodelo <nmrodelo@bu.edu>
2022-09-07 23:08:48 -0400

    update mo_itemized_deductions param path

--- openfisca_us/variables/gov/states/mo/tax/income/deductions/mo_itemized_deductions.py
+++ openfisca_us/variables/gov/states/mo/tax/income/deductions/mo_itemized_deductions.py
@@ -17,7 +17,7 @@ class mo_itemized_deductions(Variable):
     def formula(tax_unit, period, parameters):
         total_itemized_federal_deductions = parameters(
             period
-        ).states.mo.tax.income.deductions.itemized
+        ).gov.states.mo.tax.income.deductions.itemized
         deductions = [
             deduction for deduction in total_itemized_federal_deductions
         ]
