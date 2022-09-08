430b9e90236a9b4398fd1abaefc7817d13b908ff
nmrodelo <nmrodelo@bu.edu>
2022-09-07 21:51:23 -0400

    lint

--- openfisca_us/variables/gov/states/mo/tax/income/subtractions/mo_qualified_health_insurance_premiums.py
+++ openfisca_us/variables/gov/states/mo/tax/income/subtractions/mo_qualified_health_insurance_premiums.py
@@ -21,28 +21,37 @@ class mo_qualified_health_insurance_premiums(Variable):
         # taxable_ss_ratio = taxable_ss_benefits / social_security_benefits
 
         person = tax_unit.members
-        total_health_insurance_premiums = add(
-            person, period, ["health_insurance_premiums"]
-        )  # 'self_employed_health_insurance_premiums'
+        # 'self_employed_health_insurance_premiums'
 
         # Federal Schedule A, Line 1
         med_dental_out_of_pocket = person(
             "medical_out_of_pocket_expenses", period
         )
+        total_health_insurance_premiums = add(
+            person, period, ["health_insurance_premiums"]
+        )  # total_health_insurance_premiums is also a primary input to the MO side of calculation, MO Form 5695, Line 8
         # Federal Schedule A, Line 4
         med_expense_deduction = tax_unit("medical_expense_deduction", period)
 
+        # the ratio of federal medical expense deduction to total medical expenses (out of pocket + premiums)
         med_expense_deducted_ratio = where(
-            med_dental_out_of_pocket > 0,
-            round(med_expense_deduction / med_dental_out_of_pocket, 3),
+            (med_dental_out_of_pocket + total_health_insurance_premiums) > 0,
+            round(
+                med_expense_deduction
+                / (med_dental_out_of_pocket + total_health_insurance_premiums),
+                2,
+            ),
             0,
         )
-        premium_portion = (
+
+        # Line 13 of MO Form 5695, represents the portion of medical expenses already deducted via federal tax itemization
+        deducted_portion = (
             total_health_insurance_premiums * med_expense_deducted_ratio
-        )  # Line 13 of MO Form 5695,absolutely no clue what to call this
+        )
+        # subtracts the portion of premiums already deducted from federal tax
         itemized_premiums_amount = (
-            total_health_insurance_premiums - premium_portion
-        )  # same here, the logic is correct but unclear the purpose... TBD
+            total_health_insurance_premiums - deducted_portion
+        )
 
         itemizes = tax_unit("itemizes", period)
         itemized_premiums = itemizes * total_health_insurance_premiums
