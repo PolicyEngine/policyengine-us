b00633e5608890c0727fc8db5978d4456815ea4a
nmrodelo <nmrodelo@bu.edu>
2022-09-15 21:49:51 -0400

    short circuit property_tax_credit in different location

--- openfisca_us/variables/gov/states/mo/tax/income/credits/mo_property_tax_credit/mo_property_tax_credit_amount.py
+++ openfisca_us/variables/gov/states/mo/tax/income/credits/mo_property_tax_credit/mo_property_tax_credit_amount.py
@@ -15,6 +15,8 @@ class mo_property_tax_credit_amount(Variable):
     defined_for = StateCode.MO
 
     def formula(tax_unit, period, parameters):
+        return 0
+        # short circuit until fully modeled
         # Currently not including railroad retirement or veterans benefits.
         rents = tax_unit("rents", period)
         cohabitates = tax_unit("lives_with_joint_filing_spouse", period)
