9f2034d3a0f33849a5faf541ddea2bfa0d5750a2
Kevin Foster <newforza@gmail.com>
2022-09-19 10:00:27 -0700

    Oregon standard deduction Fixes #1307
    Co-authored-by: Max Ghenis <mghenis@gmail.com>

---
+++ openfisca_us/variables/gov/states/or/tax/income/deductions/or_dependent_standard_deduction.py
@@ -0,0 +1,2 @@
+#if oregon earned income + 350 < 1100, return 1100
+#else return earned income + 350 up to filing status maximum
---
+++ openfisca_us/variables/gov/states/or/tax/income/deductions/or_standard_deduction.py
@@ -0,0 +1,2 @@
+#check if dependent, if yes run dependent function and return value, if no return value based on filing status
+#check if 65 or older or blind, if yes add value based on filing status, if no return value
