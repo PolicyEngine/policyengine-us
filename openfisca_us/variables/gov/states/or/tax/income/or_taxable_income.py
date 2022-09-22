6a461b19131af9ba64d2925e48ec31912c57015b
Kevin Foster <newforza@gmail.com>
2022-09-22 09:56:35 -0700

    added Oregon taxable income variables

--- openfisca_us/variables/gov/states/or/tax/income/or_taxable_income.py
+++ openfisca_us/variables/gov/states/or/tax/income/or_taxable_income.py
@@ -8,3 +8,6 @@ class or_taxable_income(Variable):
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.OR
+
+    def formula(tax_unit, period, parameters):
+        return tax_unit("or_income_after_subtractions", period) - tax_unit("or_deductions", period) - 
