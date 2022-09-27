4bd5813ce2b8e9605a88a996f2de450278921ed9
Max Ghenis <mghenis@gmail.com>
2022-09-26 19:11:01 -0700

    Add single UBI parameter Fixes #1404

--- openfisca_us/variables/contrib/ubi_center/basic_income/basic_income_before_phase_out.py
+++ openfisca_us/variables/contrib/ubi_center/basic_income/basic_income_before_phase_out.py
@@ -9,13 +9,15 @@ class basic_income_before_phase_out(Variable):
     definition_period = YEAR
 
     def formula(tax_unit, period, parameters):
-        p = parameters(period).contrib.ubi_center.basic_income
-        # Start with person-level amount.
+        p = parameters(period).contrib.ubi_center.basic_income.amount
+        # Start with flat person-level amount.
+        total_flat_amount = p.person.flat * tax_unit("tax_unit_size", period)
+        # Add per-age person-level amount.
         person = tax_unit.members
         age = person("age", period)
-        amount = p.amount_by_age.calc(age)
-        person_level_amount = tax_unit.sum(amount)
+        amount_by_age = p.by_age.calc(age)
+        total_amount_by_age = tax_unit.sum(amount_by_age)
         # Now compute FPG amount.
         fpg = tax_unit("tax_unit_fpg", period)
         fpg_amount = p.fpg_percent * fpg
-        return person_level_amount + fpg_amount
+        return total_flat_amount + total_amount_by_age + fpg_amount
