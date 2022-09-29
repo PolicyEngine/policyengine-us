a91a945dd8a12f8dac3737520ee4fba2b8cff432
Pavel Makarchuk <pavel.ma99@gmail.com>
2022-09-28 20:41:11 -0400

    California Mental Health Services tax framework

---
+++ openfisca_us/variables/gov/states/ca/tax/income/ca_mental_health_services_tax.py
@@ -0,0 +1,19 @@
+from openfisca_us.model_api import *
+
+
+class ca_mental_health_services_tax(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "CA mental health services tax"
+    defined_for = StateCode.CA
+    unit = USD
+    definition_period = YEAR
+    reference = "https://www.ftb.ca.gov/forms/Search/Home/Confirmation"
+
+    def formula(tax_unit, period, parameters):
+        taxable_income = tax_unit("ca_taxable_income", period)
+        p = parameters(period).gov.states.ca.tax.income
+        rate = parameters(
+            period
+        ).gov.states.ca.tax.income.mental_health_service_tax
+        return taxable_income * rate
