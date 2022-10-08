6ca5789a5c9b8297ebf38c32d49d2dc80e6a9337
Alex Rand <alexrand94@gmail.com>
2022-10-08 14:18:33 -0400

    ran make format

--- openfisca_us/variables/gov/states/ca/tax/income/credits/ca_child_and_dependent_care_expenses_credit.py
+++ openfisca_us/variables/gov/states/ca/tax/income/credits/ca_child_and_dependent_care_expenses_credit.py
@@ -6,12 +6,16 @@ class ca_child_and_dependent_care_expenses_credit(Variable):
     entity = TaxUnit
     label = "California Child and Dependent Care Expenses Credit"
     unit = USD
-    documentation = "https://www.ftb.ca.gov/forms/2020/2020-3506-instructions.html"
+    documentation = (
+        "https://www.ftb.ca.gov/forms/2020/2020-3506-instructions.html"
+    )
     definition_period = YEAR
     defined_for = StateCode.CA
 
     def formula(tax_unit, period, parameters):
-        p = parameters(period).gov.states.ca.tax.income.credits.child_and_dependent_care_expenses
+        p = parameters(
+            period
+        ).gov.states.ca.tax.income.credits.child_and_dependent_care_expenses
         agi = tax_unit("adjusted_gross_income", period)
         federal_cdcc = tax_unit("cdcc", period)
         multiplier_1 = p.multiplier_1.calc(agi)
