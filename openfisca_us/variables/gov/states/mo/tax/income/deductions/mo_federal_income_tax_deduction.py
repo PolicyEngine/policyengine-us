d311fb21e1b60f25c266dc7f66d0cab8fc442eeb
nmrodelo <nmrodelo@bu.edu>
2022-10-12 17:44:47 -0400

    adjust mo_agi reference to use person not tax_unit

--- openfisca_us/variables/gov/states/mo/tax/income/deductions/mo_federal_income_tax_deduction.py
+++ openfisca_us/variables/gov/states/mo/tax/income/deductions/mo_federal_income_tax_deduction.py
@@ -13,8 +13,8 @@ class mo_federal_income_tax_deduction(Variable):
     )
     defined_for = StateCode.MO
 
-    def formula(tax_unit, period, parameters):
-        person = tax_unit.members
+    def formula(person, period, parameters):
+        tax_unit = person.tax_unit
         mo_adjusted_gross_income = person("mo_adjusted_gross_income", period)
         federal_tax = tax_unit("income_tax", period)
 
