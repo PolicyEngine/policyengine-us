1bd31daaeb4b8b5f8ecd11464c4727096d754a92
nmrodelo <nmrodelo@bu.edu>
2022-10-12 19:18:34 -0400

    revert to add formula in mo_federal_income_tax_deduction

--- openfisca_us/variables/gov/states/mo/tax/income/deductions/mo_federal_income_tax_deduction.py
+++ openfisca_us/variables/gov/states/mo/tax/income/deductions/mo_federal_income_tax_deduction.py
@@ -14,8 +14,8 @@ class mo_federal_income_tax_deduction(Variable):
     defined_for = StateCode.MO
 
     def formula(tax_unit, period, parameters):
-        person = tax_unit.members
-        mo_adjusted_gross_income = person("mo_adjusted_gross_income", period)
+        
+        mo_adjusted_gross_income = add(period, ["mo_adjusted_gross_income"])
         federal_tax = tax_unit("income_tax", period)
 
         # subtract CARES act credits, only affects year 2020, source: https://revisor.mo.gov/main/OneSection.aspx?section=143.171&bid=48731
