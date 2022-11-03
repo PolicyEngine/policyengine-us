adca8f40640ec8fdf6d99a8bb94b2aab15132e3a
nmrodelo <nmrodelo@bu.edu>
2022-11-02 20:59:13 -0400

    replace main variable logic with sub-variables

--- policyengine_us/variables/gov/states/mo/tax/income/deductions/mo_pension_and_ss_or_ssd_deduction.py
+++ policyengine_us/variables/gov/states/mo/tax/income/deductions/mo_pension_and_ss_or_ssd_deduction.py
@@ -15,26 +15,10 @@ class mo_itemized_deductions(Variable):
 
     def formula(tax_unit, period, parameters):
 
-        person = tax_unit.members
-        
-        #variables and params needed for all sections
-        mo_agi =  person('mo_adjusted_gross_income', period)
-        tax_unit_mo_agi = tax_unit.sum(mo_agi)
-        taxable_social_security_benefits = tax_unit("tax_unit_taxable_social_security")
-        agi_in_excess_of_taxable_social_security = tax_unit_mo_agi - taxable_social_security_benefits #Equivalent to Line 3 of section A and B
-        filing_status = tax_unit('filing_status',period)
-        p = parameters(period).gov.states.mo.tax.income.deductions
-        
-        
         #Section C, SS or SSD Amounts
         #We start with section C because, in situations where someone receives both a pension and taxable
         #Social Security, Section A requires information from Section C to be completed.
-        ss_or_ssd_allowance = p.ss_or_ssd_deduction_allowance[filing_status]
-        agi_over_ss_or_ssd_allowance = max(agi_in_excess_of_taxable_social_security - ss_or_ssd_allowance, 0) #different from Sections A and B, Line 3, floor at 0.
-        ssd_amount = tax_unit("social_security_disability", period)
-        ss_or_ssd = max(taxable_social_security_benefits, ssd_amount) #need logic for both of these being present, each spouse can claim one
-        eligible_ss_or_ssd = max(ss_or_ssd - agi_over_ss_or_ssd_allowance, 0)
-
+        eligible_ss_or_ssd = tax_unit('mo_pension_and_ss_or_ssd_section_a', period)
 
         #Section A, Public Pension Amounts
         #need section c finished to get to this part
@@ -42,29 +26,9 @@ class mo_itemized_deductions(Variable):
         #TODO:
         #needs additional logic to account for each individuals share of taxable social security being accounted for separately
         #unclear reference to "See instructions if Line 3 of Section C is more than $0" here: https://dor.mo.gov/forms/MO-A_2021.pdf#page=3
-
-        public_pension_allowance = p.mo_public_pension_deduction_allowance[filing_status]
-        agi_over_public_pension__allowance = max(agi_in_excess_of_taxable_social_security - public_pension_allowance,0)
-        public_pension_amount = tax_unit("public_pension_income", period)
-        max_social_security_benefit = p.mo_max_social_security_benefit # Seen on Line 7, Section A
-        public_pension_value = min(public_pension_amount, max_social_security_benefit)
-        ss_or_ssdi_exemption_threshold = p.mo_ss_or_ssdi_exemption_threshold[filing_status]
-
-        #From instructions here: https://dor.mo.gov/forms/MO-1040%20Instructions_2021.pdf#page=17
-        adjusted_ss_or_ssdi_value = where((ss_or_ssd - ss_or_ssdi_exemption_threshold) > 0,
-            eligible_ss_or_ssd,
-            ss_or_ssd,)
-      
-        ss_less_public_pension_allowance = max(adjusted_ss_or_ssdi_value - public_pension_value, 0)
-        total_public_pensions = ss_less_public_pension_allowance - agi_over_public_pension__allowance
-
+        total_public_pensions = tax_unit('mo_pension_and_ss_or_ssd_section_b', period)
 
         # Section B, Private Pension Amounts
-        private_pension_allowance = p.mo_private_pension_deduction_allowance[filing_status]
-        agi_over_private_pension_allowance = max(agi_in_excess_of_taxable_social_security - private_pension_allowance, 0)
-        private_pension_amount = tax_unit("pension_income", period)
-        max_private_pension_amount = p.mo_max_private_pension
-        private_pension_value = min(private_pension_amount, max_private_pension_amount)
-        total_private_pensions =  max(private_pension_value - agi_over_private_pension_allowance, 0)
+        total_private_pensions =  tax_unit('mo_pension_and_ss_or_ssd_section_c', period)
 
         return total_private_pensions + total_public_pensions + eligible_ss_or_ssd
