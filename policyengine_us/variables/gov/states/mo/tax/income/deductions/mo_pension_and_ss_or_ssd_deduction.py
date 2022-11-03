0bc67835ecd76446dd4b39e837a26a0f8aa96add
nmrodelo <nmrodelo@bu.edu>
2022-11-03 18:24:01 -0400

    update section a to be person level

--- policyengine_us/variables/gov/states/mo/tax/income/deductions/mo_pension_and_ss_or_ssd_deduction.py
+++ policyengine_us/variables/gov/states/mo/tax/income/deductions/mo_pension_and_ss_or_ssd_deduction.py
@@ -18,7 +18,7 @@ class mo_itemized_deductions(Variable):
         #Section C, SS or SSD Amounts
         #We start with section C because, in situations where someone receives both a pension and taxable
         #Social Security, Section A requires information from Section C to be completed.
-        eligible_ss_or_ssd = tax_unit('mo_pension_and_ss_or_ssd_section_a', period)
+        eligible_ss_or_ssd = tax_unit('mo_pension_and_ss_or_ssd_section_c', period)
 
         #Section A, Public Pension Amounts
         #need section c finished to get to this part
@@ -26,9 +26,9 @@ class mo_itemized_deductions(Variable):
         #TODO:
         #needs additional logic to account for each individuals share of taxable social security being accounted for separately
         #unclear reference to "See instructions if Line 3 of Section C is more than $0" here: https://dor.mo.gov/forms/MO-A_2021.pdf#page=3
-        total_public_pensions = tax_unit('mo_pension_and_ss_or_ssd_section_b', period)
+        total_public_pensions = tax_unit('mo_pension_and_ss_or_ssd_section_a', period)
 
         # Section B, Private Pension Amounts
-        total_private_pensions =  tax_unit('mo_pension_and_ss_or_ssd_section_c', period)
+        total_private_pensions =  tax_unit('mo_pension_and_ss_or_ssd_section_b', period)
 
         return total_private_pensions + total_public_pensions + eligible_ss_or_ssd
--- policyengine_us/variables/gov/states/mo/tax/income/deductions/mo_pension_and_ss_or_ssd_section_a.py
+++ policyengine_us/variables/gov/states/mo/tax/income/deductions/mo_pension_and_ss_or_ssd_section_a.py
@@ -3,7 +3,7 @@ from policyengine_us.model_api import *
 
 class mo_itemized_deductions(Variable):
     value_type = float
-    entity = TaxUnit
+    entity = Person
     label = "MO Pension and Social Security or SS Disability Deduction"
     unit = USD
     definition_period = YEAR
@@ -13,10 +13,11 @@ class mo_itemized_deductions(Variable):
     )
     defined_for = StateCode.MO
 
-    def formula(tax_unit, period, parameters):
+    def formula(person, period, parameters):
         mo_agi =  person('mo_adjusted_gross_income', period)
+        tax_unit = person.tax_unit
         tax_unit_mo_agi = tax_unit.sum(mo_agi)
-        taxable_social_security_benefits = tax_unit("tax_unit_taxable_social_security")
+        taxable_social_security_benefits = person("taxable_social_security", period)
         agi_in_excess_of_taxable_social_security = tax_unit_mo_agi - taxable_social_security_benefits #Equivalent to Line 3 of section A and B
         filing_status = tax_unit('filing_status',period)
         p = parameters(period).gov.states.mo.tax.income.deductions
@@ -30,14 +31,14 @@ class mo_itemized_deductions(Variable):
 
         public_pension_allowance = p.mo_public_pension_deduction_allowance[filing_status]
         agi_over_public_pension__allowance = max(agi_in_excess_of_taxable_social_security - public_pension_allowance,0)
-        public_pension_amount = tax_unit("public_pension_income", period)
+        public_pension_amount = person("public_pension_income", period)
         max_social_security_benefit = p.mo_max_social_security_benefit # Seen on Line 7, Section A
         public_pension_value = min(public_pension_amount, max_social_security_benefit)
         ss_or_ssdi_exemption_threshold = p.mo_ss_or_ssdi_exemption_threshold[filing_status]
 
-        ssd_amount = tax_unit("social_security_disability", period)
+        ssd_amount = person("social_security_disability", period)
         ss_or_ssd = max(taxable_social_security_benefits, ssd_amount) 
-        eligible_ss_or_ssd = person('mo_pension_and_ss_or_ssd_section_c', period)
+        eligible_ss_or_ssd = tax_unit('mo_pension_and_ss_or_ssd_section_c', period)
         
 
         #From instructions here: https://dor.mo.gov/forms/MO-1040%20Instructions_2021.pdf#page=17
