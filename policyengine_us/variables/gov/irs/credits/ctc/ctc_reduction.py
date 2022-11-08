002a0a9e8534ee1e0fc20f517aab3ee5a05682aa
Max Ghenis <mghenis@gmail.com>
2022-11-07 20:27:42 -0800

    Use consistent formula across years for CTC phase-out

--- policyengine_us/variables/gov/irs/credits/ctc/ctc_reduction.py
+++ policyengine_us/variables/gov/irs/credits/ctc/ctc_reduction.py
@@ -17,20 +17,13 @@ class ctc_reduction(Variable):
     definition_period = YEAR
 
     def formula(tax_unit, period, parameters):
-        income = tax_unit("adjusted_gross_income", period)
-        ctc = parameters(period).gov.irs.credits.ctc
-        phase_out_threshold = tax_unit("ctc_phase_out_threshold", period)
-        income_over_threshold = max_(0, income - phase_out_threshold)
-        reduction = ctc.phase_out.rate * income_over_threshold
-        maximum_ctc = tax_unit("ctc_maximum", period)
-        return min_(reduction, maximum_ctc)
+        # TCJA's phase-out changes are purely parametric so don't require
+        # structural reform.
 
-    # TCJA's phase-out changes are purely parametric so don't require structural reform.
-
-    def formula_2021(tax_unit, period, parameters):
         # The ARPA CTC has two phase-outs: the original, and a new phase-out
         # applying before and only to the increase in the maximum CTC under ARPA.
 
+        # Start with the normal phase-out.
         income = tax_unit("adjusted_gross_income", period)
         ctc = parameters(period).gov.irs.credits.ctc
         filing_status = tax_unit("filing_status", period)
@@ -39,10 +32,14 @@ class ctc_reduction(Variable):
         reduction = ctc.phase_out.rate * income_over_threshold
         maximum_ctc = tax_unit("ctc_maximum", period)
 
-        # Calculate the original phase-out
+        # Calculate the original phase-out.
         original_phase_out = min_(reduction, maximum_ctc)
 
-        # Calculate the income used to assess the new phase-out
+        if not ctc.phase_out.arpa.in_effect:
+            return original_phase_out
+
+        # Otherwise, calculate the ARPA phase-out.
+        # Calculate the income used to assess the new phase-out.
         income_over_arpa_threshold = max_(
             0, income - ctc.phase_out.arpa.threshold[filing_status]
         )
@@ -84,5 +81,3 @@ class ctc_reduction(Variable):
         arpa_reduction = min_(arpa_phase_out_max_reduction, arpa_reduction_max)
 
         return min_(original_phase_out + arpa_reduction, maximum_ctc)
-
-    formula_2022 = formula
