2c87e610900a695730e5e55a4c3f13dcc4de762c
Max Ghenis <mghenis@gmail.com>
2022-11-23 22:01:44 -0800

    Minimal taxsim reform (doesn't work)

---
+++ policyengine_us/reforms/taxsim.py
@@ -0,0 +1,13 @@
+from policyengine_core.reforms import Reform
+
+
+class taxsim(Reform):
+    def apply(self):
+        self.modify_parameters(modify_parameters)
+
+
+def modify_parameters(parameters):
+    parameters.contrib.nber.taxsim35_emulation.update(
+        period="2019-01-01", value=True
+    )
+    return parameters
