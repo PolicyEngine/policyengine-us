677ab9baafc4fb507bda3306bbc0370ef50d3ebc
Max Ghenis <mghenis@gmail.com>
2022-11-27 20:54:51 -0800

    Add all refundable credits

--- policyengine_us/variables/gov/states/mo/tax/income/deductions/mo_federal_income_tax_deduction.py
+++ policyengine_us/variables/gov/states/mo/tax/income/deductions/mo_federal_income_tax_deduction.py
@@ -9,32 +9,26 @@ class mo_federal_income_tax_deduction(Variable):
     definition_period = YEAR
     reference = (
         "https://dor.mo.gov/forms/MO-1040%20Instructions_2021.pdf#page=7",
-        "https://revisor.mo.gov/main/OneSection.aspx?section=143.171&bid=49937&hl=federal+income+tax+deduction%u2044",
+        "https://revisor.mo.gov/main/OneSection.aspx?section=143.171",
     )
     defined_for = StateCode.MO
 
     def formula(tax_unit, period, parameters):
         # Deduct a capped share of federal income tax liability.
         # Use a version that assumes no SALT deduction to avoid circularity.
-        # Ignore COVID-19 rebates and federal EITC.
-        # https://revisor.mo.gov/main/OneSection.aspx?section=143.171&bid=48731
-        IGNORED_CREDITS = [
-            "rrc_cares",
-            "rrc_caa",
-            "rrc_arpa",
-            "earned_income_tax_credit",
-        ]
+        # Ignore refundable credits (including COVID-19 rebates).
+        gov = parameters(period).gov
         uncapped_federal_income_tax_ignoring_credits = add(
-            tax_unit, period, ["no_salt_income_tax"] + IGNORED_CREDITS
+            tax_unit,
+            period,
+            ["no_salt_income_tax"] + gov.irs.credits.refundable,
         )
         # Law is vague, but for now, limit to nonnegative tax.
         federal_income_tax_ignoring_credits = max_(
             0, uncapped_federal_income_tax_ignoring_credits
         )
         # Apply rate based on AGI.
-        p = parameters(
-            period
-        ).gov.states.mo.tax.income.deductions.federal_income_tax
+        p = gov.states.mo.tax.income.deductions.federal_income_tax
         tax_unit_mo_agi = add(tax_unit, period, ["mo_adjusted_gross_income"])
         rate = p.rate.calc(tax_unit_mo_agi)
         uncapped = federal_income_tax_ignoring_credits * rate
