bfe0d85273e61e6458db073b34210360626f3ddf
Nikhil Woodruff <nikhil@policyengine.org>
2022-12-13 18:21:05 +0000

    Fix Social Security CPS microdata

--- policyengine_us/variables/household/income/person/pensions/pension_income.py
+++ policyengine_us/variables/household/income/person/pensions/pension_income.py
@@ -8,7 +8,7 @@ class pension_income(Variable):
     unit = USD
     documentation = "Income from pensions, annuitities, life insurance or endowment contracts."
     definition_period = YEAR
-
-    formula = sum_of_variables(
-        ["tax_exempt_pension_income", "taxable_pension_income"]
-    )
+    adds = [
+        "tax_exempt_pension_income",
+        "taxable_pension_income",
+    ]
