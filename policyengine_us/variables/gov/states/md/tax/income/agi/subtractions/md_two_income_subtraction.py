8e427fe3f3db642039b05c9218a28aa397fb4c64
martin.holmer@gmail.com <martin.holmer@gmail.com>
2022-12-12 22:17:18 -0500

    black format changes and changelog entry

--- policyengine_us/variables/gov/states/md/tax/income/agi/subtractions/md_two_income_subtraction.py
+++ policyengine_us/variables/gov/states/md/tax/income/agi/subtractions/md_two_income_subtraction.py
@@ -14,30 +14,18 @@ class md_two_income_subtraction(Variable):
         person = tax_unit.members
         # compute head and spouse US AGI portions using irs_gross_income shares
         us_agi = tax_unit("adjusted_gross_income", period)
-        #print("\nMD -----------------------")
-        #print("MD us_agi=", us_agi)
         gross_income = person("irs_gross_income", period)
-        #print("MD gross_income=", gross_income)
         is_head = person("is_tax_unit_head", period) == 1
         is_spouse = person("is_tax_unit_spouse", period) == 1
-        head_gross_income = tax_unit.sum(
-            where(is_head, gross_income, 0)
-        )
-        #print("MD head_gross_income=", head_gross_income)
+        head_gross_income = tax_unit.sum(where(is_head, gross_income, 0))
         couple_gross_income = tax_unit.sum(
             where(is_head | is_spouse, gross_income, 0)
         )
-        #print("MD couple_gross_income=", couple_gross_income)
         head_frac = where(
-            couple_gross_income > 0,
-            head_gross_income / couple_gross_income,
-            1
+            couple_gross_income > 0, head_gross_income / couple_gross_income, 1
         )
-        #print("MD head_frac=", head_frac)
         head_us_agi = head_frac * us_agi
-        #print("MD head_us_agi=", head_us_agi)
         spouse_us_agi = (1 - head_frac) * us_agi
-        #print("MD spouse_us_agi=", spouse_us_agi)
 
         # compute head and spouse MD AGI additions using ad hoc rule
         total_additions = tax_unit("md_total_additions", period)
@@ -54,9 +42,7 @@ class md_two_income_subtraction(Variable):
             if subtraction == "md_dependent_care_subtraction":
                 unit_care_amt = tax_unit(subtraction, period)
                 head_frac = where(
-                    tax_unit("filing_status", period) == "Joint",
-                    0.5,
-                    1.0
+                    tax_unit("filing_status", period) == "Joint", 0.5, 1.0
                 )
                 head_subs += head_frac * unit_care_amt
                 spouse_subs += (1 - head_frac) * unit_care_amt
@@ -72,5 +58,4 @@ class md_two_income_subtraction(Variable):
             p.max_two_income_subtraction,
             min_agi_add_sub,
         )
-        #print("MD two-income subtraction=", capped_min_agi_add_sub)
         return max_(0, capped_min_agi_add_sub)
