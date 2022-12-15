a0c38984e7cc02fc4b618bdd4ddb8ad5eb21f154
Max Ghenis <mghenis@gmail.com>
2022-12-15 13:37:57 -0800

    formatting changes

--- policyengine_us/variables/gov/states/ny/tax/income/ny_income_tax_before_credits.py
+++ policyengine_us/variables/gov/states/ny/tax/income/ny_income_tax_before_credits.py
@@ -9,9 +9,4 @@ class ny_income_tax_before_credits(Variable):
     definition_period = YEAR
     defined_for = StateCode.NY
 
-    formula = sum_of_variables(
-        [
-            "ny_main_income_tax",
-            "ny_supplemental_tax",
-        ]
-    )
+    formula = sum_of_variables(["ny_main_income_tax", "ny_supplemental_tax"])
