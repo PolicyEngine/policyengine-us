48fec1abe0aba09bed5fa4f46aa1108fb8f7138e
Max Ghenis <mghenis@gmail.com>
2022-12-24 07:44:58 -0800

    Simplify to a federal flag indicating if SNAP emergency allotments are allowed

--- policyengine_us/variables/gov/usda/snap/snap_emergency_allotment.py
+++ policyengine_us/variables/gov/usda/snap/snap_emergency_allotment.py
@@ -11,16 +11,18 @@ class snap_emergency_allotment(Variable):
     unit = USD
 
     def formula(spm_unit, period, parameters):
+        p = parameters(period).gov.usda.snap.emergency_allotment
+        if not p.allowed:
+            return 0
         # Federal SNAP rules are defined in U.S.C Title 7, Chapter 51, which also
         # defines state powers to modify the rules.
         eligible = spm_unit("is_snap_eligible", period)
         max_allotment = spm_unit("snap_max_allotment", period)
         normal_allotment = spm_unit("snap_normal_allotment", period)
         # Calculate emergency allotment, which provides all eligible households the maximum.
-        ea = parameters(period).gov.usda.snap.emergency_allotment
         state = spm_unit.household("state_code", period)
-        ea_in_effect = ea.in_effect[state]
-        ea_minimum = ea.minimum * MONTHS_IN_YEAR
+        ea_in_effect = p.in_effect[state]
+        ea_minimum = p.minimum * MONTHS_IN_YEAR
         ea_amount_if_in_effect = max_(
             ea_minimum, max_allotment - normal_allotment
         )
