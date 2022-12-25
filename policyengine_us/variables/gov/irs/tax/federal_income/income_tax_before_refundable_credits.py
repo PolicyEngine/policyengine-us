f6cec5a4feb4e81f68126daaac98661a7f31ebbc
Max Ghenis <mghenis@gmail.com>
2022-12-25 12:41:23 -0800

    Use adds/subtracts in current income_tax_before_refundable_credits variable

--- policyengine_us/variables/gov/irs/tax/federal_income/income_tax_before_refundable_credits.py
+++ policyengine_us/variables/gov/irs/tax/federal_income/income_tax_before_refundable_credits.py
@@ -8,18 +8,11 @@ class income_tax_before_refundable_credits(Variable):
     unit = USD
     label = "Income tax before refundable credits"
     documentation = "Income tax liability (including other taxes) after non-refundable credits are used, but before refundable credits are applied"
-
-    def formula(tax_unit, period, parameters):
-        income_tax_bc = tax_unit("income_tax_before_credits", period)
-        capped_credits = tax_unit(
-            "income_tax_capped_non_refundable_credits", period
-        )
-        taxes_net_nonrefundable_credits = income_tax_bc - capped_credits
-        OTHER_TAXES = [
-            "net_investment_income_tax",
-            "recapture_of_investment_credit",
-            "unreported_payroll_tax",
-            "qualified_retirement_penalty",
-        ]
-        other_taxes = add(tax_unit, period, OTHER_TAXES)
-        return taxes_net_nonrefundable_credits + other_taxes
+    adds = [
+        "income_tax_before_credits",
+        "net_investment_income_tax",
+        "recapture_of_investment_credit",
+        "unreported_payroll_tax",
+        "qualified_retirement_penalty",
+    ]
+    subtracts = ["income_tax_capped_non_refundable_credits"]
