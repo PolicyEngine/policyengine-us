809c59c31fa70547d21df368291ef34d70313252
Max Ghenis <mghenis@gmail.com>
2022-12-26 12:31:28 -0800

    Move state tax before refundable credits to top level

--- policyengine_us/variables/household/income/household/household_tax_before_refundable_credits.py
+++ policyengine_us/variables/household/income/household/household_tax_before_refundable_credits.py
@@ -12,6 +12,11 @@ class household_tax_before_refundable_credits(Variable):
         "employee_payroll_tax",
         "self_employment_tax",
         "income_tax_before_refundable_credits",  # Federal.
-        "state_income_tax_before_refundable_credits",
+        "ma_income_tax_before_refundable_credits",
+        "md_income_tax_before_refundable_credits",
+        "ny_income_tax_before_refundable_credits",
+        "or_income_tax_before_refundable_credits",
+        "pa_income_tax",  # PA has no refundable credits.
+        "wa_income_tax_before_refundable_credits",
         "flat_tax",
     ]
