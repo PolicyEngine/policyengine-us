e45f6be7475fe9bdbe7200834a11f9fae744e390
martin.holmer@gmail.com <martin.holmer@gmail.com>
2022-12-28 23:27:17 -0500

    Simplify mo_ptc_gross_income formula

--- policyengine_us/variables/gov/states/mo/tax/income/credits/property_tax/mo_ptc_gross_income.py
+++ policyengine_us/variables/gov/states/mo/tax/income/credits/property_tax/mo_ptc_gross_income.py
@@ -17,16 +17,18 @@ class mo_ptc_gross_income(Variable):
     def formula(tax_unit, period, parameters):
         person = tax_unit.members
         # compute core person-level income included in PTC gross income
-        core_inc = add(
-            person,
-            period,
-            [
-                "mo_adjusted_gross_income",  # Form MO-PTS, line 1
-                "tax_exempt_pension_income",  # Form MO-PTS, line 3
-                "tax_exempt_interest_income",  # Form MO-PTS, line 3
-            ],
+        core_income = tax_unit.sum(
+            add(
+                person,
+                period,
+                [
+                    "mo_adjusted_gross_income",  # Form MO-PTS, line 1
+                    "tax_exempt_pension_income",  # Form MO-PTS, line 3
+                    "tax_exempt_interest_income",  # Form MO-PTS, line 3
+                ],
+            )
         )
-        core_income = tax_unit.sum(core_inc)
+        # compute core taxunit-level income included in PTC gross income
         tax_exempt_socsec_benefits = tax_unit(
             "tax_exempt_social_security",  # Form MO-PTS, line 2
             period,
@@ -39,19 +41,20 @@ class mo_ptc_gross_income(Variable):
             # Form MO-PTS, line 5
             included_veterans_benefits
         )
-        # compute public assistance included in PTC gross income
-        pa_inc = add(
-            person,
-            period,
-            [
-                # Form MO-PTS, line 6
-                "ssi",
-                "state_supplement",
-                "child_support_received",
-                "tanf_person",
-            ],
+        # compute person-level public assistance included in PTC gross income
+        public_assistance_income = tax_unit.sum(
+            add(
+                person,
+                period,
+                [
+                    # Form MO-PTS, line 6
+                    "ssi",
+                    "state_supplement",
+                    "child_support_received",
+                    "tanf_person",
+                ],
+            )
         )
-        public_assistance_income = tax_unit.sum(pa_inc)
         # compute nonbusiness capital losses included in PTC gross income
         nonbusiness_losses = add(  # Form MO-PTS, line 7
             tax_unit,
