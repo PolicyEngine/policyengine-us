25a33f14dbde3853f618fe215a0f3191533afcb9
martin.holmer@gmail.com <martin.holmer@gmail.com>
2022-12-30 22:01:47 -0500

    Fix bugs introduced by PR 1547

--- policyengine_us/variables/gov/irs/credits/income_tax_refundable_credits.py
+++ policyengine_us/variables/gov/irs/credits/income_tax_refundable_credits.py
@@ -7,13 +7,6 @@ class income_tax_refundable_credits(Variable):
     definition_period = YEAR
     label = "federal refundable tax credits"
     unit = USD
-    adds = [
-        "eitc",
-        "refundable_american_opportunity_credit",
-        "refundable_ctc",
-        "recovery_rebate_credit",
-        "refundable_payroll_tax_credit",
-        "premium_tax_credit",
-        "ecpa_filer_credit",
-        "ecpa_adult_dependent_credit",
-    ]
+
+    formula = sum_of_variables("gov.irs.credits.refundable")
+    # formula must use parameters list that varies by period
