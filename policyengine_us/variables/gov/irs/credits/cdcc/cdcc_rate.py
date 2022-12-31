094c35daea6ee216d2a89fbbc36f7475b6adb413
martin.holmer@gmail.com <martin.holmer@gmail.com>
2022-12-31 13:20:35 -0500

    Add some TAXSIM35 emulation logic to cdcc_rate.py module

--- policyengine_us/variables/gov/irs/credits/cdcc/cdcc_rate.py
+++ policyengine_us/variables/gov/irs/credits/cdcc/cdcc_rate.py
@@ -10,19 +10,22 @@ class cdcc_rate(Variable):
     reference = "https://www.law.cornell.edu/uscode/text/26/21#a_2"
 
     def formula(tax_unit, period, parameters):
-        p = parameters(period).gov.irs.credits.cdcc
+        pgov = parameters(period).gov
+        taxsim_emulation = pgov.contrib.nber.taxsim35_emulation
+        pcdcc = pgov.irs.credits.cdcc
         agi = tax_unit("adjusted_gross_income", period)
-
-        # First phase-out
-        excess_agi = max_(0, agi - p.phase_out.start)
-        increments = np.ceil(excess_agi / p.phase_out.increment)
-        percentage_reduction = increments * p.phase_out.rate
+        # first phase-out
+        excess_agi = max_(0, agi - pcdcc.phase_out.start)
+        if taxsim_emulation:
+            increments = excess_agi / pcdcc.phase_out.increment
+        else:
+            increments = np.ceil(excess_agi / pcdcc.phase_out.increment)
+        pct_reduction = increments * pcdcc.phase_out.rate
         phased_out_rate = max_(
-            p.phase_out.min, p.phase_out.max - percentage_reduction
+            pcdcc.phase_out.min, pcdcc.phase_out.max - pct_reduction
         )
-
-        # Second phase-out
-        second_excess_agi = max_(0, agi - p.phase_out.second_start)
-        second_increments = np.ceil(second_excess_agi / p.phase_out.increment)
-        second_percentage_reduction = second_increments * p.phase_out.rate
-        return max_(0, phased_out_rate - second_percentage_reduction)
+        # second phase-out
+        excess_agi = max_(0, agi - pcdcc.phase_out.second_start)
+        increments = np.ceil(excess_agi / pcdcc.phase_out.increment)
+        pct_reduction = increments * pcdcc.phase_out.rate
+        return max_(0, phased_out_rate - pct_reduction)
