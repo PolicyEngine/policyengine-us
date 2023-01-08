afda5554e85f838ef3c733b53ae52c078d0abbd5
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-01-08 14:12:45 -0500

    Add formula for md_socsec_subtraction_amount and several tests

--- policyengine_us/variables/gov/states/md/tax/income/agi/subtractions/md_socsec_subtraction_amount.py
+++ policyengine_us/variables/gov/states/md/tax/income/agi/subtractions/md_socsec_subtraction_amount.py
@@ -12,4 +12,32 @@ class md_socsec_subtraction_amount(Variable):
         "https://www.marylandtaxes.gov/forms/21_forms/Resident_Booklet.pdf#page=14"
     )
     defined_for = StateCode.MD
-    adds = ["taxable_social_security"]
+
+    def formula(person, period, parameters):
+        unit_tss = person.tax_unit("tax_unit_taxable_social_security", period)
+        # allocate unit_tss to head and spouse in proportion to social_security
+        unit_socsec = person.tax_unit("tax_unit_social_security", period)
+        unit_has_socsec = unit_socsec > 0
+        ind_socsec = person("social_security", period)
+        is_spouse = person("is_tax_unit_spouse", period)
+        spouse_frac = min_(
+            1, where(is_spouse & unit_has_socsec, ind_socsec / unit_socsec, 0)
+        )
+        unit_spouse_frac = person.tax_unit.sum(spouse_frac)
+        is_head = person("is_tax_unit_head", period)
+        head_frac = where(is_head & unit_has_socsec, 1 - unit_spouse_frac, 0)
+        unit_head_frac = person.tax_unit.sum(head_frac)
+        """
+        print("\n******************************************")
+        print("unit_tss=", unit_tss)
+        print("unit_socsec", unit_socsec)
+        print("ind_socsec", ind_socsec)
+        print("spouse_frac=", spouse_frac)
+        print("unit_spouse_frac=", unit_spouse_frac)
+        print("head_frac=", head_frac)
+        print("unit_head_frac=", unit_head_frac)
+        """
+        return select(
+            [is_head, is_spouse, ~(is_head | is_spouse)],
+            [unit_head_frac * unit_tss, unit_spouse_frac * unit_tss, 0],
+        )
