9386c36bdf365079f0fda9158da9af4a92986109
Max Ghenis <mghenis@gmail.com>
2023-01-19 14:52:52 -0700

    Fix grant standard

--- policyengine_us/variables/gov/states/co/cdhs/tanf/co_tanf.py
+++ policyengine_us/variables/gov/states/co/cdhs/tanf/co_tanf.py
@@ -10,6 +10,6 @@ class co_tanf(Variable):
     defined_for = "co_tanf_eligible"
 
     def formula(spm_unit, period, parameters):
-        payment_standard = spm_unit("co_tanf_payment_standard", period)
+        grant_standard = spm_unit("co_tanf_grant_standard", period)
         income = spm_unit("co_tanf_countable_income", period)
         return max_(payment_standard - income, 0)
