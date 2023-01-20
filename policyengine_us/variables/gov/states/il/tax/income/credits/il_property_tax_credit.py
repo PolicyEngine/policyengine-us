1d530c92676ad3af676480815e207ad2f8798325
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-01-20 13:43:11 -0500

    Remove federal QBID from IL property tax credit formula

--- policyengine_us/variables/gov/states/il/tax/income/credits/il_property_tax_credit.py
+++ policyengine_us/variables/gov/states/il/tax/income/credits/il_property_tax_credit.py
@@ -11,10 +11,9 @@ class il_property_tax_credit(Variable):
     defined_for = "il_is_exemption_eligible"
 
     def formula(tax_unit, period, parameters):
-        ptax = tax_unit("property_tax_primary_residence", period)
-        qbid = tax_unit("qualified_business_income_deduction", period)
+        ptax_paid = tax_unit("property_tax_primary_residence", period)
         pre_credit_tax = tax_unit(
             "il_income_tax_before_nonrefundable_credits", period
         )
         p = parameters(period).gov.states.il.tax.income.credits
-        return min_(max_(0, ptax - qbid) * p.property_tax.rate, pre_credit_tax)
+        return min_(ptax_paid * p.property_tax.rate, pre_credit_tax)
