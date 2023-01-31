449a84c657c00b630343177f496ba93851c33b5a
Max Ghenis <mghenis@gmail.com>
2023-01-31 13:40:01 -0500

    Update policyengine_us/variables/gov/states/ma/tax/income/ma_income_tax_before_refundable_credits.py

--- policyengine_us/variables/gov/states/ma/tax/income/ma_income_tax_before_refundable_credits.py
+++ policyengine_us/variables/gov/states/ma/tax/income/ma_income_tax_before_refundable_credits.py
@@ -14,4 +14,3 @@ class ma_income_tax_before_refundable_credits(Variable):
         tax_before_credits = tax_unit("ma_income_tax_before_credits", period)
         non_refundable_credits = tax_unit("ma_non_refundable_credits", period)
         return max_(tax_before_credits - non_refundable_credits, 0)
-    subtracts = ["ma_non_refundable_credits"]
