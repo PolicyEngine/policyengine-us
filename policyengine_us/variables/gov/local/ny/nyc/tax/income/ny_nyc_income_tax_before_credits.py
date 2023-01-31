edc2424f11e63d411f44619a8edf67b946729d95
Max Ghenis <mghenis@gmail.com>
2023-01-31 17:37:45 -0500

    Reference in_nyc instead of broken county filter

--- policyengine_us/variables/gov/local/ny/nyc/tax/income/ny_nyc_income_tax_before_credits.py
+++ policyengine_us/variables/gov/local/ny/nyc/tax/income/ny_nyc_income_tax_before_credits.py
@@ -1,4 +1,4 @@
-from openfisca_us.model_api import *
+from policyengine_us.model_api import *
 
 
 class ny_nyc_income_tax_before_credits(Variable):
@@ -7,43 +7,26 @@ class ny_nyc_income_tax_before_credits(Variable):
     label = "NYC income tax before credits"
     unit = USD
     definition_period = YEAR
-    defined_for = StateCode.NY
+    defined_for = "in_nyc"
 
     def formula(tax_unit, period, parameters):
-        # determine if tax unit is in NYC
-        county = tax_unit.household("county", period)
-        counties = county.possible_values
-        is_in_nyc = (
-            (county == counties.NEW_YORK_COUNTY_NY)
-            | (county == counties.KINGS_COUNTY_NY)
-            | (county == counties.QUEENS_COUNTY_NY)
-            | (county == counties.BRONX_COUNTY_NY)
-            | (county == counties.RICHMOND_COUNTY_NY)
-        )
         taxable_income = tax_unit("ny_taxable_income", period)
         filing_status = tax_unit("filing_status", period)
-        status = filing_status.possible_values
-
+        filing_statuses = filing_status.possible_values
         rates = parameters(period).gov.local.ny.nyc.tax.income.rates
-        single = rates.single
-        joint = rates.joint
-        hoh = rates.head_of_household
-        widow = rates.widow
-        separate = rates.separate
-
-        return is_in_nyc * select(
+        return select(
             [
-                filing_status == status.SINGLE,
-                filing_status == status.JOINT,
-                filing_status == status.HEAD_OF_HOUSEHOLD,
-                filing_status == status.WIDOW,
-                filing_status == status.SEPARATE,
+                filing_status == filing_statuses.SINGLE,
+                filing_status == filing_statuses.JOINT,
+                filing_status == filing_statuses.HEAD_OF_HOUSEHOLD,
+                filing_status == filing_statuses.WIDOW,
+                filing_status == filing_statuses.SEPARATE,
             ],
             [
-                single.calc(taxable_income),
-                joint.calc(taxable_income),
-                hoh.calc(taxable_income),
-                widow.calc(taxable_income),
-                separate.calc(taxable_income),
+                rates.single.calc(taxable_income),
+                rates.joint.calc(taxable_income),
+                rates.head_of_household.calc(taxable_income),
+                rates.widow.calc(taxable_income),
+                rates.separate.calc(taxable_income),
             ],
         )
