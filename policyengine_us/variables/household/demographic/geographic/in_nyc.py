0f78ca176b83682f8fce0ad61d7a271cff1fc9f5
leogoldman <leogoldman1@gmail.com>
2023-02-01 23:41:53 -0500

    Add formula for `in_nyc` based on county Fixes #1734

--- policyengine_us/variables/household/demographic/geographic/in_nyc.py
+++ policyengine_us/variables/household/demographic/geographic/in_nyc.py
@@ -6,3 +6,21 @@ class in_nyc(Variable):
     entity = Household
     definition_period = ETERNITY
     label = "Is in NYC"
+
+    def formula(household, period, parameters):
+
+        # Get the county.
+        county = household("county_str", period)
+
+        # Get possible counties.
+        counties = county.possible_values
+
+        # Return whether the county is NYC.
+        return county in [
+            counties.NEW_YORK_COUNTY_NY,
+            counties.KINGS_COUNTY_NY,
+            counties.QUEENS_COUNTY_NY,
+            counties.RICHMOND_COUNTY_NY,
+            counties.BRONX_COUNTY_NY
+        ]
+
