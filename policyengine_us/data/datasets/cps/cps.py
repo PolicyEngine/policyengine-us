cdc45d840c1b065b618ecb698847c40a2c45d3c3
Max Ghenis <mghenis@gmail.com>
2023-02-03 14:54:37 +0000

    Assign `SS_VAL` from ASEC to types other than retirement benefits Fixes #1703

--- policyengine_us/data/datasets/cps/cps.py
+++ policyengine_us/data/datasets/cps/cps.py
@@ -284,7 +284,15 @@ def add_personal_income_variables(
         1 - p["qualified_dividend_fraction"][year]
     )
     cps["rental_income"] = person.RNT_VAL
-    cps["social_security_retirement"] = person.SS_VAL
+    # Assign Social Security retirement benefits if at least 62.
+    MINIMUM_RETIREMENT_AGE = 62
+    cps["social_security_retirement"] = np.where(
+        person.A_AGE >= MINIMUM_RETIREMENT_AGE, person.SS_VAL, 0
+    )
+    # Otherwise assign them to Social Security disability benefits.
+    cps["social_security_disability"] = (
+        person.SS_VAL - cps.social_security_retirement
+    )
     cps["unemployment_compensation"] = person.UC_VAL
     cps_pensions = person.PNSN_VAL + person.ANN_VAL
     cps["taxable_pension_income"] = cps_pensions * (
