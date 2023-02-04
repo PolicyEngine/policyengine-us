1db78a528cbe0b396fa0b2c42e9dea61275cd2be
Max Ghenis <mghenis@gmail.com>
2023-02-04 17:01:17 -0500

    fixes

--- policyengine_us/variables/gov/states/mo/tax/income/credits/mo_wftc.py
+++ policyengine_us/variables/gov/states/mo/tax/income/credits/mo_wftc.py
@@ -1,18 +1,18 @@
 from policyengine_us.model_api import *
 
 
-class ma_wftc(Variable):
+class mo_wftc(Variable):
     value_type = float
     entity = TaxUnit
-    label = "MO WFTC"
+    label = "Missouri Working Families Tax Credit"
     unit = USD
     definition_period = YEAR
-    reference = (
-        "https://revisor.mo.gov/main/OneSection.aspx?section=143.177&bid=49978&hl="
-    )
-    defined_for = StateCode.MA
+    reference = "https://revisor.mo.gov/main/OneSection.aspx?section=143.177&bid=49978&hl="
+    defined_for = StateCode.MO
 
     def formula(tax_unit, period, parameters):
-        eitc = tax_unit("earned_income_tax_credit", period)
-        rate = parameters(period).gov.states.mo.tax.income.credits.wftc.eitc_match
-        return eitc * rate
+        federal_eitc = tax_unit("earned_income_tax_credit", period)
+        rate = parameters(
+            period
+        ).gov.states.mo.tax.income.credits.wftc.eitc_match
+        return federal_eitc * rate
