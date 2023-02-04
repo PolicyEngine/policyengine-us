a7c285e1576172dc2948281355160feda211a1cc
Max Ghenis <mghenis@gmail.com>
2023-02-04 17:12:31 -0500

    make format

--- policyengine_us/variables/gov/states/mo/tax/income/income_tax/mo_income_tax_before_refundable_credits.py
+++ policyengine_us/variables/gov/states/mo/tax/income/income_tax/mo_income_tax_before_refundable_credits.py
@@ -11,7 +11,7 @@ class mo_income_tax_before_refundable_credits(Variable):
         "https://dor.mo.gov/forms/MO-1040%20Instructions_2021.pdf",
         "https://www.revisor.mo.gov/main/OneChapter.aspx?chapter=143",
         "https://revisor.mo.gov/main/OneSection.aspx?section=135.020&bid=6437",
-        "https://revisor.mo.gov/main/OneSection.aspx?section=143.177&bid=49978&hl="
+        "https://revisor.mo.gov/main/OneSection.aspx?section=143.177&bid=49978&hl=",
     )
     defined_for = StateCode.MO
     # mo_property_tax_credit is refundable, per pg.17 of: https://dor.mo.gov/forms/4711_2021.pdf and the last reference above.
