6882fbc8c47d72915cfcf6a05f13c2e711cb0ac5
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-02-08 14:46:19 -0500

    Added several pension income variables

--- policyengine_us/variables/household/income/person/pensions/public_pension_income.py
+++ policyengine_us/variables/household/income/person/pensions/public_pension_income.py
@@ -4,7 +4,12 @@ from policyengine_us.model_api import *
 class public_pension_income(Variable):
     value_type = float
     entity = Person
-    label = "Public pension income"
+    label = "public pension income"
     unit = USD
     documentation = "Income from government employee pensions."
     definition_period = YEAR
+
+    adds = [
+        "tax_exempt_public_pension_income",
+        "taxable_public_pension_income",
+    ]
