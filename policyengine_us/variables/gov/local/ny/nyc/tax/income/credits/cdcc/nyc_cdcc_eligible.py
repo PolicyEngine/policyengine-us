edaa429c5c95e626a0f29a9c244ecd9239112561
leogoldman <leogoldman1@gmail.com>
2023-02-10 21:14:47 -0500

    Commented out eligible formula Updated tests

--- policyengine_us/variables/gov/local/ny/nyc/tax/income/credits/cdcc/nyc_cdcc_eligible.py
+++ policyengine_us/variables/gov/local/ny/nyc/tax/income/credits/cdcc/nyc_cdcc_eligible.py
@@ -7,17 +7,17 @@ class nyc_cdcc_eligible(Variable):
     label = "Eligible for NYC CDCC"
     definition_period = YEAR
 
-    def formula(tax_unit, period, parameters):
-        # Eligibility is based on having a federal AGI below $30k
-        # and being an NYC full-time resident.
+    # def formula(tax_unit, period, parameters):
+    #     # Eligibility is based on having a federal AGI below $30k
+    #     # and being an NYC full-time resident.
 
-        # First get their income.
-        income = tax_unit("adjusted_gross_income", period)
+    #     # First get their income.
+    #     income = tax_unit("adjusted_gross_income", period)
 
-        # Then get the CDCC part of the parameter tree.
-        p = parameters(period).gov.local.ny.nyc.tax.income.credits.cdcc
+    #     # Then get the CDCC part of the parameter tree.
+    #     p = parameters(period).gov.local.ny.nyc.tax.income.credits.cdcc
 
-        # Then get their in_nyc status.
-        in_nyc = tax_unit("in_nyc", period)
+    #     # Then get their in_nyc status.
+    #     in_nyc = tax_unit("in_nyc", period)
 
-        return (income <= p.income_limit) * in_nyc
+    #     return (income <= p.income_limit) * in_nyc
