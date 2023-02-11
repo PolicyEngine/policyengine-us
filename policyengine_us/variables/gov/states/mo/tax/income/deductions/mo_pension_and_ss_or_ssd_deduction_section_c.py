afce7816101ecc82658cb5a2e681bc5acfad3fcb
nmrodelo <nmrodelo@bu.edu>
2023-02-11 12:13:03 -0500

    update black and make format

--- policyengine_us/variables/gov/states/mo/tax/income/deductions/mo_pension_and_ss_or_ssd_deduction_section_c.py
+++ policyengine_us/variables/gov/states/mo/tax/income/deductions/mo_pension_and_ss_or_ssd_deduction_section_c.py
@@ -36,7 +36,7 @@ class mo_pension_and_ss_or_ssd_deduction_section_c(Variable):
         larger_of_ss_ssdi = max_(
             taxable_social_security_benefits, social_security_disability
         )
-        
+
         # print(taxable_social_security_benefits, agi_over_ss_or_ssd_allowance)
         return max_(
             larger_of_ss_ssdi - agi_over_ss_or_ssd_allowance,
