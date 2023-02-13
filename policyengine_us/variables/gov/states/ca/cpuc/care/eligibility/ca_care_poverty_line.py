b1a9d590cb76b47d33a1451d03d5411a66c99e22
Brian KLemm <resetbrian@gmail.com>
2023-02-13 14:01:18 -0800

    Varible updates

--- policyengine_us/variables/gov/states/ca/cpuc/care/eligibility/ca_care_poverty_line.py
+++ policyengine_us/variables/gov/states/ca/cpuc/care/eligibility/ca_care_poverty_line.py
@@ -9,4 +9,12 @@ class ca_care_poverty_line(Variable):
     reference = "https://www.cpuc.ca.gov/industries-and-topics/electrical-energy/electric-costs/care-fera-program"
     defined_for = StateCode.CA
 
-    # # capped_size = max_(size, 2)
+    def formula(household, period, parameters):
+        n = household("household_size", period)
+        # CARE treats one-person households as two-person households.
+        adj_n = max_(n, 2)
+        state_group = household("state_group_str", period)
+        p_fpg = parameters(period).gov.hhs.fpg
+        p1 = p_fpg.first_person[state_group]
+        pn = p_fpg.additional_person[state_group]
+        return p1 + pn * (adj_nadj_n - 1)
