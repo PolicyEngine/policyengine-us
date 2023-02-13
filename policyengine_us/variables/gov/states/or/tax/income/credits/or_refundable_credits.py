2f3ecde5032ffc260460b112b7fad1d605e108d1
Kevin Foster <newforza@gmail.com>
2023-02-13 12:17:19 -0800

    Removed "uncapped" from OR refundable credits description Fixes #1782

--- policyengine_us/variables/gov/states/or/tax/income/credits/or_refundable_credits.py
+++ policyengine_us/variables/gov/states/or/tax/income/credits/or_refundable_credits.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class or_refundable_credits(Variable):
     value_type = float
     entity = TaxUnit
-    label = "OR uncapped refundable tax credits"
+    label = "OR refundable tax credits"
     unit = USD
     definition_period = YEAR
 
