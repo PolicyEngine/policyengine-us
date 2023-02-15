6065e3f537cbd4f996cb5acf52bb112c039e61f9
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-02-15 08:44:46 -0500

    Stylistic changes

--- policyengine_us/variables/gov/states/mo/tax/income/deductions/mo_pension_and_ss_or_ssd_deduction_section_b.py
+++ policyengine_us/variables/gov/states/mo/tax/income/deductions/mo_pension_and_ss_or_ssd_deduction_section_b.py
@@ -20,19 +20,24 @@ class mo_pension_and_ss_or_ssd_deduction_section_b(Variable):
         unit_mo_agi = tax_unit.sum(ind_mo_agi)
         ind_taxable_oasdi = person("taxable_social_security", period)
         unit_taxable_oasdi = tax_unit.sum(ind_taxable_oasdi)
-        line3 = max_(0, unit_mo_agi - unit_taxable_oasdi)
         p = parameters(period).gov.states.mo.tax.income.deductions
         filing_status = tax_unit("filing_status", period)
-        line4 = p.mo_private_pension_deduction_allowance[filing_status]
-        line5 = max_(0, line3 - line4)
+        excess_agi = max_(  # line5
+            0,
+            (
+                unit_mo_agi
+                - unit_taxable_oasdi
+                - p.mo_private_pension_deduction_allowance[filing_status]
+            ),
+        )
         ind_pvt_pension_amt = person("taxable_private_pension_income", period)
         max_pvt_pension_amt = p.mo_max_private_pension
         ind_pvt_pension_val = min_(ind_pvt_pension_amt, max_pvt_pension_amt)
         unit_pvt_pension_val = tax_unit.sum(ind_pvt_pension_val)  # line8
-        line9 = max_(0, unit_pvt_pension_val - line5)
-        ind_frac = where(
+        unit_deduction = max_(0, unit_pvt_pension_val - excess_agi)  # line9
+        ind_share_of_unit_deduction = where(
             ind_pvt_pension_val > 0,
             ind_pvt_pension_val / unit_pvt_pension_val,
             0,
         )
-        return ind_frac * line9
+        return ind_share_of_unit_deduction * unit_deduction
