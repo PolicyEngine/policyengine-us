d0bece76a11b5fd63fbda17bd85c2d9eba6f5bd8
Connor Fung <cmfung23@gmail.com>
2023-02-16 11:03:34 -0800

    to format files

--- policyengine_us/variables/gov/states/dc/tax/income/credits/dc_eitc.py
+++ policyengine_us/variables/gov/states/dc/tax/income/credits/dc_eitc.py
@@ -7,9 +7,7 @@ class dc_eitc(Variable):
     label = "DC EITC"
     unit = USD
     definition_period = YEAR
-    reference = (
-        "https://code.dccouncil.gov/us/dc/council/code/sections/47-1806.04"  # (f)
-    )
+    reference = "https://code.dccouncil.gov/us/dc/council/code/sections/47-1806.04"  # (f)
     defined_for = StateCode.DC
 
     def formula(tax_unit, period, parameters):
