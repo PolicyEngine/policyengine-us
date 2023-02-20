1b8a1727eb9d74ea5ec7d6b2414b5b3d26f274d5
Kathryn Panger <kpanger88@gmail.com>
2023-02-20 13:23:26 -0600

    Create variable to capture CA CARE discount if eligible Fixes #1798
    Co-authored-by: Max Ghenis <mghenis@gmail.com>
    Co-authored-by: PavelMakarchuk <PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/states/ca/cpuc/care/eligibility/ca_care_poverty_line.py
+++ policyengine_us/variables/gov/states/ca/cpuc/care/eligibility/ca_care_poverty_line.py
@@ -5,6 +5,7 @@ class ca_care_poverty_line(Variable):
     value_type = float
     entity = Household
     definition_period = YEAR
+    unit = USD
     label = "Poverty line as defined for California CARE program"
     reference = "https://www.cpuc.ca.gov/industries-and-topics/electrical-energy/electric-costs/care-fera-program"
     defined_for = StateCode.CA
