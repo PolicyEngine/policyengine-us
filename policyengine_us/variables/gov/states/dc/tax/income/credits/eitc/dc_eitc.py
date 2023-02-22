cb36b0f79f838b54c401bc481e9fdd6f4d4efb86
Connor Fung <cmfung23@gmail.com>
2023-02-21 17:25:53 -0800

    cleaned up params, utilized existing child count

--- policyengine_us/variables/gov/states/dc/tax/income/credits/eitc/dc_eitc.py
+++ policyengine_us/variables/gov/states/dc/tax/income/credits/eitc/dc_eitc.py
@@ -11,7 +11,7 @@ class dc_eitc(Variable):
     defined_for = StateCode.DC
 
     def formula(tax_unit, period, parameters):
-        return where(tax_unit("dc_eitc_has_qualifying_child", period),
+        return where(tax_unit("eitc_child_count", period) > 0,
             tax_unit("dc_eitc_with_qualifying_child", period),
             tax_unit("dc_eitc_without_qualifying_child", period)
         )
--- policyengine_us/variables/gov/states/dc/tax/income/credits/eitc/dc_eitc_without_qualifying_child.py
+++ policyengine_us/variables/gov/states/dc/tax/income/credits/eitc/dc_eitc_without_qualifying_child.py
@@ -12,13 +12,11 @@ class dc_eitc_without_qualifying_child(Variable):
 
     def formula(tax_unit, period, parameters):
         employment_income = tax_unit("employment_income", period)
-        # 0.0765 * max(employment_income, 7650)
-        p = parameters(period).gov.states.dc.tax.income.credits.eitc.not_qualifying
-        tent_eitc =  p.percent * min_(employment_income, p.cap)
+        p = parameters(period).gov.states.dc.tax.income.credits.eitc.no_children.phase_out
+        tent_eitc = tax_unit("earned_income_tax_credit", period)
         fed_agi = tax_unit("adjusted_gross_income", period)
         greater_of = max_(employment_income, fed_agi)
-        tent_eitc_2 = max_(tent_eitc - ((greater_of - p.agi_floor) * p.agi_multiplier), 0)
+        phase_out_amount = max_(greater_of - p.start, 0) * p.rate
+        dc_eitc = max_(tent_eitc - phase_out_amount, 0)
 
-        return where(fed_agi > p.agi_ceiling, 0,
-            where(fed_agi > p.agi_floor, tent_eitc_2, tent_eitc)
-        )
+        return dc_eitc
