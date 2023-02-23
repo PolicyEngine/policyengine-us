e5e6fc27edf1552eb313833b3e50a21884bebc4d
k-obenschain <kburnham11@yahoo.com>
2023-02-23 06:46:26 -0800

    Added me_personal_exemption variable Added me_agi which still needs further defining Added child care credit test but still needs more defining

--- policyengine_us/variables/gov/states/me/tax/income/deductions/personal_exemption/me_personal_exemptions.py
+++ policyengine_us/variables/gov/states/me/tax/income/deductions/personal_exemption/me_personal_exemptions.py
@@ -8,3 +8,30 @@ class me_personal_exemptions(Variable):
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.ME
+    reference = "https://www.maine.gov/revenue/sites/maine.gov.revenue/files/inline-files/22_1040me_dwnld_ff.pdf"
+
+    def formula(tax_unit, period, parameters):
+    
+        # Get filing status.
+        filing_status = tax_unit("filing_status", period)
+        
+        # Determine ME personal exemption which varies only with filing status according to Instructions for Line 13
+        filing_statuses = filing_status.possible_values 
+        personal_exemption = select(
+            [
+                filing_status == filing_statuses.SINGLE,
+                filing_status == filing_statuses.JOINT,
+                filing_status == filing_statuses.SEPARATE,
+                filing_status == filing_statuses.HEAD_OF_HOUSEHOLD,
+                filing_status == filing_statuses.WIDOW,
+            ],
+            [
+                1,
+                2,
+                1,
+                1,
+                1,
+            ],
+        )
+
+        return personal_exemption
