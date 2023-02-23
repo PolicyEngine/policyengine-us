ab5c9877b2a2f9a7cb040855ac55264c626c410e
k-obenschain <kburnham11@yahoo.com>
2023-02-23 06:46:26 -0800

    Added me_personal_exemption variable Added me_agi which still needs further defining Added child care credit test but still needs more defining

--- policyengine_us/variables/gov/states/me/adjusted_gross_income/me_agi.py
+++ policyengine_us/variables/gov/states/me/adjusted_gross_income/me_agi.py
@@ -8,3 +8,7 @@ class me_agi(Variable):
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.ME
+    reference = "https://www.maine.gov/revenue/sites/maine.gov.revenue/files/inline-files/22_1040me_dwnld_ff.pdf"
+   
+    adds = ["adjusted_gross_income", "me_agi_additions"]
+    subtracts = ["me_agi_subtractions"]
