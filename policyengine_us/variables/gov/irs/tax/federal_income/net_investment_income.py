ca3f529f23215793da0adaca6d5c297f31a5c5ee
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-03-02 10:01:58 -0500

    Specify net investment income sources in a parameter file

--- policyengine_us/variables/gov/irs/tax/federal_income/net_investment_income.py
+++ policyengine_us/variables/gov/irs/tax/federal_income/net_investment_income.py
@@ -8,9 +8,5 @@ class net_investment_income(Variable):
     label = "net investment income (NII) that is base of the NII Tax (NIIT)"
     reference = "https://www.law.cornell.edu/uscode/text/26/1411"
     unit = USD
-    adds = [
-        "taxable_interest_income",
-        "dividend_income",
-        "c01000",  # loss-limited capital gains
-        "rental_income",
-    ]
+
+    adds = "gov.irs.investment.income.sources"
