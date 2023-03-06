cce1a3a6c2170924c0b61c8bbcf9d3fee8e29569
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2023-03-06 18:19:55 +0000

    Add tests
    Co-authored-by: Max Ghenis <mghenis@gmail.com>

--- policyengine_us/variables/gov/states/ca/tax/income/credits/earned_income/ca_eitc_second_phase_out_start.py
+++ policyengine_us/variables/gov/states/ca/tax/income/credits/earned_income/ca_eitc_second_phase_out_start.py
@@ -32,19 +32,3 @@ class ca_eitc_second_phase_out_start(Variable):
             earnings_to_cover_eitc_difference
             + ca_eitc.phase_out.start.calc(count_children)
         )
-
-
-class ca_eitc_maximum(Variable):
-    value_type = float
-    entity = TaxUnit
-    label = "CalEITC maximum"
-    unit = USD
-    documentation = "California provides an earned income tax credit up to this value, based on the earned income amounts and phase-in rates, which vary with number of qualifying children."
-    definition_period = YEAR
-
-    def formula(tax_unit, period, parameters):
-        branch = get_ca_eitc_branch(tax_unit, period, parameters)
-        adjustment = parameters(
-            period
-        ).gov.states.ca.tax.income.credits.earned_income.adjustment.factor
-        return branch.calculate("eitc_maximum", period) * adjustment
