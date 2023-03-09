b61624134a1f6d99e86d97d2466579e00bced51f
leogoldman <leogoldman1@gmail.com>
2023-03-08 22:20:06 -0500

    Added empty NJ income variables Made threshold parameter one file Fixes #1871

--- policyengine_us/variables/gov/states/nj/tax/income/nj_income_tax_before_refundable_credits.py
+++ policyengine_us/variables/gov/states/nj/tax/income/nj_income_tax_before_refundable_credits.py
@@ -1,12 +1,10 @@
 from policyengine_us.model_api import *
 
 
-class nj_income_tax(Variable):
+class nj_income_tax_before_refundable_credits(Variable):
     value_type = float
     entity = TaxUnit
-    label = "NJ income tax"
+    label = "New Jersey income tax"
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.NJ
-    adds = ["ny_income_tax_before_refundable_credits"]
-    subtracts = ["ny_refundable_credits"]
