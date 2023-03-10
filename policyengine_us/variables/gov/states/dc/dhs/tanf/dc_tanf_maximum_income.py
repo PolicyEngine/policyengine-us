c36476aee9bf4e6a7711c07f0d0657110fc502e1
Qiukai Chen <chiukai.c1997@gmail.com>
2023-03-10 15:52:21 -0500

    Add childcare logic

--- policyengine_us/variables/gov/states/dc/dhs/tanf/dc_tanf_maximum_income.py
+++ policyengine_us/variables/gov/states/dc/dhs/tanf/dc_tanf_maximum_income.py
@@ -13,5 +13,14 @@ class dc_tanf_maximum_income(Variable):
         unit_size = spm_unit("spm_unit_size", period)
         p = parameters(period).gov.states.dc.dhs.tanf.eligibility.maximum_income
         base = p.main[unit_size]
-        # TODO: Add childcare addition.
-        return base * MONTHS_IN_YEAR
+        # Add childcare addition, which depends on the child's age.
+        person = spm_unit.members
+        child = person("is_child", period)
+        age = person("age", period)
+        has_childcare_expenses = spm_unit("childcare_expenses", period) > 0
+        # Look up supplement by age, and limit to children.
+        person_childcare_supplement = p.childcare.calc(age) * child
+        # Aggregate person-level childcare supplement to SPM unit.
+        spm_unit_childcare_supplement = has_childcare_expenses * spm_unit.sum(person_childcare_supplement)
+        monthly_maximum = base + spm_unit_childcare_supplement
+        return monthly_maximum * MONTHS_IN_YEAR
