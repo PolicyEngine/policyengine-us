293e34250d14be8d48dae7c2673d9a9aa603f164
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-03-11 15:15:02 -0500

    Use list of CA AGI subtractions

--- policyengine_us/variables/gov/states/ca/tax/income/ca_agi_subtractions.py
+++ policyengine_us/variables/gov/states/ca/tax/income/ca_agi_subtractions.py
@@ -7,5 +7,9 @@ class ca_agi_subtractions(Variable):
     label = "CA AGI subtractions from federal AGI"
     unit = USD
     definition_period = YEAR
-    reference = "https://www.ftb.ca.gov/forms/2021/2021-540.pdf"
+    reference = (
+        "https://www.ftb.ca.gov/forms/2021/2021-540-ca-instructions.html"
+        "https://www.ftb.ca.gov/forms/2022/2022-540-ca-instructions.html"
+    )
     defined_for = StateCode.CA
+    adds = "gov.states.ca.tax.income.agi.subtractions"
