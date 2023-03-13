bbc0f17703b0e7af4bfe6462169d8eac401fc0d1
ziqiancui <zc2633@columbia.edu>
2023-03-13 12:30:55 -0700

    run tests

--- policyengine_us/variables/gov/states/nj/tax/income/exemptions/nj_senior_exemption.py
+++ policyengine_us/variables/gov/states/nj/tax/income/exemptions/nj_senior_exemption.py
@@ -10,26 +10,24 @@ class nj_senior_exemption(Variable):
     defined_for = StateCode.NJ
 
     def formula(tax_unit, period, parameters):
-        # # First get their filing status.
-        # filing_status = tax_unit("filing_status", period)
-        joint = tax_unit("tax_unit_is_joint", period)
+        # First get their filing status.
+        filing_status = tax_unit("filing_status", period)
 
-        # # Then get the NJ senior exemptions part of the parameter tree
-        # p = parameters(period).gov.states.nj.tax.income.exemptions.senior
+        # Then get the NJ senior exemptions part of the parameter tree
+        p = parameters(period).gov.states.nj.tax.income.exemptions.senior
 
-        # # Get the individual filer's age.
-        # age_head = tax_unit("age_head", period)
+        # Get the individual filer's age.
+        age_head = tax_unit("age_head", period)
 
-        # # Determine if head of household (filer) is eligible.
-        # head_eligible = age_head >= p.age_threshold
+        # Determine if head of household (filer) is eligible.
+        head_eligible = age_head >= p.age_threshold
 
-        # # Get the spouse age, if applicable.
-        # age_spouse = tax_unit("age_spouse", period)
+        # Get the spouse age, if applicable.
+        age_spouse = tax_unit("age_spouse", period)
 
-        # # Determine whether spouse is eligible (>= age 65).
-        # joint = filing_status == filing_status.possible_values.JOINT
-        # spouse_eligible = (age_spouse >= p.age_threshold) * joint
+        # Determine whether spouse is eligible (>= age 65).
+        joint = filing_status == filing_status.possible_values.JOINT
+        spouse_eligible = (age_spouse >= p.age_threshold) * joint
 
         # Calculate total senior exemption.
-        # return (int(head_eligible) + int(spouse_eligible)) * p.amount
-        return joint
+        return (int(head_eligible) + int(spouse_eligible)) * p.amount
