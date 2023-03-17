30ba451a7a167f94898305d661a4fb16a5c0a915
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-03-17 16:12:10 -0400

    Factor out ks_num_exemptions variable and add unit tests

--- policyengine_us/variables/gov/states/ks/tax/income/exemptions/ks_exemptions.py
+++ policyengine_us/variables/gov/states/ks/tax/income/exemptions/ks_exemptions.py
@@ -14,11 +14,6 @@ class ks_exemptions(Variable):
     defined_for = StateCode.KS
 
     def formula(tax_unit, period, parameters):
-        filing_status = tax_unit("filing_status", period)
-        statuses = filing_status.possible_values
-        joint = filing_status == statuses.JOINT
-        hoh = filing_status == statuses.HEAD_OF_HOUSEHOLD
-        adults = where(joint | hoh, 2, 1)
-        dependents = tax_unit("tax_unit_dependents", period)
+        num_exemptions = tax_unit("ks_num_exemptions", period)
         p = parameters(period).gov.states.ks.tax.income.exemptions
-        return (adults + dependents) * p.amount
+        return num_exemptions * p.amount
