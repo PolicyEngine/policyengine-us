a48d0e43ffe689f5eba91718fc662d97a91e16ad
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-03-17 11:43:03 -0400

    Add ks_income_tax_before_credits formula, parameters, and unit tests

--- policyengine_us/variables/gov/states/ks/tax/income/ks_income_tax_before_credits.py
+++ policyengine_us/variables/gov/states/ks/tax/income/ks_income_tax_before_credits.py
@@ -13,28 +13,14 @@ class ks_income_tax_before_credits(Variable):
     )
     defined_for = StateCode.KS
 
-    """
     def formula(tax_unit, period, parameters):
         filing_status = tax_unit("filing_status", period)
         taxable_income = tax_unit("ks_taxable_income", period)
-        p = parameters(period).gov.states.ca.tax.income.rates
-
-        statuses = filing_status.possible_values
-
-        return select(
-            [
-                filing_status == statuses.SINGLE,
-                filing_status == statuses.SEPARATE,
-                filing_status == statuses.JOINT,
-                filing_status == statuses.WIDOW,
-                filing_status == statuses.HEAD_OF_HOUSEHOLD,
-            ],
-            [
-                p.single.calc(taxable_income),
-                p.separate.calc(taxable_income),
-                p.joint.calc(taxable_income),
-                p.widow.calc(taxable_income),
-                p.head_of_household.calc(taxable_income),
-            ],
+        p = parameters(period).gov.states.ks.tax.income.rates
+        tax = where(
+            filing_status == filing_status.possible_values.JOINT,
+            p.joint.calc(taxable_income),
+            p.other.calc(taxable_income),
         )
-    """
+        zero_tax_taxinc = p.zero_tax_threshold[filing_status]
+        return where(taxable_income <= zero_tax_taxinc, 0, tax)
