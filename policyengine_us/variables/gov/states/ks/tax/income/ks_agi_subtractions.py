1b8f5c2ba7b95bbe6c2a7bb0604f22e6c3e16118
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-03-17 06:47:53 -0400

    Add parameter and unit tests for ks_agi_subtractions

--- policyengine_us/variables/gov/states/ks/tax/income/ks_agi_subtractions.py
+++ policyengine_us/variables/gov/states/ks/tax/income/ks_agi_subtractions.py
@@ -17,6 +17,6 @@ class ks_agi_subtractions(Variable):
 
     def formula(tax_unit, period, parameters):
         agi = tax_unit("adjusted_gross_income", period)
-        taxable_oasdi = tax_unit("taxable_social_security", period)
+        taxable_oasdi = add(tax_unit, period, ["taxable_social_security"])
         p = parameters(period).gov.states.ks.tax.income.agi.subtractions
         return where(agi <= p.oasdi.agi_limit, taxable_oasdi, 0)
