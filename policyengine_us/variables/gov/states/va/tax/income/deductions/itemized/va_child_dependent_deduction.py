8f944f9de6acce44a7d7959f8579478b10fd4923
Jiaxuan Huang <jiaxuan.huang65@gmail.com>
2023-03-21 16:22:18 -0400

    Virginia income tax: Itemized deduction for child/dependent care indepense Fixes #1960

---
+++ policyengine_us/variables/gov/states/va/tax/income/deductions/itemized/va_child_dependent_deduction.py
@@ -0,0 +1,19 @@
+from policyengine_us.model_api import *
+import numpy as np
+
+class va_dependent_exemption(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "VA dependent exemption"
+    unit = USD
+    definition_period = YEAR
+
+    def formula(tax_unit, period, parameters):
+        amount = parameters(
+            period
+        ).gov.states.va.tax.income.deductions.itemized.child_dependent
+        if tax_unit("tax_unit_dependents", period)<2:
+            return amount
+        else:
+            return amount*2
+    
