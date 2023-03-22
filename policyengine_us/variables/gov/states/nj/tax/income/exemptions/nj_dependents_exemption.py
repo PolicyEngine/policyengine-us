d0b05559e9bdd826677c71de04f55186764d7c88
ziqiancui <zc2633@columbia.edu>
2023-03-22 13:12:55 -0700

    updated tests

--- policyengine_us/variables/gov/states/nj/tax/income/exemptions/nj_dependents_exemption.py
+++ policyengine_us/variables/gov/states/nj/tax/income/exemptions/nj_dependents_exemption.py
@@ -5,15 +5,12 @@ class nj_dependents_exemption(Variable):
     value_type = float
     entity = TaxUnit
     label = "New Jersey qualified and other dependent children exemption"
+    reference = ...
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.NJ
 
     def formula(tax_unit, period, parameters):
-        # First get their filing status.
-        # filing_status = tax_unit("filing_status", period)
-        # joint = filing_status == filing_status.possible_values.JOINT
-
         # Then get the NJ Exemptions part of the parameter tree.
         p = parameters(period).gov.states.nj.tax.income.exemptions.dependents
 
