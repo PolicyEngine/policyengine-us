6f64c02bb3a1ee1e220c43ed9340900dd5cf2499
Linfeng-ll <95734090+Linfeng-ll@users.noreply.github.com>
2023-03-23 22:29:45 +0000

    add ohio subtractions and agi variable

---
+++ policyengine_us/variables/gov/states/oh/tax/income/adjusted_gross_income/oh_adjusted_gross_income.py
@@ -0,0 +1,24 @@
+from policyengine_us.model_api import *
+
+
+class oh_adjusted_gross_income(Variable):
+    value_type = float
+    entity = Person
+    label = "Ohio adjusted gross income"
+    unit = USD
+    definition_period = YEAR
+    reference = (
+        "https://tax.ohio.gov/static/forms/ohio_individual/individual/2021/pit-it1040-booklet.pdf#page=20",
+        "https://codes.ohio.gov/ohio-revised-code/section-5747.055",
+    )
+    defined_for = StateCode.OH
+
+    adds = []
+    subtracts = [
+        "oh_military_pay_outside_ohio_deduction",
+        "oh_uniformed_services_retirement_income_deduction",
+        "investment_in_529_plan",
+        "pell_grant",
+        "educator_expense",
+        "disability_benefits"
+    ]
