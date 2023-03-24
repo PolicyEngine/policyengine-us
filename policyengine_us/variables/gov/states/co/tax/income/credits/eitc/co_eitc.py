8b415974d6cfd178c5f629e80ab10efa138ea738
Caleb <caleblpena@gmail.com>
2023-03-24 13:07:39 -0600

    make changes from code review

--- policyengine_us/variables/gov/states/co/tax/income/credits/eitc/co_eitc.py
+++ policyengine_us/variables/gov/states/co/tax/income/credits/eitc/co_eitc.py
@@ -8,11 +8,16 @@ class co_eitc(Variable):
     unit = USD
     definition_period = YEAR
     reference = "https://leg.colorado.gov/sites/default/files/te19_colorado_earned_income_tax_credit.pdf"
-    defined_for = "co_eitc_eligible"
+    defined_for = StateCode.CO
 
     def formula(tax_unit, period, parameters):
         federal_eitc = tax_unit("earned_income_tax_credit", period)
+        eligible = tax_unit("eitc_eligible", period)
         match_percent = parameters(
             period
         ).gov.states.co.tax.income.credits.eitc.match
-        return federal_eitc * match_percent
+        return where(
+            eligible,
+            federal_eitc * match_percent,
+            0
+        )
