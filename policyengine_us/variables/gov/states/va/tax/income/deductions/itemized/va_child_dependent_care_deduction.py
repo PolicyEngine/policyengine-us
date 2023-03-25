f30b9be159e99e48c7f8bf947da1d6c6c224dcd9
Jiaxuan Huang <jiaxuan.huang65@gmail.com>
2023-03-25 13:17:02 -0400

    Modified parameters name in the varible file, delect filling status as the result is identical for all

--- policyengine_us/variables/gov/states/va/tax/income/deductions/itemized/va_child_dependent_care_deduction.py
+++ policyengine_us/variables/gov/states/va/tax/income/deductions/itemized/va_child_dependent_care_deduction.py
@@ -5,17 +5,17 @@ import numpy as np
 class va_child_dependent_care_deduction(Variable):
     value_type = float
     entity = TaxUnit
-    label = "VA dependent exemption"
+    label = "VA child/dependent exemption"
     unit = USD
     definition_period = YEAR
 
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states.va.tax.income.deductions.itemized
-        tax_unit_dependents = tax_unit("tax_unit_dependents", period)
+        dependents_count = tax_unit("tax_unit_dependents", period)
         amount = p.child_dependent_deduction
 
         return np.where(
-            tax_unit("tax_unit_dependents", period) > 2,
+            dependents_count  > 2,
             2 * amount,
-            tax_unit("tax_unit_dependents", period) * amount,
+            dependents_count * amount,
         )
