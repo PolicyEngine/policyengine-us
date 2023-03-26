6ee80f17ff753afb2362ca407396240200a818b9
oliverh233 <127977055+oliverh233@users.noreply.github.com>
2023-03-26 22:13:45 +0000

    fixes variable

--- policyengine_us/variables/gov/states/va/tax/income/rates/va_income_tax_before_credits.py
+++ policyengine_us/variables/gov/states/va/tax/income/rates/va_income_tax_before_credits.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class va_income_tax_before_credits(Variable):
     value_type = float
     entity = TaxUnit
-    label = "VA State income tax rate"
+    label = "VA income tax before credits"
     unit = USD
     definition_period = YEAR
     reference = (
