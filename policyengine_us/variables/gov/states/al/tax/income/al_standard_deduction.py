495fe5eb790e87c74b437c48a5791cb309836dde
dnnnnn0626 <77896070+dnnnnn0626@users.noreply.github.com>
2023-03-26 10:58:21 -0400

    fixes issues

--- policyengine_us/variables/gov/states/al/tax/income/al_standard_deduction.py
+++ policyengine_us/variables/gov/states/al/tax/income/al_standard_deduction.py
@@ -13,24 +13,24 @@ class al_standard_deduction(Variable):
 
     def formula(tax_unit, period, parameters):
         gov = parameters(period).gov
-        al_standard_deductions_params_path = gov.states.al.tax.income
+        p = gov.states.al.tax.income.deductions.standard
         filing_status = tax_unit("filing_status", period)
-        base_amount = al_standard_deductions_params_path.phase_out.base_amount[
+        base_amount = p.base_amount[
             filing_status
         ]
-        increment = al_standard_deductions_params_path.phase_out.increment[
+        increment = p.phase_out.increment[
             filing_status
         ]
-        min_amount = al_standard_deductions_params_path.phase_out.min_amount[
+        min_amount = p.phase_out.min_amount[
             filing_status
         ]
-        rate = al_standard_deductions_params_path.phase_out.rate[filing_status]
-        threshold = al_standard_deductions_params_path.phase_out.threshold[
+        rate = p.phase_out.rate[filing_status]
+        threshold = p.phase_out.threshold[
             filing_status
         ]
         al_taxable_income = tax_unit("al_taxable_income", period)
         excess_income = max(0, al_taxable_income - threshold)
-        excess_deduction = excess_income // increment * increment * rate
-        standard_deduction = max(min_amount, base_amount - excess_deduction)
+        increments = excess_income // increment
+        reduction= increments * rate
 
-        return standard_deduction
+        return max(base_amount - reduction,min_amount)
