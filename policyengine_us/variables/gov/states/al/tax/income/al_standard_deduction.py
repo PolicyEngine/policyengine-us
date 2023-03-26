a223756fb8015605b5c914956bf7ad94e8e71a99
dnnnnn0626 <77896070+dnnnnn0626@users.noreply.github.com>
2023-03-26 11:00:20 -0400

    fixed issues

--- policyengine_us/variables/gov/states/al/tax/income/al_standard_deduction.py
+++ policyengine_us/variables/gov/states/al/tax/income/al_standard_deduction.py
@@ -15,22 +15,14 @@ class al_standard_deduction(Variable):
         gov = parameters(period).gov
         p = gov.states.al.tax.income.deductions.standard
         filing_status = tax_unit("filing_status", period)
-        base_amount = p.base_amount[
-            filing_status
-        ]
-        increment = p.phase_out.increment[
-            filing_status
-        ]
-        min_amount = p.phase_out.min_amount[
-            filing_status
-        ]
+        base_amount = p.base_amount[filing_status]
+        increment = p.phase_out.increment[filing_status]
+        min_amount = p.phase_out.min_amount[filing_status]
         rate = p.phase_out.rate[filing_status]
-        threshold = p.phase_out.threshold[
-            filing_status
-        ]
+        threshold = p.phase_out.threshold[filing_status]
         al_taxable_income = tax_unit("al_taxable_income", period)
         excess_income = max(0, al_taxable_income - threshold)
         increments = excess_income // increment
-        reduction= increments * rate
+        reduction = increments * rate
 
-        return max(base_amount - reduction,min_amount)
+        return max(base_amount - reduction, min_amount)
