305d56c55eff67c05563d44b7d91fd974c60b5b5
Linfeng-ll <95734090+Linfeng-ll@users.noreply.github.com>
2023-03-27 23:06:45 +0000

    Delete formula in empty variable

--- policyengine_us/variables/gov/states/oh/tax/income/credits/oh_has_not_taken_oh_lump_sum_credits.py
+++ policyengine_us/variables/gov/states/oh/tax/income/credits/oh_has_not_taken_oh_lump_sum_credits.py
@@ -12,6 +12,3 @@ class oh_has_not_taken_oh_lump_sum_credits(Variable):
         "https://codes.ohio.gov/ohio-revised-code/section-5747.055",
     )
     defined_for = StateCode.OH
-
-    def formula(person, period, parameters):
-        return 1
