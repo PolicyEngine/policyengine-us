15a03533198744ee9a2397a2b04f1f002b636322
Linfeng-ll <95734090+Linfeng-ll@users.noreply.github.com>
2023-03-29 23:19:09 +0000

    reference switch

--- policyengine_us/variables/gov/states/oh/tax/income/adjusted_gross_income/oh_agi.py
+++ policyengine_us/variables/gov/states/oh/tax/income/adjusted_gross_income/oh_agi.py
@@ -8,8 +8,8 @@ class oh_agi(Variable):
     unit = USD
     definition_period = YEAR
     reference = (
-        "https://tax.ohio.gov/static/forms/ohio_individual/individual/2021/pit-it1040-booklet.pdf#page=20",
         "https://codes.ohio.gov/ohio-revised-code/section-5747.055",
+        "https://tax.ohio.gov/static/forms/ohio_individual/individual/2021/pit-it1040-booklet.pdf#page=20",
     )
     defined_for = StateCode.OH
 
