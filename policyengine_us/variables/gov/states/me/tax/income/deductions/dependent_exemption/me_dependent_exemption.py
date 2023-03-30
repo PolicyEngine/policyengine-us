99a01dd89da688269431c2c2fda9e9686533dee9
Zixuan Ma <66671734+mzx-4real@users.noreply.github.com>
2023-03-30 16:38:50 -0400

    Reformat and suggested change

--- policyengine_us/variables/gov/states/me/tax/income/deductions/dependent_exemption/me_dependent_exemption.py
+++ policyengine_us/variables/gov/states/me/tax/income/deductions/dependent_exemption/me_dependent_exemption.py
@@ -6,9 +6,7 @@ class me_dependent_exemption(Variable):
     value_type = float
     entity = TaxUnit
     label = "Maine dependents exemption deduction"
-    reference = (
-        "https://www.mainelegislature.org/legis/statutes/36/title36sec5219-SS.html"
-    )
+    reference = "https://www.mainelegislature.org/legis/statutes/36/title36sec5219-SS.html"
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.ME
@@ -21,7 +19,9 @@ class me_dependent_exemption(Variable):
         me_agi = tax_unit("me_agi", period)
 
         # Then get the Maine Dependents Exemptions part of the parameter tree.
-        p = parameters(period).gov.states.me.tax.income.deductions.dependent_exemption
+        p = parameters(
+            period
+        ).gov.states.me.tax.income.deductions.dependent_exemption
 
         # Calculate the maximum dependents exemption amount (line 2).
         max_amount = num_dependents * p.amount
@@ -34,10 +34,10 @@ class me_dependent_exemption(Variable):
 
         # Get the excess amount, if any, in thousands of dollars (rounded up) [lines 5 and 6].
         excess = max_(me_agi - phaseout_start, 0)
-        excess = ceil(excess / 1_000)
+        increments = ceil(excess / p.phaseout.increment)
 
         # Calculate the excess part phase out amount (line 7).
-        phase_out_amount = excess * p.phaseout.step
+        phase_out_amount = increments * p.phaseout.step
 
         # Return the max exemption amount minus the phaseout amount (line 8).
-        return max(max_amount - phase_out_amount, 0)
+        return max_(max_amount - phase_out_amount, 0)
