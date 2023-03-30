9b365452fb86b4603f34db8c8ff48252b12717d9
Donglai Xu <donglaixu99@gmail.com>
2023-03-30 17:47:50 -0400

    NY TANF flat income disregard Fixes #1978

--- policyengine_us/variables/gov/states/ny/otda/tanf/income/ny_tanf_countable_earned_income.py
+++ policyengine_us/variables/gov/states/ny/otda/tanf/income/ny_tanf_countable_earned_income.py
@@ -14,4 +14,8 @@ class ny_tanf_countable_earned_income(Variable):
         gross_earned_income = spm_unit("ny_tanf_gross_earned_income", period)
         # Multiply by 100% minus the EID.
         p = parameters(period).gov.states.ny.otda.tanf
-        return gross_earned_income * (1 - p.income.earned_income_deduction)
+        return max_(
+            gross_earned_income * (1 - p.income.earned_income_deduction)
+            - p.income.earned_income_deduction_flat,
+            0,
+        )
