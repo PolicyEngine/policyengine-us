c936e97bbbefd17f198f11a0390b3bdd301ed7c6
ziqiancui <zc2633@columbia.edu>
2023-03-30 21:31:48 -0700

    make format

--- policyengine_us/variables/gov/states/nj/tax/income/exemptions/nj_total_exemptions.py
+++ policyengine_us/variables/gov/states/nj/tax/income/exemptions/nj_total_exemptions.py
@@ -13,5 +13,5 @@ class nj_total_exemptions(Variable):
         "nj_regular_exemption",
         "nj_senior_exemption",
         "nj_dependents_exemption",
-        "nj_blind_or_disabled_exemption"
+        "nj_blind_or_disabled_exemption",
     ]
