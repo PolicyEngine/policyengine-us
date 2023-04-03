d6aa807ba8febfc79ee5b7d869bbc27aabf9c981
Donglai Xu <donglaixu99@gmail.com>
2023-04-03 16:20:06 -0400

    Formula for NY TANF gross unearned income Fixes #1977

--- policyengine_us/variables/gov/states/ny/otda/tanf/income/ny_tanf_countable_gross_unearned_income.py
+++ policyengine_us/variables/gov/states/ny/otda/tanf/income/ny_tanf_countable_gross_unearned_income.py
@@ -9,8 +9,4 @@ class ny_tanf_countable_gross_unearned_income(Variable):
     definition_period = YEAR
     defined_for = StateCode.NY
 
-    def formula(spm_unit, period, parameters):
-        p = parameters(period).gov.states.ny.otda.tanf.income
-        gross_unearned = add(spm_unit, period, p.unearned_income)
-        enrolled = spm_unit("is_tanf_enrolled", period)
-        return gross_unearned
+    adds = "gov.states.ny.otda.tanf.income.unearned"
