9fe1e029730b5e32a59bd53ef7d20c4815ce80fb
ziqiancui <zc2633@columbia.edu>
2023-04-03 16:04:29 -0700

    NJ CDCC Fixes #1933

---
+++ policyengine_us/variables/gov/states/nj/tax/income/credits/cdcc/nj_cdcc.py
@@ -0,0 +1,22 @@
+from policyengine_us.model_api import *
+import numpy as np
+
+
+class nj_cdcc(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "New Jersey CDCC"
+    documentation = "New Jersey Child and Dependent Care Credit"
+    unit = USD
+    definition_period = YEAR
+    reference = "https://www.state.nj.us/treasury/taxation/pdf/current/1040i.pdf#page=44"
+    defined_for = StateCode.NJ
+
+    def formula(tax_unit, period, parameters):
+        p = parameters(
+            period
+        ).gov.states.nj.tax.income.credits.cdcc.rate
+        agi = tax_unit("adjusted_gross_income", period)
+        federal_cdcc = tax_unit("cdcc", period)
+        rate = p.calc(agi)
+        return federal_cdcc * rate
