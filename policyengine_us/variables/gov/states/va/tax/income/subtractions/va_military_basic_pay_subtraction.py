4f3658a968a37357266d077e5853f13db30d6af7
oliverh233 <127977055+oliverh233@users.noreply.github.com>
2023-04-05 19:13:08 +0000

    make format

--- policyengine_us/variables/gov/states/va/tax/income/subtractions/va_military_basic_pay_subtraction.py
+++ policyengine_us/variables/gov/states/va/tax/income/subtractions/va_military_basic_pay_subtraction.py
@@ -14,11 +14,19 @@ class va_military_basic_pay_subtraction(Variable):
 
     def formula(tax_unit, period, parameters):
         person = tax_unit.members
-        p = parameters(period).gov.states.va.tax.income.subtractions.military_basic_pay
+        p = parameters(
+            period
+        ).gov.states.va.tax.income.subtractions.military_basic_pay
         # Compute subtractable military pay for head and spouse separately.
         military_pay = person("military_pay", period)
         # Subtraction phases in and then out dollar for dollar with respect to military pay, at a given threshold.
-        subtractable_military_pay = where(military_pay < p.threshold, military_pay, max_(0, (2 * p.threshold) - military_pay))
-        is_head_or_spouse = person("is_tax_unit_head", period) | person("is_tax_unit_spouse", period)
+        subtractable_military_pay = where(
+            military_pay < p.threshold,
+            military_pay,
+            max_(0, (2 * p.threshold) - military_pay),
+        )
+        is_head_or_spouse = person("is_tax_unit_head", period) | person(
+            "is_tax_unit_spouse", period
+        )
         # Sum subtractable military pay for heads and spouses.
         return tax_unit.sum(subtractable_military_pay * is_head_or_spouse)
