76796dea24b868c3d111cdad76ebc42f91cce247
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-04-05 15:29:44 -0400

    Add lumpsum tax form references

--- policyengine_us/variables/gov/states/mn/tax/income/mn_lumpsum_tax.py
+++ policyengine_us/variables/gov/states/mn/tax/income/mn_lumpsum_tax.py
@@ -7,7 +7,10 @@ class mn_lumpsum_tax(Variable):
     label = "Minnesota lump-sum tax"
     unit = USD
     definition_period = YEAR
-    reference = ()
+    reference = (
+        "https://www.revenue.state.mn.us/sites/default/files/2021-12/m1ls_21.pdf"
+        "https://www.revenue.state.mn.us/sites/default/files/2023-01/m1ls_22.pdf"
+    )
     defined_for = StateCode.MN
 
     # def formula(tax_unit, period, parameters):
