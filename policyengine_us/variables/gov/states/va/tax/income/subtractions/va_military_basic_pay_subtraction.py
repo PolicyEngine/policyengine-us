efedd8a1ccc14aad55108116d1cb7f479459ff5e
oliverh233 <127977055+oliverh233@users.noreply.github.com>
2023-04-05 19:06:52 +0000

    modified variable

--- policyengine_us/variables/gov/states/va/tax/income/subtractions/va_military_basic_pay_subtraction.py
+++ policyengine_us/variables/gov/states/va/tax/income/subtractions/va_military_basic_pay_subtraction.py
@@ -3,7 +3,7 @@ from policyengine_us.model_api import *
 
 class va_military_basic_pay_subtraction(Variable):
     value_type = float
-    entity = Person
+    entity = TaxUnit
     label = "Virginia military basic pay subtraction"
     defined_for = StateCode.VA
     unit = USD
@@ -12,12 +12,13 @@ class va_military_basic_pay_subtraction(Variable):
         "https://law.lis.virginia.gov/vacodefull/title58.1/chapter3/article2/"
     )
 
-    def formula(person, period, parameters):
+    def formula(tax_unit, period, parameters):
+        person = tax_unit.members
         p = parameters(period).gov.states.va.tax.income.subtractions.military_basic_pay
-        # TODO: Sum for head and spouse - not just everyone in tax unit.
-        # Use is_tax_unit_head and is_tax_unit_spouse
-        head_military_pay = person("military_pay", "is_tax_unit_head", period)
-        spouse_military_pay = person("military_pay", "is_tax_unit_spouse", period)
-        military_pay_sum = sum(where(head_military_pay < p.threshold, head_military_pay, max_(0, (2 * p.threshold) - head_military_pay)),
-                               where(spouse_military_pay < p.threshold, spouse_military_pay, max_(0, (2 * p.threshold) - spouse_military_pay)))
-        return military_pay_sum
+        # Compute subtractable military pay for head and spouse separately.
+        military_pay = person("military_pay", period)
+        # Subtraction phases in and then out dollar for dollar with respect to military pay, at a given threshold.
+        subtractable_military_pay = where(military_pay < p.threshold, military_pay, max_(0, (2 * p.threshold) - military_pay)
+        is_head_or_spouse = person("is_tax_unit_head", period) | person("is_tax_unit_spouse", period)
+        # Sum subtractable military pay for heads and spouses.
+        return tax_unit.sum(subtractable_military_pay * is_head_or_spouse)
