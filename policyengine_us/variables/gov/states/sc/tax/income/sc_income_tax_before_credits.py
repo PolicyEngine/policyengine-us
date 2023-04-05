2068b2efb3983c9786e40a606e6d9a1c1b180882
Pinyan <129036923+pxu12@users.noreply.github.com>
2023-04-05 15:28:40 +0000

    South Carolina rate structure Fixes #2067
    Co-authored-by: Max Ghenis <mghenis@gmail.com>
    Co-authored-by: hua7450 <hua7450@users.noreply.github.com>

--- policyengine_us/variables/gov/states/sc/tax/income/sc_income_tax_before_credits.py
+++ policyengine_us/variables/gov/states/sc/tax/income/sc_income_tax_before_credits.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class sc_income_tax_before_credits(Variable):
     value_type = float
     entity = TaxUnit
-    label = "SC income tax before credits"
+    label = "South Carolina income tax before credits"
     defined_for = StateCode.SC
     unit = USD
     definition_period = YEAR
@@ -12,6 +12,5 @@ class sc_income_tax_before_credits(Variable):
 
     def formula(tax_unit, period, parameters):
         taxable_income = tax_unit("sc_taxable_income", period)
-        p = parameters(period).gov.states.oh.tax.income.rates
-        exempt = tax_unit("sc_income_tax_exempt", period)
-        return p.calc(taxable_income) * ~exempt
+        p = parameters(period).gov.states.sc.tax.income.rates
+        return p.calc(taxable_income)
