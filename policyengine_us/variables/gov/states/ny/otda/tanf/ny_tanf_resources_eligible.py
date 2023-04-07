628460947b9519100f92e69c32b527cbb9ac04cf
Donglai Xu <donglaixu99@gmail.com>
2023-04-07 11:18:01 -0400

    Change labels

--- policyengine_us/variables/gov/states/ny/otda/tanf/ny_tanf_resources_eligible.py
+++ policyengine_us/variables/gov/states/ny/otda/tanf/ny_tanf_resources_eligible.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class ny_tanf_resources_eligible(Variable):
     value_type = bool
     entity = SPMUnit
-    label = "New York TANF asset eligible"
+    label = "New York TANF resources eligible"
     definition_period = YEAR
     defined_for = StateCode.NY
 
