4f8e5dcfb60a4d7171fcffadcd71af05bfc38b1a
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-04-07 08:49:06 -0400

    Add mn_wfc formula, parameters, and unit tests

--- policyengine_us/variables/gov/states/mn/tax/income/credits/mn_wfc.py
+++ policyengine_us/variables/gov/states/mn/tax/income/credits/mn_wfc.py
@@ -4,14 +4,34 @@ from policyengine_us.model_api import *
 class mn_wfc(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Minnesota working family credit"
+    label = "Minnesota working family credit amount"
     unit = USD
     definition_period = YEAR
     reference = (
-        "https://www.revenue.state.mn.us/sites/default/files/2023-01/m1wfc_21.pdf"
-        "https://www.revenue.state.mn.us/sites/default/files/2023-01/m1wfc_22_0.pdf"
+        "https://www.revisor.mn.gov/statutes/2021/cite/290.0671"
+        "https://www.revisor.mn.gov/statutes/cite/290.0671"
     )
     defined_for = StateCode.MN
 
-    # def formula(tax_unit, period, parameters):
-    #    p = parameters(period).gov.states.mn.tax.income.credits
+    def formula(tax_unit, period, parameters):
+        wfc = parameters(period).gov.states.mn.tax.income.credits.wfc
+        # determine count of eligible dependents using EITC rules
+        count = tax_unit("eitc_child_count", period)
+        # determine pre-phaseout credit amount using EITC earnings
+        earnings = max_(0, tax_unit("filer_earned", period))
+        capped_earn = min_(earnings, wfc.phasein.earnings_maximum.calc(count))
+        amount = capped_earn * wfc.phasein.rate.calc(count)
+        # determine phaseout reduction
+        agi = tax_unit("adjusted_gross_income", period)
+        income = max_(earnings, agi)
+        filing_status = tax_unit("filing_status", period)
+        income_threshold = where(
+            filing_status == filing_status.possible_values.JOINT,
+            wfc.phaseout.joint_threshold.calc(count),
+            wfc.phaseout.other_threshold.calc(count),
+        )
+        excess_income = max_(0, income - income_threshold)
+        reduction = excess_income * wfc.phaseout.rate.calc(count)
+        # determine credit amount after phaseout if eligible
+        eligible = tax_unit("mn_wfc_eligible", period)
+        return eligible * max_(0, amount - reduction)
