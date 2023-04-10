5b6a0ea9f7321f7f0c7d4f49f722a2f9943bd261
Donglai Xu <donglaixu99@gmail.com>
2023-04-10 14:40:58 -0400

    Modify description

--- policyengine_us/variables/gov/states/nj/njdhs/tanf/nj_tanf_resources_eligible.py
+++ policyengine_us/variables/gov/states/nj/njdhs/tanf/nj_tanf_resources_eligible.py
@@ -9,6 +9,8 @@ class nj_tanf_resources_eligible(Variable):
     defined_for = StateCode.NJ
 
     def formula(spm_unit, period, parameters):
+        # The WFNJ/TANF resource limit is $2,000 for an assistance unit.
+        # https://www.state.nj.us/humanservices/providers/grants/public/publicnoticefiles/NJ%20TANF%20State%20Plan%20FFY%2021%20-%20FFY%2023%20DRAFT.pdf#page=20
         p = parameters(period).gov.states.nj.njdhs.tanf.eligibility.resources
         countable_resources = spm_unit("nj_tanf_countable_resources", period)
         return countable_resources <= p.resource_limit
