d47054d3d38a3fc6806f84fb33b779d828d4bad3
Jiaxuan Huang <jiaxuan.huang65@gmail.com>
2023-04-09 21:06:37 -0400

    Created new varible file for AFAGI

--- policyengine_us/variables/gov/states/va/tax/income/reductions/AFAGI.py
+++ policyengine_us/variables/gov/states/va/tax/income/reductions/AFAGI.py
@@ -1,1 +1,9 @@
+class va_AFAGI(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Available Federal adjusted gross income"
+    unit = USD
+    definition_period = YEAR
+    reference = "https://www.tax.virginia.gov/sites/default/files/vatax-pdf/2022-760-instructions.pdf#page=16"
+    defined_for = StateCode.VA
 
