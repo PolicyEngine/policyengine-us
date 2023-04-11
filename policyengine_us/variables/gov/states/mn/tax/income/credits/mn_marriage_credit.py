0d2554288b2d0d6543df1a5f9c5a46e9ef790019
Max Ghenis <mghenis@gmail.com>
2023-04-11 00:01:56 -0400

    update standard base amount filename in marriage credit file

--- policyengine_us/variables/gov/states/mn/tax/income/credits/mn_marriage_credit.py
+++ policyengine_us/variables/gov/states/mn/tax/income/credits/mn_marriage_credit.py
@@ -36,7 +36,7 @@ class mn_marriage_credit(Variable):
         # determine overall eligibility for credit
         eligible = joint & individual_income_eligible & taxable_income_eligible
         # determine credit amount
-        std_ded = mn_itax.deductions.standard.base_amount[filing_status]
+        std_ded = mn_itax.deductions.standard.base[filing_status]
         fractional_std_ded = p.standard_deduction_fraction * std_ded
         taxinc1 = max_(0, min_income - fractional_std_ded)
         itax1 = mn_itax.rates.single.calc(taxinc1)
