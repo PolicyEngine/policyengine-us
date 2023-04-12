ea961843706fc8a9e7fadacdbb2e094c64c5a109
Donglai Xu <donglaixu99@gmail.com>
2023-04-12 10:05:37 -0400

    New Jersey TANF resource limit Fixes #2099

--- policyengine_us/variables/gov/states/nj/njdhs/tanf/nj_tanf_resources_eligible.py
+++ policyengine_us/variables/gov/states/nj/njdhs/tanf/nj_tanf_resources_eligible.py
@@ -13,4 +13,4 @@ class nj_tanf_resources_eligible(Variable):
         # https://www.state.nj.us/humanservices/providers/grants/public/publicnoticefiles/NJ%20TANF%20State%20Plan%20FFY%2021%20-%20FFY%2023%20DRAFT.pdf#page=20
         p = parameters(period).gov.states.nj.njdhs.tanf.eligibility.resources
         countable_resources = spm_unit("nj_tanf_countable_resources", period)
-        return countable_resources <= p.resource_limit
+        return countable_resources <= p.limit
