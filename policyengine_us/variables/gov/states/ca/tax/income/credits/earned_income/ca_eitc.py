e5191eaafc754f211640b15cc4ceabafbea8b3cb
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-04-13 17:46:43 -0400

    black format change of comment

--- policyengine_us/variables/gov/states/ca/tax/income/credits/earned_income/ca_eitc.py
+++ policyengine_us/variables/gov/states/ca/tax/income/credits/earned_income/ca_eitc.py
@@ -33,7 +33,7 @@ class ca_eitc(Variable):
         second_phase_out_start_eitc = p.phase_out.final.start.calc(
             child_count
         )  # Expressed as the EITC amount at which the second phase-out starts,
-           # not the income level.
+        # not the income level.
 
         maximum_eitc = phase_in_max_income * phase_in_rate
         eitc_fall_by_first_phase_out = (
