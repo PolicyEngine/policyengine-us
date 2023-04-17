c3b90ce9ac2dfdb3937c1f671ac7c6e4f7012e09
yy3187 <129118329+yy3187@users.noreply.github.com>
2023-04-17 22:47:59 +0000

    change parameters

--- policyengine_us/variables/gov/states/la/la_eitc.py
+++ policyengine_us/variables/gov/states/la/la_eitc.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class la_eitc(Variable):
     value_type = float
     entity = TaxUnit
-    label = "LA EITC"
+    label = "Louisiana EITC"
     unit = USD
     definition_period = YEAR
     reference = "https://www.legis.la.gov/legis/Law.aspx?d=453085"
@@ -14,5 +14,5 @@ class la_eitc(Variable):
         federal_eitc = tax_unit("earned_income_tax_credit", period)
         match_percent = parameters(
             period
-        ).gov.states.co.tax.income.credits.eitc.match
+        ).gov.states.la.tax.income.credits.eitc.match
         return federal_eitc * match_percent
