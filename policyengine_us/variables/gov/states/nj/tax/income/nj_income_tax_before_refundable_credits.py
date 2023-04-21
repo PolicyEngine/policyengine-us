449edaa14102cbf717006c7f81bb54199e4655fd
AlexanderMies <alexmies95@gmail.com>
2023-04-21 19:28:22 -0400

    NJ Main Income Tax 2 Fixes #1932

--- policyengine_us/variables/gov/states/nj/tax/income/nj_income_tax_before_refundable_credits.py
+++ policyengine_us/variables/gov/states/nj/tax/income/nj_income_tax_before_refundable_credits.py
@@ -8,3 +8,27 @@ class nj_income_tax_before_refundable_credits(Variable):
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.NJ
+
+    def formula(tax_unit, period, parameters):
+        taxable_income = tax_unit("nj_taxable_income", period)
+        filing_status = tax_unit("filing_status", period)
+        status = filing_status.possible_values
+
+        p = parameters(period).gov.states.nj.tax.income.main
+
+        return select(
+            [
+                filing_status == status.SINGLE,
+                filing_status == status.JOINT,
+                filing_status == status.HEAD_OF_HOUSEHOLD,
+                filing_status == status.WIDOW,
+                filing_status == status.SEPARATE,
+            ],
+            [
+                p.single_separate.calc(taxable_income),
+                p.joint_hoh_spouse.calc(taxable_income),
+                p.joint_hoh_spouse.calc(taxable_income),
+                p.joint_hoh_spouse.calc(taxable_income),
+                p.single_separate.calc(taxable_income),
+            ],
+        )
