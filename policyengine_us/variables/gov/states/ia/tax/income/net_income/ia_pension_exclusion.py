26f77e9957d92f83d4dc4ed88c3969a145942350
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-04-24 15:08:59 -0400

    Add ia_pension_exclusion formula

--- policyengine_us/variables/gov/states/ia/tax/income/net_income/ia_pension_exclusion.py
+++ policyengine_us/variables/gov/states/ia/tax/income/net_income/ia_pension_exclusion.py
@@ -8,12 +8,28 @@ class ia_pension_exclusion(Variable):
     unit = USD
     definition_period = YEAR
     reference = (
-        "https://tax.iowa.gov/sites/default/files/2022-01/IA1040%2841-001%29.pdf"
-        "https://tax.iowa.gov/sites/default/files/2023-01/2021%20Expanded%20Instructions_010323.pdf"
-        "https://tax.iowa.gov/sites/default/files/2023-01/2022IA1040%2841001%29.pdf"
-        "https://tax.iowa.gov/sites/default/files/2023-03/2022%20Expanded%20Instructions_022023.pdf"
+        "https://tax.iowa.gov/sites/default/files/2023-01/2021%20Expanded%20Instructions_010323.pdf#page=26"
+        "https://tax.iowa.gov/sites/default/files/2023-03/2022%20Expanded%20Instructions_022023.pdf#page=26"
     )
     defined_for = StateCode.IA
 
-
-#    def formula(tax_unit, period, parameters):
+    def formula(tax_unit, period, parameters):
+        p = parameters(period).gov.states.ia.tax.income.net_income
+        # determine eligibility for pension exclusion
+        person = tax_unit.members
+        is_head = person("is_tax_unit_head", period)
+        is_spouse = person("is_tax_unit_spouse", period)
+        # ... determine age eligibility
+        is_elderly = person("age", period) >= p.pension_exclusion.minimum_age
+        is_elderly = (is_head & is_elderly) | (is_spouse & is_elderly)
+        # ... determine disability eligiblity
+        has_disability = person("is_permanently_and_totally_disabled", period)
+        is_disabled = (is_head & has_disability) | (is_spouse & has_disability)
+        # ... determine exclusion eligibility
+        is_eligible = is_elderly | is_disabled
+        # determine pension exclusion amount
+        pension = person("taxable_pension_income", period)
+        uncapped_exclusion = tax_unit.sum(is_eligible * pension)
+        filing_status = tax_unit("filing_status", period)
+        exclusion_cap = p.pension_exclusion.maximum_amount[filing_status]
+        return min_(uncapped_exclusion, exclusion_cap)
