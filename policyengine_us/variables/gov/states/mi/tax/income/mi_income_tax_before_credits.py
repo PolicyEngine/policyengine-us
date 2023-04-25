5165014d0b685f9a683180159c5a296d2e6ce052
PavelMakarchuk <pavel.ma99@gmail.com>
2023-04-25 17:47:43 -0400

    tests
    Co-authored-by: Matty <Mshuning@users.noreply.github.com>

--- policyengine_us/variables/gov/states/mi/tax/income/mi_income_tax_before_credits.py
+++ policyengine_us/variables/gov/states/mi/tax/income/mi_income_tax_before_credits.py
@@ -8,11 +8,8 @@ class mi_income_tax_before_credits(Variable):
     defined_for = StateCode.MI
     unit = USD
     definition_period = YEAR
-    reference = 
 
     def formula(tax_unit, period, parameters):
         taxable_income = tax_unit("mi_taxable_income", period)
         rate = parameters(period).gov.states.mi.tax.income.rate
         return taxable_income * rate
-
-#TODO: test
