28b1a39713c968446fd77af14202506f55362fa1
leogoldman <leogoldman1@gmail.com>
2023-04-25 11:40:44 -0400

    Edit comment.

--- policyengine_us/variables/gov/states/nj/tax/income/credits/eitc/nj_eitc.py
+++ policyengine_us/variables/gov/states/nj/tax/income/credits/eitc/nj_eitc.py
@@ -19,8 +19,8 @@ class nj_eitc(Variable):
 
         # Calculate NJ EITC.
         # If eligible for federal EITC, return federal EITC * percent_of_federal_eitc.
-        # If ineligible for federal EITC only because of age, return max federal EITC (for zero children) * percent_of_federal_eitc.
-        #     Note: this implies that they have no children (hence the max federal EITC input).
+        # If ineligible for federal EITC only because of age, return (max federal EITC for zero children) * percent_of_federal_eitc.
+        #     Note: this implies that they have no children.
         # Otherwise, return 0.
         # Worksheet reference: https://www.state.nj.us/treasury/taxation/pdf/current/1040i.pdf#page=43
         nj_eitc = select(
