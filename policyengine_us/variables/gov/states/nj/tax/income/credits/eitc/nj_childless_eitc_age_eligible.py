d6e3274ba8d25421b147a0c8b64f840fa1eee684
leogoldman <leogoldman1@gmail.com>
2023-04-25 23:57:21 -0400

    Fixed retrieving tax unit head

--- policyengine_us/variables/gov/states/nj/tax/income/credits/eitc/nj_childless_eitc_age_eligible.py
+++ policyengine_us/variables/gov/states/nj/tax/income/credits/eitc/nj_childless_eitc_age_eligible.py
@@ -29,7 +29,7 @@ class nj_childless_eitc_age_eligible(Variable):
         min_age = p.eligibility.age.min
         max_age = p.eligibility.age.max
         meets_age_requirements = (age >= min_age) & (age <= max_age)
-        is_filer = person("is_head", period)
+        is_filer = person("is_tax_unit_head", period)
         filer_age_eligible = tax_unit.sum(is_filer & meets_age_requirements)
 
         return (
