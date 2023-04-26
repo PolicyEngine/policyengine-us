3474bdcad50a30611d33210862fbcf8516752c3f
leogoldman <leogoldman1@gmail.com>
2023-04-26 18:26:11 -0400

    Change age check to anyone in tax unit

--- policyengine_us/variables/gov/states/nj/tax/income/credits/eitc/nj_childless_eitc_age_eligible.py
+++ policyengine_us/variables/gov/states/nj/tax/income/credits/eitc/nj_childless_eitc_age_eligible.py
@@ -18,7 +18,6 @@ class nj_childless_eitc_age_eligible(Variable):
 
         # Check if tax unit has any EITC qualifying children.
         person = tax_unit.members
-        eitc_qualifying_child = person("is_eitc_qualifying_child", period)
         no_qualifying_children = tax_unit("eitc_child_count", period) == 0
 
         # Get the NJ EITC paramaeter tree.
@@ -29,14 +28,10 @@ class nj_childless_eitc_age_eligible(Variable):
         min_age = p.eligibility.age.min
         max_age = p.eligibility.age.max
         meets_age_requirements = (age >= min_age) & (age <= max_age)
-        is_filer = person("is_tax_unit_head", period)
-        filer_age_eligible = (
-            tax_unit.sum(is_filer & meets_age_requirements) > 0
-        )
 
         return (
             ~separate
             & no_qualifying_children
-            & filer_age_eligible
+            & tax_unit.any(meets_age_requirements)
             & tax_unit("nj_eitc_income_eligible", period)
         )
