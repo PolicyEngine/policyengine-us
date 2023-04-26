48f291a5470d5eca5ddb109f50888bbe9c98d896
Shayla Wu <shaylllaaa.wu@gmail.com>
2023-04-26 10:07:59 -0400

    Maryland Tanf make format of new variable files Fixes #2154

--- policyengine_us/variables/gov/states/md/tanf/income/deductions/others/md_tanf_childcare_deduction.py
+++ policyengine_us/variables/gov/states/md/tanf/income/deductions/others/md_tanf_childcare_deduction.py
@@ -1,5 +1,7 @@
 from policyengine_us.model_api import *
-from policyengine_us.variables.gov.states.md.tanf.md_tanf_count_children import md_tanf_count_children
+from policyengine_us.variables.gov.states.md.tanf.md_tanf_count_children import (
+    md_tanf_count_children,
+)
 
 
 class md_tanf_childcare_deduction(Variable):
@@ -15,7 +17,10 @@ class md_tanf_childcare_deduction(Variable):
 
         # Get the policy parameters.
         p = parameters(period).gov.states.md.tanf.income.deductions.earned
-        childcare_deduction = 200 * (workhours >= 100) * md_tanf_count_children + 100 * (workhours < 100) * md_tanf_count_children
-        
+        childcare_deduction = (
+            200 * (workhours >= 100) * md_tanf_count_children
+            + 100 * (workhours < 100) * md_tanf_count_children
+        )
+
         # Return if initially eligible
         return childcare_deduction
--- policyengine_us/variables/gov/states/md/tanf/income/net/md_tanf_net_initial_countable_income.py
+++ policyengine_us/variables/gov/states/md/tanf/income/net/md_tanf_net_initial_countable_income.py
@@ -12,7 +12,9 @@ class md_tanf_net_initial_countable_income(Variable):
     def formula(spm_unit, period, parameters):
         # Get gross income for the SPM unit.
         gross_earned_income = spm_unit("md_tanf_gross_earned_income", period)
-        gross_unearned_income = spm_unit("md_tanf_gross_unearned_income", period)
+        gross_unearned_income = spm_unit(
+            "md_tanf_gross_unearned_income", period
+        )
         # Get initial deductions for the SPM unit.
         initial_deductions = spm_unit("md_tanf_initial_deductions", period)
         # Get alimony deductions for the SPM unit.
@@ -20,8 +22,17 @@ class md_tanf_net_initial_countable_income(Variable):
         alimony_deduction = add(spm_unit, period, p1.alimony_income)
         # Get child support deductions for the SPM unit.
         p2 = parameters(period).household.expense.child_support
-        child_support_deduction = add(spm_unit, period, p2.child_support_received)
+        child_support_deduction = add(
+            spm_unit, period, p2.child_support_received
+        )
         # Get childcare deductions for the SPM unit.
         childcare_deduction = spm_unit("md_tanf_childcare_deduction", period)
 
-        return gross_earned_income + gross_unearned_income - initial_deductions - alimony_deduction - child_support_deduction - childcare_deduction
+        return (
+            gross_earned_income
+            + gross_unearned_income
+            - initial_deductions
+            - alimony_deduction
+            - child_support_deduction
+            - childcare_deduction
+        )
