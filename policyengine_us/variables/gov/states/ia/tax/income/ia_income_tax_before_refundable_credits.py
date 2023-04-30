5eabc4540aa846c611fa720b3606cc6999039c62
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-04-30 18:13:07 -0400

    Use ia_reduced_tax in income_tax_before_refundable_credits

--- policyengine_us/variables/gov/states/ia/tax/income/ia_income_tax_before_refundable_credits.py
+++ policyengine_us/variables/gov/states/ia/tax/income/ia_income_tax_before_refundable_credits.py
@@ -9,9 +9,9 @@ class ia_income_tax_before_refundable_credits(Variable):
     definition_period = YEAR
     reference = (
         "https://tax.iowa.gov/sites/default/files/2022-01/IA1040%2841-001%29.pdf"
-        "https://tax.iowa.gov/sites/default/files/2023-01/2021%20Expanded%20Instructions_010323.pdf"
+        "https://tax.iowa.gov/sites/default/files/2023-01/2021%20Expanded%20Instructions_010323.pdf#page=60"
         "https://tax.iowa.gov/sites/default/files/2023-01/2022IA1040%2841001%29.pdf"
-        "https://tax.iowa.gov/sites/default/files/2023-03/2022%20Expanded%20Instructions_022023.pdf"
+        "https://tax.iowa.gov/sites/default/files/2023-03/2022%20Expanded%20Instructions_022023.pdf#page=60"
     )
     defined_for = StateCode.IA
 
@@ -19,4 +19,6 @@ class ia_income_tax_before_refundable_credits(Variable):
         tax_before = tax_unit("ia_income_tax_before_credits", period)
         nonref_credits = tax_unit("ia_nonrefundable_credits", period)
         is_tax_exempt = tax_unit("ia_is_tax_exempt", period)
-        return ~is_tax_exempt * max_(0, tax_before - nonref_credits)
+        tax_after = ~is_tax_exempt * max_(0, tax_before - nonref_credits)
+        tax_reduced = tax_unit("ia_reduced_tax", period)
+        return min_(tax_after, tax_reduced)
