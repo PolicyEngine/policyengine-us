ea72d38c54289d65bfeb19fa06210ca8b55f2571
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-04-30 09:39:09 -0400

    Rename modified_income_sources.yaml file

--- policyengine_us/variables/gov/states/ia/tax/income/ia_modified_income.py
+++ policyengine_us/variables/gov/states/ia/tax/income/ia_modified_income.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class ia_modified_income(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Iowa modified income used in calculations"
+    label = "Iowa modified income used in tax-exempt and alternate-tax calculations"
     unit = USD
     definition_period = YEAR
     reference = (
@@ -14,4 +14,4 @@ class ia_modified_income(Variable):
         "https://tax.iowa.gov/sites/default/files/2023-03/2022%20Expanded%20Instructions_022023.pdf#page=55"
     )
     defined_for = StateCode.IA
-    adds = "gov.states.ia.tax.income.modified_income_sources"
+    adds = "gov.states.ia.tax.income.modified_income.sources"
