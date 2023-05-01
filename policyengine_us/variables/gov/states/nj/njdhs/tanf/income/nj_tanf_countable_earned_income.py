268a8202871c3823131dc5f8d3c8c20d495c36d0
Donglai Xu <donglaixu99@gmail.com>
2023-05-01 14:35:15 -0400

    New Jersey TANF earned income disregard Fixes #2217
    Co-authored-by: VioletChen0916 <VioletChen0916@users.noreply.github.com>

---
+++ policyengine_us/variables/gov/states/nj/njdhs/tanf/income/nj_tanf_countable_earned_income.py
@@ -0,0 +1,35 @@
+from policyengine_us.model_api import *
+
+
+class nj_tanf_countable_earned_income(Variable):
+    value_type = float
+    entity = SPMUnit
+    label = "New Jersey TANF countable earned income"
+    unit = USD
+    definition_period = YEAR
+    defined_for = StateCode.NJ
+
+    def formula(spm_unit, period, parameters):
+        # Get gross earned income.
+        person = spm_unit.members
+        gross_earned_income = spm_unit("nj_tanf_gross_earned_income", period)
+        # Multiply by 100% minus the EID.
+        p = parameters(
+            period
+        ).gov.states.nj.njdhs.tanf.income.earned_income_deduction
+        months_enrolled_in_tanf = person("months_enrolled_in_tanf", period)
+        weekly_hours_worked = person("weekly_hours_worked", period)
+        # New Jerset Admin Code 10:90-3.8(b)
+        return where(
+            months_enrolled_in_tanf > 1,
+            where(
+                months_enrolled_in_tanf > 7,
+                gross_earned_income * (1 - p.additional_percent),
+                where(
+                    weekly_hours_worked >= p.work_hours_threshold,
+                    gross_earned_income * (1 - p.consecutive_month_percent),
+                    gross_earned_income * (1 - p.additional_percent),
+                ),
+            ),
+            gross_earned_income * (1 - p.first_month_percent),
+        )
