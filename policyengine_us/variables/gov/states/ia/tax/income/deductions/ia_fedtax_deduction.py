3844159560215d21be3b95193d8600c0cc5553e1
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-05-02 08:44:44 -0400

    Make ia_fedtax_deduction be a person variable; revise unit test

--- policyengine_us/variables/gov/states/ia/tax/income/deductions/ia_fedtax_deduction.py
+++ policyengine_us/variables/gov/states/ia/tax/income/deductions/ia_fedtax_deduction.py
@@ -3,7 +3,7 @@ from policyengine_us.model_api import *
 
 class ia_fedtax_deduction(Variable):
     value_type = float
-    entity = TaxUnit
+    entity = Person
     label = "Iowa deduction for selected components of federal income tax"
     unit = USD
     definition_period = YEAR
@@ -15,13 +15,24 @@ class ia_fedtax_deduction(Variable):
     )
     defined_for = StateCode.IA
 
-    def formula(tax_unit, period, parameters):
+    def formula(person, period, parameters):
         # Iowa allows a deduction of federal income taxes from Iowa net
         # income, but federal payroll taxes (FICA and SECA and Additional
         # Medicare) cannot be used to reduced Iowa net income.
-        setax = add(tax_unit, period, ["self_employment_tax"])
-        amtax = tax_unit("additional_medicare_tax", period)
-        net_income_additions = setax + amtax
-        # federal income taxes are before refundable federal credits
-        gross_ded = tax_unit("income_tax_before_refundable_credits", period)
-        return gross_ded - net_income_additions
+        is_head = person("is_tax_unit_head", period)
+        is_spouse = person("is_tax_unit_spouse", period)
+        aggregate_tax = (is_head | is_spouse) * (
+            person.tax_unit("income_tax_before_refundable_credits", period)
+            - person.tax_unit("additional_medicare_tax", period)
+        )
+        # prorate aggregate_tax among head and spouse according to net incomes
+        net_income = person("ia_net_income", period)
+        fraction = person("ia_prorate_fraction", period)
+        prorated_tax = fraction * aggregate_tax
+        # allocate any dependent self-employment tax to tax unit head
+        indiv_setax = person("self_employment_tax", period)
+        is_dependent = person("is_tax_unit_dependent", period)
+        sum_dependent_setax = person.tax_unit.sum(is_dependent * indiv_setax)
+        setax = ~is_dependent * indiv_setax + is_head * sum_dependent_setax
+        # return prorated_tax less setax
+        return max_(0, prorated_tax - setax)        
