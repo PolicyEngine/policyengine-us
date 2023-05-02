888609d082c6f0c66beb66372168023a8c166265
Donglai Xu <donglaixu99@gmail.com>
2023-05-02 11:49:43 -0400

    New Jersey TANF income eligible Fixes #2117
    Co-authored-by: VioletChen0916 <VioletChen0916@users.noreply.github.com>

---
+++ policyengine_us/variables/gov/states/nj/njdhs/tanf/nj_tanf_income_eligible.py
@@ -0,0 +1,27 @@
+from policyengine_us.model_api import *
+
+
+class nj_tanf_income_eligible(Variable):
+    value_type = bool
+    entity = SPMUnit
+    label = "New Jersey TANF income eligible"
+    definition_period = YEAR
+    defined_for = StateCode.NJ
+
+    def formula(spm_unit, period, parameters):
+        income = add(
+            spm_unit,
+            period,
+            [
+                "nj_tanf_countable_earned_income",
+                "nj_tanf_countable_gross_unearned_income",
+            ],
+        )
+        maximum_allowable_income = spm_unit(
+            "nj_tanf_maximum_allowable_income", period
+        )
+        maximum_benefit = spm_unit("nj_tanf_maximum_benefit", period)
+        # New Jersey Administrative Code 10:90-3.1 
+        return (income <= maximum_allowable_income) & (
+            income < maximum_benefit
+        )
