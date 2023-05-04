4d0f637b123c5e60ae8e68ff2953cede61ad4657
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-05-04 11:33:23 -0400

    Revise ia_basic_tax formula and unit tests

--- policyengine_us/variables/gov/states/ia/tax/income/ia_basic_tax.py
+++ policyengine_us/variables/gov/states/ia/tax/income/ia_basic_tax.py
@@ -5,18 +5,22 @@ import numpy as np
 class ia_basic_tax(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Iowa basic tax is the smaller of regular and alternate taxes"
+    label = "Iowa basic tax is smaller of base+AMT taxes for indiv and joint"
     unit = USD
     definition_period = YEAR
     reference = (
         "https://tax.iowa.gov/sites/default/files/2022-01/IA1040%2841-001%29.pdf"
-        "https://tax.iowa.gov/sites/default/files/2023-01/2021%20Expanded%20Instructions_010323.pdf#page=53"
+        "https://tax.iowa.gov/sites/default/files/2023-01/2021%20Expanded%20Instructions_010323.pdf"
         "https://tax.iowa.gov/sites/default/files/2023-01/2022IA1040%2841001%29.pdf"
-        "https://tax.iowa.gov/sites/default/files/2023-03/2022%20Expanded%20Instructions_022023.pdf#page=53"
+        "https://tax.iowa.gov/sites/default/files/2023-03/2022%20Expanded%20Instructions_022023.pdf"
     )
     defined_for = StateCode.IA
 
     def formula(tax_unit, period, parameters):
-        regular_tax = tax_unit("ia_regular_tax", period)
-        alternate_tax = tax_unit("ia_alternate_tax", period)
-        return min_(regular_tax, alternate_tax)
+        basic_indiv = add(
+            tax_unit, period, ["ia_base_tax_indiv", "ia_amt_indiv"]
+        )
+        basic_joint = add(
+            tax_unit, period, ["ia_base_tax_joint", "ia_amt_joint"]
+        )
+        return min_(basic_indiv, basic_joint)
