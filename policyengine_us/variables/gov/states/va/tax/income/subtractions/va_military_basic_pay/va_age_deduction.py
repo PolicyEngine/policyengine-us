b2c7bf9b73c1dcf5c2b787eb17b342be4500f050
Jiaxuan Huang <jiaxuan.huang65@gmail.com>
2023-05-07 19:58:00 -0400

    Changed one of the threshold parameters from 84 years old to 1939-01-01

--- policyengine_us/variables/gov/states/va/tax/income/subtractions/va_military_basic_pay/va_age_deduction.py
+++ policyengine_us/variables/gov/states/va/tax/income/subtractions/va_military_basic_pay/va_age_deduction.py
@@ -13,30 +13,32 @@ class va_age_deduction(Variable):
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states.va.tax.income.subtractions
         filing_status = tax_unit("filing_status", period)
+        # input the age of the head of household (and the spouse if applicable)
         age_head = tax_unit("age_head", period)
         age_spouse = tax_unit("age_spouse", period)
-        age_deduction_amount = 0
 
         AFAGI = tax_unit("AFAGI", period)
+        # People who were born on or before the threshold date are eligible for a full deduction
+        threshhold_date = "1939-01-01"
 
-        # calcualte the number of people eligble for age deduction in a household
+        # calcualte the number of people eligble for age deduction in a household (people who are 65 and older)
         eligible_count = sum(
-            where(age_head > 64, 1, 0) + where(age_spouse > 64, 1, 0)
+            where(age_head > 64, 1, 0), where(age_spouse > 64, 1, 0)
         )
+
         # calculate the number of people age >=84 and is eligible for a full deduction
         birth_year_head = year(period) - age_head
-        # Compare to a policy parameter for 1939 per legal code
-        eightyfour_count = sum(
-            where(age_head > 83, 1, 0) + where(age_spouse > 83, 1, 0)
+        birth_year_spouse = year(period) - age_spouse
+        full_deduction_count = sum(
+            where(birth_year_head < year(threshhold_date), 1, 0),
+            where(birth_year_spouse < year(threshhold_date), 1, 0),
         )
+
         filing_statuses = filing_status.possible_values
         joint = filing_status == filing_statuses.JOINT
         separate = filing_status == filing_statuses.SEPARATE
         return (
             12_000 * eligible_count
-            - (where(eligible_count == eightyfour_count, 0, 1))
-            * (
-                AFAGI
-                - where(joint | separate, 75_000, 50_000)
-            )
+            - (where(eligible_count == full_deduction_count, 0, 1))
+            * (AFAGI - where(joint | separate, 75_000, 50_000))
         ) / where(joint, 1, eligible_count)
