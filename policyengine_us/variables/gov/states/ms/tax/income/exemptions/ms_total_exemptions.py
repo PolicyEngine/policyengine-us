3e0f132b299d59219c3841b932a2769dd25cacd6
ziqiancui <zc2633@columbia.edu>
2023-05-08 11:17:58 -0700

    MS total exemptions Fixes #2230

---
+++ policyengine_us/variables/gov/states/ms/tax/income/exemptions/ms_total_exemptions.py
@@ -0,0 +1,15 @@
+from policyengine_us.model_api import *
+
+
+class ms_total_exemptions(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Mississippi total exemption allowance"
+    unit = USD
+    definition_period = YEAR
+    defined_for = StateCode.MS
+
+    adds = [
+        "ms_regular_exemption",
+        "ms_dependents_exemption"
+    ]
