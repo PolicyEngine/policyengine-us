f2c34f6d5cf7cf81f495d9a3eea8c5ddea7b369f
LeanneLi <112998519+FlameO3O@users.noreply.github.com>
2023-05-09 02:33:12 +0000

    md_is_child

---
+++ policyengine_us/variables/gov/states/md/tanf/md_tanf_is_child.py
@@ -0,0 +1,26 @@
+from policyengine_us.model_api import *
+
+
+
+
+class md_tanf_is_child(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = YEAR
+    label = "Wether is a child for MD TANF based on age, education, etc."
+    documentation = "0307 Age rev 11.22.doc"
+
+
+    def formula(person, period, parameters):
+        # younger than age 18
+        child_0_17 = person("is_child", period)
+        # Younger than age 19 and A full time k12 student
+        younger_than_19 = person("age", period) <= 19
+        k12 = person("is_k12_student", period)
+        k12_younger_than_19 = k12 & younger_than_19
+        # age 19 and a full time student
+        age_19 = person("age", period) == 19
+        full_time_student = person("is_full_time_college_student", period)
+        school_enrolled_19_year_old = full_time_student & younger_than_19
+        # return
+        return child_0_17 | school_enrolled_19_year_old | k12_younger_than_19
