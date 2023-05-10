5ea17201e1c3b07b85da20a9e39e367d74db4c03
leogoldman <leogoldman1@gmail.com>
2023-05-10 15:54:33 -0400

    Fix NJ Income Tax Before Refundable Credits Fixes #2232

--- policyengine_us/variables/gov/states/nj/tax/income/nj_income_tax.py
+++ policyengine_us/variables/gov/states/nj/tax/income/nj_income_tax.py
@@ -8,5 +8,31 @@ class nj_income_tax(Variable):
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.NJ
-    adds = ["ny_income_tax_before_refundable_credits"]
-    subtracts = ["ny_refundable_credits"]
+    adds = ["nj_income_tax_before_refundable_credits"]
+    subtracts = ["nj_refundable_credits"]
+
+    def formula(tax_unit, period, parameters):
+        # Get the NJ tax portion of the parameter tree.
+        p = parameters(period).gov.states.nj.tax.income
+
+        # Get NJ AGI.
+        agi = tax_unit("nj_agi", period)
+
+        # Get filing status.
+        filing_status = tax_unit("filing_status", period)
+
+        # Get the refundable credits and the income tax before refundable credits.
+        income_tax = tax_unit(
+            "nj_income_tax_before_refundable_credits", period
+        )
+        refundable_credits = tax_unit("nj_refundable_credits", period)
+
+        # If taxable income is below the filing threshold, the tax should not be positive.
+        # Tax could still be negative if filer is due refundable credits.
+        income_tax = where(
+            agi < p.filing_threshold[filing_status],
+            min_(0, income_tax - refundable_credits),
+            income_tax - refundable_credits,
+        )
+
+        return income_tax
