61abf9afa78bc49662d1c0780396043be06ae566
Pavel Makarchuk <pavel.ma99@gmail.com>
2023-05-13 17:45:29 -0400

    complete structure

---
+++ policyengine_us/variables/gov/states/pa/dhs/tanf/eligibility/pa_tanf_maximum_asset_value.py
@@ -0,0 +1,15 @@
+from policyengine_us.model_api import *
+
+
+class pa_tanf_maximum_assets(Variable):
+    value_type = bool
+    entity = SPMUnit
+    label = "PA TANF maximum assets"
+    unit = USD
+    definition_period = YEAR
+    defined_for = StateCode.PA
+
+    def formula(spm_unit, period, parameters):
+        maximum_assets = parameters(period).gov.states.pa.dhs.tanf.maximum_asset_value
+        total_assets = SPMUnit("spm_unit_assets", period)
+        return total_assets < maximum_assets
