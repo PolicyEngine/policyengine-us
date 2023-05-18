77f407657ae5c661fcf3e069d7b954f6c1e20acf
Qiukai Chen <chiukai.c1997@gmail.com>
2023-05-17 22:43:43 -0400

    add grant standard and up grant standard

---
+++ policyengine_us/variables/gov/states/va/dss/tanf/va_tanf_up_grant_standard.py
@@ -0,0 +1,27 @@
+from policyengine_us.model_api import *
+
+
+class va_tanf_up_grant_standard(Variable):
+    value_type = float
+    entity = SPMUnit
+    label = "DC TANF grant standard"
+    unit = USD
+    definition_period = YEAR
+    defined_for = StateCode.DC
+
+    def formula(spm_unit, period, parameters):
+        unit_size = spm_unit("spm_unit_size", period)
+        ceiling = min_(unit_size, 10)
+        additional = unit_size - ceiling
+
+        county = spm_unit.household("county_str", period)
+        p = parameters(period).gov.states.va.dss.tanf
+        if_group3 = county in p.localities.group3
+        p = where(
+            if_group3,
+            p.up_grant_standard.group3,
+            p.up_grant_standard.group2,
+        )
+
+        monthly = p.main[ceiling] + additional * p.addition
+        return monthly * MONTHS_IN_YEAR
