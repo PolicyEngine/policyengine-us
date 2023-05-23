094607523e32283e7186d2af5563750309041379
Pavel Makarchuk <pavel.ma99@gmail.com>
2023-05-23 12:14:39 -0400

    changelog and format
    Co-authored-by: wutongg <wutongg@users.noreply.github.com>
    Co-authored-by: qingqiuzhang <qingqiuzhang@users.noreply.github.com>

--- policyengine_us/variables/gov/states/nh/tax/income/nh_income_tax_before_refundable_credits.py
+++ policyengine_us/variables/gov/states/nh/tax/income/nh_income_tax_before_refundable_credits.py
@@ -8,7 +8,7 @@ class nh_income_tax_before_refundable_credits(Variable):
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.NH
-    
+
     def formula(tax_unit, period, parameters):
         income = tax_unit("nh_taxable_income", period)
         rate = parameters(period).gov.states.nh.tax.income.main
