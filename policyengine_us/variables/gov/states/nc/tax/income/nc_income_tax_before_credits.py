585c948c4bd566a46b81c5dc8b4341d97b64c292
Pavel Makarchuk <pavel.ma99@gmail.com>
2023-05-25 01:14:06 -0400

    non-refundable credits

--- policyengine_us/variables/gov/states/nc/tax/income/nc_income_tax_before_credits.py
+++ policyengine_us/variables/gov/states/nc/tax/income/nc_income_tax_before_credits.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class nc_income_tax_before_credits(Variable):
     value_type = float
     entity = TaxUnit
-    label = "North Carolina income tax before refundable credits"
+    label = "North Carolina income tax before credits"
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.NC
--- policyengine_us/variables/gov/states/nc/tax/income/nc_non_refundable_credits.py
+++ policyengine_us/variables/gov/states/nc/tax/income/nc_non_refundable_credits.py
@@ -1,12 +1,10 @@
 from policyengine_us.model_api import *
 
 
-class nc_income_tax(Variable):
+class nc_non_refundable_credits(Variable):
     value_type = float
     entity = TaxUnit
-    label = "North Carolina income tax"
+    label = "North Carolina non-refundable credits"
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.NC
-    adds = ["nc_income_tax_before_credits"]
-    subtracts = ["nc_non_refundable_credits"]
