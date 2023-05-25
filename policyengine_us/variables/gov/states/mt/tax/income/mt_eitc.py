66504eab185ac34a423c3a71a5226419c3a46c4b
Yinan0409 <98431521+Yinan0409@users.noreply.github.com>
2023-05-25 18:17:51 +0000

    created parameters, variables & test

--- policyengine_us/variables/gov/states/mt/tax/income/mt_eitc.py
+++ policyengine_us/variables/gov/states/mt/tax/income/mt_eitc.py
@@ -4,20 +4,12 @@ from policyengine_us.model_api import *
 class mt_eitc(Variable):
     value_type = float
     entity = TaxUnit
-    label = "CalEITC"
+    label = "MT EITC"
     unit = USD
     definition_period = YEAR
-    defined_for = StateCode.MT
+    reference = "https://leg.mt.gov/bills/mca/title_0150/chapter_0300/part_0230/section_0180/0150-0300-0230-0180.html"
 
     def formula(tax_unit, period, parameters):
-        p = parameters(period).gov.states.mt.tax.income.credits.earned_income
-
-        eligible = tax_unit("mt_eitc_eligible", period)
-
-        #How to take output of mt_child_count into rate file?
-
-        child_count = tax_unit("mt_child_count", period)
-
-        max_credits = tax_unit("rate", period)
-
-        return eligible * max_credits
+        eitc = tax_unit("earned_income_tax_credit", period)
+        rate = parameters(period).gov.states.mt.tax.income.credits.eitc.rate
+        return eitc * rate
