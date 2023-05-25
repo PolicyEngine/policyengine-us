e2d2ec0cc7faba8ceb68b682fc685093c77e0897
Yinan0409 <98431521+Yinan0409@users.noreply.github.com>
2023-05-25 18:31:58 +0000

    changelog and format

--- policyengine_us/variables/gov/states/mt/tax/income/mt_eitc.py
+++ policyengine_us/variables/gov/states/mt/tax/income/mt_eitc.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class mt_eitc(Variable):
     value_type = float
     entity = TaxUnit
-    label = "MT EITC"
+    label = "Montana EITC"
     unit = USD
     definition_period = YEAR
     reference = "https://leg.mt.gov/bills/mca/title_0150/chapter_0300/part_0230/section_0180/0150-0300-0230-0180.html"
