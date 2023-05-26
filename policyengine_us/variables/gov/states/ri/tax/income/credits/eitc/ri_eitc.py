e906739667455d983f939859d0cf0fc7ecd3acfc
Shuyu Zhan (Stella) <94806484+shuyu7zhan@users.noreply.github.com>
2023-05-26 19:48:26 +0000

    variable and test
    Co-authored-by: PavelMakarchuk <PavelMakarchuk@users.noreply.github.com>
    Co-authored-by: Yihan Wang <SchizoidMann@users.noreply.github.com>

--- policyengine_us/variables/gov/states/ri/tax/income/credits/eitc/ri_eitc.py
+++ policyengine_us/variables/gov/states/ri/tax/income/credits/eitc/ri_eitc.py
@@ -1,1 +1,15 @@
-policyengine_us/tests/policy/baseline/gov/states/ri/tax/income/ri_income_tax_before_refundable_credits.yaml
+from policyengine_us.model_api import *
+
+
+class ri_eitc(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Rhode Island earned income tax credit"
+    defined_for = StateCode.RI
+    unit = USD
+    definition_period = YEAR
+
+    def formula(tax_unit, period, parameters):
+        fed_eitc = tax_unit("earned_income_tax_credit", period)
+        rate = parameters(period).gov.states.ri.tax.income.credits.eitc.match
+        return fed_eitc * rate
