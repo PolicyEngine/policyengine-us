a6eb9c7330eefc641c66be31510fc6a62e72e642
PavelMakarchuk <pavel.ma99@gmail.com>
2023-05-26 01:38:25 -0400

    consolidation of rates

--- policyengine_us/variables/gov/states/mt/tax/income/mt_income_tax_before_refundable_credits.py
+++ policyengine_us/variables/gov/states/mt/tax/income/mt_income_tax_before_refundable_credits.py
@@ -12,12 +12,9 @@ class mt_income_tax_before_refundable_credits(Variable):
     def formula(tax_unit, period, parameters):
         income = tax_unit("mt_taxable_income", period)
         p = parameters(period).gov.states.mt.tax.income.main
-        # Before 2024 Montana has a flat tax rate for each filing status
-        temporary_rate = p.temporary_rate.calc(income)
-        # After 2024 the tax rate depends on the filing status
         filing_status = tax_unit("filing_status", period)
         status = filing_status.possible_values
-        progressive_rate = select(
+        return select(
             [
                 filing_status == status.SINGLE,
                 filing_status == status.JOINT,
@@ -25,11 +22,9 @@ class mt_income_tax_before_refundable_credits(Variable):
                 filing_status == status.SEPARATE,
             ],
             [
-                p.progressive.single.calc(income),
-                p.progressive.joint.calc(income),
-                p.progressive.head_of_household.calc(income),
-                p.progressive.separate.calc(income),
+                p.single.calc(income),
+                p.joint.calc(income),
+                p.head_of_household.calc(income),
+                p.separate.calc(income),
             ],
         )
-        rate_change = period.start.year >= 2024
-        return where(rate_change, progressive_rate, temporary_rate)
