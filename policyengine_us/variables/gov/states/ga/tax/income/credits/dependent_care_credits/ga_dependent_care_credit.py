24bf24d01a35c8618ff672f07f7deaa7b1226565
“Eleanor-Zhu” <qzhu6976@usc.edu>
2023-05-29 16:05:55 +0000

    minor adjustments Co-authored-by: Pavel Makarchuk <PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/states/ga/tax/income/credits/dependent_care_credits/ga_dependent_care_credit.py
+++ policyengine_us/variables/gov/states/ga/tax/income/credits/dependent_care_credits/ga_dependent_care_credit.py
@@ -1,6 +1,7 @@
 from policyengine_us.model_api import *
 
-class ga_dependent_care_credit(Variable):
+
+class ga_cdcc(Variable):
     value_type = float
     entity = TaxUnit
     label = "Georgia dependent care credit"
@@ -9,6 +10,8 @@ class ga_dependent_care_credit(Variable):
     defined_for = StateCode.GA
 
     def formula(tax_unit, period, parameters):
-        expenses = spm_unit("childcare_expenses", period)
-        rate = parameters(period).gov.states.ga.tax.income.main.qualified_child.rate
+        expenses = tax_unit("cdcc", period)
+        rate = parameters(
+            period
+        ).gov.states.ga.tax.income.credits.qualified_child.rate
         return expenses * rate
