8336749185ec9f5063b4fb331ea419cbc266a94f
jnaux <89711387+jnaux@users.noreply.github.com>
2023-05-29 14:14:40 +0000

    Integrated files, edited based on edit suggestions

--- policyengine_us/variables/gov/states/va/tax/income/subtractions/va_age_deduction.py
+++ policyengine_us/variables/gov/states/va/tax/income/subtractions/va_age_deduction.py
@@ -15,21 +15,19 @@ class va_age_deduction(Variable):
         p = parameters(
             period
         ).gov.states.va.tax.income.subtractions.age_deduction
+
         filing_status = tax_unit("filing_status", period)
-        # input the age of the head of household (and the spouse if applicable)
         filing_statuses = filing_status.possible_values
-        single = filing_status == filing_statuses.SINGLE
         joint = filing_status == filing_statuses.JOINT
         separate = filing_status == filing_statuses.SEPARATE
 
+        # input the age of the head of household (and the spouse if applicable)
         age_head = tax_unit("age_head", period)
         age_spouse = where(single, 0, tax_unit("age_spouse", period))
 
-        afagi = tax_unit("va_afagi", period)
-        # People who were born on or before the threshold date are eligible for a full deduction
-        threshhold_year = p.va_age_date
+        AFAGI = tax_unit("va_afagi", period)
 
-        # calcualte the number of people eligble for age deduction in a household (people who are 65 and older)
+        # calculate the number of people eligble for age deduction in a household (people who are 65 and older)
         eligible_count = where(age_head >= p.va_age, 1, 0) + where(
             age_spouse >= p.va_age, 1, 0
         )
@@ -38,24 +36,26 @@ class va_age_deduction(Variable):
         birth_year_head = period.start.year - age_head
         birth_year_spouse = period.start.year - age_spouse
         full_deduction_count = sum(
-            where(birth_year_head < threshhold_year, 1, 0),
-            where(birth_year_spouse < threshhold_year, 1, 0),
+            where(birth_year_head < p.birth_year_limit_for_full_amount, 1, 0),
+            where(birth_year_spouse < p.birth_year_limit_for_full_amount, 1, 0),
         )
 
-        maximum_allowable_deduction_amount_adjusted_by_filing_status = (
-            p.maximum_allowable_amount * eligible_count
+        maximum_allowable_deduction = p.amount * eligible_count
+        exceeded_amount = AFAGI - where(
+            joint | separate,
+            p.threshold.max[filing_status],
+            p.threshold.min[filing_status],
         )
-        exceeded_amount = afagi - where(
-            joint | separate, p.married_limit, p.single_limit
+        # If the people eligible for an age deduction do not qualify for a full deduction, the age deduction will be adjusted by a reduction.
+        reduction = exceeded_amount * where(
+            eligible_count == full_deduction_count, 0, 1
         )
-        married_filing_status = where(
-            joint, 1, eligible_count
-        )  # The deduction amount for married taxpayers is different when filing jointly vs. separately.
+
+        married_filing_status = where(joint, 1, eligible_count)
+
+        # Special case: for all married taxpayers, the age deduction will differ when filing separately vs. filing jointly.
         age_deduction = (
-            maximum_allowable_deduction_amount_adjusted_by_filing_status
-            - where(
-                eligible_count == full_deduction_count, 0, 1
-            )  # If taxpayers who are eligible for age deduction also meet the full deduction criterion, then they will receive a full deduction without any adjustment.
-            * exceeded_amount
+            maximum_allowable_deduction - reduction
         ) / married_filing_status
+
         return where(math.isnan(age_deduction), 0, age_deduction)
