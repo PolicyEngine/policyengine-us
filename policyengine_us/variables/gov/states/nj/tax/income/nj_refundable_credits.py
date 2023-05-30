4e8264cd9f1720f39dca22e9757316af7e137570
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-05-30 19:03:46 -0400

    Fix bugs in nj_refundable_credits.py module

--- policyengine_us/variables/gov/states/nj/tax/income/nj_refundable_credits.py
+++ policyengine_us/variables/gov/states/nj/tax/income/nj_refundable_credits.py
@@ -7,7 +7,7 @@ class nj_refundable_credits(Variable):
     label = "New Jersey refundable credits"
     unit = USD
     definition_period = YEAR
-    defined_for = StateCode.NY
+    defined_for = StateCode.NJ
     adds = [
         "nj_property_tax_credit",
         "nj_eitc",
