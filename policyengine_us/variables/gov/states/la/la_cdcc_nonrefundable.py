dad2f91cd235c3b9743702ec03bea0db6d8dd27f
yy3187 <129118329+yy3187@users.noreply.github.com>
2023-05-30 21:49:35 +0000

    variables and test

---
+++ policyengine_us/variables/gov/states/la/la_cdcc_nonrefundable.py
@@ -0,0 +1,23 @@
+from policyengine_us.model_api import *
+
+
+
+class la_cdcc_nonrefundable(Variable):
+   value_type = float
+   entity = TaxUnit
+   label = "Louisiana nonrefundable cdcc"
+   unit = USD
+   definition_period = YEAR
+   reference = "http://legis.la.gov/Legis/Law.aspx?d=101769"
+   defined_for = StateCode.LA
+
+
+   def formula(tax_unit, period, parameters):
+       p = parameters(period).gov.states.la.tax.income.credits
+       # determine AGI eligibility
+       us_agi = tax_unit("adjusted_gross_income", period)
+       agi_eligible = us_agi > p.cdcc.agi_threshold
+       # determine LA nonrefundable cdcc amount
+       us_cdcc = tax_unit("cdcc", period)
+       la_cdcc = us_cdcc * p.cdcc.non_refundable.fraction
+       return agi_eligible * la_cdcc
---
+++ policyengine_us/variables/gov/states/la/la_cdcc_refundable.py
@@ -0,0 +1,24 @@
+from policyengine_us.model_api import *
+
+
+
+
+class la_cdcc_refundable(Variable):
+   value_type = float
+   entity = TaxUnit
+   label = "Louisiana refundable cdcc"
+   unit = USD
+   definition_period = YEAR
+   reference = "http://legis.la.gov/Legis/Law.aspx?d=101769"
+   defined_for = StateCode.LA
+
+
+   def formula(tax_unit, period, parameters):
+       p = parameters(period).gov.states.la.tax.income.credits
+       # determine AGI eligibility
+       us_agi = tax_unit("adjusted_gross_income", period)
+       agi_eligible = us_agi <= p.cdcc.agi_threshold
+       # determine LA refundable cdcc amount
+       us_cdcc = tax_unit("cdcc", period)
+       la_cdcc = us_cdcc * p.cdcc.refundable.fraction.calc(us_agi)
+       return agi_eligible * la_cdcc
