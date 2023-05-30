7553f97340d3cec43ea61e4def7a8c625597c03b
Jaewoong Yun <56903210+jaewoongy@users.noreply.github.com>
2023-05-30 01:23:05 +0000

    parameters and variables

---
+++ policyengine_us/variables/gov/states/mi/tax/income/credits/homestead_property_tax_credit/mi_homestead_property_tax_credit.py
@@ -0,0 +1,19 @@
+from policyengine_us.model_api import *
+
+# policyengine-core test ./policyengine_us/tests/policy/baseline/gov/states/mi/
+
+class mi_homestead_property_tax_credit(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Michigan Homestead Property Tax Credit"
+    unit = USD
+    definition_period = YEAR
+    defined_for = StateCode.MI
+
+    def formula(tax_unit, period, parameters):
+        #property_tax = household("property_tax", period)
+        p = parameters(period).gov.state.mi.homestead_property_tax_credit
+        household_resources = p.household_resources
+        total_household_resources = add(tax_unit, period, ["household_resources"])
+        credit_percentage = total_household_resources * p.phase_out
+        return p.max_amount * p.phase_out
