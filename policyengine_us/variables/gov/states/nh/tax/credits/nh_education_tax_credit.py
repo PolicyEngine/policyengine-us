9be46066e2b66f1d2cc7d9a7f78efd55a20c6abd
qingqiuzhang <90161322+qingqiuzhang@users.noreply.github.com>
2023-05-31 00:29:18 +0000

    fixes format

--- policyengine_us/variables/gov/states/nh/tax/credits/nh_education_tax_credit.py
+++ policyengine_us/variables/gov/states/nh/tax/credits/nh_education_tax_credit.py
@@ -8,18 +8,18 @@ class nh_education_tax_credit(Variable):
     documentation = "New Hampshire Education Tax Credit"
     unit = USD
     definition_period = YEAR
-    reference = "https://www.gencourt.state.nh.us/rsa/html/NHTOC/NHTOC-V-77-G.htm"
+    reference = (
+        "https://www.gencourt.state.nh.us/rsa/html/NHTOC/NHTOC-V-77-G.htm"
+    )
     defined_for = StateCode.NH
 
     def formula(tax_unit, period, parameters):
-        p = parameters(
-            period
-        ).gov.states.nh.tax.credits.education_tax_credit
+        p = parameters(period).gov.states.nh.tax.credits.education_tax_credit
 
         # Get Rate for donation
         donation = add(
-            tax_unit, 
-            period, 
+            tax_unit,
+            period,
             ["charitable_cash_donations", "charitable_non_cash_donations"],
         )
         return p.rate.calc(donation)
