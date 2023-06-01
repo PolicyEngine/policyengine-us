cb1689693e66f7fb21637fdf06a26e5285f9e45a
jiajiagong0123 <114700434+jiajiagong0123@users.noreply.github.com>
2023-06-01 00:47:17 +0000

    paramters variable and tests
    Co-authored-by: Pavel Makarchuk <PavelMakarchuk@users.noreply.github.com>
    Co-authored-by: TurnerLuo <TurnerLuo@users.noreply.github.com>

--- policyengine_us/variables/gov/states/nc/tax/income/credits/nc_child_tax_credits.py
+++ policyengine_us/variables/gov/states/nc/tax/income/credits/nc_child_tax_credits.py
@@ -3,11 +3,29 @@ from policyengine_us.model_api import *
 class nc_ctc(Variable):
     value_type = float
     entity = TaxUnit
-    label = "NC CTC"
+    label = "North Carolina CTC"
     definition_period = YEAR
     unit = USD
     documentation = "North Carolina Tax Credit"
     reference = "https://www.ncdor.gov/taxes-forms/individual-income-tax/credit-children"
-    defined_for = NCDOR
+    defined_for = StateCode.NC
 
-    def formula()
+    def formula(tax_unit, period, parameters):
+        children = tax_unit("tax_unit_children", period)
+        income = tax_unit("adjusted_gross_income", period)
+        filing_status = tax_unit("filing_status", period)
+        p = parameters(period).gov.states.nc.tax.income.credits.ctc
+        status = filing_status.possible_values
+        credit_amount = select(
+            [
+                filing_status == status.SINGLE,
+                filing_status == status.HEAD_OF_HOUSEHOLD,
+                filing_status == status.JOINT,
+            ],
+            [
+                p.single.calc(income),
+                p.head_of_household.calc(income),
+                p.married.calc(income),
+            ],
+        )
+        return children * credit_amount
