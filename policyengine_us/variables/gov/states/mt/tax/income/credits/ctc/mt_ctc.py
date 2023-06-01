64090e8797dbe919db4bfcde96b61254e2f548c1
Eric Yoon <65987964+Yoon-Eric@users.noreply.github.com>
2023-06-01 17:20:42 +0000

    Co-authored-by: Pavel Makarchuk <PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/states/mt/tax/income/credits/ctc/mt_ctc.py
+++ policyengine_us/variables/gov/states/mt/tax/income/credits/ctc/mt_ctc.py
@@ -18,19 +18,21 @@ class mt_ctc(Variable):
             gross_income <= p.income_threshold
         )
         investment_income_eligible = (
-            tax_unit("investment_income", period) <= p.investment_threshold
+            tax_unit("net_investment_income", period) <= p.investment_threshold
         )
         person = tax_unit.members
         dependent = person("is_tax_unit_dependent", period)
         meets_age_limit = person("age", period) < p.child_age_eligibility
         eligible_child = dependent & meets_age_limit
-        eligible_children = tax_unit.sum(eligible_child)
+
+        eligible_children = tax_unit.any(eligible_child)
+        
         eligible = income_eligible * investment_income_eligible
+       
         child_amount = eligible_children * p.amount
         credit = eligible * child_amount
         # reduction
         reduction_rate = p.reduction.rate * (max_((gross_income - p.reduction.treshold),0) // p.reduction.increment)
         reduced_credit = max_((credit - reduction_rate),0)
-        return where((gross_income > p.reduction.treshold), reduced_credit , credit)
 
-#where(condition, if_true, if_false)
+        return where((gross_income > p.reduction.treshold), reduced_credit , credit)
