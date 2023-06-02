91b6fe03aa6cfe3131251f8ac24970fa86775fd6
keepjojoyoung <106553218+keepjojoyoung@users.noreply.github.com>
2023-06-02 17:30:59 +0000

    edited variables and parameters

--- policyengine_us/variables/gov/states/mi/tax/income/home_heating/rent_include_heating_cost.py
+++ policyengine_us/variables/gov/states/mi/tax/income/home_heating/rent_include_heating_cost.py
@@ -1,10 +1,9 @@
 from policyengine_us.model_api import *
-class rent_include_heating _cost(Variable):
+
+class rent_include_heating_cost(Variable):
     value_type = bool
     entity = TaxUnit
     label = "Rent include heating cost"
     definition_period = YEAR
 
-    def formula(tax_unit, period, parameters):
-        return add(tax_unit, period, ["rent_include_heating _cost"]) > 0
 
