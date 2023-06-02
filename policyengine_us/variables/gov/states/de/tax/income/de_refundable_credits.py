f64925b3b510ef8afd311f283aec5f3eee4226a5
Max Ghenis <mghenis@gmail.com>
2023-06-02 17:52:11 -0400

    Update policyengine_us/variables/gov/states/de/tax/income/de_refundable_credits.py

--- policyengine_us/variables/gov/states/de/tax/income/de_refundable_credits.py
+++ policyengine_us/variables/gov/states/de/tax/income/de_refundable_credits.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class de_refundable_credits(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Delaware refundbale credits"
+    label = "Delaware refundable credits"
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.DE
