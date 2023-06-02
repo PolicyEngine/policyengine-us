90a4f45e01a8aab456c52d51c36056dd79aee0c2
Jaewoong Yun <56903210+jaewoongy@users.noreply.github.com>
2023-06-02 21:18:18 +0000

    changed household resources and added a few tests

--- policyengine_us/variables/gov/states/mi/tax/income/credits/homestead_property_tax_credit/mi_homestead_property_tax_credit.py
+++ policyengine_us/variables/gov/states/mi/tax/income/credits/homestead_property_tax_credit/mi_homestead_property_tax_credit.py
@@ -11,9 +11,16 @@ class mi_homestead_property_tax_credit(Variable):
     defined_for = StateCode.MI
 
     def formula(tax_unit, period, parameters):
-        #property_tax = household("property_tax", period)
         p = parameters(period).gov.state.mi.homestead_property_tax_credit
         household_resources = p.household_resources
         total_household_resources = add(tax_unit, period, ["household_resources"])
-        credit_percentage = total_household_resources * p.phase_out
-        return p.max_amount * p.phase_out
+        percentage = p.phase_out.threshold[total_household_resources]
+        return p.max_amount * percentage
+
+    #def formula(tax_unit, period, parameters):
+        #property_tax = household("property_tax", period)
+    #    p = parameters(period).gov.state.mi.homestead_property_tax_credit
+    #    household_resources = p.household_resources
+    #    total_household_resources = add(tax_unit, period, ["household_resources"])
+    #    credit_percentage = total_household_resources * p.phase_out
+    #    return min(p.max_amount * credit_percentage, p.max_amount)
