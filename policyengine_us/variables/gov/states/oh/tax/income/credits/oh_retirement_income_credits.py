b37c697b40b05eb0ef09c8f98d970f86e188b7eb
Linfeng-ll <95734090+Linfeng-ll@users.noreply.github.com>
2023-06-02 16:49:22 +0000

    make format

--- policyengine_us/variables/gov/states/oh/tax/income/credits/oh_retirement_income_credits.py
+++ policyengine_us/variables/gov/states/oh/tax/income/credits/oh_retirement_income_credits.py
@@ -4,14 +4,12 @@ from policyengine_us.model_api import *
 class oh_retirement_income_credits(Variable):
     value_type = float
     entity = TaxUnit
-    label = (
-         "Ohio Retirement Income Credit"
-     )
+    label = "Ohio Retirement Income Credit"
     unit = USD
     definition_period = YEAR
     reference = (
         "https://tax.ohio.gov/static/forms/ohio_individual/individual/2021/pit-it1040-booklet.pdf#page=20",
-        "https://tax.ohio.gov/static/forms/ohio_individual/individual/2021/pit-it1040-booklet.pdf#page=29"
+        "https://tax.ohio.gov/static/forms/ohio_individual/individual/2021/pit-it1040-booklet.pdf#page=29",
     )
     defined_for = StateCode.OH
 
@@ -24,7 +22,13 @@ class oh_retirement_income_credits(Variable):
         pension_income = sum(person("pension_income", period))
 
         agi = tax_unit("oh_agi", period)
-        has_not_taken_lump_sum_distribution = person("oh_has_not_taken_oh_lump_sum_credits", period)
+        has_not_taken_lump_sum_distribution = person(
+            "oh_has_not_taken_oh_lump_sum_credits", period
+        )
 
         eligible = agi < p.agi_cap and pension_income > 0
-        return p.agi_credit_amount.calc(agi) * eligible * has_not_taken_lump_sum_distribution
+        return (
+            p.agi_credit_amount.calc(agi)
+            * eligible
+            * has_not_taken_lump_sum_distribution
+        )
