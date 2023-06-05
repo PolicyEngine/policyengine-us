f61de9910dfa65b7202e0836ebc77f95b50eaad5
“Eleanor-Zhu” <qzhu6976@usc.edu>
2023-06-05 23:40:08 +0000

    resolve the recomendation changes

--- policyengine_us/variables/gov/states/ga/tax/income/credits/cdcc/ga_cdcc.py
+++ policyengine_us/variables/gov/states/ga/tax/income/credits/cdcc/ga_cdcc.py
@@ -4,14 +4,14 @@ from policyengine_us.model_api import *
 class ga_cdcc(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Georgia dependent care credit"
+    label = "Georgia non-refundable dependent care credit"
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.GA
 
     def formula(tax_unit, period, parameters):
-        expenses = tax_unit("cdcc", period)
+        federal_cdcc = tax_unit("cdcc", period)
         rate = parameters(
             period
         ).gov.states.ga.tax.income.credits.dependent_care_credit.rate
-        return expenses * rate
+        return federal_cdcc * rate
