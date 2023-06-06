1b4e9afbe50a0068d65e7c67495ae6f2721d9e2e
Matty <31402450+Mshuning@users.noreply.github.com>
2023-06-06 23:08:44 +0000

    improve

--- policyengine_us/variables/gov/states/mi/tax/income/deductions/mi_standard_deduction.py
+++ policyengine_us/variables/gov/states/mi/tax/income/deductions/mi_standard_deduction.py
@@ -16,24 +16,16 @@ class mi_standard_deduction(Variable):
     defined_for = StateCode.MI
 
     def formula(tax_unit, period, parameters):
-        p = parameters(period).gov.states["mi"].tax.income.deductions.standard
+        p = parameters(period).gov.states.mi.tax.income.deductions.standard
         # Core deduction based on filing status.
         filing_status = tax_unit("filing_status", period)
 
-        # Michigan Standard Deduction
-        age_min_threshold = p.min_age
-        age_max_threshold = p.max_age
         age_older = max_(
             tax_unit("age_head", period), tax_unit("age_spouse", period)
         )
-        # aged_head = (
-        #     tax_unit("age_head", period) >= age_min_threshold
-        #     and tax_unit("age_head", period) <= age_max_threshold
-        # ).astype(int)
-        # aged_spouse = (
-        #     tax_unit("age_spouse", period) >= age_min_threshold
-        #     and tax_unit("age_spouse", period) <= age_max_threshold
-        # ).astype(int)
+        # Michigan Standard Deduction
+        age_min_threshold = p.min_age
+        age_max_threshold = p.max_age
         aged_count = (
             age_older >= age_min_threshold and age_older <= age_max_threshold
         ).astype(int)
@@ -41,17 +33,9 @@ class mi_standard_deduction(Variable):
         standrad_deduction = aged_count * amount_per_aged
 
         # Retirement Benefits Deduction Tier 1
-        tier1_age_threshold = p.retirement_benefits_deduction.tier1_age
-        rb1_age_older = max_(
-            tax_unit("age_head", period), tax_unit("age_spouse", period)
-        )
-        # rb1_aged_head = (
-        #     tax_unit("age_head", period) >= tier1_age_threshold
-        # ).astype(int)
-        # rb1_aged_spouse = (
-        #     tax_unit("age_spouse", period) >= tier1_age_threshold
-        # ).astype(int)
-        rb1_aged_count = (rb1_age_older >= tier1_age_threshold).astype(int)
+        tier1_age_threshold = p.retirement_benefits_deduction.tier1_birth_year
+        rb1_birth_year = -(age_older - period.start.year)
+        rb1_aged_count = (rb1_birth_year < tier1_age_threshold).astype(int)
         rb1_amount_per_aged = p.retirement_benefits_deduction.tier1_amount[
             filing_status
         ]
@@ -60,20 +44,9 @@ class mi_standard_deduction(Variable):
         # Retirement Benefits Deduction Tier 3
         tier3_age_min_threshold = p.retirement_benefits_deduction.tier3_min_age
         tier3_age_max_threshold = p.retirement_benefits_deduction.tier3_max_age
-        rb3_age_older = max_(
-            tax_unit("age_head", period), tax_unit("age_spouse", period)
-        )
-        # rb3_aged_head = (
-        #     tax_unit("age_head", period) >= tier3_age_min_threshold
-        #     and tax_unit("age_head", period) <= tier3_age_max_threshold
-        # ).astype(int)
-        # rb3_aged_spouse = (
-        #     tax_unit("age_spouse", period) >= tier3_age_min_threshold
-        #     and tax_unit("age_spouse", period) <= tier3_age_max_threshold
-        # ).astype(int)
         rb3_aged_count = (
-            rb3_age_older >= tier3_age_min_threshold
-            and rb3_age_older <= tier3_age_max_threshold
+            age_older >= tier3_age_min_threshold
+            and age_older <= tier3_age_max_threshold
         ).astype(int)
         rb3_amount_per_aged = p.retirement_benefits_deduction.tier3_amount[
             filing_status
@@ -84,16 +57,7 @@ class mi_standard_deduction(Variable):
         senior_age_threshold = (
             p.interest_dividends_capital_gains_deduction.senior_age
         )
-        idcg_age_older = max_(
-            tax_unit("age_head", period), tax_unit("age_spouse", period)
-        )
-        # idcg_aged_head = (
-        #     tax_unit("age_head", period) >= senior_age_threshold
-        # ).astype(int)
-        # idcg_aged_spouse = (
-        #     tax_unit("age_spouse", period) >= senior_age_threshold
-        # ).astype(int)
-        idcg_aged_count = (idcg_age_older >= senior_age_threshold).astype(int)
+        idcg_aged_count = (age_older >= senior_age_threshold).astype(int)
         idcg_amount_per_aged = (
             p.interest_dividends_capital_gains_deduction.senior_amount[
                 filing_status
