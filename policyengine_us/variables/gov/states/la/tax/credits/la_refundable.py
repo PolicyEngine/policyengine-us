f36387fd75eab8bdfd0b5e157e83902e1e72a1dc
XZ-mabel <131919825+XZ-mabel@users.noreply.github.com>
2023-06-06 21:37:52 +0000

    Louisiana Child Care Expense Credits Variables files created. Changed reference. Changed year to 2018 in threshold.yaml Fixes #2371

---
+++ policyengine_us/variables/gov/states/la/tax/credits/la_refundable.py
@@ -0,0 +1,24 @@
+from policyengine_us.model_api import *
+
+
+
+
+class la_childexpensetaxcredits_nonrefundable(Variable):
+   value_type = float
+   entity = TaxUnit
+   label = "Louisiana Child Expense Tax credits"
+   unit = star
+   definition_period = YEAR
+   reference = https://www.revenue.louisiana.gov/IndividualIncomeTax/SchoolReadinessTaxCredit
+   defined_for = StateCode.LA
+
+
+   def formula(tax_unit, period, parameters):
+       p = parameters(period).gov.states.la.tax.credits
+       # determine if it is nonrefundable
+       us_agi = tax_unit("adjusted_gross_income", period)
+       agi_eligible = us_agi <=p.threshold
+       # determine LA nonrefundable amount
+       us_qualityrating = tax_unit("quality rating", period)
+       la_childexpense = us_qualityrating * p.nonrefundable.nonrefundable
+       return la_childexpense
