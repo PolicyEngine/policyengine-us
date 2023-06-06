9d1bcec86708c1d0202cf2da7b06c241de18a772
yedeer12 <104268340+yedeer12@users.noreply.github.com>
2023-06-06 17:43:49 +0000

    Fix by the comments

--- policyengine_us/variables/gov/states/wv/tax/income/wv_taxable_income.py
+++ policyengine_us/variables/gov/states/wv/tax/income/wv_taxable_income.py
@@ -7,5 +7,5 @@ class wv_taxable_income(Variable):
     label = "West Virginia taxable income"
     unit = USD
     definition_period = YEAR
-    reference = "https://code.wvlegislature.gov/11-21/"
+    reference = "https://code.wvlegislature.gov/11-21-4E/"
     defined_for = StateCode.WV
