d302dbae9ffe26c6221b93212732a4b19c5dbfe8
PavelMakarchuk <pavel.ma99@gmail.com>
2023-06-08 11:04:58 -0400

    amount variable

---
+++ policyengine_us/variables/gov/states/az/tax/income/credits/az_dependent_credit_amount.py
@@ -0,0 +1,19 @@
+from policyengine_us.model_api import *
+
+
+class az_dependent_credit_amount(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Arizona dependent care credit amount"
+    unit = USD
+    documentation = "https://azdor.gov/file/12346/download?token=7FAdFbnT"
+    definition_period = YEAR
+    defined_for = StateCode.AZ
+
+    def formula(tax_unit, period, parameters):
+        p = parameters(period).gov.states.az.tax.income.credits.dependent_credit
+        dependent = person("is_tax_unit_dependent", period)
+        person = tax_unit.members
+        age = person("age", period)
+        dependent_amount = p.amount.calc(age) * dependent
+        return tax_unit.sum(dependent_amount)
