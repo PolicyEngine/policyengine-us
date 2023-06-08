d354585eb70f9979c0005fb07a17c6a0b6c0b493
WeihaoZ724 <128203510+WeihaoZ724@users.noreply.github.com>
2023-06-08 13:52:12 +0000

    kentucky variables, parameter and changelog

---
+++ policyengine_us/variables/gov/states/ky/tax/income/deductions/ky_standard_deduction.py
@@ -0,0 +1,11 @@
+from policyengine_us.model_api import *
+
+
+class ky_standard_deduction(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Kentucky standard deduction"
+    unit = USD
+    definition_period = YEAR
+    defined_for = StateCode.KY
+    adds = "gov.states.ky.tax.income.deductions.standard_deduction"
