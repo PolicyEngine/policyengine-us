c8924eae182b23c9cbc828952c7734e89fcffab3
wutongg <94206699+wutongg@users.noreply.github.com>
2023-06-08 01:21:59 +0000

    Fix comments

--- policyengine_us/variables/gov/states/nh/tax/income/exemptions/nh_base_exemption.py
+++ policyengine_us/variables/gov/states/nh/tax/income/exemptions/nh_base_exemption.py
@@ -4,19 +4,19 @@ from policyengine_us.model_api import *
 class nh_base_exemption(Variable):
     value_type = float
     entity = TaxUnit
-    label = "New Hampshire base exemption"
+    label = "New Hampshire base exemption household level"
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.NH
 
     def formula(tax_unit, period, parameters):
-        # First get their filing status.
-        filing_status = tax_unit("filing_status", period)
 
-        joint = filing_status == filing_status.possible_values.JOINT
+        # get number of tax unit
+        tax_unit_size = tax_unit("tax_unit_size", period) 
 
-        # get the NH Exemptions part of the parameter tree.
-        p = parameters(period).gov.states.nh.tax.income.exemptions.amount
-
-        # Get their base exemption amounts
-        return where(joint, 2 * p.base, p.base)
+        # How can I use the person level base exemption for this household one?
+        
+        # I think the ADD function you mentioned is used in nh_total_exemptions.py, which adds all different 
+        # typels of exemptions. 
+        
+        return 
