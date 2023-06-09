2bf7ff702bb070a4eebb5435fb2a0de65f099d53
Stella Hu <123130713+xhu493@users.noreply.github.com>
2023-06-09 21:25:19 +0000

    PA TANF Income Fixes #2443
    Co-authored-by: Patricia-1998 <Patricia-1998@users.noreply.github.com>
    Co-authored-by: itsmexw <itsmexw@users.noreply.github.com>

---
+++ policyengine_us/variables/gov/states/pa/tanf/income/earned_income/pa_tanf_earned_income.py
@@ -0,0 +1,12 @@
+from policyengine_us.model_api import *
+
+
+class pa_tanf_earned_income(Variable):
+    value_type = float
+    entity = SPMUnit
+    label = "PA TANF earned income"
+    unit = USD
+    definition_period = YEAR
+    defined_for = StateCode.PA
+
+    # adds = "gov.states.dc.dhs.tanf.income.earned"
