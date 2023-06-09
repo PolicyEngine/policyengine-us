f561f8dc87bc0c355907870ccc78be4afb000c17
PavelMakarchuk <pavel.ma99@gmail.com>
2023-06-09 00:12:50 -0400

    minor format

--- policyengine_us/variables/gov/states/mi/tax/income/deductions/mi_social_security_retirement.py
+++ policyengine_us/variables/gov/states/mi/tax/income/deductions/mi_social_security_retirement.py
@@ -4,6 +4,6 @@ from policyengine_us.model_api import *
 class mi_social_security_retirement(Variable):
     value_type = bool
     entity = TaxUnit
-    label = "receive retirement benefits from employment exempt from Social Security"
+    label = "Retirement benefits from employment exempt from Social Security"
     documentation = "Whether a recipient receive retirement benefits from employment exempt from Social Security"
     definition_period = YEAR
--- policyengine_us/variables/gov/states/mi/tax/income/deductions/mi_standard_deduction.py
+++ policyengine_us/variables/gov/states/mi/tax/income/deductions/mi_standard_deduction.py
@@ -35,16 +35,16 @@ class mi_standard_deduction(Variable):
         # Retirement Benefits Deduction Tier 1
         tier1_age_threshold = p.retirement_benefits_deduction.tier_one.birth_year
         rb1_birth_year = -(age_older - period.start.year)
-        rb1_aged_count = (rb1_birth_year < tier1_age_threshold).astype(int)
+        rb1_age_eligibility = (rb1_birth_year < tier1_age_threshold).astype(int)
         rb1_amount_per_aged = p.retirement_benefits_deduction.tier_one.amount[
             filing_status
         ]
-        rb1_deduction = rb1_aged_count * rb1_amount_per_aged
+        rb1_deduction = rb1_age_eligibility * rb1_amount_per_aged
 
         # Retirement Benefits Deduction Tier 3
         tier3_age_min_threshold = p.retirement_benefits_deduction.tier_three.min_age
         tier3_age_max_threshold = p.retirement_benefits_deduction.tier_three.max_age
-        rb3_aged_count = (
+        rb3_age_eligibility = (
             age_older >= tier3_age_min_threshold
             and age_older <= tier3_age_max_threshold
         ).astype(int)
@@ -52,7 +52,7 @@ class mi_standard_deduction(Variable):
             filing_status
         ]
         rb3_deduction = (
-            rb3_aged_count
+            rb3_age_eligibility
             * rb3_amount_per_aged
             * tax_unit("mi_social_security_retirement", period)
         )
