412369c126ce811688135f79866ec744dddeb47c
Matty <31402450+Mshuning@users.noreply.github.com>
2023-06-11 23:28:15 +0000

    Add Michigan Additions and Subtractions Fixes #2445

--- policyengine_us/variables/gov/states/mi/tax/income/mi_taxable_income.py
+++ policyengine_us/variables/gov/states/mi/tax/income/mi_taxable_income.py
@@ -8,3 +8,13 @@ class mi_taxable_income(Variable):
     defined_for = StateCode.MI
     unit = USD
     definition_period = YEAR
+
+    def formula(tax_unit, period, parameters):
+        agi = tax_unit("adjusted_gross_income", period)
+        std_ded = tax_unit("mi_standard_deduction", period)
+        itm_ded = tax_unit(
+            "itemized_taxable_income_deductions", period
+        )  # equal to federal itmded
+        deductions = where(itm_ded > std_ded, itm_ded, std_ded)
+        exemptions = tax_unit("mi_exemptions", period)
+        return max_(0, agi - deductions - exemptions)
