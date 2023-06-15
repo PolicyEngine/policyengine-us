5b4bf9c67974346755b290b19a9bedc0ea946152
ShiqiLiu <54519061+Kiki-67@users.noreply.github.com>
2023-06-15 17:16:11 +0000

    Delaware Additional Personal Credits Fixes #2456

---
+++ policyengine_us/variables/gov/states/de/tax/income/deductions/de_aged_additional_personal_credits.py
@@ -0,0 +1,20 @@
+from policyengine_us.model_api import *
+
+
+class de_aged_personal_credits_deduction(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Delaware aged additional personal credits deduction"
+    unit = USD
+    definition_period = YEAR
+
+    def formula(tax_unit, period, parameters):
+        p = parameters(period).gov.states.de.tax.income.deductions
+
+        age_head = tax_unit("age_head", period)
+        head_eligible = (age_head >= p.age_eligibility).astype(int)
+
+        age_spouse = tax_unit("age_spouse", period)
+        spouse_eligible = (age_spouse >= p.age_eligibility).astype(int)
+
+        return (head_eligible + spouse_eligible) * p.aged
