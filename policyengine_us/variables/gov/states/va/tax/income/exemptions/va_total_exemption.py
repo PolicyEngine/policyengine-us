dc09fdcf87a5162e28394f2d82ed0756a62accd2
jnaux <89711387+jnaux@users.noreply.github.com>
2023-06-19 20:09:02 +0000

    Deleted parameter file, fixed the variable file

--- policyengine_us/variables/gov/states/va/tax/income/exemptions/va_total_exemption.py
+++ policyengine_us/variables/gov/states/va/tax/income/exemptions/va_total_exemption.py
@@ -11,10 +11,4 @@ class va_total_exemption(Variable):
     reference = (
         "https://law.lis.virginia.gov/vacodefull/title58.1/chapter3/article2/"
     )
-
-    def formula(tax_unit, period, parameters):
-        p = parameters(period).gov.states.va.tax.income.exemptions
-        personal_exemption = p.personal
-        age_blind_exemption = p.age_blind
-        total_exemption = p.personal + p.age_blind
-        return total_exemption
+    adds = ["va_personal_exemption", "va_aged_blind_exemption"]
