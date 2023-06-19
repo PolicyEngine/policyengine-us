87f48acd72064a78c2d06f9a67c5f21cdc96673b
PavelMakarchuk <pavel.ma99@gmail.com>
2023-06-19 15:57:19 -0400

    finish variable

--- policyengine_us/variables/gov/states/in/tax/income/credits/elderly_tax_credit/in_unified_elderly_tax_credit.py
+++ policyengine_us/variables/gov/states/in/tax/income/credits/elderly_tax_credit/in_unified_elderly_tax_credit.py
@@ -1,19 +1,36 @@
 from policyengine_us.model_api import *
 
 
-class in_unified_elderly_tax_income(Variable):
+class in_unified_elderly_tax_credit(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Indiana unified elderly tax credit income"
+    label = "Indiana unified elderly tax credit"
     unit = USD
     definition_period = YEAR
-    documention = "Income that is utilized for the elderly credit calculation"
-    reference = "https://forms.in.gov/Download.aspx?id=15394 "
+    reference = "https://forms.in.gov/Download.aspx?id=15394"
     defined_for = StateCode.IN
 
     def formula(tax_unit, period, parameters):
         person = tax_unit.members
+        p = (
+            parameters(period)
+            .gov.states["in"]
+            .tax.income.credits.unified_tax_credit_for_elderly
+        )
         income = person("in_unified_elderly_tax_income", period)
         total_income = tax_unit.sum(income)
         age_head = tax_unit("age_head", period)
         spouse_age = tax_unit("age_spouse", period)
+        filing_status = tax_unit("filing_status", period)
+        status = filing_status.possible_values
+        married = status.JOINT
+        head_eligible = age_head >= p.age_eligibility
+        spouse_eligible = spouse_age >= p.age_eligibility
+        both_eligible = head_eligible and spouse_eligible
+        married_amount = where(
+            both_eligible,
+            p.amount.married.two_aged.calc(total_income),
+            p.amount.married.one_aged.calc(total_income),
+        )
+        single_amount = p.amount.single.calc(total_income) * head_eligible
+        return where(married, married_amount, single_amount)
