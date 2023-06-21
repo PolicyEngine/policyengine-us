d907b581ae1006f9beea930f2e673a96aae61ccc
Shayla Wu <125515687+ShaylaWoo@users.noreply.github.com>
2023-06-21 03:28:55 +0000

    save all

--- policyengine_us/variables/gov/states/md/tanf/md_tanf.py
+++ policyengine_us/variables/gov/states/md/tanf/md_tanf.py
@@ -7,26 +7,11 @@ class md_tanf(Variable):
     label = "Maryland TANF"
     unit = USD
     definition_period = YEAR
-    defined_for = "md_tanf_eligible"
+    defined_for = StateCode.MD
 
     def formula(spm_unit, period, parameters):
-        grant_standard = spm_unit("md_tanf_maximun_benefit", period)
-        earned_income = spm_unit(
-            "md_tanf_countable_gross_earned_income", period
-        )
-        earned_income_deduction = spm_unit(
-            "md_tanf_countable_gross_earned_income_deduction", period
-        )
-        unearned_income = spm_unit(
-            "md_tanf_countable_gross_earned_income", period
-        )
-        unearned_income_deduction = spm_unit(
-            "md_tanf_countable_gross_unearned_income_deduction", period
-        )
-        income = add(
-            spm_unit,
-            period,
-            earned_income - earned_income_deduction,
-            unearned_income - unearned_income_deduction,
-        )
+        # Maximum TANF amount recieved
+        grant_standard = spm_unit("md_tanf_maximum_benefit", period)
+        # SPM unit income after continuous deduction
+        income = spm_unit("md_tanf_net_countable_income", period)
         return max(grant_standard - income, 0)
