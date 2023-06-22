0567653a74701ab1308de2ffd4ff22a793075b66
JudyZ13 <112495414+JudyZ13@users.noreply.github.com>
2023-06-22 15:03:05 +0000

    Arizona Credits Fixes #2460
    Co-authored-by: Pavel Makarchuk <PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/states/az/tax/income/credits/family_tax_credit/az_family_tax_credit.py
+++ policyengine_us/variables/gov/states/az/tax/income/credits/family_tax_credit/az_family_tax_credit.py
@@ -10,13 +10,14 @@ class az_family_tax_credit(Variable):
     defined_for = StateCode.AZ
 
     def formula(tax_unit, period, parameters):
+        person = tax_unit.members
         p = parameters(period).gov.states.az.tax.income.credits
         income = tax_unit("adjusted_gross_income", period)
         filing_status = tax_unit("filing_status", period) 
         status = filing_status.possible_values
         dependents = tax_unit("tax_unit_dependents", period)
-        head = tax_unit("is_tax_unit_head", period).astype(int)
-        spouse = tax_unit("is_tax_unit_spouse", period).astype(int)
+        head = tax_unit.any(person("is_tax_unit_head", period).astype(int))
+        spouse = tax_unit.any(person("is_tax_unit_spouse", period).astype(int))
         max_income = select(
             [
                 filing_status == status.SINGLE,
@@ -34,7 +35,7 @@ class az_family_tax_credit(Variable):
                 ]
 
         )
-        household_size = head + spouse + dependents
+        household_size = (head + spouse + dependents)
         amount = p.amount * household_size
         eligible = income <= max_income
-        return eligible * min_(amount, p.max_amount)  
+        return eligible * min_(amount, p.max_amount[filing_status])  
