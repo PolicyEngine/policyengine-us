e2465e4ca7223495a0a36b1d7f537058dc170204
PavelMakarchuk <pavel.ma99@gmail.com>
2023-06-23 12:55:53 -0400

    make format

--- policyengine_us/variables/gov/states/nh/tax/income/exemptions/nh_disabled_exemption.py
+++ policyengine_us/variables/gov/states/nh/tax/income/exemptions/nh_disabled_exemption.py
@@ -26,7 +26,4 @@ class nh_disabled_exemption(Variable):
         spouse_eligible = (disabled_spouse & age_spouse).astype(int)
 
         # Calculate total blind exemption.
-        return (
-            (head_eligible + spouse_eligible)
-            * p.amount.disabled_addition
-        )
+        return (head_eligible + spouse_eligible) * p.amount.disabled_addition
