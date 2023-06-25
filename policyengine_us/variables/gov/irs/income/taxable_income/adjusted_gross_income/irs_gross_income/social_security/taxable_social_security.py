cf67c80560d5c71b652a5129d7b4544d47157527
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-06-25 11:05:06 -0400

    Avoid use of where() in taxable_social_security formula

--- policyengine_us/variables/gov/irs/income/taxable_income/adjusted_gross_income/irs_gross_income/social_security/taxable_social_security.py
+++ policyengine_us/variables/gov/irs/income/taxable_income/adjusted_gross_income/irs_gross_income/social_security/taxable_social_security.py
@@ -1,4 +1,5 @@
 from policyengine_us.model_api import *
+import numpy as np
 
 
 class taxable_social_security(Variable):
@@ -11,16 +12,16 @@ class taxable_social_security(Variable):
     def formula(person, period, parameters):
         unit_tss = person.tax_unit("tax_unit_taxable_social_security", period)
         # allocate unit_tss to head and spouse in proportion to social_security
-        unit_socsec = person.tax_unit("tax_unit_social_security", period)
-        unit_has_socsec = unit_socsec > 0
         ind_socsec = person("social_security", period)
+        unit_socsec = person.tax_unit("tax_unit_social_security", period)
+        person_frac = np.zeros_like(unit_socsec)
+        mask = unit_socsec > 0
+        person_frac[mask] = ind_socsec[mask] / unit_socsec[mask]
         is_spouse = person("is_tax_unit_spouse", period)
-        spouse_frac = min_(
-            1, where(is_spouse & unit_has_socsec, ind_socsec / unit_socsec, 0)
-        )
+        spouse_frac = min_(1, where(is_spouse, person_frac, 0))
         unit_spouse_frac = person.tax_unit.sum(spouse_frac)
         is_head = person("is_tax_unit_head", period)
-        head_frac = where(is_head & unit_has_socsec, 1 - unit_spouse_frac, 0)
+        head_frac = where(is_head, 1 - unit_spouse_frac, 0)
         unit_head_frac = person.tax_unit.sum(head_frac)
         return unit_tss * select(
             [is_head, is_spouse], [unit_head_frac, unit_spouse_frac], default=0
