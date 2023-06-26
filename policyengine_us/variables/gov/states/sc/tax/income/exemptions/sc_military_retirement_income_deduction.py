1b259985498bdd3f992af2ba5081d150c5279aa7
hua7450 <113824411+hua7450@users.noreply.github.com>
2023-06-26 12:19:50 -0400

    Update policyengine_us/variables/gov/states/sc/tax/income/exemptions/sc_military_retirement_income_deduction.py
    Co-authored-by: Max Ghenis <mghenis@gmail.com>

--- policyengine_us/variables/gov/states/sc/tax/income/exemptions/sc_military_retirement_income_deduction.py
+++ policyengine_us/variables/gov/states/sc/tax/income/exemptions/sc_military_retirement_income_deduction.py
@@ -1,7 +1,7 @@
 from policyengine_us.model_api import *
 
 
-class sc_military_retirement_income_deduction(Variable):
+class sc_military_retirement_income_deduction_head(Variable):
     value_type = float
     entity = TaxUnit
     label = "South Carolina military retirement income deduction for self"
