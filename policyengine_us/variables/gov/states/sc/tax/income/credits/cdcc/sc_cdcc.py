1efc47db44a3cc75f3ef8c721568bb472fbfd2e4
Pinyan <129036923+pxu12@users.noreply.github.com>
2023-06-26 17:25:54 +0000

    modified formula
    Co-authored-by: hua7450 <hua7450@users.noreply.github.com>

--- policyengine_us/variables/gov/states/sc/tax/income/credits/cdcc/sc_cdcc.py
+++ policyengine_us/variables/gov/states/sc/tax/income/credits/cdcc/sc_cdcc.py
@@ -16,6 +16,7 @@ class sc_cdcc(Variable):
     def formula(tax_unit, period, parameters):
         # Get South Carolina CDCC rate.
         p = parameters(period).gov.states.sc.tax.income.credits.cdcc
+        p2 = parameters(period).gov.irs.credits.cdcc
 
         # Get child care expenses.
         childcare_expenses = tax_unit("tax_unit_childcare_expenses", period)
@@ -26,10 +27,10 @@ class sc_cdcc(Variable):
 
         # Number of qualifying people
         count_cdcc_eligible = min_(
-            tax_unit("count_cdcc_eligible", period), p.dependent_cap
+            tax_unit("count_cdcc_eligible", period), p2.eligibility.max
         )
-
+        # Maximum value cannot exceed cap
         # Calculate total CDCC
         max_match = childcare_expenses * p.rate
-        cap = p.amount * count_cdcc_eligible
+        cap = p2.max *count_cdcc_eligible*p.rate
         return eligible * min_(max_match, cap)
