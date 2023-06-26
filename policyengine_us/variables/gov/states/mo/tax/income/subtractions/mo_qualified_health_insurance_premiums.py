cbd9c4da9cc8f0b65092463915aa3e7678f9ae64
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-06-26 13:53:26 -0400

    Shorten comments in mo_qualified_health_insurance_premiums.py

--- policyengine_us/variables/gov/states/mo/tax/income/subtractions/mo_qualified_health_insurance_premiums.py
+++ policyengine_us/variables/gov/states/mo/tax/income/subtractions/mo_qualified_health_insurance_premiums.py
@@ -8,9 +8,9 @@ class mo_qualified_health_insurance_premiums(Variable):
     unit = USD
     definition_period = YEAR
     reference = (
-        "https://dor.mo.gov/forms/5695.pdf",  # Form MO 5695
-        "https://www.irs.gov/pub/irs-pdf/f1040sa.pdf",  # Federal Form 1040 Schedule A
-        "https://www.irs.gov/pub/irs-pdf/f1040.pdf",  # Federal Form 1040
+        "https://dor.mo.gov/forms/5695.pdf",  # MO Form 5695
+        "https://www.irs.gov/pub/irs-pdf/f1040sa.pdf",  # US Form 1040 Sch A
+        "https://www.irs.gov/pub/irs-pdf/f1040.pdf",  # US Form 1040
     )
     defined_for = StateCode.MO
 
@@ -18,15 +18,19 @@ class mo_qualified_health_insurance_premiums(Variable):
         # Variable involves person- and tax unit-level variables.
         tax_unit = person.tax_unit
 
-        # total_health_insurance_premiums is also a primary input to the MO side of calculation, MO Form 5695, Line 8
+        # total_health_insurance_premiums is also a primary input to
+        # the MO side of calculation, MO Form 5695, Line 8
         # Federal Schedule A, Line 4
         med_expense_deduction = add(
             tax_unit, period, ["medical_expense_deduction"]
         )
 
-        # the ratio of federal medical expense deduction to total medical expenses (out of pocket + premiums)
-        # need division because med_dental_out_of_pocket is in federal tax, but no MO tax
-        # this ratio is then used to scale the health_insurance_premium amount that can be claimed
+        # the ratio of federal medical expense deduction to
+        # total medical expenses (out of pocket + premiums)
+        # need division because med_dental_out_of_pocket is
+        # in federal tax, but no MO tax
+        # this ratio is then used to scale the health_insurance_premium
+        # amount that can be claimed
         # IRS Schedule A Line 1
         tax_unit_health_expenses = add(
             tax_unit,
@@ -34,7 +38,7 @@ class mo_qualified_health_insurance_premiums(Variable):
             ["medical_out_of_pocket_expenses", "health_insurance_premiums"],
         )
         # Apply a where statement to avoid division by zero.
-        med_expense_deducted_ratio = where(
+        med_expense_ratio = where(
             tax_unit_health_expenses > 0,
             med_expense_deduction / tax_unit_health_expenses,
             0,
@@ -43,8 +47,9 @@ class mo_qualified_health_insurance_premiums(Variable):
         person_premiums = person("health_insurance_premiums", period)
         tax_unit_premiums = tax_unit.sum(person_premiums)
 
-        # Line 13 of MO Form 5695, represents the portion of medical expenses already deducted via federal tax itemization
-        deducted_portion = tax_unit_premiums * med_expense_deducted_ratio
+        # Line 13 of MO Form 5695, represents the portion of
+        # medical expenses already deducted via federal tax itemization
+        deducted_portion = tax_unit_premiums * med_expense_ratio
         # subtracts the portion of premiums already deducted from federal tax
         itemized_premiums_amount = tax_unit_premiums - deducted_portion
         itemizes = tax_unit("tax_unit_itemizes", period)
