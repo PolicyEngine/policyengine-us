192d86764d136fd56d6fd20e1fbedbb8a61e58e4
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-06-26 11:57:58 -0400

    Eliminate array divide warning in mo_pension_and_ss_or_ssd_deduction_section_b.py

--- policyengine_us/variables/gov/states/mo/tax/income/deductions/mo_pension_and_ss_or_ssd_deduction_section_b.py
+++ policyengine_us/variables/gov/states/mo/tax/income/deductions/mo_pension_and_ss_or_ssd_deduction_section_b.py
@@ -30,16 +30,13 @@ class mo_pension_and_ss_or_ssd_deduction_section_b(Variable):
                 - p.mo_private_pension_deduction_allowance[filing_status]
             ),
         )
-        ind_pvt_pension_amt = person("taxable_private_pension_income", period)
-        ind_pvt_pension_val = min_(
-            ind_pvt_pension_amt,
-            p.mo_max_private_pension,
-        )
-        unit_pvt_pension_val = tax_unit.sum(ind_pvt_pension_val)  # line8
-        unit_deduction = max_(0, unit_pvt_pension_val - excess_agi)  # line9
-        ind_share_of_unit_deduction = where(
-            ind_pvt_pension_val > 0,
-            ind_pvt_pension_val / unit_pvt_pension_val,
-            0,
-        )
-        return ind_share_of_unit_deduction * unit_deduction
+        ind_pvt_pen_amt = person("taxable_private_pension_income", period)
+        ind_pvt_pen_val = min_(ind_pvt_pen_amt, p.mo_max_private_pension)
+        unit_pvt_pen_val = tax_unit.sum(ind_pvt_pen_val)  # line8
+        unit_deduction = max_(0, unit_pvt_pen_val - excess_agi)  # line9
+
+        ind_share = np.zeros_like(ind_pvt_pen_val)
+        mask = ind_pvt_pen_val > 0
+        ind_share[mask] = ind_pvt_pen_val[mask] / unit_pvt_pen_val[mask]
+
+        return ind_share * unit_deduction
