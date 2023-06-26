e946798022f1cb24b9cba8370e43ecdf5e03fa4d
Kevintowne <89811054+Kevintowne@users.noreply.github.com>
2023-06-26 20:45:43 +0000

    arkansas itemized deduction

--- policyengine_us/variables/gov/states/ar/tax/income/deductions/itemized/ar_itemized_deductions.py
+++ policyengine_us/variables/gov/states/ar/tax/income/deductions/itemized/ar_itemized_deductions.py
@@ -11,15 +11,17 @@ class ar_itemized_deductions(Variable):
     defined_for = StateCode.AR
 
     def formula(tax_unit, period, parameters):
-        p = parameters(period).gov.irs.deductions
-        itm_deds = [
-            deduction
-            for deduction in p.itemized_deductions
-            if deduction not in ["salt_deduction"]
-        ]
-        or_itemized_deductions_less_salt = add(tax_unit, period, itm_deds)
-        property_taxes = add(tax_unit, period, ["real_estate_taxes"])
-        salt = p.itemized.salt_and_real_estate
-        cap = salt.cap[tax_unit("filing_status", period)]
-        capped_property_taxes = min_(property_taxes, cap)
-        return or_itemized_deductions_less_salt + capped_property_taxes
+        p = parameters(period).gov.states.ar.tax.income.deductions.itemized
+
+        # itm_deds = [
+        #     deduction
+        #     for deduction in p.itemized_deductions
+        #     if deduction not in ["salt_deduction"]
+        # ]
+        # or_itemized_deductions_less_salt = add(tax_unit, period, itm_deds)
+        # property_taxes = add(tax_unit, period, ["real_estate_taxes"])
+        # salt = p.itemized.salt_and_real_estate
+        # cap = salt.cap[tax_unit("filing_status", period)]
+        # capped_property_taxes = min_(property_taxes, cap)
+        # return or_itemized_deductions_less_salt + capped_property_taxes
+        return 0
