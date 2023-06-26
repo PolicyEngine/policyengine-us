f4134a96cb13cf92106245790c2c9158274cd034
Yihan Wang <91220978+SchizoidMann@users.noreply.github.com>
2023-06-26 17:59:53 +0000

    Rhode Island Adjusted Gross Income Fixes #2448
    Co-authored-by: Shuyu Zhan (Stella) <shuyu7zhan@users.noreply.github.com>

---
+++ policyengine_us/variables/gov/states/ri/tax/income/adjusted_gross_income/ri_agi_subtractions.py
@@ -0,0 +1,12 @@
+from policyengine_us.model_api import *
+
+
+class ri_agi_subtractions(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "RI AGI subtractions from federal AGI"
+    unit = USD
+    definition_period = YEAR
+    reference = "https://tax.ri.gov/sites/g/files/xkgbur541/files/2022-12/2022%201041%20Schedule%20M_w.pdf"
+    defined_for = StateCode.RI
+    adds = "gov.states.ri.tax.income.agi.subtractions"
