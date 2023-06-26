74ba0067b65de6b58c58a0da9eae3aa2bc722c0a
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-06-26 08:30:33 -0400

    Eliminate array divide warnings in mo_net_state_income_taxes module

--- policyengine_us/variables/gov/states/mo/tax/income/taxable_income/mo_net_state_income_taxes.py
+++ policyengine_us/variables/gov/states/mo/tax/income/taxable_income/mo_net_state_income_taxes.py
@@ -24,22 +24,29 @@ class mo_net_state_income_taxes(Variable):
             "state_and_local_sales_or_income_tax", period
         )
 
-        # Technically, state and local income tax could include the "local_earnings_tax" variable,
-        # but because the definition of net_state_income_taxes is simply (state_tax + local_tax) - local_tax,
+        # Technically, state and local income tax could include the
+        # "local_earnings_tax" variable,
+        # but because the definition of net_state_income_taxes is
+        # simply (state_tax + local_tax) - local_tax,
         # it is redundant to keep it
-        # More info about local income tax from the Federal W2 on page 26 here: https://dor.mo.gov/forms/MO-1040%20Instructions_2021.pdf
+        # More info about local income tax from the Federal W2 on page 26 here:
+        # https://dor.mo.gov/forms/MO-1040%20Instructions_2021.pdf
 
         net_state_income_taxes = tax_unit("state_income_tax", period)
 
-        income_tax_to_total_ratio = (
-            net_state_income_taxes / state_and_local_sales_or_income_tax
+        tax_ratio = np.ones_like(net_state_income_taxes)
+        mask = state_and_local_sales_or_income_tax != 0
+        tax_ratio[mask] = (
+            net_state_income_taxes[mask]
+            / state_and_local_sales_or_income_tax[mask]
         )
 
-        # The threshold is the same as the adjustment_base_amount, i.e. the SALT cap introduced by TCJA
+        # The threshold is the same as the adjustment_base_amount,
+        # i.e. the SALT cap introduced by TCJA
         threshold = adjustment_base_amount
 
         return where(
             state_and_local_sales_or_income_tax > threshold,
-            adjustment_base_amount * (income_tax_to_total_ratio),
+            adjustment_base_amount * tax_ratio,
             net_state_income_taxes,
         )
