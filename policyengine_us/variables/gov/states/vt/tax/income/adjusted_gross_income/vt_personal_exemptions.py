9bcd76a6af2d551d7341a697e06894fd2314f0e5
Zixuan Ma <66671734+mzx-4real@users.noreply.github.com>
2023-06-27 17:58:53 +0800

    Vermont Personal Exemption Fixes #2469

---
+++ policyengine_us/variables/gov/states/vt/tax/income/adjusted_gross_income/vt_personal_exemptions.py
@@ -0,0 +1,32 @@
+from policyengine_us.model_api import *
+
+
+class vt_personal_exemptions(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Vermont personal exemptions"
+    unit = USD
+    definition_period = YEAR
+    defined_for = StateCode.VT
+    reference = (
+        "https://tax.vermont.gov/sites/tax/files/documents/IN-111-2022.pdf"
+    )
+
+    def formula(tax_unit, period, parameters):
+        personal_eligibility_amount = parameters(
+            period
+        ).gov.states.vt.tax.income.exemption.personal
+
+        # First, determine whether either the head or the spouse of the tax unit is claimable as a dependent in another unit.(line 5a, line5b)
+        claimable_count = add(tax_unit, period, ["dsi_spouse", "dsi"])
+        eligible_count = select(
+            [
+                claimable_count == 0,
+                claimable_count == 1,
+                claimable_count == 2,
+            ],
+            [2, 1, 0],
+        )
+        # Then, add number of other dependents claimed on federal Form 1040.(line 5c)
+        eligible_count += add(tax_unit, period, "tax_unit_count_dependents")
+        return eligible_count * personal_eligibility_amount
