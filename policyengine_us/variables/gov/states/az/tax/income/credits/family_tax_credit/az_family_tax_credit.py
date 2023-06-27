5e8aebcaafa7e9f12d8731607a38f65b11678132
JudyZ13 <112495414+JudyZ13@users.noreply.github.com>
2023-06-27 17:11:40 +0000

    Co-authored-by: Pavel Makarchuk <PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/states/az/tax/income/credits/family_tax_credit/az_family_tax_credit.py
+++ policyengine_us/variables/gov/states/az/tax/income/credits/family_tax_credit/az_family_tax_credit.py
@@ -13,7 +13,7 @@ class az_family_tax_credit(Variable):
         person = tax_unit.members
         p = parameters(period).gov.states.az.tax.income.credits
         income = tax_unit("adjusted_gross_income", period)
-        filing_status = tax_unit("filing_status", period) 
+        filing_status = tax_unit("filing_status", period)
         status = filing_status.possible_values
         dependents = tax_unit("tax_unit_dependents", period)
         head = tax_unit.any(person("is_tax_unit_head", period).astype(int))
@@ -32,10 +32,9 @@ class az_family_tax_credit(Variable):
                 p.eligibility.head_of_household.calc(dependents),
                 p.eligibility.separate,
                 p.eligibility.widow.calc(dependents),
-                ]
-
+            ],
         )
-        household_size = (head + spouse + dependents)
+        household_size = head + spouse + dependents
         amount = p.amount * household_size
         eligible = income <= max_income
-        return eligible * min_(amount, p.max_amount[filing_status])  
+        return eligible * min_(amount, p.max_amount[filing_status])
