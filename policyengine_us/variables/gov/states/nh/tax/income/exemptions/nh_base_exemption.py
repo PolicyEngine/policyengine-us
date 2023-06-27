cf8503f36aa35a118890e7bb2f5c2fdc07636bc2
PavelMakarchuk <pavel.ma99@gmail.com>
2023-06-27 11:30:56 -0400

    base adjustment

--- policyengine_us/variables/gov/states/nh/tax/income/exemptions/nh_base_exemption.py
+++ policyengine_us/variables/gov/states/nh/tax/income/exemptions/nh_base_exemption.py
@@ -4,9 +4,13 @@ from policyengine_us.model_api import *
 class nh_base_exemption(Variable):
     value_type = float
     entity = TaxUnit
-    label = "New Hampshire base exemption household level"
+    label = "New Hampshire base exemption"
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.NH
 
-    adds = "gov.states.nh.tax.income.exemptions.amount.base"
+    def formula(tax_unit, period, parameters):
+        exemptions = tax_unit("exemptions", period)
+        income_eligible = tax_unit("nh_income_tax", period) > 0
+        base = parameters(period).gov.states.nh.tax.income.exemptions.amount.base
+        return income_eligible * (exemptions * base)
