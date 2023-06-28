d0517084752feaf6ddbf65d2eca6cd9c86712530
“Eleanor-Zhu” <qzhu6976@usc.edu>
2023-06-28 23:39:44 +0000

    Georgia low income credit Fixes #2414

--- policyengine_us/variables/gov/states/ga/tax/income/credits/ga_low_income_credit.py
+++ policyengine_us/variables/gov/states/ga/tax/income/credits/ga_low_income_credit.py
@@ -7,19 +7,23 @@ class ga_low_income_credit(Variable):
     label = "Georgia low income credit"
     unit = USD
     definition_period = YEAR
-    reference = (
-        "https://dor.georgia.gov/document/document/2022-it-511-individual-income-tax-booklet/download"
-    )
+    reference = "https://dor.georgia.gov/document/document/2022-it-511-individual-income-tax-booklet/download"
     defined_for = StateCode.GA
 
     def formula(tax_unit, period, parameters):
         # federal ajusted gross income
         federal_agi = tax_unit("adjusted_gross_income", period)
-        p = parameters(period).gov.states.ga.tax.income.credits.low_income_credit
+        p = parameters(
+            period
+        ).gov.states.ga.tax.income.credits.low_income_credit
         # age threshold
         age_threshold = p.supplement_age_eligibility
-        aged_head = (tax_unit("age_head", period) >= age_threshold).astype(int) # if so, return 1, otherwise return 0
-        aged_spouse = (tax_unit("age_spouse", period) >= age_threshold).astype(int) # if so, return 1, otherwise return 0
+        aged_head = (tax_unit("age_head", period) >= age_threshold).astype(
+            int
+        )  # if so, return 1, otherwise return 0
+        aged_spouse = (tax_unit("age_spouse", period) >= age_threshold).astype(
+            int
+        )  # if so, return 1, otherwise return 0
         aged_count = aged_head + aged_spouse
         exemptions = tax_unit("exemptions", period)
         total_exemptions = aged_count + exemptions
