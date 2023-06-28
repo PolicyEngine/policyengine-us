90dda970803d535877f69374131ad26d8b355c6a
PavelMakarchuk <pavel.ma99@gmail.com>
2023-06-28 11:42:39 -0400

    format

--- policyengine_us/variables/gov/states/sc/tax/income/exemptions/sc_senior_exemption.py
+++ policyengine_us/variables/gov/states/sc/tax/income/exemptions/sc_senior_exemption.py
@@ -31,16 +31,28 @@ class sc_senior_exemption(Variable):
         spouse_eligible = ((age_spouse >= p.age_threshold) * joint).astype(int)
 
         # Get SC retirement income deduction and military retirement income deduction
-        
-        head_deductions = add(tax_unit, period, ["sc_retirement_income_deduction_head", "sc_military_retirement_income_deduction_head"])
-        spouse_deductions = add(tax_unit, period, ["sc_retirement_income_deduction_spouse", "sc_military_retirement_income_deduction_spouse"])
+
+        head_deductions = add(
+            tax_unit,
+            period,
+            [
+                "sc_retirement_income_deduction_head",
+                "sc_military_retirement_income_deduction_head",
+            ],
+        )
+        spouse_deductions = add(
+            tax_unit,
+            period,
+            [
+                "sc_retirement_income_deduction_spouse",
+                "sc_military_retirement_income_deduction_spouse",
+            ],
+        )
 
         # Calculate senior exemption. The exemption can not be less than 0. Add head and spouse together.
         # Per the legal code, this applies separately:
         # "(2) In the case of married taxpayers who file a joint federal income tax return, the reduction required by item (1) applies to each individual separately"
-        head_exemption = max_(
-            head_eligible * p.amount - head_deductions, 0
-        )
+        head_exemption = max_(head_eligible * p.amount - head_deductions, 0)
         spouse_exemption = max_(
             spouse_eligible * p.spouse_amount - spouse_deductions, 0
         )
