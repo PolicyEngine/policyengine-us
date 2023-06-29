731dcf5950278cb4a1083755079b77b9cca602f6
Linfeng-ll <95734090+Linfeng-ll@users.noreply.github.com>
2023-06-29 21:21:06 +0000

    add test cases and make format

--- policyengine_us/variables/gov/states/nm/tax/income/rebates/nm_low_income_comprehensive_tax_rebate.py
+++ policyengine_us/variables/gov/states/nm/tax/income/rebates/nm_low_income_comprehensive_tax_rebate.py
@@ -9,20 +9,18 @@ class nm_low_income_comprehensive_tax_rebate(Variable):
     definition_period = YEAR
     reference = (
         "https://klvg4oyd4j.execute-api.us-west-2.amazonaws.com/prod/PublicFiles/34821a9573ca43e7b06dfad20f5183fd/1afc56af-ea90-4d48-82e5-1f9aeb43255a/PITbook2022.pdf#page=58",
-        "https://klvg4oyd4j.execute-api.us-west-2.amazonaws.com/prod/PublicFiles/34821a9573ca43e7b06dfad20f5183fd/856ebf4b-3814-49dd-8631-ebe579d6a42b/Personal%20Income%20Tax.pdf#page=51"
-    ) 
+        "https://klvg4oyd4j.execute-api.us-west-2.amazonaws.com/prod/PublicFiles/34821a9573ca43e7b06dfad20f5183fd/856ebf4b-3814-49dd-8631-ebe579d6a42b/Personal%20Income%20Tax.pdf#page=51",
+    )
     # 7-2-7.6. 2021 INCOME TAX REBATE
     # SECTION II: LOW INCOME COMPREHENSIVE TAX REBATE
     defined_for = StateCode.NM
 
     def formula(tax_unit, period, parameters):
-        p = parameters(
-            period
-        ).gov.states.nm.tax.rebates.low_income_rebate
+        p = parameters(period).gov.states.nm.tax.rebates.low_income_rebate
 
         agi = tax_unit("adjusted_gross_income", period)
         exemption_num = tax_unit("nm_number_of_exemption", period)
-        
+
         rebate = select(
             [
                 exemption_num == 1,
@@ -30,7 +28,7 @@ class nm_low_income_comprehensive_tax_rebate(Variable):
                 exemption_num == 3,
                 exemption_num == 4,
                 exemption_num == 5,
-                exemption_num >= 6
+                exemption_num >= 6,
             ],
             [
                 p.exemption_amount1.calc(agi),
@@ -38,7 +36,7 @@ class nm_low_income_comprehensive_tax_rebate(Variable):
                 p.exemption_amount3.calc(agi),
                 p.exemption_amount4.calc(agi),
                 p.exemption_amount5.calc(agi),
-                p.exemption_amount6.calc(agi)
+                p.exemption_amount6.calc(agi),
             ],
         )
 
@@ -47,5 +45,5 @@ class nm_low_income_comprehensive_tax_rebate(Variable):
 
         if filing_status == statuses:
             return rebate / 2
-            
+
         return rebate
