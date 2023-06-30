9c4fb13dd3debfc440484174bdcbabd33cc04fb7
yy3187 <129118329+yy3187@users.noreply.github.com>
2023-06-30 04:05:39 +0000

    add parameters, variables, and tests

---
+++ policyengine_us/variables/gov/states/la/tax/income/deductions/la_itemized_deductions.py
@@ -0,0 +1,16 @@
+from policyengine_us.model_api import *
+
+class la_itemized_deduction(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Louisiana itemized deduction"
+    unit = USD
+    definition_period = YEAR
+    reference=(
+        "https://revenue.louisiana.gov/TaxForms/IT540iWEB(2022)D1.pdf"
+    )
+    defined_for=StateCode.LA
+    def formula(tax_unit, period, parameters):
+         v = tax_unit("medical_expense_deduction", period)
+        excess_itemized=max(medical_expenses-tax_unit("la_standard_deduction",period),0)
+        return excess_itemized
---
+++ policyengine_us/variables/gov/states/la/tax/income/deductions/la_standard_deduction.py
@@ -0,0 +1,17 @@
+from policyengine_us.model_api import *
+
+
+class la_standard_deduction(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Louisiana standard deduction"
+    unit = USD
+    definition_period = YEAR
+    reference=(
+        "https://revenue.louisiana.gov/TaxForms/IT540iWEB(2022)D1.pdf"
+    )
+    defined_for=StateCode.LA
+    def formula(tax_unit, period, parameters):
+        filing_status = tax_unit("filing_status", period)
+        p = parameters(period).gov.states.la.tax.income.deductions.standard
+        return p.amount[filing_status]
