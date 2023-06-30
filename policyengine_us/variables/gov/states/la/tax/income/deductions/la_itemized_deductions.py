0fe66845ff7e7183f544f5e8237fc3920f394156
yy3187 <129118329+yy3187@users.noreply.github.com>
2023-06-30 04:11:21 +0000

    fix identation

--- policyengine_us/variables/gov/states/la/tax/income/deductions/la_itemized_deductions.py
+++ policyengine_us/variables/gov/states/la/tax/income/deductions/la_itemized_deductions.py
@@ -6,11 +6,10 @@ class la_itemized_deduction(Variable):
     label = "Louisiana itemized deduction"
     unit = USD
     definition_period = YEAR
-    reference=(
-        "https://revenue.louisiana.gov/TaxForms/IT540iWEB(2022)D1.pdf"
-    )
-    defined_for=StateCode.LA
+    reference = "https://revenue.louisiana.gov/TaxForms/IT540iWEB(2022)D1.pdf"
+    defined_for = StateCode.LA
+
     def formula(tax_unit, period, parameters):
-         v = tax_unit("medical_expense_deduction", period)
-        excess_itemized=max(medical_expenses-tax_unit("la_standard_deduction",period),0)
+        medical_expenses = tax_unit("medical_expense_deduction", period)
+        excess_itemized = max(medical_expenses - tax_unit("la_standard_deduction", period), 0)
         return excess_itemized
