89ddd099956ba749b2143806f80b6713f5a7b077
SirMalamute <neeliyer14@gmail.com>
2023-07-02 16:48:34 +0000

    Indiana Earned Income Credit Fixes #2478 added child eligible variable

---
+++ policyengine_us/variables/gov/states/in/tax/income/credits/eitc/in_is_child_eligible_eic.py
@@ -0,0 +1,15 @@
+from policyengine_us.model_api import *
+
+
+class is_child_eligible_eic(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = YEAR
+    label = "Child-level eligiblity for parents filing Indiana EIC"
+    documentation = "Whether a child whose parent filing for Indiana EIC meets the demographic requirements."
+
+    def formula(person, period, parameters):
+        is_19 = person("age", period) < 19
+        is_24 = person("age", period) < 24
+        full_time_student = person("is_full_time_student", period)
+        return is_19 | (is_24 && full_time_student)
