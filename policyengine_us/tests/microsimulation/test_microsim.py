a0c8d06a8dddf893d8550e38f5d22d4941296064
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-07-03 10:18:27 -0400

    Strengthen new tests

--- policyengine_us/tests/microsimulation/test_microsim.py
+++ policyengine_us/tests/microsimulation/test_microsim.py
@@ -5,4 +5,6 @@ def test_microsim_runs_cps():
     hnet = sim.calc("household_net_income")
     assert not hnet.isna().any(), "Some households have NaN net income."
     hidecile = sim.calc("household_income_decile")
+    assert (hidecile >= 0 & hidecile <= 10).all()
     sidecile = sim.calc("spm_unit_income_decile")
+    assert (sidecile >= 0 & sidecile <= 10).all()
