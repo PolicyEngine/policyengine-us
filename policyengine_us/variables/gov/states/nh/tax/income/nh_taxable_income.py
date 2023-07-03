0b897c241a27c5fd609e625d60fcde1f24ca8e2c
Max Ghenis <mghenis@gmail.com>
2023-07-03 19:52:55 -0400

    Update policyengine_us/variables/gov/states/nh/tax/income/nh_taxable_income.py

--- policyengine_us/variables/gov/states/nh/tax/income/nh_taxable_income.py
+++ policyengine_us/variables/gov/states/nh/tax/income/nh_taxable_income.py
@@ -9,5 +9,8 @@ class nh_taxable_income(Variable):
     definition_period = YEAR
     reference = "https://www.gencourt.state.nh.us/rsa/html/V/77/77-4.htm "
     defined_for = StateCode.NH
+
+    # New Hampshire allows for negative taxable income.
+    # It limits tax to nonnegative values in the tax computation instead.
     adds = ["dividend_income", "interest_income"]
     subtracts = ["nh_total_exemptions"]
