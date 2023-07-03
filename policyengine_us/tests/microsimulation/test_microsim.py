0a0fedbd946d58448b250f84e0a48d92ad7adc7a
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-07-03 10:05:01 -0400

    Add income_decile variables to test_microsim_runs_cps

--- policyengine_us/tests/microsimulation/test_microsim.py
+++ policyengine_us/tests/microsimulation/test_microsim.py
@@ -4,3 +4,5 @@ def test_microsim_runs_cps():
     sim = Microsimulation()
     hnet = sim.calc("household_net_income")
     assert not hnet.isna().any(), "Some households have NaN net income."
+    hidecile = sim.calc("household_income_decile")
+    sidecile = sim.calc("spm_unit_income_decile")
