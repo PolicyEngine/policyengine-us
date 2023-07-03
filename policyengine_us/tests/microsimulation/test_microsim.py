1faf05fe319caf71c5c896f3b74cdfb22bebb43b
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-07-03 11:53:26 -0400

    Revise new tests

--- policyengine_us/tests/microsimulation/test_microsim.py
+++ policyengine_us/tests/microsimulation/test_microsim.py
@@ -1,10 +1,11 @@
 def test_microsim_runs_cps():
+    import numpy as np
     from policyengine_us import Microsimulation
 
     sim = Microsimulation()
     hnet = sim.calc("household_net_income")
     assert not hnet.isna().any(), "Some households have NaN net income."
     hidecile = sim.calc("household_income_decile")
-    assert (hidecile >= 1 & hidecile <= 10).all()
+    assert np.all(hidecile >= 1) and np.all(hidecile <= 10)
     sidecile = sim.calc("spm_unit_income_decile")
-    assert (sidecile >= 1 & sidecile <= 10).all()
+    assert np.all(sidecile >= 1) and np.all(sidecile <= 10)
