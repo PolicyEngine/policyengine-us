196c032e4505c99ab67358237893966979f210b1
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-07-03 10:03:13 -0400

    Skip income_decile variables in test_variables

--- policyengine_us/tests/test_variables.py
+++ policyengine_us/tests/test_variables.py
@@ -11,6 +11,8 @@ DEFAULT_SITUATION = {
 }
 
 EXEMPTIONS = (
+    "household_income_decile",  # because DEFAULT_SITUATION has no weights
+    "spm_unit_income_decile",  # because DEFAULT_SITUATION has no weights
     "tanf_max_amount",
     "tanf",
     "tanf_amount_if_eligible",
