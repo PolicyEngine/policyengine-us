8dc8b4f2f19a1434e62e8bc1f20a71b1580fb9d3
Zixuan Ma <66671734+mzx-4real@users.noreply.github.com>
2023-07-04 17:54:50 +0800

    Fix

--- policyengine_us/variables/gov/states/vt/tax/income/adjusted_gross_income/vt_personal_exemptions.py
+++ policyengine_us/variables/gov/states/vt/tax/income/adjusted_gross_income/vt_personal_exemptions.py
@@ -1,4 +1,5 @@
 from policyengine_us.model_api import *
+from numpy import invert
 
 
 class vt_personal_exemptions(Variable):
@@ -19,19 +20,14 @@ class vt_personal_exemptions(Variable):
         # First, Need to determine the tax unit filing status, if they are filing jointly
         filing_status = tax_unit("filing_status", period)
         is_joint = filing_status == filing_status.possible_values.JOINT
-        # Then, determine whether either the head or the spouse of the tax unit is claimable as a dependent in another unit.(line 5a, line 5b)
-        claimable_head = (tax_unit("dsi", period)).astype(int)
-        claimable_spouse = (tax_unit("dsi_spouse", period)).astype(int)
-        claimable_count = claimable_head + claimable_spouse
-        eligible_count = select(
-            [
-                (claimable_count == 0 & is_joint),
-                (claimable_count == 1 & is_joint),
-                (claimable_count == 2 & is_joint),
-                (claimable_count == 0 & ~is_joint),
-                (claimable_count == 1 & ~is_joint),
-            ],
-            [2, 1, 0, 1, 0],
+        # Then, determine whether either the head or the spouse of the tax unit is claimable as a dependent in another unit.
+        claimable_head = tax_unit("dsi", period)
+        claimable_spouse = tax_unit("dsi_spouse", period)
+        # If claimable elsewhere, it is not eligible for vt personal exemption (line 5a, line 5b)
+        eligible_head = invert(claimable_head).astype(int)
+        eligible_spouse = invert(claimable_spouse).astype(int)
+        eligible_count = where(
+            is_joint, (eligible_head + eligible_spouse), eligible_head
         )
         # Last, add number of other dependents claimed on federal Form 1040.(line 5c)
         total_exemption_count = eligible_count + tax_unit(
