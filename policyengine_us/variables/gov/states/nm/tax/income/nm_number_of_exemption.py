4d72182342413d6ac9dc76ba455ce090d681f17b
Linfeng-ll <95734090+Linfeng-ll@users.noreply.github.com>
2023-07-04 20:59:14 +0000

    change period

--- policyengine_us/variables/gov/states/nm/tax/income/nm_number_of_exemption.py
+++ policyengine_us/variables/gov/states/nm/tax/income/nm_number_of_exemption.py
@@ -9,7 +9,7 @@ class nm_number_of_exemption(Variable):
     definition_period = YEAR
     reference = (
         "https://klvg4oyd4j.execute-api.us-west-2.amazonaws.com/prod/PublicFiles/34821a9573ca43e7b06dfad20f5183fd/1afc56af-ea90-4d48-82e5-1f9aeb43255a/PITbook2022.pdf#page=58",
-        "https://klvg4oyd4j.execute-api.us-west-2.amazonaws.com/prod/PublicFiles/34821a9573ca43e7b06dfad20f5183fd/856ebf4b-3814-49dd-8631-ebe579d6a42b/Personal%20Income%20Tax.pdf#page=51",
+        "https://klvg4oyd4j.execute-api.us-west-2.amazonaws.com/prod/PublicFiles/34821a9573ca43e7b06dfad20f5183fd/856ebf4b-3814-49dd-8631-ebe579d6a42b/Personal%20Income%20Tax.pdf#page=67",
     )
     # 7-2-7.6. 2021 INCOME TAX REBATE
     # SECTION II: LOW INCOME COMPREHENSIVE TAX REBATE
