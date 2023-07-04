99f263b2fe6271578045c10c531c283c344f4f45
Max Ghenis <mghenis@gmail.com>
2023-07-04 08:17:53 -0400

    fix typos and shorten code

--- policyengine_us/variables/gov/states/pa/dhs/tanf/eligibility/pa_tanf_resources_eligible.py
+++ policyengine_us/variables/gov/states/pa/dhs/tanf/eligibility/pa_tanf_resources_eligible.py
@@ -4,16 +4,12 @@ from policyengine_us.model_api import *
 class pa_tanf_resources_eligible(Variable):
     value_type = bool
     entity = SPMUnit
-    label = "Pennsylvania TANF resources eligible"
+    label = "Meets Pennsylvania TANF resource limit"
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.PA
 
     def formula(spm_unit, period, parameters):
-        resource_limit = parameters(
-            period
-        ).gov.states.pa.dhs.tanf.resource_limit
-        total_assets = spm_unit("pa_tanf_countable_resources", period)
-        return (
-            spm_unit("pa_tanf_countable_resources", period) <= resource_limit
-        )
+        p = parameters(period).gov.states.pa.dhs.tanf
+        resources = spm_unit("pa_tanf_countable_resources", period)
+        return resources <= p.resource_limit
