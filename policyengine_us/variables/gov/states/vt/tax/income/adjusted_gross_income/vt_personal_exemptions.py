b0625229041cbaf864eaf16e492c9ddeb4009896
Zixuan Ma <66671734+mzx-4real@users.noreply.github.com>
2023-07-04 23:16:48 +0800

    variable name small change

--- policyengine_us/variables/gov/states/vt/tax/income/adjusted_gross_income/vt_personal_exemptions.py
+++ policyengine_us/variables/gov/states/vt/tax/income/adjusted_gross_income/vt_personal_exemptions.py
@@ -21,11 +21,11 @@ class vt_personal_exemptions(Variable):
         filing_status = tax_unit("filing_status", period)
         is_joint = filing_status == filing_status.possible_values.JOINT
         # Then, determine whether either the head or the spouse of the tax unit is claimable as a dependent in another unit.
-        claimable_head = tax_unit("dsi", period)
-        claimable_spouse = tax_unit("dsi_spouse", period)
+        claimable_elsewhere_head = tax_unit("dsi", period)
+        claimable_elsewhere_spouse = tax_unit("dsi_spouse", period)
         # If claimable elsewhere, it is not eligible for vt personal exemption (line 5a, line 5b)
-        eligible_head = invert(claimable_head).astype(int)
-        eligible_spouse = invert(claimable_spouse).astype(int)
+        eligible_head = invert(claimable_elsewhere_head).astype(int)
+        eligible_spouse = invert(claimable_elsewhere_spouse).astype(int)
         eligible_count = where(
             is_joint, (eligible_head + eligible_spouse), eligible_head
         )
