366c335ed8f8838edbaac5e6d6581cb27cafb257
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-07-05 11:05:19 -0400

    Refer to health (not medical) insurance

--- policyengine_us/variables/gov/states/wi/tax/income/subtractions/wi_health_insurance_subtraction.py
+++ policyengine_us/variables/gov/states/wi/tax/income/subtractions/wi_health_insurance_subtraction.py
@@ -1,10 +1,10 @@
 from policyengine_us.model_api import *
 
 
-class wi_medical_insurance_subtraction(Variable):
+class wi_health_insurance_subtraction(Variable):
     value_type = float
     entity = TaxUnit
-    label = "WI medical insurance subtraction from federal adjusted gross income"
+    label = "WI health insurance subtraction from federal adjusted gross income"
     unit = USD
     definition_period = YEAR
     reference = (
