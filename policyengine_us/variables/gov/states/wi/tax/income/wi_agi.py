cc9aa120867c3eabcb72330db764e9d799342915
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-07-05 16:27:35 -0400

    Update wi_agi.py variable file

--- policyengine_us/variables/gov/states/wi/tax/income/wi_agi.py
+++ policyengine_us/variables/gov/states/wi/tax/income/wi_agi.py
@@ -4,14 +4,14 @@ from policyengine_us.model_api import *
 class wi_agi(Variable):
     value_type = float
     entity = TaxUnit
-    label = "WI AGI, adjusted gross income"
+    label = "WI Adjusted Gross Income"
     unit = USD
     definition_period = YEAR
     reference = (
-        "https://www.revenue.wi.gov/TaxForms2021/2021-ScheduleAD.pdf"
-        "https://www.revenue.wi.gov/TaxForms2021/2021-ScheduleAD-inst.pdf"
-        "https://www.revenue.wi.gov/TaxForms2022/2022-ScheduleADf.pdf"
-        "https://www.revenue.wi.gov/TaxForms2022/2022-ScheduleAD-Inst.pdf"
+        "https://www.revenue.wi.gov/TaxForms2021/2021-Form1f.pdf"
+        "https://www.revenue.wi.gov/TaxForms2021/2021-Form1-Inst.pdf"
+        "https://www.revenue.wi.gov/TaxForms2022/2022-Form1f.pdf"
+        "https://www.revenue.wi.gov/TaxForms2022/2022-Form1-Inst.pdf"
     )
     defined_for = StateCode.WI
     adds = ["adjusted_gross_income", "wi_income_additions"]
