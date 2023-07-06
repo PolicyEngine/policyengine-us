c3c7d77c7e7bb8c6477a4921f96d41aee228886b
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-07-06 15:48:07 -0400

    Add wi_married_couple_credit formula, parameters, unit tests

--- policyengine_us/variables/gov/states/wi/tax/income/credits/nonrefundable/wi_married_couple_credit.py
+++ policyengine_us/variables/gov/states/wi/tax/income/credits/nonrefundable/wi_married_couple_credit.py
@@ -8,13 +8,29 @@ class wi_married_couple_credit(Variable):
     unit = USD
     definition_period = YEAR
     reference = (
-        "https://www.revenue.wi.gov/TaxForms2021/2021-Form1f.pdf#page=2"
+        "https://www.revenue.wi.gov/TaxForms2021/2021-Form1f.pdf#page=4"
         "https://www.revenue.wi.gov/TaxForms2021/2021-Form1-Inst.pdf#page=21"
-        "https://www.revenue.wi.gov/TaxForms2022/2022-Form1f.pdf#page=2"
+        "https://www.revenue.wi.gov/TaxForms2022/2022-Form1f.pdf#page=4"
         "https://www.revenue.wi.gov/TaxForms2022/2022-Form1-Inst.pdf#page=21"
         "https://docs.legis.wisconsin.gov/misc/lfb/informational_papers/january_2023/0002_individual_income_tax_informational_paper_2.pdf"
     )
     defined_for = StateCode.WI
 
     def formula(tax_unit, period, parameters):
-        return 0
+        fstatus = tax_unit("filing_status", period)
+        eligible = fstatus == fstatus.possible_values.JOINT
+        print("\n>>>> eligible", eligible)
+        person = tax_unit.person
+        p = parameters(period).gov.states.wi.tax.income.credits.nonrefundable
+        print(">>>> sources", p.married_couple.income_sources)
+        income = add(person, period, p.married_couple.income_sources)
+        print(">>>> income", income)
+        is_head = person("is_tax_unit_head", period)
+        is_spouse = person("is_tax_unit_spouse", period)
+        head_income = tax_unit.sum(is_head * income)
+        spouse_income = tax_unit.sum(is_spouse * income)
+        lower_income = min_(head_income, spouse_income)
+        return min_(
+            eligible * lower_income * p.married_couple.rate,
+            p.married_couple.max,
+        )
