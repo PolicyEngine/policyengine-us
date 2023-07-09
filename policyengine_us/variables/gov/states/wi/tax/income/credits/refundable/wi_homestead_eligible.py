fc9820d37564b44009e59aa5eda74b459ca4723d
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-07-09 12:07:45 -0400

    Add wi_homestead_eligible formula and associated parameter files

--- policyengine_us/variables/gov/states/wi/tax/income/credits/refundable/wi_homestead_eligible.py
+++ policyengine_us/variables/gov/states/wi/tax/income/credits/refundable/wi_homestead_eligible.py
@@ -6,14 +6,29 @@ class wi_homestead_eligible(Variable):
     entity = TaxUnit
     label = "Wisconsin homestead credit eligibility status"
     definition_period = YEAR
-    reference = (
-        "https://www.revenue.wi.gov/TaxForms2021/2021-Form1f.pdf#page=3"
-        "https://www.revenue.wi.gov/TaxForms2021/2021-Form1-Inst.pdf#page=27"
-        "https://www.revenue.wi.gov/TaxForms2022/2022-Form1f.pdf#page=3"
-        "https://www.revenue.wi.gov/TaxForms2022/2022-Form1-Inst.pdf#page=28"
-        "https://docs.legis.wisconsin.gov/misc/lfb/informational_papers/january_2023/0002_individual_income_tax_informational_paper_2.pdf"
-    )
+    reference = "https://docs.legis.wisconsin.gov/misc/lfb/informational_papers/january_2021/0013_homestead_tax_credit_informational_paper_13.pdf#page=7"
     defined_for = StateCode.WI
 
     def formula(tax_unit, period, parameters):
-        return True
+        p = parameters(period).gov.states.wi.tax.income.credits.refundable
+        # minimum age eligibility
+        head_age = tax_unit("age_head", period)
+        spouse_age = tax_unit("age_spouse", period)
+        min_age = p.homestead.eligible.min_age
+        age_eligible = (head_age >= min_age) | (spouse_age >= min_age)
+        # earnings eligibility
+        earnings = add(tax_unit, period, p.homestead.eligible.earnings_sources)
+        has_positive_earnings = earnings > 0
+        min_elderly_age = p.homestead.eligible.min_elderly_age
+        head_is_elderly = head_age >= min_elderly_age
+        spouse_is_elderly = spouse_age >= min_elderly_age
+        is_elderly = head_is_elderly | spouse_is_elderly
+        head_is_disabled = tax_unit("head_is_disabled", period)
+        spouse_is_disabled = tax_unit("spouse_is_disabled", period)
+        is_disabled = head_is_disabled | spouse_is_disabled
+        earnings_eligible = has_positive_earnings | is_elderly | is_disabled
+        # income eligibility
+        homestead_income = tax_unit("wi_homestead_income", period)
+        income_eligible = homestead_income < p.homestead.eligible.max_income
+        # overall eligibility
+        return age_eligible & earnings_eligible & income_eligible
