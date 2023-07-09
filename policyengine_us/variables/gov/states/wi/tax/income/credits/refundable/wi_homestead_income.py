3153e96c72bb5dd40d0794be83ae51b5a4cdea77
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-07-09 13:14:01 -0400

    Add wi_homestead_income formula and associated parameter files

--- policyengine_us/variables/gov/states/wi/tax/income/credits/refundable/wi_homestead_income.py
+++ policyengine_us/variables/gov/states/wi/tax/income/credits/refundable/wi_homestead_income.py
@@ -8,13 +8,13 @@ class wi_homestead_income(Variable):
     unit = USD
     definition_period = YEAR
     reference = (
-        "https://www.revenue.wi.gov/TaxForms2021/2021-Form1f.pdf#page=3"
-        "https://www.revenue.wi.gov/TaxForms2021/2021-Form1-Inst.pdf#page=27"
-        "https://www.revenue.wi.gov/TaxForms2022/2022-Form1f.pdf#page=3"
-        "https://www.revenue.wi.gov/TaxForms2022/2022-Form1-Inst.pdf#page=28"
-        "https://docs.legis.wisconsin.gov/misc/lfb/informational_papers/january_2023/0002_individual_income_tax_informational_paper_2.pdf"
+        "https://www.revenue.wi.gov/TaxForms2021/2021-ScheduleH.pdf"
+        "https://www.revenue.wi.gov/TaxForms2022/2022-ScheduleH.pdf"
     )
     defined_for = StateCode.WI
 
     def formula(tax_unit, period, parameters):
-        return 0
+        p = parameters(period).gov.states.wi.tax.income.credits.refundable
+        income = add(tax_unit, period, p.homestead.income.sources)
+        dependents = tax_unit("tax_unit_dependents", period)
+        return income - dependents * p.homestead.income.exemption
