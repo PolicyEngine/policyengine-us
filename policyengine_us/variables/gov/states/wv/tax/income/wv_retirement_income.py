d364b5c325d193e1e4ffa4b014714ae57accf503
Naiqiao Ye <104268340+yedeer12@users.noreply.github.com>
2023-07-09 21:17:35 +0000

    Add variable wv_taxable_income

---
+++ policyengine_us/variables/gov/states/wv/tax/income/wv_retirement_income.py
@@ -0,0 +1,11 @@
+from policyengine_us.model_api import *
+
+
+class wv_taxable_income(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "West Virginia other retirement subtraction"
+    unit = USD
+    definition_period = YEAR
+    reference = "https://code.wvlegislature.gov/11-21-12/"
+    defined_for = StateCode.WV
