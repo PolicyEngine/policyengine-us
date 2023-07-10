be383e35658ac599cc7ab7392921d630fd96ee2c
Zixuan Ma <66671734+mzx-4real@users.noreply.github.com>
2023-07-10 18:50:46 +0800

    Vermont taxable income standard deductions draft Fixes #2601

--- policyengine_us/variables/gov/states/vt/tax/income/taxable_income/vt_standard_deductions.py
+++ policyengine_us/variables/gov/states/vt/tax/income/taxable_income/vt_standard_deductions.py
@@ -12,3 +12,14 @@ class vt_standard_deduction(Variable):
     )
     reference = "Hold"
     defined_for = StateCode.VT
+
+    def formula(tax_unit, period, parameters):
+        p = parameters(period).gov.states.vt.tax.income.deductions.standard
+        # Base deduction based on filing status.
+        filing_status = tax_unit("filing_status", period)
+        base_deduction = p.base[filing_status]
+        # Aged/blind extra standard deduction.
+        aged_blind_count = tax_unit("aged_blind_count", period)
+        amount_per_aged_blind = p.extra[filing_status]
+        aged_blind_deduction = aged_blind_count * amount_per_aged_blind
+        return base_deduction + aged_blind_deduction
