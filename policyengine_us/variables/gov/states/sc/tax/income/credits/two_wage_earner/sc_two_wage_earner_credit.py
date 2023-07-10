347ee95a514ebd66cb5e82d3d34f255fd9f46813
hua7450 <113824411+hua7450@users.noreply.github.com>
2023-07-10 23:20:31 +0000

    South Carolina Two wage earner credit Fixes #2307

--- policyengine_us/variables/gov/states/sc/tax/income/credits/two_wage_earner/sc_two_wage_earner_credit.py
+++ policyengine_us/variables/gov/states/sc/tax/income/credits/two_wage_earner/sc_two_wage_earner_credit.py
@@ -1,5 +1,6 @@
 from policyengine_us.model_api import *
 
+
 class sc_two_wage_earner_credits(Variable):
     value_type = float
     entity = TaxUnit
@@ -14,16 +15,16 @@ class sc_two_wage_earner_credits(Variable):
         # Determine eligibility. Must be a joint filer.
         filing_status = tax_unit("filing_status", period)
         eligible = filing_status == filing_status.possible_values.JOINT
-       # Find relevant gross income of head and spouse.
+        # Find relevant gross income of head and spouse.
         person = tax_unit.members
         income = person("sc_gross_earned_income", period)
         head_income = tax_unit.max(income * person("is_tax_unit_head", period))
-        spouse_income = tax_unit.max(income * person("is_tax_unit_spouse", period))
+        spouse_income = tax_unit.max(
+            income * person("is_tax_unit_spouse", period)
+        )
         # Determine lesser of head and spouse income.
         lesser_head_spouse_income = min_(head_income, spouse_income)
         # Calculate credit based on rate.
-        p = parameters(
-            period
-        ).gov.states.sc.tax.income.credits.two_wage_earner
+        p = parameters(period).gov.states.sc.tax.income.credits.two_wage_earner
         credit_if_eligible = p.rate.calc(lesser_head_spouse_income)
         return credit_if_eligible * eligible
