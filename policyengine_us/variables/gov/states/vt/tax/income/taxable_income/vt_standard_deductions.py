ce32d92903a47bab5f40462569d8b5a5cc6a0c39
Zixuan Ma <66671734+mzx-4real@users.noreply.github.com>
2023-07-10 17:17:08 +0800

    Draft

--- policyengine_us/variables/gov/states/vt/tax/income/taxable_income/vt_standard_deductions.py
+++ policyengine_us/variables/gov/states/vt/tax/income/taxable_income/vt_standard_deductions.py
@@ -1,13 +1,14 @@
 from policyengine_us.model_api import *
 
 
-class vt_deductions(Variable):
+class vt_standard_deduction(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Vermont income deductions"
+    label = "VT standard deduction"
     unit = USD
     definition_period = YEAR
+    documentation = (
+        "Vermont standard deduction, including bonus for aged or blind."
+    )
     reference = "Hold"
     defined_for = StateCode.VT
-
-    adds = ["vt_standard_deductions"]
