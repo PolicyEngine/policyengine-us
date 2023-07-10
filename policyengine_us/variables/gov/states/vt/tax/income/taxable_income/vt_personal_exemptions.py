81925dcc8594cd1b6b2aac9ac5d1fae2de15df7f
Zixuan Ma <66671734+mzx-4real@users.noreply.github.com>
2023-07-10 16:25:32 +0800

    suggested change

--- policyengine_us/variables/gov/states/vt/tax/income/taxable_income/vt_personal_exemptions.py
+++ policyengine_us/variables/gov/states/vt/tax/income/taxable_income/vt_personal_exemptions.py
@@ -1,5 +1,4 @@
 from policyengine_us.model_api import *
-from numpy import invert
 
 
 class vt_personal_exemptions(Variable):
@@ -14,9 +13,7 @@ class vt_personal_exemptions(Variable):
     )
 
     def formula(tax_unit, period, parameters):
-        personal_eligibility_amount = parameters(
-            period
-        ).gov.states.vt.tax.income.exemption.personal
+        p = parameters(period).gov.states.vt.tax.income.exemption.personal
         # First, Need to determine the tax unit filing status, if they are filing jointly
         filing_status = tax_unit("filing_status", period)
         is_joint = filing_status == filing_status.possible_values.JOINT
@@ -24,13 +21,10 @@ class vt_personal_exemptions(Variable):
         claimable_elsewhere_head = tax_unit("dsi", period)
         claimable_elsewhere_spouse = tax_unit("dsi_spouse", period)
         # If claimable elsewhere, it is not eligible for vt personal exemption (line 5a, line 5b)
-        eligible_head = invert(claimable_elsewhere_head).astype(int)
-        eligible_spouse = invert(claimable_elsewhere_spouse).astype(int)
-        eligible_count = where(
-            is_joint, (eligible_head + eligible_spouse), eligible_head
-        )
+        eligible_head = (~claimable_elsewhere_head).astype(int)
+        eligible_spouse = (~claimable_elsewhere_spouse).astype(int)
+        eligible_count = eligible_head + (eligible_spouse * is_joint)
         # Last, add number of other dependents claimed on federal Form 1040.(line 5c)
-        total_exemption_count = eligible_count + tax_unit(
-            "tax_unit_count_dependents", period
-        )
-        return total_exemption_count * personal_eligibility_amount
+        dependents = tax_unit("tax_unit_count_dependents", period)
+        total_exemption_count = eligible_count + dependents
+        return total_exemption_count * p
