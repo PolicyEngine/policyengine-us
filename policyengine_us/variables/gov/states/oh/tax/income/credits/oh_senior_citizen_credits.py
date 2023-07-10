2a3d8dad00d2c70f897edba41652bfd7fffa9cd7
Linfeng-ll <95734090+Linfeng-ll@users.noreply.github.com>
2023-07-10 19:26:45 +0000

    change senior ciziten credit parameter file

--- policyengine_us/variables/gov/states/oh/tax/income/credits/oh_senior_citizen_credits.py
+++ policyengine_us/variables/gov/states/oh/tax/income/credits/oh_senior_citizen_credits.py
@@ -14,20 +14,17 @@ class oh_senior_citizen_credits(Variable):
     defined_for = StateCode.OH
 
     def formula(tax_unit, period, parameters):
-        person = tax_unit.members
         p = parameters(period).gov.states.oh.tax.income.credits.senior_citizen
+        person = tax_unit.members
 
         has_not_taken_lump_sum_distribution = person(
             "oh_has_not_taken_oh_lump_sum_credits", period
         )
-
         age = person("age", period)
+
         any_elderly = tax_unit.any(age >= p.age_threshold)
-        income_eligible = tax_unit("oh_agi", period) < p.agi_cap
+        credit_amount = p.agi_limit.calc(tax_unit("oh_agi", period))
 
         return (
-            any_elderly
-            * income_eligible
-            * p.credit_amount
-            * has_not_taken_lump_sum_distribution
+            any_elderly * has_not_taken_lump_sum_distribution * credit_amount
         )
