79d832d101f4a77a69c689ac78f93b0af5126933
Matty <31402450+Mshuning@users.noreply.github.com>
2023-07-10 21:48:47 +0000

    reference & format

--- policyengine_us/variables/gov/states/mi/tax/income/exemptions/mi_exemptions.py
+++ policyengine_us/variables/gov/states/mi/tax/income/exemptions/mi_exemptions.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class mi_exemptions(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Michigan Exemptions"
+    label = "Michigan exemptions"
     defined_for = StateCode.MI
     unit = USD
     definition_period = YEAR
@@ -21,22 +21,22 @@ class mi_exemptions(Variable):
         personal_exemption = person * p.filer
 
         # Dependent exemptions
-        dependent = tax_unit("tax_unit_dependents", period)
-        dependent_exemption = dependent * p.dependent
+        dependents = tax_unit("tax_unit_dependents", period)
+        dependent_exemption = dependents * p.dependent
 
         # Stillborn Exemptions
-        stillborn = tax_unit("tax_unit_stillbirths", period)
-        stillborn_exemption = stillborn * p.stillborn
+        stillbirths = tax_unit("tax_unit_stillbirths", period)
+        stillborn_exemption = stillbirths * p.stillborn
 
         # Disabled exemptions
         disabled_people = add(tax_unit, period, ["is_disabled"])
         disabled_exemption = disabled_people * p.disabled
 
         # Disabled veteran exemptions
-        disabled_veteran = add(
+        disabled_veterans = add(
             tax_unit, period, ["is_fully_disabled_service_connected_veteran"]
         )
-        disabled_veteran_exemption = disabled_veteran * p.disabled_veteran
+        disabled_veteran_exemption = disabled_veterans * p.disabled_veteran
 
         # Dependent on other return exemptions
         filing_status = tax_unit("filing_status", period)
