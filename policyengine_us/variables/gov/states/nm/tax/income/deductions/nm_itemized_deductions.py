76da3524b74e2eb2e1b98efdd4e47e46161126c1
Linfeng-ll <95734090+Linfeng-ll@users.noreply.github.com>
2023-07-11 20:49:21 +0000

    New Mexico Itemized Deduction Fixes #2420

--- policyengine_us/variables/gov/states/nm/tax/income/deductions/nm_itemized_deductions.py
+++ policyengine_us/variables/gov/states/nm/tax/income/deductions/nm_itemized_deductions.py
@@ -10,7 +10,7 @@ class nm_itemized_deductions(Variable):
     reference = (
         "https://klvg4oyd4j.execute-api.us-west-2.amazonaws.com/prod/PublicFiles/34821a9573ca43e7b06dfad20f5183fd/1afc56af-ea90-4d48-82e5-1f9aeb43255a/PITbook2022.pdf#page=28",
         "https://www.irs.gov/pub/irs-pdf/f1040sa.pdf",
-        "https://www.irs.gov/pub/irs-pdf/f1040.pdf"
+        "https://www.irs.gov/pub/irs-pdf/f1040.pdf",
     )
     defined_for = StateCode.NM
 
