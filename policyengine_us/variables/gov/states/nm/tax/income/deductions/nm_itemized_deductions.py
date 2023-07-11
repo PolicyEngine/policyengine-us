7966d78bce9ada94304dccdd6e0b04d4c059b139
Linfeng-ll <95734090+Linfeng-ll@users.noreply.github.com>
2023-07-11 01:56:37 +0000

    edit variable

--- policyengine_us/variables/gov/states/nm/tax/income/deductions/nm_itemized_deductions.py
+++ policyengine_us/variables/gov/states/nm/tax/income/deductions/nm_itemized_deductions.py
@@ -13,19 +13,34 @@ class nm_itemized_deductions(Variable):
     defined_for = StateCode.NM
 
     def formula(tax_unit, period, parameters):
-        # tax/income
+        filing_status = tax_unit("filing_status", period)
+
+        # tax/income, federal Schedule A, line 5a.
         slat_sales_or_income = tax_unit(
             "state_and_local_sales_or_income_tax", period
         )
-        # state_and_local_tax
+        # state_and_local_tax, line 5d
         total_salt = tax_unit("real_estate_taxes", period) + slat_sales_or_income
-        salt_claimed = tax_unit("salt_deduction", period)
-        ratio = round(slat_sales_or_income / total_salt)
-
-        filing_status = tax_unit("filing_status", period)
-        adjustment_base_amount = parameters(
-            period
-        ).gov.irs.deductions.itemized.salt_and_real_estate.cap[filing_status]
-
+        ratio = round(slat_sales_or_income / total_salt, 4)
+        
+        # line 5e
+        # salt_claimed = tax_unit("salt_deduction", period)
+        salt = salt_claimed * min(ratio, 1)
+        
         standard_deduction = tax_unit("standard_deduction", period)
+        p = parameters(period).gov.irs.deductions
+        itm_deds = [
+            deduction
+            for deduction in p.itemized_deductions
+        ]
+        us_itmemized_deductions = add(tax_unit, period, itm_deds)
+        item_deds = max(us_itmemized_deductions - standard_deduction, 0)
+        return min(salt, item_deds)
+
+        # 
+        # adjustment_base_amount = parameters(
+            # period
+        # ).gov.irs.deductions.itemized.salt_and_real_estate.cap[filing_status]
+
+        
 
