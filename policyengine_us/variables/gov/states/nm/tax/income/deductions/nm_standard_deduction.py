aec088c97f250dc39aabfa75fac1a349a54dade8
PavelMakarchuk <pavel.ma99@gmail.com>
2023-07-11 19:40:07 -0400

    variable

--- policyengine_us/variables/gov/states/nm/tax/income/deductions/nm_standard_deduction.py
+++ policyengine_us/variables/gov/states/nm/tax/income/deductions/nm_standard_deduction.py
@@ -6,16 +6,7 @@ class nm_standard_deduction(Variable):
     entity = TaxUnit
     label = "New Mexico standard deduction"
     unit = USD
-    documentation = "https://www.irs.gov/instructions/i1040gi#en_US_2022_publink24811vd0e10057"
+    documentation = 'https://www.irs.gov/instructions/i1040gi#en_US_2022_publink24811vd0e10057'
     definition_period = YEAR
     defined_for = StateCode.NM
-    # adds = ["basic_standard_deduction", "additional_standard_deduction"]
-
-    def formula(tax_unit, period, parameters):
-        p = parameters(period).gov.states.nm.tax.income.deductions.standard
-        filing_status = tax_unit("filing_status", period)
-        base_amt = p.base[filing_status]
-        # is_aged or blind?
-        aged_blind_count = tax_unit("aged_blind_count", period)
-        extra_amt = aged_blind_count * p.elderly_blind[filing_status]
-        return base_amt + extra_amt
+    adds = ["standard_deduction"]
