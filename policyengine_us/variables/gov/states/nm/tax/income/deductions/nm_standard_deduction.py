139c2ca991c6437a99bb29e2d34a8fc6e84ec1ad
PavelMakarchuk <pavel.ma99@gmail.com>
2023-07-11 19:41:34 -0400

    format

--- policyengine_us/variables/gov/states/nm/tax/income/deductions/nm_standard_deduction.py
+++ policyengine_us/variables/gov/states/nm/tax/income/deductions/nm_standard_deduction.py
@@ -6,7 +6,7 @@ class nm_standard_deduction(Variable):
     entity = TaxUnit
     label = "New Mexico standard deduction"
     unit = USD
-    documentation = 'https://www.irs.gov/instructions/i1040gi#en_US_2022_publink24811vd0e10057'
+    documentation = "https://www.irs.gov/instructions/i1040gi#en_US_2022_publink24811vd0e10057"
     definition_period = YEAR
     defined_for = StateCode.NM
     adds = ["standard_deduction"]
