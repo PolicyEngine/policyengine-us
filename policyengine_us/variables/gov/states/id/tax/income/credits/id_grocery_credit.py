f158d1b93577572b07865c14a866ee976be9c34d
Gene Jung <85149548+universescraper@users.noreply.github.com>
2023-07-12 17:58:31 +0000

    adding a test for Id_gc

--- policyengine_us/variables/gov/states/id/tax/income/credits/id_grocery_credit.py
+++ policyengine_us/variables/gov/states/id/tax/income/credits/id_grocery_credit.py
@@ -9,20 +9,21 @@ class id_grocery_credit(Variable):
     definition_period = YEAR
     defined_for = "StateCode.ID"
 
-
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states.id.tax.income.credits.gc
 
-        #100$ for each dependent
+        # 100$ for each dependent
         person = tax_unit.members
-        dependent = person("is_tax_unit_dependent", period) # example: [0, 0, 1, 1, 1]
-        total_dependents = tax_unit.sum(dependent) #example sum [3] 
+        dependent = person(
+            "is_tax_unit_dependent", period
+        )  # example: [0, 0, 1, 1, 1]
+        total_dependents = tax_unit.sum(dependent)  # example sum [3]
         dependet_amount = total_dependents * p.amount_dependent
 
-        #20$ extra for each aged person
+        # 20$ extra for each aged person
         person_aged = person("age", period) > p.age_older_eligibility
         total_aged = tax_unit.sum(person_aged)
         aged_amount = total_aged * p.amount_65_older
-        
-        #100$ for self
+
+        # 100$ for self
         return p.amount + dependet_amount + aged_amount
