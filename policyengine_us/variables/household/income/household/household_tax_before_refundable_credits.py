5910273f1ba506c079f20b3e3cc070912f075904
Max Ghenis <mghenis@gmail.com>
2023-07-12 09:06:39 -0400

    Hook up New Hampshire tax to net income tree Fixes #2619

--- policyengine_us/variables/household/income/household/household_tax_before_refundable_credits.py
+++ policyengine_us/variables/household/income/household/household_tax_before_refundable_credits.py
@@ -22,6 +22,7 @@ class household_tax_before_refundable_credits(Variable):
         "mo_income_tax_before_refundable_credits",
         "nd_income_tax_before_refundable_credits",
         "ne_income_tax_before_refundable_credits",
+        "nh_income_tax_before_refundable_credits",
         "ny_income_tax_before_refundable_credits",
         "or_income_tax_before_refundable_credits",
         "pa_income_tax",  # PA has no refundable credits.
