edf0a11aeb74313cefdbdf3882067d59de2a9926
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2023-07-13 14:57:35 +0100

    Get test running

--- policyengine_us/reforms/reforms.py
+++ policyengine_us/reforms/reforms.py
@@ -1,5 +1,7 @@
 from .congress.delauro import create_american_family_act_with_baby_bonus_reform
 from .winship import create_eitc_winship_reform
+from policyengine_core.reforms import Reform
+import warnings
 
 
 def create_structural_reforms_from_parameters(parameters, period):
@@ -8,4 +10,11 @@ def create_structural_reforms_from_parameters(parameters, period):
     )
     winship_reform = create_eitc_winship_reform(parameters, period)
 
-    return afa_reform, winship_reform
+    if afa_reform is not None:
+        if winship_reform is not None:
+            warnings.warn(
+                "Both the American Family Act and the Winship EITC reform are enabled. Multiple structural reforms are not yet supported, so only the American Family Act will be applied."
+            )
+        return afa_reform
+    elif winship_reform is not None:
+        return winship_reform
