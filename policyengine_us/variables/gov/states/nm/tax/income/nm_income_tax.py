ee2fd32db13431766a4e46caef8ecb996288fa9f
PavelMakarchuk <pavel.ma99@gmail.com>
2023-07-12 22:11:57 -0400

    calc

--- policyengine_us/variables/gov/states/nm/tax/income/nm_income_tax.py
+++ policyengine_us/variables/gov/states/nm/tax/income/nm_income_tax.py
@@ -8,5 +8,10 @@ class nm_income_tax(Variable):
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.NM
-    adds = ["nm_income_tax_before_non_refundable_credits"]
-    subtracts = ["nm_refundable_credits"]
+
+    def formula(tax_unit, period, parameters):
+        before_non_refundable_credits = tax_unit("nm_income_tax_before_non_refundable_credits", period)
+        non_refundable_credits = tax_unit("nm_non_refundable_credit", period)
+        refundable_credits = tax_unit("nm_refundable_credits", period)
+        capped_non_refunable_credits = max_(before_non_refundable_credits - non_refundable_credits, 0)
+        return capped_non_refunable_credits - refundable_credits
