714b1ce45824be7a790e577c70e1f338ceba6b5f
Linfeng-ll <95734090+Linfeng-ll@users.noreply.github.com>
2023-07-14 20:09:44 +0000

    New Mexico Itemized Deduction Fixes #2420 change logic

--- policyengine_us/variables/gov/states/nm/tax/income/deductions/nm_itemized_deductions.py
+++ policyengine_us/variables/gov/states/nm/tax/income/deductions/nm_itemized_deductions.py
@@ -15,27 +15,29 @@ class nm_itemized_deductions(Variable):
     def formula(tax_unit, period, parameters):
         itemizes = tax_unit("tax_unit_itemizes", period)
         filing_status = tax_unit("filing_status", period)
-
         p = parameters(period).gov.irs.deductions
-        # tax/income, federal Schedule A, line 5a.
+
+        # tax/income, federal Schedule A, line 5a. 1
         slat_sales_or_income = tax_unit(
             "state_and_local_sales_or_income_tax", period
         )
-        # state_and_local_tax, line 5d
+        # state_and_local_tax, line 5d. 2
         total_salt = (
             add(tax_unit, period, ["real_estate_taxes"]) + slat_sales_or_income
         )
-
         # ratio = round(slat_sales_or_income[0] / total_salt[0], 4)
-
+        # ratio. 3
         salt_ratio = np.zeros_like(total_salt)
         mask = total_salt != 0
         salt_ratio[mask] = slat_sales_or_income[mask] / total_salt[mask]
 
+        # line 5e. 4
         salt_cap = p.itemized.salt_and_real_estate.cap[filing_status]
-        # salt_claimed = tax_unit("salt_deduction", period)
+        salt_claimed = min_(salt_cap, total_salt)
 
-        salt = salt_ratio * min_(salt_cap, total_salt)
+        # 5 = salt_claimed * salt_ratio
+        # 6 = min(5, 4)
+        salt = min_(salt_claimed * salt_ratio, salt_claimed)
 
         standard_deduction = tax_unit("standard_deduction", period)
         itm_deds = [deduction for deduction in p.itemized_deductions]
