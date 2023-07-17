5301afe9e22fce2f16647ddd0d08a80148331154
SirMalamute <neeliyer14@gmail.com>
2023-07-17 12:14:14 +0000

    Indiana Earned Income Credit Fixes #2478
    Co-authored-by: Pavel Makarchuk <PavelMakarchuk@users.noreply.github.com>
    minor

--- policyengine_us/variables/gov/states/in/tax/income/credits/eitc/in_eitc.py
+++ policyengine_us/variables/gov/states/in/tax/income/credits/eitc/in_eitc.py
@@ -2,23 +2,25 @@ from policyengine_us.model_api import *
 
 
 class in_eitc(Variable):
-    value_type = bool
+    value_type = float
     entity = TaxUnit
     definition_period = YEAR
     label = "Indiana EIC"
-    unit = "currency-USD"
-    defined_for = StateCode.ID
+    unit = USD
+    defined_for = StateCode.IN
 
     def formula(tax_unit, period, parameters):
         person = tax_unit.members
         eligible_children = tax_unit.sum(person("is_child_eligible_eitc", period))
         p = parameters(period).gov.states["in"].tax.income.credits.eitc
         eitc = tax_unit("earned_income_tax_credit", period) # needs to be > 0
-        earned_income = tax_unit("employment_income", period) # needs to be less than parameter
+        earned_income = person("earned_income", period) # needs to be less than parameter
+        total_earned_income = tax_unit.sum(earned_income)
         federal_agi = tax_unit("adjusted_gross_income", period) # needs to be less than parameter
 
-        earned_eligible = p.child_income_bracket_eligibility.calc(eligible_children) < earned_income 
-        agi_eligible = p.child_income_bracket_eligibility.calc(eligible_children) < federal_agi
-        eligible = earned_eligible & agi_eligible
+        earned_eligible = (p.child_income_bracket_eligibility.calc(eligible_children)) > total_earned_income 
+        agi_eligible = p.child_income_bracket_eligibility.calc(eligible_children) > federal_agi
+        eligible = earned_eligible & agi_eligible        
+
 
         return eligible * (eitc * p.match)
