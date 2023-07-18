3bc29fa71eb7c9c914ed810d391e7aca20e77343
phywzc <97962358+phywzc@users.noreply.github.com>
2023-07-18 11:50:45 -0400

    Update policyengine_us/variables/gov/states/az/tax/income/credits/increased_excise_tax_credit.py
    Co-authored-by: Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/states/az/tax/income/credits/increased_excise_tax_credit.py
+++ policyengine_us/variables/gov/states/az/tax/income/credits/increased_excise_tax_credit.py
@@ -29,7 +29,7 @@ class az_increased_excise_tax_credit(Variable):
         filing_status = tax_unit("filing_status", period)
         max_income = p.maximum[filing_status]
         eligible = agi <= max_income
-        dependents1 = tax_unit("tax_unit_dependents", period)
+        head_or_spouse_dependents = tax_unit("tax_unit_dependents", period)
         dependents2 = p.dependent2[filing_status]
         total_dependents = dependents1 + dependents2
         current_credit = total_dependents * p.credit_based_on_cal_dependents
