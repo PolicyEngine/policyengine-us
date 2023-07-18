3e3b069cbba0fce69a70317f35d2734e23448fdc
phywzc <97962358+phywzc@users.noreply.github.com>
2023-07-18 17:21:29 +0000

    Arizona Itemized Deduction Fixes #2527

--- policyengine_us/variables/gov/states/az/tax/income/deductions/itemized/az_itemized_deduction_adjustments.py
+++ policyengine_us/variables/gov/states/az/tax/income/deductions/itemized/az_itemized_deduction_adjustments.py
@@ -20,21 +20,21 @@ class az_itemized_deduction_adjustments(Variable):
         line1_medical_expense = add(tax_unit, period, ["medical_expense"])
         medical = parameters(period).gov.irs.deductions.itemized.medical
         line2_medical_floor = medical.floor * tax_unit("positive_agi", period)
-        line3_4_adjustments_medical=abs_(expense - medical_floor)
-
+        line3_4_adjustments_medical = abs_(expense - medical_floor)
 
         # Adjustment to Interest Deduction
         # line5=If you received a federal credit for interest paid on mortgage credit certificates (from federal Form 8396),
         #  enter the amount of mortgage interest you paid for 2022 that is equal to the amount of your 2022federal credit.
-        line5 = 0 
+        line5 = 0
         # Currently, I considere it is 0 since I didn't find anything in variable.gov.irs.income.taxable_income.deductions.itemizing
-
 
         # Adjustments to Charitable Contributions
         # Amount of charitable contributions for which you are claiming a credit under Arizona law
         # I assume it is the same as federal law
         cash_donations = add(tax_unit, period, ["charitable_cash_donations"])
-        non_cash_donations = add(tax_unit, period, ["charitable_non_cash_donations"])
+        non_cash_donations = add(
+            tax_unit, period, ["charitable_non_cash_donations"]
+        )
         positive_agi = tax_unit("positive_agi", period)
         ceiling = parameters(
             period
@@ -42,37 +42,30 @@ class az_itemized_deduction_adjustments(Variable):
         capped_non_cash_donations = min_(
             non_cash_donations, ceiling.non_cash * positive_agi
         )
-        line6 = min_(capped_non_cash_donations + cash_donations, ceiling.all * positive_agi)
-
+        line6 = min_(
+            capped_non_cash_donations + cash_donations,
+            ceiling.all * positive_agi,
+        )
 
         # If yes, line7=0. I assume we claim income taxes on federal Sechedule A.
-        line1A=tax_unit("state_and_local_sales_or_income_tax", period) 
+        line1A = tax_unit("state_and_local_sales_or_income_tax", period)
         # line2A= Amount included in the line 1A for which you claimed an Arizona credit
-        line3A = line1A-line2A
+        line3A = line1A - line2A
         p = parameters(period).gov.states.az.tax.income.deductions.itemized
         line4A = p.limit_from_federal_schedule_a[filing_status]
         line5A = min_(line3A, line4A)
         # line6A = Enter total state income taxes claimed on federal Schedule A (after limitation)
-        line7 = line6A-line5A
+        line7 = line6A - line5A
 
-        # Other Adjustments
-        # line8 = Amount allowed as a federal itemized deduction that relates to income not subject to Arizona tax
         # This part was marked as not included after meeting
         line8 = 0
 
-
         # adjusted itemized deduction
-        line9=line3+line5
-        line10=line4+line6+line7+line8
+        line9 = line3 + line5
+        line10 = line4 + line6 + line7 + line8
         # line11=Total federal itemized deductions allowed to be taken on federal return
+        # line11=tax_unit("taxable_income_deductions", period)
         line12 = line9
-        line13 = line11+line12
+        line13 = line11 + line12
         line14 = line10
         return max_(line13 - line14, 0)
-
-
-
-
-
-
-
