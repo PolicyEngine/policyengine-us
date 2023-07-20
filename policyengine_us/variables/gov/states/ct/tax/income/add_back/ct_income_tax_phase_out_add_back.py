64721e3b79a1a0628af9ccde44ca2eafb42c4bfd
PavelMakarchuk <pavel.ma99@gmail.com>
2023-07-20 14:55:52 -0400

    variable for addback and recapture

--- policyengine_us/variables/gov/states/ct/tax/income/add_back/ct_income_tax_phase_out_add_back.py
+++ policyengine_us/variables/gov/states/ct/tax/income/add_back/ct_income_tax_phase_out_add_back.py
@@ -10,3 +10,14 @@ class ct_income_tax_phase_out_add_back(Variable):
     defined_for = StateCode.CT
 
     def formula(tax_unit, period, parameters):
+        income = tax_unit("ct_agi", period)
+        p = parameters(period).gov.states.ct.tax.income.add_back
+        filing_status = tax_unit("filing_status", period)
+        start = p.start[filing_status]
+        max_amount = p.max_amount[filing_status]
+        brackets = p.brackets[filing_status]
+        amount = p.amount[filing_status]
+        income_start = max_(income - start, 0)
+        income_bracktes = income_start / brackets
+        amount = income_bracktes * amount
+        return min_(max_amount, amount)
