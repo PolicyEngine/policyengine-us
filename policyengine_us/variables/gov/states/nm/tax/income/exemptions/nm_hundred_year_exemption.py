a6ddad58b654070b18b7464b8369b75bc96bbc74
Jingjing Wang <87988673+Jingj-WANG@users.noreply.github.com>
2023-07-20 23:30:16 +0000

    New Mexico Exemption; income of individuals one hundred years of age or older. Fixes #2656

---
+++ policyengine_us/variables/gov/states/nm/tax/income/exemptions/nm_hundred_year_exemption.py
@@ -0,0 +1,16 @@
+from policyengine_us.model_api import *
+
+
+class nm_hundred_year_exemption(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "New Mexico hundred year exemption"
+    unit = USD
+    definition_period = YEAR
+    reference = "https://nmonesource.com/nmos/nmsa/en/item/4340/index.do#!fragment/zoupio-_Toc140503677/BQCwhgziBcwMYgK4DsDWszIQewE4BUBTADwBdoAvbRABwEtsBaAfX2zgEYAWABgFYeAZgBsAdlEBKADTJspQhACKiQrgCe0AOSapEQmFwJlqjdt37DIAMp5SAIQ0AlAKIAZZwDUAggDkAws5SpGAARtCk7BISQA"
+    defined_for = StateCode.NM
+
+    def formula(tax_unit, period, parameters):
+        age_head = tax_unit("age_head", period)
+        age_spouse = tax_unit("age_spouse", period)
+        
