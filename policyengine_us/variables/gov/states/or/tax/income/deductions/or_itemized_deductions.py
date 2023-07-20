58dc688129a59b2d2d88cea2c80352e7bd920a87
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-07-20 09:40:04 -0400

    Simplify OR itemized deductions code

--- policyengine_us/variables/gov/states/or/tax/income/deductions/or_itemized_deductions.py
+++ policyengine_us/variables/gov/states/or/tax/income/deductions/or_itemized_deductions.py
@@ -10,13 +10,4 @@ class or_itemized_deductions(Variable):
     reference = "https://www.oregonlegislature.gov/bills_laws/ors/ors316.html"  # 316.695 (1)(d)
     defined_for = StateCode.OR
 
-    def formula(tax_unit, period, parameters):
-        or_itemized_deductions_less_salt = tax_unit(
-            "itemized_deductions_less_salt", period
-        )
-        property_taxes = add(tax_unit, period, ["real_estate_taxes"])
-        p = parameters(period).gov.irs.deductions
-        salt = p.itemized.salt_and_real_estate
-        cap = salt.cap[tax_unit("filing_status", period)]
-        capped_property_taxes = min_(property_taxes, cap)
-        return or_itemized_deductions_less_salt + capped_property_taxes
+    adds = ["itemized_deductions_less_salt", "capped_property_taxes"]
