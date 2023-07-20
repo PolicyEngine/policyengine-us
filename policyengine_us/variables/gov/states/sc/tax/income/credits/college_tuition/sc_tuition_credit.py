5248716c8f0740cba7aa37d7adbf4a06a7eacc36
hua7450 <113824411+hua7450@users.noreply.github.com>
2023-07-20 07:59:39 +0000

    minor fix

--- policyengine_us/variables/gov/states/sc/tax/income/credits/college_tuition/sc_tuition_credit.py
+++ policyengine_us/variables/gov/states/sc/tax/income/credits/college_tuition/sc_tuition_credit.py
@@ -16,12 +16,18 @@ class sc_tuition_credit(Variable):
         # line 1
         total_hours = tax_unit("total_college_hours", period)
         # line 2
-        qualified_tuition_expenses = add(tax_unit, period, ["qualified_tuition_expenses"])
+        qualified_tuition_expenses = add(
+            tax_unit, period, ["qualified_tuition_expenses"]
+        )
         # line 3
         annual_hour_requirement = p.annual_hour_requirement
-        tuition_limit = p.max_amount.tuition * total_hours / annual_hour_requirement
+        tuition_limit = (
+            p.max_amount.tuition * total_hours / annual_hour_requirement
+        )
         # line 7 (lesser of line 2 or 3 and multiply by rate)
-        uncapped_credit = min_(qualified_tuition_expenses, tuition_limit) * p.rate
-        
-        # compare line 7 with credit max amount, take lesser amount 
+        uncapped_credit = (
+            min_(qualified_tuition_expenses, tuition_limit) * p.rate
+        )
+
+        # compare line 7 with credit max amount, take lesser amount
         return min_(uncapped_credit, p.max_amount.credit)
