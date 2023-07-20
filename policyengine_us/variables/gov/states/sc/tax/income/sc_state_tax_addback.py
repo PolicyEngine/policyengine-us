15358e93f57b0347a33a8742c98cc6e9be3d154a
hua7450 <113824411+hua7450@users.noreply.github.com>
2023-07-20 08:37:23 +0000

    South Carolina State Tax Addback Fixes #2575

--- policyengine_us/variables/gov/states/sc/tax/income/sc_state_tax_addback.py
+++ policyengine_us/variables/gov/states/sc/tax/income/sc_state_tax_addback.py
@@ -8,25 +8,32 @@ class sc_state_tax_addback(Variable):
     unit = USD
     definition_period = YEAR
     reference = "https://dor.sc.gov/forms-site/Forms/SC1040_2022.pdf"
+    reference = (
+        "https://dor.sc.gov/forms-site/Forms/SC1040inst_2022.pdf#page=2"
+    )
     defined_for = StateCode.SC
 
     def formula(tax_unit, period, parameters):
         p_us = parameters(period).gov.irs.deductions
-        itm_deds = [deduction for deduction in p_us.itemized_deductions]
+        itm_deds = [
+            deduction
+            for deduction in p_us.itemized_deductions
+            if deduction not in ["salt_deduction"]
+        ]
         deds_if_not_itm = [
             deduction for deduction in p_us.deductions_if_not_itemizing
         ]
+        # us_itemizing = add(tax_unit,period,["tax_unit_itemizes"])
+        # standard_deduction = add(tax_unit,period,["standard_deduction"])
         filing_status = tax_unit("filing_status", period)
         eligible = filing_status != filing_status.possible_values.SEPARATE
         # line 1
         federal_itemized_deduction = add(tax_unit, period, itm_deds)
         # line 2
-        federal_deduction_if_not_itemizing = (
-            add(tax_unit, period, deds_if_not_itm) * eligible
-        )
+        federal_deduction = add(tax_unit, period, deds_if_not_itm) * eligible
         # line 3
         less_itm_amount = max_(
-            0, federal_itemized_deduction - federal_deduction_if_not_itemizing
+            0, federal_itemized_deduction - federal_deduction
         )
         # line 4
         salt = tax_unit("state_and_local_sales_or_income_tax", period)
@@ -35,9 +42,10 @@ class sc_state_tax_addback(Variable):
             add(tax_unit, period, ["real_estate_taxes"]),
             p_us.itemized.salt_and_real_estate.cap[filing_status],
         )
-        less_income_amount = (
+        less_income_amount = max_(
+            0,
             p_us.itemized.salt_and_real_estate.cap[filing_status]
-            - capped_property_taxes
+            - capped_property_taxes,
         )
         # compare line 3,4,5. get the minimum
         return min(less_itm_amount, salt, less_income_amount)
