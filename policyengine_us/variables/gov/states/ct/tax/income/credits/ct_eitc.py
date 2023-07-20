8160df5643835d687dd5c7daa62ccd8d9a739618
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2023-07-20 22:09:48 +0000

    Changes

--- policyengine_us/variables/gov/states/ct/tax/income/credits/ct_eitc.py
+++ policyengine_us/variables/gov/states/ct/tax/income/credits/ct_eitc.py
@@ -7,12 +7,14 @@ class ct_eitc(Variable):
     label = "CT EITC"
     unit = USD
     definition_period = YEAR
-    reference = "https://portal.ct.gov/-/media/DRS/Forms/2022/Income/Schedule-CT-EITC_1222.pdf" 
+    reference = "https://portal.ct.gov/-/media/DRS/Forms/2022/Income/Schedule-CT-EITC_1222.pdf"
     defined_for = StateCode.CT
 
     def formula(tax_unit, period, parameters):
         eitc = tax_unit("earned_income_tax_credit", period)
-        rate = parameters(period).gov.states.ct.tax.income.credits.eitc.refundable
+        rate = parameters(
+            period
+        ).gov.states.ct.tax.income.credits.eitc.refundable
         tentative_ct_eic = eitc * rate
         household_credit = tax_unit("ct_household_credit", period)
         return max_(0, tentative_ct_eic - household_credit)
