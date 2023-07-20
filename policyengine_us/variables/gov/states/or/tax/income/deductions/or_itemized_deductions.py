8c66f9dee46ff2defa944222b74b28e04a28c44e
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-07-20 09:19:34 -0400

    Use itemized_deductions_less_salt in OR income tax code

--- policyengine_us/variables/gov/states/or/tax/income/deductions/or_itemized_deductions.py
+++ policyengine_us/variables/gov/states/or/tax/income/deductions/or_itemized_deductions.py
@@ -11,14 +11,11 @@ class or_itemized_deductions(Variable):
     defined_for = StateCode.OR
 
     def formula(tax_unit, period, parameters):
-        p = parameters(period).gov.irs.deductions
-        itm_deds = [
-            deduction
-            for deduction in p.itemized_deductions
-            if deduction not in ["salt_deduction"]
-        ]
-        or_itemized_deductions_less_salt = add(tax_unit, period, itm_deds)
+        or_itemized_deductions_less_salt = tax_unit(
+            "itemized_deductions_less_salt", period
+        )
         property_taxes = add(tax_unit, period, ["real_estate_taxes"])
+        p = parameters(period).gov.irs.deductions
         salt = p.itemized.salt_and_real_estate
         cap = salt.cap[tax_unit("filing_status", period)]
         capped_property_taxes = min_(property_taxes, cap)
