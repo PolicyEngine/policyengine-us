0f2a1905456c169e921ae63744b2c92a00ad1d01
Jingjing Wang <87988673+Jingj-WANG@users.noreply.github.com>
2023-07-20 23:12:47 +0000

    Co-authored-by: Pavel Makarchuk <PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/states/nm/tax/income/exemptions/nm_low_and_middle_income_exemption.py
+++ policyengine_us/variables/gov/states/nm/tax/income/exemptions/nm_low_and_middle_income_exemption.py
@@ -17,9 +17,9 @@ class nm_low_and_middle_income_exemption(Variable):
         agi = tax_unit("adjusted_gross_income", period)
 
         # Calculate the eligibility based on AGI and filing status
-        eligible = agi <= p.income_threshold[filing_status]
+        eligible = agi <= p.income_limit[filing_status]
 
-        # Calculate the examption amount based on AGI and filing status
+        # Calculate the exemption amount based on AGI and filing status
         reduction_threshold = p.reduction.income_threshold[filing_status]
         excess = max_(agi - reduction_threshold, 0)
         reduction_rate = p.reduction.rate[filing_status]
