0ccf0f5ae45c88d7c6ec1a6b886da65d70e102df
phywzc <97962358+phywzc@users.noreply.github.com>
2023-07-20 09:59:41 +0000

    Arizona Itemized Deduction Fixes #2527

--- policyengine_us/variables/gov/states/az/tax/income/deductions/itemized/az_itemized_deduction_adjustments.py
+++ policyengine_us/variables/gov/states/az/tax/income/deductions/itemized/az_itemized_deduction_adjustments.py
@@ -7,6 +7,8 @@ class az_itemized_deduction_adjustments(Variable):
     label = "Arizona itemized deduction adjustments"
     unit = USD
     documentation = "Arizona Form 140 Schedule A"
+    definition_period = YEAR
+    defined_for = StateCode.AZ
     reference = (
         "https://azdor.gov/forms/individual/itemized-deduction-adjustments-form"
         "https://azdor.gov/forms/individual/form-140-resident-personal-income-tax-form-calculating"
@@ -15,13 +17,13 @@ class az_itemized_deduction_adjustments(Variable):
     defined_for = StateCode.AZ
 
     def formula(tax_unit, period, parameters):
-        # itemizing = tax_unit("tax_unit_itemizes", period)
         us_p = parameters(period).gov.irs.deductions
-        line1_medical_expense = add(tax_unit, period, ["medical_expense"])
+        line1 = add(tax_unit, period, ["medical_expense"])
         medical = parameters(period).gov.irs.deductions.itemized.medical
-        line2_medical_floor = medical.floor * tax_unit("positive_agi", period)
-        line3_4_adjustments_medical = abs_(expense - medical_floor)
-
+        line2 = medical.floor * tax_unit("positive_agi", period)
+        line3 = line1 - line2 if line1 >= line2 else 0
+        line4 = line2 - line1 if line2 > line1 else 0
+        
         # Adjustment to Interest Deduction
         # line5=If you received a federal credit for interest paid on mortgage credit certificates (from federal Form 8396),
         #  enter the amount of mortgage interest you paid for 2022 that is equal to the amount of your 2022federal credit.
@@ -31,30 +33,18 @@ class az_itemized_deduction_adjustments(Variable):
         # Adjustments to Charitable Contributions
         # Amount of charitable contributions for which you are claiming a credit under Arizona law
         # I assume it is the same as federal law
-        cash_donations = add(tax_unit, period, ["charitable_cash_donations"])
-        non_cash_donations = add(
-            tax_unit, period, ["charitable_non_cash_donations"]
-        )
-        positive_agi = tax_unit("positive_agi", period)
-        ceiling = parameters(
-            period
-        ).gov.irs.deductions.itemized.charity.ceiling
-        capped_non_cash_donations = min_(
-            non_cash_donations, ceiling.non_cash * positive_agi
-        )
-        line6 = min_(
-            capped_non_cash_donations + cash_donations,
-            ceiling.all * positive_agi,
-        )
+        line6 = tax_unit("charitable_deduction", period)
 
         # If yes, line7=0. I assume we claim income taxes on federal Sechedule A.
         line1A = tax_unit("state_and_local_sales_or_income_tax", period)
         # line2A= Amount included in the line 1A for which you claimed an Arizona credit
+        line2A = tax_unit("az_total_credit", period)
         line3A = line1A - line2A
         p = parameters(period).gov.states.az.tax.income.deductions.itemized
-        line4A = p.limit_from_federal_schedule_a[filing_status]
+        filing_status = tax_unit("filing_status", period)
+        line4A = p.itemized_deduction_limit[filing_status]
         line5A = min_(line3A, line4A)
-        # line6A = Enter total state income taxes claimed on federal Schedule A (after limitation)
+        line6A = tax_unit("salt_deduction", period)
         line7 = line6A - line5A
 
         # This part was marked as not included after meeting
@@ -64,7 +54,7 @@ class az_itemized_deduction_adjustments(Variable):
         line9 = line3 + line5
         line10 = line4 + line6 + line7 + line8
         # line11=Total federal itemized deductions allowed to be taken on federal return
-        # line11=tax_unit("taxable_income_deductions", period)
+        line11 = tax_unit("az_total_allowed_itemized_deduction_irs", period)
         line12 = line9
         line13 = line11 + line12
         line14 = line10
---
+++ policyengine_us/variables/gov/states/az/tax/income/deductions/itemized/az_total_itemized_deduction_allowed_by_irs.py
@@ -0,0 +1,21 @@
+from policyengine_us.model_api import *
+
+
+class az_total_allowed_itemized_deduction_irs(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "line11(Total federal itemized deductions allowed to be taken on federal return) from Arizona Form 140 Schedule A Form 140"
+    unit = USD
+    documentation = "Arizona Form 140 Schedule A Form 140"
+    definition_period = YEAR
+    defined_for = StateCode.AZ
+    reference = (
+        "https://azdor.gov/forms/individual/itemized-deduction-adjustments-form"
+        "https://azdor.gov/forms/individual/form-140-resident-personal-income-tax-form-calculating"
+    )
+
+    def formula(tax_unit, period, parameters):
+        p = parameters(period).gov.irs.deductions
+        items = [deduction for deduction in p.itemized_deductions]
+        federal_itemized_deductions = add(tax_unit, period, items)
+        return federal_itemized_deductions
