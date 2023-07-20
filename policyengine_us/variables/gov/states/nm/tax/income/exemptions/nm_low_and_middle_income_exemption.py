6c82223e523b225e07248ee88435a6f2e0e62da5
Jingjing Wang <87988673+Jingj-WANG@users.noreply.github.com>
2023-07-20 21:33:45 +0000

    New Mexico low- and middle income tax exemption Fixes #2422

--- policyengine_us/variables/gov/states/nm/tax/income/exemptions/nm_low_and_middle_income_exemption.py
+++ policyengine_us/variables/gov/states/nm/tax/income/exemptions/nm_low_and_middle_income_exemption.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class nm_low_and_middle_income_exemption(Variable):
     value_type = float
     entity = TaxUnit
-    label = "New Mexico low- and middle income exemption"
+    label = "New Mexico low- and middle-income exemption"
     defined_for = StateCode.NM
     unit = USD
     definition_period = YEAR
@@ -19,16 +19,14 @@ class nm_low_and_middle_income_exemption(Variable):
         # Calculate the eligibility based on AGI and filing status
         eligible = agi <= p.income_threshold[filing_status]
 
-        # Calculate the reduction based on AGI and filing status
-        reduction_threshold = max_(
-            agi - p.reduction.income_threshold[filing_status], 0
-        )
-        reduction_amount = (
-            reduction_threshold * p.reduction.rate[filing_status]
-        )
-        reduced_exemption_amount = p.max_amount - reduction_amount
+        # Calculate the examption amount based on AGI and filing status
+        reduction_threshold = p.reduction.income_threshold[filing_status]
+        excess = max_(agi - reduction_threshold, 0)
+        reduction_rate = p.reduction.rate[filing_status]
+        reduction_amount = excess * reduction_rate
+        exemption_amount = p.max_amount - reduction_amount
 
         # Multiply the exemption amount by the number of exemptions
         exemptions = tax_unit("exemptions", period)
 
-        return eligible * (reduced_exemption_amount * exemptions)
+        return eligible * (exemption_amount * exemptions)
