2e441913db931f6451800adb86cf8ecaa5aca673
PavelMakarchuk <pavel.ma99@gmail.com>
2023-07-21 12:30:42 -0400

    fix

--- policyengine_us/variables/gov/states/nm/tax/income/credits/nm_working_families_credit.py
+++ policyengine_us/variables/gov/states/nm/tax/income/credits/nm_working_families_credit.py
@@ -20,9 +20,4 @@ class nm_working_families_credit(Variable):
         rate = parameters(
             period
         ).gov.states.nm.tax.income.credits.working_families_tax.match
-        # Credit is halved for married filing separately
-        filing_status = tax_unit("filing_status", period)
-        separate = filing_status == filing_status.possible_values.SEPARATE
-        denominator = where(separate, 2, 1)
-        numerator = eitc * rate
-        return numerator / denominator
+        return eitc * rate
