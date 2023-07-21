6a1370bb873c66dd6fb189cb0bf2fa7bb0d40c06
PavelMakarchuk <pavel.ma99@gmail.com>
2023-07-20 21:13:19 -0400

    final variable

--- policyengine_us/variables/gov/states/nm/tax/income/exemptions/nm_medical_expense_exemption.py
+++ policyengine_us/variables/gov/states/nm/tax/income/exemptions/nm_medical_expense_exemption.py
@@ -1,17 +1,17 @@
 from policyengine_us.model_api import *
 
 
-class nm_medical_expense_credit(Variable):
+class nm_medical_expense_exemption(Variable):
     value_type = float
     entity = TaxUnit
-    label = "New Mexico unreimbursed medical expense credit"
+    label = "New Mexico unreimbursed medical expense exemption"
     definition_period = YEAR
-    reference = "https://nmonesource.com/nmos/nmsa/en/item/4340/index.do#!fragment/zoupio-_Toc140503776/BQCwhgziBcwMYgK4DsDWszIQewE4BUBTADwBdoAvbRABwEtsBaAfX2zgEYAWABgFYeAZgDswgGwBKADTJspQhACKiQrgCe0AOSapEQmFwJlqjdt37DIAMp5SAIQ0AlAKIAZZwDUAggDkAws5SpGAARtCk7BISQA"
+    reference = "https://nmonesource.com/nmos/nmsa/en/item/4340/index.do#!fragment/zoupio-_Toc140503680/BQCwhgziBcwMYgK4DsDWszIQewE4BUBTADwBdoAvbRABwEtsBaAfX2zgEYAWABgFYeAZgBsADh4BKADTJspQhACKiQrgCe0AOSapEQmFwJlqjdt37DIAMp5SAIQ0AlAKIAZZwDUAggDkAws5SpGAARtCk7BISQA"
     defined_for = StateCode.NM
 
     def formula(tax_unit, period, parameters):
         person = tax_unit.members
-        p = parameters(period).gov.states.nm.tax.income.credits.unreimbursed_medical_expense
+        p = parameters(period).gov.states.nm.tax.income.exemptions.unreimbursed_medical_expense
         age = person("age", period)
         medical_exepenses = tax_unit("medical_out_of_pocket_expenses", period)
         age_eligible = tax_unit.any(age >= p.age_eligibility)
