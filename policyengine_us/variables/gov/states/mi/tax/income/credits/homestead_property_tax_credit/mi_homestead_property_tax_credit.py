4684da19786445a4c3dd0b90d31b9e05c08853d4
Matty <31402450+Mshuning@users.noreply.github.com>
2023-07-24 05:09:29 +0000

    debug

--- policyengine_us/variables/gov/states/mi/tax/income/credits/homestead_property_tax_credit/mi_homestead_property_tax_credit.py
+++ policyengine_us/variables/gov/states/mi/tax/income/credits/homestead_property_tax_credit/mi_homestead_property_tax_credit.py
@@ -10,13 +10,45 @@ class mi_homestead_property_tax_credit(Variable):
     defined_for = StateCode.MI
 
     def formula(tax_unit, period, parameters):
-        person = tax_unit.members
         p = parameters(
             period
         ).gov.states.mi.tax.income.credits.homestead_property_tax_credit
-        total_household_resources = tax_unit("mi_household_resources", period)
-        rent = tax_unit("rents", period)
-        property_value = tax_unit, sum("assessed_property_value", period)
-        eligibility = rent > 0 & property_value < p.max_property_value
-        percentage = p.percentage.calc(total_household_resources)
-        return eligibility * (p.max_amount * percentage)
+
+        thr = tax_unit("mi_household_resources", period)
+
+        # disabled
+        disabled_people = add(tax_unit, period, ["is_disabled"])
+        nf_percentage = where(
+            disabled_people > 0,
+            p.disabled.not_refundable_percentage.calc(thr),
+            p.not_refundable_percentage,
+        )
+
+        # seniors
+        age_older = tax_unit("age_head", period)
+        nf_percentage = where(
+            age_older >= p.senior.min_age,
+            p.senior.not_refundable_percentage.calc(thr),
+            p.not_refundable_percentage,
+        )
+        po_percentage = where(
+            age_older >= p.senior.min_age,
+            p.senior.phase_out_percentage.calc(thr),
+            p.phase_out_percentage.calc(thr),
+        )
+
+        property_value = add(tax_unit, period, ["assessed_property_value"])
+        rents = add(tax_unit, period, ["rent"])
+        eligibility = where(
+            rents > 0,
+            (rents * p.rent_percentage > thr * nf_percentage),
+            (property_value > thr * nf_percentage)
+            & (property_value < p.max_property_value),
+        )
+        difference = where(
+            rents > 0,
+            rents * p.rent_percentage - thr * nf_percentage,
+            property_value - thr * nf_percentage,
+        )
+
+        return min_(eligibility * difference * po_percentage, p.max_amount)
