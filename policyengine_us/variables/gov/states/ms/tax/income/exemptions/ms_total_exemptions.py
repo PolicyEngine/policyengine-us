639fcc8286f1b2b14fe7bd88745c306f6237b3f7
ZiqianCui <zc2633@columbia.edu>
2023-07-25 09:52:37 -0700

    minor fixes

--- policyengine_us/variables/gov/states/ms/tax/income/exemptions/ms_total_exemptions.py
+++ policyengine_us/variables/gov/states/ms/tax/income/exemptions/ms_total_exemptions.py
@@ -8,6 +8,7 @@ class ms_total_exemptions(Variable):
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.MS
+    reference = "https://www.dor.ms.gov/sites/default/files/Forms/Individual/80100221.pdf#page=6"
 
     adds = [
         "ms_regular_exemption",
