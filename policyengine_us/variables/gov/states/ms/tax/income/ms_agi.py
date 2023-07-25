9d36f70f4c74d1859a605f02569c003a3ce7b242
ZiqianCui <zc2633@columbia.edu>
2023-07-25 10:53:12 -0700

    update reference

--- policyengine_us/variables/gov/states/ms/tax/income/ms_agi.py
+++ policyengine_us/variables/gov/states/ms/tax/income/ms_agi.py
@@ -7,7 +7,7 @@ class ca_agi(Variable):
     label = "Mississippi AGI"
     unit = USD
     definition_period = YEAR
-    reference = "https://www.dor.ms.gov/sites/default/files/Forms/Individual/80100221.pdf#15"
+    reference = "https://www.dor.ms.gov/sites/default/files/Forms/Individual/80100221.pdf#page=15"
     defined_for = StateCode.MS
     adds = ["adjusted_gross_income", "ms_agi_additions"]
     subtracts = ["ms_agi_subtractions"]
