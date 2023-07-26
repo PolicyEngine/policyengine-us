546e8a6a6bec121c17838eca19c0c04f1ab4cc87
XZ-mabel <131919825+XZ-mabel@users.noreply.github.com>
2023-07-26 14:21:46 -0400

    Update policyengine_us/variables/gov/states/la/tax/credits/quality_rating_of_child_care_facility.py
    Co-authored-by: Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/states/la/tax/credits/quality_rating_of_child_care_facility.py
+++ policyengine_us/variables/gov/states/la/tax/credits/quality_rating_of_child_care_facility.py
@@ -3,7 +3,7 @@ from policyengine_us.model_api import *
 
 class quality_rating_of_child_care_facility(Variable):
     value_type = int
-    entity = TaxUnit
+    entity = Person
     label = "Quality rating of child care facility"
     definition_period = YEAR
     reference = "https://www.revenue.louisiana.gov/IndividualIncomeTax/SchoolReadinessTaxCredit"
