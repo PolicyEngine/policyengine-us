4d080b3dd6b12d4ccc6585115eaf67e627b4906b
XZ-mabel <131919825+XZ-mabel@users.noreply.github.com>
2023-07-26 14:20:42 -0400

    Update policyengine_us/variables/gov/states/la/tax/credits/la_child_expense_tax_credit.py
    Co-authored-by: Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/states/la/tax/credits/la_child_expense_tax_credit.py
+++ policyengine_us/variables/gov/states/la/tax/credits/la_child_expense_tax_credit.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class la_child_expense_tax_credit(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Louisiana non refundable Child Expense Tax credit"
+    label = "Louisiana non-refundable Child Expense Tax Credit"
     unit = USD
     definition_period = YEAR
     reference = "https://www.revenue.louisiana.gov/IndividualIncomeTax/SchoolReadinessTaxCredit"
