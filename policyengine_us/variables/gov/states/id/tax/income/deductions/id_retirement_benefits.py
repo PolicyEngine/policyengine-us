8dc9bf3527179ba477eaa6700581719b8de3b880
LeqiGogo <100904908+LeqiGogo@users.noreply.github.com>
2023-07-26 17:04:20 +0000

    Idaho Retirement Benefit Deduction Fixes #2648

---
+++ policyengine_us/variables/gov/states/id/tax/income/deductions/id_retirement_benefits.py
@@ -0,0 +1,41 @@
+from policyengine_us.model_api import *
+
+
+class id_retirement_benefits(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Idaho retirement benefits"
+    unit = USD
+    documentation = "https://tax.idaho.gov/wp-content/uploads/forms/EFO00088/EFO00088_12-30-2022.pdf"
+    definition_period = YEAR
+    defined_for = StateCode.ID
+
+    def formula(tax_unit, period, parameters):
+        person = tax_unit.members
+        head = person("is_tax_unit_head", period)
+        p = parameters(period).gov.states.id.tax.income.deductions.retirement_benefits
+        filing_status = tax_unit("filing_status", period)
+        
+        # check if filer is eligible for the deduction
+        age_threshold = p.age_eligibility
+        age_threshold_disabled = p.age_eligibility_disabled
+        disabled_head = tax_unit("disabled_head", period)
+        is_eligible = where(disabled_head == False, 
+                            tax_unit("age_head", period) >= age_threshold, 
+                            tax_unit("age_head", period) >= age_threshold_disabled)
+
+        if is_eligible == False:
+            retire_ded_amt = 0
+        else:
+            # Base retirement benefits deduction amount
+            base_amt = p.amount[filing_status]
+            # Social Security benefits received amount
+            ss_amt = head("social_security_retirement", period)
+            # Base amount minus social Security benefits received amount
+            ded_amt = max(base_amt - ss_amt, 0)
+            # Qualified retirement benefits included in federal income
+            fi_amt = head("taxable_pension_income", period) + head("military_retirement_pay", period)
+            # The smaller one
+            retire_ded_amt = min(ded_amt, fi_amt)
+
+        return retire_ded_amt
