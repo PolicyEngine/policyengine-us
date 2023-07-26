8a9d3f572c5922f037add036b5dd4a32f82ca988
PavelMakarchuk <pavel.ma99@gmail.com>
2023-07-26 21:32:17 +0200

    var PR

--- policyengine_us/variables/gov/states/nm/tax/income/nm_subtractions.py
+++ policyengine_us/variables/gov/states/nm/tax/income/nm_subtractions.py
@@ -9,10 +9,4 @@ class nm_subtractions(Variable):
     definition_period = YEAR
     defined_for = StateCode.NM
 
-    adds = [
-        "tax_exempt_interest_income",  # Line 6
-        "dividend_income",  # Line 6
-        "us_govt_interest",  # Line 8
-        "investment_in_529_plan",  # Line 14
-        "military_service_income",  # Line 16, Line 19
-    ]
+    adds = "gov.states.nm.tax.income.subtractions"
