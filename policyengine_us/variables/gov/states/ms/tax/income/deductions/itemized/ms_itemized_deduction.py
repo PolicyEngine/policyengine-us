dda51ba4178fdd514aae44048feb64a695c4a91c
Xinyi-Shao <ellashao99@gmail.com>
2023-07-26 13:51:27 -0400

    updated

--- policyengine_us/variables/gov/states/ms/tax/income/deductions/itemized/ms_itemized_deduction.py
+++ policyengine_us/variables/gov/states/ms/tax/income/deductions/itemized/ms_itemized_deduction.py
@@ -9,7 +9,7 @@ class ms_itemized_deduction(Variable):
     definition_period = YEAR
 
     reference = (
-        "https://www.dor.ms.gov/sites/default/files/Forms/Individual/80100221.pdf"
+        "https://www.dor.ms.gov/sites/default/files/Forms/Individual/80100221.pdf#page=15"
         "https://www.dor.ms.gov/sites/default/files/Forms/Individual/80108228.pdf"
     )
     defined_for = StateCode.MS
@@ -24,20 +24,14 @@ class ms_itemized_deduction(Variable):
         ]
         itm_deds_less_salt = add(tax_unit, period, itm_deds)
 
-        filing_status = tax_unit("filing_status", period)
-
         # calculate itemized deductions total amount
         exempt_deds = add(
             tax_unit,
             period,
             [
-                "medical_expense_deduction",
-                "casualty_loss_deduction",
                 "itemized_taxable_income_deductions",
-                "interest_deduction",
-                "charitable_contribution",
                 "misc_deduction",
             ],
         )
 
-        return max_(0, exempt_deds)
+        return exempt_deds + itm_deds_less_salt
