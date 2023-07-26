06d208bc0d5f64bee21e59255408eeccbabf802e
XZ-mabel <131919825+XZ-mabel@users.noreply.github.com>
2023-07-26 14:21:25 -0400

    Update policyengine_us/variables/gov/states/la/tax/credits/quality_rating_of_child_care_facility.py
    Co-authored-by: Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/states/la/tax/credits/quality_rating_of_child_care_facility.py
+++ policyengine_us/variables/gov/states/la/tax/credits/quality_rating_of_child_care_facility.py
@@ -2,7 +2,7 @@ from policyengine_us.model_api import *
 
 
 class quality_rating_of_child_care_facility(Variable):
-    value_type = float
+    value_type = int
     entity = TaxUnit
     label = "Quality rating of child care facility"
     definition_period = YEAR
