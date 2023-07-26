72ed8e760ae6daf7c1b4972ab098701fef7d934b
Naiqiao Ye <104268340+yedeer12@users.noreply.github.com>
2023-07-26 02:47:40 +0000

    change name of variable

--- policyengine_us/variables/gov/states/wv/tax/income/subtractions/wv_public_pension_subtraction.py
+++ policyengine_us/variables/gov/states/wv/tax/income/subtractions/wv_public_pension_subtraction.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class wv_public_pension_subtraction(Variable):
     value_type = float
     entity = TaxUnit
-    label = "West Virginia other retirement subtraction"
+    label = "West Virginia public pension subtraction"
     defined_for = StateCode.WV
     unit = USD
     definition_period = YEAR
@@ -12,8 +12,10 @@ class wv_public_pension_subtraction(Variable):
 
     def formula(tax_unit, period, parameters):
         person = tax_unit.members
-        public_pension_income = person("taxable_public_pension_income", period)
+        public_pension_income = person(
+            "wv_taxable_public_pension_income", period
+        )
         p = parameters(
             period
         ).gov.states.wv.tax.income.subtractions.public_pension
-        return min_(pension_income, p.max_amount)
+        return min_(public_pension_income, p.max_amount)
--- policyengine_us/variables/gov/states/wv/tax/income/wv_taxable_public_pension_income.py
+++ policyengine_us/variables/gov/states/wv/tax/income/wv_taxable_public_pension_income.py
@@ -1,10 +1,10 @@
 from policyengine_us.model_api import *
 
 
-class wv_pension_income(Variable):
+class wv_taxable_public_pension_income(Variable):
     value_type = float
     entity = TaxUnit
-    label = "West Virginia public pension income"
+    label = "West Virginia taxable public pension income"
     unit = USD
     definition_period = YEAR
     reference = "https://code.wvlegislature.gov/11-21-12/"
