12961e6cdd4b92a4603ed41a92c02e7409bc1d5f
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2023-07-27 20:49:58 +0100

    Ensure `household_state_income_tax` respects reported state tax parameter Fixes #2720

--- policyengine_us/variables/household/income/household/household_state_income_tax.py
+++ policyengine_us/variables/household/income/household/household_state_income_tax.py
@@ -52,3 +52,20 @@ class household_state_income_tax(Variable):
         "ut_refundable_credits",  # Utah.
         "wi_refundable_credits",  # Wisconsin.
     ]
+
+    def formula(tax_unit, period, parameters):
+        if parameters(period).simulation.reported_state_income_tax:
+            spm_unit = tax_unit.spm_unit
+            person = spm_unit.members
+            is_head = person("is_tax_unit_head", period)
+            total_tax_unit_heads = spm_unit.sum(is_head)
+            spm_unit_state_tax = spm_unit(
+                "spm_unit_state_tax_reported", period
+            )
+            return where(
+                total_tax_unit_heads > 0,
+                spm_unit_state_tax / total_tax_unit_heads,
+                0,
+            )
+        else:
+            return add(tax_unit, period, household_state_income_tax.adds)
