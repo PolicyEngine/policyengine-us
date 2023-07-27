7ff45cba07c53687495671d945f0025fa1f58bb3
PavelMakarchuk <pavel.ma99@gmail.com>
2023-07-27 05:53:26 +0200

    typo

--- policyengine_us/variables/gov/states/az/tax/income/az_income_tax_before_non_refundable_credits.py
+++ policyengine_us/variables/gov/states/az/tax/income/az_income_tax_before_non_refundable_credits.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class az_income_tax_before_non_refundable_credits(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Arizona income tax non-refundable before credits"
+    label = "Arizona income tax before non-refundable credits"
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.AZ
