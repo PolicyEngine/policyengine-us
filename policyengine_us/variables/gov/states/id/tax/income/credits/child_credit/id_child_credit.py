1db7d3d534bfdf1aeb283ff1069f7da4709f43b2
LeqiGogo <100904908+LeqiGogo@users.noreply.github.com>
2023-07-28 17:20:32 +0000

    Idaho child tax credit Fixes #2292

--- policyengine_us/variables/gov/states/id/tax/income/credits/child_credit/id_child_credit.py
+++ policyengine_us/variables/gov/states/id/tax/income/credits/child_credit/id_child_credit.py
@@ -12,7 +12,16 @@ class id_child_credit(Variable):
     def formula(tax_unit, period, parameters):
         # Get relevant parameter subtree.
         p = parameters(period).gov.states.id.tax.income.credits.child_credit
+        # check if child is eligible
+        eligible_age = p.age_eligibility
+        person = tax_unit.members
+        print(person("age", period))
         # Count number of eligible children in the tax unit.
-        count_eligible = tax_unit("ctc_qualifying_children", period)
+        count_eligible = sum(
+            [
+                age <= eligible_age
+                for age in person("age", period)
+            ])
+        #count_eligible = tax_unit("ctc_qualifying_children", period)
         # Multiply by the amount per child.
         return count_eligible * p.amount
