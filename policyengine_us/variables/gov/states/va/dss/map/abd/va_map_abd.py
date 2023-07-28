fda47730fe095c81c307f668bc93f54dcf87384a
Chiukai-C <127340408+Chiukai-C@users.noreply.github.com>
2023-07-28 17:55:56 +0000

    add VA MAP ABD

--- policyengine_us/variables/gov/states/va/dss/map/abd/va_map_abd.py
+++ policyengine_us/variables/gov/states/va/dss/map/abd/va_map_abd.py
@@ -1,10 +1,10 @@
 from policyengine_us.model_api import *
 
 
-class va_tanf(Variable):
+class va_map_abd(Variable):
     value_type = float
     entity = SPMUnit
-    label = "VA MAP ABD"
+    label = "VA MAP ABD benefits"
     unit = USD
     definition_period = YEAR
     defined_for = "va_map_abd_eligibility"
