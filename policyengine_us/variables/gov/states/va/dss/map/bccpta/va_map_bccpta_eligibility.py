795163253340bc774e4fb8b537c5b675d321c94a
Chiukai-C <127340408+Chiukai-C@users.noreply.github.com>
2023-07-28 02:34:14 +0000

    add BCCPTA

--- policyengine_us/variables/gov/states/va/dss/map/bccpta/va_map_bccpta_eligibility.py
+++ policyengine_us/variables/gov/states/va/dss/map/bccpta/va_map_bccpta_eligibility.py
@@ -13,6 +13,9 @@ class va_map_bccpta_eligibility(Variable):
         famis = spm_unit("va_map_famis_eligibility", period)
         pregnant = spm_unit("va_map_pregnant_eligibility", period)
         ssi = spm_unit("va_map_ssi_eligiblity", period)
+        abd = spm_unit("va_map_abd_eligibility", period)
+        qi = spm_unit("va_map_mb_qi_eligibility", period)
+        qmb = spm_unit("va_map_mb_qmb_eligibility", period)
+        slmb = spm_unit("va_map_mb_slmb_eligibility", period)
 
-
-        return 
+        return ~(famis | pregnant | ssi | abd | qi | qmb | slmb)
