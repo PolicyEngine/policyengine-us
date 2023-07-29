5c3744f77d5cc8e438a433c8517e68c3070724ab
Shuyu Zhan (Stella) <94806484+shuyu7zhan@users.noreply.github.com>
2023-07-29 16:04:04 +0000

    Rhode Island Property Tax Relief Fixes #2514

--- policyengine_us/variables/gov/states/ri/tax/income/credits/property_tax/ri_property_tax_credit.py
+++ policyengine_us/variables/gov/states/ri/tax/income/credits/property_tax/ri_property_tax_credit.py
@@ -15,21 +15,15 @@ class ri_property_tax_credit(Variable):
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states.ri.tax.income.credits.property_tax
         agi = tax_unit("adjusted_gross_income", period)
-        num_household = tax_unit("tax_unit_size", period)
 
-        person = tax_unit.members
-        direct_property_taxes = tax_unit.sum(
-            person("real_estate_taxes", period)
-        )
-        rent = tax_unit.sum(person("rent", period))
-        paid_property_taxes = (direct_property_taxes + rent) > 0
-
-        base = where(
-            num_household == 1,
+        threshold_agi_percent = where(
+            tax_unit("tax_unit_size", period) == 1,
             p.rate.one_person.calc(agi),
             p.rate.multiple_people.calc(agi),
         )
-        credit_amount = base * agi
-        exceedance = max_(direct_property_taxes + rent - credit_amount, 0)
-        credit = exceedance * paid_property_taxes
-        return min_(credit, p.max_amount)
+        threshold = threshold_agi_percent * agi
+        rent_plus_property_tax = add(
+            tax_unit, period, ["real_estate_taxes", "rent"]
+        )
+        uncapped_credit = max_(rent_plus_property_tax - threshold, 0)
+        return min_(uncapped_credit, p.max_amount)
