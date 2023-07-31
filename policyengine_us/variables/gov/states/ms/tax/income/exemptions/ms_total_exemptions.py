be6da81c3f5e6d01000765fa57d582bf8bae16db
ZiqianCui <zc2633@columbia.edu>
2023-07-31 09:50:41 -0700

    updated comments

--- policyengine_us/variables/gov/states/ms/tax/income/exemptions/ms_total_exemptions.py
+++ policyengine_us/variables/gov/states/ms/tax/income/exemptions/ms_total_exemptions.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class ms_total_exemptions(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Mississippi total exemption allowance"
+    label = "Mississippi total exemptions"
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.MS
