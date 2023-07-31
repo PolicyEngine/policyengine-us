ba372f397603f43065b9088730ebe02c51cb92aa
SirMalamute <neeliyer14@gmail.com>
2023-07-31 19:34:32 +0000

    Indiana Earned Income Credit Fixes #2478

--- policyengine_us/variables/gov/states/in/tax/income/credits/eitc/in_eic_eligible_child.py
+++ policyengine_us/variables/gov/states/in/tax/income/credits/eitc/in_eic_eligible_child.py
@@ -11,8 +11,8 @@ class in_eic_eligible_child(Variable):
 
     def formula(person, period, parameters):
         p = parameters(period).gov.states["in"].tax.income.credits.eic.age
-        is_19 = person("age", period) < p.age_child
-        is_24 = person("age", period) < p.age_full_time_student
+        is_19 = person("age", period) < p.child
+        is_24 = person("age", period) < p.full_time_student
         full_time_student = person("is_full_time_student", period)
         child_disabled = person("is_disabled", period)
         return (is_19 | (is_24 & full_time_student)) | child_disabled
