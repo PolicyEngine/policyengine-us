c17494d6eaffd3445634dda8ae41c11b8e83888e
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2023-08-02 14:42:49 +0100

    Add test

--- policyengine_us/variables/household/income/household/equiv_household_net_income.py
+++ policyengine_us/variables/household/income/household/equiv_household_net_income.py
@@ -9,6 +9,6 @@ class equiv_household_net_income(Variable):
     unit = USD
 
     def formula(household, period, parameters):
-        number_of_people = household.nb_persons()
+        number_of_people = household("household_size", period)
         net_income = household("household_net_income", period)
         return net_income / (number_of_people**0.5)
