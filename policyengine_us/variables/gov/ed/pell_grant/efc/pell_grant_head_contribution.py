c476735e5341bb19e04f407de7921b17b81db7a4
Caleb <caleblpena@gmail.com>
2023-08-02 16:28:33 -0600

    remove if statement

--- policyengine_us/variables/gov/ed/pell_grant/efc/pell_grant_head_contribution.py
+++ policyengine_us/variables/gov/ed/pell_grant/efc/pell_grant_head_contribution.py
@@ -10,9 +10,7 @@ class pell_grant_head_contribution(Variable):
     def formula(person, period, parameters):
         available_income = person("pell_grant_head_available_income", period)
         dependents = person("pell_grant_dependents_in_college", period)
-        formula = person("pell_grant_formula", period)
-        if formula.decode_to_str() == "B":
-            return available_income / dependents
+        formula = person("pell_grant_formula", period).decode_to_str()
         p = parameters(period).gov.ed.pell_grant.efc.head
         base = p.base.calc(available_income)
         additional = p.percent.calc(available_income)
@@ -20,4 +18,8 @@ class pell_grant_head_contribution(Variable):
         total_head_contribution = base + (
             (available_income - threshold) * additional
         )
-        return total_head_contribution / dependents
+        return where(
+            formula == "B",
+            available_income / dependents,
+            total_head_contribution / dependents,
+        )
