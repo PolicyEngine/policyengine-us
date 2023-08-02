bdbe2deea17dadae2c39f5ba55a3d199730a3c18
yy3187 <129118329+yy3187@users.noreply.github.com>
2023-08-02 03:15:11 +0000

    update test case

--- policyengine_us/variables/gov/states/la/tax/income/credits/cdcc/la_cdcc_non_refundable.py
+++ policyengine_us/variables/gov/states/la/tax/income/credits/cdcc/la_cdcc_non_refundable.py
@@ -20,6 +20,6 @@ class la_cdcc_non_refundable(Variable):
         la_non_refundable_cdcc = us_cdcc * p.cdcc.non_refundable.rate.calc(
             us_agi
         )
-        if us_agi > 60000 and la_non_refundable_cdcc > p.cdcc.non_refundable.hi_threshold:
-            la_non_refundable_cdcc = p.cdcc.non_refundable.hi_threshold
+        if us_agi > 60000:
+            la_non_refundable_cdcc = min(p.cdcc.non_refundable.hi_threshold,la_non_refundable_cdcc)
         return agi_eligible * la_non_refundable_cdcc
