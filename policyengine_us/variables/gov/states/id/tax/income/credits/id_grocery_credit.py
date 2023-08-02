5c96eff51ff7ea18568c56b50991bad2193a6bf5
Gene Jung <85149548+universescraper@users.noreply.github.com>
2023-08-02 16:58:21 +0000

    updating for snap eligibility  for id_gc

--- policyengine_us/variables/gov/states/id/tax/income/credits/id_grocery_credit.py
+++ policyengine_us/variables/gov/states/id/tax/income/credits/id_grocery_credit.py
@@ -13,13 +13,13 @@ class id_grocery_credit(Variable):
         p = parameters(period).gov.states.id.tax.income.credits.gc
 
         # 100$ for each dependent
-        person = tax_unit.members
+        person1 = tax_unit.members = p.snap_threshold.Household_Size
+        person = person1("income", period) > p.snap_threshold
         dependent = person(
             "is_tax_unit_dependent", period
         )  # example: [0, 0, 1, 1, 1]
         total_dependents = tax_unit.sum(dependent)  # example sum [3]
         dependet_amount = total_dependents * p.amount_dependent
-
         # 20$ extra for each aged person
         person_aged = person("age", period) > p.age_older_eligibility
         total_aged = tax_unit.sum(person_aged)
