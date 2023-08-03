838aff748160787511fff7018d51bc8badbfa985
phywzc <97962358+phywzc@users.noreply.github.com>
2023-08-03 15:02:48 +0000

    Arizona Itemized Deduction Fixes #2527
    Co-authored-by: Pavel Makarchuk <PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/states/az/tax/income/credits/increased_excise_tax_credit.py
+++ policyengine_us/variables/gov/states/az/tax/income/credits/increased_excise_tax_credit.py
@@ -14,7 +14,7 @@ class az_increased_excise_tax_credit(Variable):
     def formula(tax_unit, period, parameters):
         p = parameters(
             period
-        ).gov.states.az.tax.income.credits.increased_excise_tax_credit
+        ).gov.states.az.tax.income.credits.increased_excise
         agi = tax_unit("adjusted_gross_income", period)
         filing_status = tax_unit("filing_status", period)
         max_income = p.income_threshold[filing_status]
@@ -22,5 +22,5 @@ class az_increased_excise_tax_credit(Variable):
         head_or_spouse_dependents = tax_unit("tax_unit_dependents", period)
         dependents_eligible = p.number_of_qualifying_dependents[filing_status]
         total_dependents = head_or_spouse_dependents + dependents_eligible
-        uncapped_credit = total_dependents * p.credit_based_on_cal_dependents
+        uncapped_credit = total_dependents * p.amount
         return eligible * min_(uncapped_credit, p.max_amount)
