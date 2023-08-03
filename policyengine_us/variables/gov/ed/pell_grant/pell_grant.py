f6f40621adf4753651f2fd7e15f20f94d9c7d3b6
Caleb <caleblpena@gmail.com>
2023-08-03 15:42:26 -0600

    don't reuse python variables

--- policyengine_us/variables/gov/ed/pell_grant/pell_grant.py
+++ policyengine_us/variables/gov/ed/pell_grant/pell_grant.py
@@ -14,8 +14,7 @@ class pell_grant(Variable):
         months_in_school = person("pell_grant_months_in_school", period)
         efc = person("pell_grant_efc", period)
         p = parameters(period).gov.ed.pell_grant
-        minimum = p.min_grant
-        maximum = p.max_grant
-        amount = min_(coa - efc, maximum)
-        amount = where(amount < minimum, 0, amount)
+        unbounded = coa - efc
+        capped = min_(unbounded, p.amount.max)
+        amount = where(capped < p.min_grant, 0, capped)
         return amount * (months_in_school / p.months_in_school_year)
