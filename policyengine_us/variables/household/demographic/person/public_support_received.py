d57c77906cf98988961da64b04fd2bb7f47d5ce8
Ruby Ren <38923159+YichunRen@users.noreply.github.com>
2023-08-03 20:21:02 +0000

    corrections

--- policyengine_us/variables/household/demographic/person/public_support_received.py
+++ policyengine_us/variables/household/demographic/person/public_support_received.py
@@ -4,6 +4,5 @@ from policyengine_us.model_api import *
 class public_support_received(Variable):
     value_type = bool
     entity = Person
-    label = "child received public support or not"
+    label = "Child receiving public support"
     definition_period = YEAR
-    defined_for = StateCode.HI
