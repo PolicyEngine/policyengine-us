c83fa0bbc247d68678cc7839bc878ef67bd7bbf9
PavelMakarchuk <pavel.ma99@gmail.com>
2023-08-04 15:58:31 +0200

    final adj.

--- policyengine_us/variables/gov/states/mt/tax/income/deductions/standard/mt_standard_deduction.py
+++ policyengine_us/variables/gov/states/mt/tax/income/deductions/standard/mt_standard_deduction.py
@@ -12,14 +12,13 @@ class mt_standard_deduction(Variable):
     def formula(tax_unit, period, parameters):
         filing_status = tax_unit("filing_status", period)
         itemizing = tax_unit("tax_unit_itemizes", period)
-        standard_eligible = itemizing == [False]
         p = parameters(period).gov.states.mt.tax.income.deductions.standard
         agi = tax_unit("mt_agi", period)
         # standard deduction is a percentage of AGI that
         # is bounded by a min/max by filing status.
         min_amount = p.min[filing_status]
         max_amount = p.max[filing_status]
-        unbounded = p.rate * agi
-        deduction_amount = min_(unbounded, max_amount)
+        uncapped_amount = p.rate * agi
+        deduction_amount = min_(uncapped_amount, max_amount)
 
-        return standard_eligible * max_(deduction_amount, min_amount)
+        return ~itemizing * max_(deduction_amount, min_amount)
