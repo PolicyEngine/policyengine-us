0b5330f3856cf3a1c8f6ee3973d87537efd01655
Patricia-1998 <126123651+Patricia-1998@users.noreply.github.com>
2023-08-04 19:42:48 +0000

    modified the code

--- policyengine_us/variables/gov/states/oh/tax/income/credits/oh_exemption_credit.py
+++ policyengine_us/variables/gov/states/oh/tax/income/credits/oh_exemption_credit.py
@@ -7,13 +7,13 @@ class oh_exemption_credit(Variable):
     label = "Ohio Exemption Credit"
     unit = USD
     definition_period = YEAR
-    reference = ("https://codes.ohio.gov/ohio-revised-code/section-5747.022")
+    reference = "https://codes.ohio.gov/ohio-revised-code/section-5747.022"
     defined_for = StateCode.OH
 
     def formula(tax_unit, period, parameters):
         agi = tax_unit("oh_agi", period)
         exemptions = tax_unit("exemptions", period)
-        p = parameters(period).gov.states.oh.tax.income.credits.exemption
+        p = parameters(period).gov.states.oh.tax.income.exemptions
         credit_amount = exemptions * p.amount
         modified_agi = agi - credit_amount
         agi_eligible = modified_agi < p.income_threshold
--- policyengine_us/variables/gov/states/oh/tax/income/exemptions/oh_personal_exemption.py
+++ policyengine_us/variables/gov/states/oh/tax/income/exemptions/oh_personal_exemption.py
@@ -8,17 +8,15 @@ class oh_personal_exemption(Variable):
     unit = USD
     definition_period = YEAR
     reference = (
-        "https://tax.ohio.gov/static/forms/ohio_individual/individual/2021/pit-it1040-booklet.pdf#page=20",
+        "https://tax.ohio.gov/static/forms/ohio_individual/individual/2021/pit-it1040-booklet.pdf#page=14",
     )
     defined_for = StateCode.OH
 
     def formula(tax_unit, period, parameters):
-        head_and_spouse = tax_unit("head_spouse_count", period)
-        dependents = tax_unit("tax_unit_dependents", period)
-        total_exemptions = head_and_spouse + dependents
+        total_exemptions = tax_unit("tax_unit_size", period)
         dependent_on_another_return = tax_unit("dsi", period)
         agi = tax_unit("oh_agi", period)
-        p = parameters(period).gov.states.oh.exemptions.personal
+        p = parameters(period).gov.states.oh.tax.income.exemptions.personal
         credit_amount = p.amount.calc(agi)
         total_credit = total_exemptions * credit_amount
         return ~dependent_on_another_return * total_credit
