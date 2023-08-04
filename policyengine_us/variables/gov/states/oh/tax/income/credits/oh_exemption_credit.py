0c669af568545c598075e943fcd83ea1ca08b03d
Patricia-1998 <126123651+Patricia-1998@users.noreply.github.com>
2023-08-04 18:45:35 +0000

    Co-authored-by: itsmexw <itsmexw@users.noreply.github.com>

--- policyengine_us/variables/gov/states/oh/tax/income/credits/oh_exemption_credit.py
+++ policyengine_us/variables/gov/states/oh/tax/income/credits/oh_exemption_credit.py
@@ -7,15 +7,15 @@ class oh_exemption_credit(Variable):
     label = "Ohio Exemption Credit"
     unit = USD
     definition_period = YEAR
-    reference = (
-        "https://tax.ohio.gov/static/forms/ohio_individual/individual/2021/pit-it1040-booklet.pdf#page=20",
-    )
+    reference = ("https://codes.ohio.gov/ohio-revised-code/section-5747.022")
     defined_for = StateCode.OH
 
     def formula(tax_unit, period, parameters):
         agi = tax_unit("oh_agi", period)
         exemptions = tax_unit("exemptions", period)
-        p = parameters(period).gov.states.oh.tax.income.credits
-        agi_eligible = agi < p.exemption.income_threshold
-        credit_amount = exemptions * p.exemption.amount
+        p = parameters(period).gov.states.oh.tax.income.credits.exemption
+        credit_amount = exemptions * p.amount
+        modified_agi = agi - credit_amount
+        agi_eligible = modified_agi < p.income_threshold
+
         return credit_amount * agi_eligible
