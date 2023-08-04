e4086a75664d553cbae0276ca20b33ba0ed8fbd6
yy3187 <129118329+yy3187@users.noreply.github.com>
2023-08-04 18:06:11 +0000

    add parameter

--- policyengine_us/variables/gov/states/la/tax/income/deductions/la_itemized_deductions.py
+++ policyengine_us/variables/gov/states/la/tax/income/deductions/la_itemized_deductions.py
@@ -9,13 +9,18 @@ class la_itemized_deductions(Variable):
     definition_period = YEAR
     reference = [
         "https://revenue.louisiana.gov/TaxForms/IT540iWEB(2022)D1.pdf",
-        "https://www.legis.la.gov/Legis/Law.aspx?d=101760",
+        "https://www.legis.la.gov/Legis/Law.aspx?d=101760",  # (3)
     ]
     defined_for = StateCode.LA
 
     def formula(tax_unit, period, parameters):
+        p = parameters(period).gov.states.la.tax.income.deductions.itemized
         itemizes = tax_unit("tax_unit_itemizes", period)
-        medical_expenses = tax_unit("medical_expense_deduction", period)
-        return itemizes * max_(
-            medical_expenses - tax_unit("standard_deduction", period), 0
+        medical_expenses = add(tax_unit, period, ["medical_expense"])
+        return (
+            itemizes
+            * max_(
+                medical_expenses - tax_unit("standard_deduction", period), 0
+            )
+            * p.exceedance
         )
