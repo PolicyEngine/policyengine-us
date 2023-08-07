7948f1ac183e430f1eb37e55ed30b4f5d503afd3
JudyZ13 <112495414+JudyZ13@users.noreply.github.com>
2023-08-07 21:30:22 +0000

    senior

--- policyengine_us/variables/gov/states/az/tax/income/exemptions/az_senior_exemption.py
+++ policyengine_us/variables/gov/states/az/tax/income/exemptions/az_senior_exemption.py
@@ -10,11 +10,10 @@ class az_senior_exemption(Variable):
     defined_for = StateCode.AZ
 
     def formula(tax_unit, period, parameters):
-        p = parameters(period).gov.states.az.tax.income.exemptions
+        p = parameters(period).gov.states.az.tax.income.exemptions.senior
 
-        payment_eligible = (
-            tax_unit("care_and_support_payment", period) > p.min_payment
-        )
+        payment_eligible = (tax_unit("care_and_support_payment", period) > p.min_payment) || 
+        (tax_unit("care_and_support_payment", period) > tax_unit("care_and_support_costs", period) * p.cost_rate)
         eligibility = payment_eligible.astype(int)
 
-        return p.amount.senior * eligibility
+        return p.amount * eligibility
