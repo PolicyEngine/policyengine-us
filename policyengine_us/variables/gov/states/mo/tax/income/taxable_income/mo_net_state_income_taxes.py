e4f617f1f974bb8b3f21053ae9e35e360cda6a70
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-08-07 17:41:24 -0400

    Avoid negative state income tax in itemized deduction calculations

--- policyengine_us/variables/gov/states/mo/tax/income/taxable_income/mo_net_state_income_taxes.py
+++ policyengine_us/variables/gov/states/mo/tax/income/taxable_income/mo_net_state_income_taxes.py
@@ -20,12 +20,12 @@ class mo_net_state_income_taxes(Variable):
         p = parameters(period).gov.irs.deductions.itemized
         salt_cap = p.salt_and_real_estate.cap[filing_status]
 
-        uncapped_itax = add(tax_unit, period, ["state_income_tax"])
+        uncapped_itax = max_(0, add(tax_unit, period, ["state_income_tax"]))
         uncapped_ptax = add(tax_unit, period, ["real_estate_taxes"])
         uncapped_salt = uncapped_itax + uncapped_ptax
 
         ratio = np.zeros_like(uncapped_salt)
-        mask = uncapped_salt > 0
+        mask = uncapped_salt != 0
         ratio[mask] = uncapped_itax[mask] / uncapped_salt[mask]
 
         return where(uncapped_salt > salt_cap, salt_cap * ratio, uncapped_itax)
