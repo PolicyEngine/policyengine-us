c721f49f9fe220732f637c814bfd5ba9220a5a67
JudyZ13 <112495414+JudyZ13@users.noreply.github.com>
2023-08-07 16:11:19 -0500

    Update policyengine_us/variables/household/demographic/tax_unit/cohabitating_grandparent.py
    Co-authored-by: Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/household/demographic/tax_unit/cohabitating_grandparent.py
+++ policyengine_us/variables/household/demographic/tax_unit/cohabitating_grandparent.py
@@ -1,7 +1,7 @@
 from policyengine_us.model_api import *
 
 
-class cohabitating_parent(Variable):
+class cohabitating_grandparent(Variable):
     value_type = bool
     entity = TaxUnit
     definition_period = YEAR
