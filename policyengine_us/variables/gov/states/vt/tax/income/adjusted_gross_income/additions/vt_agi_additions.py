faeefa0027124ead6d5c62e4c436d5528c9807aa
Zixuan Ma <66671734+mzx-4real@users.noreply.github.com>
2023-08-07 20:46:04 +0800

    Suggested change without interest and dividend add back

--- policyengine_us/variables/gov/states/vt/tax/income/adjusted_gross_income/additions/vt_agi_additions.py
+++ policyengine_us/variables/gov/states/vt/tax/income/adjusted_gross_income/additions/vt_agi_additions.py
@@ -9,18 +9,7 @@ class vt_agi_additions(Variable):
     documentation = "Additions to VT AGI over federal AGI."
     definition_period = YEAR
     defined_for = StateCode.VT
-    reference = [
-        {
-            "title": "2022 Schedule IN-112 Vermont Tax Adjustments and Credits, PART 1 ADDITIONS TO FEDERAL ADJUSTED GROSS INCOME",
-            "href": "https://tax.vermont.gov/sites/tax/files/documents/IN-112-2022.pdf",
-        },
-        {
-            "title": "Legal Code Titl. 32 V.S.A. § 5811(21)(A)(i)(ii)",
-            "href": "https://tax.vermont.gov/sites/tax/files/documents/IN-112-2022.pdf",
-        },
-    ]
-
-    adds = [
-        "vt_interest_and_dividend_add_back",
-        "vt_bonus_depreciation_add_back",
-    ]
+    reference = (
+        "https://tax.vermont.gov/sites/tax/files/documents/IN-112-2022.pdf"
+        "https://tax.vermont.gov/sites/tax/files/documents/IN-112-2022.pdf"
+    )
--- policyengine_us/variables/gov/states/vt/tax/income/adjusted_gross_income/additions/vt_interest_and_dividend_add_back.py
+++ policyengine_us/variables/gov/states/vt/tax/income/adjusted_gross_income/additions/vt_interest_and_dividend_add_back.py
@@ -8,20 +8,11 @@ class vt_interest_and_dividend_add_back(Variable):
     definition_period = YEAR
     defined_for = StateCode.VT
     documentation = "Part of additions to VT AGI over federal AGI. The amount equals to interest and dividend income from non-Vermont state and local obligations which are exempted from federal taxable income are taxable in Vermont."
-    reference = [
-        {
-            "title": "2022 Schedule IN-112 Vermont Tax Adjustments and Credits, PART 1 ADDITIONS TO FEDERAL ADJUSTED GROSS INCOME, Line3",
-            "href": "https://tax.vermont.gov/sites/tax/files/documents/IN-112-2022.pdf",
-        },
-        {
-            "title": "Instruction for SCHEDULE IN-112 Vermont Tax Adjustments and Credits, Line3",
-            "href": "https://tax.vermont.gov/sites/tax/files/documents/IN-112%20Instr-2022.pdf",
-        },
-        {
-            "title": "Legal Code Titl. 32 V.S.A. § 5811(21)(A)(i)",
-            "href": "https://tax.vermont.gov/sites/tax/files/documents/IN-112-2022.pdf",
-        },
-    ]
+    reference = (
+        "https://tax.vermont.gov/sites/tax/files/documents/IN-112-2022.pdf"
+        "https://tax.vermont.gov/sites/tax/files/documents/IN-112%20Instr-2022.pdf"
+        "https://tax.vermont.gov/sites/tax/files/documents/IN-112-2022.pdf"
+    )
 
     def formula(tax_unit, period, parameters):
         # First get the total interest and dividend income received from all state and local obligations exempted from federal tax
