c48a68b3b5194aba8a2b61369c971547f6367678
qingqiuzhang <90161322+qingqiuzhang@users.noreply.github.com>
2023-08-08 01:24:07 +0000

    update references

--- policyengine_us/variables/gov/states/oh/tax/income/credits/oh_adoption_credit.py
+++ policyengine_us/variables/gov/states/oh/tax/income/credits/oh_adoption_credit.py
@@ -8,10 +8,13 @@ class oh_adoption_credit(Variable):
     unit = USD
     definition_period = YEAR
     reference = (
-        # Ohio 2021 Instructions for Filing Original and Amended - Line 17 – Ohio Adoption Credit
-        "https://tax.ohio.gov/static/forms/ohio_individual/individual/2021/pit-it1040-booklet.pdf#page=21",
+        # Ohio 2022 Instructions for Filing Original and Amended - Line 18 – Ohio Adoption Credit
+        "https://tax.ohio.gov/static/forms/ohio_individual/individual/2022/it1040-sd100-instruction-booklet.pdf#page=21",
         # Ohio Income - Individual Credits (Education, Displaced Workers & Adoption)
         "https://tax.ohio.gov/wps/portal/gov/tax/help-center/faqs/income+-+individual+credits/income-individual-credits",
+        # The ohio adoption credit has been repealed in the newest revised code, but can still be found in the previous legal code and 2022 tax form
+        # 2022 Ohio Revised Code Title 57 | Taxation Chapter 5747 | Income Tax Section 5747.37 | Credit for Legally Adopted Minor Child.
+        "https://law.justia.com/codes/ohio/2022/title-57/chapter-5747/section-5747-37/",
     )
     defined_for = StateCode.OH
 
