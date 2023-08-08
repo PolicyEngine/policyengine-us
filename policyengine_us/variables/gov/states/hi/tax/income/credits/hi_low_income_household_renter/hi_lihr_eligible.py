b30901e109db2c7d96dec810bbb208cb1ae627ec
xianxinzhu <97819588+xianxinzhu@users.noreply.github.com>
2023-08-08 03:32:58 +0000

    Create test and variable files for Hawaii low-income household renters tax credit.

---
+++ policyengine_us/variables/gov/states/hi/tax/income/credits/hi_low_income_household_renter/hi_lihr_eligible.py
@@ -0,0 +1,12 @@
+from policyengine_us.model_api import *
+
+
+class hi_lihr_eligible(Variable):
+    #value_type = 
+    #entity = TaxUnit
+    #label = 
+    #definition_period = YEAR
+
+    def formula(tax_unit, period, parameters):
+        
+        return None
