8458081ccb4596eeece0e4389fbd9434701285d3
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2023-08-08 13:28:52 +0000

    Updating CT EITC formula with adjustments in test cases

--- policyengine_us/variables/gov/states/ct/tax/income/credits/ct_eitc.py
+++ policyengine_us/variables/gov/states/ct/tax/income/credits/ct_eitc.py
@@ -12,15 +12,14 @@ class ct_eitc(Variable):
 
     def formula(tax_unit, period, parameters):
         eitc = tax_unit("earned_income_tax_credit", period)
-        rate = parameters(
-            period
-        ).gov.states.ct.tax.income.credits.eitc.rate
+        rate = parameters(period).gov.states.ct.tax.income.credits.eitc.rate
         filing_status = tax_unit("filing_status", period)
         filing_statuses = filing_status.possible_values
-        separate_status = (filing_status == filing_statuses.SINGLE)
-        agi_separate = tax_unit("adjusted_gross_income", period)
-        #how do differentiate the two AGI's?
-        agi_joint = None
-        if separate_status:
-            eitc * rate * (agi_single / agi_joint)
-        return eitc * rate
+        separate_status = filing_status == filing_statuses.SEPARATE
+        agi_separate = tax_unit("adjusted_gross_income_person", period)
+        agi_joint = tax_unit("adjusted_gross_income", period)
+        return where(
+            separate_status,
+            eitc * rate * (agi_separate / agi_joint),
+            eitc * rate,
+        )
