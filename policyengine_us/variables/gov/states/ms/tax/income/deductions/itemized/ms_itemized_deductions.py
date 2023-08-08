0e7faf15db95c025417f63a905a2127e64df59cb
Xinyi-Shao <ellashao99@gmail.com>
2023-08-08 14:29:44 -0400

    updated

--- policyengine_us/variables/gov/states/ms/tax/income/deductions/itemized/ms_itemized_deductions.py
+++ policyengine_us/variables/gov/states/ms/tax/income/deductions/itemized/ms_itemized_deductions.py
@@ -12,7 +12,7 @@ class ms_itemized_deductions(Variable):
         "https://www.dor.ms.gov/sites/default/files/Forms/Individual/80100221.pdf#page=15"
         "https://www.dor.ms.gov/sites/default/files/Forms/Individual/80108228.pdf"
         "https://advance.lexis.com/documentpage/teaserdocument/?pdmfid=1000516&crid=9f552d68-042d-4ab6-b45d-4b5ec25d1742&config=00JABhZDIzMTViZS04NjcxLTQ1MDItOTllOS03MDg0ZTQxYzU4ZTQKAFBvZENhdGFsb2f8inKxYiqNVSihJeNKRlUp&pddocfullpath=%2fshared%2fdocument%2fstatutes-legislation%2furn%3acontentItem%3a659V-W3G3-CGX8-0009-00008-00&pddocid=urn%3acontentItem%3a659V-W3G3-CGX8-0009-00008-00&pdcontentcomponentid=234190&pdteaserkey=h3&pditab=allpods&ecomp=7s65kkk&earg=sr0&prid=a60fa5e9-9e96-4e4a-88be-5db3d101cc92"
-    ) 
+    )
     # Variable modeled after Tax Form: Schedule A
     defined_for = StateCode.MS
 
