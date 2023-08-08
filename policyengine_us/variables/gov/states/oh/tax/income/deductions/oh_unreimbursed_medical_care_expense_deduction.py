07f4097f8a85f8635d39b535d593cbc03246aac0
Linfeng-ll <95734090+Linfeng-ll@users.noreply.github.com>
2023-08-08 22:09:26 +0000

    rename files

--- policyengine_us/variables/gov/states/oh/tax/income/deductions/oh_unreimbursed_medical_care_expense_deduction.py
+++ policyengine_us/variables/gov/states/oh/tax/income/deductions/oh_unreimbursed_medical_care_expense_deduction.py
@@ -1,10 +1,10 @@
 from policyengine_us.model_api import *
 
 
-class oh_unreimbursed_medical_care_expenses(Variable):
+class oh_unreimbursed_medical_care_expense_deduction(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Ohio Unreimbursed Medical and Health Care Expenses"
+    label = "Ohio Unreimbursed Medical and Health Care Expense Deduction"
     unit = USD
     definition_period = YEAR
     reference = (
