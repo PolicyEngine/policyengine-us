4879815909461a643b672a274fb9f3cd3a82a02e
phywzc <97962358+phywzc@users.noreply.github.com>
2023-08-08 01:32:08 +0000

    Arizona Itemized Deduction Fixes #2527

--- policyengine_us/variables/gov/states/az/tax/income/deductions/itemized/az_itemized_deduction.py
+++ policyengine_us/variables/gov/states/az/tax/income/deductions/itemized/az_itemized_deduction.py
@@ -1,5 +1,6 @@
 from policyengine_us.model_api import *
 
+
 class az_itemized_deduction(Variable):
     value_type = float
     entity = TaxUnit
@@ -16,7 +17,7 @@ class az_itemized_deduction(Variable):
     defined_for = StateCode.AZ
 
     def formula(tax_unit, period, parameters):
-        us_p = parameters(period).gov.irs.deductions
+        # Adjustment to Medical and Dental Expresses
         medical_expense = add(tax_unit, period, ["medical_expense"])
         medical = parameters(period).gov.irs.deductions.itemized.medical
         medical_expense_irs = medical.floor * tax_unit("positive_agi", period)
@@ -35,43 +36,59 @@ class az_itemized_deduction(Variable):
         # mortgage_interest = the amount of mortgage interest you paid for 2022 that is equal to
         # the amount of federal credit, if you received a federal credit for interest paid on
         # mortgage credit certificates (from federal Form 8396).
+        # adjusrment_intertest_deduction = 0
 
         # Adjustments to Charitable Contributions
         # Amount of charitable contributions for which you are claiming a credit under Arizona law
-        # I assume it is the same as federal law
+        # I assume it is the same as irs after discussing with Pavel
         charitable_deduction = tax_unit("charitable_deduction", period)
 
-        # I assume we claim income taxes on federal Sechedule A.
+        # Adjustment to State Income Taxes
         state_income_tax_before_federal_limitation = tax_unit(
             "state_and_local_sales_or_income_tax", period
         )
         # line2A= Amount included in the line 1A for which you claimed an Arizona credit
+        # line2A=az_total_credit
         az_total_credit = tax_unit("az_total_credit", period)
-        diff = state_income_tax_before_federal_limitation - az_total_credit
-        p = parameters(period).gov.states.az.tax.income.deductions.itemized
+        total_state_income_taxes_unclaimed_for_credit = (
+            state_income_tax_before_federal_limitation - az_total_credit
+        )
         filing_status = tax_unit("filing_status", period)
-        federal_schedule_limit = p.itemized_deduction_limit[filing_status]
-        line5A = min_(diff, federal_schedule_limit)
+        federal_schedule_limit = parameters(
+            period
+        ).gov.irs.deductions.itemized.salt_and_real_estate.cap[filing_status]
+        allowed_state_income_taxes_for_credit = min_(
+            total_state_income_taxes_unclaimed_for_credit,
+            federal_schedule_limit,
+        )
         salt_deduction = tax_unit("salt_deduction", period)
-        adjustment_to_state_income_taxes = salt_deduction - line5A
+        adjustment_to_state_income_taxes = (
+            salt_deduction - allowed_state_income_taxes_for_credit
+        )
 
+        # Other Adjustments
         # This part was marked as not included after meeting
         # other_adjustments = 0
 
-        # adjusted itemized deduction
-        adjustment_medical_mortgage = medical_expense_larger_then_irs_allowed
-        # if we have mortgage interest, we need to add mortgage interest into "adjustment_medical_mortgage".
-        adjustment_medical_charitable_stateTax_other = (
+        # Adjusted Itemized Deduction
+        adjustment_medical_mortgage = medical_expense_larger_then_irs_allowed  # + adjusrment_intertest_deduction
+        # if we have mortgage interest, we need to add "adjusrment_intertest_deduction" into "adjustment_medical_mortgage"
+        adjustment_medical_charitable_stateIncomeTax_other = (
             medical_expense_less_then_irs_allowed
             + charitable_deduction
             + adjustment_to_state_income_taxes
-        )  
-        # If you have other_adjustments, we need to add "other_adjustments" 
-        # into "adjustment_medical_charitable_stateTax_other"
-        az_itemized_deductions_less_salt = tax_unit(
-            "az_itemized_deductions_less_salt", period
+            # + other_adjustments
         )
-        # line12 = adjustment_medical_mortgage
-        line13 = az_itemized_deductions_less_salt + adjustment_medical_mortgage
-        line14 = adjustment_medical_charitable_stateTax_other
-        return max_(line13 - line14, 0)
+        # If you have other_adjustments, we need to add "other_adjustments"
+        # into "adjustment_medical_charitable_stateIncomeTax_other"
+        itemized_deductions_less_salt = tax_unit(
+            "itemized_deductions_less_salt", period
+        )
+        federal_itemized_deduction_with_adjustment_for_medical_and_interest = (
+            itemized_deductions_less_salt + adjustment_medical_mortgage
+        )
+        return max_(
+            federal_itemized_deduction_with_adjustment_for_medical_and_interest
+            - adjustment_medical_charitable_stateIncomeTax_other,
+            0,
+        )
