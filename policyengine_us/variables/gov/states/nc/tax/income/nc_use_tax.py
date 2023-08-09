81b2ce38f2de88757c3c3b553724e6d6b6cee63b
TurnerLuo <97751137+TurnerLuo@users.noreply.github.com>
2023-08-09 16:48:34 +0000

    Add NC use tax variable

---
+++ policyengine_us/variables/gov/states/nc/tax/income/nc_use_tax.py
@@ -0,0 +1,12 @@
+from policyengine_us.model_api import *
+
+class nc_use_tax(variable):
+    value_type = float
+    entity = TaxUnit
+    label = "NC use tax"
+    definition_period = YEAR
+
+    def formula(tax_unit, period, parameters):
+        agi = tax_unit("adjusted_gross_income", period)
+        p = parameters(period).gov.states.nc.tax.use_tax.without_record
+        return p.amount.calc(agi) + (p.rate.calc(agi) * agi)
