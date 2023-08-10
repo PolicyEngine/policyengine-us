b3312a2575b2119726ce35e225a9cbd1c784c7bb
TurnerLuo <97751137+TurnerLuo@users.noreply.github.com>
2023-08-10 20:23:03 +0000

    Adding Tests for NC Use Tax

--- policyengine_us/variables/gov/states/nc/tax/income/nc_use_tax.py
+++ policyengine_us/variables/gov/states/nc/tax/income/nc_use_tax.py
@@ -5,9 +5,11 @@ class nc_use_tax(variable):
     value_type = float
     entity = TaxUnit
     label = "NC use tax"
+    unit = USD
     definition_period = YEAR
+    defined_for = StateCode.NC
 
     def formula(tax_unit, period, parameters):
         agi = tax_unit("adjusted_gross_income", period)
-        p = parameters(period).gov.states.nc.tax.use_tax.without_record
+        p = parameters(period).gov.states.nc.tax.use_tax
         return p.amount.calc(agi) + (p.rate.calc(agi) * agi)
