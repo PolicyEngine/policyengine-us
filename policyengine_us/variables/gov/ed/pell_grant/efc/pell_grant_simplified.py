a979e31ada5ea0e5eb9720d9ca86030d6e3cedc3
Caleb <caleblpena@gmail.com>
2023-08-11 14:25:54 -0600

    use parameter for benefits

--- policyengine_us/variables/gov/ed/pell_grant/efc/pell_grant_simplified.py
+++ policyengine_us/variables/gov/ed/pell_grant/efc/pell_grant_simplified.py
@@ -10,19 +10,11 @@ class pell_grant_simplified(Variable):
     def formula(person, period, parameters):
         tax_unit = person.tax_unit
         head_income = tax_unit("pell_grant_head_income", period)
-        max_income = parameters(
+        p = parameters(
             period
-        ).gov.ed.pell_grant.efc.simplified.max_income
+        ).gov.ed.pell_grant.efc.simplified
+        max_income = p.max_income
         income_eligible = head_income < max_income
-        medicaid = add(tax_unit, period, ["medicaid"])
-        ssi = add(tax_unit, period, ["ssi"])
-        snap = person.spm_unit("snap", period)
-        free_lunch = person.spm_unit("free_school_meals", period)
-        reduced_lunch = person.spm_unit("reduced_price_school_meals", period)
-        tanf = person.spm_unit("tanf", period)
-        wic = add(tax_unit, period, ["wic"])
-        total_benefits = (
-            medicaid + ssi + snap + free_lunch + reduced_lunch + tanf + wic
-        )
-        has_benefits = total_benefits > 0
+        total_benefits = add(tax_unit, period, p.benefits)
+        has_benefits = np.any(total_benefits > 0)
         return income_eligible & has_benefits
