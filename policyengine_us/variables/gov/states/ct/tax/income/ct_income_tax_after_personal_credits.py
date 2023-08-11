fbdc9d7d153d1b818f2e003dd8ebc662b306ed10
PavelMakarchuk <pavel.ma99@gmail.com>
2023-08-11 05:38:42 +0200

    Connecticut personal income tax schedule Fixes #2585

--- policyengine_us/variables/gov/states/ct/tax/income/ct_income_tax_after_personal_credits.py
+++ policyengine_us/variables/gov/states/ct/tax/income/ct_income_tax_after_personal_credits.py
@@ -12,9 +12,7 @@ class ct_income_tax_after_personal_credits(Variable):
     def formula(tax_unit, period, parameters):
         income = tax_unit("ct_agi", period)
         personal_exemptions = tax_unit("ct_personal_exemptions", period)
-        income_after_personal_exemptions = max_(
-            income - personal_exemptions, 0
-        )
+        taxable_income = max_(income - personal_exemptions, 0)
         filing_status = tax_unit("filing_status", period)
         status = filing_status.possible_values
         p = parameters(period).gov.states.ct.tax.income.main
@@ -27,11 +25,11 @@ class ct_income_tax_after_personal_credits(Variable):
                 filing_status == status.HEAD_OF_HOUSEHOLD,
             ],
             [
-                p.single.calc(income_after_personal_exemptions),
-                p.joint.calc(income_after_personal_exemptions),
-                p.separate.calc(income_after_personal_exemptions),
-                p.widow.calc(income_after_personal_exemptions),
-                p.head_of_household.calc(income_after_personal_exemptions),
+                p.single.calc(taxable_income),
+                p.joint.calc(taxable_income),
+                p.separate.calc(taxable_income),
+                p.widow.calc(taxable_income),
+                p.head_of_household.calc(taxable_income),
             ],
         )
         add_back = tax_unit("ct_income_tax_phase_out_add_back", period)
