ae406801ac6f084d4243d24db2cdaa721efabc0b
Caleb <caleblpena@gmail.com>
2023-08-11 12:03:40 -0600

    fix typo

--- policyengine_us/variables/gov/ed/pell_grant/efc/dependent/pell_grant_dependent_available_income.py
+++ policyengine_us/variables/gov/ed/pell_grant/efc/dependent/pell_grant_dependent_available_income.py
@@ -4,5 +4,5 @@ from policyengine_us.model_api import *
 class pell_grant_dependent_available_income(Variable):
     value_type = float
     entity = Person
-    label = "Pell Grant dependent availble income"
+    label = "Pell Grant dependent available income"
     definition_period = YEAR
