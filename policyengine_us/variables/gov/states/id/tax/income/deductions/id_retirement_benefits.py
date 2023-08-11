4407170f0d723c56078204e922f56b52e08d98f0
LeqiGogo <100904908+LeqiGogo@users.noreply.github.com>
2023-08-11 19:11:45 +0000

    Add tests and modify references

--- policyengine_us/variables/gov/states/id/tax/income/deductions/id_retirement_benefits.py
+++ policyengine_us/variables/gov/states/id/tax/income/deductions/id_retirement_benefits.py
@@ -6,7 +6,7 @@ class id_retirement_benefits(Variable):
     entity = TaxUnit
     label = "Idaho retirement benefits"
     unit = USD
-    documentation = "https://tax.idaho.gov/wp-content/uploads/forms/EFO00088/EFO00088_12-30-2022.pdf"
+    documentation = "https://tax.idaho.gov/wp-content/uploads/forms/EFO00088/EFO00088_12-30-2022.pdf#page=1"
     definition_period = YEAR
     defined_for = StateCode.ID
 
@@ -27,11 +27,11 @@ class id_retirement_benefits(Variable):
         # Base retirement benefits deduction amount
         base_amt = p.amount[filing_status]
         # Social Security benefits received amount
-        ss_amt = head("social_security_retirement", period)
+        ss_amt = person("social_security_retirement", period)
         # Base amount minus social Security benefits received amount
         ded_amt = max(base_amt - ss_amt, 0)
         # Qualified retirement benefits included in federal income
-        fi_amt = head("taxable_pension_income", period) + head("military_retirement_pay", period)
+        fi_amt = person("taxable_pension_income", period) + person("military_retirement_pay", period)
         # The smaller one
         retire_ded_amt = min(ded_amt, fi_amt)
 
