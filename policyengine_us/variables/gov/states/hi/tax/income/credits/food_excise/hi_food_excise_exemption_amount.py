5eaf7a8b8ea71b7f5c1a4dbb31626697b4af1fda
Ruby Ren <38923159+YichunRen@users.noreply.github.com>
2023-08-11 19:46:57 +0000

    use minor child variable

--- policyengine_us/variables/gov/states/hi/tax/income/credits/food_excise/hi_food_excise_exemption_amount.py
+++ policyengine_us/variables/gov/states/hi/tax/income/credits/food_excise/hi_food_excise_exemption_amount.py
@@ -11,15 +11,6 @@ class hi_food_excise_exemption_amount(Variable):
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states.hi.tax.income.credits.food_excise_tax
 
-        person = tax_unit.members
-        public_support_over_half = person(
-            "hi_food_excise_credit_child_receiving_public_support", period
-        )
-        is_child = person("is_child", period)
-        minor = person("age", period) < p.minor_child.age_threshold
-        eligible_minor_child = is_child & minor & public_support_over_half
-        minor_children = tax_unit.sum(eligible_minor_child)
-
         income = tax_unit("adjusted_gross_income", period)
         # Count exemptions as number of people.
         # The legal code defines exemptions as Hawaii exemptions,
@@ -27,6 +18,10 @@ class hi_food_excise_exemption_amount(Variable):
         # but excludes those additional exemptions for this program.
         exemptions = tax_unit("exemptions", period)
         # Reduce number of exemptions by the number of minor children
+        minor_children = (
+            tax_unit("hi_food_excise_credit_minor_child_amount", period)
+            / p.minor_child.amount
+        )
         claimable_exemptions = exemptions - minor_children
         # Determine the amount per exemption based on income
         # and filing status
