b8c4cd17305213ba7ae54635f6b80c538495cadb
Masasa <73863365+ThapeloMasasa@users.noreply.github.com>
2023-08-13 22:49:57 +0000

    all tests passing

--- policyengine_us/variables/gov/states/ct/tax/income/credits/ct_credit_based_on_agi.py
+++ policyengine_us/variables/gov/states/ct/tax/income/credits/ct_credit_based_on_agi.py
@@ -5,7 +5,6 @@ class ct_personal_credits(Variable):
     value_type = float
     entity = TaxUnit
     label = "Connecticut Personal Credits"
-    unit = USD
     definition_period = YEAR
     defined_for = StateCode.CT
 
