e321097017f017c3f81acdc8db76b8987d6d0da4
JudyZ13 <112495414+JudyZ13@users.noreply.github.com>
2023-08-14 22:11:44 +0000

    new folder

--- policyengine_us/variables/gov/states/az/tax/income/credits/family_tax_credit/az_family_tax_credit.py
+++ policyengine_us/variables/gov/states/az/tax/income/credits/family_tax_credit/az_family_tax_credit.py
@@ -11,7 +11,7 @@ class az_family_tax_credit(Variable):
 
     def formula(tax_unit, period, parameters):
         person = tax_unit.members
-        p = parameters(period).gov.states.az.tax.income.credits
+        p = parameters(period).gov.states.az.tax.income.credits.family_tax_credits
         income = tax_unit("adjusted_gross_income", period)
         filing_status = tax_unit("filing_status", period)
         status = filing_status.possible_values
