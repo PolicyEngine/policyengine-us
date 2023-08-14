774b81bc709bd45b46ed4d55716113a0a0576917
XZ-mabel <131919825+XZ-mabel@users.noreply.github.com>
2023-08-14 19:13:59 +0000

    Created an empty la_state_child_credit python file

--- policyengine_us/variables/gov/states/la/tax/credits/la_state_child_care_credit.py
+++ policyengine_us/variables/gov/states/la/tax/credits/la_state_child_care_credit.py
@@ -9,23 +9,3 @@ class la_state_child_care_credit(Variable):
     definition_period = YEAR
     reference = "https://www.revenue.louisiana.gov/IndividualIncomeTax/SchoolReadinessTaxCredit"
     defined_for = StateCode.LA
-
-    def formula(tax_unit, period, parameters):
-        p = parameters(
-            period
-        ).gov.states.la.tax.credits.child_care_expense_credit.rate
-
-        # calculate louisian child care credit
-        us_agi = tax_unit("adjusted_gross_income", period)
-        federal_credit = tax_unit("", period)
-        child_care_credit = federal_credit * p.la_state_credit.calc(us_agi)
-
-        # check if agi is more than sixty-thousand dollars
-        credit_income_threshold = us_agi > p.income_threshold_for_credit
-
-        # if agi is more than sixty-thousand dollars, check if the state child care credit is less than twenty-five dollars
-        credit_threshold = (
-            child_care_credit > p.credit_threshold
-        ) * credit_income_threshold
-
-        return where(credit_threshold, 25, child_care_credit)
