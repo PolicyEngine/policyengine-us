7f527003fd761cb3f8b6c1d9610f0139af0d4b83
Xinyi-Shao <ellashao99@gmail.com>
2023-08-15 15:24:50 -0400

    updated

--- policyengine_us/variables/gov/irs/income/taxable_income/deductions/itemizing/misc_deduction.py
+++ policyengine_us/variables/gov/irs/income/taxable_income/deductions/itemizing/misc_deduction.py
@@ -7,3 +7,13 @@ class misc_deduction(Variable):
     label = "Miscellaneous deduction"
     unit = USD
     definition_period = YEAR
+
+    def formula(tax_unit, period, parameters):
+        # calculate federal level miscellaneous cap
+        p = parameters(period).gov.irs.deductions.itemized.misc
+        misc_floor = p.floor * tax_unit("positive_agi", period)
+        cap = max_(0, misc_floor)
+
+        # individial miscellaneous income
+        misc_income = add(tax_unit, period["miscellaneous_income"])
+        return min_(misc_income, cap)
--- policyengine_us/variables/gov/states/ms/tax/income/deductions/itemized/ms_itemized_deductions.py
+++ policyengine_us/variables/gov/states/ms/tax/income/deductions/itemized/ms_itemized_deductions.py
@@ -10,23 +10,19 @@ class ms_itemized_deductions(Variable):
 
     reference = (
         "https://www.dor.ms.gov/sites/default/files/Forms/Individual/80100221.pdf#page=15"
-        "https://www.dor.ms.gov/sites/default/files/Forms/Individual/80108228.pdf"
+        "https://www.dor.ms.gov/sites/default/files/Forms/Individual/80108228.pdf, # Line 7"
         "https://advance.lexis.com/documentpage/teaserdocument/?pdmfid=1000516&crid=9f552d68-042d-4ab6-b45d-4b5ec25d1742&config=00JABhZDIzMTViZS04NjcxLTQ1MDItOTllOS03MDg0ZTQxYzU4ZTQKAFBvZENhdGFsb2f8inKxYiqNVSihJeNKRlUp&pddocfullpath=%2fshared%2fdocument%2fstatutes-legislation%2furn%3acontentItem%3a659V-W3G3-CGX8-0009-00008-00&pddocid=urn%3acontentItem%3a659V-W3G3-CGX8-0009-00008-00&pdcontentcomponentid=234190&pdteaserkey=h3&pditab=allpods&ecomp=7s65kkk&earg=sr0&prid=a60fa5e9-9e96-4e4a-88be-5db3d101cc92"
     )
     # Variable modeled after Tax Form: Schedule A
     defined_for = StateCode.MS
 
     def formula(tax_unit, period, parameters):
-        # get the parameters for calcualtions
-        agi = tax_unit("adjusted_gross_income", period)
-        misc = tax_unit("misc_deduction", period)
-
-        p = parameters(period).gov.irs.deductions.itemized.misc
-
         # compute itemized deduction maximum less salt
         itm_deds_less_salt = tax_unit("itemized_deductions_less_salt", period)
 
         # calculate maximum miscellanous amount
-        misc_deduction = min_(misc, p.floor * agi)
+        misc_income = add(tax_unit, period["miscellaneous_income"])
+        misc_deduction = tax_unit("misc_deduction", period)
+        misc_deduction_total = max_(misc_income - misc_deduction, 0)
 
-        return itm_deds_less_salt + misc_deduction
+        return itm_deds_less_salt + misc_deduction_total
