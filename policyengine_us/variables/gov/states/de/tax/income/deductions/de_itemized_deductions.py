484e1a9fcbc76184d2c2fc7da0fb4dcabfb432dd
shuangding415 <131055332+shuangding415@users.noreply.github.com>
2023-08-15 05:07:58 +0000

    Delaware Itemized Deductions Fixes #2703

--- policyengine_us/variables/gov/states/de/tax/income/deductions/de_itemized_deductions.py
+++ policyengine_us/variables/gov/states/de/tax/income/deductions/de_itemized_deductions.py
@@ -22,8 +22,6 @@ class de_itemized_deductions(Variable):
         #   itemized or standard deduction.
         # ... calculate pre-limitation itemized deductions
         itm_deds_less_salt = tax_unit("itemized_deductions_less_salt", period)
-        capped_property_taxes = tax_unit("capped_property_taxes", period)
-        de_itm_deds = itm_deds_less_salt + capped_property_taxes
         # ... calculate itemized deductions offset
         p = parameters(period).gov.states.mn.tax.income.deductions
         exempt_deds = add(
@@ -36,11 +34,11 @@ class de_itemized_deductions(Variable):
                 "salt_deduction",
             ],
         )
-        net_deds = max_(0, de_itm_deds - exempt_deds)
+        net_deds = max_(0, itm_deds_less_salt - exempt_deds)
         net_deds_offset = p.deduction_fraction * net_deds
         agi = tax_unit("adjusted_gross_income", period)
         filing_status = tax_unit("filing_status", period)
         excess_agi = max_(0, agi - p.agi_threshold[filing_status])
         excess_agi_offset = p.excess_agi_fraction * excess_agi
         offset = min_(net_deds_offset, excess_agi_offset)
-        return max_(0, de_itm_deds - offset)
+        return max_(0, itm_deds_less_salt - offset)
