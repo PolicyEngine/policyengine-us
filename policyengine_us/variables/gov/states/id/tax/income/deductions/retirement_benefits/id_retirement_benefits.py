82ac7b04d368f40eedd8468592d8795ad2a063da
LeqiGogo <100904908+LeqiGogo@users.noreply.github.com>
2023-08-16 19:29:15 +0000

    new variable for eligibility

--- policyengine_us/variables/gov/states/id/tax/income/deductions/retirement_benefits/id_retirement_benefits.py
+++ policyengine_us/variables/gov/states/id/tax/income/deductions/retirement_benefits/id_retirement_benefits.py
@@ -8,7 +8,7 @@ class id_retirement_benefits(Variable):
     unit = USD
     documentation = "https://tax.idaho.gov/wp-content/uploads/forms/EFO00088/EFO00088_12-30-2022.pdf#page=1"
     definition_period = YEAR
-    defined_for = StateCode.ID
+    defined_for = "id_retirement_benefits_eligible"
 
     def formula(tax_unit, period, parameters):
         person = tax_unit.members
@@ -17,12 +17,7 @@ class id_retirement_benefits(Variable):
         filing_status = tax_unit("filing_status", period)
         
         # check if filer is eligible for the deduction
-        age_threshold = p.age_eligibility
-        age_threshold_disabled = p.age_eligibility_disabled
-        disabled_head = tax_unit("disabled_head", period)
-        eligible = where(disabled_head == False, 
-                            tax_unit("age_head", period) >= age_threshold, 
-                            tax_unit("age_head", period) >= age_threshold_disabled)
+        eligible = tax_unit("id_retirement_benefits_eligible", period)
 
         # Base retirement benefits deduction amount
         base_amt = p.amount[filing_status]
