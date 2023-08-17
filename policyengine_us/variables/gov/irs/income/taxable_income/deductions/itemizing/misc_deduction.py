58c390e2b0f681b801e2382805e5458e59ddb443
Xinyi-Shao <ellashao99@gmail.com>
2023-08-17 14:25:23 -0400

    added misc_deduction test

--- policyengine_us/variables/gov/irs/income/taxable_income/deductions/itemizing/misc_deduction.py
+++ policyengine_us/variables/gov/irs/income/taxable_income/deductions/itemizing/misc_deduction.py
@@ -8,6 +8,8 @@ class misc_deduction(Variable):
     unit = USD
     definition_period = YEAR
 
+    reference = "https://www.irs.gov/publications/p529, Introduction Line #3"
+
     def formula(tax_unit, period, parameters):
         # calculate federal level miscellaneous cap
         p = parameters(period).gov.irs.deductions.itemized.misc
