34c66e4c11488cb7236fd87b2680fa68cc712418
Jingjing Wang <87988673+Jingj-WANG@users.noreply.github.com>
2023-08-17 23:10:16 +0000

    Colorado Low income child care expense credit Fixes #2724

--- policyengine_us/variables/gov/states/co/tax/income/credits/cdcc/low_income/co_low_income_cdcc.py
+++ policyengine_us/variables/gov/states/co/tax/income/credits/cdcc/low_income/co_low_income_cdcc.py
@@ -15,31 +15,26 @@ class co_low_income_cdcc(Variable):
         # The childcare expenses can not exceed the earned income of the filer
         # if filing jointly - the lesser of either individual's earned income
         earned_income = tax_unit("min_head_spouse_earned", period)
-        # Get sum of all qualified child care expenses
-        spm_unit = tax_unit.spm_unit
-        spm_unit_cdcc_expense = spm_unit("childcare_expenses", period)
 
-        # Distribute the SPM unit's cdcc evenly across children in SPM unit's Tax units
-        # reference: class tax_unit_childcare_expenses(Variable)
-        spm_unit_count_children = add(spm_unit, period, ["is_child"])
-        tax_unit_count_children = add(tax_unit, period, ["is_child"])
-        child_ratio = np.zeros_like(spm_unit_count_children)
-        mask = spm_unit_count_children > 0
-        child_ratio[mask] = (
-            tax_unit_count_children[mask] / spm_unit_count_children[mask]
-        )
-        tax_unit_cdcc_expense = spm_unit_cdcc_expense * child_ratio
-
+        # Get sum of all qualified child care expenses, class "childcare_expenses" is based on SPMunit
+        # Better choice to use class "tax_unit_childcare_expenses", which based on Taxunit and then distribute to SPMunit
+        # reference: class nyc_cdcc_age_restricted_expenses(Variable)
+        children = tax_unit("tax_unit_children", period)
         # Check child's age is under 13 and check class is_child -- under tax_unit
         person = tax_unit.members
-        age = person("age", period)
-        eligible_child = age < p.child_age_threshold
+        eligible_child = person("age", period) < p.child_age_threshold
+        eligible_children = tax_unit.sum(eligible_child)
+        tax_unit_childcare_expenses = tax_unit(
+            "tax_unit_childcare_expenses", period
+        )
+        # avoid divide-by-zero warnings by not using where() function
+        eligible_child_ratio = np.zeros_like(children)
+        mask = children > 0
+        eligible_child_ratio[mask] = eligible_children[mask] / children[mask]
+        total_expenses = tax_unit_childcare_expenses * eligible_child_ratio
 
-        eligible_expenses = eligible_child * tax_unit_cdcc_expense
-        total_expenses = tax_unit.sum(eligible_expenses)
         capped_expenses = min_(earned_income, total_expenses)
         co_low_income_cdcc = capped_expenses * p.rate
         # Credit is capped at $500 or $1,000 for 1 or over 2 dependents, respectively.
-        eligible_children = tax_unit.sum(eligible_child)
         max_amount = p.max_amount.calc(eligible_children)
         return min_(co_low_income_cdcc, max_amount)
