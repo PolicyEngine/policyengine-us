019bd407efbffc8be1cd9bdb3c881c9af12b3c73
LeqiGogo <100904908+LeqiGogo@users.noreply.github.com>
2023-08-18 18:54:34 +0000

    modified variables

--- policyengine_us/variables/gov/states/id/tax/income/deductions/retirement_benefits/id_retirement_benefits.py
+++ policyengine_us/variables/gov/states/id/tax/income/deductions/retirement_benefits/id_retirement_benefits.py
@@ -12,22 +12,17 @@ class id_retirement_benefits(Variable):
 
     def formula(tax_unit, period, parameters):
         person = tax_unit.members
-        head = person("is_tax_unit_head", period)
         p = parameters(period).gov.states.id.tax.income.deductions.retirement_benefits
         filing_status = tax_unit("filing_status", period)
         
-        # check if filer is eligible for the deduction
-        eligible = tax_unit("id_retirement_benefits_eligible", period)
-
         # Base retirement benefits deduction amount
         base_amt = p.amount[filing_status]
         # Social Security benefits received amount
-        ss_amt = person("social_security_retirement", period)
+        ss_amt = add(tax_unit, period, ["social_security_retirement"])
         # Base amount minus social Security benefits received amount
         ded_amt = max_(base_amt - ss_amt, 0)
         # Qualified retirement benefits included in federal income
-        fi_amt = person("taxable_pension_income", period) + person("military_retirement_pay", period)
+        # fi_amt = person("taxable_pension_income", period) + person("military_retirement_pay", period)
+        fi_amt = tax_unit.sum(person("taxable_pension_income", period) + person("military_retirement_pay", period))
         # The smaller one
-        retire_ded_amt = min_(ded_amt, fi_amt)
-
-        return eligible * tax_unit.sum(retire_ded_amt)
+        return min_(ded_amt, fi_amt)
--- policyengine_us/variables/gov/states/id/tax/income/deductions/retirement_benefits/id_retirement_benefits_eligible.py
+++ policyengine_us/variables/gov/states/id/tax/income/deductions/retirement_benefits/id_retirement_benefits_eligible.py
@@ -2,24 +2,22 @@ from policyengine_us.model_api import *
 
 
 class id_retirement_benefits_eligible(Variable):
-    value_type = float
+    value_type = bool
     entity = TaxUnit
-    label = "Idaho retirement benefits"
-    unit = USD
+    label = "Eligible for the Idaho retirement benefits"
     documentation = "https://tax.idaho.gov/wp-content/uploads/forms/EFO00088/EFO00088_12-30-2022.pdf#page=1"
     definition_period = YEAR
     defined_for = StateCode.ID
 
     def formula(tax_unit, period, parameters):
         person = tax_unit.members
-        head = person("is_tax_unit_head", period)
         p = parameters(period).gov.states.id.tax.income.deductions.retirement_benefits
 
         age_threshold = p.age_eligibility.main
         age_threshold_disabled = p.age_eligibility.disabled
         disabled_head = tax_unit("disabled_head", period)
-        eligible = where(disabled_head == False, 
-                        tax_unit("age_head", period) >= age_threshold, 
-                        tax_unit("age_head", period) >= age_threshold_disabled)
+        head_age = tax_unit("age_head", period)
+        return where(disabled_head, 
+                        head_age >= age_threshold_disabled, 
+                        head_age >= age_threshold)
         
-        return eligible
