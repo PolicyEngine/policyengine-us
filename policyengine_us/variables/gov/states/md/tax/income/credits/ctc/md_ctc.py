fa3e24d0725b1c1bc94c696faa0c78f379c4f282
PavelMakarchuk <pavel.ma99@gmail.com>
2023-08-18 18:18:45 +0300

    minor changes

--- policyengine_us/variables/gov/states/md/tax/income/credits/ctc/md_ctc.py
+++ policyengine_us/variables/gov/states/md/tax/income/credits/ctc/md_ctc.py
@@ -11,25 +11,23 @@ class md_ctc(Variable):
         "https://casetext.com/statute/code-of-maryland/article-tax-general/title-10-income-tax/subtitle-7-income-tax-credits/section-10-751-effective-until-712026-tax-credit-for-qualified-child"
         "https://law.justia.com/codes/maryland/2022/tax-general/title-10/subtitle-7/section-10-751/"
     )
-    defined_for = StateCode.MD
+    defined_for = "md_ctc_eligible"
 
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states.md.tax.income.credits.ctc
-        income_eligible = (
-            tax_unit("adjusted_gross_income", period) <= p.agi_cap
-        )
         person = tax_unit.members
         dependent = person("is_tax_unit_dependent", period)
         disabled = person("is_disabled", period)
+        age = person("age", period)
         meets_disabled_age_limit = (
-            person("age", period) < p.age_threshold.disabled
+            age < p.age_threshold.disabled
         )
         eligible_disabled_child = (
             dependent & disabled & meets_disabled_age_limit
         )
         eligible_child = dependent & (
-            person("age", period) < p.age_threshold.main
+            age < p.age_threshold.main
         )
         eligible = eligible_disabled_child | eligible_child
         eligible_children = tax_unit.sum(eligible)
-        return income_eligible * eligible_children * p.amount
+        return eligible_children * p.amount
