968f05662bea478a714b8fb1093d06973a74c4f0
xianxinzhu <97819588+xianxinzhu@users.noreply.github.com>
2023-08-18 02:55:41 +0000

    Fix tests

--- policyengine_us/variables/gov/states/hi/tax/income/credits/hi_low_income_household_renters/hi_lihrtc_eligible.py
+++ policyengine_us/variables/gov/states/hi/tax/income/credits/hi_low_income_household_renters/hi_lihrtc_eligible.py
@@ -7,7 +7,7 @@ class hi_lihrtc_eligible(Variable):
     label = "Eligible for the Hawaii low income household renters tax credit"
     definition_period = YEAR
     reference = " https://files.hawaii.gov/tax/legal/har/har_235.pdf#page=105"  # §18-235-55.7 (b)
-    defined_for = "StateCode.HI"
+    defined_for = StateCode.HI
 
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states.hi.tax.income.credits.lihrtc
