d6a7eb1e046d4772e3847c369ae1197b2082f215
Max Ghenis <mghenis@gmail.com>
2023-08-19 00:08:55 -0400

    Update Indiana tax rate for 2023 Fixes #2864

--- policyengine_us/variables/gov/states/in/tax/income/deductions/in_renters_deduction.py
+++ policyengine_us/variables/gov/states/in/tax/income/deductions/in_renters_deduction.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class in_renters_deduction(Variable):
     value_type = float
     entity = TaxUnit
-    label = "IN renter's deductions"
+    label = "Indiana renter's deduction"
     unit = USD
     definition_period = YEAR
     reference = "http://iga.in.gov/legislative/laws/2021/ic/titles/006#6-3-2-6"
