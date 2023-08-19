7cac9380be5f5a0bea6d38fdccfca88aed5a2d46
Max Ghenis <mghenis@gmail.com>
2023-08-19 00:15:06 -0400

    Update names of Indiana variable labels and use real_estate_taxes in Indiana homeowners property tax deduction

--- policyengine_us/variables/gov/states/in/tax/income/deductions/in_homeowners_property_tax_deduction.py
+++ policyengine_us/variables/gov/states/in/tax/income/deductions/in_homeowners_property_tax_deduction.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class in_homeowners_property_tax_deduction(Variable):
     value_type = float
     entity = TaxUnit
-    label = "IN homeowner's residential property tax deduction"
+    label = "Indiana homeowner's residential property tax deduction"
     unit = USD
     definition_period = YEAR
     reference = "http://iga.in.gov/legislative/laws/2021/ic/titles/006#6-3"
@@ -15,5 +15,5 @@ class in_homeowners_property_tax_deduction(Variable):
         max_homeowners_property_tax_deduction = p.homeowners_property_tax.max[
             filing_status
         ]
-        property_tax = tax_unit("in_homeowners_property_tax", period)
+        property_tax = tax_unit("real_estate_taxes", period)
         return min_(property_tax, max_homeowners_property_tax_deduction)
--- policyengine_us/variables/gov/states/in/tax/income/taxes/in_agi_tax.py
+++ policyengine_us/variables/gov/states/in/tax/income/taxes/in_agi_tax.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class in_agi_tax(Variable):
     value_type = float
     entity = TaxUnit
-    label = "IN adjusted gross income tax"
+    label = "Indiana adjusted gross income tax"
     definition_period = YEAR
     unit = USD
     reference = "http://iga.in.gov/legislative/laws/2021/ic/titles/006#6-3-2-1"  # (a)(3)
