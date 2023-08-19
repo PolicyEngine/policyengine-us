75eba54f5d2616f9dccd2d226035548b1aa6cc8b
xianxinzhu <97819588+xianxinzhu@users.noreply.github.com>
2023-08-19 04:28:44 +0000

    FIx variable, test and parameter files

--- policyengine_us/variables/gov/states/hi/tax/income/credits/hi_low_income_household_renters/hi_low_income_household_renters_tax_credit.py
+++ policyengine_us/variables/gov/states/hi/tax/income/credits/hi_low_income_household_renters/hi_low_income_household_renters_tax_credit.py
@@ -1,29 +1,29 @@
 from policyengine_us.model_api import *
 
 
-class hi_low_income_household_renters_tax_credit(Variable):
+class hi_tax_credit_for_low_income_household_renters(Variable):
     value_type = float
     entity = TaxUnit
     label = "Hawaii low income household renters tax credit"
     unit = USD
     definition_period = YEAR
-    defined_for = "hi_lihrtc_eligible"
+    defined_for = "hi_tax_credit_for_low_income_household_renters_eligible"
 
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states.hi.tax.income.credits.lihrtc
 
         tax_before_credit = tax_unit("hi_income_tax_before_credits", period)
         # Aged extra exemptions
-        aged_head = (tax_unit("age_head", period) >= p.threshold.age).astype(
-            int
-        )
+        aged_head = (
+            tax_unit("age_head", period) >= p.aged_age_threshold
+        ).astype(int)
         aged_spouse = (
-            tax_unit("age_spouse", period) >= p.threshold.age
+            tax_unit("age_spouse", period) >= p.aged_age_threshold.age
         ).astype(int)
 
         aged_exemptions = aged_head + aged_spouse
         exemptions = tax_unit("exemptions", period)
 
         total_exemptions = exemptions + aged_exemptions
-        credit_amount = p.base * total_exemptions
+        credit_amount = p.amount * total_exemptions
         return min_(credit_amount, tax_before_credit)
