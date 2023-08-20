d2f2cfb9c6de5651b9a007a8daba99d052a0f7cd
Carolinecc123 <60077689+Carolinecc123@users.noreply.github.com>
2023-08-20 05:42:09 +0000

    formula

---
+++ policyengine_us/variables/gov/states/ar/tax/income/credit/personal/blind/ar_blind_credit.py
@@ -0,0 +1,20 @@
+from policyengine_us.model_api import *
+
+
+class ar_blind(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "AR presonal credit for blind"
+    unit = USD
+    definition_period = YEAR
+    reference = (
+        "https://www.dfa.arkansas.gov/images/uploads/incomeTaxOffice/2021_AR1000F_FullYearResidentIndividualIncomeTaxReturn.pdf"
+        "https://www.dfa.arkansas.gov/images/uploads/incomeTaxOffice/2022_AR1000F_FullYearResidentIndividualIncomeTaxReturn.pdf#page=1"
+        "https://www.dfa.arkansas.gov/images/uploads/incomeTaxOffice/2022_AR1000F_and_AR1000NR_Instructions.pdf#page=12"
+    )
+    defined_for = StateCode.AR
+
+    def formula(tax_unit, period, parameters):
+        us_blind = tax_unit("blind", period)
+        p_ar = parameters(period).gov.states.ar.tax.income.credits.personal_credits
+        return us_blind * p_ar
