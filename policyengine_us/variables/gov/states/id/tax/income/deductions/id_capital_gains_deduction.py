b0ed27ace843de7ffb7cfb8e727a7051643f8b90
rachellin0 <91289121+rachellin0@users.noreply.github.com>
2023-08-22 18:49:12 +0000

    net_capital_gain

--- policyengine_us/variables/gov/states/id/tax/income/deductions/id_capital_gains_deduction.py
+++ policyengine_us/variables/gov/states/id/tax/income/deductions/id_capital_gains_deduction.py
@@ -15,23 +15,12 @@ class id_capital_gains_deduction(Variable):
             period
         ).gov.states.id.tax.income.deductions.capital_gains
         # taxpayer must report capital gain net income
-        net_capital_gain = add(tax_unit, period, ["capital_gains"])
-
-        # capital_gain_net_income
-        net_capital_loss = tax_unit.members("capital_losses", period)
-        # Idaho capital gains deduction may not be netted against
-        # gains from property qualifying for the Idaho capital gains
-        # deduction before the amount of the deduction is determined
-        capital_gain_net_income = max_(net_capital_gain - net_capital_loss, 0)
+        net_capital_gain = tax_unit("net_capital_gain", period)
 
         # Filers can deduct 60% of captial gains
-        decuctions = p.percentage * net_capital_gain
-
-        # capital gains deduction may not exceed the capital gain net income included in taxable income
-        # qualified_deductions = min_(capital_gain_net_income, decuctions)
 
         # Ordinary Income do not qualify for the Idaho capital gains deduction
         # Gain from dispositions of certain depreciable property treated as ordinary income
 
         # Return calculated amount.
-        return min_(capital_gain_net_income, decuctions)
+        return p.percentage * net_capital_gain
