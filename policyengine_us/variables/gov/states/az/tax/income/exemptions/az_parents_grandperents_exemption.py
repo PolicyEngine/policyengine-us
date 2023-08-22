ec60f1aafd3ca210f6004bd8dd85e6462b5a9669
JudyZ13 <112495414+JudyZ13@users.noreply.github.com>
2023-08-22 17:34:04 +0000

    Co-authored-by: Pavel Makarchuk <PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/states/az/tax/income/exemptions/az_parents_grandperents_exemption.py
+++ policyengine_us/variables/gov/states/az/tax/income/exemptions/az_parents_grandperents_exemption.py
@@ -16,22 +16,21 @@ class az_parents_grandparents_exemption(Variable):
         # payments from teh filer
         care_and_support_payment = person("care_and_support_payment", period)
         care_and_support_costs = person("care_and_support_costs", period)
-        payment_eligible = np.zeros_like(care_and_support_payment)
-        mask = care_and_support_costs > 0
-        payment_eligible[mask] = (
-            care_and_support_payment[mask] / care_and_support_costs[mask]
-        ) > 0
+        support_payment_ratio = np.zeros_like(care_and_support_costs)
+        mask = care_and_support_costs != 0
+        support_payment_ratio[mask] = care_and_support_payment[mask] / care_and_support_costs[mask]
+        payment_eligiblity = support_payment_ratio > 0
         # Eligible parents of ancestors of parents have to be at or over 65 as well as cohabiting with the filer
         age = person("age", period)
         age_eligible = age >= p.parent_grandparent.min_age
 
         cohabitating_parent = person("cohabitating_parent", period)
-        eligible_co_age_p = cohabitating_parent * age_eligible
-        eligible_parent = eligible_co_age_p * payment_eligible
+        eligible_co_age_p = cohabitating_parent & age_eligible
+        eligible_parent = eligible_co_age_p & payment_eligiblity
 
         cohabitating_grandparent = person("cohabitating_grandparent", period)
-        eligible_co_age_g = cohabitating_grandparent * age_eligible
-        eligible_grandparent = eligible_co_age_g * payment_eligible
+        eligible_co_age_g = cohabitating_grandparent & age_eligible
+        eligible_grandparent = eligible_co_age_g & payment_eligiblity
 
         total_exemptions = eligible_parent + eligible_grandparent
         return p.parent_grandparent.amount * tax_unit.sum(total_exemptions)
