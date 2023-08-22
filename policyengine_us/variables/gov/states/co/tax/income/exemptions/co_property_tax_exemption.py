d4b8a5d6188c4423fca98f021556e1f9b0778a17
phywzc <97962358+phywzc@users.noreply.github.com>
2023-08-22 22:26:06 +0000

    minor changes

--- policyengine_us/variables/gov/states/co/tax/income/exemptions/co_property_tax_exemption.py
+++ policyengine_us/variables/gov/states/co/tax/income/exemptions/co_property_tax_exemption.py
@@ -9,4 +9,3 @@ class co_property_tax_exemption(Variable):
     definition_period = YEAR
     reference = "https://casetext.com/statute/colorado-revised-statutes/title-39-taxation/property-tax/exemptions/article-3-exemptions/part-2-property-tax-exemption-for-qualifying-seniors-and-disabled-veterans/section-39-3-203-property-tax-exemption-qualifications"
     defined_for = StateCode.CO
-
