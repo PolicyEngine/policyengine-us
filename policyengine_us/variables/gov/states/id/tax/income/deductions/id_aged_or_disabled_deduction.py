9e5840d29df612d92ccd0a0703944e46aeca1333
PavelMakarchuk <pavel.ma99@gmail.com>
2023-08-23 23:34:13 +0300

    variable adj.

--- policyengine_us/variables/gov/states/id/tax/income/deductions/id_aged_or_disabled_deduction.py
+++ policyengine_us/variables/gov/states/id/tax/income/deductions/id_aged_or_disabled_deduction.py
@@ -12,12 +12,14 @@ class id_aged_or_disabled_deduction(Variable):
     def formula(tax_unit, period, parameters):
         person = tax_unit.members
         age = person("age", period)
+        head = person("is_tax_unit_head", period)
+        spouse = person("is_tax_unit_spouse", period)
         disabled = person("is_disabled", period)
         p = parameters(
             period
-        ).gov.states.id.tax.income.deductions.development_disabilities
-        age_eligible = age >= p.age_eligibility
+        ).gov.states.id.tax.income.deductions.aged_or_disabled
+        age_eligible = (age >= p.age_eligibility) & ~(head | spouse)
         eligible = age_eligible | disabled
         total_eligible = tax_unit.sum(eligible)
-        capped_eligible = min_(total_eligible, p.max_amount)
+        capped_eligible = min_(total_eligible, p.max_deductions)
         return capped_eligible * p.amount
