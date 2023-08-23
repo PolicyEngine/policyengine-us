1c0b197a480eab66795e1c84158ff9d606394307
Masasa <73863365+ThapeloMasasa@users.noreply.github.com>
2023-08-23 04:58:31 +0000

    added changelog

--- policyengine_us/variables/gov/states/ct/tax/income/credits/ct_property_tax_credit.py
+++ policyengine_us/variables/gov/states/ct/tax/income/credits/ct_property_tax_credit.py
@@ -7,9 +7,11 @@ class ct_property_tax_credit(Variable):
     label = "CT property tax credit"
     unit = USD
     definition_period = YEAR
-    reference = ()
+    reference = (
+        
+    )
     defined_for = "ct_ptc_taxunit_eligible"
 
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states.ct.tax.income.credits.property_tax
-        # computation to come here
+        # 
