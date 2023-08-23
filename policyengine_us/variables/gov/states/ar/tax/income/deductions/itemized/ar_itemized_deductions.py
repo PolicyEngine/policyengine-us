9d035415d7d2b2caf7fe20454dd6e2487fdb7a40
Kevintowne <89811054+Kevintowne@users.noreply.github.com>
2023-08-23 21:29:58 +0000

    Fixes parameters and variables

--- policyengine_us/variables/gov/states/ar/tax/income/deductions/itemized/ar_itemized_deductions.py
+++ policyengine_us/variables/gov/states/ar/tax/income/deductions/itemized/ar_itemized_deductions.py
@@ -11,48 +11,20 @@ class ar_itemized_deductions(Variable):
     defined_for = StateCode.AR
 
     def formula(tax_unit, period, parameters):
-        p = parameters(period).gov.irs.deductions
-        
-        items = [
-            deduction
-            for deduction in p.itemized_deductions
-            if deduction not in ["salt_deduction"]
-        ]
-        us_itm_deds_less_salt = add(tax_unit, period, items)
-        filing_status = tax_unit("filing_status", period)
-        capped_property_taxes = min_(
-            add(tax_unit, period, ["real_estate_taxes"]),
-            p.itemized.salt_and_real_estate.cap[filing_status],
-        )
-        ar_itm_deds = us_itm_deds_less_salt + capped_property_taxes
-        #######################
-        
+        less_salt_deds = tax_unit("itemized_deductions_less_salt", period)
         agi = tax_unit("adjusted_gross_income",period)
         spouse_agi = tax_unit("spouse_separate_adjusted_gross_income",period)
 
-        # Medical and dental expenses
-        mde = tax_unit("medical_expense_deduction",period)
-
         # Real estate tax + Personal property tax
-        rst = tax_unit("real_estate_taxes",period)
-
-        # Interest Expense
-        ied = tax_unit("interest_deduction",period)
-
-        # Contributions
-        contri = tax_unit("ira_contributions",period)  
-
-        # Casualty and Theft Loss
-        cl = tax_unit("casualty_loss_deduction",period) 
+        real_estate_deds = tax_unit("real_estate_taxes",period)
 
         # Post-secondary Education Tuition Deduction
-        qte = tax_unit("qualified_tuition_expenses",period)
+        tuition_deds = tax_unit("ar_post_secondary_education_tuition_deductions",period)
         
-
         # Miscellaneous Deductions 
-        misc_deduc = tax_unit("misc_deduction",period)
+        misc_deds = tax_unit("misc_deduction",period)
 
-        total_itemized_deduction = mde + + rst + ied + contri + cl + qte + misc_deduc
+        total_itemized_deduction = less_salt_deds + + real_estate_deds + tuition_deds + misc_deds
 
         # Prorated itemized deductions
         filing_status = tax_unit("filing_status", period)
---
+++ policyengine_us/variables/gov/states/ar/tax/income/deductions/itemized/ar_post_secondary_education_tuition_deductions.py
@@ -0,0 +1,17 @@
+from policyengine_us.model_api import *
+
+
+class ar_post_secondary_education_tuition_deductions(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Arkansas post-secondary education tuition deduction"
+    unit = USD
+    definition_period = YEAR
+    reference = "https://www.dfa.arkansas.gov/images/uploads/incomeTaxOffice/2022_AR1000F_and_AR1000NR_Instructions.pdf#page=21"
+    defined_for = StateCode.AR
+
+    def formula(tax_unit, period, parameters):
+        p = parameters(period).gov.states.ar.tax.income.deductions.itemized.post_secondary_education_tuition_deductions
+        tuition_expense = tax_unit("qualified_tuition_expenses", period)
+            
+        return min(p.ratio*tuition_expense, p.ratio*p.two_year_college, p.ratio*p.four_year_college, p.ratio*p.technical_institutes)
