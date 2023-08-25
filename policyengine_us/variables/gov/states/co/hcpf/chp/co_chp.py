c2e03108e073d3414d20dfa4f6e4cb5a0568104b
Caleb <caleblpena@gmail.com>
2023-08-25 16:56:52 -0600

    make all copay savings one variable

--- policyengine_us/variables/gov/states/co/hcpf/chp/co_chp.py
+++ policyengine_us/variables/gov/states/co/hcpf/chp/co_chp.py
@@ -4,16 +4,18 @@ from policyengine_us.model_api import *
 class co_chp(Variable):
     value_type = float
     entity = Person
-    label = "Colorado Child Health Plan Plus"
+    label = "Colorado Child Health Plan Plus expense savings"
     definition_period = YEAR
-    defined_for = "co_chp_eligible"
-    adds = [
-        "co_chp_ambulance_saving",
-        "co_chp_er_visit_saving",
-        "co_chp_inpatient_saving",
-        "co_chp_lab_saving",
-        "co_chp_outpatient_saving",
-        "co_chp_physician_services_saving",
-        "co_chp_prescription_saving",
-        "co_chp_urgent_care_saving",
-    ]
+    defined_for = StateCode.CO
+
+    def formula(person, period, parameters):
+        saving = 0
+        income_level = person("medicaid_income_level", period)
+        is_pregnant = person("is_pregnant", period)
+        p = parameters(period).gov.states.co.hcpf.chp
+        elements = p.expenses
+        for element in elements:
+            copay = ~is_pregnant * p.copays[element].calc(income_level)
+            expense = person(element + "_expense", period)
+            saving += max_(0, expense - copay)
+        return saving
