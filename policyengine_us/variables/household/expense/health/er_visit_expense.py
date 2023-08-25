16d52cb1cff9cd1d2244cec393786a9d35c8ddce
Caleb <caleblpena@gmail.com>
2023-08-25 14:28:11 -0600

    update label for er_visit_expense

--- policyengine_us/variables/household/expense/health/er_visit_expense.py
+++ policyengine_us/variables/household/expense/health/er_visit_expense.py
@@ -4,6 +4,6 @@ from policyengine_us.model_api import *
 class er_visit_expense(Variable):
     value_type = float
     entity = Person
-    label = "ER visit expenses"
+    label = "Emergency room visit expenses"
     unit = USD
     definition_period = YEAR
