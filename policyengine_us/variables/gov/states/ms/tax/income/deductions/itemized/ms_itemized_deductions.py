4fb007215370ceb954be4ca0574d88d92d233ac1
Xinyi-Shao <ellashao99@gmail.com>
2023-08-25 15:20:48 -0400

    updated reference and test

--- policyengine_us/variables/gov/states/ms/tax/income/deductions/itemized/ms_itemized_deductions.py
+++ policyengine_us/variables/gov/states/ms/tax/income/deductions/itemized/ms_itemized_deductions.py
@@ -11,7 +11,7 @@ class ms_itemized_deductions(Variable):
     reference = (
         "https://www.dor.ms.gov/sites/default/files/Forms/Individual/80100221.pdf#page=15"
         "https://www.dor.ms.gov/sites/default/files/Forms/Individual/80108228.pdf, # Line 7"
-        "https://advance.lexis.com/documentpage/teaserdocument/?pdmfid=1000516&crid=9f552d68-042d-4ab6-b45d-4b5ec25d1742&config=00JABhZDIzMTViZS04NjcxLTQ1MDItOTllOS03MDg0ZTQxYzU4ZTQKAFBvZENhdGFsb2f8inKxYiqNVSihJeNKRlUp&pddocfullpath=%2fshared%2fdocument%2fstatutes-legislation%2furn%3acontentItem%3a659V-W3G3-CGX8-0009-00008-00&pddocid=urn%3acontentItem%3a659V-W3G3-CGX8-0009-00008-00&pdcontentcomponentid=234190&pdteaserkey=h3&pditab=allpods&ecomp=7s65kkk&earg=sr0&prid=a60fa5e9-9e96-4e4a-88be-5db3d101cc92"
+        "https://casetext.com/statute/mississippi-code-1972/title-27-taxation-and-finance/chapter-7-income-tax-and-withholding/article-1-income-tax/section-27-7-17-deductions-allowed?__cf_chl_rt_tk=8Kelu8kHpIXTp_FnAJLHvqa7rtrZYE1U.NAeBM8L.Nc-1692990420-0-gaNycGzNEmU"
     )
     # Variable modeled after Tax Form: Schedule A
     defined_for = StateCode.MS
@@ -20,7 +20,4 @@ class ms_itemized_deductions(Variable):
         # compute itemized deduction maximum less salt
         itm_deds_less_salt = tax_unit("itemized_deductions_less_salt", period)
 
-        # calculate maximum miscellanous amount
-        misc_deduction = tax_unit("misc_deduction", period)
-
-        return itm_deds_less_salt + misc_deduction
+        return itm_deds_less_salt
