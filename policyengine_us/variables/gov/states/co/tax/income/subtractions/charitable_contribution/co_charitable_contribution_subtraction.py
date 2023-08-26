e84b195ae20e66ca536eb8a52ee1d37fa2e78cfc
PavelMakarchuk <pavel.ma99@gmail.com>
2023-08-26 20:10:15 +0300

    eligible for charitable

--- policyengine_us/variables/gov/states/co/tax/income/subtractions/charitable_contribution/co_charitable_contribution_subtraction.py
+++ policyengine_us/variables/gov/states/co/tax/income/subtractions/charitable_contribution/co_charitable_contribution_subtraction.py
@@ -13,13 +13,9 @@ class co_charitable_contribution_subtraction(Variable):
         "https://casetext.com/regulation/colorado-administrative-code/department-200-department-of-revenue/division-201-taxation-division/rule-1-ccr-201-2-income-tax/rule-39-22-1044m-charitable-contribution-subtraction-for-taxpayers-claiming-the-federal-standard-deduction"
         # C.R.S. 39-22-104(4)(m)(1)
     )
-    defined_for = StateCode.CO
+    defined_for = "co_charitable_contribution_subtraction_eligible"
 
     def formula(tax_unit, period, parameters):
-        # Only available to filers who do not itemize on their federal tax return.
-        # The tax form instructions also limit to filers who do not deduct charitable deductions,
-        # but this is a redundant criterion.
-        eligible = ~tax_unit("tax_unit_itemizes", period)
         p = parameters(
             period
         ).gov.states.co.tax.income.subtractions.charitable_contribution
@@ -27,9 +23,5 @@ class co_charitable_contribution_subtraction(Variable):
         non_cash_donations = add(
             tax_unit, period, ["charitable_non_cash_donations"]
         )
-        charitable_contributions = add(
-            tax_unit,
-            period,
-            ["charitable_cash_donations", "charitable_non_cash_donations"],
-        )
-        return eligible * max_(charitable_contributions - p.adjustment, 0)
+        charitable_contributions = cash_donations + non_cash_donations
+        return max_(charitable_contributions - p.adjustment, 0)
