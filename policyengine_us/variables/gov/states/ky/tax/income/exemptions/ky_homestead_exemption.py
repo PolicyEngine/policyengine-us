6dada36b5b0fca324889cf76090dafea6fe2f8b2
WeihaoZ724 <128203510+WeihaoZ724@users.noreply.github.com>
2023-08-27 09:26:43 +0000

    Kentucky homestead exemption fixes

--- policyengine_us/variables/gov/states/ky/tax/income/exemptions/ky_homestead_exemption.py
+++ policyengine_us/variables/gov/states/ky/tax/income/exemptions/ky_homestead_exemption.py
@@ -7,19 +7,8 @@ class ky_homestead_exemption(Variable):
     label = "Kentucky homestead exemption"
     unit = USD
     definition_period = YEAR
-    defined_for = StateCode.KY
+    defined_for = "ky_homestead_exemption_eligible"
 
     def formula(tax_unit, period, parameters):
-        p = parameters(period).gov.states.ky.tax.income.exemptions.homestead
-
-        # Get the individual filer's age.
-        age_head = tax_unit("age_head", period)
-        
-        # Get the disability status of the head of the household.
-        is_disabled = tax_unit("is_disabled", period)
-
-        # Determine if head of (filer) is eligible considering age and disability.
-        head_eligible = (age_head >= p.age_threshold) & is_disabled
-        eligible_count = head_eligible.astype(int)
-        
-        return eligible_count * p.amount
+        p = parameters(period).gov.states.ky.tax.income.exemptions.homestead      
+        return p.amount
---
+++ policyengine_us/variables/gov/states/ky/tax/income/exemptions/ky_homestead_exemption_eligible.py
@@ -0,0 +1,21 @@
+from policyengine_us.model_api import *
+
+
+class ky_homestead_exemption_eligible(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Whether this tax unit is eligible for Kentucky exemptions"
+    unit = USD
+    definition_period = YEAR
+    defined_for = StateCode.KY
+
+    def formula(tax_unit, period, parameters):
+        p = parameters(period).gov.states.nh.tax.income.exemptions.homestead
+
+        # Get the individual disabled and age status.
+        disabled_head = tax_unit("disabled_head", period)
+        age_head = tax_unit("age_head", period) < p.disability_age_threshold
+        head_eligible = (disabled_head & age_head).astype(int)
+
+        # Calculate total exemption.
+        return head_eligible * p.amount.disabled_addition
