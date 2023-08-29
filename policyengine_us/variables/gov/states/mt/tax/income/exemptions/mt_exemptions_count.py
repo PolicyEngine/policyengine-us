e7b133bd006dee5697c920d4cd654afbc548d846
PavelMakarchuk <pavel.ma99@gmail.com>
2023-08-29 19:46:44 +0300

    label

--- policyengine_us/variables/gov/states/mt/tax/income/exemptions/mt_exemptions_count.py
+++ policyengine_us/variables/gov/states/mt/tax/income/exemptions/mt_exemptions_count.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class mt_exemptions_count(Variable):
     value_type = int
     entity = TaxUnit
-    label = "Montana exemptions count"
+    label = "Number of Montana exemptions"
     definition_period = YEAR
     defined_for = StateCode.MT
 
