f12df0e6d390c9e250fdd4bf5bad5496b5807bcd
keepjojoyoung <106553218+keepjojoyoung@users.noreply.github.com>
2023-08-29 18:00:06 -0400

    Update policyengine_us/variables/gov/states/mi/tax/income/credits/home_heating/mi_heating_cost.py
    Co-authored-by: Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/states/mi/tax/income/credits/home_heating/mi_heating_cost.py
+++ policyengine_us/variables/gov/states/mi/tax/income/credits/home_heating/mi_heating_cost.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class mi_heating_cost(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Michigan household heating cost"
+    label = "Total heating cost"
     defined_for = StateCode.MI
     unit = USD
     definition_period = YEAR
