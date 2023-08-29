5e744aea3b610cff5e7203794ffd68c07d50717c
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-08-29 14:06:41 -0400

    Add ga_taxable_income formula

--- policyengine_us/variables/gov/states/ga/tax/income/ga_taxable_income.py
+++ policyengine_us/variables/gov/states/ga/tax/income/ga_taxable_income.py
@@ -8,3 +8,9 @@ class ga_taxable_income(Variable):
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.GA
+
+    def formula(tax_unit, period, parameters):
+        agi = tax_unit("ga_agi", period)
+        deductions = tax_unit("ga_deductions", period)
+        exemptions = tax_unit("ga_exemptions", period)
+        return max_(0, agi - deductions - exemptions)
