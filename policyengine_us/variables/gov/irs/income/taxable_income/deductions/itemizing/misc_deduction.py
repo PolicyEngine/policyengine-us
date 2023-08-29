3b9ac220bfd08313c6a42caed6068f0d29f4197e
PavelMakarchuk <pavel.ma99@gmail.com>
2023-08-30 02:10:00 +0300

    changes

--- policyengine_us/variables/gov/irs/income/taxable_income/deductions/itemizing/misc_deduction.py
+++ policyengine_us/variables/gov/irs/income/taxable_income/deductions/itemizing/misc_deduction.py
@@ -9,13 +9,3 @@ class misc_deduction(Variable):
     definition_period = YEAR
 
     reference = "https://www.irs.gov/publications/p529, Introduction Line #3"
-
-    def formula(tax_unit, period, parameters):
-        # calculate federal level miscellaneous cap
-        p = parameters(period).gov.irs.deductions.itemized.misc
-        misc_floor = p.floor * tax_unit("positive_agi", period)
-        cap = max_(0, misc_floor)
-
-        # individial miscellaneous income
-        misc_income = add(tax_unit, period, ["miscellaneous_income"])
-        return min_(misc_income, cap)
--- policyengine_us/variables/gov/states/ms/tax/income/deductions/itemized/ms_itemized_deductions.py
+++ policyengine_us/variables/gov/states/ms/tax/income/deductions/itemized/ms_itemized_deductions.py
@@ -13,11 +13,9 @@ class ms_itemized_deductions(Variable):
         "https://www.dor.ms.gov/sites/default/files/Forms/Individual/80108228.pdf, # Line 7"
         "https://casetext.com/statute/mississippi-code-1972/title-27-taxation-and-finance/chapter-7-income-tax-and-withholding/article-1-income-tax/section-27-7-17-deductions-allowed?__cf_chl_rt_tk=8Kelu8kHpIXTp_FnAJLHvqa7rtrZYE1U.NAeBM8L.Nc-1692990420-0-gaNycGzNEmU"
     )
-    # Variable modeled after Tax Form: Schedule A
     defined_for = StateCode.MS
 
-    def formula(tax_unit, period, parameters):
-        # compute itemized deduction maximum less salt
-        itm_deds_less_salt = tax_unit("itemized_deductions_less_salt", period)
+    adds = "itemized_deductions_less_salt"
 
-        return itm_deds_less_salt
+    # Mississippi allows itemized deductions for gaming establishments and gender transition procedures
+    # which are currently not modeled
