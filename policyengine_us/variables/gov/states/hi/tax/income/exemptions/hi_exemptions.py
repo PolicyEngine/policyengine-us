1ff9a65403bd2700628f4b285892dd6cbaa308dd
xianxinzhu <97819588+xianxinzhu@users.noreply.github.com>
2023-08-29 02:36:29 +0000

    Fix tests. variables and parameters

--- policyengine_us/variables/gov/states/hi/tax/income/exemptions/hi_exemptions.py
+++ policyengine_us/variables/gov/states/hi/tax/income/exemptions/hi_exemptions.py
@@ -6,28 +6,39 @@ class hi_exemptions(Variable):
     entity = TaxUnit
     label = "Hawaii exemptions"
     unit = USD
-    documentation = "https://files.hawaii.gov/tax/forms/2022/n11ins.pdf#page=20"
+    documentation = (
+        "https://files.hawaii.gov/tax/forms/2022/n11ins.pdf#page=20"
+    )
     definition_period = YEAR
     defined_for = StateCode.HI
 
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states.hi.tax.income.exemptions
-        
+        person = tax_unit.members
+
         num_exemp = tax_unit("exemptions", period)
-        disabled_head = tax_unit("disabled_head", period)
-        disabled_spouse = tax_unit("disabled_spouse", period)
-        aged_spouse = tax_unit("age_spouse", period)
-        
+        is_disabled = person("is_disabled", period)
+        is_spouse = person("is_tax_unit_spouse", period)
+        spouse_disabled = tax_unit("spouse_is_disabled", period)
+        spouse_aged = tax_unit("aged_spouse", period)
+
         return where(
-            disabled_head,
+            is_disabled,  # If the tax unit is disabled
             where(
-                disabled_spouse,
-                p.disability_exemptions.disabled_spouse,
+                is_spouse,  # If the tax unit is an individual
                 where(
-                    aged_spouse,
-                    p.disability_exemptions.aged_and_undisabled_spouse,
-                    p.disability_exemptions.unaged_undisabled_spouse
-                )
+                    spouse_disabled,  # If the spouse of the tax unit is disabled
+                    p.disability_exemptions.disabled_spouse
+                    * num_exemp,  # 14_000
+                    where(
+                        spouse_aged,  # If the non-disabled spouse is aged
+                        p.disability_exemptions.aged_undisabled_spouse
+                        * num_exemp,  # 9_288
+                        p.disability_exemptions.unaged_undisabled_spouse
+                        * num_exemp,  # 8_144
+                    ),
+                ),
+                p.disability_exemptions.individual*num_exemp,  # 7_000
             ),
-            p.regular_exemptions * num_exemp
+            p.regular_exemptions*num_exemp,  # 1_144
         )
