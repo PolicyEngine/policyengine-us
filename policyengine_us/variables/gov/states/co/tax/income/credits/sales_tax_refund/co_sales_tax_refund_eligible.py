7f7e23600d255008271aa3c8f1ae1789752ca539
PavelMakarchuk <pavel.ma99@gmail.com>
2023-08-31 00:25:37 +0300

    format

--- policyengine_us/variables/gov/states/co/tax/income/credits/sales_tax_refund/co_sales_tax_refund_eligible.py
+++ policyengine_us/variables/gov/states/co/tax/income/credits/sales_tax_refund/co_sales_tax_refund_eligible.py
@@ -14,17 +14,19 @@ class co_sales_tax_refund_eligible(Variable):
             period
         ).gov.states.co.tax.income.credits.sales_tax_refund
         age_head = tax_unit("age_head", period)
-        head_eligible = age_head >= p.age_threshold
+        head_age_eligible = age_head >= p.age_threshold
         age_spouse = tax_unit("age_spouse", period)
-        spouse_eligible = age_spouse >= p.age_threshold
+        spouse_age_eligible = age_spouse >= p.age_threshold
         # Legal code is ambiguous, but the form points to the line corresponding to
         # tax before non-refundable credits.
         income_tax_eligible = (
             tax_unit("co_income_tax_before_non_refundable_credits", period) > 0
         )
+        # Filers that had Colorado tax withheld are also eligible.
+        # Use employment income as a proxy.
         employment_income_eligible = (
             add(tax_unit, period, ["employment_income"]) > 0
         )
         income_eligible = employment_income_eligible | income_tax_eligible
-        age_eligible = head_eligible | spouse_eligible
+        age_eligible = head_age_eligible | spouse_age_eligible
         return age_eligible | income_eligible
