aedac55198b9f0abf421a9b2059ebf3373d59dff
Linfeng-ll <95734090+Linfeng-ll@users.noreply.github.com>
2023-08-30 03:47:41 +0000

    fix ref

--- policyengine_us/variables/gov/states/oh/tax/income/deductions/oh_unreimbursed_medical_care_expense_deduction.py
+++ policyengine_us/variables/gov/states/oh/tax/income/deductions/oh_unreimbursed_medical_care_expense_deduction.py
@@ -10,7 +10,7 @@ class oh_unreimbursed_medical_care_expense_deduction(Variable):
     reference = (
         "https://tax.ohio.gov/static/forms/ohio_individual/individual/2022/it1040-sd100-instruction-booklet.pdf#page=18",  # Line 36
         "https://tax.ohio.gov/static/forms/ohio_individual/individual/2022/it1040-sd100-instruction-booklet.pdf#page=27",
-        "https://codes.ohio.gov/ohio-revised-code/section-5747.01",  # R.C. 5747.01(A)(10)
+        "https://codes.ohio.gov/ohio-revised-code/section-5747.01",  # R.C. 5747.01(10)
     )
     defined_for = StateCode.OH
 
