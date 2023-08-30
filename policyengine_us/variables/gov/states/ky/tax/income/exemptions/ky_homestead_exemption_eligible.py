9040a407a9ae46b0f83743c4be6c7aceb5881dcc
WeihaoZ724 <128203510+WeihaoZ724@users.noreply.github.com>
2023-08-30 17:22:36 +0000

    Kentucky homestead exemption fixes

--- policyengine_us/variables/gov/states/ky/tax/income/exemptions/ky_homestead_exemption_eligible.py
+++ policyengine_us/variables/gov/states/ky/tax/income/exemptions/ky_homestead_exemption_eligible.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class ky_homestead_exemption_eligible(Variable):
     value_type = bool
     entity = TaxUnit
-    label = "Eligible for the Kentucky homestead exemptions"
+    label = "Eligible for the Kentucky homestead exemption"
     definition_period = YEAR
     defined_for = StateCode.KY
 
@@ -14,7 +14,5 @@ class ky_homestead_exemption_eligible(Variable):
         # Get the individual disabled and age status.
         disabled_head = tax_unit("disabled_head", period)
         age_head = tax_unit("age_head", period) < p.disability_age_threshold
-        head_eligible = (disabled_head & age_head).astype(int)
-
-        # Calculate total exemption.
-        return head_eligible * p.amount.disabled_addition
+        
+        return (disabled_head & age_head).astype(int)
