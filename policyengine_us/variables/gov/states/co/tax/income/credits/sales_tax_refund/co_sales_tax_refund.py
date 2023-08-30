75f423fb19537be27b4f448e8b2a161fa7f462f3
PavelMakarchuk <pavel.ma99@gmail.com>
2023-08-30 19:31:15 +0300

    test adj.

--- policyengine_us/variables/gov/states/co/tax/income/credits/sales_tax_refund/co_sales_tax_refund.py
+++ policyengine_us/variables/gov/states/co/tax/income/credits/sales_tax_refund/co_sales_tax_refund.py
@@ -8,7 +8,23 @@ class co_sales_tax_refund(Variable):
     unit = USD
     definition_period = YEAR
     reference = "https://tax.colorado.gov/sites/tax/files/documents/DR_0104_Book_2022.pdf#page=23"
-    defined_for = StateCode.CO
+    defined_for = "co_sales_tax_refund_eligible"
 
     def formula(tax_unit, period, parameters):
-        
+        filing_status = tax_unit("filing_status", period)
+        status = filing_status.possible_values
+        p = parameters(period).gov.states.co.tax.income.credits.sales_tax_refund.amount
+        agi = tax_unit("co_modified_agi", period)
+        return select([ 
+            filing_status == status.SINGLE,
+            filing_status == status.JOINT,
+            filing_status == status.HEAD_OF_HOUSEHOLD,
+            filing_status == status.SEPARATE,
+            filing_status == status.WIDOW,            
+        ],[
+            p.single.calc(agi),
+            p.joint.calc(agi),
+            p.head_of_household.calc(agi),
+            p.separate.calc(agi),
+            p.widow.calc(agi),
+        ],)
--- policyengine_us/variables/gov/states/co/tax/income/credits/sales_tax_refund/co_sales_tax_refund_eligible.py
+++ policyengine_us/variables/gov/states/co/tax/income/credits/sales_tax_refund/co_sales_tax_refund_eligible.py
@@ -2,15 +2,21 @@ from policyengine_us.model_api import *
 
 
 class co_sales_tax_refund_eligible(Variable):
-    value_type = float
+    value_type = bool
     entity = TaxUnit
     label = "Eligible for the Colorado sales tax refund"
-    unit = USD
     definition_period = YEAR
     reference = "https://tax.colorado.gov/sites/tax/files/documents/DR_0104_Book_2022.pdf#page=23"
     defined_for = StateCode.CO
 
     def formula(tax_unit, period, parameters):
-        person = tax_unit.members
         p = parameters(period).gov.states.co.tax.income.credits.sales_tax_refund
-        
+        age_head = tax_unit("age_head", period)
+        head_eligible = age_head >= p.age_threshold
+        age_spouse = tax_unit("age_spouse", period)
+        spouse_eligible = age_spouse >= p.age_threshold
+        income_tax = tax_unit("co_income_tax", period)
+        filing_status = tax_unit("filing_status", period)
+        joint = filing_status == filing_status.possible_values.JOINT
+        age_eligible = where(joint, head_eligible & spouse_eligible, head_eligible)
+        return age_eligible | (income_tax > 0)
