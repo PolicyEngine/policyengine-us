f257ca3344de444ecccaf0d752d762b77ce88be3
PavelMakarchuk <pavel.ma99@gmail.com>
2023-09-01 19:26:08 +0300

    format

--- policyengine_us/variables/gov/states/id/tax/income/deductions/id_deductions.py
+++ policyengine_us/variables/gov/states/id/tax/income/deductions/id_deductions.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class id_deductions(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Idaho dedcutions"
+    label = "Idaho deductions"
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.ID
