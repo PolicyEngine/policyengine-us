ed9532c5581b03921327f4588de0e7eea817ba6d
xianxinzhu <97819588+xianxinzhu@users.noreply.github.com>
2023-09-01 03:52:05 +0000

    Fix variables and parameters

--- policyengine_us/variables/gov/states/hi/tax/income/exemptions/hi_exemptions.py
+++ policyengine_us/variables/gov/states/hi/tax/income/exemptions/hi_exemptions.py
@@ -17,15 +17,47 @@ class hi_exemptions(Variable):
         person = tax_unit.members
 
         num_exemp = tax_unit("exemptions", period)
-        is_disabled = person("is_disabled", period)
-        is_spouse = person("is_tax_unit_spouse", period)
-        spouse_disabled = tax_unit("spouse_is_disabled", period)
-        spouse_aged = tax_unit("aged_spouse", period)
+        
+        head_exemptions = where(
+            person("head_is_disabled", period),
+            num_exemp * p.disabled,       # head is disabled
+            num_exemp * p.base  # regular exemptions
+        )
+
+        spouse_exemptions = where(
+            person("is_tax_unit_spouse", period),    # the tax unit is an individual
+            where(
+                person("spouse_is_disabled", period),   # the spouse is disabled
+                num_exemp * p.disabled,
+                where(
+                    person("aged_spouse", period),                 # the spouse is aged
+                    (num_exemp + 1) * p.base,
+                    num_exemp * p.base
+                )
+            ),
+            0
+        )
+
+        return head_exemptions + spouse_exemptions
+        
+
+
+
+
+
+
+
+        base_amount = exemptions * base parameter
+        head_disabled = where(head_is_disabled, ....)
+        spouse_disabled = where(spouse_is_disabled, ....)
+        return x + y+ z
+
+
 
         return where(
-            is_disabled,  # If the tax unit is disabled
+            head_disabled,  # If the tax unit is disabled
             where(
-                is_spouse,  # If the tax unit is an individual
+                spouse_disabled,  # If the tax unit is an individual
                 where(
                     spouse_disabled,  # If the spouse of the tax unit is disabled
                     p.disability_exemptions.disabled_spouse
@@ -38,7 +70,7 @@ class hi_exemptions(Variable):
                         * num_exemp,  # 8_144
                     ),
                 ),
-                p.disability_exemptions.individual * num_exemp,  # 7_000
+                p.disabled * num_exemp,  # 7_000
             ),
-            p.regular_exemptions * num_exemp,  # 1_144
+            p.base * num_exemp,  # 1_144
         )
