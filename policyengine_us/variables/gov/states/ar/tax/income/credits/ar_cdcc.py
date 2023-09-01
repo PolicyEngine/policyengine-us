f004753ac9eedbb9784579a5525aa4f8c19941fe
Jingjing Wang <87988673+Jingj-WANG@users.noreply.github.com>
2023-09-01 20:25:45 +0000

    readme

--- policyengine_us/variables/gov/states/ar/tax/income/credits/ar_cdcc.py
+++ policyengine_us/variables/gov/states/ar/tax/income/credits/ar_cdcc.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class ar_cdcc(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Arkansas Child Care Expenses Credit"
+    label = "Arkansas Child and Dependent Care Credit"
     unit = USD
     documentation = "https://codes.findlaw.com/ar/title-26-taxation/ar-code-sect-26-51-502/"
     definition_period = YEAR
