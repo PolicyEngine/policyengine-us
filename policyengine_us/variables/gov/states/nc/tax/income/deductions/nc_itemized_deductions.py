910172cd594586ddd6b8e48ae710055d66991978
PavelMakarchuk <pavel.ma99@gmail.com>
2023-09-03 19:42:26 +0300

    typos

--- policyengine_us/variables/gov/states/nc/tax/income/deductions/nc_itemized_deductions.py
+++ policyengine_us/variables/gov/states/nc/tax/income/deductions/nc_itemized_deductions.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class nc_itemized_deductions(Variable):
     value_type = float
     entity = TaxUnit
-    label = "North Carolina itemized deduction"
+    label = "North Carolina itemized deductions"
     unit = USD
     definition_period = YEAR
     reference = "https://www.ncdor.gov/taxes-forms/individual-income-tax/north-carolina-standard-deduction-or-north-carolina-itemized-deductions "
