7e3f30fe0ad9b1f3020da2ffb85ebe0439ebe386
JudyZ13 <112495414+JudyZ13@users.noreply.github.com>
2023-09-04 15:28:35 -0500

    Update policyengine_us/variables/gov/states/az/tax/income/credits/property_tax_credit/az_property_tax_credit_eligible.py
    Co-authored-by: Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/states/az/tax/income/credits/property_tax_credit/az_property_tax_credit_eligible.py
+++ policyengine_us/variables/gov/states/az/tax/income/credits/property_tax_credit/az_property_tax_credit_eligible.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class az_property_tax_credit_eligible(Variable):
     value_type = bool
     entity = TaxUnit
-    label = "Arizona Property Tax Credit Eligibility"
+    label = "Eligible for the Arizona Property Tax Credit"
     definition_period = YEAR
     defined_for = StateCode.AZ
 
