cb6b0e8628896d6ad25740844019b9b0a701af33
xianxinzhu <97819588+xianxinzhu@users.noreply.github.com>
2023-09-05 04:16:36 +0000

    Fixes parameters, tests and variables

--- policyengine_us/variables/gov/states/hi/tax/income/exemptions/hi_exemptions.py
+++ policyengine_us/variables/gov/states/hi/tax/income/exemptions/hi_exemptions.py
@@ -14,27 +14,35 @@ class hi_exemptions(Variable):
 
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states.hi.tax.income.exemptions
-
         person = tax_unit.members
-
         exemp = tax_unit("exemptions", period)
-        
-        disabled_head = tax_unit("head_is_disabled", period).astype(int)
 
+        disabled_head = tax_unit("head_is_disabled", period).astype(int)
         disabled_spouse = tax_unit("spouse_is_disabled", period).astype(int)
 
-        aged_head = person("age_head", period) >= p.age_theshold.astype(int)
+        aged_head = (tax_unit("age_head", period) >= p.age_threshold).astype(
+            int
+        )
+        aged_spouse = (
+            tax_unit("age_spouse", period) >= p.age_threshold
+        ).astype(int)
 
-        aged_spouse = person("age_spouse", period) >= p.age_theshold.astype(int)
-
-        head_total_exemptions = where(disabled_head == 1, exemp - disabled_head, exemp +  aged_head)
-
-        spouse_total_exemptions = where(disabled_spouse == 1, exemp - disabled_spouse, exemp +  aged_spouse)
-
-        exemption_base_amount = (head_total_exemptions + spouse_total_exemptions) * p.base
+        adjusted_exemp = where(
+            disabled_head == 1, exemp - disabled_head, exemp + aged_head
+        )
+        base_exemp = where(
+            disabled_spouse == 1,
+            adjusted_exemp - disabled_spouse,
+            adjusted_exemp + aged_spouse,
+        )
 
         disabled_exemptions = (disabled_head + disabled_spouse) * p.disabled
+        exemption_base_amount = base_exemp * p.base
 
+        # head_total_exemptions = where(disabled_head == 1, exemp - disabled_head, exemp +  aged_head)
+        # spouse_total_exemptions = where(disabled_spouse == 1, exemp - disabled_spouse, exemp +  aged_spouse)
+
+        # exemption_base_amount = (head_total_exemptions + spouse_total_exemptions) * p.base
+        # disabled_exemptions = (disabled_head + disabled_spouse) * p.disabled
 
         return exemption_base_amount + disabled_exemptions
-        
