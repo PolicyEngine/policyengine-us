7c4333dc351b59a2e1431e6a2c38d0b8d7c63334
Diana Shi <dianashi0607@gmail.com>
2023-09-05 00:07:48 +0000

    Adjustment

--- policyengine_us/variables/gov/states/ga/tax/income/deductions/ga_deductions.py
+++ policyengine_us/variables/gov/states/ga/tax/income/deductions/ga_deductions.py
@@ -6,5 +6,15 @@ class ga_deductions(Variable):
     entity = TaxUnit
     label = "Georgia deductions"
     unit = USD
+    reference = (
+        "https://law.justia.com/codes/georgia/2022/title-48/chapter-7/article-2/section-48-7-27/",
+        "https://dor.georgia.gov/document/document/2022-it-511-individual-income-tax-booklet/download",
+    )
     definition_period = YEAR
     defined_for = StateCode.GA
+
+    def formula(tax_unit, period, parameters):
+        itemizes = tax_unit("tax_unit_itemizes", period)
+        sd = tax_unit("ga_standard_deduction", period)
+        itemized = tax_unit("itemized_deductions_less_salt", period)
+        return where(itemizes, itemized, sd)
