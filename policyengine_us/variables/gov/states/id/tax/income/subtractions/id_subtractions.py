a020e0994065333078d3b6b9b8e9b75c33e1f363
jialyu0411 <98068600+jialyu0411@users.noreply.github.com>
2023-09-06 18:08:15 +0000

    Idaho household and dependent care deduction Fixes #2921
    Co-authored-by: Pavel Makarchuk <PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/states/id/tax/income/subtractions/id_subtractions.py
+++ policyengine_us/variables/gov/states/id/tax/income/subtractions/id_subtractions.py
@@ -1,11 +1,11 @@
 from policyengine_us.model_api import *
 
 
-class id_deductions(Variable):
+class id_subtractions(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Idaho dedcutions"
+    label = "Idaho subtractions"
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.ID
-    adds = "gov.states.id.tax.income.deductions.deductions"
+    adds = "gov.states.id.tax.income.subtractions.subtractions"
