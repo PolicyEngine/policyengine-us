5c287f2e4fd6158d1d063815110777dc5c51a764
PavelMakarchuk <pavel.ma99@gmail.com>
2023-09-06 22:42:06 +0300

    Cost of attending college variable adjustment Fixes #2959

--- policyengine_us/variables/household/expense/education/cost_of_attending_college.py
+++ policyengine_us/variables/household/expense/education/cost_of_attending_college.py
@@ -4,5 +4,5 @@ from policyengine_us.model_api import *
 class cost_of_attending_college(Variable):
     value_type = float
     entity = Person
-    label = "Pell Grant cost of attendance"
+    label = "Cost of college attendance"
     definition_period = YEAR
