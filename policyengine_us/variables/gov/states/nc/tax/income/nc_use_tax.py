4fbb3c75ab85c16c2625279fc5194ae6e8038b6f
TurnerLuo <97751137+TurnerLuo@users.noreply.github.com>
2023-09-06 23:36:24 +0000

    North Carolina Use Tax Fixes #2761

--- policyengine_us/variables/gov/states/nc/tax/income/nc_use_tax.py
+++ policyengine_us/variables/gov/states/nc/tax/income/nc_use_tax.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class nc_use_tax(Variable):
     value_type = float
     entity = TaxUnit
-    label = "NC use tax"
+    label = "North Carolina use tax"
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.NC
