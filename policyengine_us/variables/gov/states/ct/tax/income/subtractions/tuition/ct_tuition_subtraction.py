c7ec87afa6593237735ee5706666fa106dd4e1da
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2023-09-06 13:05:03 +0000

    Adding unit tests and minor fixes

--- policyengine_us/variables/gov/states/ct/tax/income/subtractions/tuition/ct_tuition_subtraction.py
+++ policyengine_us/variables/gov/states/ct/tax/income/subtractions/tuition/ct_tuition_subtraction.py
@@ -1,19 +1,18 @@
 from policyengine_us.model_api import *
 
 
-class ct_tuition_subtractions(Variable):
+class ct_tuition_subtraction(Variable):
     value_type = float
     entity = TaxUnit
-    label = "CT tuition subtractions"
+    label = "Connecticut tuition subtraction"
     unit = USD
     definition_period = YEAR
     reference = "https://www.cga.ct.gov/current/pub/chap_229.htm#sec_12-701a"
     defined_for = StateCode.CT
 
     def formula(tax_unit, period, parameters):
-        p = parameters(period).gov.states.ct.tax.income.subtractions.tuition.max_amount
+        p = parameters(period).gov.states.ct.tax.income.subtractions.tuition
         filing_status = tax_unit("filing_status", period)
-        max_amount = p[filing_status]
+        max_amount = p.max_amount[filing_status]
         tuition = add(tax_unit, period, ["qualified_tuition_expenses"])
-        subtraction = min(tuition, max_amount)
-        return subtraction
+        return min_(tuition, max_amount)
