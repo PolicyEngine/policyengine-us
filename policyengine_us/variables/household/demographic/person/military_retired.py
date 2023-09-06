865ce2b8447e499f32e6987996fd3a6753c910f2
ShiqiLiu <54519061+Kiki-67@users.noreply.github.com>
2023-09-06 06:58:55 +0000

    delete useless file

--- policyengine_us/variables/household/demographic/person/military_retired.py
+++
@@ -1,8 +0,0 @@
-from policyengine_us.model_api import *
-
-
-class military_retired(Variable):
-    value_type = bool
-    entity = Person
-    label = "Tax unit head is retired from United State military"
-    definition_period = YEAR
