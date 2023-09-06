7d0b6fac439eb5e0052af3c3e35712cd81b518d8
jialyu0411 <98068600+jialyu0411@users.noreply.github.com>
2023-09-06 18:13:20 +0000

    fix label

--- policyengine_us/variables/gov/states/id/tax/income/subtractions/id_subtractions.py
+++ policyengine_us/variables/gov/states/id/tax/income/subtractions/id_subtractions.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class id_subtractions(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Idaho deductions"
+    label = "Idaho subtractions"
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.ID
