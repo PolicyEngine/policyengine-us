cd7e1edf50bf1ca181203e2be36c9853afc1027e
WeihaoZ724 <128203510+WeihaoZ724@users.noreply.github.com>
2023-09-06 18:29:33 +0000

    Kentucky homestead exemption fixes

--- policyengine_us/variables/gov/states/ky/tax/income/exemptions/ky_homestead_exemption.py
+++ policyengine_us/variables/gov/states/ky/tax/income/exemptions/ky_homestead_exemption.py
@@ -13,5 +13,5 @@ class ky_homestead_exemption(Variable):
         p = parameters(period).gov.states.ky.tax.income.exemptions.homestead
         exemption_amount = p.amount
         #Add Property Value
-        property_value = tax_unit("assessed_property_value", period)
-        return max_(exemption_amount, property_value)
+        property_value = add(tax_unit, period, ["assessed_property_value"])
+        return min_(exemption_amount, property_value)
