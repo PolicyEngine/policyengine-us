65bb34b4eefec36d0a3c507be5697f6cd0468081
Ruby Ren <38923159+YichunRen@users.noreply.github.com>
2023-09-07 21:01:56 +0000

    add reference

--- policyengine_us/variables/gov/states/hi/tax/income/credits/cdcc/dependent_care_benefit/hi_min_head_spouse_earned.py
+++ policyengine_us/variables/gov/states/hi/tax/income/credits/cdcc/dependent_care_benefit/hi_min_head_spouse_earned.py
@@ -4,11 +4,16 @@ from policyengine_us.model_api import *
 class hi_min_head_spouse_earned(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Hawaii minimum income between head and spouse"
+    label = "Hawaii minimum income between head and spouse for the CDCC"
     defined_for = StateCode.HI
     unit = USD
     definition_period = YEAR
-    reference = "https://files.hawaii.gov/tax/forms/2022/n11ins.pdf#page=28"
+    reference = (
+        "https://files.hawaii.gov/tax/forms/2022/n11ins.pdf#page=28"
+        "https://files.hawaii.gov/tax/forms/2022/n11ins.pdf#page=29"
+        "https://files.hawaii.gov/tax/legal/hrs/hrs_235.pdf#page=41"
+        "https://files.hawaii.gov/tax/forms/2022/schx_i.pdf#page=2"
+    )
 
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states.hi.tax.income.credits.cdcc
