3e4e2efb33092163aa01f8da4165db161dc6872f
Zixuan Ma <66671734+mzx-4real@users.noreply.github.com>
2023-09-07 22:32:45 +0800

    Refine vt capital gain exclusion Fixes #2624

--- policyengine_us/variables/gov/states/vt/tax/income/adjusted_gross_income/subtractions/vt_capital_gain_exclusion.py
+++ policyengine_us/variables/gov/states/vt/tax/income/adjusted_gross_income/subtractions/vt_capital_gain_exclusion.py
@@ -10,17 +10,29 @@ class vt_capital_gain_exclusion(Variable):
     definition_period = YEAR
     defined_for = StateCode.VT
     reference = (
-        "https://tax.vermont.gov/sites/tax/files/documents/IN-112-2022.pdf#page=1"  # PART 1 SUBTRACTIONS TO FEDERAL ADJUSTED GROSS INCOME
+        "https://tax.vermont.gov/sites/tax/files/documents/IN-153-2022.pdf#page=1"  # 2022 Schedule IN-153 Vermont Capital Gains Exclusion Calculation
         "https://legislature.vermont.gov/statutes/section/32/151/05811"  # Titl. 32 V.S.A. § 5811(21)(B)(ii)
-        "https://tax.vermont.gov/sites/tax/files/documents/IN-112%20Instr-2022.pdf"
+        "https://tax.vermont.gov/sites/tax/files/documents/IN-153%20Instr-2022.pdf"
     )
 
     def formula(tax_unit, period, parameters):
-        capital_gains_excluded_from_taxable_income = tax_unit(
-            "capital_gains_excluded_from_taxable_income", period
+        # Get adjusted net capital gain
+        adjusted_net_capital_gain = tax_unit(
+            "adjusted_net_capital_gain", period
         )
         p = parameters(
             period
         ).gov.states.vt.tax.income.agi.capital_gain_exclusion
-        # The exclusion amount have a maximum value
-        return min_(capital_gains_excluded_from_taxable_income, p.max)
+        # The flat exclusion is the less of $5,000 or the actual amount of net adjusted capital gains
+        flat_exclusion = min_(adjusted_net_capital_gain, p.flat.max)
+        # The percentage exclusion equals to 40% of the adjusted net capital gain and has a maximum value
+        percentage_exclusion = (
+            adjusted_net_capital_gain * p.percentage.percentage
+        )
+        percentage_exclusion = min_(percentage_exclusion, p.percentage.max)
+        # Filer can choose from flat or percentage exclusion. Assume the filer will always choose the larger one
+        chosen_exclusion = max_(flat_exclusion, percentage_exclusion)
+        # The chosen exclusion should not exceed 40% of federal taxable income
+        federal_taxable_income = tax_unit("taxable_income", period)
+        cap = federal_taxable_income * p.cap
+        return min_(chosen_exclusion, cap)
