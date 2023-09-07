130bec054a7109f6c814503b46bb27acc535ccef
qingqiuzhang <90161322+qingqiuzhang@users.noreply.github.com>
2023-09-07 02:56:00 +0000

    add 2020 & 2021 values

--- policyengine_us/variables/gov/states/wv/tax/income/subtractions/wv_social_security_benefits_subtraction.py
+++ policyengine_us/variables/gov/states/wv/tax/income/subtractions/wv_social_security_benefits_subtraction.py
@@ -10,8 +10,12 @@ class wv_social_security_benefits_subtraction(Variable):
     reference = "https://tax.wv.gov/Documents/PIT/2022/PersonalIncomeTaxFormsAndInstructions.2022.pdf#Page=25"
     defined_for = "wv_social_security_benefits_subtraction_eligible"
 
-    def formula(tax_unit, period):
-        # social_security is for person, not tax_unit, fix this
-        return tax_unit(
+    def formula(tax_unit, period, parameters):
+        p = parameters(
+            period
+        ).gov.states.wv.tax.income.subtractions.social_security_benefits
+        person = tax_unit.members
+        amount = person(
             "social_security", period # or use taxable_social_security?
-        )
+        ) * p.rate
+        return tax_unit.sum(amount)
