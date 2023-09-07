67c5ae4686915661ab403abc1ce8272b04d0c5d2
hua7450 <113824411+hua7450@users.noreply.github.com>
2023-09-07 15:51:42 +0000

    add sc_retirement_deduction_survivors.py, add a boolean parameter subtract_military.yaml Fixes #2926

---
+++ policyengine_us/variables/gov/states/sc/tax/income/subtractions/retirement_deduction/sc_retirement_deduction_indv.py
@@ -0,0 +1,43 @@
+from policyengine_us.model_api import *
+
+
+class sc_retirement_deduction_indv(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "South Carolina retirement deduction for eligible individuals"
+    defined_for = StateCode.SC
+    unit = USD
+    reference = (
+        "https://www.scstatehouse.gov/code/t12c006.php",  # SECTION 12-6-1170(A)(1)
+        "https://dor.sc.gov/forms-site/Forms/IITPacket_2021.pdf#page=1",
+    )
+    definition_period = YEAR
+
+    def formula(tax_unit, period, parameters):
+        p = parameters(
+            period
+        ).gov.states.sc.tax.income.subtractions.retirement_deduction
+        p_cap = p.max_amount
+        person = tax_unit.members
+        age = person("age", period)
+        head = person("is_tax_unit_head", period)
+        spouse = person("is_tax_unit_spouse", period)
+        head_or_spouse = head | spouse
+        # line 1
+        max_deduction_allowed = where(
+            age >= p.age_threshold,
+            p_cap.older,
+            p_cap.younger,
+        )
+        # line 2
+        military_retirement_pay = person("military_retirement_pay", period)
+        # line 3
+        retirement_deduction_available = max_(
+            max_deduction_allowed - military_retirement_pay, 0
+        )
+        # line 4
+        retirement_income = person("taxable_pension_income", period)
+        # line 5
+        return tax_unit.sum(
+            min_(retirement_deduction_available, retirement_income)
+        )
---
+++ policyengine_us/variables/gov/states/sc/tax/income/subtractions/retirement_deduction/sc_retirement_deduction_survivors.py
@@ -0,0 +1,43 @@
+from policyengine_us.model_api import *
+
+
+class sc_retirement_deduction_survivors(Variable):
+    value_type = float
+    entity = Person
+    label = "South Carolina retirement deduction for survivors"
+    defined_for = StateCode.SC
+    unit = USD
+    reference = (
+        "https://www.scstatehouse.gov/code/t12c006.php",  # SECTION 12-6-1170(A)(1)
+        "https://dor.sc.gov/forms-site/Forms/IITPacket_2021.pdf#page=17",
+    )
+    definition_period = YEAR
+
+    def formula(person, period, parameters):
+        p = parameters(
+            period
+        ).gov.states.sc.tax.income.subtractions.retirement_deduction
+        p_cap = p.max_amount
+        age = person("age", period)
+        # line 1
+        max_deduction_allowed = where(
+            age >= p.age_threshold,
+            p_cap.older,
+            p_cap.younger,
+        )
+        # line 2
+        military_retirement_pay_survivors = person(
+            "military_retirement_pay_survivors", period
+        )
+        # line 3
+        retirement_deduction_available = max_(
+            max_deduction_allowed - military_retirement_pay_survivors, 0
+        )
+        # line 4
+        retirement_income_survivors = person("pension_survivors", period)
+
+        if p.subtract_military:
+            return min_(
+                retirement_deduction_available, retirement_income_survivors
+            )
+        return min_(max_deduction_allowed, retirement_income_survivors)
