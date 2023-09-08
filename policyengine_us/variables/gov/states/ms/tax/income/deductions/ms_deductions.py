ff9948d49579893c8c77921bc805156456dae302
Xinyi-Shao <ellashao99@gmail.com>
2023-09-08 15:14:24 -0400

    updated format

--- policyengine_us/variables/gov/states/ms/tax/income/deductions/ms_deductions.py
+++ policyengine_us/variables/gov/states/ms/tax/income/deductions/ms_deductions.py
@@ -7,9 +7,7 @@ class ms_deductions(Variable):
     label = "Mississippi deductions"
     unit = USD
     definition_period = YEAR
-    reference = (
-        "https://www.dor.ms.gov/sites/default/files/Forms/Individual/80108228.pdf#page=1"
-    )
+    reference = "https://www.dor.ms.gov/sites/default/files/Forms/Individual/80108228.pdf#page=1"
     defined_for = StateCode.MS
 
     def formula(tax_unit, period, parameters):
