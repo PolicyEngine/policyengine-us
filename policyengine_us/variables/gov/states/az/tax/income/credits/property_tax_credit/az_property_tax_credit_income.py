28ecedc886716272fd690cd63dea56df20339a41
JudyZ13 <112495414+JudyZ13@users.noreply.github.com>
2023-09-08 16:30:53 -0500

    Update policyengine_us/variables/gov/states/az/tax/income/credits/property_tax_credit/az_property_tax_credit_income.py
    Co-authored-by: Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/states/az/tax/income/credits/property_tax_credit/az_property_tax_credit_income.py
+++ policyengine_us/variables/gov/states/az/tax/income/credits/property_tax_credit/az_property_tax_credit_income.py
@@ -9,4 +9,4 @@ class az_property_tax_credit_income(Variable):
     definition_period = YEAR
     defined_for = StateCode.AZ
 
-    adds = ("gov.states.az.tax.income.property_tax_credits.income_sources",)
+    adds = "gov.states.az.tax.income.property_tax_credits.income_sources"
