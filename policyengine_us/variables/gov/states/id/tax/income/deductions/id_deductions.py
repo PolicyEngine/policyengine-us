78283097fd95721dcbf650281afd425c05a1a33a
PavelMakarchuk <pavel.ma99@gmail.com>
2023-09-10 04:08:01 +0300

    max function in variable

--- policyengine_us/variables/gov/states/id/tax/income/deductions/id_deductions.py
+++ policyengine_us/variables/gov/states/id/tax/income/deductions/id_deductions.py
@@ -7,11 +7,13 @@ class id_deductions(Variable):
     label = "Idaho deductions"
     unit = USD
     definition_period = YEAR
-    reference = "https://legislature.idaho.gov/statutesrules/idstat/Title63/T63CH30/SECT63-3022/" # (j)
+    reference = (
+        "https://legislature.idaho.gov/statutesrules/idstat/Title63/T63CH30/SECT63-3022/",  # (j)
+        "file:///Users/pavelmakarchuk/Desktop/PolicyEngine/Tax%20Forms/Idaho/EFO00089_12-30-2022.pdf#page=4",
+    )
     defined_for = StateCode.ID
 
     def formula(tax_unit, period, parameters):
-        itemizes = tax_unit("tax_unit_itemizes", period)
         itm_ded = tax_unit("itemized_deductions_less_salt", period)
         std_ded = tax_unit("standard_deduction", period)
-        return where(itemizes, itm_ded, std_ded)
+        return max_(itm_ded, std_ded)
