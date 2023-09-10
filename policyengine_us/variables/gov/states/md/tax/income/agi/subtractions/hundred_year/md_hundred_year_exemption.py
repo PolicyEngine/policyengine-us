1d94c7be88a6237a02ed0cf0245bbca5c3cfe357
SirMalamute <neeliyer14@gmail.com>
2023-09-10 19:29:39 +0000

    Maryland 100 year subtraction Fixes #2818

--- policyengine_us/variables/gov/states/md/tax/income/agi/subtractions/hundred_year/md_hundred_year_exemption.py
+++ policyengine_us/variables/gov/states/md/tax/income/agi/subtractions/hundred_year/md_hundred_year_exemption.py
@@ -3,19 +3,12 @@ from policyengine_us.model_api import *
 
 class md_hundred_year_exemption(Variable):
     value_type = float
-    entity = TaxUnit
+    entity = Person
     label = "Maryland hundred year exemption"
     unit = USD
     definition_period = YEAR
     reference = "https://trackbill.com/bill/maryland-house-bill-186-income-tax-subtraction-modification-for-centenarians/2173534/"
-    defined_for = StateCode.MD
+    defined_for = "md_hundred_year_exemption_eligible"
 
-    def formula(tax_unit, period, parameters):
-        eligible = tax_unit("md_hundred_year_exemption_eligible", period)
-        return (
-            where(
-                eligible, 
-                p.amount,
-                0
-            )
-        )
+    def formula(person, period, parameters):
+        return parameters(period).gov.states.md.tax.income.agi.subtractions.hundred_year.amount
--- policyengine_us/variables/gov/states/md/tax/income/agi/subtractions/hundred_year/md_hundred_year_exemption_eligible.py
+++ policyengine_us/variables/gov/states/md/tax/income/agi/subtractions/hundred_year/md_hundred_year_exemption_eligible.py
@@ -1,15 +1,15 @@
 from policyengine_us.model_api import *
 
 
-class md_hundred_year_exemption(Variable):
+class md_hundred_year_exemption_eligible(Variable):
     value_type = bool
-    entity = TaxUnit
+    entity = Person
     label = "Maryland hundred year exemption eligibility"
     definition_period = YEAR
     reference = "https://trackbill.com/bill/maryland-house-bill-186-income-tax-subtraction-modification-for-centenarians/2173534/"
     defined_for = StateCode.MD
 
-    def formula(tax_unit, period, parameters):
-        age = tax_unit("age_head", period)
+    def formula(person, period, parameters):
+        age = person("age", period)
         p = parameters(period).gov.states.md.tax.income.agi.subtractions.hundred_year
-        return age_head >= p.age_threshold
+        return age >= p.age_threshold
