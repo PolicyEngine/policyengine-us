190335b35ef0c8e17dea983a7685e18f1a8a8d85
Linfeng-ll <95734090+Linfeng-ll@users.noreply.github.com>
2023-09-10 06:29:03 +0000

    make format

--- policyengine_us/variables/gov/states/oh/tax/income/credits/oh_lump_sum_retirement_credit.py
+++ policyengine_us/variables/gov/states/oh/tax/income/credits/oh_lump_sum_retirement_credit.py
@@ -11,9 +11,7 @@ class oh_lump_sum_retirement_credit(Variable):
     defined_for = "oh_retirement_income_credit_eligible"
 
     def formula(tax_unit, period, parameters):
-        p = parameters(
-            period
-        ).gov.states.oh.tax.income.credits
+        p = parameters(period).gov.states.oh.tax.income.credits
 
         person = tax_unit.members
         is_spouse = person("is_tax_unit_spouse", period)
@@ -29,12 +27,22 @@ class oh_lump_sum_retirement_credit(Variable):
         head_pension = tax_unit.sum(is_head * pension_income)
         spouse_pension = tax_unit.sum(is_spouse * pension_income)
 
-        head_credit = p.retirement_income.amount.calc(head_pension / head_divisor, right=True) * head_divisor
-        spouse_credit = p.retirement_income.amount.calc(
-            spouse_pension / spouse_divisor, right=True
-        ) * spouse_divisor
+        head_credit = (
+            p.retirement_income.amount.calc(
+                head_pension / head_divisor, right=True
+            )
+            * head_divisor
+        )
+        spouse_credit = (
+            p.retirement_income.amount.calc(
+                spouse_pension / spouse_divisor, right=True
+            )
+            * spouse_divisor
+        )
 
         # distribution_received = tax_unit("c05700", period) > 0 # received lump sum distribution
-        distribution_received = tax_unit("form_4972_lumpsum_distributions", period) > 0 
-        
+        distribution_received = (
+            tax_unit("form_4972_lumpsum_distributions", period) > 0
+        )
+
         return (head_credit + spouse_credit) if distribution_received else 0
