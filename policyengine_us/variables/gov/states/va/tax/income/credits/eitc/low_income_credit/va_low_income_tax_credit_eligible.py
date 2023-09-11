9c67517665ae6eeff22142321251596d7a9a1e74
jnaux <89711387+jnaux@users.noreply.github.com>
2023-09-11 09:37:20 +0000

    Edited eliglibility calculation

--- policyengine_us/variables/gov/states/va/tax/income/credits/eitc/low_income_credit/va_low_income_tax_credit_eligible.py
+++ policyengine_us/variables/gov/states/va/tax/income/credits/eitc/low_income_credit/va_low_income_tax_credit_eligible.py
@@ -8,7 +8,11 @@ class va_low_income_tax_credit_eligible(Variable):
     definition_period = YEAR
     defined_for = StateCode.VA
 
-    def formula(tax_unit, period, parameters):
-        fpg = tax_unit("tax_unit_fpg", period)
+    def formula(spm_unit, period, parameters):
         agi = tax_unit("va_agi", period)
-        return agi <= fpg
+        n = tax_unit("exemptions", period)
+        state_group = "CONTIGUOUS_US"
+        p_fpg = parameters(period).gov.hhs.fpg
+        p1 = p_fpg.first_person[state_group]
+        pn = p_fpg.additional_person[state_group]
+        return agi <= p1 + pn * (n - 1)
