cbfa689234cb4a7f1395949b6736edbd8a370439
jnaux <89711387+jnaux@users.noreply.github.com>
2023-09-12 00:41:23 +0000

    Changed var input

--- policyengine_us/variables/gov/states/va/tax/income/credits/eitc/low_income_credit/va_low_income_tax_credit_eligible.py
+++ policyengine_us/variables/gov/states/va/tax/income/credits/eitc/low_income_credit/va_low_income_tax_credit_eligible.py
@@ -8,7 +8,7 @@ class va_low_income_tax_credit_eligible(Variable):
     definition_period = YEAR
     defined_for = StateCode.VA
 
-    def formula(spm_unit, period, parameters):
+    def formula(tax_unit, period, parameters):
         agi = tax_unit("va_agi", period)
         n = tax_unit("exemptions", period)
         state_group = "CONTIGUOUS_US"
