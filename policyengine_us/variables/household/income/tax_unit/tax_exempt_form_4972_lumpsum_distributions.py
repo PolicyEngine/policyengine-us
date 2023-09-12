37055213a5854790dde191a439ca8452169133d6
PavelMakarchuk <pavel.ma99@gmail.com>
2023-09-13 02:53:26 +0300

    description

--- policyengine_us/variables/household/income/tax_unit/tax_exempt_form_4972_lumpsum_distributions.py
+++ policyengine_us/variables/household/income/tax_unit/tax_exempt_form_4972_lumpsum_distributions.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class tax_exempt_form_4972_lumpsum_distributions(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Lump-sum distributions reported on IRS Form 4972 not included in federal taxable income"
+    label = "Lump-sum distributions reported on IRS Form 4972 not included in federal adjusted gross income"
     unit = USD
     definition_period = YEAR
-    documentation = "Lump-sum distributions reported on IRS Form 4972 not included in federal taxable income."
+    documentation = "Lump-sum distributions reported on IRS Form 4972 not included in federal adjusted gross income."
