321e5c644de22b2a6ca2baac29ea7b0a30ba260c
xianxinzhu <97819588+xianxinzhu@users.noreply.github.com>
2023-09-12 08:04:05 +0000

    Fixes parameters and variables

--- policyengine_us/variables/gov/states/hi/tax/income/computation/alternative_tax_on_capital_gains.py
+++ policyengine_us/variables/gov/states/hi/tax/income/computation/alternative_tax_on_capital_gains.py
@@ -10,14 +10,22 @@ class alternative_tax_on_capital_gains(Variable):
     defined_for = StateCode.HI
 
     def formula(tax_unit, period, parameters):
+        person = tax_unit.members
+        p = parameters(period).gov.states.hi.tax.income.computation
         taxable_income = tax_unit("taxable_income", period)
-        # federal_lt_capital_gain = max_(0,add(person, period, ["long_term_capital_gains"])) # neagative? 
-        # federal_st_capital_gain = max_(0,add(person, period, ["long_term_capital_gains"])) # negative?
+        net_lt_capital_gain = add(person, period, ["long_term_capital_gains"])
+        net_capital_gain = tax_unit("net_capital_gain", period)
+         # - line_9 how to calculate Net gain from the disposition of property held for investment
+        claimed = p.alternative_tax_claim[filing_status]
+        ineligible_income = max_(
+            taxable_income - min_(net_capital_gain, net_lt_capital_gain),
+            claimed
+        )  #line_13 how to calculate the tax according to Tax Table or Tax Rate Schedules
+        eligible_income = taxable_income - ineligible_income
 
-        federal_net_capital_gain = tax_unit("net_capital_gain", period)
-        # Hawaii_net_capital_gain = lt_adjustments + federal_net capital gain + st_adjustments
+        #ineligible_tax =  line 15 (tax for line 13)
+        #taxable_income_tax =  line 18 (tax for taxable income)
 
-        federal_lt_capital_gain = add(person, period, ["long_term_capital_gains"])
-        # Hawaii_net_lt_capital_gain = federal_lt_capital_gain + lt_adjustments
-        # 
-        return 1
+        alternative_tax = ineligible_tax + eligible_income * p.alternative_tax_rate  #line 17
+    
+        return min_(taxable_income_tax, alternative_tax)
