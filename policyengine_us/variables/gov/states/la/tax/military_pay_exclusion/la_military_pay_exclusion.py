9cd00c0ec800b3506a75bfdfedca93a6cc6ef089
XZ-mabel <131919825+XZ-mabel@users.noreply.github.com>
2023-09-12 05:44:20 +0000

    Added reference

--- policyengine_us/variables/gov/states/la/tax/military_pay_exclusion/la_military_pay_exclusion.py
+++ policyengine_us/variables/gov/states/la/tax/military_pay_exclusion/la_military_pay_exclusion.py
@@ -8,6 +8,7 @@ class la_military_pay_exclusion(Variable):
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.LA
+    reference = "http://www.legis.la.gov/legis/Law.aspx?d=101760"
 
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states.la.tax.military_pay_exclusion
