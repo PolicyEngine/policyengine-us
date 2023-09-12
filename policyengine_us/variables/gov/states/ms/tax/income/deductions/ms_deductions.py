3a417f9463b3c96cd464ad0e14f4888d10c98b8f
Xinyi-Shao <ellashao99@gmail.com>
2023-09-12 14:13:33 -0400

    updated legal code reference

--- policyengine_us/variables/gov/states/ms/tax/income/deductions/ms_deductions.py
+++ policyengine_us/variables/gov/states/ms/tax/income/deductions/ms_deductions.py
@@ -7,7 +7,10 @@ class ms_deductions(Variable):
     label = "Mississippi deductions"
     unit = USD
     definition_period = YEAR
-    reference = "https://www.dor.ms.gov/sites/default/files/Forms/Individual/80108228.pdf#page=1"
+    reference = (
+        "https://www.dor.ms.gov/sites/default/files/Forms/Individual/80108228.pdf#page=1"
+        "https://www.law.cornell.edu/regulations/mississippi/35-Miss-Code-R-SS-3-02-11-103"
+    )
     defined_for = StateCode.MS
 
     def formula(tax_unit, period, parameters):
