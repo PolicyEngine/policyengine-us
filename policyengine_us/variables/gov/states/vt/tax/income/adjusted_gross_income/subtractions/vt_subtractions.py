566df8f1ef4c405c69b0be2adbf1e9bb4cd61099
Zixuan Ma <66671734+mzx-4real@users.noreply.github.com>
2023-09-13 18:15:59 +0800

    Suggested change

--- policyengine_us/variables/gov/states/vt/tax/income/adjusted_gross_income/subtractions/vt_subtractions.py
+++ policyengine_us/variables/gov/states/vt/tax/income/adjusted_gross_income/subtractions/vt_subtractions.py
@@ -10,9 +10,9 @@ class vt_subtractions(Variable):
     definition_period = YEAR
     defined_for = StateCode.VT
     reference = (
-        "https://tax.vermont.gov/sites/tax/files/documents/IN-112-2022.pdf#page=1"  # PART 1 SUBTRACTIONS TO FEDERAL ADJUSTED GROSS INCOME
-        "https://legislature.vermont.gov/statutes/section/32/151/05811"  # Titl. 32 V.S.A. § 5811(21)(B)(i), (C)(iv), (B)(vi), (B)(ii), (B)(iv)
-        "https://tax.vermont.gov/sites/tax/files/documents/IN-112%20Instr-2022.pdf"
+        "https://tax.vermont.gov/sites/tax/files/documents/IN-112-2022.pdf#page=1",  # PART 1 SUBTRACTIONS TO FEDERAL ADJUSTED GROSS INCOME
+        "https://legislature.vermont.gov/statutes/section/32/151/05811",  # Titl. 32 V.S.A. § 5811(21)(B)(i), (C)(iv), (B)(vi), (B)(ii), (B)(iv)
+        "https://tax.vermont.gov/sites/tax/files/documents/IN-112%20Instr-2022.pdf",
     )
     # Get parameter list
     adds = "gov.states.vt.tax.income.agi.subtractions"
