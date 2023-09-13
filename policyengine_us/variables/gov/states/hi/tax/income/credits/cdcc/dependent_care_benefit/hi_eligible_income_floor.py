2caf946e89d743feef9a42ceb3808fafaf56304e
Ruby Ren <38923159+YichunRen@users.noreply.github.com>
2023-09-13 19:35:27 +0000

    add eligible_income_floor

--- policyengine_us/variables/gov/states/hi/tax/income/credits/cdcc/dependent_care_benefit/hi_eligible_income_floor.py
+++ policyengine_us/variables/gov/states/hi/tax/income/credits/cdcc/dependent_care_benefit/hi_eligible_income_floor.py
@@ -1,10 +1,10 @@
 from policyengine_us.model_api import *
 
 
-class hi_income_floor_eligible(Variable):
+class hi_eligible_income_floor(Variable):
     value_type = float
     entity = Person
-    label = "Hawaii income floor eligible"
+    label = "Hawaii eligible income floor"
     defined_for = StateCode.HI
     unit = USD
     definition_period = YEAR
@@ -16,7 +16,10 @@ class hi_income_floor_eligible(Variable):
     )
 
     def formula(person, period, parameters):
-
+        p = parameters(period).gov.states.hi.tax.income.credits.cdcc
+        qualified_children = person.tax_unit("count_cdcc_eligible", period)
+        # Floor depends on number of eligible dependents
+        income_floor = p.disabled_student_income_floor.calc(qualified_children)
         income = person("earned_income", period)
 
-        return income
+        return max_(income, income_floor)
--- policyengine_us/variables/gov/states/hi/tax/income/credits/cdcc/dependent_care_benefit/hi_min_head_spouse_earned.py
+++ policyengine_us/variables/gov/states/hi/tax/income/credits/cdcc/dependent_care_benefit/hi_min_head_spouse_earned.py
@@ -16,7 +16,6 @@ class hi_min_head_spouse_earned(Variable):
     )
 
     def formula(tax_unit, period, parameters):
-        p = parameters(period).gov.states.hi.tax.income.credits.cdcc
         person = tax_unit.members
         head = person("is_tax_unit_head", period)
         spouse = person("is_tax_unit_spouse", period)
@@ -26,37 +25,27 @@ class hi_min_head_spouse_earned(Variable):
             person("is_disabled", period)
             | person("is_full_time_student", period)
         )
-        qualified_children = tax_unit("count_cdcc_eligible", period)
-
-        # Floor depends on number of eligible dependents
-        income_floor = p.disabled_student_income_floor.calc(qualified_children)
         income = person("earned_income", period)
-        head_or_spouse_income = head_or_spouse * income
-        increased_income = max_(head_or_spouse_income, income_floor)
+        # increased_income = max_(head_or_spouse_income, income_floor)
+        increased_income = person("hi_eligible_income_floor", period)
         uncapped_income = where(
             income_floor_eligible,
             increased_income,
-            head_or_spouse_income,
+            income,
         )
-        # remove impact of 0 income output for dependents
+        # remove impact of income for dependents
         head_spouse_income = where(
             head_or_spouse, uncapped_income, tax_unit.max(uncapped_income)
         )
         # Edge case: both spouses were students or disabled:
-        # compare original income with eligible income
-        both_disabled_income = where(
-            head_or_spouse,
-            min_(head_or_spouse_income, uncapped_income),
-            head_spouse_income,
-        )
         # If both filers are disabled / student below the floor limit,
-        # only one person gets elevated to the floor
+        # only one person with larger earning gets elevated to the floor
         # If both filers are disabled / student but only one person is below the floor limit,
-        # then the person with the lower earnings will be elevated to the floor
-        reach_income_floor = head_or_spouse_income < uncapped_income
+        # then the person below the floor limit will be elevated to the floor
+        reach_income_floor = income < uncapped_income
         head_spouse_income = where(
-            (sum(income_floor_eligible) == 2) & (sum(reach_income_floor) == 2),
-            both_disabled_income,
+            (sum(income_floor_eligible) == 2) & (sum(reach_income_floor) > 1),
+            min_(income, uncapped_income),
             head_spouse_income,
         )
         return tax_unit.min(head_spouse_income)
