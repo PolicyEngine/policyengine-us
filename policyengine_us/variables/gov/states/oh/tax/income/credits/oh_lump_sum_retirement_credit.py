f6d00796a74940f54398936f282af95910451e7d
Linfeng-ll <95734090+Linfeng-ll@users.noreply.github.com>
2023-09-13 14:34:19 +0000

    Ohio lump sum retirement credit Fixes #2958

--- policyengine_us/variables/gov/states/oh/tax/income/credits/oh_lump_sum_retirement_credit.py
+++ policyengine_us/variables/gov/states/oh/tax/income/credits/oh_lump_sum_retirement_credit.py
@@ -20,24 +20,34 @@ class oh_lump_sum_retirement_credit(Variable):
         pension_income = person("pension_income", period)
         age_head = tax_unit("age_head", period)
         age_spouse = tax_unit("age_spouse", period)
+        head_eligible = where(
+            age_head >= p.lump_sum_retirement.age_threshold, 1, 0
+        )
+        spouse_eligible = where(
+            age_spouse >= p.lump_sum_retirement.age_threshold, 1, 0
+        )
 
-        head_divisor = p.lump_sum_retirement_multiple.calc(age_head)
-        spouse_divisor = p.lump_sum_retirement_multiple.calc(age_spouse)
+        head_divisor = p.lump_sum_retirement.multiple.calc(age_head)
+        spouse_divisor = p.lump_sum_retirement.multiple.calc(age_spouse)
 
         head_pension = tax_unit.sum(is_head * pension_income)
         spouse_pension = tax_unit.sum(is_spouse * pension_income)
 
-        head_credit = (
+        head_credit = where(
+            head_divisor == 0,
+            0,
             p.retirement_income.amount.calc(
                 head_pension / head_divisor, right=True
             )
-            * head_divisor
+            * head_divisor,
         )
-        spouse_credit = (
+        spouse_credit = where(
+            spouse_divisor == 0,
+            0,
             p.retirement_income.amount.calc(
                 spouse_pension / spouse_divisor, right=True
             )
-            * spouse_divisor
+            * spouse_divisor,
         )
 
         # distribution_received = tax_unit("c05700", period) > 0 # received lump sum distribution
@@ -45,4 +55,8 @@ class oh_lump_sum_retirement_credit(Variable):
             tax_unit("form_4972_lumpsum_distributions", period) > 0
         )
 
-        return (head_credit + spouse_credit) if distribution_received else 0
+        return np.where(
+            distribution_received,
+            head_credit * head_eligible + spouse_credit * spouse_eligible,
+            0,
+        )
