e5bae2e24fd4b1085d40c690331bafcf89c136a8
qingqiuzhang <90161322+qingqiuzhang@users.noreply.github.com>
2023-09-14 20:47:00 +0000

    update varaibles and tests

--- policyengine_us/variables/gov/states/co/cccap/co_ccap_entry_eligible.py
+++ policyengine_us/variables/gov/states/co/cccap/co_ccap_entry_eligible.py
@@ -1,29 +1,34 @@
 from policyengine_us.model_api import *
 
 
-class co_ccap_eligible(Variable):
-    value_type = bool
+# class co_ccap_entry_eligible(Variable):
+#     # value_type = str
+#     entity = TaxUnit
+#     label = "Colorado"
+#     unit = USD
+#     definition_period = YEAR
+#     defined_for = StateCode.CO
+
+#     def formula(tax_unit, period, parameters):
+#         p = parameters(period).gov.states.co.cccap
+        
+#         # agi = tax_unit("adjusted_gross_income", period)
+#         # county = tax_unit.household("county_str", period)
+#         # print(county)
+#         # return county
+#         # county = tax_unit.household("county_str", period)
+#         return p.entry_threshold.ADAMS_COUNTY_CO
+
+
+class co_ccap_entry_eligible(Variable):
+    value_type = float
     entity = TaxUnit
-    label = "Colorado TANF eligible"
+    label = "Colorado income tax before non-refundable credits"
+    unit = USD
     definition_period = YEAR
     defined_for = StateCode.CO
 
     def formula(tax_unit, period, parameters):
-        p = parameters(period).gov.states.co.cccap
-        agi = tax_unit("adjusted_gross_income", period)
-        fpg = parameters(period).gov.hhs.fpg
-        fpg_rate = p.entry_threshold
-        spm_unit = tax_unit.spm_unit
-        smi = spm_unit("hhs_smi", period)
-        smi_rate = p.initial_income_eligibility
-        income_eligible = (agi < smi * smi_rate) & (agi < fpg * fpg_rate)
-
-        person = spm_unit.members
-         
-        disabled = person("is_disabled", period)
-        if disabled:
-            child_age_eligible = person("age", period) < p.disabled_child_age_limit
-        else:
-            child_age_eligible = person("age", period) < p.age_limit
-        return income_eligible & child_age_eligible
-
+        income = tax_unit("co_taxable_income", period)
+        rate = parameters(period).gov.states.co.tax.income.rate
+        return income * rate
