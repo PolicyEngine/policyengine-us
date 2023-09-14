46c1d21066af92bbfa0ff83611d91e64eb89d286
Shayla Wu <125515687+ShaylaWoo@users.noreply.github.com>
2023-09-14 23:06:58 +0000

    fix

---
+++ policyengine_us/variables/gov/states/mt/tax/income/mt_disqualified_income.py
@@ -0,0 +1,11 @@
+from policyengine_us.model_api import *
+
+
+class mt_disqualified_income(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Montana disqualified income"
+    unit = USD
+    definition_period = YEAR
+    defined_for = StateCode.MT
+    adds = gov.states.mt.tax.income.credits.ctc.income.disqualified_income
