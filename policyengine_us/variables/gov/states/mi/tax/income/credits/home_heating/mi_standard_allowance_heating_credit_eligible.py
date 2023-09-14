ca33d87feafb4622b996f0cc9f8868d5e2edef6f
keepjojoyoung <106553218+keepjojoyoung@users.noreply.github.com>
2023-09-14 16:05:19 -0400

    Update policyengine_us/variables/gov/states/mi/tax/income/credits/home_heating/mi_standard_allowance_heating_credit_eligible.py
    Co-authored-by: Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/states/mi/tax/income/credits/home_heating/mi_standard_allowance_heating_credit_eligible.py
+++ policyengine_us/variables/gov/states/mi/tax/income/credits/home_heating/mi_standard_allowance_heating_credit_eligible.py
@@ -5,7 +5,7 @@ from policyengine_us.model_api import *
 class mi_standard_allowance_heating_credit_eligible(Variable):
     value_type = bool
     entity = TaxUnit
-    label = "Standard allowance can be claimed"
+    label = "Eligible for the Michigan home heating credit standard allowance"
     definition_period = YEAR
     defined_for = StateCode.MI
 
