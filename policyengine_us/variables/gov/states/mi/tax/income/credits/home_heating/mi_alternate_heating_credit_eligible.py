00e1ca3ce305c6dd56800af8fde7ee300b5db59f
keepjojoyoung <106553218+keepjojoyoung@users.noreply.github.com>
2023-09-14 16:04:54 -0400

    Update policyengine_us/variables/gov/states/mi/tax/income/credits/home_heating/mi_alternate_heating_credit_eligible.py
    Co-authored-by: Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/states/mi/tax/income/credits/home_heating/mi_alternate_heating_credit_eligible.py
+++ policyengine_us/variables/gov/states/mi/tax/income/credits/home_heating/mi_alternate_heating_credit_eligible.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class mi_alternate_heating_credit_eligible(Variable):
     value_type = bool
     entity = TaxUnit
-    label = "Alternate credit can be claimed"
+    label = "Eligible for the Michigan alternate heating credit"
     definition_period = YEAR
     defined_for = StateCode.MI
 
