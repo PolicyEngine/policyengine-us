daf5025faa11464df4f181a72298482befa74003
jiajiagong0123 <114700434+jiajiagong0123@users.noreply.github.com>
2023-09-14 19:29:40 -0700

    Update policyengine_us/variables/gov/irs/tax/federal_income/capital_gains/property_tax_net_capital_gain.py
    Co-authored-by: Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/irs/tax/federal_income/capital_gains/property_tax_net_capital_gain.py
+++ policyengine_us/variables/gov/irs/tax/federal_income/capital_gains/property_tax_net_capital_gain.py
@@ -1,7 +1,7 @@
 from policyengine_us.model_api import *
 
 
-class property_tax_net_capital_gain(Variable):
+class property_sales_net_capital_gain(Variable):
     value_type = float
     entity = TaxUnit
     label = "Idaho property tax net capital gain"
