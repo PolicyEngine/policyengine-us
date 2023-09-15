deb66e64e791b2b99ac1901f66c842c7d5a325c0
Chiukai-C <127340408+Chiukai-C@users.noreply.github.com>
2023-09-15 19:03:45 +0000

    update

--- policyengine_us/variables/gov/states/ca/cdss/tanf/eligibility/ca_tanf_income_limit.py
+++ policyengine_us/variables/gov/states/ca/cdss/tanf/eligibility/ca_tanf_income_limit.py
@@ -10,14 +10,15 @@ class ca_tanf_income_limit(Variable):
     defined_for = StateCode.CA
 
     def formula(spm_unit, period, parameters):
+        p = parameters(period).gov.states.ca.cdss.tanf.income.monthly_limit
         unit_size = spm_unit("spm_unit_size", period)
-        ceiling = min_(unit_size, 10)
-        extra = unit_size - ceiling
+        au_size = min_(unit_size, p.max_au_size)
+        additional_people = unit_size - au_size
         region1 = spm_unit("ca_tanf_region1", period)
-        p = parameters(period).gov.states.ca.cdss.tanf.income.limit
 
-        main = where(region1, p.region1.main[ceiling], p.region2.main[ceiling])
+
+        main = where(region1, p.region1.main[au_size], p.region2.main[au_size])
         additional = where(region1, p.region1.additional, p.region2.additional)
 
-        monthly = main + additional * extra
-        return monthly * MONTHS_IN_YEAR
+        monthly_limit = main + additional * additional_people
+        return monthly_limit * MONTHS_IN_YEAR
