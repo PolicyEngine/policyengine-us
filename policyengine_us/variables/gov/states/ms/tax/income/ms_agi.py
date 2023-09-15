bf573e4bfdcb3f6d5e7980bc2890f7db991ff2ef
Ziqian Cui <45090757+ZiqianCui@users.noreply.github.com>
2023-09-15 19:06:19 +0000

    added files

---
+++ policyengine_us/variables/gov/states/ms/tax/income/ms_agi.py
@@ -0,0 +1,15 @@
+from policyengine_us.model_api import *
+
+
+class ms_agi(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Mississippi adjusted gross income"
+    unit = USD
+    definition_period = YEAR
+    reference = "https://www.dor.ms.gov/sites/default/files/Forms/Individual/80100221.pdf#page=14"
+    defined_for = StateCode.MS
+
+    # AGI = Income - Total adjustments from gross income
+    adds = ["ms_taxable_income"]
+    subtracts = ["ms_agi_subtractions"]
