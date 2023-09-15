37337f3cb924141ff5d426a51babc3126b5fb4a8
jiajiagong0123 <114700434+jiajiagong0123@users.noreply.github.com>
2023-09-14 19:29:33 -0700

    Update policyengine_us/variables/gov/irs/tax/federal_income/capital_gains/property_tax_net_capital_gain.py
    Co-authored-by: Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/irs/tax/federal_income/capital_gains/property_tax_net_capital_gain.py
+++ policyengine_us/variables/gov/irs/tax/federal_income/capital_gains/property_tax_net_capital_gain.py
@@ -8,4 +8,3 @@ class property_tax_net_capital_gain(Variable):
     unit = USD
     definition_period = YEAR
     reference = "https://casetext.com/regulation/idaho-administrative-code/title-idapa-35-tax-commission-state/rule-350101-income-tax-administrative-rules/section-350101170-idaho-capital-gains-deduction-in-general"
-    defined_for = StateCode.ID
