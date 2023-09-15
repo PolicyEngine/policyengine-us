3dcac0f872948d0098e12fb83260601a197f471d
Chiukai-C <127340408+Chiukai-C@users.noreply.github.com>
2023-09-15 15:08:24 -0400

    Update policyengine_us/variables/gov/states/ca/cdss/tanf/income/ca_tanf_unearned_income.py
    Co-authored-by: Leo <33919642+leogoldman@users.noreply.github.com>

--- policyengine_us/variables/gov/states/ca/cdss/tanf/income/ca_tanf_unearned_income.py
+++ policyengine_us/variables/gov/states/ca/cdss/tanf/income/ca_tanf_unearned_income.py
@@ -8,7 +8,7 @@ class ca_tanf_unearned_income(Variable):
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.CA
-
+reference = "http://epolicy.dpss.lacounty.gov/epolicy/epolicy/server/general/projects_responsive/ePolicyMaster/index.htm?&area=general&type=responsivehelp&ctxid=&project=ePolicyMaster#t=mergedProjects%2FCalWORKs%2FCalWORKs%2F44-101_Income_Definitions%2F44-101_Income_Definitions.htm%23Policybc-2&rhtocid=_3_1_6_0_1"
     """
     descripton: Unearned Income - Income that is not earned or disability-based income. 
     reference:
