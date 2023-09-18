a6c6a46c2d4d05cdda45cbf0291975b8c68785d0
Naiqiao Ye <104268340+yedeer12@users.noreply.github.com>
2023-09-18 21:25:25 +0000

    modify

--- policyengine_us/variables/gov/states/wv/tax/income/credits/sctc/wv_sctc_eligible.py
+++ policyengine_us/variables/gov/states/wv/tax/income/credits/sctc/wv_sctc_eligible.py
@@ -5,12 +5,20 @@ class wv_senior_citizens_tax_credit_eligible(Variable):
     value_type = bool
     entity = TaxUnit
     label = "Eligible for the West Virginia senior citizens tax credit"
+    reference = (
+        "https://code.wvlegislature.gov/11-21-21/"
+        "https://tax.wv.gov/Documents/TaxForms/2021/it140.pdf#page=27 "
+    )
     definition_period = YEAR
     defined_for = StateCode.WV
 
+    # The senior citizens tax credit is used to calculate the Homestead access property tax credit
+    # and provides a credit against property taxes as opposed to income taxes
     def formula(tax_unit, period, parameters):
         wv_agi = tax_unit("wv_agi", period)
-        assessed_value = add(tax_unit, period, ["assessed_property_value"])
+        assessed_property_value = add(
+            tax_unit, period, ["assessed_property_value"]
+        )
         # amt_income
 
         p = parameters(period).gov.states.wv.tax.income.credits.sctc
@@ -19,7 +27,8 @@ class wv_senior_citizens_tax_credit_eligible(Variable):
         income_threshold = p.fpg_percentage * fpg
 
         meets_agi_condition = wv_agi <= income_threshold
-        meets_value_condition = p.home_value_threshold < assessed_value
-        eligible = meets_agi_condition & meets_value_condition
+        meets_home_value_condition = (
+            p.home_value_threshold < assessed_property_value
+        )
 
-        return eligible
+        return meets_agi_condition & meets_home_value_condition
