ab9d31ba3db3b8ba26c6de600e1c2df8f64f75fb
leogoldman <leogoldman1@gmail.com>
2023-09-18 12:08:26 -0400

    Updated WFHDC credit variable and main parameter

--- policyengine_us/variables/gov/states/or/tax/income/credits/wfhdc/or_wfhdc.py
+++ policyengine_us/variables/gov/states/or/tax/income/credits/wfhdc/or_wfhdc.py
@@ -14,57 +14,14 @@ class or_wfhdc(Variable):
         # Get the parameter tree for WFHDC.
         p = parameters(period).gov.states["or"].tax.income.credits.wfhdc
 
-        # Get the household size.
-        household_size = tax_unit("tax_unit_size", period)[0]
+        # Get the corresponding table letter for each household.
+        table_letter = tax_unit("or_wfhdc_table_letter", period)
 
-        # Get the household income, considered the larger of federal and Oregon AGI.
-        federal_agi = tax_unit("adjusted_gross_income", period)
-        or_agi = tax_unit("or_income_after_subtractions", period)
-        household_income = max_(federal_agi, or_agi)
+        # Get the corresponding table column for each household.
+        table_column = tax_unit("or_wfhdc_table_column", period)
 
-        # Get the age of the youngest qualifying child.
-        person = tax_unit.members
-        age = person("age", period)
-        min_age = age.min()
-
-        # Determine if the youngest child is disabled.
-        # Assume that the household has a child because they are WFHDC eligible.
-        disabled = person("is_disabled", period)
-        youngest_and_disabled = (age == min_age) & disabled
-
-        # This will be true if any child with the lowest age is disabled.
-        youngest_is_disabled = youngest_and_disabled.sum() > 0
-
-        hh_size_param_name = "household_size_" + str(household_size)
-
-        conditions = [
-            min_age < 3,
-            min_age < 6,
-            min_age < 13,
-            (min_age < 18) & youngest_is_disabled,
-            youngest_is_disabled,
-        ]
-
-        values = [
-            p.table_threshold[hh_size_param_name]["age_under_3"].calc(
-                household_income
-            ),
-            p.table_threshold[hh_size_param_name]["age_3_to_6"].calc(
-                household_income
-            ),
-            p.table_threshold[hh_size_param_name]["age_6_to_13"].calc(
-                household_income
-            ),
-            p.table_threshold[hh_size_param_name]["disabled_13_to_18"].calc(
-                household_income
-            ),
-            p.table_threshold[hh_size_param_name]["disabled_above_18"].calc(
-                household_income
-            ),
-        ]
-
-        # Get the corresponding row from WFHDC tables.
-        percentage = select(conditions, values)
+        # Get the OR WFHDC percentage based on the table letter and column.
+        percentage = p.table_threshold[table_letter][table_column]
 
         # Get the relevant expenses.
         expenses = tax_unit("cdcc_relevant_expenses", period)
