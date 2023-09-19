b2c717a58d7188930a117bed7a84f3982b168dc8
XZ-mabel <131919825+XZ-mabel@users.noreply.github.com>
2023-09-19 04:38:09 +0000

    Added test and variable files Fixes #3007

---
+++ policyengine_us/variables/gov/states/az/tax/az_military_retirement_subtraction.py
@@ -0,0 +1,19 @@
+from policyengine_us.model_api import *
+
+
+class az_income_tax_before_refundable_credits(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Arizona Military Retirement Subtraction"
+    unit = USD
+    definition_period = YEAR
+    defined_for = StateCode.AZ
+
+    def formula(tax_unit, period, parameters):
+        p = parameters(
+            period
+        ).gov.states.az.tax.income
+        military_retirement_pay = tax_unit(
+            "military_retirement_pay", period
+        )
+        return min_(military_retirement_pay, p.military_retirement_subtraction)
