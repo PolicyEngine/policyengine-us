c6a69cf23961ed02ffd58d2df009ad61f3dcec2a
XZ-mabel <131919825+XZ-mabel@users.noreply.github.com>
2023-09-19 16:00:09 +0000

    Changed test and variable files

--- policyengine_us/variables/gov/states/az/tax/az_military_retirement_subtraction.py
+++ policyengine_us/variables/gov/states/az/tax/az_military_retirement_subtraction.py
@@ -6,20 +6,22 @@ class az_military_retirement_subtraction(Variable):
     entity = TaxUnit
     label = "Arizona Military Retirement Subtraction"
     unit = USD
+    reference = (
+        "https://www.azleg.gov/ars/43/01022.html",
+        "https://azdor.gov/sites/default/files/2023-03/FORMS_INDIVIDUAL_2022_140BOOKLET.pdf#page=25",
+        "https://azdor.gov/sites/default/files/2023-03/FORMS_INDIVIDUAL_2021_140BOOKLET.pdf#page=27",
+    )
     definition_period = YEAR
     defined_for = StateCode.AZ
 
     def formula(tax_unit, period, parameters):
-        p = parameters(
-            period
-        ).gov.states.az.tax.income
-        #
-        military_retirement_pay = tax_unit(
-        head = person("is_tax_unit_spouse",
-        spouse - same 
+        p = parameters(period).gov.states.az.tax.income
+        person = tax_unit.members
+        head = person("is_tax_unit_head", period)
+        spouse = person("is_tax_unit_spouse", period)
         head_or_spouse = head | spouse
-        military_retirement_pay = person(
-            "military_retirement_pay", period
-        )
-        eligible_military_pay = military_retirement_pay * head_or_spouse 
-        return min_(tax_unit.sum(eligible_military_pay), p.military_retirement_subtraction)
+        military_retirement_pay = person("military_retirement_pay", period)
+        eligible_military_pay = military_retirement_pay * head_or_spouse
+        capped_military_pay = min_(eligible_military_pay, p.military_retirement_subtraction)
+        return tax_unit.sum(capped_military_pay)
+           
