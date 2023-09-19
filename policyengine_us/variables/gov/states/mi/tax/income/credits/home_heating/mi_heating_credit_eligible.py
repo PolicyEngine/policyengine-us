2d31093ec802fb9cc7b73f6e45bc50be3eea066b
keepjojoyoung <106553218+keepjojoyoung@users.noreply.github.com>
2023-09-19 01:36:04 +0000

    Michigan Home Heating Credit Fixes #2312

--- policyengine_us/variables/gov/states/mi/tax/income/credits/home_heating/mi_heating_credit_eligible.py
+++ policyengine_us/variables/gov/states/mi/tax/income/credits/home_heating/mi_heating_credit_eligible.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class mi_heating_credit_eligible(Variable):
     value_type = bool
     entity = TaxUnit
-    label = "Michigan heating credit can be claimed"
+    label = "Eligible Household for the Michigan heating credit"
     definition_period = YEAR
     defined_for = StateCode.MI
 
@@ -12,5 +12,8 @@ class mi_heating_credit_eligible(Variable):
         person = tax_unit.members
         is_not_dsi = ~tax_unit("dsi", period)
         is_not_ft_student = ~person("is_full_time_student", period)
+        is_head = person("is_tax_unit_head", period)
+        is_spouse = person("is_tax_unit_spouse", period)
+        head_or_spouse = is_head | is_spouse
 
-        return is_not_dsi & is_not_ft_student
+        return is_not_dsi & tax_unit.any(is_not_ft_student & head_or_spouse)
