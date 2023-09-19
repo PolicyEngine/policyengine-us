85d05d2f0a2d3a845790336cc3336019beaa42b0
Diana Shi <dianashi0607@gmail.com>
2023-09-19 00:01:35 +0000

    Solved

--- policyengine_us/variables/gov/states/ga/tax/adjusted_gross_income/ga_military_retirement_exclusion.py
+++ policyengine_us/variables/gov/states/ga/tax/adjusted_gross_income/ga_military_retirement_exclusion.py
@@ -8,8 +8,8 @@ class ga_military_retirement_exclusion(Variable):
     unit = USD
     definition_period = YEAR
     reference = (
-        "https://dor.georgia.gov/document/booklet/2021-it-511-individual-income-tax-booklet/download#page=15"
-        "https://dor.georgia.gov/document/document/2022-it-511-individual-income-tax-booklet/download#page=15"
+        "https://dor.georgia.gov/document/booklet/2021-it-511-individual-income-tax-booklet/download#page=16"
+        "https://dor.georgia.gov/document/document/2022-it-511-individual-income-tax-booklet/download#page=16"
         "https://advance.lexis.com/documentpage/?pdmfid=1000516&crid=fb5db531-a80f-4790-bddb-eefc8327ef60&config=00JAA1MDBlYzczZi1lYjFlLTQxMTgtYWE3OS02YTgyOGM2NWJlMDYKAFBvZENhdGFsb2feed0oM9qoQOMCSJFX5qkd&pddocfullpath=%2Fshared%2Fdocument%2Fstatutes-legislation%2Furn%3AcontentItem%3A65D2-CDH3-CGX8-044N-00008-00&pdcontentcomponentid=234186&pdteaserkey=sr1&pditab=allpods&ecomp=8s65kkk&earg=sr1&prid=66f02b0a-c5ae-4162-9535-127751546807"
     )
     defined_for = StateCode.GA
@@ -31,5 +31,8 @@ class ga_military_retirement_exclusion(Variable):
             p.additional.amount,
             0,
         )
-        total = min_(base + additional, military_income)
-        return tax_unit.sum(total)
+        uncapped_exclusion_amount = base + additional
+        capped_exclusion_amount = min_(
+            uncapped_exclusion_amount, military_income
+        )
+        return tax_unit.sum(capped_exclusion_amount)
