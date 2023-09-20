cb1fb8ae7a4d37f49d557bcd3551780730fac98a
SirMalamute <neeliyer14@gmail.com>
2023-09-20 10:05:52 +0000

    minor

--- policyengine_us/variables/gov/states/md/tax/income/agi/subtractions/hundred_year/md_hundred_year_subtraction.py
+++ policyengine_us/variables/gov/states/md/tax/income/agi/subtractions/hundred_year/md_hundred_year_subtraction.py
@@ -8,7 +8,7 @@ class md_hundred_year_subtraction(Variable):
     unit = USD
     definition_period = YEAR
     reference = "https://trackbill.com/bill/maryland-house-bill-186-income-tax-subtraction-modification-for-centenarians/2173534/"
-    defined_for = "md_hundred_year_exemption_eligible"
+    defined_for = "md_hundred_year_subtraction_eligible"
 
     def formula(person, period, parameters):
         return parameters(
