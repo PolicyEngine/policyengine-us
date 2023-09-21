303b672138306bde1fdc3d510f8f02f3f6dfd57f
JudyZ13 <112495414+JudyZ13@users.noreply.github.com>
2023-09-21 09:26:12 -0500

    Update policyengine_us/variables/gov/states/az/tax/income/deductions/net_capital_gains/az_net_capital_gains_subtraction.py
    Co-authored-by: Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/states/az/tax/income/deductions/net_capital_gains/az_net_capital_gains_subtraction.py
+++ policyengine_us/variables/gov/states/az/tax/income/deductions/net_capital_gains/az_net_capital_gains_subtraction.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class az_long_term_capital_gains_subtraction(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Arizona net capital gains subtraction"
+    label = "Arizona long-term capital gains subtraction"
     unit = USD
     documentation = "https://azdor.gov/sites/default/files/2023-03/FORMS_INDIVIDUAL_2022_140i.pdf#page=31"
     definition_period = YEAR
