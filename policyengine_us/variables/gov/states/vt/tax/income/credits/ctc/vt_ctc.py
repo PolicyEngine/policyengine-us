c53ffa9bc11b10152339fc7bc3200a83a47af44d
jongwoniee <ljw3728@gmail.com>
2023-09-22 12:41:25 -0400

    Vermont Child Tax Credit Fixes #2826

--- policyengine_us/variables/gov/states/vt/tax/income/credits/ctc/vt_ctc.py
+++ policyengine_us/variables/gov/states/vt/tax/income/credits/ctc/vt_ctc.py
@@ -14,8 +14,8 @@ class vt_ctc(Variable):
         # Get age status of all people in the tax unit.
         person = tax_unit.members
         age = person("age", period)
-        p = parameters(period).gov.states["vt"].tax.income.credits.ctc
-        eligible = age < p.age_threshold
+        p = parameters(period).gov.states.vt.tax.income.credits.ctc
+        eligible = age <= p.age_threshold
         count_eligible = tax_unit.sum(eligible)
         # Get maximum credit amount.
         max_credit = p.amount * count_eligible
@@ -23,7 +23,6 @@ class vt_ctc(Variable):
         agi = tax_unit("adjusted_gross_income", period)
         # Reduce credit amount over the phaseout range.
         excess_agi = max_(agi - p.reduction.start, 0)
-        # using a mask to avoid division by zero.
         increments = where(
             p.reduction.increment > 0, excess_agi / p.reduction.increment, 0
         )
