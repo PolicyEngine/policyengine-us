5bba1c56485c8299b8da745cb9c3aed24d02eb9b
Xinyi-Shao <ellashao99@gmail.com>
2023-09-22 14:41:59 -0400

    Merge branch 'master' of https://github.com/PolicyEngine/policyengine-us into Xinyi-Shao/issue3024

--- policyengine_us/variables/gov/states/vt/tax/income/vt_eitc.py
+++ policyengine_us/variables/gov/states/vt/tax/income/vt_eitc.py
@@ -4,14 +4,13 @@ from policyengine_us.model_api import *
 class vt_eitc(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Vermont EITC"
+    label = "Vermont earned income tax credit"
     unit = USD
     definition_period = YEAR
-    reference = (
-        "https://tax.vermont.gov/sites/tax/files/documents/IN-112-2022.pdf"
-    )
+    reference = "https://tax.vermont.gov/sites/tax/files/documents/IN-112-2022.pdf#page=1"
     defined_for = StateCode.VT
 
     def formula(tax_unit, period, parameters):
         eitc = tax_unit("earned_income_tax_credit", period)
         rate = parameters(period).gov.states.vt.tax.income.credits.eitc.rate
+        return eitc * rate
