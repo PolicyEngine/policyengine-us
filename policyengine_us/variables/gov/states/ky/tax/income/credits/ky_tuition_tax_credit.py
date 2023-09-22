a140de0125a06de7a333e72e3ed1b43d23c48096
Aiwen Peng <88301987+aiwennnpeng@users.noreply.github.com>
2023-09-22 01:10:55 +0000

    Kentucky tuition tax credit Fixes #2992

--- policyengine_us/variables/gov/states/ky/tax/income/credits/ky_tuition_tax_credit.py
+++ policyengine_us/variables/gov/states/ky/tax/income/credits/ky_tuition_tax_credit.py
@@ -9,12 +9,15 @@ class ky_tuition_tax_credit(Variable):
     definition_period = YEAR
     defined_for = StateCode.KY
 
-    def formula(tax_unit, parameters):
-        #line 12
+    def formula(tax_unit, period, parameters):
         tentative_tax_credit = add(tax_unit, period, ["american_opportunity_credit","lifetime_learning_credit"])
-        rate = parameters(period).gov.states.ky.tax.income.credits.tuition_tax
-        return tentative_tax_credit * rate
-
+        rate = parameters(period).gov.states.ky.tax.income.credits.tuition_tax.rate
+        tuition_credit = tentative_tax_credit * rate
+        tax_before_non_refundable = tax_unit(
+            "ky_income_tax_before_non_refundable_credits", period
+        )
+        return min_(-tax_before_non_refundable, tuition_credit)
+       
 
 
 
