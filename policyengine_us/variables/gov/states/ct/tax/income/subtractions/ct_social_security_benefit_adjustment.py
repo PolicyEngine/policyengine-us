f190a74189283211b30d8091c5967bb6afe09bfc
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2023-09-23 19:52:04 +0000

    Adding eligibility file

--- policyengine_us/variables/gov/states/ct/tax/income/subtractions/ct_social_security_benefit_adjustment.py
+++ policyengine_us/variables/gov/states/ct/tax/income/subtractions/ct_social_security_benefit_adjustment.py
@@ -14,14 +14,14 @@ class ct_social_security_benefit_adjustment(Variable):
             period
         ).gov.states.ct.tax.income.subtractions.social_security
         filing_status = tax_unit("filing_status", period)
+        is_eligible = tax_unit("ct_social_security_benefit_adjustment_eligible", period)
         ss_benefit = add(tax_unit, period, ["social_security"])
         agi = add(tax_unit, period, ["adjusted_gross_income"])
-        max_amount = p.max_amount[filing_status]
         base_amount = p.base_amount[filing_status]
-        ss_benefit_frac = 0.25
-        ss_agi_frac = 0.5
+        ss_benefit_frac = parameters(period).gov.states.ct.tax.income.subtractions.social_security.ss_fraction
+        ss_agi_frac = parameters(period).gov.states.ct.tax.income.subtractions.social_security.excess_fraction
         includable_ss = ss_benefit * ss_benefit_frac
         excess = agi + ss_agi_frac * ss_benefit - base_amount
         max_inclusion = min_(includable_ss, ss_benefit_frac * excess)
         adjusted_ss_benefit = max_(max_inclusion - includable_ss, 0)
-        return where(agi < max_amount, 0, adjusted_ss_benefit)
+        return where(is_eligible, 0, adjusted_ss_benefit)
