efeffd4ee3c318a9085fdf2e409cd478e1850f28
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2023-09-23 19:53:30 +0000

    Formatting files

--- policyengine_us/variables/gov/states/ct/tax/income/subtractions/ct_social_security_benefit_adjustment.py
+++ policyengine_us/variables/gov/states/ct/tax/income/subtractions/ct_social_security_benefit_adjustment.py
@@ -14,12 +14,18 @@ class ct_social_security_benefit_adjustment(Variable):
             period
         ).gov.states.ct.tax.income.subtractions.social_security
         filing_status = tax_unit("filing_status", period)
-        is_eligible = tax_unit("ct_social_security_benefit_adjustment_eligible", period)
+        is_eligible = tax_unit(
+            "ct_social_security_benefit_adjustment_eligible", period
+        )
         ss_benefit = add(tax_unit, period, ["social_security"])
         agi = add(tax_unit, period, ["adjusted_gross_income"])
         base_amount = p.base_amount[filing_status]
-        ss_benefit_frac = parameters(period).gov.states.ct.tax.income.subtractions.social_security.ss_fraction
-        ss_agi_frac = parameters(period).gov.states.ct.tax.income.subtractions.social_security.excess_fraction
+        ss_benefit_frac = parameters(
+            period
+        ).gov.states.ct.tax.income.subtractions.social_security.ss_fraction
+        ss_agi_frac = parameters(
+            period
+        ).gov.states.ct.tax.income.subtractions.social_security.excess_fraction
         includable_ss = ss_benefit * ss_benefit_frac
         excess = agi + ss_agi_frac * ss_benefit - base_amount
         max_inclusion = min_(includable_ss, ss_benefit_frac * excess)
