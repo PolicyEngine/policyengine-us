39ee8d34f1a3951fa306a67ae7f853ae11c88f3d
keepjojoyoung <106553218+keepjojoyoung@users.noreply.github.com>
2023-09-25 17:51:51 -0400

    Update policyengine_us/variables/gov/states/mi/tax/income/credits/home_heating/mi_reduced_standard_allowance.py
    Co-authored-by: Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/states/mi/tax/income/credits/home_heating/mi_reduced_standard_allowance.py
+++ policyengine_us/variables/gov/states/mi/tax/income/credits/home_heating/mi_reduced_standard_allowance.py
@@ -10,7 +10,7 @@ class mi_reduced_standard_allowance(Variable):
     unit = USD
     definition_period = YEAR
     reference = (
-        "https://www.michigan.gov/taxes/iit/accordion/credits/table-a-2022-home-heating-credit-mi-1040cr-7-standard-allowance"
+        "https://www.michigan.gov/taxes/iit/accordion/credits/table-a-2022-home-heating-credit-mi-1040cr-7-standard-allowance",
         "http://www.legislature.mi.gov/(S(keapvg1h2vndkn25rtmpyyse))/mileg.aspx?page=getObject&objectName=mcl-206-527a"
     )
     defined_for = StateCode.MI
