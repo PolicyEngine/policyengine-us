61a12e9c111d38cd79cf5c91cb7106f97e89e06d
Masasa <73863365+ThapeloMasasa@users.noreply.github.com>
2023-09-25 01:22:13 +0000

    Connecticut Pension or Annuity Income Subtraction Fixes #3011

--- policyengine_us/variables/gov/states/ct/tax/income/subtractions/pension_annuity/ct_pension_annuity_subtraction.py
+++ policyengine_us/variables/gov/states/ct/tax/income/subtractions/pension_annuity/ct_pension_annuity_subtraction.py
@@ -11,27 +11,13 @@ class ct_pension_annuity_subtraction(Variable):
 
     def formula(tax_unit, period, parameters):
         person = tax_unit.members
-        head = person("is_tax_unit_head", period) 
+        head = person("is_tax_unit_head", period)
         spouse = person("is_tax_unit_spouse", period)
-        rate = parameters(period).gov.states.ct.tax.income.subtractions.pensions_or_annuity.rate
+        rate = parameters(
+            period
+        ).gov.states.ct.tax.income.subtractions.pensions_or_annuity.rate
         head_or_spouse = head | spouse
         pension_income = person("taxable_pension_income", period)
         eligible_pension = pension_income * head_or_spouse
         total_pension = tax_unit.sum(eligible_pension)
         return total_pension * rate
-
-        
-
-# person = tax_unit.members  
-
-# head = person("is_tax_unit_head", period) - bool
-        
-# spouse = person("is_tax_unit_spouse", period) - bool
-
-# head_or_spouse = head | spouse  - bool
-
-# pension = person("taxable_pension_income", period)
-
-# eligibl_pension = pension * head_or_spouse
-
-# total_pension = tax_unit.sum(eligibl_pension)
--- policyengine_us/variables/gov/states/ct/tax/income/subtractions/pension_annuity/ct_pension_annuity_subtraction_eligible.py
+++ policyengine_us/variables/gov/states/ct/tax/income/subtractions/pension_annuity/ct_pension_annuity_subtraction_eligible.py
@@ -4,8 +4,7 @@ from policyengine_us.model_api import *
 class ct_pension_annuity_subtraction_eligible(Variable):
     value_type = bool
     entity = TaxUnit
-    label = "Connecticut pension and annuity subtraction eligibility"
-    unit = USD
+    label = "Eligible for the Connecticut pension and annuity subtraction"
     definition_period = YEAR
     defined_for = StateCode.CT
 
@@ -13,6 +12,7 @@ class ct_pension_annuity_subtraction_eligible(Variable):
         ct_agi = tax_unit("ct_agi", period)
         filing_status = tax_unit("filing_status", period)
         p = parameters(period).gov.states.ct.tax.income.subtractions
-        ct_pension_threshold = p.pensions_or_annuity.income_threshold[filing_status]
-        return ct_agi < ct_pension_threshold 
-        
+        ct_pension_threshold = p.pensions_or_annuity.income_threshold[
+            filing_status
+        ]
+        return ct_agi < ct_pension_threshold
