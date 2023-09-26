bb460c6367fb4aadb1944cc6e290fd81319a9509
Ziqian Cui <45090757+ZiqianCui@users.noreply.github.com>
2023-09-26 17:44:20 +0000

    Connecticut alternative minimum tax rate Fixes #3062

--- policyengine_us/variables/gov/states/ct/tax/income/ct_amt.py
+++ policyengine_us/variables/gov/states/ct/tax/income/ct_amt.py
@@ -3,11 +3,28 @@ from policyengine_us.model_api import *
 
 class ct_amt(Variable):
     value_type = float
-    entity = Person
+    entity = TaxUnit
     label = "Connecticut alternative minimum tax (amt) rate"
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.CT
+    reference = (
+        "https://portal.ct.gov/-/media/DRS/Forms/2022/Income/2022-CT-1040-Instructions_1222.pdf#page=22"  # line 9
+        "https://portal.ct.gov/-/media/DRS/Forms/2022/Income/CT-1040_1222.pdf#page=1"  # line 9
+        "https://portal.ct.gov/-/media/DRS/Forms/2022/Income/CT-6251_1222.pdf#page=1"  # line 23
+    )
 
-    def formula(person, period, parameters):
-        return [alternative_minimum_tax]
+    def formula(tax_unit, period, parameters):
+        # assign amt parameters
+        p = parameters(period).gov.states.ia.tax.income
+        amt = p.alternative_minimum_tax
+
+        ct_taxable_income = tax_unit("ct_taxable_income", period)
+        taxable_income = tax_unit("amt_income", period)
+        federal_minimum_tax = tax_unit("alternative_minimum_tax", period)
+
+        ct_minimum_tax = min_(
+            taxable_income * amt.rate, federal_minimum_tax * amt.fraction
+        )
+
+        return max_(ct_minimum_tax - ct_taxable_income, 0)
