c34bdfddd73d59e4e7c6be0a2f54f7ad714da3fc
WeihaoZ724 <128203510+WeihaoZ724@users.noreply.github.com>
2023-09-27 18:15:04 +0000

    Kentucky adjusted gross income Fixes #3081

--- policyengine_us/variables/gov/states/ky/tax/income/ky_subtractions.py
+++ policyengine_us/variables/gov/states/ky/tax/income/ky_subtractions.py
@@ -8,6 +8,7 @@ class ky_subtractions(Variable):
     unit = USD
     definition_period = YEAR
     reference = (
+        "https://apps.legislature.ky.gov/law/statutes/statute.aspx?id=53498"
         "https://revenue.ky.gov/Forms/Schedule%20M%202022.pdf#page=1"
         "https://revenue.ky.gov/Forms/740%20Packet%20Instructions%205-9-23.pdf#page=23"
         "https://revenue.ky.gov/Forms/Schedule%20M-2021.pdf#page=1"
