60e904893f4201194168a9810277b4cd2ad55822
Shuyu Zhan (Stella) <94806484+shuyu7zhan@users.noreply.github.com>
2023-09-27 01:16:00 +0000

    Hawaii Subtractions from Federal AGI #3091

--- policyengine_us/variables/gov/states/hi/tax/income/hi_subtractions.py
+++ policyengine_us/variables/gov/states/hi/tax/income/hi_subtractions.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class hi_subtractions(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Hawaii subtractions from Federal AGI"
+    label = "Hawaii subtractions from federal adjusted gross income"
     reference = "https://files.hawaii.gov/tax/forms/2022/n11ins.pdf#page=13"
     defined_for = StateCode.HI
     unit = USD
