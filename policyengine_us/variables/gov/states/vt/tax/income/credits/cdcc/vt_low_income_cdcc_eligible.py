d3f9b64345a2d99fed0055a4128de23c2e1fe6f5
Jingjing Wang <87988673+Jingj-WANG@users.noreply.github.com>
2023-09-28 17:23:27 +0000

    Vermont Child and Dependent Care Credit Fixes #3070

--- policyengine_us/variables/gov/states/vt/tax/income/credits/cdcc/vt_low_income_cdcc_eligible.py
+++ policyengine_us/variables/gov/states/vt/tax/income/credits/cdcc/vt_low_income_cdcc_eligible.py
@@ -2,10 +2,9 @@ from policyengine_us.model_api import *
 
 
 class vt_low_income_cdcc_eligible(Variable):
-    value_type = float
+    value_type = bool
     entity = TaxUnit
-    label = "Vermont low-income child care and dependent care credit"
-    unit = USD
+    label = "Eligible for the Vermont low-income child care and dependent care credit"
     definition_period = YEAR
     reference = (
         "https://tax.vermont.gov/sites/tax/files/documents/IN-112-2021.pdf#page=2"
@@ -16,7 +15,6 @@ class vt_low_income_cdcc_eligible(Variable):
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states.vt.tax.income.credits.cdcc.low_income
         filing_status = tax_unit("filing_status", period)
-        federal_agi_threshold = p.federal_agi_threshold[filing_status]
+        federal_agi_threshold = p.income_threshold[filing_status]
         federal_agi = tax_unit("adjusted_gross_income", period)
-        agi_eligible = federal_agi <= federal_agi_threshold
-        return agi_eligible
+        return federal_agi <= federal_agi_threshold
