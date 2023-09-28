0741393d97f64c7349a71e6a4e90ed5a5418c5b2
JudyZ13 <112495414+JudyZ13@users.noreply.github.com>
2023-09-28 03:58:04 +0000

    add more tests

---
+++ policyengine_us/variables/gov/states/az/tax/income/subtractions/college_savings/az_529_college_savings_plans_subtraction.py
@@ -0,0 +1,27 @@
+from policyengine_us.model_api import *
+
+
+class az_529_college_savings_plans_subtraction(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Arizona 529 college savings plans subtraction"
+    unit = USD
+    documentation = "https://azdor.gov/sites/default/files/2023-03/FORMS_INDIVIDUAL_2022_140i.pdf#page=15"
+    definition_period = YEAR
+    defined_for = StateCode.AZ
+
+    def formula(tax_unit, period, parameters):
+        p = parameters(
+            period
+        ).gov.states.az.tax.income.subtractions.college_savings
+
+        filing_status = tax_unit("filing_status", period)
+        contributions_529 = tax_unit("investment_in_529_plan", period)
+
+        cap = p.cap[filing_status]
+        person = tax_unit.members
+        head = person("is_tax_unit_head", period)
+        spouse = person("is_tax_unit_spouse", period)
+        max_sub = cap * tax_unit.sum(head + spouse)
+
+        return min_(contributions_529, max_sub)
