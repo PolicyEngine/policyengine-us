af978dbb07a67dfff45bfe0b8c6105e8eed9741f
Chiukai-C <127340408+Chiukai-C@users.noreply.github.com>
2023-09-29 04:07:16 +0000

    add RMR ceilings standard & weekend

--- policyengine_us/variables/gov/states/ca/cdss/child_care/eligibility/ca_child_care_welfare_to_work.py
+++ policyengine_us/variables/gov/states/ca/cdss/child_care/eligibility/ca_child_care_welfare_to_work.py
@@ -5,7 +5,7 @@ class ca_child_care_welfare_to_work(Variable):
     value_type = float
     entity = SPMUnit
     label = "California CalWORKs Welfare to Work"
-    unit = HOUR
+    unit = "hour"
     definition_period = YEAR
     defined_for = StateCode.CA
 
