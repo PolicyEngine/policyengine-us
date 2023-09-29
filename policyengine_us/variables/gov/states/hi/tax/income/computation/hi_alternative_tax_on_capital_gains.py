09f2ac235079f529e1d72de74ad10107cbd065cf
xianxinzhu <97819588+xianxinzhu@users.noreply.github.com>
2023-09-29 06:14:44 +0000

    Fixes variables and tests

--- policyengine_us/variables/gov/states/hi/tax/income/computation/hi_alternative_tax_on_capital_gains.py
+++ policyengine_us/variables/gov/states/hi/tax/income/computation/hi_alternative_tax_on_capital_gains.py
@@ -13,11 +13,16 @@ class hi_alternative_tax_on_capital_gains(Variable):
         person = tax_unit.members
         p = parameters(period).gov.states.hi.tax.income.alternative_tax
         filing_status = tax_unit("filing_status", period)
-        taxable_income = tax_unit("taxable_income", period)
+        taxable_income = tax_unit("hi_taxable_income", period)
         net_lt_capital_gain = add(person, period, ["long_term_capital_gains"])
         net_capital_gain = tax_unit("net_capital_gain", period)
         # line_9 how to calculate Net gain from the disposition of property held for investment
         cap = p.max_amount[filing_status]
+        # income_tax
+        income_tax = tax_unit("hi_income_tax_before_credits", period)
+        eligibility = tax_unit(
+            "hi_alternative_tax_on_capital_gains_eligible", period
+        )
 
         # line 13
         ineligible_income = max_(
@@ -51,9 +56,8 @@ class hi_alternative_tax_on_capital_gains(Variable):
             ],
         )
 
-        # income_tax
-        income_tax = tax_unit("hi_income_tax_before_credits", period)
-
         alternative_tax = ineligible_tax + eligible_income * p.rate  # line 17
 
-        return min_(income_tax, alternative_tax)
+        return where(
+            eligibility, min_(income_tax, alternative_tax), income_tax
+        )
---
+++ policyengine_us/variables/gov/states/hi/tax/income/computation/hi_alternative_tax_on_capital_gains_eligible .py
@@ -0,0 +1,17 @@
+from policyengine_us.model_api import *
+
+
+class hi_alternative_tax_on_capital_gains_eligible(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Hawaii eligible alternative tax on capital gains"
+    unit = USD
+    definition_period = YEAR
+    defined_for = StateCode.HI
+
+    def formula(tax_unit, period, parameters):
+        p = parameters(period).gov.states.hi.tax.income.alternative_tax
+        filing_status = tax_unit("filing_status", period)
+        taxable_income = tax_unit("taxable_income", period)
+        cap = p.max_amount[filing_status]
+        return taxable_income > cap
