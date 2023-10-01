061105781cc9d2424dd23821a985f32e2557e27d
leogoldman <leogoldman1@gmail.com>
2023-10-01 14:29:35 -0400

    Made home_mortage_interest Person var

--- policyengine_us/variables/household/income/person/interest/home_mortgage_interest.py
+++ policyengine_us/variables/household/income/person/interest/home_mortgage_interest.py
@@ -3,7 +3,7 @@ from policyengine_us.model_api import *
 
 class home_mortgage_interest(Variable):
     value_type = float
-    entity = TaxUnit
+    entity = Person
     label = "Interest paid on a home mortgage"
     unit = USD
     definition_period = YEAR
