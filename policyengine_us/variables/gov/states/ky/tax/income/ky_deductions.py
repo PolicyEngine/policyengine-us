e3964609b78cfdd0688acd39a4cc53063c2e105e
leogoldman <leogoldman1@gmail.com>
2023-10-01 14:25:17 -0400

    Changed itemized deductions to list param

--- policyengine_us/variables/gov/states/ky/tax/income/ky_deductions.py
+++ policyengine_us/variables/gov/states/ky/tax/income/ky_deductions.py
@@ -4,10 +4,10 @@ from policyengine_us.model_api import *
 class ky_deductions(Variable):
     value_type = float
     entity = TaxUnit
-    label = "KY income deductions"
+    label = "Kentucky income deductions"
     unit = USD
     definition_period = YEAR
-    reference = "https://law.justia.com/codes/kentucky/2022/chapter-141/section-141-019/"
+    reference = "https://law.justia.com/codes/kentucky/2022/chapter-141/section-141-019/"  # (2)(i)
     defined_for = StateCode.KY
 
     def formula(tax_unit, period, parameters):
