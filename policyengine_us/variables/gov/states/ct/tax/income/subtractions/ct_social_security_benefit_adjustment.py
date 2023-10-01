aa5da4729db8646fc37676ada0e964b75300d3b6
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2023-10-01 18:52:03 +0000

    Using irs parameters

--- policyengine_us/variables/gov/states/ct/tax/income/subtractions/ct_social_security_benefit_adjustment.py
+++ policyengine_us/variables/gov/states/ct/tax/income/subtractions/ct_social_security_benefit_adjustment.py
@@ -10,24 +10,23 @@ class ct_social_security_benefit_adjustment(Variable):
     defined_for = "ct_social_security_benefit_adjustment_eligible"
 
     def formula(tax_unit, period, parameters):
-        p = parameters(
-            period
-        ).gov.states.ct.tax.income.subtractions.social_security
+        p = parameters(period).gov.irs.social_security.taxability
+        income_base_amount = p.threshold.lower
+        ss_agi_frac = p.rate.lower
         filing_status = tax_unit("filing_status", period)
-        is_eligible = tax_unit(
-            "ct_social_security_benefit_adjustment_eligible", period
-        )
-        ss_benefit = add(tax_unit, period, ["social_security"])
-        agi = add(tax_unit, period, ["adjusted_gross_income"])
-        base_amount = p.base_amount[filing_status]
+        base_amount = income_base_amount[filing_status]
         ss_rate = parameters(
             period
         ).gov.states.ct.tax.income.subtractions.social_security.rate
-        ss_agi_frac = parameters(
-            period
-        ).gov.states.ct.tax.income.subtractions.social_security.excess_fraction
+        # is_eligible = tax_unit(
+        #     "ct_social_security_benefit_adjustment_eligible", period
+        # )
+        ss_benefit = add(tax_unit, period, ["social_security"])
+        agi = add(tax_unit, period, ["adjusted_gross_income"])
+
         includable_ss = ss_benefit * ss_rate
         excess = agi + ss_agi_frac * ss_benefit - base_amount
         max_inclusion = min_(includable_ss, ss_rate * excess)
         adjusted_ss_benefit = max_(max_inclusion - includable_ss, 0)
-        return where(is_eligible, 0, adjusted_ss_benefit)
+        # return where(is_eligible, 0, adjusted_ss_benefit)
+        return adjusted_ss_benefit
