86c6223d7a5b94b6d2cb8994c1ed4d045ce50805
JudyZ13 <112495414+JudyZ13@users.noreply.github.com>
2023-10-03 16:41:49 +0000

    define az_agi

--- policyengine_us/variables/gov/states/az/tax/income/agi/az_agi.py
+++ policyengine_us/variables/gov/states/az/tax/income/agi/az_agi.py
@@ -5,6 +5,6 @@ class az_agi(Variable):
     value_type = float
     entity = TaxUnit
     label = "Arizona adjusted gross income"
+    defined_for = StateCode.AZ
     unit = USD
     definition_period = YEAR
-    defined_for = StateCode.AZ
