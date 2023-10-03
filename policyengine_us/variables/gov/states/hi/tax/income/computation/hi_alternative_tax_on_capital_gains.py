d2ba3251fef19ff6b42c3d9a550f1250a65f4e11
xianxinzhu <97819588+xianxinzhu@users.noreply.github.com>
2023-10-03 04:57:30 +0000

    Fixes variables

--- policyengine_us/variables/gov/states/hi/tax/income/computation/hi_alternative_tax_on_capital_gains.py
+++ policyengine_us/variables/gov/states/hi/tax/income/computation/hi_alternative_tax_on_capital_gains.py
@@ -7,7 +7,7 @@ class hi_alternative_tax_on_capital_gains(Variable):
     label = "Hawaii alternative tax on capital gains"
     unit = USD
     definition_period = YEAR
-    defined_for = StateCode.HI
+    defined_for = "hi_alternative_tax_on_capital_gains_eligible"
 
     def formula(tax_unit, period, parameters):
         person = tax_unit.members
@@ -16,9 +16,9 @@ class hi_alternative_tax_on_capital_gains(Variable):
         taxable_income = tax_unit("hi_taxable_income", period)
         net_lt_capital_gain = add(person, period, ["long_term_capital_gains"])
         net_capital_gain = tax_unit("net_capital_gain", period)
-        # line_9 how to calculate Net gain from the disposition of property held for investment
+        # line_9
         cap = p.max_amount[filing_status]
-        # income_tax
+        # income_tax line 18
         income_tax = tax_unit("hi_income_tax_before_credits", period)
         eligibility = tax_unit(
             "hi_alternative_tax_on_capital_gains_eligible", period
@@ -31,12 +31,12 @@ class hi_alternative_tax_on_capital_gains(Variable):
             cap,
         )
 
-        # What if line 1 < line 13
+        # line 14
         eligible_income = max_(
             0, taxable_income - ineligible_income
         )  # net capital gains eligble for alternative tax
 
-        # ineligible_tax --- tax for line 13
+        # ineligible_tax --- line 15
         statuses = filing_status.possible_values
         rate_p = parameters(period).gov.states.hi.tax.income.rates
         ineligible_tax = select(
@@ -56,8 +56,10 @@ class hi_alternative_tax_on_capital_gains(Variable):
             ],
         )
 
-        alternative_tax = ineligible_tax + eligible_income * p.rate  # line 17
+        # line 17
+        alternative_tax = ineligible_tax + eligible_income * p.rate
 
         return where(
-            eligibility, min_(income_tax, alternative_tax), income_tax
+            eligibility & (alternative_tax < income_tax), 
+            eligible_income
         )
