7231fb7532311a4778aa4bd07aba70ffb809d30b
Linfeng-ll <95734090+Linfeng-ll@users.noreply.github.com>
2023-10-03 07:41:35 +0000

    Ohio lump sum retirement credit Fixes #2958

--- policyengine_us/variables/gov/states/oh/tax/income/credits/oh_lump_sum_retirement_credit.py
+++ policyengine_us/variables/gov/states/oh/tax/income/credits/oh_lump_sum_retirement_credit.py
@@ -20,12 +20,8 @@ class oh_lump_sum_retirement_credit(Variable):
         pension_income = person("pension_income", period)
         age_head = tax_unit("age_head", period)
         age_spouse = tax_unit("age_spouse", period)
-        head_eligible = where(
-            age_head >= p.lump_sum_retirement.age_threshold, 1, 0
-        )
-        spouse_eligible = where(
-            age_spouse >= p.lump_sum_retirement.age_threshold, 1, 0
-        )
+        head_eligible = age_head >= p.lump_sum_retirement.age_threshold
+        spouse_eligible = age_spouse >= p.lump_sum_retirement.age_threshold
 
         head_divisor = p.lump_sum_retirement.divisor.calc(age_head)
         spouse_divisor = p.lump_sum_retirement.divisor.calc(age_spouse)
@@ -33,22 +29,20 @@ class oh_lump_sum_retirement_credit(Variable):
         head_pension = tax_unit.sum(is_head * pension_income)
         spouse_pension = tax_unit.sum(is_spouse * pension_income)
 
-        head_credit = where(
-            head_divisor == 0,
-            0,
+        head_credit = (
             p.retirement_income.amount.calc(
                 head_pension / head_divisor, right=True
             )
-            * head_divisor,
+            * head_divisor
         )
-        spouse_credit = where(
-            spouse_divisor == 0,
-            0,
+        head_credit = where(head_divisor == 0, 0, head_credit)
+        spouse_credit = (
             p.retirement_income.amount.calc(
                 spouse_pension / spouse_divisor, right=True
             )
             * spouse_divisor,
         )
+        spouse_credit = where(spouse_divisor == 0, 0, spouse_credit)
 
         # distribution_received = tax_unit("c05700", period) > 0 # received lump sum distribution
         distribution_received = (
