c70af1a36d4d121bec6332d8885fc9504b7657bb
Diana Shi <dianashi0607@gmail.com>
2023-10-04 22:14:07 +0000

    Revised

--- policyengine_us/variables/gov/states/mt/tax/income/exemptions/mt_interest_exemptions.py
+++ policyengine_us/variables/gov/states/mt/tax/income/exemptions/mt_interest_exemptions.py
@@ -1,7 +1,7 @@
 from policyengine_us.model_api import *
 
 
-class mt_exemptions(Variable):
+class mt_interest_exemptions(Variable):
     value_type = float
     entity = TaxUnit
     label = "Montana exemptions amount"
@@ -11,13 +11,15 @@ class mt_exemptions(Variable):
     defined_for = StateCode.MT
 
     def formula(tax_unit, period, parameters):
-        p = parameters(period).gov.states.mt.tax.income.exemptions
+        p = parameters(period).gov.states.mt.tax.income.exemptions.interest
         filing_status = tax_unit("filing_status", period)
         cap_amount = p.amount[filing_status]
-        dividend_income = tax_unit(
-            "taxsim_dividends", period
+        person = tax_unit.members
+        dividend_income = person(
+            "dividend_income", period
         )  # dividends or share accounts
-        qalified_amount = min_(cap_amount, dividend_income)
+        dividend_income_unit = tax_unit.sum(dividend_income)
+        qalified_amount = min_(cap_amount, dividend_income_unit)
         age_head = tax_unit("age_head", period)
         eligible_aged_head = age_head >= p.age
         age_spouse = tax_unit("age_spouse", period)
