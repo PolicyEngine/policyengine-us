44f7b88adcf40a47f40248abbf44c99b38e37bf0
jiajiagong0123 <114700434+jiajiagong0123@users.noreply.github.com>
2023-10-04 23:26:02 +0000

    revised the comments

--- policyengine_us/variables/gov/states/id/tax/income/deductions/id_capital_gains_deduction.py
+++ policyengine_us/variables/gov/states/id/tax/income/deductions/id_capital_gains_deduction.py
@@ -17,7 +17,7 @@ class id_capital_gains_deduction(Variable):
         # taxpayer must report capital gain net income
         net_capital_gain = tax_unit("property_sales_net_capital_gain", period)
 
-        # Filers can deduct 60% of captial gains
+
 
         # Ordinary Income do not qualify for the Idaho capital gains deduction
         # Gain from dispositions of certain depreciable property treated as ordinary income
