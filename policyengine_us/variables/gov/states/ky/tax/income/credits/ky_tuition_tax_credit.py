0adf0cdf0af5805068f626f013b2ef20a0e4a92e
Aiwen Peng <88301987+aiwennnpeng@users.noreply.github.com>
2023-10-04 01:17:11 +0000

    variables

--- policyengine_us/variables/gov/states/ky/tax/income/credits/ky_tuition_tax_credit.py
+++ policyengine_us/variables/gov/states/ky/tax/income/credits/ky_tuition_tax_credit.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class ky_tuition_tax_credit(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Kentucky tuition tax credits (from Form 8863-K)"
+    label = "Kentucky tuition tax credits"  # Form 8863-K
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.KY
@@ -15,11 +15,7 @@ class ky_tuition_tax_credit(Variable):
             period,
             ["american_opportunity_credit", "lifetime_learning_credit"],
         )
-        rate = parameters(
-            period
-        ).gov.states.ky.tax.income.credits.tuition_tax.rate
+        rate = parameters(period).gov.states.ky.tax.income.credits.tuition_tax.rate
         tuition_credit = tentative_tax_credit * rate
-        tax_before_non_refundable = tax_unit(
-            "ky_income_tax_before_non_refundable_credits", period
-        )
+        tax_before_non_refundable = tax_unit("ky_income_tax_before_non_refundable_credits", period)
         return min_(tax_before_non_refundable, tuition_credit)
