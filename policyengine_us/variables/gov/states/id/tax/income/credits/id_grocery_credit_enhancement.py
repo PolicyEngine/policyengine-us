7f2d4e7b47862cccebb807bc9e8a902cc4df3a40
jialyu0411 <98068600+jialyu0411@users.noreply.github.com>
2023-10-04 17:34:01 +0000

    few changes

--- policyengine_us/variables/gov/states/id/tax/income/credits/id_grocery_credit_enhancement.py
+++ policyengine_us/variables/gov/states/id/tax/income/credits/id_grocery_credit_enhancement.py
@@ -12,7 +12,8 @@ class id_grocery_credit_enhancement(Variable):
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states.id.tax.income.credits.gc
 
-        #  use id_grocery_credit_enhancement_eligible in defined for and get number of dependents * the amount ($100) as a parameter
+        # use id_grocery_credit_enhancement_eligible in defined for and get number of dependents * the amount of credit as a parameter
+
         dependents = tax_unit("tax_unit_dependents", period)
         base_credit = p.amount  # 100
 
--- policyengine_us/variables/gov/states/id/tax/income/id_agi.py
+++ policyengine_us/variables/gov/states/id/tax/income/id_agi.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class id_agi(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Idaho adjusted grocery credit"
+    label = "Idaho adjusted gross income"
     unit = USD
     definition_period = YEAR
     defined_for = "StateCode.ID"
