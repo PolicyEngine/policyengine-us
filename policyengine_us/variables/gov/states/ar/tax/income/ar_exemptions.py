39a4c880619cd04ba0b13e092005d9e20e9fe7cd
Xinyi-Shao <ellashao99@gmail.com>
2023-10-06 14:46:15 -0400

    Arkansas Exemptions Fixes #3129

---
+++ policyengine_us/variables/gov/states/ar/tax/income/ar_exemptions.py
@@ -0,0 +1,11 @@
+from policyengine_us.model_api import *
+
+
+class ar_exemptions(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Arkansas exemptions from income tax"
+    definition_period = YEAR
+    unit = USD
+    reference = "https://www.dfa.arkansas.gov/images/uploads/incomeTaxOffice/2022_AR1000F_and_AR1000NR_Instructions.pdf#page=10"
+    defined_for = StateCode.AR
