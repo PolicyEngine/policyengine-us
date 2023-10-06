ed987e78693935966f763925a569ed9137bb04e4
PavelMakarchuk <pavel.ma99@gmail.com>
2023-10-06 17:47:46 +0200

    multiplier added

--- policyengine_us/variables/gov/states/co/tax/income/credits/sales_tax_refund/co_sales_tax_refund.py
+++ policyengine_us/variables/gov/states/co/tax/income/credits/sales_tax_refund/co_sales_tax_refund.py
@@ -14,10 +14,11 @@ class co_sales_tax_refund(Variable):
         p = parameters(
             period
         ).gov.states.co.tax.income.credits.sales_tax_refund.amount
+        multiplier = p.multiplier[filing_status]
         if not p.flat_amount_enabled:
             filing_status = tax_unit("filing_status", period)
             agi = tax_unit("co_modified_agi", period)
             multiplier = p.multiplier[filing_status]
             amount = p.scale.calc(agi)
             return multiplier * amount
-        return p.flat
+        return multiplier * p.flat
