10a082a360026745d750b072e2504eb92201b2c7
Ruby Ren <38923159+YichunRen@users.noreply.github.com>
2023-10-06 15:28:55 +0000

    move irs params

--- policyengine_us/variables/gov/states/hi/tax/income/deductions/itemized/hi_reduced_itemized_deduction.py
+++ policyengine_us/variables/gov/states/hi/tax/income/deductions/itemized/hi_reduced_itemized_deduction.py
@@ -15,33 +15,29 @@ class hi_reduced_itemized_deduction(Variable):
 
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states.hi.tax.income.deductions.itemized
+        p_irs = parameters(period).gov.irs.deductions.itemized.reduced_rate
 
         total_deductions = tax_unit("hi_total_itemized_deduction", period)
-
-        hi_medical_expense_deduction = tax_unit(
-            "hi_medical_expense_deduction", period
-        )
-        investment_interest = tax_unit("investment_income_form_4952", period)
-        hi_casualty_loss_deduction = tax_unit(
-            "hi_casualty_loss_deduction", period
-        )
-        partial_total_deductions = (
-            hi_medical_expense_deduction
-            + investment_interest
-            + hi_casualty_loss_deduction
+        partial_itemized_deductions = [
+            "hi_medical_expense_deduction",
+            "investment_income_form_4952",
+            "hi_casualty_loss_deduction",
+        ]
+        partial_total_deductions = add(
+            tax_unit, period, partial_itemized_deductions
         )
 
         # eligible check 1: deduction_difference need to be greater than 0 to have reduced deduction
         difference_eligible = partial_total_deductions < total_deductions
         deduction_difference = total_deductions - partial_total_deductions
-        reduced_difference = deduction_difference * p.rate.reduction
+        reduced_difference = deduction_difference * p_irs.amount
         # eligible check 2: actual agi need to be smaller than agi cap
         hi_agi = tax_unit("hi_agi", period)
         filing_status = tax_unit("filing_status", period)
         agi_cap = p.cap.agi[filing_status]
         agi_eligible = agi_cap < hi_agi
         agi_cap_difference = hi_agi - agi_cap
-        reduced_agi_difference = agi_cap_difference * p.rate.reduced_agi_rate
+        reduced_agi_difference = agi_cap_difference * p_irs.excess_agi
 
         smaller_reduced = min_(reduced_difference, reduced_agi_difference)
         reduced_deductions = max_(0, total_deductions - smaller_reduced)
