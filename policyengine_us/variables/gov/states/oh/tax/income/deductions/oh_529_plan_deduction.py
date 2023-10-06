dc20adc9351af69bb0803bfcd46d2bf08fb5fbb7
Linfeng-ll <95734090+Linfeng-ll@users.noreply.github.com>
2023-10-06 14:39:03 +0000

    fix requests

--- policyengine_us/variables/gov/states/oh/tax/income/deductions/oh_529_plan_deduction.py
+++ policyengine_us/variables/gov/states/oh/tax/income/deductions/oh_529_plan_deduction.py
@@ -13,7 +13,9 @@ class oh_529_plan_deduction(Variable):
     defined_for = StateCode.OH
 
     def formula(tax_unit, period, parameters):
-        p = parameters(period).gov.states.oh.tax.income.deductions.529_plan_contributions
+        p = parameters(
+            period
+        ).gov.states.oh.tax.income.deductions.plan_529_contributions
         contribution_amount = tax_unit("investment_in_529_plan", period)
         capped_amount = min_(contribution_amount, p.max_amount)
         return max_(capped_amount, 0)
