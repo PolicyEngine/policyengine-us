9dba77ae0e5a709e24bab3ae2e40e620a3f501d6
Linfeng-ll <95734090+Linfeng-ll@users.noreply.github.com>
2023-10-06 11:26:45 +0000

    add oh 529 plan deduction

---
+++ policyengine_us/variables/gov/states/oh/tax/income/deductions/oh_529_plan_deduction.py
@@ -0,0 +1,19 @@
+from policyengine_us.model_api import *
+
+
+class oh_529_plan_deduction(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Contributions to an Ohio 529 plan deduction"
+    unit = USD
+    definition_period = YEAR
+    reference = (
+        "https://tax.ohio.gov/static/forms/ohio_individual/individual/2021/pit-it1040-booklet.pdf#page=18",
+    )
+    defined_for = StateCode.OH
+
+    def formula(tax_unit, period, parameters):
+        p = parameters(period).gov.states.oh.tax.income.deductions
+        contribution_529 = tax_unit("investment_in_529_plan", period)
+        deduction_529 = min_(contribution_529, p.plan_529_deduction_threshold)
+        return max_(deduction_529, 0)
