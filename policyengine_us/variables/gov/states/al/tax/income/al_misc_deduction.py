d717cb10190d506df749c2e4e2cb8e2c824c7e9d
dnnnnn0626 <77896070+dnnnnn0626@users.noreply.github.com>
2023-10-07 01:22:18 -0400

    fix 2

--- policyengine_us/variables/gov/states/al/tax/income/al_misc_deduction.py
+++ policyengine_us/variables/gov/states/al/tax/income/al_misc_deduction.py
@@ -7,7 +7,6 @@ class al_misc_deduction(Variable):
     label = "Alabama Work Related Expense"
     unit = USD
     definition_period = YEAR
-    defined_for = StateCode.AL
     reference = (
         "https://law.justia.com/codes/alabama/2021/title-40/chapter-18/article-1/section-40-18-15/"
         "https://www.revenue.alabama.gov/ultraviewer/viewer/basic_viewer/index.html?form=2023/01/22f40schabdc_blk.pdf#page=1"
