84ca2837910cab9af033947663bc7ebc1787c7e5
dnnnnn0626 <77896070+dnnnnn0626@users.noreply.github.com>
2023-10-06 23:22:59 -0400

    fixed tests

--- policyengine_us/variables/gov/states/al/tax/income/al_itemized_deduction.py
+++ policyengine_us/variables/gov/states/al/tax/income/al_itemized_deduction.py
@@ -15,5 +15,13 @@ class al_itemized_deductions(Variable):
 
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states.al.tax.income.deductions.itemized
-        deduction_value = add(tax_unit, period, p.itemized)
+        deduction_value = (('real_estate_tax', period, p.itemized)+
+                         ('mortage_interest_deduction', period, p.itemized) +
+                         ('investment_intertest_deduction', period, p.itemized) +
+                         ('charitable_deduction', period, p.itemized)+
+                         ('taxsim_tfica', period, p.itemized) +
+                         ('self_employment_tax', period, p.itemized)+
+                         ('al_medical_expense_deduction', period, p.itemized) +
+                         ('al_misc_deduction', period, p.itemized)
+                        )
         return deduction_value
