806bc52b31a76024b6f04bc6d0f9f04574adf2ad
Diana Shi <dianashi0607@gmail.com>
2023-10-09 03:36:31 +0000

    Revised

--- policyengine_us/variables/gov/states/mt/tax/income/exemptions/mt_interest_exemptions.py
+++ policyengine_us/variables/gov/states/mt/tax/income/exemptions/mt_interest_exemptions.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class mt_interest_exemptions(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Montana exemptions amount"
+    label = "Montana interest exemptions"
     unit = USD
     definition_period = YEAR
     reference = "https://mtrevenue.gov/wp-content/uploads/dlm_uploads/2022/12/Form-2-2022-Instructions.pdf#page=25"
@@ -13,16 +13,14 @@ class mt_interest_exemptions(Variable):
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states.mt.tax.income.exemptions.interest
         filing_status = tax_unit("filing_status", period)
-        cap_amount = p.amount[filing_status]
+        cap = p.cap[filing_status]
         person = tax_unit.members
-        dividend_income = person(
-            "dividend_income", period
-        )  # dividends or share accounts
+        dividend_income = add(person, period, ["taxable_interest_income"])
         dividend_income_unit = tax_unit.sum(dividend_income)
-        qualified_amount = min_(cap_amount, dividend_income_unit)
+        capped_amount = min_(cap, dividend_income_unit)
         age_head = tax_unit("age_head", period)
         eligible_aged_head = age_head >= p.age
         age_spouse = tax_unit("age_spouse", period)
         eligible_aged_spouse = age_spouse >= p.age
         eligible_tax_unit = eligible_aged_head | eligible_aged_spouse
-        return where(eligible_tax_unit, qualified_amount, 0)
+        return eligible_tax_unit * capped_amount
