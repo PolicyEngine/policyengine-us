94224c1f3e7bfa1f17da1e35c0818025a93a47aa
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2023-10-09 18:35:47 +0000

    Including missed condition

--- policyengine_us/variables/gov/states/ct/tax/income/subtractions/ct_social_security_benefit_adjustment.py
+++ policyengine_us/variables/gov/states/ct/tax/income/subtractions/ct_social_security_benefit_adjustment.py
@@ -7,7 +7,7 @@ class ct_social_security_benefit_adjustment(Variable):
     unit = USD
     label = "Connecticut social security benefit adjustment"
     definition_period = YEAR
-    defined_for = "ct_social_security_benefit_adjustment_eligible"
+    defined_for = StateCode.CT
 
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.irs.social_security.taxability
@@ -15,12 +15,15 @@ class ct_social_security_benefit_adjustment(Variable):
         ss_agi_frac = p.rate.lower
         filing_status = tax_unit("filing_status", period)
         base_amount = income_base_amount[filing_status]
-        ss_rate = parameters(
+
+        c = parameters(
             period
-        ).gov.states.ct.tax.income.subtractions.social_security.rate
-        # is_eligible = tax_unit(
-        #     "ct_social_security_benefit_adjustment_eligible", period
-        # )
+        ).gov.states.ct.tax.income.subtractions.social_security
+        ss_rate = c.rate
+        filing_status = tax_unit("filing_status", period)
+        agi = add(tax_unit, period, ["adjusted_gross_income"])
+        income_threshold = c.income_threshold[filing_status]
+
         ss_benefit = add(tax_unit, period, ["taxable_social_security"])
         agi = add(tax_unit, period, ["adjusted_gross_income"])
 
@@ -28,5 +31,4 @@ class ct_social_security_benefit_adjustment(Variable):
         excess = agi + ss_agi_frac * ss_benefit - base_amount
         max_inclusion = min_(includable_ss, ss_rate * excess)
         adjusted_ss_benefit = max_(max_inclusion - includable_ss, 0)
-        # return where(is_eligible, 0, adjusted_ss_benefit)
-        return adjusted_ss_benefit
+        return where(agi < income_threshold, ss_benefit, adjusted_ss_benefit)
