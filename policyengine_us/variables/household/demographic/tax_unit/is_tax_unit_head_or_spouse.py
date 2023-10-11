a46c873e4817392e17b155d26b3935dd96131a03
Max Ghenis <mghenis@gmail.com>
2023-10-11 08:49:45 -0400

    Update policyengine_us/variables/household/demographic/tax_unit/is_tax_unit_head_or_spouse.py

--- policyengine_us/variables/household/demographic/tax_unit/is_tax_unit_head_or_spouse.py
+++ policyengine_us/variables/household/demographic/tax_unit/is_tax_unit_head_or_spouse.py
@@ -3,7 +3,7 @@ from policyengine_us.model_api import *
 
 class is_tax_unit_head_or_spouse(Variable):
     value_type = bool
-    entity = TaxUnit
+    entity = Person
     label = "Head or Spouse of tax unit"
     definition_period = YEAR
 
