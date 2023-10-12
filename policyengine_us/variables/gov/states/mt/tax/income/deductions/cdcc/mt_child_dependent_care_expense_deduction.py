b458f34d55816cec5c49c57b53ee93a428b304b3
jiajiagong0123 <114700434+jiajiagong0123@users.noreply.github.com>
2023-10-12 00:23:15 +0000

    change parameters'name and add 0 threshold
    Co-authored-by: Pavel Makarchuk <PavelMakarchuk@users.noreply.github.com>
    Co-authored-by: TurnerLuo <TurnerLuo@users.noreply.github.com>

--- policyengine_us/variables/gov/states/mt/tax/income/deductions/cdcc/mt_child_dependent_care_expense_deduction.py
+++ policyengine_us/variables/gov/states/mt/tax/income/deductions/cdcc/mt_child_dependent_care_expense_deduction.py
@@ -16,10 +16,9 @@ class mt_child_dependent_care_expense_deduction(Variable):
         num_dependents = tax_unit("num_dependents", period)
 
         # Determine the number of qualifying children (under 15 or disabled)
-        qualifying_child = num_dependents[(num_dependents <= 15) | (num_dependents == "disabled")]
+        qualifying_child = num_dependents[(num_dependents <= p.threshold.age) | (num_dependents == "disabled")]
 
-        child_qualifies = qualifying_child >= 1
         income_qualifies = agi <= p.income_threshold[qualifying_child]
         
 
-        return child_qualifies * income_qualifies * min(p.amount[qualifying_child], care_expense)
+        return income_qualifies * min_(p.amount[qualifying_child], care_expenses)
