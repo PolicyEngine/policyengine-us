e7eaa476b42ea349e605bd0c9e5961212ad49382
Ruby Ren <38923159+YichunRen@users.noreply.github.com>
2023-10-13 15:10:44 +0000

    exemptions unit test

---
+++ policyengine_us/variables/gov/states/hi/tax/income/exemptions/hi_exemptions.py
@@ -0,0 +1,14 @@
+from policyengine_us.model_api import *
+
+
+class hi_exemptions(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Hawaii exemptions"
+    unit = USD
+    documentation = (
+        "https://files.hawaii.gov/tax/forms/2022/n11ins.pdf#page=20"
+    )
+    definition_period = YEAR
+    defined_for = StateCode.HI
+    
