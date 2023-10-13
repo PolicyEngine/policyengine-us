85b518ec30c275c30b9c06b0ca9f07110839619d
xianxinzhu <97819588+xianxinzhu@users.noreply.github.com>
2023-10-13 00:29:50 +0000

    Fixes variables

--- policyengine_us/variables/gov/states/hi/tax/income/exemptions/hi_exemptions.py
+++ policyengine_us/variables/gov/states/hi/tax/income/exemptions/hi_exemptions.py
@@ -15,10 +15,10 @@ class hi_exemptions(Variable):
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states.hi.tax.income.exemptions
         exemp = tax_unit("exemptions", period)
-
+        # disability exemption
         disabled_head = tax_unit("head_is_disabled", period).astype(int)
         disabled_spouse = tax_unit("spouse_is_disabled", period).astype(int)
-
+        # aged exemption
         aged_head = (tax_unit("age_head", period) >= p.age_threshold).astype(
             int
         )
@@ -26,15 +26,18 @@ class hi_exemptions(Variable):
             tax_unit("age_spouse", period) >= p.age_threshold
         ).astype(int)
 
+        # if the head or spouse has disabled exemption, aged exemption will be excluded from the total exemptions
         adjusted_exemp = where(
-            disabled_head == 1, exemp - disabled_head, exemp + aged_head
+            disabled_head, exemp - disabled_head, exemp + aged_head
         )
+        # exemp_base stores the normal exemptions other than disability exemptions
+        # and the exemp_base will be multiply by 1_144
         exemp_base = where(
-            disabled_spouse == 1,
-            adjusted_exemp - disabled_spouse,
+            disabled_spouse,
+            max_(0, adjusted_exemp - disabled_spouse),
             adjusted_exemp + aged_spouse,
         )
-
+        # disabled_exemptions will be multiplied by 7_000 instead
         disabled_exemptions = (disabled_head + disabled_spouse) * p.disabled
         exemption_base_amount = exemp_base * p.base
 
