4cd49c566ea1cf597c992d3259fae10554dd5aca
dnnnnn0626 <77896070+dnnnnn0626@users.noreply.github.com>
2023-10-14 16:37:59 -0400

    fixed by comments

--- policyengine_us/variables/gov/states/al/tax/income/al_itemized_deductions.py
+++ policyengine_us/variables/gov/states/al/tax/income/al_itemized_deductions.py
@@ -8,18 +8,10 @@ class al_itemized_deductions(Variable):
     unit = USD
     definition_period = YEAR
     reference = (
-        "https://law.justia.com/codes/alabama/2022/title-40/chapter-18/article-1/section-40-18-15/"
-        "https://www.revenue.alabama.gov/ultraviewer/viewer/basic_viewer/index.html?form=2023/01/22f40schabdc_blk.pdf#page=1"
+        "https://law.justia.com/codes/alabama/2022/title-40/chapter-18/article-1/section-40-18-15/"  # Code of Alabama Section 40-18-15
+        "https://www.revenue.alabama.gov/ultraviewer/viewer/basic_viewer/index.html?form=2023/01/22f40schabdc_blk.pdf#page=1"  # 2022 Schedule A (Form 1040)
+        "https://www.revenue.alabama.gov/ultraviewer/viewer/basic_viewer/index.html?form=2022/06/21f40schabdc_blk.pdf#page=1"  # 2021 Schedule A (Form 1040)
     )
     defined_for = StateCode.AL
 
-    def formula(tax_unit, period, parameters):
-        return (
-            add(tax_unit, period, ["real_estate_taxes"])
-            + add(tax_unit, period, ["charitable_deduction"])
-            + add(tax_unit, period, ["taxsim_tfica"])
-            + tax_unit("al_medical_expense_deduction", period)
-            + tax_unit("al_misc_deduction", period)
-            + tax_unit("al_investment_interest_deduction", period)
-            + tax_unit("al_mortgage_interest_deduction", period)
-        )
+    adds = "gov.states.al.tax.income.deductions.itemized.sources"
