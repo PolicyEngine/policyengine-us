2926a385ae3d3dee8bfe84b38bc591ee940a9ee5
jiajiagong0123 <114700434+jiajiagong0123@users.noreply.github.com>
2023-10-18 23:05:35 +0000

    revise variables

--- policyengine_us/variables/gov/states/mt/tax/income/deductions/cdcc/mt_child_dependent_care_expense_deduction_eligible.py
+++ policyengine_us/variables/gov/states/mt/tax/income/deductions/cdcc/mt_child_dependent_care_expense_deduction_eligible.py
@@ -8,7 +8,7 @@ class mt_child_dependent_care_expense_deduction_eligible(Variable):
     defined_for = StateCode.MT
 
     def formula(tax_unit, period, parameters):
-        p = parameters(period).gov.states.mt.tax.income.deductions.standard.mt_child_dependent_care_expense_deduction
+        p = parameters(period).gov.states.mt.tax.income.deductions.standard.child_dependent_care_expense_deduction
         agi = tax_unit("mt_agi", period)
         eligible_children = tax_unit("mt_child_dependent_care_expense_deduction_eligible_children", period)
         return agi <= p.threshold[eligible_children]
