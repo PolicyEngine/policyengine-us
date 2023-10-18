5600e253c5286ee665390339071eeb8a82d2fd70
Ziqian Cui <45090757+ZiqianCui@users.noreply.github.com>
2023-10-18 16:19:48 +0000

    added joint indiv to each policy

--- policyengine_us/variables/gov/states/ms/tax/income/deductions/ms_deductions_indiv.py
+++ policyengine_us/variables/gov/states/ms/tax/income/deductions/ms_deductions_indiv.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class ms_deductions_indiv(Variable):
     value_type = float
     entity = Person
-    label = "Mississippi deductions"
+    label = "Mississippi deductions filing seperately"
     unit = USD
     definition_period = YEAR
     reference = (
--- policyengine_us/variables/gov/states/ms/tax/income/deductions/ms_deductions_joint.py
+++ policyengine_us/variables/gov/states/ms/tax/income/deductions/ms_deductions_joint.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class ms_deductions_joint(Variable):
     value_type = float
     entity = Person
-    label = "Mississippi deductions"
+    label = "Mississippi deductions filing jointly"
     unit = USD
     definition_period = YEAR
     reference = (
