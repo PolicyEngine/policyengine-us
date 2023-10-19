1ea68ecae05186e5efd442cc1b6ad13bea14dd47
leogoldman <leogoldman1@gmail.com>
2023-10-19 01:23:04 -0400

    Simplified exemption eligible class

--- policyengine_us/variables/gov/states/ky/tax/income/exclusions/pension_income/ky_pension_income_exclusion_exemption_eligible.py
+++ policyengine_us/variables/gov/states/ky/tax/income/exclusions/pension_income/ky_pension_income_exclusion_exemption_eligible.py
@@ -9,17 +9,10 @@ class ky_pension_income_exclusion_exemption_eligible(Variable):
     defined_for = StateCode.KY
     reference = "https://revenue.ky.gov/Forms/Schedule%20P%202022.pdf"
 
-    def formula(person, period, parameters):
-        # Determine if person is eligible for exemption.
-        # True if they retired from federal or KY government or receive railroad benefits.
-        retired_from_fed_gov = person(
-            "retired_from_federal_government", period
-        )
-        retired_from_ky_gov = person("retired_from_ky_government", period)
-        receives_railroad_benefits = person("railroad_benefits", period) > 0
-
-        return (
-            retired_from_fed_gov
-            | retired_from_ky_gov
-            | receives_railroad_benefits
-        )
+    # Determine if person is eligible for exemption.
+    # True if they retired from federal or KY government or receive railroad benefits.
+    adds = [
+        "retired_from_federal_government",
+        "retired_from_ky_government",
+        "railroad_benefits",
+    ]
