056f801ce2c328616f748cfcea6bfe2736c1f1b9
JudyZ13 <112495414+JudyZ13@users.noreply.github.com>
2023-10-31 16:52:20 -0500

    Update policyengine_us/variables/gov/states/az/tax/income/exemptions/az_parents_grandparents_exmeption.py
    Co-authored-by: Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/states/az/tax/income/exemptions/az_parents_grandparents_exmeption.py
+++ policyengine_us/variables/gov/states/az/tax/income/exemptions/az_parents_grandparents_exmeption.py
@@ -12,7 +12,7 @@ class az_parents_grandparents_exemption(Variable):
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states.az.tax.income.exemptions
         person = tax_unit.members
-        # The exemption is provided for parents and grandparents who receive over 50% of their care and support
+        # The exemption is provided for parents and grandparents who receive over a percentage of their care and support
         # payments from the filer.
         care_and_support_payment = person("care_and_support_payment", period)
         care_and_support_costs = person("care_and_support_costs", period)
