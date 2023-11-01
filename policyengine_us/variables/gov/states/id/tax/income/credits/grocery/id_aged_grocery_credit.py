369d875da99d047611d21f7f74d8c13c4ae3d74d
jialyu0411 <98068600+jialyu0411@users.noreply.github.com>
2023-11-01 18:23:21 +0000

    update unit tests

--- policyengine_us/variables/gov/states/id/tax/income/credits/grocery/id_aged_grocery_credit.py
+++ policyengine_us/variables/gov/states/id/tax/income/credits/grocery/id_aged_grocery_credit.py
@@ -11,24 +11,20 @@ class id_aged_grocery_credit(Variable):
 
     def formula(tax_unit, period, parameters):
         #### Define path to gc ####
-        path_gc = parameters(period).gov.states.id.tax.income.credits.gc
+        p = parameters(period).gov.states.id.tax.income.credits.gc
 
         #### Get ages for both the head and spouse ####
-        person = tax_unit.members
-        head = person("is_tax_unit_head", period)
-        spouse = person("is_tax_unit_spouse", period)
-        total_head_and_spouse = tax_unit.sum(head + spouse)
         age_head = tax_unit("age_head", period)
         age_spouse = tax_unit("age_spouse", period)
 
         # Create boolean variables head_aged and spouse_aged to identify if they're over 65
-        age_threshold = path_gc.age_eligibility  # age threshold
+        age_threshold = p.age_eligibility  # age threshold
         head_aged = age_head >= age_threshold
         spouse_aged = age_spouse >= age_threshold  # age threshold
-        total_aged = head_aged + spouse_aged
+        total_aged = sum(head_aged) + sum(spouse_aged)
 
         #### Define constant ####
-        age_amount = path_gc.aged_amount  # additional amount
+        age_amount = p.aged_amount  # additional amount
         aged_addition = age_amount * total_aged
 
         return aged_addition
