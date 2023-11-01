775f685348bc47a90ea1b87df53c7f482676b715
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-11-01 11:24:15 -0400

    Move and rename incapable_of_self_care to is_incapable_of_self_care

--- policyengine_us/variables/gov/irs/taxcalc/sources.py
+++ policyengine_us/variables/gov/irs/taxcalc/sources.py
@@ -1,15 +1,6 @@
 from policyengine_us.model_api import *
 
 
-class incapable_of_self_care(Variable):
-    value_type = bool
-    entity = Person
-    label = "Incapable of self-care"
-    documentation = "Whether this person is physically or mentally incapable of caring for themselves."
-    definition_period = YEAR
-    reference = "https://www.law.cornell.edu/uscode/text/26/21"
-
-
 class f6251(Variable):
     value_type = bool
     entity = TaxUnit
