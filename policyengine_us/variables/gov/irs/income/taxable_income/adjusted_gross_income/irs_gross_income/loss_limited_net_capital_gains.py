c0d1becd125efc1539de4c77dfb7a2845f2d0790
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-11-02 15:32:43 -0400

    Add capital_gains/loss_limit.yaml parameter and use it in formula

--- policyengine_us/variables/gov/irs/income/taxable_income/adjusted_gross_income/irs_gross_income/loss_limited_net_capital_gains.py
+++ policyengine_us/variables/gov/irs/income/taxable_income/adjusted_gross_income/irs_gross_income/loss_limited_net_capital_gains.py
@@ -9,6 +9,8 @@ class loss_limited_net_capital_gains(Variable):
     unit = USD
 
     def formula(tax_unit, period, parameters):
-        loss_limit = 3000.0 / tax_unit("sep", period)
+        p = parameters(period).gov.irs
+        filing_status = tax_unit("filing_status", period)
+        loss_limit = p.capital_gains.loss_limit[filing_status]
         net_capital_gains = tax_unit("net_capital_gains", period)
         return max_(-loss_limit, net_capital_gains)
