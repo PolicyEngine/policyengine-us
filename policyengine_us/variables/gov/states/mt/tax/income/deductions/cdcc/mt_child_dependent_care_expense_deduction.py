6dc5f8a6be82949d701ca78fc97941351fbf2270
jiajiagong0123 <114700434+jiajiagong0123@users.noreply.github.com>
2023-11-02 16:36:42 +0000

    revise test files Montana Child and Dependent Care expense Deduction Fixes #3059

--- policyengine_us/variables/gov/states/mt/tax/income/deductions/cdcc/mt_child_dependent_care_expense_deduction.py
+++ policyengine_us/variables/gov/states/mt/tax/income/deductions/cdcc/mt_child_dependent_care_expense_deduction.py
@@ -11,6 +11,6 @@ class mt_child_dependent_care_expense_deduction(Variable):
 
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states.mt.tax.income.deductions.standard.child_dependent_care_expense_deduction
-        care_expenses = ("tax_unit_childcare_expenses", period)
+        care_expenses = tax_unit("tax_unit_childcare_expenses", period)
         eligible_children = tax_unit("mt_child_dependent_care_expense_deduction_eligible_children", period)
         return min_(p.amount[eligible_children], care_expenses)
