1f8deef50b28084f08071c79778de5839aeba91f
jialyu0411 <98068600+jialyu0411@users.noreply.github.com>
2023-11-03 18:47:00 +0000

    Co-authored-by: Pavel Makarchuk <PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/states/id/tax/income/credits/grocery/id_aged_grocery_credit.py
+++ policyengine_us/variables/gov/states/id/tax/income/credits/grocery/id_aged_grocery_credit.py
@@ -17,14 +17,7 @@ class id_aged_grocery_credit(Variable):
         age_head = tax_unit("age_head", period)
         age_spouse = tax_unit("age_spouse", period)
 
-        # Create boolean variables head_aged and spouse_aged to identify if they're over a certain threshold 
-        age_threshold = p.age_eligibility  # age threshold
-        head_aged = age_head >= age_threshold
-        spouse_aged = age_spouse >= age_threshold  # age threshold
-        total_aged = head_aged + spouse_aged
-
-        #### Define constant ####
-        age_amount = p.aged_amount  # additional amount
-        aged_addition = age_amount * total_aged
-
-        return age_amount * total_aged 
+        # Create boolean variables head_aged and spouse_aged to identify if they're over a certain threshold
+        head_aged = p.aged_amount.calc(age_head)
+        spouse_aged = p.aged_amount.calc(age_spouse)  # age threshold
+        return head_aged + spouse_aged
