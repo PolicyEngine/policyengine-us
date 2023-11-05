4a2f334927de2f7997310a9aa5a3f4dcc9d80fc5
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2023-11-05 22:08:04 +0000

    Fixing formatting

--- policyengine_us/variables/gov/states/oh/tax/income/credits/oh_senior_citizen_credit.py
+++ policyengine_us/variables/gov/states/oh/tax/income/credits/oh_senior_citizen_credit.py
@@ -23,9 +23,9 @@ class oh_senior_citizen_credit(Variable):
 
         age = person("age", period)
         any_elderly = age >= p.age_threshold
-        elderly_eligible = tax_unit.sum(has_not_taken_lump_sum_distribution & any_elderly)
-        credit_amount = p.agi_limit.calc(tax_unit("oh_agi", period))
-        
-        return (
-            elderly_eligible * credit_amount
+        elderly_eligible = tax_unit.sum(
+            has_not_taken_lump_sum_distribution & any_elderly
         )
+        credit_amount = p.agi_limit.calc(tax_unit("oh_agi", period))
+
+        return elderly_eligible * credit_amount
