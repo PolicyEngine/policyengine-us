9a80c1a878381cbadab0fe09b51c688a031943db
yy3187 <129118329+yy3187@users.noreply.github.com>
2023-11-06 07:08:17 +0000

    change variables

--- policyengine_us/variables/gov/states/mt/tax/income/subtractions/mt_disability_income_subtraction.py
+++ policyengine_us/variables/gov/states/mt/tax/income/subtractions/mt_disability_income_subtraction.py
@@ -11,15 +11,15 @@ class mt_disability_income_subtraction(Variable):
     reference = "https://rules.mt.gov/gateway/RuleNo.asp?RN=42%2E15%2E217"
 
     def formula(tax_unit, period, parameters):
+        #first select head and spouse with ages below the specific threshold, and 
+        #select those who are retired on total disability and disabiled, and
+        #calculate the corresbonding disability benefits. 
         person = tax_unit.members
         p = parameters(
             period
         ).gov.states.mt.tax.income.subtractions.disability_income
-        age_eligible = person("age", period) < p.age_threshold
-        is_head = person("is_tax_unit_head", period)
-        age_eligible_head = age_eligible & is_head
-        is_spouse = person("is_tax_unit_spouse", period)
-        age_eligible_spouse = age_eligible & is_spouse
+        age_eligible_head = tax_unit("age_head", period) < p.age_threshold
+        age_eligible_spouse = tax_unit("age_spouse", period) < p.age_threshold
         is_retired_on_disability = person(
             "retired_on_total_disability", period
         )
