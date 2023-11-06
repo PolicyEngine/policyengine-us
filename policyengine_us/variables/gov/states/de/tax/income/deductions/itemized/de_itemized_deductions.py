645877f5faaebd3370017497ad5aba4fb5cd4e41
ShiqiLiu <54519061+Kiki-67@users.noreply.github.com>
2023-11-06 05:06:40 +0000

    add test and salt

--- policyengine_us/variables/gov/states/de/tax/income/deductions/itemized/de_itemized_deductions.py
+++ policyengine_us/variables/gov/states/de/tax/income/deductions/itemized/de_itemized_deductions.py
@@ -7,38 +7,35 @@ class de_itemized_deductions(Variable):
     label = "Delaware itemized deductions"
     unit = USD
     definition_period = YEAR
-    reference = (
-        "https://revenuefiles.delaware.gov/2022/PIT-RES_TY22_2022-02_Instructions.pdf#page=7"
-        "https://revenuefiles.delaware.gov/2022/PIT-RES_TY22_2022-02_Instructions.pdf#page=11"
-        "https://casetext.com/statute/delaware-code/title-30-state-taxes/part-ii-income-inheritance-and-estate-taxes/chapter-11-personal-income-tax/subchapter-ii-resident-individuals/section-1109-itemized-deductions-for-application-of-this-section-see-66-del-laws-c-86-section-8"
-    )
+    reference = "https://revenuefiles.delaware.gov/2022/TY22_PIT-RSA_2022-02_PaperInteractive.pdf"
     defined_for = StateCode.DE
 
     def formula(tax_unit, period, parameters):
-        p = parameters(
-            period
-        ).gov.states.de.tax.income.subtractions.exclusions.itemized
+        p = parameters(period).gov.irs.deductions
+        filing_status = tax_unit("filing_status", period)
         deductions = [
             deduction
             for deduction in p.itemized_deductions
-            if deduction
-            not in [
-                "interest_deduction",
-                "charitable_deduction",
-            ]
+            if deduction not in ["interest_deduction", "salt_deduction"]
         ]
         federal_deductions = add(tax_unit, period, deductions)
 
-        de_donation = add(
-            tax_unit,
-            period,
-            ["charitable_cash_donations", "charitable_non_cash_donations"],
+        salt_sales_or_income = tax_unit(
+            "state_and_local_sales_or_income_tax", period
         )
 
-        de_interest = add(
+        total_salt = (
+            add(tax_unit, period, ["real_estate_taxes"]) + salt_sales_or_income
+        )
+
+        salt_amount = min_(
+            total_salt, p.itemized.salt_and_real_estate.cap[filing_status]
+        )
+
+        interest = add(
             tax_unit,
             period,
             ["mortgage_interest", "investment_income_form_4952"],
         )
 
-        return federal_deductions + de_donation + de_interest
+        return federal_deductions + salt_amount + interest
