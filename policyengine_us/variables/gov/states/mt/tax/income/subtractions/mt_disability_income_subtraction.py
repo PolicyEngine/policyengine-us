ab158cab3661455a460f79231b2941c34592e962
yy3187 <129118329+yy3187@users.noreply.github.com>
2023-11-07 08:26:35 +0000

    change parameters and variables

--- policyengine_us/variables/gov/states/mt/tax/income/subtractions/mt_disability_income_subtraction.py
+++ policyengine_us/variables/gov/states/mt/tax/income/subtractions/mt_disability_income_subtraction.py
@@ -12,23 +12,21 @@ class mt_disability_income_subtraction(Variable):
 
     def formula(tax_unit, period, parameters):
         # first select head and spouse with ages below the specific threshold, and
-        # select those who are retired on total disability and disabiled, and
+        # select those who are retired on total disability and disabled, and
         # calculate the corresbonding disability benefits.
         person = tax_unit.members
         p = parameters(
             period
         ).gov.states.mt.tax.income.subtractions.disability_income
-        age_eligible_head = tax_unit("age_head", period) < p.age_threshold
-        age_eligible_spouse = tax_unit("age_spouse", period) < p.age_threshold
+        age_eligible = person("age", period) < p.age_threshold
+        head_or_spouse = person("is_tax_unit_head_or_spouse", period)
         is_retired_on_disability = person(
             "retired_on_total_disability", period
         )
-        retired_head = age_eligible_head & is_retired_on_disability
-        retired_spouse = age_eligible_spouse & is_retired_on_disability
+        retired = age_eligible & is_retired_on_disability
         is_disabled = person("is_disabled", period)
-        qualified_head = retired_head & is_disabled
-        qualified_spouse = retired_spouse & is_disabled
-        qualified_head_or_spouse = qualified_head | qualified_spouse
+        retired_disabled = retired & is_disabled
+        qualified_head_or_spouse = retired_disabled & head_or_spouse
         eligible_benefits = (
             person("disability_benefits", period) * qualified_head_or_spouse
         )
