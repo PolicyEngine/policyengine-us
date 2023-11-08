d27fbf81e2e91880e9a15219e02fdc562d97ecdd
PavelMakarchuk <pavel.ma99@gmail.com>
2023-11-08 23:50:08 +0100

    references and placeholder

--- policyengine_us/variables/gov/states/al/tax/income/al_taxable_income.py
+++ policyengine_us/variables/gov/states/al/tax/income/al_taxable_income.py
@@ -8,13 +8,9 @@ class al_taxable_income(Variable):
     defined_for = StateCode.AL
     unit = USD
     definition_period = YEAR
-    # The Code of Alabama 1975
     reference = (
-        "https://law.justia.com/codes/alabama/2022/title-40/chapter-18/article-1/section-40-18-15/"  # Code of Alabama Section 40-18-15
-        "https://www.revenue.alabama.gov/ultraviewer/viewer/basic_viewer/index.html?form=2023/01/22f40schabdc_blk.pdf#page=1"  # 2022 Schedule A (Form 1040)
-        "https://www.revenue.alabama.gov/ultraviewer/viewer/basic_viewer/index.html?form=2022/06/21f40schabdc_blk.pdf#page=1"  # 2021 Schedule A (Form 1040)
+        "https://www.revenue.alabama.gov/ultraviewer/viewer/basic_viewer/index.html?form=2023/01/22f40abk.pdf"  # 2022 Form 40A Booklet
     )
-
     def formula(tax_unit, period, parameters):
         al_ded = tax_unit("al_deductions", period)
         al_agi = tax_unit("al_agi", period)
--- policyengine_us/variables/gov/states/al/tax/income/deductions/al_deductions.py
+++ policyengine_us/variables/gov/states/al/tax/income/deductions/al_deductions.py
@@ -8,16 +8,14 @@ class al_deductions(Variable):
     unit = USD
     definition_period = YEAR
     reference = (
-        "https://law.justia.com/codes/alabama/2022/title-40/chapter-18/article-1/section-40-18-15/"  # Code of Alabama Section 40-18-15
-        "https://www.revenue.alabama.gov/ultraviewer/viewer/basic_viewer/index.html?form=2023/01/22f40schabdc_blk.pdf#page=1"  # 2022 Schedule A (Form 1040)
-        "https://www.revenue.alabama.gov/ultraviewer/viewer/basic_viewer/index.html?form=2022/06/21f40schabdc_blk.pdf#page=1"  # 2021 Schedule A (Form 1040)
+        "https://www.revenue.alabama.gov/ultraviewer/viewer/basic_viewer/index.html?form=2023/01/22f40abk.pdf"  # 2022 Form 40A Booklet
     )
     defined_for = StateCode.AL
 
     def formula(tax_unit, period, parameters):
         itm_ded = tax_unit("al_itemized_deductions", period)
         std_ded = tax_unit("al_standard_deduction", period)
+        federal_ded = tax_unit("al_federal_tax_deduction_deduction", period)
         al_ded = max_(itm_ded, std_ded)
-        per_exm = tax_unit("al_personal_exemption", period)
-        dep_exm = tax_unit("al_dependent_exemption", period)
-        return al_ded + per_exm + dep_exm
+        exemptions = add(tax_unit, period, ["al_personal_exemption", "al_dependent_exemption"])
+        return al_ded + federal_ded + exemptions 
