7e20d3552ecc1788c15af13927388e042d00a416
Kevintowne <89811054+Kevintowne@users.noreply.github.com>
2023-11-09 19:54:33 +0000

    Fixes variable description

--- policyengine_us/variables/gov/irs/gross_income/disqualified_income.py
+++ policyengine_us/variables/gov/irs/gross_income/disqualified_income.py
@@ -4,8 +4,8 @@ from policyengine_us.model_api import *
 class disqualified_income(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Disqualified income"
+    label = "Earned Income Disqualified income"
     unit = USD
-    reference = "https://www.law.cornell.edu/uscode/text/26/32#:~:text=In%20the%20case%20of%20an,exceed%20the%20earned%20income%20amount"
+    reference = "https://www.law.cornell.edu/uscode/text/26/32"
     definition_period = YEAR
     adds = "gov.irs.gross_income.disqualified_income"
