31185264bed2a4153f28074e0899583352566f02
leogoldman <leogoldman1@gmail.com>
2023-11-08 21:11:10 -0500

    Added gambling_losses_capped_at_winnings

--- policyengine_us/variables/household/income/person/gambling_winnings.py
+++ policyengine_us/variables/household/income/person/gambling_winnings.py
@@ -1,9 +1,9 @@
 from policyengine_us.model_api import *
 
 
-class gambling_losses(Variable):
+class gambling_winnings(Variable):
     value_type = float
     entity = Person
-    label = "Gambling losses"
+    label = "Gambling winnings"
     unit = USD
     definition_period = YEAR
