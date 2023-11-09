269052070f37be5e270cad899115b49fae16b448
JudyZ13 <112495414+JudyZ13@users.noreply.github.com>
2023-11-09 01:13:30 -0600

    Update policyengine_us/variables/gov/states/az/tax/income/subtractions/college_savings/az_529_college_savings_plans_subtraction.py
    Co-authored-by: Max Ghenis <mghenis@gmail.com>

--- policyengine_us/variables/gov/states/az/tax/income/subtractions/college_savings/az_529_college_savings_plans_subtraction.py
+++ policyengine_us/variables/gov/states/az/tax/income/subtractions/college_savings/az_529_college_savings_plans_subtraction.py
@@ -18,7 +18,7 @@ class az_529_college_savings_plans_subtraction(Variable):
         filing_status = tax_unit("filing_status", period)
         contributions_529 = tax_unit("investment_in_529_plan", period)
 
-        cap = p.cap[filing_status]
+        cap_per_beneficiary = p.cap[filing_status]
         person = tax_unit.members
         beneficiary = add(
             tax_unit, period, ["college_savigns_plan_529_benefitiary"]
