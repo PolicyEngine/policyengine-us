61397acaa10f4942f9692acb6ed3143725a23039
jiajiagong0123 <114700434+jiajiagong0123@users.noreply.github.com>
2023-11-09 23:34:53 +0000

    revise the variable file and delete unnecessary files

--- policyengine_us/variables/gov/states/mt/tax/income/deductions/cdcc/mt_child_dependent_care_expense_deduction.py
+++ policyengine_us/variables/gov/states/mt/tax/income/deductions/cdcc/mt_child_dependent_care_expense_deduction.py
@@ -13,9 +13,12 @@ class mt_child_dependent_care_expense_deduction(Variable):
         p = parameters(
             period
         ).gov.states.mt.tax.income.deductions.standard.child_dependent_care_expense_deduction
+        agi = tax_unit("mt_agi", period)
         care_expenses = tax_unit("tax_unit_childcare_expenses", period)
+        reduced_expense = max_(0, care_expenses - p.threshold.income.calc(agi))  
         eligible_children = tax_unit(
             "mt_child_dependent_care_expense_deduction_eligible_children",
             period,
         )
-        return min_(p.amount.calc(eligible_children), care_expenses)
+        return min_(p.amount.calc(eligible_children), reduced_expense)
+    
