46dcc0c17ab95cb47aa9008a20a1356a082ba1e7
hua7450 <113824411+hua7450@users.noreply.github.com>
2023-11-11 00:57:00 +0000

    add variable

--- policyengine_us/variables/gov/states/sc/tax/income/sc_taxable_income_less_salt.py
+++ policyengine_us/variables/gov/states/sc/tax/income/sc_taxable_income_less_salt.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class sc_taxable_income_less_salt(Variable):
     value_type = float
     entity = TaxUnit
-    label = "South Carolina federal taxable income exculde salt"
+    label = "South Carolina taxable income exculde salt"
     unit = USD
     definition_period = YEAR
 
