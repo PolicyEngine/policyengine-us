dc0305971cbe5d8a158e30601d24db09288ce80e
hua7450 <113824411+hua7450@users.noreply.github.com>
2023-11-11 00:53:52 +0000

    add a variable that exculde salt deductions Fixes #3068

--- policyengine_us/variables/gov/states/sc/tax/income/sc_taxable_income.py
+++ policyengine_us/variables/gov/states/sc/tax/income/sc_taxable_income.py
@@ -9,5 +9,5 @@ class sc_taxable_income(Variable):
     unit = USD
     definition_period = YEAR
 
-    adds = ["taxable_income", "sc_additions"]
+    adds = ["sc_taxable_income_less_salt", "sc_additions"]
     subtracts = ["sc_subtractions"]
---
+++ policyengine_us/variables/gov/states/sc/tax/income/sc_taxable_income_less_salt.py
@@ -0,0 +1,14 @@
+from policyengine_us.model_api import *
+
+
+class sc_taxable_income_less_salt(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "South Carolina federal taxable income exculde salt"
+    unit = USD
+    definition_period = YEAR
+
+    def formula(tax_unit, period, parameters):
+        agi = tax_unit("adjusted_gross_income", period)
+        deductions = tax_unit("itemized_deductions_less_salt", period)
+        return max_(0, agi - deductions)
