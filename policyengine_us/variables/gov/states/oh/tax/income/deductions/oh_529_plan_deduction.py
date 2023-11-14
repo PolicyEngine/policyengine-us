fad39ce57f27f9cfd54530e7e008685412342a74
Max Ghenis <mghenis@gmail.com>
2023-11-14 15:34:09 -0500

    Update policyengine_us/variables/gov/states/oh/tax/income/deductions/oh_529_plan_deduction.py

--- policyengine_us/variables/gov/states/oh/tax/income/deductions/oh_529_plan_deduction.py
+++ policyengine_us/variables/gov/states/oh/tax/income/deductions/oh_529_plan_deduction.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class oh_529_plan_deduction(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Ohio contributions to 529 plan deduction"
+    label = "Ohio deduction for contributions to 529 plans"
     unit = USD
     definition_period = YEAR
     reference = (
