291bcfd496f45389ab08f82110b97d3a8ce7ab5d
hua7450 <113824411+hua7450@users.noreply.github.com>
2023-11-14 14:39:08 +0000

    minor change

--- policyengine_us/variables/gov/states/mt/tax/income/deductions/itemized/mt_misc_deduction.py
+++ policyengine_us/variables/gov/states/mt/tax/income/deductions/itemized/mt_misc_deduction.py
@@ -10,7 +10,7 @@ class mt_misc_deduction(Variable):
     reference = (
         "https://mtrevenue.gov/wp-content/uploads/dlm_uploads/2023/05/Montana-Idividiual-Income-Tax-Return-Form-2-2022v6.2.pdf#page=7"
         "https://leg.mt.gov/bills/mca/title_0150/chapter_0300/part_0210/section_0310/0150-0300-0210-0310.html"
-        # Montana Code Annotated 2021 15-30-2131 
+        # Montana Code Annotated 2021 15-30-2131 (1)(a),(c)
     )
     defined_for = StateCode.MT
 
