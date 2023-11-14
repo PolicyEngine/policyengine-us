9523f3db3058aa75f43aa79dafab8bde1672e305
Ruby Ren <38923159+YichunRen@users.noreply.github.com>
2023-11-14 02:53:23 +0000

    draft

--- policyengine_us/variables/gov/states/hi/tax/income/hi_state_addback.py
+++ policyengine_us/variables/gov/states/hi/tax/income/hi_state_addback.py
@@ -9,9 +9,17 @@ class hi_state_addback(Variable):
     definition_period = YEAR
     reference = (
         "https://files.hawaii.gov/tax/forms/2022/n11ins.pdf#page=11"
+        "https://files.hawaii.gov/tax/forms/2022/n11ins.pdf#page=31" # State Tax Refund Worksheet
     )
     defined_for = StateCode.HI
 
     def formula(tax_unit, period, parameters):
+        # check eligible?
+        # need go through Form N-11 – State Tax Refund Worksheet
+        # only part b of Hawaii Additions Worksheet?
+        # how to include values from 2021 n-11(the old one)
+        p = parameters(period).gov.irs.deductions
+        standard_deduction = tax_unit("standard_deduction", period)
+        filing_status = tax_unit("filing_status", period)
         
         return 
