18e84653492f1373bcf82e365992fd5dd45006b3
yy3187 <129118329+yy3187@users.noreply.github.com>
2023-11-14 15:54:11 +0000

    add medical_expense bool parameters

--- policyengine_us/variables/gov/states/la/tax/income/deductions/la_itemized_deductions.py
+++ policyengine_us/variables/gov/states/la/tax/income/deductions/la_itemized_deductions.py
@@ -17,9 +17,16 @@ class la_itemized_deductions(Variable):
         p = parameters(
             period
         ).gov.states.la.tax.income.deductions.itemized.medical
-        itemizes = tax_unit("tax_unit_itemizes", period)
-        medical_expenses = add(tax_unit, period, ["medical_expense"])
-        excess = max_(
-            medical_expenses - tax_unit("standard_deduction", period), 0
-        )
-        return itemizes * excess * p.exceedance
+        p_us = parameters(period).gov.irs.deductions
+        itm_deds = [
+            deduction
+            for deduction in p_us.itemized_deductions
+            if deduction not in ["salt_deduction"]
+        ]
+        us_itemizing = tax_unit("tax_unit_itemizes", period)
+        federal_itemized_deduction = (add(tax_unit, period, itm_deds) * us_itemizing)
+        federal_standard_deduction = tax_unit("standard_deduction", period)
+        less_itm_amount = max_(0, federal_itemized_deduction - federal_standard_deduction) #2021 
+        medical_expenses = (add(tax_unit, period, ["medical_expense"]) * us_itemizing)
+        less_itm_amount_medical = max_(medical_expenses - federal_standard_deduction, 0) * p.exceedance #2022
+        return where(p.medical_expense_exceedance_calculation,less_itm_amount_medical,less_itm_amount)
