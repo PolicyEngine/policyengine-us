31a7ed06a1bba32df68f036b8768feccb3ba4f52
hua7450 <113824411+hua7450@users.noreply.github.com>
2023-11-14 14:42:12 +0000

    make format

--- policyengine_us/variables/gov/states/mt/tax/income/deductions/itemized/mt_itemized_deductions.py
+++ policyengine_us/variables/gov/states/mt/tax/income/deductions/itemized/mt_itemized_deductions.py
@@ -9,7 +9,7 @@ class mt_itemized_deductions(Variable):
     definition_period = YEAR
     reference = (
         "https://mtrevenue.gov/wp-content/uploads/dlm_uploads/2023/05/Montana-Idividiual-Income-Tax-Return-Form-2-2022v6.2.pdf#page=7"
-        "https://leg.mt.gov/bills/mca/title_0150/chapter_0300/part_0210/section_0310/0150-0300-0210-0310.html" 
+        "https://leg.mt.gov/bills/mca/title_0150/chapter_0300/part_0210/section_0310/0150-0300-0210-0310.html"
         # Montana Code Annotated 2021 15-30-2131 (1)(a),(c)
     )
     defined_for = StateCode.MT
