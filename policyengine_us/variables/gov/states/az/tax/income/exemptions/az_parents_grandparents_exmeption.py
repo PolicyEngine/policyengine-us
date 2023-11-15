b2dc2a96e1e077dc0a9f65c2f26d21d53df45fa3
JudyZ13 <112495414+JudyZ13@users.noreply.github.com>
2023-11-15 23:06:23 +0000

    created share_of_care_and_support_costs_paid_by_tax_filer

--- policyengine_us/variables/gov/states/az/tax/income/exemptions/az_parents_grandparents_exmeption.py
+++ policyengine_us/variables/gov/states/az/tax/income/exemptions/az_parents_grandparents_exmeption.py
@@ -10,30 +10,24 @@ class az_parents_grandparents_exemption(Variable):
     defined_for = StateCode.AZ
 
     def formula(tax_unit, period, parameters):
-        p = parameters(period).gov.states.az.tax.income.exemptions
+        p = parameters(
+            period
+        ).gov.states.az.tax.income.exemptions.parent_grandparent
         person = tax_unit.members
-        # The exemption is provided for parents and grandparents who receive over a percentage of their care and support
-        # payments from the filer.
-        care_and_support_payment = person("care_and_support_payment", period)
-        care_and_support_costs = person("care_and_support_costs", period)
-        support_payment_ratio = np.zeros_like(care_and_support_costs)
-        mask = care_and_support_costs != 0
-        support_payment_ratio[mask] = (
-            care_and_support_payment[mask] / care_and_support_costs[mask]
-        )
-        payment_eligible = (
-            support_payment_ratio > p.parent_grandparent.cost_rate
-        )
-        # Eligible parents of ancestors of parents have to be at or over a certain age threshold
-        # as well as cohabiting with the filer
+
+        parent = person("is_parent_of_filer_or_spouse", period)
+        grandparent = person("is_grandparent_of_filer_or_spouse", period)
+
         age = person("age", period)
-        age_eligible = age >= p.parent_grandparent.min_age
+        age_eligible = age >= p.min_age
 
-        eligible_parent = age_eligible & payment_eligible
-
-        eligible_grandparent = age_eligible & payment_eligible
-
-        total_exemption_count = eligible_parent + eligible_grandparent
-        return p.parent_grandparent.amount * tax_unit.sum(
-            total_exemption_count
+        ratio = person(
+            "share_of_care_and_support_costs_paid_by_tax_filer", period
         )
+        ratio_eligible = ratio >= p.cost_rate
+
+        total_exemption_count = (
+            (parent | grandparent) & age_eligible & ratio_eligible
+        )
+
+        return p.amount * tax_unit.sum(total_exemption_count)
