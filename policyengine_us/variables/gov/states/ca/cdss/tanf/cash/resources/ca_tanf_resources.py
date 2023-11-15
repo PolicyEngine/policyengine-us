9a1367d45c57206118dd032fd6f355829c919549
Qiukai Chen <chiukai.c1997@gmail.com>
2023-11-15 00:08:34 -0500

    fix bugs

--- policyengine_us/variables/gov/states/ca/cdss/tanf/cash/resources/ca_tanf_resources.py
+++ policyengine_us/variables/gov/states/ca/cdss/tanf/cash/resources/ca_tanf_resources.py
@@ -10,4 +10,4 @@ class ca_tanf_resources(Variable):
     defined_for = StateCode.CA
     reference = "http://epolicy.dpss.lacounty.gov/epolicy/epolicy/server/general/projects_responsive/ePolicyMaster/index.htm?&area=general&type=responsivehelp&ctxid=&project=ePolicyMaster#t=mergedProjects%2FCalWORKs%2FCalWORKs%2F42-200_Property%2F42-200_Property.htm%23Policybc-2&rhtocid=_3_1_2_0_1"
 
-    adds = "gov.states.ca.cdss.tanf.resources.sources"
+    adds = "gov.states.ca.cdss.tanf.cash.resources.sources"
