372fc0a4ba0c8e706aa683b9e7868e5002256105
JudyZ13 <112495414+JudyZ13@users.noreply.github.com>
2023-11-16 09:44:39 -0600

    Update policyengine_us/variables/household/demographic/person/share_of_care_and_support_costs_paid_by_tax_filer.py

--- policyengine_us/variables/household/demographic/person/share_of_care_and_support_costs_paid_by_tax_filer.py
+++ policyengine_us/variables/household/demographic/person/share_of_care_and_support_costs_paid_by_tax_filer.py
@@ -5,4 +5,4 @@ class share_of_care_and_support_costs_paid_by_tax_filer(Variable):
     value_type = float
     entity = Person
     definition_period = YEAR
-    label = "The percentage of care and support costs of a senior paid by tax filer"
+    label = "The percentage of care and support costs of a senior paid by the tax filer"
