bbb83644f3c6755689bf4c209a8ee16d719d5ab6
Jingjing Wang <87988673+Jingj-WANG@users.noreply.github.com>
2023-11-16 20:19:57 +0000

    format,merge

--- policyengine_us/variables/household/demographic/person/care_and_support_payments_from_tax_filer.py
+++ policyengine_us/variables/household/demographic/person/care_and_support_payments_from_tax_filer.py
@@ -7,17 +7,3 @@ class care_and_support_payments_from_tax_filer(Variable):
     unit = USD
     definition_period = YEAR
     label = "Amount of payments made by the tax filer for this person's care and support"
-
-    def formula(tax_unit, period, parameters):
-        person = tax_unit.members
-        care_and_support_payment = person(
-            "care_and_support_payments_from_tax_filer", period
-        )
-        care_and_support_costs = person("care_and_support_costs", period)
-        support_payment_ratio = np.zeros_like(care_and_support_costs)
-        mask = care_and_support_costs != 0
-        support_payment_ratio[mask] = (
-            care_and_support_payment[mask] / care_and_support_costs[mask]
-        )
-
-        return support_payment_ratio
