5303e5124b1b8a3c8d2c795f5d559372b618ccb1
Jingjing Wang <87988673+Jingj-WANG@users.noreply.github.com>
2023-11-16 00:13:52 +0000

    rename, variables, test

--- policyengine_us/variables/gov/states/id/tax/income/subtractions/id_aged_or_disabled_deduction.py
+++ policyengine_us/variables/gov/states/id/tax/income/subtractions/id_aged_or_disabled_deduction.py
@@ -1,10 +1,10 @@
 from policyengine_us.model_api import *
 
 
-class id_aged_or_disabled_credit(Variable):
+class id_aged_or_disabled_deduction(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Idaho aged or disabled credit"
+    label = "Idaho aged or disabled deduction"
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.ID
@@ -13,11 +13,11 @@ class id_aged_or_disabled_credit(Variable):
         # The program includes individuals in the household, including those not in the filing unit.
         # We simplify by limiting to those in the filing unit.
         person = tax_unit.members
-        p = parameters(
-            period
-        ).gov.states.id.tax.income.credits.aged_or_disabled
         age = person("age", period)
         head_or_spouse = person("is_tax_unit_head_or_spouse", period)
+        p = parameters(
+            period
+        ).gov.states.id.tax.income.subtractions.aged_or_disabled
         # Aged-eligible individuals cannot be head or spouse.
         age_eligible = (age >= p.age_threshold) & ~head_or_spouse
         # Disabled-eligible individuals can include head and spouse.
@@ -29,10 +29,6 @@ class id_aged_or_disabled_credit(Variable):
         )
         payment_eligible = support_payment_ratio > p.support_fraction_threshold
         eligible_person = (age_eligible | disabled_eligible) & payment_eligible
-        total_eligible_people = sum(eligible_person)
+        total_eligible_people = tax_unit.sum(eligible_person)
         capped_eligible_people = min_(total_eligible_people, p.person_cap)
-        # The aged or disabled credit can only be claimed if the aged
-        # or disabled deduction is not claimed
-        aged_deduction = tax_unit("id_aged_or_disabled_deduction", period)
-        credit_amount = capped_eligible_people * p.amount
-        return where(aged_deduction == 0, credit_amount, 0)
+        return capped_eligible_people * p.amount
