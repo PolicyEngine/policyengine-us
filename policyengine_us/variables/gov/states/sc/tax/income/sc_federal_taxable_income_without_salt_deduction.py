61aeef0ed8219ac9a6bee0095416a649ccfb1734
hua7450 <113824411+hua7450@users.noreply.github.com>
2023-11-17 22:52:54 +0000

    make format

--- policyengine_us/variables/gov/states/sc/tax/income/sc_federal_taxable_income_without_salt_deduction.py
+++ policyengine_us/variables/gov/states/sc/tax/income/sc_federal_taxable_income_without_salt_deduction.py
@@ -8,14 +8,14 @@ class sc_federal_taxable_income_without_salt_deduction(Variable):
     unit = USD
     definition_period = YEAR
     reference = (
-        "https://dor.sc.gov/forms-site/Forms/SC1040_2022.pdf#page=2", # line 1 
+        "https://dor.sc.gov/forms-site/Forms/SC1040_2022.pdf#page=2",  # line 1
         "https://dor.sc.gov/forms-site/Forms/SC1040inst_2022.pdf#page=2",
         "https://www.scstatehouse.gov/code/t12c006.php",  # SECTION 12-6-1110
     )
+
     def formula(tax_unit, period, parameters):
         agi = tax_unit("adjusted_gross_income", period)
         deductions_less_salt = tax_unit(
             "itemized_deductions_less_salt", period
         )
         return max_(0, agi - deductions_less_salt)
-        
