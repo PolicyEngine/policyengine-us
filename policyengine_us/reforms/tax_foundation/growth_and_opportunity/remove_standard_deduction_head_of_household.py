b74f28a2eade86ec03f43c5cea527ee20cdab47f
Zixuan Ma <66671734+mzx-4real@users.noreply.github.com>
2023-11-18 20:58:38 +0800

    Suggested change Fixes #3162

--- policyengine_us/reforms/tax_foundation/growth_and_opportunity/remove_standard_deduction_head_of_household.py
+++ policyengine_us/reforms/tax_foundation/growth_and_opportunity/remove_standard_deduction_head_of_household.py
@@ -2,98 +2,48 @@ from policyengine_us.model_api import *
 
 
 def create_remove_standard_deduction_head_of_household() -> Reform:
-    class basic_standard_deduction(Variable):
-        value_type = float
+    class FilingStatus(Enum):
+        SINGLE = "Single"
+        JOINT = "Joint"
+        SEPARATE = "Separate"
+        HEAD_OF_HOUSEHOLD = "Head of household"
+        WIDOW = "Widow(er)"
+
+    class filing_status(Variable):
+        value_type = Enum
         entity = TaxUnit
-        label = "Basic Standard Deduction (Eliminating HoH)"
-        unit = USD
-        documentation = "The Basic standard deduction under the tax foundation growth and opportunity plan"
+        possible_values = FilingStatus
+        default_value = FilingStatus.SINGLE
         definition_period = YEAR
+        label = "Filing_status (Eliminating HoH)"
+        documentation = "Eliminating HoH under the tax foundation growth and opportunity plan"
 
         def formula(tax_unit, period, parameters):
-            std = parameters(period).gov.irs.deductions.standard
-            filing_status = tax_unit("filing_status", period)
-            statuses = filing_status.possible_values
-            separate_filer_itemizes = tax_unit(
-                "separate_filer_itemizes", period
-            )
-            claimed_as_dependent_elsewhere = tax_unit(
-                "head_is_dependent_elsewhere", period
-            )
-            standard_deduction = select(
-                [
-                    filing_status == statuses.SINGLE,
-                    filing_status == statuses.SEPARATE,
-                    filing_status == statuses.JOINT,
-                    filing_status == statuses.WIDOW,
-                    filing_status == statuses.HEAD_OF_HOUSEHOLD,
-                ],
-                [
-                    std.amount[filing_status],
-                    std.amount[filing_status],
-                    std.amount[filing_status],
-                    std.amount[filing_status],
-                    13850,  # this should be single amount, use fix amount as temp
-                ],
-            )
-
-            standard_deduction_if_dependent = min_(
-                standard_deduction,
-                max_(
-                    std.dependent.additional_earned_income
-                    + tax_unit("tax_unit_earned_income", period),
-                    std.dependent.amount,
-                ),
-            )
-
+            has_spouse = add(tax_unit, period, ["is_tax_unit_spouse"]) > 0
+            has_dependents = tax_unit("tax_unit_dependents", period) > 0
+            person = tax_unit.members
+            is_separated = tax_unit.any(person("is_separated", period))
+            is_widowed = tax_unit.any(person("is_widowed", period))
             return select(
                 [
-                    separate_filer_itemizes,
-                    claimed_as_dependent_elsewhere,
+                    has_dependents & ~has_spouse,
+                    has_spouse,
+                    is_separated,
+                    is_widowed,
                     True,
                 ],
                 [
-                    0,
-                    standard_deduction_if_dependent,
-                    standard_deduction,
+                    FilingStatus.SINGLE,
+                    FilingStatus.JOINT,
+                    FilingStatus.SEPARATE,
+                    FilingStatus.WIDOW,
+                    FilingStatus.SINGLE,
                 ],
             )
-
-    class additional_standard_deduction(Variable):
-        value_type = float
-        entity = TaxUnit
-        label = "Additional Standard Deduction (Eliminating HoH)"
-        unit = USD
-        documentation = "The additional standard deduction under the tax foundation growth and opportunity plan"
-        definition_period = YEAR
-
-        def formula(tax_unit, period, parameters):
-            std = parameters(period).gov.irs.deductions.standard
-            filing_status = tax_unit("filing_status", period)
-            statuses = filing_status.possible_values
-            aged_blind_count = tax_unit("aged_blind_count", period)
-            additional_standard_deduction_per_count = select(
-                [
-                    filing_status == statuses.SINGLE,
-                    filing_status == statuses.SEPARATE,
-                    filing_status == statuses.JOINT,
-                    filing_status == statuses.WIDOW,
-                    filing_status == statuses.HEAD_OF_HOUSEHOLD,
-                ],
-                [
-                    std.aged_or_blind.amount[filing_status],
-                    std.aged_or_blind.amount[filing_status],
-                    std.aged_or_blind.amount[filing_status],
-                    std.aged_or_blind.amount[filing_status],
-                    1850,  # this should be single amount, use fix amount as temp
-                ],
-            )
-            return aged_blind_count * additional_standard_deduction_per_count
 
     class reform(Reform):
         def apply(self):
-            self.update_variable(basic_standard_deduction)
-            self.update_variable(additional_standard_deduction)
+            self.update_variable(filing_status)
 
     return reform
 
