cd3baa0f0d1f03805154411f4a625628e0062388
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2023-11-19 21:33:00 +0000

    tuition subtraction files

---
+++ policyengine_us/variables/gov/states/mt/tax/income/subtractions/mt_tuition_subtraction.py
@@ -0,0 +1,19 @@
+from policyengine_us.model_api import *
+
+
+class mt_tuition_subtraction(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Montana tuition subtraction"
+    definition_period = YEAR
+    reference = "https://leg.mt.gov/bills/mca/title_0150/chapter_0300/part_0210/section_0200/0150-0300-0210-0200.html"
+    defined_for = "mt_tuition_subtraction_eligible"
+
+    def formula(tax_unit, period, parameters):
+        p = parameters(period).gov.states.mt.tax.income.subtractions.tuition
+        threshold = p.threshold
+        contributions = add(tax_unit, period, ["investment_in_529_plan"])
+        eligible = tax_unit("mt_tuition_subtraction_eligible", period)
+        eligible_contributions = contributions * eligible
+        taxable_income_reductions = min_(contributions, threshold)
+        return taxable_income_reductions
---
+++ policyengine_us/variables/gov/states/mt/tax/income/subtractions/mt_tuition_subtraction_eligible.py
@@ -0,0 +1,14 @@
+from policyengine_us.model_api import *
+
+
+class mt_tuition_subtraction_eligible(Variable):
+    value_type = bool
+    entity = TaxUnit
+    label = "Montana tuition subtraction eligible"
+    definition_period = YEAR
+    reference = "https://leg.mt.gov/bills/mca/title_0150/chapter_0300/part_0210/section_0200/0150-0300-0210-0200.html"
+    defined_for = StateCode.MT
+
+    def formula(tax_unit, period, parameters):
+        contributions = add(tax_unit, period, ["investment_in_529_plan"])
+        return contributions > 0
