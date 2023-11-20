ee240806dfb439dec60bd2f3a900fd431774de43
Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>
2023-11-20 18:30:00 +0100

    Update policyengine_us/variables/gov/states/ar/tax/income/ar_agi_person.py

--- policyengine_us/variables/gov/states/ar/tax/income/ar_agi_person.py
+++ policyengine_us/variables/gov/states/ar/tax/income/ar_agi_person.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class ar_agi_person(Variable):
     value_type = float
     entity = Person
-    label = "Arkansas person adjusted gross income"
+    label = "Arkansas adjusted gross income for each person"
     unit = USD
     definition_period = YEAR
     reference = "https://www.dfa.arkansas.gov/images/uploads/incomeTaxOffice/2022_AR1000F_and_AR1000NR_Instructions.pdf#page=22"
