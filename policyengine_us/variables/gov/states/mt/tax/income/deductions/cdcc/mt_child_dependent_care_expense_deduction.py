9b53a173a468d0941ae6d1943c61741615d81157
jiajiagong0123 <114700434+jiajiagong0123@users.noreply.github.com>
2023-11-21 05:58:17 +0000

    final adjustment made Montana Child and Dependent Care expense Deduction Fixes #3059

--- policyengine_us/variables/gov/states/mt/tax/income/deductions/cdcc/mt_child_dependent_care_expense_deduction.py
+++ policyengine_us/variables/gov/states/mt/tax/income/deductions/cdcc/mt_child_dependent_care_expense_deduction.py
@@ -7,17 +7,20 @@ class mt_child_dependent_care_expense_deduction(Variable):
     label = "Montana child dependent care expense deduction"
     unit = USD
     definition_period = YEAR
+    reference = "https://mtrevenue.gov/wp-content/uploads/dlm_uploads/2022/12/2441-M_2022.pdf#page=1"
     defined_for = StateCode.MT
 
     def formula(tax_unit, period, parameters):
         p = parameters(
             period
-        ).gov.states.mt.tax.income.deductions.standard.child_dependent_care_expense_deduction
+        ).gov.states.mt.tax.income.deductions.standard.child_dependent_care_expense
         agi = tax_unit("mt_agi", period)
         care_expenses = tax_unit("tax_unit_childcare_expenses", period)
-        reduced_expenses = max_(0, care_expenses - p.phase_out_rate.calc(agi))
+        reduction = p.reduction.calc(agi)
+        reduced_expenses = max_(0, care_expenses - reduction)
         eligible_children = tax_unit(
             "mt_child_dependent_care_expense_deduction_eligible_children",
             period,
         )
-        return min_(p.amount.calc(eligible_children), reduced_expenses)
+        max_deduction = p.amount.calc(eligible_children)
+        return min_(max_deduction, reduced_expenses)
--- policyengine_us/variables/gov/states/mt/tax/income/deductions/cdcc/mt_child_dependent_care_expense_deduction_eligible_children.py
+++ policyengine_us/variables/gov/states/mt/tax/income/deductions/cdcc/mt_child_dependent_care_expense_deduction_eligible_children.py
@@ -6,18 +6,19 @@ class mt_child_dependent_care_expense_deduction_eligible_children(Variable):
     entity = TaxUnit
     label = "Montana child dependent care expense deduction"
     definition_period = YEAR
+    reference = "https://mtrevenue.gov/wp-content/uploads/dlm_uploads/2022/12/2441-M_2022.pdf#page=1"
     defined_for = StateCode.MT
 
     def formula(tax_unit, period, parameters):
         p = parameters(
             period
-        ).gov.states.mt.tax.income.deductions.standard.child_dependent_care_expense_deduction
+        ).gov.states.mt.tax.income.deductions.standard.child_dependent_care_expense
         person = tax_unit.members
         age = person("age", period)
         dependent = person("is_tax_unit_dependent", period)
 
         qualifying_child = dependent & (
-            (age <= p.age_threshold) | person("is_disabled", period)
+            (age <= p.age_limit) | person("is_disabled", period)
         )
 
         return tax_unit.sum(qualifying_child)
