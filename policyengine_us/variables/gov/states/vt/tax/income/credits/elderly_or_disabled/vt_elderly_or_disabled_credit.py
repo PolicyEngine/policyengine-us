e42ed6968d2dfff07db50541d955d47300dc2602
Kingsley Yang <87620347+Shinuing@users.noreply.github.com>
2023-11-21 20:30:09 +0000

    resolve conversations

--- policyengine_us/variables/gov/states/vt/tax/income/credits/elderly_or_disabled/vt_elderly_or_disabled_credit.py
+++ policyengine_us/variables/gov/states/vt/tax/income/credits/elderly_or_disabled/vt_elderly_or_disabled_credit.py
@@ -1,7 +1,7 @@
 from policyengine_us.model_api import *
 
 
-class vt_elderly_or_disabled_credit_exclusion(Variable):
+class vt_elderly_or_disabled_credit(Variable):
     value_type = float
     entity = TaxUnit
     definition_period = YEAR
@@ -11,10 +11,10 @@ class vt_elderly_or_disabled_credit_exclusion(Variable):
     reference = (
         "https://tax.vermont.gov/individuals/personal-income-tax/tax-credits"
     )
-    defined_for = "vt_elderly_or_disabled_credit_exclusion_eligible"
+    defined_for = StateCode.VT
 
     def formula(tax_unit, period, parameters):
         p = parameters(
             period
-        ).gov.states.vt.tax.income.agi.exclusions.elderly_or_disabled
+        ).gov.states.vt.tax.income.credits.elderly_or_disabled
         return p.match * tax_unit("elderly_disabled_credit", period)
