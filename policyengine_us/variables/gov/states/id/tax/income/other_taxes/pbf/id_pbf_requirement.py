d3f88cb03744b44faae6fa035e7a7ceb748c7e69
Yihan Wang <91220978+mylittlebecca@users.noreply.github.com>
2023-11-22 19:33:05 +0000

    Add Idaho Permanent Building Fund Tax Fixes #3179

--- policyengine_us/variables/gov/states/id/tax/income/other_taxes/pbf/id_pbf_requirement.py
+++ policyengine_us/variables/gov/states/id/tax/income/other_taxes/pbf/id_pbf_requirement.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class id_pbf_requirement(Variable):
     value_type = bool
     entity = TaxUnit
-    label = "Idaho PBF tax exemption credit"
+    label = "Required to pay the Idaho permanent building fund tax"
     definition_period = YEAR
     reference = "https://tax.idaho.gov/wp-content/uploads/forms/EIN00046/EIN00046_11-15-2021.pdf#page=10"
     defined_for = StateCode.ID
@@ -16,25 +16,29 @@ class id_pbf_requirement(Variable):
         # eligible if income less than filing status specified amount
         income = tax_unit("adjusted_gross_income", period)
         filing_status = tax_unit("filing_status", period)
-        # joint_status = filing_status.possible_values.JOINT
-        # widow_status = filing_status.possible_values.WIDOW
-        # dependent_count = tax_unit("tax_unit_dependents", period)
 
         elder_head = tax_unit("age_head", period) >= p.age_threshold
         elder_spouse = tax_unit("age_spouse", period) >= p.age_threshold
+
+        # income check for three age conditions
+        income_ineligible_older_two_aged = (
+            income < p.income_threshold.older.two_aged[filing_status]
+        )
+        income_ineligible_older_one_aged = (
+            income < p.income_threshold.older.one_aged[filing_status]
+        )
+        income_ineligible_younger = (
+            income < p.income_threshold.younger[filing_status]
+        )
+
         income_ineligible_check = where(
             elder_head | elder_spouse,
-            income
-            < p.income_threshold.older.threshold_partial_65_and_above[
-                filing_status
-            ],
-            income
-            < p.income_threshold.younger.threshold_under_65[filing_status],
+            income_ineligible_older_one_aged,
+            income_ineligible_younger,
         )
         income_ineligible = where(
             elder_head & elder_spouse,
-            income
-            < p.income_threshold.older.threshold_65_and_above[filing_status],
+            income_ineligible_older_two_aged,
             income_ineligible_check,
         )
 
