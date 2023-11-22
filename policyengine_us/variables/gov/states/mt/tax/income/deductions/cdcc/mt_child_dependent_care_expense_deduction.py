8058059d61c1d72296c70063eb8c9e197c53bddb
jiajiagong0123 <114700434+jiajiagong0123@users.noreply.github.com>
2023-11-22 01:21:30 +0000

    revise the final adjustment

--- policyengine_us/variables/gov/states/mt/tax/income/deductions/cdcc/mt_child_dependent_care_expense_deduction.py
+++ policyengine_us/variables/gov/states/mt/tax/income/deductions/cdcc/mt_child_dependent_care_expense_deduction.py
@@ -11,17 +11,16 @@ class mt_child_dependent_care_expense_deduction(Variable):
     defined_for = StateCode.MT
 
     def formula(tax_unit, period, parameters):
-        p = parameters(
-            period
-        ).gov.states.mt.tax.income.deductions.child_dependent_care_expense
         eligible_children = tax_unit(
             "mt_child_dependent_care_expense_deduction_eligible_children",
             period,
-        )
+        ) # Line 1
+        p = parameters(
+            period
+        ).gov.states.mt.tax.income.deductions.child_dependent_care_expense
+        cap = p.amount.calc(eligible_children)
         care_expenses = tax_unit("tax_unit_childcare_expenses", period)
-        agi = tax_unit("mt_agi", period)
-        reduction = p.reduction.calc(agi)
-        reduced_expenses = max_(0, care_expenses - reduction)
-
-        max_deduction = p.amount.calc(eligible_children)
-        return min_(max_deduction, reduced_expenses)
+        capped_expenses = min_(care_expenses, cap) # Line 2
+        agi = tax_unit("mt_agi", period) # Line 3
+        reduction = p.reduction.calc(agi) # Line 6
+        return max_(0, care_expenses - reduction)
--- policyengine_us/variables/gov/states/mt/tax/income/deductions/cdcc/mt_child_dependent_care_expense_deduction_eligible_children.py
+++ policyengine_us/variables/gov/states/mt/tax/income/deductions/cdcc/mt_child_dependent_care_expense_deduction_eligible_children.py
@@ -17,8 +17,7 @@ class mt_child_dependent_care_expense_deduction_eligible_children(Variable):
         age = person("age", period)
         dependent = person("is_tax_unit_dependent", period)
 
-        qualifying_child = dependent & (
-            (age < p.age_limit) | person("is_disabled", period)
-        )
+        age_eligible = age < p.age_limit
+        eligible = dependent & (age_eligible | person("is_disabled", period))  
 
         return tax_unit.sum(qualifying_child)
