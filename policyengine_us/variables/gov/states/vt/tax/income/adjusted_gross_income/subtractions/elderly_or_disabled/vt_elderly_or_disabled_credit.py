fc62a02171ecf62ade1cdf91683f97e7ea34553d
Kingsley Yang <87620347+Shinuing@users.noreply.github.com>
2023-11-22 19:52:13 +0000

    add to subtractions

--- policyengine_us/variables/gov/states/vt/tax/income/adjusted_gross_income/subtractions/elderly_or_disabled/vt_elderly_or_disabled_credit.py
+++ policyengine_us/variables/gov/states/vt/tax/income/adjusted_gross_income/subtractions/elderly_or_disabled/vt_elderly_or_disabled_credit.py
@@ -5,16 +5,17 @@ class vt_elderly_or_disabled_credit(Variable):
     value_type = float
     entity = TaxUnit
     definition_period = YEAR
-    label = "Vermont Elderly or disabled credit"
+    label = "Vermont elderly or disabled credit"
     documentation = "Schedule R credit for the elderly and the disabled"
     unit = USD
     reference = (
         "https://tax.vermont.gov/individuals/personal-income-tax/tax-credits"
     )
     defined_for = StateCode.VT
+    # the Investment Tax Credit and Vermont Farm Income Averaging Credit also get computed but leave them out here
 
     def formula(tax_unit, period, parameters):
         p = parameters(
             period
-        ).gov.states.vt.tax.income.credits.elderly_or_disabled
+        ).gov.states.vt.tax.income.agi.exclusions.elderly_or_disabled
         return p.match * tax_unit("elderly_disabled_credit", period)
