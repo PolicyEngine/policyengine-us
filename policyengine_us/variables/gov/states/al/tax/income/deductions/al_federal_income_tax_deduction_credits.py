e24a410e15d896d67ae78c710bcc205e1ce89834
Masasa <73863365+ThapeloMasasa@users.noreply.github.com>
2023-11-23 21:17:54 +0000

    addressed requested changes

--- policyengine_us/variables/gov/states/al/tax/income/deductions/al_federal_income_tax_deduction_credits.py
+++ policyengine_us/variables/gov/states/al/tax/income/deductions/al_federal_income_tax_deduction_credits.py
@@ -1,13 +1,13 @@
 from policyengine_us.model_api import *
 
 
-class al_federal_tax_deduction_credits(Variable):
+class al_federal_income_tax_deduction_credits(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Alabama federal tax deduction credits"
+    label = "Alabama federal income tax deduction credits"
     unit = USD
     definition_period = YEAR
     reference = "https://www.revenue.alabama.gov/ultraviewer/viewer/basic_viewer/index.html?form=2023/01/22f40abk.pdf#page=20"
     defined_for = StateCode.AL
 
-    adds = "gov.states.al.tax.income.deductions.credits"
+    adds = "gov.states.al.tax.income.deductions.federal_tax.credits"
