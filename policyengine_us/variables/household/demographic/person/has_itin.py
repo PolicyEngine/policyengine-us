861a1014efbcdc9e1f07e0aa31363eafd050aed1
martin.holmer@gmail.com <martin.holmer@gmail.com>
2023-11-23 10:45:59 -0500

    Add has_itin variable

---
+++ policyengine_us/variables/household/demographic/person/has_itin.py
@@ -0,0 +1,11 @@
+from policyengine_us.model_api import *
+
+
+class has_itin(Variable):
+    value_type = bool
+    entity = Person
+    label = "Has ITIN or SSN"
+    definition_period = YEAR
+
+    def formula(person, period, parameters):
+        return person("is_citizen", period)
