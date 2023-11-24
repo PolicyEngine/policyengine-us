6facbc62687d988c401cda5924c9ae75f0d9bd36
qingqiuzhang <90161322+qingqiuzhang@users.noreply.github.com>
2023-11-24 15:52:42 +0000

    make format

--- policyengine_us/variables/gov/states/mt/tax/income/additions/mt_other_additions.py
+++ policyengine_us/variables/gov/states/mt/tax/income/additions/mt_other_additions.py
@@ -7,6 +7,8 @@ class mt_other_additions(Variable):
     label = "Montana additions except addition to federal taxable social security benefits"
     unit = USD
     definition_period = YEAR
-    reference = "https://mtrevenue.gov/wp-content/uploads/dlm_uploads/2023/05/Montana-Idividiual-Income-Tax-Return-Form-2-2022v6.2.pdf#page=4",
+    reference = (
+        "https://mtrevenue.gov/wp-content/uploads/dlm_uploads/2023/05/Montana-Idividiual-Income-Tax-Return-Form-2-2022v6.2.pdf#page=4",
+    )
     defined_for = StateCode.MT
     adds = ["tax_exempt_dividend_income"]
