1d3c26a2ec2ab1a2a11291fc4faba2cdbca5f9e5
Matty <31402450+Mshuning@users.noreply.github.com>
2023-11-27 14:50:43 +0000

    unit test

--- policyengine_us/variables/gov/states/mi/tax/income/deductions/retirement/tier_one/mi_retirement_benefits_deduction_tier_one.py
+++ policyengine_us/variables/gov/states/mi/tax/income/deductions/retirement/tier_one/mi_retirement_benefits_deduction_tier_one.py
@@ -11,44 +11,55 @@ class mi_retirement_benefits_deduction_tier_one(Variable):
         "http://legislature.mi.gov/doc.aspx?mcl-206-30",  # (1)(f)
         "https://www.michigan.gov/taxes/-/media/Project/Websites/taxes/Forms/2022/2022-IIT-Forms/BOOK_MI-1040.pdf#page=17",
         "https://www.michigan.gov/taxes/iit/retirement-and-pension-benefits",
+        "https://www.michigan.gov/taxes/-/media/Project/Websites/taxes/Forms/2022/2022-IIT-Forms/4884.pdf#page=2",
     )
     defined_for = "mi_retirement_benefits_deduction_tier_one_eligible"
 
+    # Modeled after 2022 MICHIGAN Pension Schedule (Form 4884) Section A
     def formula(tax_unit, period, parameters):
         p = parameters(
             period
         ).gov.states.mi.tax.income.deductions.retirement_benefits.tier_one
         filing_status = tax_unit("filing_status", period)
 
+        private_cap = p.amount[filing_status]  # Line 9
         person = tax_unit.members
         # "Recipients born before 1946 may subtract all qualifying retirement and
         # pension benefits received from federal or Michigan public sources"
         # all public benefits can be deducted
-        uncapped_public_benefits = person(
-            "taxable_public_pension_income", period
-        )
-        # deductable private benefits are capped
-        uncapped_private_benefits = person(
-            "taxable_private_pension_income", period
-        )
-        # the cap is reduced by the amount of military retirement pay
-        military_retirement_pay = person("military_retirement_pay", period)
         is_head_or_spouse = person("is_tax_unit_head_or_spouse", period)
-        cap_reduction = tax_unit.sum(
-            military_retirement_pay * is_head_or_spouse
+        military_retirement_pay = (
+            person("military_retirement_pay", period) * is_head_or_spouse
+        )  # Line 10
+
+        total_military_retirement_pay = tax_unit.sum(military_retirement_pay)
+        # the cap is reduced by the amount of military retirement pay
+        reduced_private_cap = max_(
+            private_cap - total_military_retirement_pay, 0
+        )  # Line 11
+
+        public_benefits = (
+            person("taxable_public_pension_income", period) * is_head_or_spouse
         )
-        cap = max_(p.amount[filing_status] - cap_reduction, 0)
+        total_public_benefit = tax_unit.sum(public_benefits)  # Line 12
+
         # If your public retirement benefits are greater than the maximum amount,
         # you are not entitled to claim an additional subtraction for private pensions.
-        capped_public_benefits = max_(
-            cap - tax_unit.sum(uncapped_public_benefits * is_head_or_spouse), 0
-        )
-        capped_private_benefits = min_(
-            tax_unit.sum(uncapped_private_benefits * is_head_or_spouse),
-            capped_public_benefits,
-        )
+        reduced_private_cap_reduced_by_public_benefits = max_(
+            reduced_private_cap - total_public_benefit, 0
+        )  # Line 13
 
-        return (
-            tax_unit.sum(uncapped_public_benefits * is_head_or_spouse)
-            + capped_private_benefits
+        uncapped_private_benefits = (
+            person("taxable_private_pension_income", period)
+            * is_head_or_spouse
         )
+        total_uncapped_private_benefits = tax_unit.sum(
+            uncapped_private_benefits
+        )  # Line 14
+
+        capped_private_benefits = min_(
+            reduced_private_cap_reduced_by_public_benefits,
+            total_uncapped_private_benefits,
+        )  # Line 15
+
+        return capped_private_benefits + total_public_benefit  # Line 16
