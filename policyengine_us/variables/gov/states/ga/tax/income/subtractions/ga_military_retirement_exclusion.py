6dc35271cef214903f9f406f2a415ac8b9a5aa77
Diana Shi <dianashi0607@gmail.com>
2023-11-28 10:20:46 +0000

    Restructured

--- policyengine_us/variables/gov/states/ga/tax/income/subtractions/ga_military_retirement_exclusion.py
+++ policyengine_us/variables/gov/states/ga/tax/income/subtractions/ga_military_retirement_exclusion.py
@@ -8,27 +8,32 @@ class ga_military_retirement_exclusion(Variable):
     unit = USD
     definition_period = YEAR
     reference = (
-        "https://dor.georgia.gov/document/booklet/2021-it-511-individual-income-tax-booklet/download"  # Page 16
-        "https://dor.georgia.gov/document/document/2022-it-511-individual-income-tax-booklet/download"  # Page 16
+        "https://dor.georgia.gov/document/booklet/2021-it-511-individual-income-tax-booklet/download"
+        "https://dor.georgia.gov/document/document/2022-it-511-individual-income-tax-booklet/download"
         "https://advance.lexis.com/documentpage/?pdmfid=1000516&crid=fb5db531-a80f-4790-bddb-eefc8327ef60&config=00JAA1MDBlYzczZi1lYjFlLTQxMTgtYWE3OS02YTgyOGM2NWJlMDYKAFBvZENhdGFsb2feed0oM9qoQOMCSJFX5qkd&pddocfullpath=%2Fshared%2Fdocument%2Fstatutes-legislation%2Furn%3AcontentItem%3A65D2-CDH3-CGX8-044N-00008-00&pdcontentcomponentid=234186&pdteaserkey=sr1&pditab=allpods&ecomp=8s65kkk&earg=sr1&prid=66f02b0a-c5ae-4162-9535-127751546807"
     )
     defined_for = StateCode.GA
 
     def formula(tax_unit, period, parameters):
         person = tax_unit.members
-        p = parameters(period).gov.states.ga.tax.income.agi.exclusions.military_retirement
+        p = parameters(
+            period
+        ).gov.states.ga.tax.income.agi.exclusions.military_retirement
+        eligible_person = person(
+            "ga_military_retirement_exclusion_eligible_person", period
+        )
         earned_income = person("earned_income", period)
         additional_income_eligible = (
             earned_income > p.additional.threshold.earned_income
         )
         military_income = person("military_retirement_pay", period)
-        age = person("age", period)
-        base = p.max_amount.calc(age)
+        base = where(eligible_person, p.base_amount, 0)
         additional = where(
-            additional_income_eligible, p.additional.amount.calc(age), 0
+            eligible_person & additional_income_eligible,
+            p.additional.amount,
+            0,
         )
-        head_or_spouse = person("is_tax_unit_head_or_spouse", period)
-        uncapped_exclusion_amount = head_or_spouse * (base + additional)
+        uncapped_exclusion_amount = base + additional
         capped_exclusion_amount = min_(
             uncapped_exclusion_amount, military_income
         )
--- policyengine_us/variables/gov/states/ga/tax/income/subtractions/ga_retirement_exclusion.py
+++ policyengine_us/variables/gov/states/ga/tax/income/subtractions/ga_retirement_exclusion.py
@@ -19,17 +19,28 @@ class ga_retirement_exclusion(Variable):
         p = parameters(
             period
         ).gov.states.ga.tax.income.agi.exclusions.retirement
+        age_younger = (person("age", period) >= p.age.younger) & (
+            person("age", period) < p.age.older
+        )
+        age_older = person("age", period) >= p.age.older
+        cap_younger = p.cap.younger
+        cap_older = p.cap.older
         retirement_income = person(
-            "ga_retirement_income_exclusion_retirement_income(", period
+            "ga_retirement_income_exclusion_retirement_income", period
         )
-        age = person("age", period)
-        age_cap = p.max_amount.calc(age)
+        capped_younger_exclusion = min_(retirement_income, cap_younger)
+        capped_older_exclusion = min_(retirement_income, cap_older)
+        head = person("is_tax_unit_head", period)
+        spouse = person("is_tax_unit_spouse", period)
+        head_or_spuse = head | spouse
+        older_eligible = (head_or_spuse) & age_older
+        older_exclusion = where(older_eligible, capped_older_exclusion, 0)
+        younger_eligible = (head_or_spuse) & age_younger
+        # Disabled filers can qualify for the younger age cap amount if they are below the age threshold.
+        below_age_younger = person("age", period) < p.age.younger
         disabled = person("is_disabled", period)
-        # Disabled filers can qualify for the smaller amount ([1]) if they are below the age threshold.
-        disabled_cap = where(disabled, p.max_amount.amounts[1], 0)
-        cap = max_(age_cap, disabled_cap)
-        capped_exclusion = min_(retirement_income, cap)
-        exclusion = (
-            person("is_tax_unit_head_or_spouse", period) * capped_exclusion
+        disabled_eligible = (head_or_spuse) & below_age_younger & disabled
+        younger_exclusion = where(
+            younger_eligible | disabled_eligible, capped_younger_exclusion, 0
         )
-        return tax_unit.sum(exclusion)
+        return tax_unit.sum(older_exclusion + younger_exclusion)
