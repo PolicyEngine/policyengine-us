2f2be1e418a765166b19e500288279f5567d58f7
Jingjing Wang <87988673+Jingj-WANG@users.noreply.github.com>
2023-11-30 21:43:17 +0000

    add eligible_person.py, change all test files, change title: receive credit

--- policyengine_us/variables/gov/states/id/tax/income/id_receives_aged_or_disabled_credit.py
+++ policyengine_us/variables/gov/states/id/tax/income/id_receives_aged_or_disabled_credit.py
@@ -1,10 +1,12 @@
 from policyengine_us.model_api import *
 
 
-class id_selected_aged_or_disabled_credit(Variable):
+class id_receives_aged_or_disabled_credit(Variable):
     value_type = bool
     entity = TaxUnit
-    label = "Idaho selected aged or disabled credit"
+    label = (
+        "Filer receives the Idaho aged or disabled credit over the deduction"
+    )
     definition_period = YEAR
     defined_for = StateCode.ID
     default_value = True
--- policyengine_us/variables/gov/states/id/tax/income/subtractions/id_aged_or_disabled_deduction.py
+++ policyengine_us/variables/gov/states/id/tax/income/subtractions/id_aged_or_disabled_deduction.py
@@ -13,26 +13,16 @@ class id_aged_or_disabled_deduction(Variable):
         # The program includes individuals in the household, including those not in the filing unit.
         # We simplify by limiting to those in the filing unit.
         person = tax_unit.members
-        age = person("age", period)
-        head_or_spouse = person("is_tax_unit_head_or_spouse", period)
         p = parameters(
             period
         ).gov.states.id.tax.income.subtractions.aged_or_disabled
-        # Aged-eligible individuals cannot be head or spouse.
-        age_eligible = (age >= p.age_threshold) & ~head_or_spouse
-        # Disabled-eligible individuals can include head and spouse.
-        disabled_eligible = person("is_disabled", period)
-        # To claim aged or disabled credit, filers also have to maintain a household for family members
-        # and provide more than one-half of the family member’s support for the year
-        support_payment_ratio = person(
-            "share_of_care_and_support_costs_paid_by_tax_filer", period
+        eligible_person = person(
+            "id_aged_or_disabled_deduction_eligible_person", period
         )
-        payment_eligible = support_payment_ratio > p.support_fraction_threshold
-        eligible_person = (age_eligible | disabled_eligible) & payment_eligible
         total_eligible_people = tax_unit.sum(eligible_person)
         capped_eligible_people = min_(total_eligible_people, p.person_cap)
         deduction_amount = capped_eligible_people * p.amount
-        selected_credit = tax_unit(
-            "id_selected_aged_or_disabled_credit", period
+        receive_credit = tax_unit(
+            "id_receives_aged_or_disabled_credit", period
         )
-        return where(selected_credit, 0, deduction_amount)
+        return where(receive_credit, 0, deduction_amount)
