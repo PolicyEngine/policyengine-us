ee2c5bf3af5bf7e3b199d1fd2fdc9e051406e624
yy3187 <129118329+yy3187@users.noreply.github.com>
2023-11-30 08:20:43 +0000

    add comments to variable

--- policyengine_us/variables/gov/states/la/tax/income/exemptions/personal/la_blind_exemption.py
+++ policyengine_us/variables/gov/states/la/tax/income/exemptions/personal/la_blind_exemption.py
@@ -8,8 +8,12 @@ class la_blind_exemption(Variable):
     unit = USD
     definition_period = YEAR
     reference = (
-        "https://www.revenue.louisiana.gov/taxforms/6935(11_02)F.pdf#page=1"
+        "https://www.revenue.louisiana.gov/taxforms/6935(11_02)F.pdf#page=1",
+        "https://revenue.louisiana.gov/TaxForms/IT540iWEB(2022)D1.pdf#page=2"
     )
+    #Even though the tax computation worksheet refers "blind exemption" as credits, the instructions for
+    #preparing tax form line 6a-6b specifies it as exemption. (the legal code also mentions deaf and totally disabled conditions,
+    # but they are not mentioned either in the tax computation worksheet or tax form.)
     defined_for = StateCode.LA
 
     def formula(tax_unit, period, parameters):
