cd8992b3ca9c206d654c6ac245fec96cb3f035f1
qingqiuzhang <90161322+qingqiuzhang@users.noreply.github.com>
2023-12-01 00:14:05 +0000

    reference

--- policyengine_us/variables/gov/states/mt/tax/income/adjustments/social_security_benefit/mt_social_security_benefits_eligible.py
+++ policyengine_us/variables/gov/states/mt/tax/income/adjustments/social_security_benefit/mt_social_security_benefits_eligible.py
@@ -8,7 +8,7 @@ class mt_social_security_benefits_eligible(Variable):
     unit = USD
     definition_period = YEAR
     reference = (
-        "https://law.justia.com/codes/montana/2022/title-15/chapter-30/part-21/section-15-30-2110/",
+        "https://www.leg.mt.gov/bills/mca/title_0150/chapter_0300/part_0210/section_0010/0150-0300-0210-0010.html",
         "https://mtrevenue.gov/wp-content/uploads/mdocs/form%202%202021.pdf#page=6",
         "https://mtrevenue.gov/wp-content/uploads/dlm_uploads/2023/05/Montana-Idividiual-Income-Tax-Return-Form-2-2022v6.2.pdf#page=6",
     )
