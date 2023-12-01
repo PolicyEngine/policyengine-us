2cf03da2ac6a4e7f6cd6da3cc007793a8ac3b82b
jnaux <89711387+jnaux@users.noreply.github.com>
2023-12-01 12:05:34 +0000

    Added in prorate_fraction

---
+++ policyengine_us/variables/gov/states/va/tax/income/va_agi_individual.py
@@ -0,0 +1,13 @@
+from policyengine_us.model_api import *
+
+
+class va_agi_individual(Variable):
+    value_type = float
+    entity = Person
+    label = "Virginia Adjusted Gross Income (VAGI) for each individual"
+    unit = USD
+    definition_period = YEAR
+    reference = (
+        "https://law.lis.virginia.gov/vacodefull/title58.1/chapter3/article2/"
+    )
+    defined_for = StateCode.VA
