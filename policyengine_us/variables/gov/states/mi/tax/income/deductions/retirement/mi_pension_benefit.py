8217e6e988bb827aa27ac3e783a34121259891cc
Matty <31402450+Mshuning@users.noreply.github.com>
2023-12-01 08:45:53 +0000

    subtractions

--- policyengine_us/variables/gov/states/mi/tax/income/deductions/retirement/mi_pension_benefit.py
+++ policyengine_us/variables/gov/states/mi/tax/income/deductions/retirement/mi_pension_benefit.py
@@ -1,25 +1,23 @@
 from policyengine_us.model_api import *
 
 
-class mi_standard_deduction_and_pension_benefit(Variable):
+class mi_pension_benefit(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Michigan standard deduction and pension benefit"
+    label = "Michigan pension benefit"
     unit = USD
     definition_period = YEAR
-    documentation = "Michigan standard deduction, consist of standard deduction, retirement and pension benefits, and interest, dividends, and capital gains deduction of qualifying age."
+    documentation = (
+        "Michigan retirement and pension benefits of qualifying age."
+    )
     reference = (
         "http://legislature.mi.gov/doc.aspx?mcl-206-30",
-        "https://www.michigan.gov/taxes/iit/retirement-and-pension-benefits/michigan-standard-deduction",
         "https://www.michigan.gov/taxes/-/media/Project/Websites/taxes/Forms/2022/2022-IIT-Forms/BOOK_MI-1040.pdf#page=18",
         "https://www.michigan.gov/taxes/iit/retirement-and-pension-benefits",
     )
     defined_for = StateCode.MI
 
     adds = [
-        "mi_standard_deduction_tier_two",
-        "mi_standard_deduction_tier_three",
         "mi_retirement_benefits_deduction_tier_one",
         "mi_retirement_benefits_deduction_tier_three",
-        "mi_interest_dividends_capital_gains_deduction",
     ]
--- policyengine_us/variables/gov/states/mi/tax/income/mi_subtractions.py
+++ policyengine_us/variables/gov/states/mi/tax/income/mi_subtractions.py
@@ -7,6 +7,11 @@ class mi_subtractions(Variable):
     label = "Michigan subtractions"
     unit = USD
     definition_period = YEAR
+    reference = (
+        "https://www.michigan.gov/taxes/-/media/Project/Websites/taxes/Forms/2022/2022-IIT-Forms/Schedule-1.pdf", 
+        "https://www.michigan.gov/taxes/-/media/Project/Websites/taxes/Forms/2022/2022-IIT-Forms/BOOK_MI-1040.pdf", 
+        "https://www.michigan.gov/-/media/Project/Websites/taxes/2022RM/UNCAT/2019_Taxpayer_Assistance_Manual.pdf", 
+    )
     defined_for = StateCode.MI
 
     adds = "gov.states.mi.tax.income.subtractions"
