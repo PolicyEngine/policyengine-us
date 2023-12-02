2bb5455c17f3303e695fa9fc1cb4c8273f44bb72
jnaux <89711387+jnaux@users.noreply.github.com>
2023-12-02 09:12:30 +0000

    Minor edits

--- policyengine_us/variables/gov/states/va/tax/income/va_agi_individual.py
+++ policyengine_us/variables/gov/states/va/tax/income/va_agi_individual.py
@@ -7,6 +7,6 @@ class va_agi_individual(Variable):
     label = "Virginia Adjusted Gross Income (VAGI)"
     unit = USD
     definition_period = YEAR
-    # Virginia defines a person level AGI for the spouse tax adjustment calculation.
+    # Virginia includes an individual level definition for AGI
     reference = "https://www.tax.virginia.gov/laws-rules-decisions/rulings-tax-commissioner/13-5"
     defined_for = StateCode.VA
