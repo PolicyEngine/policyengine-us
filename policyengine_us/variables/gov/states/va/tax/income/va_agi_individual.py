8a6a7e0c827562a77ec7c8cea2a1b8df06f554e1
jnaux <89711387+jnaux@users.noreply.github.com>
2023-12-02 04:43:14 +0000

    Changed var name for person level va_agi

--- policyengine_us/variables/gov/states/va/tax/income/va_agi_individual.py
+++ policyengine_us/variables/gov/states/va/tax/income/va_agi_individual.py
@@ -1,11 +1,12 @@
 from policyengine_us.model_api import *
 
 
-class va_agi(Variable):
+class va_agi_individual(Variable):
     value_type = float
     entity = Person
     label = "Virginia Adjusted Gross Income (VAGI)"
     unit = USD
     definition_period = YEAR
+    # Virginia defines a person level AGI for the spouse tax adjustment calculation.
     reference = "https://www.tax.virginia.gov/laws-rules-decisions/rulings-tax-commissioner/13-5"
     defined_for = StateCode.VA
