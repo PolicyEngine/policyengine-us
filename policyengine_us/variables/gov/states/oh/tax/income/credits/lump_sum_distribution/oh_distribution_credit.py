666795f284f50dfab908ba4a13bb09d737a8b993
Masasa <73863365+ThapeloMasasa@users.noreply.github.com>
2023-12-04 06:37:40 +0000

    Lump sum ditribution credit

---
+++ policyengine_us/variables/gov/states/oh/tax/income/credits/lump_sum_distribution/oh_distribution_credit.py
@@ -0,0 +1,23 @@
+from policyengine_us.model_api import *
+
+
+class oh_distribution_credit(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Ohio lump sum distribution credit"
+    unit = USD
+    definition_period = YEAR
+    reference = (
+        "https://tax.ohio.gov/static/forms/ohio_individual/individual/2021/pit-it1040-booklet.pdf#page=29",
+    )
+    defined_for = StateCode.OH
+
+    def formula(tax_unit, period, parameters):
+        person = tax_unit.members
+        p = parameters(period).gov.states.oh.tax.income.credits
+        age = person("age", period)
+        amount = p.lump_sum_distribution.amount
+        age_threshold = p.senior_citizen.age_threshold
+        rate = p.lump_sum_distribution.rate.calc(age)
+        result = where(age >= age_threshold, amount * rate, 0)
+        return result
