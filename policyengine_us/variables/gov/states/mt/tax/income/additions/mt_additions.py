853fed314c078310ea0efa75b6bff2c8af83d2fe
hua7450 <113824411+hua7450@users.noreply.github.com>
2023-12-05 23:26:25 +0000

    minor fix Fixes #3307

--- policyengine_us/variables/gov/states/mt/tax/income/additions/mt_additions.py
+++ policyengine_us/variables/gov/states/mt/tax/income/additions/mt_additions.py
@@ -1,12 +1,11 @@
 from policyengine_us.model_api import *
 
 
-class mt_refundable_credits(Variable):
+class mt_additions(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Montana refundable credits"
+    label = "Montana additions"
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.MT
-
-    adds = "gov.states.mt.tax.income.credits.refundable"
+    
