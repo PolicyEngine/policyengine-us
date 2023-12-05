fca6af8ef1b211ced6040499f593d449778e329d
JudyZ13 <112495414+JudyZ13@users.noreply.github.com>
2023-12-05 03:46:13 +0000

    updates

--- policyengine_us/variables/gov/states/az/tax/income/exemptions/az_parents_grandparents_exmeption.py
+++ policyengine_us/variables/gov/states/az/tax/income/exemptions/az_parents_grandparents_exmeption.py
@@ -3,17 +3,17 @@ from policyengine_us.model_api import *
 
 class az_parents_grandparents_exemption(Variable):
     value_type = float
-    entity = TaxUnit
+    entity = Person
     label = "Arizona parents and grandparents exemption"
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.AZ
 
-    def formula(tax_unit, period, parameters):
+    def formula(person, period, parameters):
         p = parameters(
             period
         ).gov.states.az.tax.income.exemptions.parent_grandparent
-        person = tax_unit.members
+        tax_unit = person.tax_unit
 
         parent = person("is_parent_of_filer_or_spouse", period)
         grandparent = person("is_grandparent_of_filer_or_spouse", period)
@@ -26,8 +26,6 @@ class az_parents_grandparents_exemption(Variable):
         )
         ratio_eligible = ratio >= p.cost_rate
 
-        total_exemption_count = (
-            (parent | grandparent) & age_eligible & ratio_eligible
-        )
+        eligible = (parent | grandparent) & age_eligible & ratio_eligible
 
-        return p.amount * tax_unit.sum(total_exemption_count)
+        return p.amount * tax_unit.sum(eligible)
