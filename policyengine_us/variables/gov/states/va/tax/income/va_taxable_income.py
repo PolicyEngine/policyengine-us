e905815653c6bbc9692122ffbdea4bff8e076a4e
Xinyi-Shao <ellashao99@gmail.com>
2023-12-05 13:22:59 -0500

    updated

--- policyengine_us/variables/gov/states/va/tax/income/va_taxable_income.py
+++ policyengine_us/variables/gov/states/va/tax/income/va_taxable_income.py
@@ -9,11 +9,16 @@ class va_taxable_income(Variable):
     definition_period = YEAR
     reference = (
         "https://law.lis.virginia.gov/vacodefull/title58.1/chapter3/article2/"
+        "https://www.tax.virginia.gov/sites/default/files/taxforms/individual-income-tax/2022/760-2022.pdf#page=1"
     )
     defined_for = StateCode.VA
 
     def formula(tax_unit, period, parameters):
-        agi = tax_unit("va_afagi", period)
-        deductions = tax_unit("va_standard_deduction", period)
+        afagi = tax_unit("va_afagi", period)
+        age_ded = tax_unit("va_age_deduction", period)
+        std_ded = tax_unit("va_standard_deduction", period)
+        itm_ded = tax_unit("va_itemized_deduction", period)
+        vagi = afagi - age_ded
+        ded = itm_ded if itm_ded > 0 else std_ded
         exemptions = tax_unit("va_total_exemptions", period)
-        return max_(agi - deductions - exemptions, 0)
+        return max_(vagi - ded - exemptions, 0)
