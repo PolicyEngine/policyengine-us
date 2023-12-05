8ba95087aaf2a1c8e0d886428c1f18d261a67405
Chiukai-C <127340408+Chiukai-C@users.noreply.github.com>
2023-12-05 21:03:39 +0000

    change child care age entity to person

--- policyengine_us/variables/gov/states/ca/cdss/tanf/child_care/reimbursement/ca_calworks_child_care_payment_standard.py
+++ policyengine_us/variables/gov/states/ca/cdss/tanf/child_care/reimbursement/ca_calworks_child_care_payment_standard.py
@@ -6,7 +6,7 @@ class ca_calworks_child_care_payment_standard(Variable):
     entity = Person
     label = "California CalWORKs Child Care Payment Standard"
     definition_period = YEAR
-    defined_for = "is_child"
+    defined_for = "ca_calworks_child_care_age_child_eligible"
     reference = "http://epolicy.dpss.lacounty.gov/epolicy/epolicy/server/general/projects_responsive/ePolicyMaster/index.htm?&area=general&type=responsivehelp&ctxid=&project=ePolicyMaster#t=mergedProjects%2FChild%20Care%2FChild_Care%2F1210_8_Regional_Market_Rate_Ceilings%2F1210_8_Regional_Market_Rate_Ceilings.htm%23Contactbc-13&rhtocid=_3_3_8_12"
 
     def formula(person, period, parameters):
