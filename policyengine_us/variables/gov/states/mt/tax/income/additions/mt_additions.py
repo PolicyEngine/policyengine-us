4914279df619a77db63953dbb41877e60290fd24
hua7450 <113824411+hua7450@users.noreply.github.com>
2023-12-06 23:12:00 +0000

    minor fix Fixes #3307

--- policyengine_us/variables/gov/states/mt/tax/income/additions/mt_additions.py
+++ policyengine_us/variables/gov/states/mt/tax/income/additions/mt_additions.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class mt_additions(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Montana additions"
+    label = "Montana additions to adjusted gross income"
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.MT
