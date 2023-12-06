5eb1a10ac0d8a243750c761c1772d09bf90285cd
Kingsley Yang <87620347+Shinuing@users.noreply.github.com>
2023-12-06 22:19:39 +0000

    add edge case for low income filers

--- policyengine_us/variables/gov/states/ny/tax/income/ny_supplemental_tax.py
+++ policyengine_us/variables/gov/states/ny/tax/income/ny_supplemental_tax.py
@@ -13,6 +13,7 @@ class ny_supplemental_tax(Variable):
     def formula(tax_unit, period, parameters):
         ny_taxable_income = tax_unit("ny_taxable_income", period)
         ny_agi = tax_unit("ny_agi", period)
+        ny_main_income_tax = tax_unit("ny_main_income_tax", period)
         tax = parameters(period).gov.states.ny.tax.income
         sup_tax = tax.supplemental
 
@@ -89,8 +90,54 @@ class ny_supplemental_tax(Variable):
                 sup_tax.incremental_benefit.widow.calc(ny_taxable_income),
             ],
         )
+        supplemental_tax_general = (
+            recapture_base + phase_in_fraction * incremental_benefit
+        )
 
-        return recapture_base + phase_in_fraction * incremental_benefit
+        # edge case for low income
+        min_taxable_income = select(
+            [
+                filing_status == status.SINGLE,
+                filing_status == status.SEPARATE,
+                filing_status == status.JOINT,
+                filing_status == status.HEAD_OF_HOUSEHOLD,
+                filing_status == status.WIDOW,
+            ],
+            [
+                sup_tax.incremental_benefit.single.thresholds[0],
+                sup_tax.incremental_benefit.separate.thresholds[0],
+                sup_tax.incremental_benefit.joint.thresholds[0],
+                sup_tax.incremental_benefit.head_of_household.thresholds[0],
+                sup_tax.incremental_benefit.widow.thresholds[0],
+            ],
+        )
+        low_income_rate = select(
+            in_each_status,
+            [scale.marginal_rates(min_taxable_income + 1) for scale in scales],
+        )
+        supplemental_tax_low_income = (
+            ny_taxable_income * low_income_rate
+            - phase_in_fraction * ny_main_income_tax
+        )
+
+        # edge case for high agi > $25,000,000 TODO: check length
+        high_income_rate = select(
+            in_each_status,
+            [scale.marginal_rates(sup_tax.max_agi) for scale in scales],
+        )
+
+        # supplemental_tax_high_income = (
+        #     ny_taxable_income * high_income_rate - ny_main_income_tax
+        # )
+
+        # supplemental_tax = select(
+        #     [ny_taxable_income < min_taxable_income, ny_agi > max_agi],
+        #     [supplemental_tax_low_income, supplemental_tax_high_income],
+        #     default=supplemental_tax_general,
+        # )
+
+        return sup_tax.max_agi
+
         # rate = select(
         #     in_each_status,
         #     [
