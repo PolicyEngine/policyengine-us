6a766c1c489757b23d42228959905b0dd64b39b9
Ruby Ren <38923159+YichunRen@users.noreply.github.com>
2023-12-07 19:51:12 +0000

    handle case only spouse is disabled

--- policyengine_us/variables/gov/states/hi/tax/income/exemptions/hi_disabled_exemptions.py
+++ policyengine_us/variables/gov/states/hi/tax/income/exemptions/hi_disabled_exemptions.py
@@ -27,19 +27,40 @@ class hi_disabled_exemptions(Variable):
         disabled_head = tax_unit("head_is_disabled", period)
         disabled_exemption_disabled_head = disabled_head * p.disabled
         disabled_exemption_non_disabled_head = p.base * (1 + aged_head)
-        disabled_exemption_head = max_(
-            disabled_exemption_disabled_head,
+        disabled_exemption_head = where(
+            disabled_head,
+            max_(
+                disabled_exemption_disabled_head,
+                disabled_exemption_non_disabled_head,
+            ),
             disabled_exemption_non_disabled_head,
         )
         # Same for spouse.
         disabled_spouse = tax_unit("spouse_is_disabled", period)
         disabled_exemption_disabled_spouse = disabled_spouse * p.disabled
         disabled_exemption_non_disabled_spouse = p.base * (1 + aged_spouse)
-        disabled_exemption_spouse = max_(
-            disabled_exemption_disabled_spouse,
+        disabled_exemption_spouse = where(
+            disabled_spouse,
+            max_(
+                disabled_exemption_disabled_spouse,
+                disabled_exemption_non_disabled_spouse,
+            ),
             disabled_exemption_non_disabled_spouse,
         )
 
-        return (
-            disabled_exemption_head + disabled_exemption_spouse
-        ) * disabled_head
+        # if filling status is not join, the disabled_exemption_spouse should be zero
+        # The taxpayer shall not take additional exemptions with regard to the taxpayer’s disability. 
+        filing_status = tax_unit("filing_status", period)
+        disabled_exemption_spouse = where(
+            filing_status == filing_status.possible_values.JOINT,
+            disabled_exemption_spouse,
+            0,
+        )
+        no_disabled_head_or_spouse = (
+            tax_unit.sum(disabled_head + disabled_spouse) == 0
+        )
+        return where(
+            no_disabled_head_or_spouse,
+            0,
+            disabled_exemption_head + disabled_exemption_spouse,
+        )
