3ccd7265f4030135c86ceec482bc950b94ca9139
PavelMakarchuk <pavel.ma99@gmail.com>
2023-12-07 13:13:37 +0100

    variable names

--- policyengine_us/variables/gov/states/mt/tax/income/deductions/cdcc/mt_child_dependent_care_expense_deduction.py
+++ policyengine_us/variables/gov/states/mt/tax/income/deductions/cdcc/mt_child_dependent_care_expense_deduction.py
@@ -18,9 +18,9 @@ class mt_child_dependent_care_expense_deduction(Variable):
         p = parameters(
             period
         ).gov.states.mt.tax.income.deductions.child_dependent_care_expense
-        cap = p.amount.calc(eligible_children)
+        cap = p.cap.calc(eligible_children)
         care_expenses = tax_unit("tax_unit_childcare_expenses", period)
         capped_expenses = min_(care_expenses, cap)  # Line 2
         agi = tax_unit("mt_agi", period)  # Line 3
         reduction = p.phase_out.calc(agi)  # Line 6
-        return max_(0, care_expenses - reduction)
+        return max_(0, capped_expenses - reduction)
