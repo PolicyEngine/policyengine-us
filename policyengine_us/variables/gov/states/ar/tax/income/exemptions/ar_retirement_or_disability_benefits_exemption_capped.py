a5985f52bca70e487155f14e8fe1e8178b7b3d65
Kevintowne <89811054+Kevintowne@users.noreply.github.com>
2023-12-07 05:00:22 +0000

    Fixes variables and tests

---
+++ policyengine_us/variables/gov/states/ar/tax/income/exemptions/ar_retirement_or_disability_benefits_exemption_capped.py
@@ -0,0 +1,28 @@
+from policyengine_us.model_api import *
+
+
+class ar_retirement_or_disability_benefits_exemption_capped(Variable):
+    value_type = float
+    entity = Person
+    label = "Arkansas capped retirement or disability benefits exemption"
+    unit = USD
+    definition_period = YEAR
+    reference = "https://www.dfa.arkansas.gov/images/uploads/incomeTaxOffice/2022_AR1000F_and_AR1000NR_Instructions.pdf#page=13"
+    defined_for = StateCode.AR
+
+    def formula(person, period, parameters):
+        p = parameters(
+            period
+        ).gov.states.ar.tax.income.exemptions.retirement_or_disability_benefits
+        eligible_pension_income = person(
+            "ar_retirement_or_disability_benefits_exemption_indv", period
+        )
+        military_retirement_exemption = person(
+            "ar_military_retirement_exemption", period
+        )
+        capped_exemption = (
+            eligible_pension_income > military_retirement_exemption,
+            eligible_pension_income,
+            military_retirement_exemption,
+        )
+        return min_(capped_exemption, p.cap)
--- policyengine_us/variables/gov/states/ar/tax/income/exemptions/ar_retirement_or_disability_benefits_exemption_indv.py
+++ policyengine_us/variables/gov/states/ar/tax/income/exemptions/ar_retirement_or_disability_benefits_exemption_indv.py
@@ -15,22 +15,14 @@ class ar_retirement_or_disability_benefits_exemption_indv(Variable):
             period
         ).gov.states.ar.tax.income.exemptions.retirement_or_disability_benefits
         head_or_spouse = person("is_tax_unit_head_or_spouse", period)
-        eligible_person = person(
-            "ar_retirement_or_disability_benefits_exemption_eligible_person",
-            period,
-        )
         pension_income = (
             person("taxable_pension_income", period) * head_or_spouse
         )
         # Filers over a certain age can deduct IRA distributions in addition to pension income
-        ira_distributions = (
+        eligible_person = head_or_spouse & (
+            person("age", period) >= p.age_threshold
+        )
+        eligible_ira_distributions = (
             person("regular_ira_distributions", period) * eligible_person
         )
-        eligible_pension_income = pension_income + ira_distributions
-        military_retirement_exemption = person(
-            "ar_military_retirement_exemption", period
-        )
-        capped_exemption = max_(
-            eligible_pension_income, military_retirement_exemption
-        )
-        return min_(capped_exemption, p.cap)
+        return pension_income + eligible_ira_distributions
