db0f55210f416884d5564ac21bd26d14b14d7ff9
Diana Shi <dianashi0607@gmail.com>
2023-12-07 03:50:50 +0000

    Revised

--- policyengine_us/variables/gov/states/ga/tax/income/subtractions/ga_retirement_exclusion_person.py
+++ policyengine_us/variables/gov/states/ga/tax/income/subtractions/ga_retirement_exclusion_person.py
@@ -1,9 +1,9 @@
 from policyengine_us.model_api import *
 
 
-class ga_retirement_exclusion(Variable):
+class ga_retirement_exclusion_person(Variable):
     value_type = float
-    entity = TaxUnit
+    entity = Person
     label = "Georgia retirement exclusion"
     unit = USD
     definition_period = YEAR
@@ -12,4 +12,16 @@ class ga_retirement_exclusion(Variable):
         "https://dor.georgia.gov/document/document/2022-it-511-individual-income-tax-booklet/download",  # Page 15
         "https://advance.lexis.com/documentpage/?pdmfid=1000516&crid=fb5db531-a80f-4790-bddb-eefc8327ef60&config=00JAA1MDBlYzczZi1lYjFlLTQxMTgtYWE3OS02YTgyOGM2NWJlMDYKAFBvZENhdGFsb2feed0oM9qoQOMCSJFX5qkd&pddocfullpath=%2Fshared%2Fdocument%2Fstatutes-legislation%2Furn%3AcontentItem%3A65D2-CDH3-CGX8-044N-00008-00&pdcontentcomponentid=234186&pdteaserkey=sr1&pditab=allpods&ecomp=8s65kkk&earg=sr1&prid=66f02b0a-c5ae-4162-9535-127751546807",
     )
-    adds = ["ga_retirement_exclusion_person"]
+    defined_for = "ga_retirement_exclusion_eligible_person"
+
+    def formula(person, period, parameters):
+        p = parameters(
+            period
+        ).gov.states.ga.tax.income.agi.exclusions.retirement
+        cap = where(
+            person("age", period) >= p.age.older, p.cap.older, p.cap.younger
+        )
+        retirement_income = person(
+            "ga_retirement_income_exclusion_retirement_income", period
+        )
+        return min_(retirement_income, cap)
