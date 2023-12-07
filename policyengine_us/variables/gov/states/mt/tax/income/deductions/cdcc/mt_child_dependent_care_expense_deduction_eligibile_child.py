29498547baeb62806863be02b6ee8600c38cd450
jiajiagong0123 <114700434+jiajiagong0123@users.noreply.github.com>
2023-12-07 01:16:23 +0000

    make format Montana Child and Dependent Care expense Deduction Fixes #3059

--- policyengine_us/variables/gov/states/mt/tax/income/deductions/cdcc/mt_child_dependent_care_expense_deduction_eligibile_child.py
+++ policyengine_us/variables/gov/states/mt/tax/income/deductions/cdcc/mt_child_dependent_care_expense_deduction_eligibile_child.py
@@ -1,9 +1,12 @@
 from policyengine_us.model_api import *
 
+
 class mt_child_dependent_care_expense_deduction_eligible_child(Variable):
     value_type = bool
     entity = Person
-    label = "Eligible child for the Montana child dependent care expense deduction"
+    label = (
+        "Eligible child for the Montana child dependent care expense deduction"
+    )
     definition_period = YEAR
     defined_for = StateCode.MT
 
