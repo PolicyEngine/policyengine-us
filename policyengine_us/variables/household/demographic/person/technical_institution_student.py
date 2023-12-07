0e7c368eaff8a916e38db6d06bdc446f9e16a1e5
Kevintowne <89811054+Kevintowne@users.noreply.github.com>
2023-12-07 05:50:45 +0000

    Fixes variables and add legal codes

--- policyengine_us/variables/household/demographic/person/technical_institution_student.py
+++ policyengine_us/variables/household/demographic/person/technical_institution_student.py
@@ -4,5 +4,5 @@ from policyengine_us.model_api import *
 class technical_institution_student(Variable):
     value_type = bool
     entity = Person
-    label = "Is a technical institution student or not"
+    label = "Is a technical institution student"
     definition_period = YEAR
