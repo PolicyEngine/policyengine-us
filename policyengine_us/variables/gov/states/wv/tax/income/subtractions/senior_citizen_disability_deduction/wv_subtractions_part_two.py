be42d93d8f52922e35730452ed2b2c73395790c5
Naiqiao Ye <104268340+yedeer12@users.noreply.github.com>
2023-12-07 21:30:42 +0000

    Add West Virginia elderly/disabled AGI subtraction Fixes #3242

--- policyengine_us/variables/gov/states/wv/tax/income/subtractions/senior_citizen_disability_deduction/wv_subtractions_part_two.py
+++ policyengine_us/variables/gov/states/wv/tax/income/subtractions/senior_citizen_disability_deduction/wv_subtractions_part_two.py
@@ -3,7 +3,7 @@ from policyengine_us.model_api import *
 
 class wv_subtractions_part_two(Variable):
     value_type = float
-    entity = TaxUnit
+    entity = Person
     label = "West Virginia subtractions from the adjusted gross income part two for senior citizen or disability deduction"
     unit = USD
     definition_period = YEAR
