082cff1ed72fca5ac43ce9f7ce9abc2a0375a0bc
Naiqiao Ye <104268340+yedeer12@users.noreply.github.com>
2023-12-07 21:04:02 +0000

    modify

--- policyengine_us/variables/gov/states/wv/tax/income/subtractions/senior_citizen_disability_deduction/wv_senior_citizen_disability_deduction.py
+++ policyengine_us/variables/gov/states/wv/tax/income/subtractions/senior_citizen_disability_deduction/wv_senior_citizen_disability_deduction.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class wv_total_modification(Variable):
     value_type = float
     entity = Person
-    label = "West Virginia total modification as a limit for senior citizen or disability deduction"
+    label = "West Virginia senior citizen or disability deduction"
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.WV
