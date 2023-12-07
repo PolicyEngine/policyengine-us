3aedae02eaadbcf0867f3119c727ebf260141974
PavelMakarchuk <pavel.ma99@gmail.com>
2023-12-07 19:57:10 +0100

    Add mi_income_tax formula Fixes #3371

--- policyengine_us/variables/gov/states/mi/tax/income/mi_refundable_credits.py
+++ policyengine_us/variables/gov/states/mi/tax/income/mi_refundable_credits.py
@@ -1,13 +1,12 @@
 from policyengine_us.model_api import *
 
 
-class mi_income_tax(Variable):
+class mi_refundable_credits(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Michigan income tax"
+    label = "Michigan refundable credits"
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.MI
 
-    adds = ["mi_income_tax_before_refundable_credits"]
-    subtracts = ["mi_refundable_credits"]
+    adds = "gov.states.mi.tax.income.credits.refundable"
