67336365a92242946b81349dca561464fb6bdac7
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2023-12-10 17:33:23 +0000

    Final changes

--- policyengine_us/variables/gov/states/ct/tax/income/subtractions/ct_social_security_benefit_adjustment.py
+++ policyengine_us/variables/gov/states/ct/tax/income/subtractions/ct_social_security_benefit_adjustment.py
@@ -14,14 +14,15 @@ class ct_social_security_benefit_adjustment(Variable):
             period
         ).gov.states.ct.tax.income.subtractions.social_security
         filing_status = tax_unit("filing_status", period)
-        ss_rate = p.rate
-        agi = tax_unit("adjusted_gross_income", period)
-        income_threshold = p.income_threshold[filing_status]
         taxable_ss = add(tax_unit, period, ["taxable_social_security"])
-        excess = tax_unit("ct_magi_excess_over_base", period)
 
+        ss_rate = p.rate
         total_ss = add(tax_unit, period, ["social_security"])
         ss_fraction = total_ss * ss_rate
+        excess = tax_unit("ct_magi_excess_over_base", period)
         max_inclusion = min_(ss_fraction, ss_rate * excess)
+
+        agi = tax_unit("adjusted_gross_income", period)
         adjusted_ss_benefit = max_(total_ss - max_inclusion, 0)
-        return where(agi < income_threshold, total_ss, adjusted_ss_benefit)
+        income_limit = p.income_limit[filing_status]
+        return where(agi < income_limit, total_ss, adjusted_ss_benefit)
