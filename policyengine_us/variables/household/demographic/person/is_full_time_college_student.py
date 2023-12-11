12ead31c28ddfe1ed975a26c5e47f6a26ee74d94
Max Ghenis <mghenis@gmail.com>
2023-12-11 11:14:21 -0500

    Update policyengine_us/variables/household/demographic/person/is_full_time_college_student.py
    Co-authored-by: Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/household/demographic/person/is_full_time_college_student.py
+++ policyengine_us/variables/household/demographic/person/is_full_time_college_student.py
@@ -4,5 +4,5 @@ from policyengine_us.model_api import *
 class is_full_time_college_student(Variable):
     value_type = bool
     entity = Person
-    label = "Is a full time college student or not"
+    label = "Is a full time college student"
     definition_period = YEAR
