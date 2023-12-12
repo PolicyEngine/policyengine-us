ad6eb61f5bc0271f16acc53d081fed790488e1ca
PavelMakarchuk <pavel.ma99@gmail.com>
2023-12-12 15:13:34 +0100

    make format

--- policyengine_us/variables/gov/states/ga/tax/income/subtractions/retirement/ga_retirement_exclusion.py
+++ policyengine_us/variables/gov/states/ga/tax/income/subtractions/retirement/ga_retirement_exclusion.py
@@ -13,5 +13,5 @@ class ga_retirement_exclusion(Variable):
         "https://advance.lexis.com/documentpage/?pdmfid=1000516&crid=fb5db531-a80f-4790-bddb-eefc8327ef60&config=00JAA1MDBlYzczZi1lYjFlLTQxMTgtYWE3OS02YTgyOGM2NWJlMDYKAFBvZENhdGFsb2feed0oM9qoQOMCSJFX5qkd&pddocfullpath=%2Fshared%2Fdocument%2Fstatutes-legislation%2Furn%3AcontentItem%3A65D2-CDH3-CGX8-044N-00008-00&pdcontentcomponentid=234186&pdteaserkey=sr1&pditab=allpods&ecomp=8s65kkk&earg=sr1&prid=66f02b0a-c5ae-4162-9535-127751546807",
     )
     defined_for = StateCode.GA
-    
+
     adds = ["ga_retirement_exclusion_person"]
