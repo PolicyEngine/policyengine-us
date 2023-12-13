9aed03d9d95cc44f854c3199f8189b07a75c4795
jialyu0411 <98068600+jialyu0411@users.noreply.github.com>
2023-12-13 03:13:15 +0000

    Adjust formatting for the Maine child care credit variable and test Fixes #3378

--- policyengine_us/variables/gov/states/me/tax/income/credits/child_care_credit/me_child_care_credit.py
+++ policyengine_us/variables/gov/states/me/tax/income/credits/child_care_credit/me_child_care_credit.py
@@ -1,13 +1,16 @@
 from policyengine_us.model_api import *
 
 
-class child_care_credit(Variable):
+class me_child_care_credit(Variable):
     value_type = float
     entity = TaxUnit
     label = "Maine child care credit"
     unit = USD
     definition_period = YEAR
-    reference = "https://www.mainelegislature.org/legis/statutes/36/title36sec5218.html"
+    reference = [
+        "https://www.mainelegislature.org/legis/statutes/36/title36sec5218.html",
+        "https://www.maine.gov/revenue/sites/maine.gov.revenue/files/inline-files/22_1040me_sched_a_ff.pdf#page=2",
+    ]
     defined_for = StateCode.ME
 
     def formula(tax_unit, period, parameters):
@@ -15,7 +18,7 @@ class child_care_credit(Variable):
 
         # Get share of expenses that went to step 4 programs
         step_4_share_of_expenses = tax_unit(
-            "step_4_share_of_child_care_expenses", period
+            "me_step_4_share_of_child_care_expenses", period
         )
         # Line 2: Divide Federal CDCC according to share of regular vs. Step 4 expenses
         cdcc = tax_unit("cdcc", period)
