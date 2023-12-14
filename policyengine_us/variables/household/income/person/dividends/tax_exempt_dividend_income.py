3dd0b8e49bbe40d616c45f807b40f453a709509c
qingqiuzhang <90161322+qingqiuzhang@users.noreply.github.com>
2023-12-14 22:26:41 +0000

    formatting

--- policyengine_us/variables/household/income/person/dividends/tax_exempt_dividend_income.py
+++
@@ -1,9 +0,0 @@
-from policyengine_us.model_api import *
-
-
-class tax_exempt_dividend_income(Variable):
-    value_type = float
-    entity = Person
-    label = "tax-exempt dividend income"
-    unit = USD
-    definition_period = YEAR
