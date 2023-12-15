19ec42bd459d25b4c862ad5d40357ac526aae1db
Ziqian Cui <45090757+ZiqianCui@users.noreply.github.com>
2023-12-15 17:28:26 +0000

    rename and delete

--- policyengine_us/variables/gov/states/ms/tax/income/deductions/standard/ms_standard_deduction.py
+++ policyengine_us/variables/gov/states/ms/tax/income/deductions/standard/ms_standard_deduction.py
@@ -1,23 +1,20 @@
 from policyengine_us.model_api import *
 
 
-class ms_standard_deduction_joint(Variable):
+class ms_standard_deduction(Variable):
     value_type = float
-    entity = Person
-    label = "Mississippi standard deduction when married couples file jointly"
+    entity = TaxUnit
+    label = "Mississippi personal standard deduction"
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.MS
 
-    def formula(person, period, parameters):
-        # Determine if its head of household
-        is_head = person("is_tax_unit_head", period)
-
+    def formula(tax_unit, period, parameters):
         # First get their filing status.
-        filing_status = person.tax_unit("filing_status", period)
+        filing_status = tax_unit("filing_status", period)
 
         # Then get the MS Standard Deduction part of the parameter tree.
         p = parameters(period).gov.states.ms.tax.income.deductions.standard
 
         # Get their standard deduction amount based on their filing status.
-        return is_head * p.amount[filing_status]
+        return p.amount[filing_status]
