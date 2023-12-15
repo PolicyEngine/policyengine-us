52e7654ee6ad903e89b4df924e85fa55a808dfd1
ShiqiLiu <54519061+Kiki-67@users.noreply.github.com>
2023-12-15 16:57:53 +0000

    change on adds & subtracs

--- policyengine_us/variables/gov/states/va/tax/income/va_agi.py
+++ policyengine_us/variables/gov/states/va/tax/income/va_agi.py
@@ -8,14 +8,15 @@ class va_agi(Variable):
     unit = USD
     definition_period = YEAR
     reference = (
-        "https://law.lis.virginia.gov/vacodefull/title58.1/chapter3/article2/"
+        "https://www.tax.virginia.gov/sites/default/files/taxforms/individual-income-tax/2022/760-2022.pdf",
+        "https://law.lis.virginia.gov/vacodefull/title58.1/chapter3/article2/",
     )
     defined_for = StateCode.VA
 
-    adds = [
-        "adjusted_gross_income",
+    adds = ["adjusted_gross_income", "va_additions"]
+
+    subtracts = [
+        "va_subtractions",
         "va_age_deduction",
-        "va_additions",
         "taxable_social_security",
     ]
-    subtracts = ["va_subtractions"]
