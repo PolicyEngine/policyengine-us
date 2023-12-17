dc5ff90fae252f566232bbce0ddd4882ad0a2901
PavelMakarchuk <pavel.ma99@gmail.com>
2023-12-17 23:02:17 +0100

    bug fix

--- policyengine_us/variables/gov/states/al/tax/income/deductions/federal_income_tax/al_federal_income_tax_deduction.py
+++ policyengine_us/variables/gov/states/al/tax/income/deductions/federal_income_tax/al_federal_income_tax_deduction.py
@@ -10,9 +10,8 @@ class al_federal_income_tax_deduction(Variable):
     reference = "https://www.revenue.alabama.gov/ultraviewer/viewer/basic_viewer/index.html?form=2023/01/22f40abk.pdf#page=20"
     defined_for = StateCode.AL
 
-    
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states.al.tax.income.deductions.federal_tax
-        income_sources = p.countable_sources
-        federal_credits = p.credits
+        income_sources = add(tax_unit, period, p.countable_sources)
+        federal_credits = add(tax_unit, period, p.credits)
         return max_(income_sources - federal_credits, 0)
