83ff5af6a47eef051f07b9b7e0c5f9d116300ca0
Shuyu Zhan (Stella) <94806484+shuyu7zhan@users.noreply.github.com>
2023-12-17 15:25:38 +0000

    idaho pbf

--- policyengine_us/variables/gov/states/id/tax/income/other_taxes/pbf/id_pbf_requirement.py
+++ policyengine_us/variables/gov/states/id/tax/income/other_taxes/pbf/id_pbf_requirement.py
@@ -10,11 +10,14 @@ class id_pbf_requirement(Variable):
     defined_for = StateCode.ID
 
     def formula(tax_unit, period, parameters):
-        #spm_unit = tax_unit.spm_unit
-        #p = parameters(period).gov.states.id.tax.income.other_taxes.pbf
+        # spm_unit = tax_unit.spm_unit
+        # p = parameters(period).gov.states.id.tax.income.other_taxes.pbf
 
         # eligible if income less than filing status specified amount
-        income_ineligible = tax_unit("id_income_tax_before_non_refundable_credits", period) <= 0
+        income_ineligible = (
+            tax_unit("id_income_tax_before_non_refundable_credits", period)
+            <= 0
+        )
 
         # eligible if receiving public assistance tanf
         tanf_received = tax_unit("tanf", period)
