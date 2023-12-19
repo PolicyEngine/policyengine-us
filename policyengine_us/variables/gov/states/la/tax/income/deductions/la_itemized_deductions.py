220d521c383a0a482c6a70de200f5f559e1474ec
yy3187 <129118329+yy3187@users.noreply.github.com>
2023-12-19 16:26:35 +0000

    add more comments Co-authored-by: Pavel Makarchuk <PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/states/la/tax/income/deductions/la_itemized_deductions.py
+++ policyengine_us/variables/gov/states/la/tax/income/deductions/la_itemized_deductions.py
@@ -18,25 +18,32 @@ class la_itemized_deductions(Variable):
         p = parameters(
             period
         ).gov.states.la.tax.income.deductions.itemized.medical_expenses.exceedance
-        p_us = parameters(period).gov.irs.deductions
-        us_itemizing = tax_unit("tax_unit_itemizes", period)
-        us_itemized_deduction = (
-            tax_unit("itemized_deductions_less_salt", period) * us_itemizing
-        )
         us_standard_deduction = tax_unit("standard_deduction", period)
-        medical_expense_deduction = add(
-            tax_unit, period, ["medical_expense_deduction"]
-        )
+        itemizing = tax_unit("tax_unit_itemizes", period)
         # In 2022 Louisiana limits the itemized deductions to the amount of federal medical
-        # expense deduction
+        # expense deduction less the federal standard deduction
         if p.limit_to_medical_expense_deduction:
             # In the legal code the deduction is multiplied by a certain rate, which is not
             # represented in the tax form
-            return (
+            medical_expense_deduction = add(
+                tax_unit, period, ["medical_expense_deduction"]
+            )
+            medical_exepense_ded_less_standard_ded = (
                 max_(medical_expense_deduction - us_standard_deduction, 0)
                 * p.rate
+            )
+            return where(
+                itemizing, medical_exepense_ded_less_standard_ded, 0
             )  # 2022 itemized deductions
+        # In 2021 Louisiana limits the state itemized deductions value to the value of the federal
+        # itemized deductions less the federal standard deduction
         else:
-            return max_(
+            us_itemized_deduction = tax_unit(
+                "itemized_deductions_less_salt", period
+            )
+            itemized_less_standard_ded = max_(
                 0, us_itemized_deduction - us_standard_deduction
+            )
+            return where(
+                itemizing, itemized_less_standard_ded, 0
             )  # 2021 itemized deductions
