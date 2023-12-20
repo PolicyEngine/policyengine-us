f2cfe87b4b92153a0279720d754a559c5e653618
Shuyu Zhan (Stella) <94806484+shuyu7zhan@users.noreply.github.com>
2023-12-20 23:56:30 +0000

    modify variables

--- policyengine_us/variables/gov/states/id/tax/income/other_taxes/pbf/id_pbf.py
+++ policyengine_us/variables/gov/states/id/tax/income/other_taxes/pbf/id_pbf.py
@@ -7,7 +7,7 @@ class id_pbf(Variable):
     label = "Idaho permanent building tax"
     definition_period = YEAR
     reference = "https://tax.idaho.gov/wp-content/uploads/forms/EIN00046/EIN00046_11-15-2021.pdf#page=10"
-    defined_for = "id_pbf_requirement"
+    defined_for = "id_pbf_liable"
 
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states.id.tax.income.other_taxes.pbf
--- policyengine_us/variables/gov/states/id/tax/income/other_taxes/pbf/id_pbf_liable.py
+++ policyengine_us/variables/gov/states/id/tax/income/other_taxes/pbf/id_pbf_liable.py
@@ -1,27 +1,31 @@
 from policyengine_us.model_api import *
 
 
-class id_pbf_requirement(Variable):
+class id_pbf_liable(Variable):
     value_type = bool
     entity = TaxUnit
-    label = "Required to pay the Idaho permanent building fund tax"
+    label = "liable for the Idaho permanent building fund tax"
     definition_period = YEAR
     reference = "https://tax.idaho.gov/wp-content/uploads/forms/EIN00046/EIN00046_11-15-2021.pdf#page=10"
     defined_for = StateCode.ID
 
     def formula(tax_unit, period, parameters):
         # Not required to pay if there is no income tax
-        income_ineligible = (
+        owes_income_tax_before_credits = (
             tax_unit("id_income_tax_before_non_refundable_credits", period)
             <= 0
         )
 
         # Not required to pay if receiving public assistance, tanf
-        tanf_received = tax_unit.spm_unit("tanf", period) > 0
+        receives_tanf = tax_unit.spm_unit("tanf", period) > 0
 
         # Not required to pay if head or spouse is blind
         blind_head = tax_unit("blind_head", period)
         blind_spouse = tax_unit("blind_spouse", period)
         blind_ineligible = blind_head | blind_spouse
 
-        return ~income_ineligible & ~tanf_received & ~blind_ineligible
+        return (
+            ~owes_income_tax_before_credits
+            & ~receives_tanf
+            & ~blind_ineligible
+        )
