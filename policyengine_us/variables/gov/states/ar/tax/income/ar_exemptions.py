dcd45eb9a34e57e3f548bca2fb82f50068ac59e8
PavelMakarchuk <pavel.ma99@gmail.com>
2023-12-21 10:34:09 +0100

    variables until agi

--- policyengine_us/variables/gov/states/ar/tax/income/ar_exemptions.py
+++ policyengine_us/variables/gov/states/ar/tax/income/ar_exemptions.py
@@ -3,7 +3,7 @@ from policyengine_us.model_api import *
 
 class ar_exemptions(Variable):
     value_type = float
-    entity = TaxUnit
+    entity = Person
     label = "Arkansas exemptions from income tax"
     reference = "https://www.dfa.arkansas.gov/images/uploads/incomeTaxOffice/2022_AR1000F_and_AR1000NR_Instructions.pdf#page=10"
     defined_for = StateCode.AR
