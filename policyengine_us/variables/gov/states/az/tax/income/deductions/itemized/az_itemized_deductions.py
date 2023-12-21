65eb7111e425fdb6491259643474537d349837db
PavelMakarchuk <pavel.ma99@gmail.com>
2023-12-21 06:47:38 +0100

    medical expense logic

--- policyengine_us/variables/gov/states/az/tax/income/deductions/itemized/az_itemized_deductions.py
+++ policyengine_us/variables/gov/states/az/tax/income/deductions/itemized/az_itemized_deductions.py
@@ -24,22 +24,14 @@ class az_itemized_deductions(Variable):
             for deduction in p.itemized_deductions
             if deduction
             not in [
-                "salt_deduction",
                 "medical_expense_deduction",
                 "charitable_deduction",
             ]
         ]
         federal_deductions = add(tax_unit, period, deductions)
-        # Arizona allows a deduction for medical and dental expenses
-        # that cannot be deducted from federal income taxes.
-        medical_expense = add(tax_unit, period, ["medical_expense"])
-        medical_expense_deduction = tax_unit(
-            "medical_expense_deduction", period
-        )
+        # Arizona allows a complete deduction for medical and dental expenses
+        medical_expenses = add(tax_unit, period, ["medical_expense"])
 
-        az_medical_expense_deduction = max_(
-            0, medical_expense - medical_expense_deduction
-        )
 
         # Adjustments to Charitable Contributions
         # Amount of charitable contributions for which you are claiming
@@ -48,16 +40,18 @@ class az_itemized_deductions(Variable):
         charitable_contributions_credit = tax_unit(
             "az_charitable_contributions_credit", period
         )
-
+        # The charitable deduction is reduced by the amount which is used for the 
+        # Arizona charitable contributions credit, 
+        # assuming that the same charitable contributions
         charitable_deduction_after_credit = max_(
             charitable_deduction - charitable_contributions_credit, 0
         )
-
-        salt_deduction = tax_unit("az_salt_deduction", period)
-        
+        # The state and local income tax is reduced by the amount which is used 
+        # to claim the Arizona charitable contributions credit
+        # Since the Arizona charitable contributions credit is based on contributions 
+        # to qulalifying foster care organizations, we do not reduce the salt deduction
         return (
             federal_deductions
-            + az_medical_expense_deduction
+            + medical_expenses
             + charitable_deduction_after_credit
-            + salt_deduction
         )
