7db81c723574922224742c43ff24a8e609057135
PavelMakarchuk <pavel.ma99@gmail.com>
2023-12-21 04:16:48 +0100

    rename retirement income files

--- policyengine_us/variables/gov/states/ri/tax/income/agi/subtractions/taxable_retirement_income/ri_retirement_income_subtraction.py
+++ policyengine_us/variables/gov/states/ri/tax/income/agi/subtractions/taxable_retirement_income/ri_retirement_income_subtraction.py
@@ -1,14 +1,14 @@
 from policyengine_us.model_api import *
 
 
-class ri_taxable_retirement_income_subtraction(Variable):
+class ri_retirement_income_subtraction(Variable):
     value_type = float
     entity = TaxUnit
     label = "Rhode Island Taxable retirement income subtraction"
     unit = USD
     definition_period = YEAR
     reference = "http://webserver.rilin.state.ri.us/Statutes/title44/44-30/44-30-12.HTM"
-    defined_for = "ri_taxable_retirement_income_subtraction_eligible"
+    defined_for = "ri_retirement_income_subtraction_eligible"
 
     def formula(tax_unit, period, parameters):
         taxable_pension = tax_unit.members("taxable_pension_income", period)
--- policyengine_us/variables/gov/states/ri/tax/income/agi/subtractions/taxable_retirement_income/ri_retirement_income_subtraction_eligible.py
+++ policyengine_us/variables/gov/states/ri/tax/income/agi/subtractions/taxable_retirement_income/ri_retirement_income_subtraction_eligible.py
@@ -1,7 +1,7 @@
 from policyengine_us.model_api import *
 
 
-class ri_taxable_retirement_income_subtraction_eligible(Variable):
+class ri_retirement_income_subtraction_eligible(Variable):
     value_type = bool
     entity = TaxUnit
     label = (
