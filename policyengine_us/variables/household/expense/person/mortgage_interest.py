1bdd28537b5bec8fec43b8b396f5896de6e68807
PavelMakarchuk <pavel.ma99@gmail.com>
2023-12-21 18:28:14 +0100

    label

--- policyengine_us/variables/household/expense/person/mortgage_interest.py
+++ policyengine_us/variables/household/expense/person/mortgage_interest.py
@@ -4,6 +4,6 @@ from policyengine_us.model_api import *
 class mortgage_interest(Variable):
     value_type = float
     entity = Person
-    label = "Mortgage interest expense"
+    label = "Mortgage interest"
     unit = USD
     definition_period = YEAR
