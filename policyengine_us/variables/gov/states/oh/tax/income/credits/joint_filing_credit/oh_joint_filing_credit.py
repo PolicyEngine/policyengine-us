5a26129049680f61d48a0ed65882def31584d684
PavelMakarchuk <pavel.ma99@gmail.com>
2023-12-24 05:09:08 +0100

    delete extra Ohio file

--- policyengine_us/variables/gov/states/oh/tax/income/credits/joint_filing_credit/oh_joint_filing_credit.py
+++ policyengine_us/variables/gov/states/oh/tax/income/credits/joint_filing_credit/oh_joint_filing_credit.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class oh_joint_filing_credit(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Ohio Joint Filing Credit"
+    label = "Ohio joint filing credit"
     unit = USD
     definition_period = YEAR
     reference = "https://codes.ohio.gov/ohio-revised-code/section-5747.05"
@@ -16,10 +16,10 @@ class oh_joint_filing_credit(Variable):
         )
         p = parameters(period).gov.states.oh.tax.income.credits.joint_filing
         # Ohio use MAGI for the credit computation, which is Ohio AGI with
-        # the addition of the business income deduction, which is currently not included in the model
-        # hence, we use Ohio AGI for the credit computation
+        # the addition of the business income deduction, which is currently not included in the model,
+        # hence, we use the Ohio AGI for the credit computation
         oh_agi = tax_unit("oh_agi", period)
-        exemption = tax_unit("oh_exemptions", period)
+        exemption = tax_unit("oh_personal_exemptions", period)
         agi_less_exepmtion = max_(oh_agi - exemption, 0)
         percentage = p.rate.calc(agi_less_exepmtion)
         return min_(tax_before_joint_filing_crdedit * percentage, p.cap)
