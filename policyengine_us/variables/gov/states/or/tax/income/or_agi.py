f61d7b417729a735b4f4e4b7e5b177ec3ee4144e
hua7450 <113824411+hua7450@users.noreply.github.com>
2023-12-26 10:56:40 +0000

    minor change

--- policyengine_us/variables/gov/states/or/tax/income/or_agi.py
+++ policyengine_us/variables/gov/states/or/tax/income/or_agi.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class or_agi(Variable):
     value_type = float
     entity = TaxUnit
-    label = "OR adjust gross income"
+    label = "OR adjusted gross income"
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.OR
