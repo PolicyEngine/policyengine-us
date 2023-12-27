af684e39917a1ec67e79dbf05987a509fda2d152
PavelMakarchuk <pavel.ma99@gmail.com>
2023-12-27 02:05:50 +0100

    change to real estate tax

--- policyengine_us/variables/gov/states/wv/tax/income/credits/heptc/wv_homestead_excess_property_tax_credit.py
+++ policyengine_us/variables/gov/states/wv/tax/income/credits/heptc/wv_homestead_excess_property_tax_credit.py
@@ -18,10 +18,10 @@ class wv_homestead_excess_property_tax_credit(Variable):
         wv_sctc = tax_unit(
             "wv_sctc", period
         )  # West Virginia homestead excess property tax credit
-        property_tax = tax_unit("property_tax_primary_residence", period)
+        property_tax = add(tax_unit, period, ["real_estate_taxes"])
         wv_ghi = tax_unit("wv_gross_household_income", period)
 
         p = parameters(period).gov.states.wv.tax.income.credits.heptc
-        heptc_amount = property_tax - wv_sctc - p.rate.household_income * wv_ghi
+        heptc_amount = max_(property_tax - wv_sctc - p.rate.household_income * wv_ghi, 0)
 
         return min_(heptc_amount, p.cap)
