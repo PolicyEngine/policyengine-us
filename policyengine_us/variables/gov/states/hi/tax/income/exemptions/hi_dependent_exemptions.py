49978ebc766d980294ede65292770c186022d0f6
PavelMakarchuk <pavel.ma99@gmail.com>
2023-12-29 15:05:38 +0100

    test fixes

--- policyengine_us/variables/gov/states/hi/tax/income/exemptions/hi_dependent_exemptions.py
+++ policyengine_us/variables/gov/states/hi/tax/income/exemptions/hi_dependent_exemptions.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class hi_dependent_exemptions(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Hawaii regular exemptions"
+    label = "Hawaii dependent exemptions"
     unit = USD
     documentation = (
         "https://files.hawaii.gov/tax/forms/2022/n11ins.pdf#page=20"
@@ -17,8 +17,8 @@ class hi_dependent_exemptions(Variable):
         # The tax unit can claim exemptions for each dependent
         dependents = tax_unit("tax_unit_dependents", period)
         # The the head or the spouse is disabled, the dependent exemption goes to 0
-        disabled_head = tax_unit("head_disabled", period)
-        disabled_spouse = tax_unit("spouse_disabled", period)
+        disabled_head = tax_unit("head_is_disabled", period)
+        disabled_spouse = tax_unit("spouse_is_disabled", period)
         head_or_spouse_disabled = disabled_head | disabled_spouse
         base_amount = p.base * dependents
-        return where(head_or_spouse_disabled, base_amount, 0)
+        return where(head_or_spouse_disabled, 0, base_amount)
