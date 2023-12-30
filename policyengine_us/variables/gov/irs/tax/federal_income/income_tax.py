ba33eac124a761bdda9ef6c8f7d31724ef5820e7
Shashank <40920035+shashank524@users.noreply.github.com>
2023-12-29 19:04:29 -0600

    Update income_tax.py
    Removed adds/subtracts

--- policyengine_us/variables/gov/irs/tax/federal_income/income_tax.py
+++ policyengine_us/variables/gov/irs/tax/federal_income/income_tax.py
@@ -8,22 +8,18 @@ class income_tax(Variable):
     unit = USD
     label = "Federal income tax"
     documentation = "Total federal individual income tax liability."
-    adds = [
-        "income_tax_before_credits",
-        "income_tax_before_refundable_credits"
-    ]
-    subtracts = [
-        "income_tax_refundable_credits",
-        "income_tax_capped_non_refundable_credits",
-        "income_tax_refundable_credits"
-    ]
 
     def formula(person, period, parameters):
-        if parameters(
-            period
-        ).gov.contrib.ubi_center.flat_tax.abolish_federal_income_tax:
+        if parameters(period).gov.contrib.ubi_center.flat_tax.abolish_federal_income_tax:
             return 0
         else:
-            added_components = add(person, period, income_tax.adds)
-            subtracted_components = add(person, period, income_tax.subtracts)
+            added_components = add(person, period, [
+                "income_tax_before_credits",
+                "income_tax_before_refundable_credits"
+            ])
+            subtracted_components = add(person, period, [
+                "income_tax_refundable_credits",
+                "income_tax_capped_non_refundable_credits",
+                "income_tax_refundable_credits"
+            ])
             return added_components - subtracted_components
