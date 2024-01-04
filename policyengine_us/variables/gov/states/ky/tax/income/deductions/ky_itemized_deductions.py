6bac5b9e0a7069563d829f2f1cc3990c53e8642c
leogoldman <leogoldman1@gmail.com>
2024-01-04 02:20:56 -0500

    Change KY itemized to federal minus SALT and medical deductions.
    Added wagering losses deduction to federal itemized deductions

--- policyengine_us/variables/gov/states/ky/tax/income/deductions/ky_itemized_deductions.py
+++ policyengine_us/variables/gov/states/ky/tax/income/deductions/ky_itemized_deductions.py
@@ -12,7 +12,5 @@ class ky_itemized_deductions(Variable):
         "https://law.justia.com/codes/kentucky/2022/chapter-141/section-141-019/"  # (2)(i)
     )
     defined_for = StateCode.KY
-    adds = "gov.states.ky.tax.income.deductions.itemized"
-
-    # Assume that married filing separate spouses each fill out their own Schedule A.
-    # They are allowed to do this or file a joint Schedule A and divide by share of income.
+    adds = ["itemized_deductions_less_salt"]
+    subtracts = ["medical_expense_deduction"]
