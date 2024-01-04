c7acdc5e5a0bdfbcda6131519d653cbd3ca10dd4
PavelMakarchuk <pavel.ma99@gmail.com>
2024-01-04 01:34:58 +0100

    still duplicate code in regular exemptions

--- policyengine_us/variables/gov/states/hi/tax/income/exemptions/hi_regular_exemptions.py
+++ policyengine_us/variables/gov/states/hi/tax/income/exemptions/hi_regular_exemptions.py
@@ -1,10 +1,10 @@
 from policyengine_us.model_api import *
 
 
-class hi_exemptions(Variable):
+class hi_regular_exemptions(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Hawaii exemptions"
+    label = "Hawaii regular exemptions"
     unit = USD
     documentation = (
         "https://files.hawaii.gov/tax/forms/2022/n11ins.pdf#page=20"
@@ -13,6 +13,6 @@ class hi_exemptions(Variable):
     defined_for = StateCode.HI
 
     def formula(tax_unit, period, parameters):
-        reuglar_exemptions = tax_unit("hi_regular_exemptions", period)
-        disabled_exemptions = tax_unit("hi_disabled_exemptions", period)
-        return max_(reuglar_exemptions, disabled_exemptions)
+        exemptions = tax_unit("exemptions_count", period)
+        p = parameters(period).gov.states.hi.tax.income.exemptions
+        return exemptions * p.base
