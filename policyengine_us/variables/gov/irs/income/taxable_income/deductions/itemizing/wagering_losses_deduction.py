28b1c5cbd9cf172cc9194400ec8c2ba80a199e75
Max Ghenis <mghenis@gmail.com>
2024-01-06 09:54:39 -0500

    Update policyengine_us/variables/gov/irs/income/taxable_income/deductions/itemizing/wagering_losses_deduction.py

--- policyengine_us/variables/gov/irs/income/taxable_income/deductions/itemizing/wagering_losses_deduction.py
+++ policyengine_us/variables/gov/irs/income/taxable_income/deductions/itemizing/wagering_losses_deduction.py
@@ -8,7 +8,7 @@ class wagering_losses_deduction(Variable):
     unit = USD
     documentation = "Deduction from taxable income for gambling losses, capped at gambling winnings."
     definition_period = YEAR
-    reference = "https://www.law.cornell.edu/uscode/text/26/165"  # (d)
+    reference = "https://www.law.cornell.edu/uscode/text/26/165#d"
 
     def formula(tax_unit, period, parameters):
         gambling_losses = add(tax_unit, period, ["gambling_losses"])
