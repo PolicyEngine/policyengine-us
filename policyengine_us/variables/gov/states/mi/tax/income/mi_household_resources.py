e0ef9cffb94ceb2d0beea14184b666b554715abf
PavelMakarchuk <pavel.ma99@gmail.com>
2024-01-06 15:41:29 +0100

    formula hh resources

--- policyengine_us/variables/gov/states/mi/tax/income/mi_household_resources.py
+++ policyengine_us/variables/gov/states/mi/tax/income/mi_household_resources.py
@@ -9,4 +9,4 @@ class mi_household_resources(Variable):
     definition_period = YEAR
     defined_for = StateCode.MI
     reference = "http://www.legislature.mi.gov/(S(keapvg1h2vndkn25rtmpyyse))/mileg.aspx?page=getObject&objectName=mcl-206-527a"
-    adds = "gov.states.mi.tax.income.credits.household_resources"
+    adds = "gov.states.mi.tax.income.household_resources"
