46813110bb961ab260c1405e8e8c03e77b49284d
PavelMakarchuk <pavel.ma99@gmail.com>
2024-01-07 17:28:07 +0100

    formatting

--- policyengine_us/variables/gov/states/la/tax/income/deductions/la_itemized_deductions.py
+++ policyengine_us/variables/gov/states/la/tax/income/deductions/la_itemized_deductions.py
@@ -15,35 +15,15 @@ class la_itemized_deductions(Variable):
     defined_for = StateCode.LA
 
     def formula(tax_unit, period, parameters):
-        p = parameters(
-            period
-        ).gov.states.la.tax.income.deductions.itemized.medical_expenses.exceedance
+        p = parameters(period).gov.states.la.tax.income.deductions.itemized
         federal_standard_deduction = tax_unit("standard_deduction", period)
-        itemizing = tax_unit("tax_unit_itemizes", period)
-        # In 2022 Louisiana limits the itemized deductions to the amount of federal medical
-        # expense deduction less the federal standard deduction
-        if p.limit_to_medical_expense_deduction:
-            # In the legal code the deduction is multiplied by a certain rate, which is not
-            # represented in the tax form
-            medical_expense_deduction = add(
-                tax_unit, period, ["medical_expense_deduction"]
-            )
-            medical_exepense_ded_less_standard_ded = (
-                max_(medical_expense_deduction - federal_standard_deduction, 0)
-                * p.excess_fraction
-            )
-            return where(
-                itemizing, medical_exepense_ded_less_standard_ded, 0
-            )  # 2022 itemized deductions
-        # In 2021 Louisiana limits the state itemized deductions value to the value of the federal
-        # itemized deductions less the federal standard deduction
-        else:
-            federal_itemized_deduction = tax_unit(
-                "itemized_deductions_less_salt", period
-            )
-            itemized_less_standard_ded = max_(
-                0, federal_itemized_deduction - federal_standard_deduction
-            )
-            return where(
-                itemizing, itemized_less_standard_ded, 0
-            )  # 2021 itemized deductions
+        # Louisiana limits the itemized deductions to the amount of either the federal medical
+        # expense deduction or the total federal itemized deductions
+        # less the federal standard deduction
+        relevant_itemized_deductions = add(
+            tax_unit, period, p.relevant_federal_deductions
+        )
+        reduced_relevant_itemized_deductions = max_(
+            relevant_itemized_deductions - federal_standard_deduction, 0
+        )
+        return reduced_relevant_itemized_deductions * p.excess_fraction
