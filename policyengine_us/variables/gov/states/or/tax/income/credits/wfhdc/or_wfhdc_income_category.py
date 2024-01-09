4093bee56b9b6b83e11598cb20cf709cf5103082
PavelMakarchuk <pavel.ma99@gmail.com>
2024-01-09 13:05:30 +0100

    allign to legal code to represent fgp %

--- policyengine_us/variables/gov/states/or/tax/income/credits/wfhdc/or_wfhdc_income_category.py
+++ policyengine_us/variables/gov/states/or/tax/income/credits/wfhdc/or_wfhdc_income_category.py
@@ -11,15 +11,6 @@ class or_wfhdc_income_category(Variable):
     reference = "https://www.oregon.gov/dor/forms/FormsPubs/publication-or-wfhdc-tb_101-458_2021.pdf#page=1"
 
     def formula(tax_unit, period, parameters):
-        # Get the parameter tree for the WFHDC table row letter.
-        p = (
-            parameters(period)
-            .gov.states["or"]
-            .tax.income.credits.wfhdc.household_size
-        )
-
-        # Get the household size.
-        household_size = tax_unit("tax_unit_size", period)
 
         # Get the household income, considered the larger of federal and Oregon AGI.
         federal_agi = tax_unit("adjusted_gross_income", period)
@@ -27,26 +18,12 @@ class or_wfhdc_income_category(Variable):
             "or_income_after_subtractions", period
         )
         household_income = max_(federal_agi, or_income_after_subtractions)
-
-        # Get the table row number based on household size and income.
-        return select(
-            [
-                household_size == 2,
-                household_size == 3,
-                household_size == 4,
-                household_size == 5,
-                household_size == 6,
-                household_size == 7,
-                household_size >= 8,
-            ],
-            [
-                p.two.calc(household_income, right=True),
-                p.three.calc(household_income, right=True),
-                p.four.calc(household_income, right=True),
-                p.five.calc(household_income, right=True),
-                p.six.calc(household_income, right=True),
-                p.seven.calc(household_income, right=True),
-                p.eight_or_more.calc(household_income, right=True),
-            ],
-            default=0,
-        )
+        # The table row number is based on the percentage of the tax unit fpg
+        fpg = tax_unit("tax_unit_fpg", period)
+        fpg_rate = household_income / fpg
+        # The rate can not exceed 300%.
+        p = parameters(period).gov.states["or"].tax.income.credits.wfhdc
+        capped_fpg_rate = min_(fpg_rate, p.fpg_limit)
+        # Aggeregate the fpg ratio to the nearest 10%.
+        rounded_fpg_rate = np.round(capped_fpg_rate, 3) * 10
+        return np.ceil(rounded_fpg_rate)
