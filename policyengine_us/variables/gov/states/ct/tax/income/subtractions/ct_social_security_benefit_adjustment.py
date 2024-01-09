28e725d9bf2dd3a61e7ec54f3cf54df1e3504f3a
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2024-01-09 23:37:36 +0000

    Final fixes

--- policyengine_us/variables/gov/states/ct/tax/income/subtractions/ct_social_security_benefit_adjustment.py
+++ policyengine_us/variables/gov/states/ct/tax/income/subtractions/ct_social_security_benefit_adjustment.py
@@ -16,25 +16,28 @@ class ct_social_security_benefit_adjustment(Variable):
     defined_for = StateCode.CT
 
     def formula(tax_unit, period, parameters):
-        p = parameters(
-            period
-        ).gov.states.ct.tax.income.subtractions.social_security
+        p = parameters(period).gov.states.ct.tax.income.subtractions
         filing_status = tax_unit("filing_status", period)
 
-        ss_rate = p.rate.social_security
-        magi_rate = p.rate.magi_excess
+        ss_rate = p.social_security.rate.social_security
+        magi_rate = p.magi_excess
         # Line 41, Part A and Part B
         us_taxable_ss = tax_unit("tax_unit_taxable_social_security", period)
         ss_fraction = us_taxable_ss * ss_rate
         excess = tax_unit("ct_magi_excess_over_base", period)
         # Line 41, Part C and Part D
         # Lesser of 25% of MAGI excess and 25% of taxable social security benefits
-        max_inclusion = min_(ss_fraction, magi_rate * excess)
+        capped_ss_fraction = min_(ss_fraction, magi_rate * excess)
 
         agi = tax_unit("adjusted_gross_income", period)
         # Line 41, Part E and Part F
-        # Difference between taxable social security benefits and lesser amount from above
-        adjusted_ss_benefit = max_(us_taxable_ss - max_inclusion, 0)
-        income_limit = p.income_limit[filing_status]
-        # Adjustment determined based on AGI amount compared to income limit
-        return where(agi < income_limit, us_taxable_ss, adjusted_ss_benefit)
+        # Difference between taxable social security benefits and capped social security fraction
+        adjusted_ss_benefit = max_(us_taxable_ss - capped_ss_fraction, 0)
+        reduction_threshold = p.social_security.reduction_threshold[
+            filing_status
+        ]
+        # Adjustment determined based on AGI amount compared to reduction threshold
+        agi_over_reduction_threshold = agi < reduction_threshold
+        return where(
+            agi_over_reduction_threshold, us_taxable_ss, adjusted_ss_benefit
+        )
