689f69103ec42415ca83a00c7943ee40084e0bcb
PavelMakarchuk <pavel.ma99@gmail.com>
2024-01-11 08:28:50 +0100

    regular exemptions format

--- policyengine_us/variables/gov/states/hi/tax/income/exemptions/hi_regular_exemptions.py
+++ policyengine_us/variables/gov/states/hi/tax/income/exemptions/hi_regular_exemptions.py
@@ -13,12 +13,14 @@ class hi_regular_exemptions(Variable):
     defined_for = StateCode.HI
 
     def formula(tax_unit, period, parameters):
-        exemptions = tax_unit("exemptions_count", period)
+        exemptions_count = tax_unit("exemptions_count", period)
         p = parameters(period).gov.states.hi.tax.income.exemptions
         # Aged heads and spouses get an extra base exemption.
         person = tax_unit.members
         head_or_spouse = person("is_tax_unit_head_or_spouse", period)
         aged = person("age", period) >= p.aged_threshold
-        aged_head_or_spouse = tax_unit.sum(aged & head_or_spouse)
-        total_exemptions_including_aged = exemptions + aged_head_or_spouse
-        return total_exemptions_including_aged * p.base
+        aged_head_spouse_count = tax_unit.sum(aged & head_or_spouse)
+        total_exemption_count_including_aged = (
+            exemptions_count + aged_head_spouse_count
+        )
+        return total_exemption_count_including_aged * p.base
