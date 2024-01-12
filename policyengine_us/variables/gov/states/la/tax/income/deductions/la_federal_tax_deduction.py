5323deff17e8edb0633a78498654b30f18ec3d73
PavelMakarchuk <pavel.ma99@gmail.com>
2024-01-12 17:38:10 +0100

    legal code

--- policyengine_us/variables/gov/states/la/tax/income/deductions/la_federal_tax_deduction.py
+++ policyengine_us/variables/gov/states/la/tax/income/deductions/la_federal_tax_deduction.py
@@ -8,9 +8,9 @@ class la_federal_tax_deduction(Variable):
     unit = USD
     definition_period = YEAR
     reference = [
-        "https://revenue.louisiana.gov/TaxForms/IT540iWEB(2022)D1.pdf#page=2",  # 2022 line 8B-line 8C
-        "https://revenue.louisiana.gov/TaxForms/IT540i(2021)%20Instructions.pdf#page=3",  # 2021 line 8A-line 8C
-        "https://www.legis.la.gov/Legis/Law.aspx?d=101760",  # (3)
+        "https://revenue.louisiana.gov/TaxForms/IT540iWEB(2022)D1.pdf#page=2",  # 2022 repealed
+        "https://revenue.louisiana.gov/TaxForms/IT540i(2021)%20Instructions.pdf#page=3",  # 2021 line 9
+        "https://law.justia.com/codes/louisiana/2021/revised-statutes/title-47/rs-298/",  # (3)
     ]
     defined_for = StateCode.LA
 
