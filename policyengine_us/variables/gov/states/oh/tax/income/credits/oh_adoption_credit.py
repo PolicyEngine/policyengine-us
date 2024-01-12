f08104eafd740017fdc07758af88da70f9cc7f98
Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>
2024-01-12 18:36:34 +0100

    Fix Ohio adoption credit (#3593)
    * Fix Ohio adoption credit
    Fixes #3592
    
    * changelog
    
    * adopted this year var

--- policyengine_us/variables/gov/states/oh/tax/income/credits/oh_adoption_credit.py
+++ policyengine_us/variables/gov/states/oh/tax/income/credits/oh_adoption_credit.py
@@ -27,9 +27,11 @@ class oh_adoption_credit(Variable):
         )
         p = parameters(period).gov.states.oh.tax.income.credits.adoption
         child_age_eligible = person("age", period) < p.age_limit
+        adopted_this_year = person("adopted_this_year", period)
+        age_eligible_adopted_child = child_age_eligible & adopted_this_year
         credit_if_eligible = min_(
             max_(eligible_adoption_related_expenses, p.amount.min),
             p.amount.max,
         )
-        credit = credit_if_eligible * child_age_eligible
+        credit = credit_if_eligible * age_eligible_adopted_child
         return tax_unit.sum(credit)
