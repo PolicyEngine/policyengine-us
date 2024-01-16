26459814932ae6bd7e208cbaac7e14369f5b16ad
xianxinzhu <97819588+xianxinzhu@users.noreply.github.com>
2024-01-16 06:15:55 +0000

    Fixes variables

--- policyengine_us/variables/gov/states/de/tax/income/combined_separate_status/eitc/de_refundable_eitc.py
+++ policyengine_us/variables/gov/states/de/tax/income/combined_separate_status/eitc/de_refundable_eitc.py
@@ -1,10 +1,10 @@
 from policyengine_us.model_api import *
 
 
-class de_nonrefundable_eitc(Variable):
+class de_refundable_eitc(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Delaware individual nonrefundable earned income credit"
+    label = "Delaware individual refundable earned income credit"
     unit = USD
     definition_period = YEAR
     reference = "https://revenuefiles.delaware.gov/2021/TY21_PIT-RSS_2021-01_PaperInteractive.pdf#page=1"
@@ -24,10 +24,9 @@ class de_nonrefundable_eitc(Variable):
         
         federal_eitc = tax_unit("eitc", period)
         refundable_eitc_cal = federal_eitc * p.refundable
-        non_refundable_eitc_cal = federal_eitc * p.non_refundable
-        non_refundabele_eitc_eligibility = refundable_eitc_cal < tax_after_non_refundable_credit
+        refundabele_eitc_eligibility = refundable_eitc_cal >= tax_after_non_refundable_credit
         return where(
-            non_refundabele_eitc_eligibility,
-            min_(non_refundable_eitc_cal, tax_after_non_refundable_credit),
+            refundabele_eitc_eligibility,
+            refundable_eitc_cal,
             0
         )
