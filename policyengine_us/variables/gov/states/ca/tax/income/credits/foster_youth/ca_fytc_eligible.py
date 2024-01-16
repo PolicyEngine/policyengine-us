f3c2474b2f8a0a50ad2d2052bb02bbb123cbac59
XZ-mabel <131919825+XZ-mabel@users.noreply.github.com>
2024-01-16 00:45:47 -0500

    Update policyengine_us/variables/gov/states/ca/tax/income/credits/foster_youth/ca_fytc_eligible.py
    Co-authored-by: Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/states/ca/tax/income/credits/foster_youth/ca_fytc_eligible.py
+++ policyengine_us/variables/gov/states/ca/tax/income/credits/foster_youth/ca_fytc_eligible.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class ca_fytc_eligible(Variable):
     value_type = bool
     entity = Person
-    label = "Eligible for the California foster youth tax credit"
+    label = "Eligible person for the California foster youth tax credit"
     definition_period = YEAR
     reference = "https://www.ftb.ca.gov/forms/2022/2022-3514.pdf#page=4"
     defined_for = StateCode.CA
