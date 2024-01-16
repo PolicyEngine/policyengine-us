d6429bb04b5dc8bd5bd5cc4dfc26643855e42099
XZ-mabel <131919825+XZ-mabel@users.noreply.github.com>
2024-01-16 00:45:54 -0500

    Update policyengine_us/variables/gov/states/ca/tax/income/credits/foster_youth/ca_fytc.py
    Co-authored-by: Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/states/ca/tax/income/credits/foster_youth/ca_fytc.py
+++ policyengine_us/variables/gov/states/ca/tax/income/credits/foster_youth/ca_fytc.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class ca_fytc(Variable):
     value_type = float
     entity = TaxUnit
-    label = "California Foster Youth Tax Credit"
+    label = "California foster youth tax credit"
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.CA
