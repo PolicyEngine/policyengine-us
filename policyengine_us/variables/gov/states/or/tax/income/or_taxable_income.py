7a9fdca9ef91f12d23f021fb4688cb44d2a02307
Kevintowne <89811054+Kevintowne@users.noreply.github.com>
2024-01-17 02:54:32 +0000

    Fixes variables and tests

--- policyengine_us/variables/gov/states/or/tax/income/or_taxable_income.py
+++ policyengine_us/variables/gov/states/or/tax/income/or_taxable_income.py
@@ -10,8 +10,9 @@ class or_taxable_income(Variable):
     defined_for = StateCode.OR
 
     def formula(tax_unit, period, parameters):
-        income_after_subtractions = tax_unit(
-            "or_income_after_subtractions", period
-        )
+        # income_after_subtractions = tax_unit(
+        #     "or_income_after_subtractions", period
+        # )
+        or_agi = tax_unit("or_agi", period)
         deductions = tax_unit("or_deductions", period)
-        return max_(income_after_subtractions - deductions, 0)
+        return max_(or_agi - deductions, 0)
