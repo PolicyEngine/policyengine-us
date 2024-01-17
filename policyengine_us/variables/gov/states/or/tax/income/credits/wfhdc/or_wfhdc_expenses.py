3e9cb86908246b292aa61a5bd5889586c6d6f7e2
Kingsley Yang <87620347+Shinuing@users.noreply.github.com>
2024-01-17 21:32:40 +0000

    resolve conversations

--- policyengine_us/variables/gov/states/or/tax/income/credits/wfhdc/or_wfhdc_expenses.py
+++ policyengine_us/variables/gov/states/or/tax/income/credits/wfhdc/or_wfhdc_expenses.py
@@ -7,21 +7,20 @@ class or_wfhdc_expenses(Variable):
     label = "Oregon working family household and dependent care expenses"
     unit = USD
     definition_period = YEAR
-    reference = ("https://oregon.public.law/statutes/ors_315.264",)
+    reference = "https://oregon.public.law/statutes/ors_315.264"
 
     def formula(tax_unit, period, parameters):
         # First, cap based on the number of eligible care receivers
-        expenses = tax_unit("tax_unit_childcare_expenses", period)
+        childcare_expenses = tax_unit("tax_unit_childcare_expenses", period)
         p_cdcc = parameters(period).gov.irs.credits.cdcc
         p_or = parameters(period).gov.states["or"].tax.income.credits.wfhdc
-        count_eligible = min_(
+        eligible_people = min_(
             p_cdcc.eligibility.max, tax_unit("count_cdcc_eligible", period)
         )
-        eligible_capped_expenses = min_(
-            expenses, p_or.max_amount * count_eligible
-        )
 
-        # Then, cap further to the lowest earnings between the taxpayer and spouse
+        total_max_amount = p_or.cap * eligible_people
+        eligible_capped_expenses = min_(childcare_expenses, total_max_amount)
+
         return min_(
             eligible_capped_expenses,
             tax_unit("min_head_spouse_earned", period),
