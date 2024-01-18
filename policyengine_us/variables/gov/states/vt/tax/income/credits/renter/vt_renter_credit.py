b6c26f43c26af843a6c1142de338ea076a0119e3
hua7450 <113824411+hua7450@users.noreply.github.com>
2024-01-18 05:45:16 +0000

    figure out formula

--- policyengine_us/variables/gov/states/vt/tax/income/credits/renter/vt_renter_credit.py
+++ policyengine_us/variables/gov/states/vt/tax/income/credits/renter/vt_renter_credit.py
@@ -14,4 +14,8 @@ class vt_renter_credit(Variable):
     defined_for = StateCode.VT
 
     def formula(tax_unit, period, parameters):
-        
+        # if no subsidized: (upper cap - lower cap)/(upper cap - income) * 0.1 * number of rent month/12 * fair market rent * 0.5 if shared rent
+        # if subsidized:  combined amount = subsidized amount + non-subsidized amount
+        # subsidized amount: actual pay rent amount * 0.1 * subsidzed mouth/number of rent month
+        # non-subsidized amount: (number of rent month - subsized month)/number of rent month * fair market rent
+        # result: (upper cap - lower cap)/(upper cap - income) * number of rent month/12 * combined amount
