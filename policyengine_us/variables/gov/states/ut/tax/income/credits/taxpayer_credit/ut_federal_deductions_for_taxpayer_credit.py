b1b08b5e0f459de7ca2211b069760e6e9b1af50c
JudyZ13 <112495414+JudyZ13@users.noreply.github.com>
2024-01-18 23:03:42 +0000

    updates

--- policyengine_us/variables/gov/states/ut/tax/income/credits/taxpayer_credit/ut_federal_deductions_for_taxpayer_credit.py
+++ policyengine_us/variables/gov/states/ut/tax/income/credits/taxpayer_credit/ut_federal_deductions_for_taxpayer_credit.py
@@ -12,23 +12,26 @@ class ut_federal_deductions_for_taxpayer_credit(Variable):
     reference = "https://le.utah.gov/xcode/Title59/Chapter10/59-10-S114.html?v=C59-10-S114_2022032320220323"
 
     def formula(tax_unit, period, parameters):
-        # itemized deductions (exclude SALT)
         p = parameters(period).gov.irs.deductions
-
         filing_status = tax_unit("filing_status", period)
+
+        us_itemizing = tax_unit("tax_unit_itemizes", period)
+        std_ded = tax_unit("standard_deduction", period)
+
+        # Subtract SALT from Itemized Deductions
         deductions = [
             deduction
             for deduction in p.itemized_deductions
             if deduction not in ["salt_deduction"]
         ]
+        ite_ded = add(tax_unit, period, deductions)
 
-        ite_deductions = add(tax_unit, period, deductions)
-
-        # include capped real estate taxes in federal deductions
+        # Include Real Estate Taxes in Itemized Deductions
         real_estate_tax = add(tax_unit, period, ["real_estate_taxes"])
 
         capped_real_estate_tax = min_(
             real_estate_tax, p.itemized.salt_and_real_estate.cap[filing_status]
         )
 
-        return ite_deductions + capped_real_estate_tax
+        # Line 12. Federal Standard or Itemized Deductions
+        return where(us_itemizing, ite_ded + capped_real_estate_tax, std_ded)
