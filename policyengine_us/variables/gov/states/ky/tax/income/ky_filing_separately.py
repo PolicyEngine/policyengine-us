8a5862cd15a61509e52771dfb8fad92ab19bee1d
Ruby Ren <38923159+YichunRen@users.noreply.github.com>
2024-01-19 21:11:28 +0000

    update unit

--- policyengine_us/variables/gov/states/ky/tax/income/ky_filing_separately.py
+++ policyengine_us/variables/gov/states/ky/tax/income/ky_filing_separately.py
@@ -4,8 +4,7 @@ from policyengine_us.model_api import *
 class ky_files_separately(Variable):
     value_type = bool
     entity = TaxUnit
-    label = "married couple files separately on Kentucky tax return"
-    unit = USD
+    label = "Married couple file separately on the Kentucky tax return"
     definition_period = YEAR
     reference = "https://revenue.ky.gov/Forms/740%20Packet%20Instructions%205-9-23.pdf#page=11"
     defined_for = StateCode.KY
