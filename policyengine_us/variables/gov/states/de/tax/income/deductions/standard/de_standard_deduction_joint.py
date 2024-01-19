58e7456a622d4f61f8c2c4ff9800a18ed0e07ad7
xianxinzhu <97819588+xianxinzhu@users.noreply.github.com>
2024-01-19 04:16:40 +0000

    Fixes variables

---
+++ policyengine_us/variables/gov/states/de/tax/income/deductions/standard/de_standard_deduction_joint.py
@@ -0,0 +1,17 @@
+from policyengine_us.model_api import *
+
+
+class de_standard_deduction_joint(Variable):
+    value_type = float
+    entity = Person
+    label = "Delaware standard deduction when married filing jointly"
+    unit = USD
+    definition_period = YEAR
+    reference = "https://revenuefiles.delaware.gov/2022/PIT-RES_TY22_2022-02_Instructions.pdf#page=8"
+    defined_for = StateCode.DE
+
+    def formula(person, period, parameters):
+        p = parameters(period).gov.states.de.tax.income.deductions.standard
+        filing_status = person.tax_unit("filing_status", period)
+        is_head = person("is_tax_unit_head", period)
+        return is_head * p.amount[filing_status]
