c906b8369c384e0e78c0b3679f0e30392cadee66
Matty <31402450+Mshuning@users.noreply.github.com>
2024-01-22 06:21:50 +0000

    integration & mi_exemptions_count

--- policyengine_us/variables/gov/states/mi/tax/income/exemptions/mi_exemptions_count.py
+++ policyengine_us/variables/gov/states/mi/tax/income/exemptions/mi_exemptions_count.py
@@ -14,27 +14,8 @@ class mi_exemptions_count(Variable):
         "https://www.michigan.gov/-/media/Project/Websites/taxes/2022RM/IIT/MI-1040CR7.pdf",
         "https://www.michigan.gov/-/media/Project/Websites/taxes/2022RM/IIT/BOOK_MI-1040CR-7.pdf#page=7",
     )
-
-    def formula(tax_unit, period):
-        # Personal Exemptions
-        personal_count = add(tax_unit, period, ["is_tax_unit_head_or_spouse"])
-
-        # Disabled
-        disabled_count = add(
-            tax_unit, period, ["mi_disabled_exemption_eligible_person"]
-        )
-
-        # Disabled veteran exemptions
-        disabled_veterans_count = add(
-            tax_unit, period, ["is_fully_disabled_service_connected_veteran"]
-        )
-
-        # Child & Dependent adult
-        dependent_count = tax_unit("tax_unit_dependents", period)
-
-        return (
-            personal_count
-            + disabled_count
-            + disabled_veterans_count
-            + dependent_count
-        )
+    adds = [
+        "tax_unit_size",
+        "mi_disabled_exemption_eligible_person",
+        "is_fully_disabled_service_connected_veteran",
+    ]
