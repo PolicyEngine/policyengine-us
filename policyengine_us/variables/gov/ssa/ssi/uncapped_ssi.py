8012b5ed2a214f3d3350eba882095b7c3710fe5f
Caleb <caleblpena@gmail.com>
2024-01-24 16:24:32 -0700

    remove ssi_income_in_sga from uncapped_ssi

--- policyengine_us/variables/gov/ssa/ssi/uncapped_ssi.py
+++ policyengine_us/variables/gov/ssa/ssi/uncapped_ssi.py
@@ -12,9 +12,8 @@ class uncapped_ssi(Variable):
     def formula(person, period, parameters):
         amount = person("ssi_amount_if_eligible", period)
         meets_resource_test = person("meets_ssi_resource_test", period)
-        meets_income_test = person("ssi_income_in_sga", period)
         eligible = person("is_ssi_eligible_individual", period)
         countable_income = person("ssi_countable_income", period)
-        return (meets_resource_test & meets_income_test & eligible) * (
+        return (meets_resource_test & eligible) * (
             amount - countable_income
         )
