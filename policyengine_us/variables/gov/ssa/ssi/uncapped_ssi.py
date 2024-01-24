97aa3c50a0163122fb123a4022d834ed098c46b5
Caleb <caleblpena@gmail.com>
2024-01-24 12:17:55 -0700

    use ssi_income_eligible in uncapped_ssi

--- policyengine_us/variables/gov/ssa/ssi/uncapped_ssi.py
+++ policyengine_us/variables/gov/ssa/ssi/uncapped_ssi.py
@@ -12,6 +12,7 @@ class uncapped_ssi(Variable):
     def formula(person, period, parameters):
         amount = person("ssi_amount_if_eligible", period)
         meets_resource_test = person("meets_ssi_resource_test", period)
+        meets_income_test = person("ssi_income_eligible", period)
         eligible = person("is_ssi_eligible_individual", period)
         countable_income = person("ssi_countable_income", period)
-        return (meets_resource_test & eligible) * (amount - countable_income)
+        return (meets_resource_test & meets_income_test & eligible) * (amount - countable_income)
