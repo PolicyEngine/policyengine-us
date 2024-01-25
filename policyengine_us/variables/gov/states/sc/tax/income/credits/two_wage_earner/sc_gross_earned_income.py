45bd0cb465a8b26f4c3ab03354d03e39267cbc75
PavelMakarchuk <pavel.ma99@gmail.com>
2024-01-25 18:39:13 +0100

    make format

--- policyengine_us/variables/gov/states/sc/tax/income/credits/two_wage_earner/sc_gross_earned_income.py
+++ policyengine_us/variables/gov/states/sc/tax/income/credits/two_wage_earner/sc_gross_earned_income.py
@@ -13,7 +13,7 @@ class sc_gross_earned_income(Variable):
         p = parameters(
             period
         ).gov.states.sc.tax.income.credits.two_wage_earner.earned_income
-        # Based on the legal code and the tax form worksheet, 
+        # Based on the legal code and the tax form worksheet,
         # we use earned income as the income base for the credit.
         earned_income = person("earned_income", period)
         subtractions = add(person, period, p.subtractions)
