a7984e7199f2425d9bce4523e79b2bd164f017a6
jialyu0411 <98068600+jialyu0411@users.noreply.github.com>
2024-01-25 00:35:09 +0000

    Add Mississippi's 2024 2025 2026 state income tax rates Fixes #3645

---
+++ policyengine_us/variables/gov/states/ms/tax/income/credits/charitable_contribution/ms_charitable_contributions_credit.py
@@ -0,0 +1,27 @@
+from policyengine_us.model_api import *
+
+
+class ms_charitable_contributions_credit(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Mississippi charitable contributions credit"
+    unit = USD
+    definition_period = YEAR
+    defined_for = StateCode.MS
+    reference = "https://www.dor.ms.gov/sites/default/files/Forms/Individual/80100231.pdf#page=3"
+
+    def formula(tax_unit, period, parameters):
+        p = parameters(
+            period
+        ).gov.states.ms.tax.income.credits.charitable_contribution
+
+        foster_care_contributions = tax_unit(
+            "ms_charitable_contributions_to_qualifying_foster_care_organizations",
+            period,
+        )
+        filing_status = tax_unit("filing_status", period)
+
+        capped_foster_care_contributions = min_(
+            foster_care_contributions, p.qualifying_foster[filing_status]
+        )
+        return capped_foster_care_contributions
