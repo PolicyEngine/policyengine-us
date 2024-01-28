9f313a9bcb7ba688f655a38fa703185dede4c41a
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2024-01-28 17:25:26 +0000

    Fixing formatting

--- policyengine_us/variables/gov/states/ct/tax/income/subtractions/ct_social_security_benefit_adjustment.py
+++ policyengine_us/variables/gov/states/ct/tax/income/subtractions/ct_social_security_benefit_adjustment.py
@@ -16,7 +16,9 @@ class ct_social_security_benefit_adjustment(Variable):
     defined_for = StateCode.CT
 
     def formula(tax_unit, period, parameters):
-        p = parameters(period).gov.states.ct.tax.income.subtractions.social_security
+        p = parameters(
+            period
+        ).gov.states.ct.tax.income.subtractions.social_security
         filing_status = tax_unit("filing_status", period)
         # Line 41, Part A and Part B
         us_taxable_ss = tax_unit("tax_unit_taxable_social_security", period)
@@ -34,9 +36,7 @@ class ct_social_security_benefit_adjustment(Variable):
         # Line 41, Part E and Part F
         # Difference between taxable social security benefits and capped social security fraction
         adjusted_ss_benefit = max_(us_taxable_ss - capped_ss_fraction, 0)
-        reduction_threshold = p.reduction_threshold[
-            filing_status
-        ]
+        reduction_threshold = p.reduction_threshold[filing_status]
         # Adjustment determined based on AGI amount compared to reduction threshold
         agi_under_reduction_threshold = agi < reduction_threshold
         return where(
