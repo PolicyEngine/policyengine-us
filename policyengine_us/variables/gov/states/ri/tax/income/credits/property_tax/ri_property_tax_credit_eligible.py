20b34426dbcea4dea0229853df8e8ecff7a3fc88
PavelMakarchuk <pavel.ma99@gmail.com>
2024-01-30 01:30:48 +0100

    format

--- policyengine_us/variables/gov/states/ri/tax/income/credits/property_tax/ri_property_tax_credit_eligible.py
+++ policyengine_us/variables/gov/states/ri/tax/income/credits/property_tax/ri_property_tax_credit_eligible.py
@@ -7,7 +7,8 @@ class ri_property_tax_credit_eligible(Variable):
     label = "Rhode Island property tax credit eligibility status"
     definition_period = YEAR
     reference = (
-        "http://webserver.rilin.state.ri.us/Statutes/TITLE44/44-33/44-33-3.htm"
+        "http://webserver.rilin.state.ri.us/Statutes/TITLE44/44-33/44-33-3.htm",  # (1) & (2) determines age and disability eligibility
+        "https://tax.ri.gov/sites/g/files/xkgbur541/files/2022-01/2021-ri-1040h_w.pdf#page=1",
     )
     defined_for = StateCode.RI
 
@@ -21,9 +22,11 @@ class ri_property_tax_credit_eligible(Variable):
         # disability eligibility
         head_is_disabled = tax_unit("head_is_disabled", period)
         spouse_is_disabled = tax_unit("spouse_is_disabled", period)
-        is_disabled = head_is_disabled | spouse_is_disabled
+        head_or_spouse_disabled = head_is_disabled | spouse_is_disabled
         household_income = tax_unit("ri_property_tax_household_income", period)
-        household_income_eligible = (
-            household_income <= p.rate.one_person.thresholds[-1]
-        )
-        return (age_eligible | is_disabled) & household_income_eligible
+        income_threshold = p.rate.one_person.thresholds[-1]
+        # The tax form RI-1040H specifies the income of a household as a eligibility requirement
+        household_income_eligible = household_income <= income_threshold
+        return (
+            age_eligible | head_or_spouse_disabled
+        ) & household_income_eligible
