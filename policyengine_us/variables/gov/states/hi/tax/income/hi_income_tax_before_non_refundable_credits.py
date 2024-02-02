76d0240d5d42620bfc0b9f1bb98dddfbdea17d13
xianxinzhu <97819588+xianxinzhu@users.noreply.github.com>
2024-02-02 06:45:23 +0000

    Fixes hi conflict variable

--- policyengine_us/variables/gov/states/hi/tax/income/hi_income_tax_before_non_refundable_credits.py
+++ policyengine_us/variables/gov/states/hi/tax/income/hi_income_tax_before_non_refundable_credits.py
@@ -1,6 +1,5 @@
 from policyengine_us.model_api import *
 
-
 class hi_income_tax_before_non_refundable_credits(Variable):
     value_type = float
     entity = TaxUnit
@@ -10,7 +9,7 @@ class hi_income_tax_before_non_refundable_credits(Variable):
     definition_period = YEAR
     # Hawaii Tax Rate Schedules
     reference = " https://tax.hawaii.gov/forms/d_18table-on/d_18table-on_p13/"
-
+    
     def formula(tax_unit, period, parameters):
         filing_status = tax_unit("filing_status", period)
         taxable_income = tax_unit("hi_taxable_income", period)
@@ -18,19 +17,22 @@ class hi_income_tax_before_non_refundable_credits(Variable):
 
         statuses = filing_status.possible_values
 
-        return select(
+        income_tax = select(
             [
                 filing_status == statuses.SINGLE,
                 filing_status == statuses.SEPARATE,
-                filing_status == statuses.JOINT,
-                filing_status == statuses.WIDOW,
-                filing_status == statuses.HEAD_OF_HOUSEHOLD,
-            ],
-            [
-                p.single.calc(taxable_income),
-                p.separate.calc(taxable_income),
-                p.joint.calc(taxable_income),
-                p.widow.calc(taxable_income),
                 p.head_of_household.calc(taxable_income),
             ],
+        )
+        alternative_tax = tax_unit(
+            "hi_alternative_tax_on_capital_gains", period
+        )
+
+        alternative_tax_eligible = tax_unit(
+            "hi_alternative_tax_on_capital_gains_eligible", period
+        )
+        return where(
+            alternative_tax_eligible,
+            min_(income_tax, alternative_tax),
+            income_tax,
         )
