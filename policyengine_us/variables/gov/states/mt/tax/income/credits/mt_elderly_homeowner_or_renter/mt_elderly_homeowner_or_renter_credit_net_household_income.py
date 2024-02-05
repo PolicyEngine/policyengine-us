45e56adf6e108d0427a0045a2ffd8aa6d6765196
PavelMakarchuk <pavel.ma99@gmail.com>
2024-02-06 00:53:40 +0100

    partial split of elderly

--- policyengine_us/variables/gov/states/mt/tax/income/credits/mt_elderly_homeowner_or_renter/mt_elderly_homeowner_or_renter_credit_net_household_income.py
+++ policyengine_us/variables/gov/states/mt/tax/income/credits/mt_elderly_homeowner_or_renter/mt_elderly_homeowner_or_renter_credit_net_household_income.py
@@ -3,7 +3,7 @@ from policyengine_us.model_api import *
 
 class mt_elderly_homeowner_or_renter_credit_net_household_income(Variable):
     value_type = float
-    entity = TaxUnit
+    entity = Person
     label = (
         "Net household income for Montana elderly homeowner or renter credit"
     )
@@ -11,14 +11,14 @@ class mt_elderly_homeowner_or_renter_credit_net_household_income(Variable):
     definition_period = YEAR
     defined_for = "mt_elderly_homeowner_or_renter_credit_eligible"
 
-    def formula(tax_unit, period, parameters):
+    def formula(person, period, parameters):
         p = parameters(
             period
         ).gov.states.mt.tax.income.credits.elderly_homeowner_or_renter
         # Only one claim is allowed per household
         # married taxpayer who are living apart may qualify for only one credit per year
         standard_exclusion = p.net_household_income.standard_exclusion
-        gross_household_income = tax_unit(
+        gross_household_income = person(
             "mt_elderly_homeowner_or_renter_credit_gross_household_income",
             period,
         )
