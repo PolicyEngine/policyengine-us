81d870b4dd9ecd642a4075605a99b6268ecd2bd5
Matty <31402450+Mshuning@users.noreply.github.com>
2024-02-06 14:40:29 +0000

    label

--- policyengine_us/variables/gov/states/mi/tax/income/mi_is_senior_for_tax.py
+++ policyengine_us/variables/gov/states/mi/tax/income/mi_is_senior_for_tax.py
@@ -4,10 +4,9 @@ from policyengine_us.model_api import *
 class mi_is_senior_for_tax(Variable):
     value_type = bool
     entity = TaxUnit
-    label = "Michigan filer is senior"
-    unit = USD
+    label = "Michigan filer is a senior"
     definition_period = YEAR
-    reference = ("http://legislature.mi.gov/doc.aspx?mcl-206-514",)
+    reference = "http://legislature.mi.gov/doc.aspx?mcl-206-514"
     defined_for = StateCode.MI
 
     def formula(tax_unit, period, parameters):
