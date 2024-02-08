30f1c534c8d25e3be27a2e45ef3e3013048d404c
Anthony V <anth.volk@gmail.com>
2024-02-08 22:55:26 +0200

    Update policyengine_us/variables/household/income/household/household_refundable_tax_credits.py
    Co-authored-by: Max Ghenis <mghenis@gmail.com>

--- policyengine_us/variables/household/income/household/household_refundable_tax_credits.py
+++ policyengine_us/variables/household/income/household/household_refundable_tax_credits.py
@@ -9,7 +9,7 @@ class household_refundable_tax_credits(Variable):
     unit = USD
 
     def formula(household, period, parameters):
-        params = parameters(period)
+        p = parameters(period)
         added_components = params.gov.household_refundable_credits
         p = params.gov.contrib.ubi_center.flat_tax
         if p.abolish_federal_income_tax:
