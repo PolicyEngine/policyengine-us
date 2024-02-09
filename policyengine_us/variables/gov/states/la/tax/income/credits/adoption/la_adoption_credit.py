475d1fe6a02ab6b33139f850942abdd9ab2b967c
yy3187 <129118329+yy3187@users.noreply.github.com>
2024-02-09 13:42:10 +0000

    update variables and parameters

--- policyengine_us/variables/gov/states/la/tax/income/credits/adoption/la_adoption_credit.py
+++ policyengine_us/variables/gov/states/la/tax/income/credits/adoption/la_adoption_credit.py
@@ -3,7 +3,7 @@ from policyengine_us.model_api import *
 
 class la_adoption_credit(Variable):
     value_type = float
-    entity = Person
+    entity = TaxUnit
     label = "Louisiana adoption credit"
     reference = "https://legis.la.gov/legis/Law.aspx?d=1336834"
     unit = USD
