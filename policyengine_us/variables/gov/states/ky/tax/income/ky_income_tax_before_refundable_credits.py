29b33c6f708d30f1e7331b6ff780e97fb2edeb41
Ruby Ren <38923159+YichunRen@users.noreply.github.com>
2024-02-12 19:59:15 +0000

    ky_income_tax_before_refundable_credits

--- policyengine_us/variables/gov/states/ky/tax/income/ky_income_tax_before_refundable_credits.py
+++ policyengine_us/variables/gov/states/ky/tax/income/ky_income_tax_before_refundable_credits.py
@@ -13,5 +13,18 @@ class ky_income_tax_before_refundable_credits(Variable):
         tax_before_non_refundable = tax_unit(
             "ky_income_tax_before_non_refundable_credits", period
         )
-        non_refundable_credits = tax_unit("ky_non_refundable_credits", period)
+        ky_files_separately = tax_unit("ky_files_separately", period)
+        person = tax_unit.members
+        ky_personal_tax_credits = where(
+            ky_files_separately,
+            person("ky_personal_tax_credits_indiv", period),
+            person("ky_personal_tax_credits_joint", period),
+        )
+        other_non_refundable_credits = tax_unit(
+            "ky_non_refundable_credits", period
+        )
+        non_refundable_credits = other_non_refundable_credits + tax_unit.sum(
+            ky_personal_tax_credits
+        )
+
         return max_(tax_before_non_refundable - non_refundable_credits, 0)
