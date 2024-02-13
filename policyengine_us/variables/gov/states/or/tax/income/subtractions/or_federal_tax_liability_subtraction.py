7c0650a5277e1279d349d9df94e6ee2f11b95127
Kingsley Yang <87620347+Shinuing@users.noreply.github.com>
2024-02-13 19:06:54 +0000

    remore self_employment_tax

--- policyengine_us/variables/gov/states/or/tax/income/subtractions/or_federal_tax_liability_subtraction.py
+++ policyengine_us/variables/gov/states/or/tax/income/subtractions/or_federal_tax_liability_subtraction.py
@@ -14,45 +14,14 @@ class or_federal_tax_liability_subtraction(Variable):
     defined_for = StateCode.OR
 
     def formula(tax_unit, period, parameters):
-        # Federal tax liability
+        # calculate Oregon concept of federal income tax
         federal_itax = tax_unit("income_tax", period)
-        # child and dependent tax credit, excluding 2022 and on
-        non_refundable_ctc = tax_unit("non_refundable_ctc", period)
-        refundable_ctc = tax_unit("refundable_ctc", period)
-        cdcc = tax_unit("cdcc", period)
-
-        year = period.start.year
-        if year >= 2022:
-            non_refundable_ctc = refundable_ctc = cdcc = 0
-
-        # American opportunity credit
-        american_opportunity_credit = tax_unit(
-            "american_opportunity_credit", period
-        )
-        # recovery rebate credit
-        recovery_rebate_credit = tax_unit("recovery_rebate_credit", period)
-        # Premium tax credit
-        premium_tax_credit = tax_unit("premium_tax_credit", period)
-
-        federal_itax_inclusive = federal_itax + non_refundable_ctc
-        other_taxes_exclusive = (
-            refundable_ctc
-            + american_opportunity_credit
-            + recovery_rebate_credit
-            + premium_tax_credit
-            + cdcc
-        )
-        # Other taxes and any additions to tax - not modelled here
-        # Federal economic stimulus payments - not modelled here
-        # Excess advance premium tax credit - not modelled here
-        or_federal_income_tax = max_(
-            0, federal_itax_inclusive - other_taxes_exclusive
-        )
-
+        federal_eitc = tax_unit("eitc", period)
+        or_federal_income_tax = max_(0, federal_itax + federal_eitc)
         # limit subtraction based on caps scaled to federal AGI
         filing_status = tax_unit("filing_status", period)
         status = filing_status.possible_values
-        caps = (
+        p = (
             parameters(period)
             .gov.states["or"]
             .tax.income.subtractions.federal_tax_liability.cap
@@ -67,11 +36,11 @@ class or_federal_tax_liability_subtraction(Variable):
                 filing_status == status.WIDOW,
             ],
             [
-                caps.single.calc(federal_agi),
-                caps.joint.calc(federal_agi),
-                caps.head_of_household.calc(federal_agi),
-                caps.separate.calc(federal_agi),
-                caps.widow.calc(federal_agi),
+                p.single.calc(federal_agi),
+                p.joint.calc(federal_agi),
+                p.head_of_household.calc(federal_agi),
+                p.separate.calc(federal_agi),
+                p.widow.calc(federal_agi),
             ],
         )
         return min_(or_federal_income_tax, cap)
