5210e2a83495e2f92015731d2623262545512f6f
PavelMakarchuk <pavel.ma99@gmail.com>
2024-02-15 19:18:16 +0100

    Reduce the `adjusted_net_capital_gain` by the qualified dividend income in the `vt_capital_gains_exclusion` calcualtion Fixes #3900

--- policyengine_us/variables/gov/states/vt/tax/income/adjusted_gross_income/subtractions/vt_capital_gain_exclusion/vt_percentage_capital_gains_exclusion.py
+++ policyengine_us/variables/gov/states/vt/tax/income/adjusted_gross_income/subtractions/vt_capital_gain_exclusion/vt_percentage_capital_gains_exclusion.py
@@ -20,10 +20,16 @@ class vt_percentage_capital_gains_exclusion(Variable):
         adjusted_net_capital_gain = tax_unit(
             "adjusted_net_capital_gain", period
         )
+        dividend_income = add(tax_unit, period, ["qualified_dividend_income"])
+        reduced_adjusted_net_capital_gain = max_(
+            adjusted_net_capital_gain - dividend_income, 0
+        )
         p = parameters(
             period
         ).gov.states.vt.tax.income.agi.exclusions.capital_gain
         # The percentage exclusion equals to a percentage of
         # the adjusted net capital gain and has a maximum value
-        percentage_exclusion = adjusted_net_capital_gain * p.percentage.rate
+        percentage_exclusion = (
+            reduced_adjusted_net_capital_gain * p.percentage.rate
+        )
         return min_(percentage_exclusion, p.percentage.cap)
