5d56995bb4b69a7a76a674e961d23a3bcc1242c2
PavelMakarchuk <pavel.ma99@gmail.com>
2024-02-16 17:57:33 +0100

    split deductions

--- policyengine_us/variables/gov/states/mt/tax/income/mt_taxable_income_indiv.py
+++ policyengine_us/variables/gov/states/mt/tax/income/mt_taxable_income_indiv.py
@@ -15,19 +15,41 @@ class mt_taxable_income_indiv(Variable):
 
     def formula(person, period, parameters):
         mt_agi = person("mt_agi", period)
-
-        standard_deduction = person("mt_standard_deduction_indiv", period)
-        itemized_deductions = person("mt_itemized_deductions_indiv", period)
+        standard_deduction = add(
+            person.tax_unit, period, ["mt_standard_deduction_indiv"]
+        )
+        itemized_deductions = add(
+            person.tax_unit, period, ["mt_itemized_deductions_indiv"]
+        )
         # Tax units can claim the larger of the itemized or standard deductions
-        deductions = max_(itemized_deductions, standard_deduction)
-        exemptions = person("mt_exemptions_indiv", period)
+        itemizes = person.tax_unit("mt_tax_unit_itemizes", period)
+        deductions = where(itemizes, itemized_deductions, standard_deduction)
+        exemptions = add(person.tax_unit, period, ["mt_exemptions_indiv"])
         deductions_and_exemptions = deductions + exemptions
-        # allocata the deduction and exemption amounts based on the difference between and spouse AGI
-        head_agi = person("is_tax_unit_head", period) * mt_agi
-        spouse_agi = person("is_tax_unit_spouse", period) * mt_agi
-        head_over_spouse_agi = head_agi > spouse_agi
-        difference = min_(abs(head_agi - spouse_agi), deductions_and_exemptions)
-        halved_excess_deduction = max_(deductions_and_exemptions - difference, 0) / 2
-        head_reduced_agi = head_agi - halved_excess_deduction
-        spouse_reduced_agi = spouse_agi - halved_excess_deduction
-        return where(head_over_spouse_agi, max_(head_reduced_agi - difference, 0), max_(spouse_reduced_agi - difference, 0))
+        # Allocate the exemptions and deductions based on the following process:
+        # 1. Determine whether the head or spouse AGI is larger
+        head = person("is_tax_unit_head", period)
+        spouse = person("is_tax_unit_spouse", period)
+        head_agi = mt_agi * head
+        spouse_agi = mt_agi * spouse
+        spouse_agi_attributed_to_head = person.tax_unit.sum(spouse_agi) * head
+        head_over_spouse_agi = head_agi > spouse_agi_attributed_to_head
+        # 2. Determine the difference between the two AGIs and cap the deductions at that amount
+        difference = person.tax_unit.sum(
+            abs(head_agi - spouse_agi_attributed_to_head)
+        )
+        deductions_capped_at_agi_difference = min_(
+            difference, deductions_and_exemptions
+        )
+        # 3. Halve the remaining deductions amount
+        halved_capped_deductions = (
+            (deductions_and_exemptions - deductions_capped_at_agi_difference)
+            * 0.5 ) 
+        halved_deductions_allocated_to_head_or_spouse = halved_capped_deductions * (head | spouse)
+        # 4. Allocate the difference to the higher AGI and the halved deductions to each head and spouse
+        difference_applied_to_larger_agi = (
+            deductions_capped_at_agi_difference
+            * where(head_over_spouse_agi, head, spouse)
+        )
+        reduced_agi = mt_agi - difference_applied_to_larger_agi - halved_deductions_allocated_to_head_or_spouse
+        return max_(reduced_agi, 0)
