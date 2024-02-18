bfffb026884f54622b25cc5eec5b1a8f11355858
PavelMakarchuk <pavel.ma99@gmail.com>
2024-02-19 00:42:13 +0100

    reference

--- policyengine_us/variables/gov/states/id/tax/income/subtractions/household_and_dependent_care/id_cdcc_limit.py
+++ policyengine_us/variables/gov/states/id/tax/income/subtractions/household_and_dependent_care/id_cdcc_limit.py
@@ -7,11 +7,12 @@ class id_cdcc_limit(Variable):
     label = "Federal CDCC-relevant care expense limit for Idaho tax purposes"
     unit = USD
     definition_period = YEAR
+    reference = "https://tax.idaho.gov/governance/statutes/irc/"
     defined_for = StateCode.ID
 
     def formula(tax_unit, period, parameters):
         if period.start.year == 2021:
-            instant_str = f"2022-01-01"
+            instant_str = f"2020-01-01"
         else:
             instant_str = period
         p = parameters(instant_str).gov.irs.credits.cdcc
