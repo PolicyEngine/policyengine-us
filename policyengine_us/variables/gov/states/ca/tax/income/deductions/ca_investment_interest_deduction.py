477a57e05f4f2dd01d3dd520ae66f511ba5a3334
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2024-02-19 14:16:06 +0000

    Changing net investment income

--- policyengine_us/variables/gov/states/ca/tax/income/deductions/ca_investment_interest_deduction.py
+++ policyengine_us/variables/gov/states/ca/tax/income/deductions/ca_investment_interest_deduction.py
@@ -17,11 +17,12 @@ class ca_investment_interest_deduction(Variable):
             tax_unit, period, ["investment_interest_expense"]
         )
         # Line 5
-        investment_expenses = add(tax_unit, period, ["investment_expenses"])
+        # investment_expenses = add(tax_unit, period, ["investment_expenses"])
         # Line 6
-        net_investment_income = (
-            investment_interest_expense - investment_expenses
-        )
+        # net_investment_income = (
+        #     investment_interest_expense - investment_expenses
+        # )
+        net_investment_income = tax_unit("net_investment_income", period)
         # Line 8
         investment_interest_expense_deduction = min_(
             investment_interest_expense, net_investment_income
