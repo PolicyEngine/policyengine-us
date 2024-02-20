5601e217ad6f82e69715e4be89420cc2675f8f32
PavelMakarchuk <pavel.ma99@gmail.com>
2024-02-20 03:24:26 +0100

    Adjust the OH retirement credit eligiblity and applicable pension income Fixes #3958

--- policyengine_us/variables/gov/states/oh/tax/income/credits/retirement_income/pension_based/oh_pension_based_retirement_income_credit_eligible.py
+++ policyengine_us/variables/gov/states/oh/tax/income/credits/retirement_income/pension_based/oh_pension_based_retirement_income_credit_eligible.py
@@ -15,4 +15,6 @@ class oh_pension_based_retirement_income_credit_eligible(Variable):
         ).gov.states.oh.tax.income.credits.retirement.pension_based
 
         agi = tax_unit("oh_agi", period)
-        return agi < p.income_limit
+        exemptions = tax_unit("oh_personal_exemptions", period)
+        reduced_agi = max_(0, agi - exemptions)
+        return reduced_agi < p.income_limit
