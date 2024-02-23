de182ffa7cfa3f8ca0b153f145107dd86f0ffc7f
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2024-02-23 01:57:07 +0000

    Using new variable file

--- policyengine_us/variables/gov/states/ca/tax/income/deductions/ca_investment_interest_deduction.py
+++ policyengine_us/variables/gov/states/ca/tax/income/deductions/ca_investment_interest_deduction.py
@@ -9,3 +9,26 @@ class ca_investment_interest_deduction(Variable):
     documentation = "https://www.ftb.ca.gov/forms/2021/2021-3526.pdf"
     definition_period = YEAR
     defined_for = StateCode.CA
+
+    def formula(tax_unit, period, parameters):
+        # Lines 2, 3, 4a, 4b, 4c, 4d, 4e, 4f, 7 from tax form not included in calculations
+        # Line 1
+        investment_interest_expense = add(
+            tax_unit, period, ["investment_interest_expense"]
+        )
+        # Line 5
+        investment_expenses = add(tax_unit, period, ["investment_expenses"])
+        # Line 6
+        net_investment_income = (
+            investment_interest_expense - investment_expenses
+        )
+        # Line 8
+        investment_interest_expense_deduction = min_(
+            investment_interest_expense, net_investment_income
+        )
+        # Line 9
+        form_4952_amount = tax_unit("investment_income_form_4952", period)
+        # Line 10
+        return np.absolute(
+            form_4952_amount - investment_interest_expense_deduction
+        )
