6bf25113abce356a3afc2a33b3b3020d586e6cba
XZ-mabel <131919825+XZ-mabel@users.noreply.github.com>
2024-02-26 19:47:42 +0000

    Changed eitc_eligible back to original Fixes #3397

--- policyengine_us/variables/gov/states/ca/tax/income/credits/earned_income/ca_eitc_eligible.py
+++ policyengine_us/variables/gov/states/ca/tax/income/credits/earned_income/ca_eitc_eligible.py
@@ -10,18 +10,24 @@ class ca_eitc_eligible(Variable):
     defined_for = StateCode.CA
 
     def formula(tax_unit, period, parameters):
-        p = parameters(period).gov.states.ca.tax.income.credits.earned_income
         person = tax_unit.members
+        p = parameters(period).gov.states.ca.tax.income.credits.earned_income
+
         age = person("age", period)
         is_dependent = person("is_tax_unit_dependent", period)
-        age_eligible = (age >= p.eligibility.age.min) & (
-            age <= p.eligibility.age.max
+
+        meets_age_requirements = tax_unit.any(
+            (age >= p.eligibility.age.min)
+            & (age <= p.eligibility.age.max)
+            & ~is_dependent
         )
-        meets_age_requirements = tax_unit.any(age_eligible & ~is_dependent)
+
         eitc_investment_income = tax_unit(
             "eitc_relevant_investment_income", period
         )
-        investment_income_eligible = (
+
+        meets_investment_income_requirements = (
             eitc_investment_income <= p.eligibility.max_investment_income
         )
-        return meets_age_requirements & investment_income_eligible
+
+        return meets_age_requirements & meets_investment_income_requirements
