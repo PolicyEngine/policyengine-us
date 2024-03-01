f4ad5d0a4de00fceeb3a56514b469487777570af
XZ-mabel <131919825+XZ-mabel@users.noreply.github.com>
2024-03-01 07:16:17 +0000

    created ca_foster_youth_credit_person.py

--- policyengine_us/variables/gov/states/ca/tax/income/credits/foster_youth/ca_foster_youth_tax_credit.py
+++ policyengine_us/variables/gov/states/ca/tax/income/credits/foster_youth/ca_foster_youth_tax_credit.py
@@ -9,17 +9,5 @@ class ca_foster_youth_tax_credit(Variable):
     definition_period = YEAR
     defined_for = "ca_eitc_eligible"
 
-    def formula(tax_unit, period, parameters):
-        p = parameters(period).gov.states.ca.tax.income.credits.foster_youth
-        person = tax_unit.members
-        age = person("age", period)
-        eligible_person = person(
-            "ca_foster_youth_tax_credit_eligible_person", period
-        )
-        base_credit = p.amount.calc(age) * eligible_person
-        total_base_credit = tax_unit.sum(base_credit)
-        earned_income = tax_unit("tax_unit_earned_income", period)
-        excess_earned_income = max_(earned_income - p.phase_out.start, 0)
-        reduction_increments = excess_earned_income / p.phase_out.increment
-        reduction_amount = max_(0, reduction_increments * p.phase_out.amount)
-        return max_(0, total_base_credit - reduction_amount)
+    adds = ['ca_foster_youth_credit_person']
+
