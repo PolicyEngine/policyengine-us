7724bce441cf21fa679e903883ad9a147be42fd8
Kingsley Yang <87620347+Shinuing@users.noreply.github.com>
2024-03-01 16:56:42 +0000

    fix ks/ky withheld income tax

--- policyengine_us/variables/gov/states/ky/tax/income/ky_withheld_income_tax.py
+++ policyengine_us/variables/gov/states/ky/tax/income/ky_withheld_income_tax.py
@@ -11,9 +11,10 @@ class ky_withheld_income_tax(Variable):
 
     def formula(person, period, parameters):
         employment_income = person("irs_employment_income", period)
-        p = parameters(period).gov.states.ca.tax.income
-        standard_deduction = p.deductions.standard.amount["SINGLE"]
+        p = parameters(period).gov.states.ky.tax.income
+        # We apply the base standard deduction amount
+        standard_deduction = p.deductions.standard
         reduced_employment_income = max_(
             employment_income - standard_deduction, 0
         )
-        return p.rates.single.calc(reduced_employment_income)
+        return p.rate * reduced_employment_income
