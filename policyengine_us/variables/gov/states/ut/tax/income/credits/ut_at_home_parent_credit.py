990442940ef03ad888c531edec0727652b58740e
Matty <31402450+Mshuning@users.noreply.github.com>
2024-03-01 05:25:24 +0000

    one_parent_income_eligible

--- policyengine_us/variables/gov/states/ut/tax/income/credits/ut_at_home_parent_credit.py
+++ policyengine_us/variables/gov/states/ut/tax/income/credits/ut_at_home_parent_credit.py
@@ -7,7 +7,7 @@ class ut_at_home_parent_credit(Variable):
     label = "Utah at-home parent credit"
     unit = USD
     definition_period = YEAR
-    defined_for = "ut_at_home_parent_credit_eligible"
+    defined_for = "ut_at_home_parent_credit_agi_eligible"
     reference = (
         "https://le.utah.gov/xcode/Title59/Chapter10/59-10-S1005.html",
         "https://www.taxformfinder.org/forms/2021/2021-utah-tc-40-full-packet.pdf#page=23",
@@ -20,8 +20,9 @@ class ut_at_home_parent_credit(Variable):
         p = parameters(period).gov.states.ut.tax.income.credits.at_home_parent
         count_children = tax_unit.sum((age < p.max_child_age) & is_dependent)
 
-        is_joint = tax_unit("tax_unit_is_joint", period)
-        at_home_parent_credit = p.amount * count_children
-        return where(
-            is_joint, at_home_parent_credit * 2, at_home_parent_credit
+        one_parent_income_eligible = person(
+            "ut_at_home_parent_credit_one_parent_income_eligible", period
+        )
+        return tax_unit.sum(
+            p.amount * count_children * one_parent_income_eligible
         )
--- policyengine_us/variables/gov/states/ut/tax/income/credits/ut_at_home_parent_credit_one_parent_income_eligible.py
+++ policyengine_us/variables/gov/states/ut/tax/income/credits/ut_at_home_parent_credit_one_parent_income_eligible.py
@@ -1,10 +1,10 @@
 from policyengine_us.model_api import *
 
 
-class ut_at_home_parent_credit_eligible(Variable):
+class ut_at_home_parent_credit_one_parent_income_eligible(Variable):
     value_type = bool
-    entity = TaxUnit
-    label = "Eligible for the Utah at-home parent credit"
+    entity = Person
+    label = "Eligible for the Utah at-home parent credit income"
     definition_period = YEAR
     defined_for = StateCode.UT
     reference = (
@@ -12,18 +12,12 @@ class ut_at_home_parent_credit_eligible(Variable):
         "https://www.taxformfinder.org/forms/2021/2021-utah-tc-40-full-packet.pdf#page=23",
     )
 
-    def formula(tax_unit, period, parameters):
-        person = tax_unit.members
+    def formula(person, period, parameters):
         p = parameters(period).gov.states.ut.tax.income.credits.at_home_parent
 
         head_or_spouse_eligible = person("is_tax_unit_head_or_spouse", period)
         one_parent_income_eligible = (
             person("earned_income", period) <= p.parent_max_earnings
         )
-        employment_income_eligible = (
-            tax_unit.sum(head_or_spouse_eligible * one_parent_income_eligible)
-            > 0
-        )
-        agi_eligible = tax_unit("adjusted_gross_income", period) <= p.max_agi
 
-        return employment_income_eligible & agi_eligible
+        return (head_or_spouse_eligible * one_parent_income_eligible) > 0
