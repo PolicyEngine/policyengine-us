51cbb35dcb64a1626b3b9d12de19ab0bce6fdc30
Kingsley Yang <87620347+Shinuing@users.noreply.github.com>
2024-03-04 17:24:29 +0000

    resolve conversation

--- policyengine_us/variables/household/expense/utilities/has_heating_cooling_expense.py
+++ policyengine_us/variables/household/expense/utilities/has_heating_cooling_expense.py
@@ -8,8 +8,6 @@ class has_heating_cooling_expense(Variable):
     definition_period = YEAR
 
     def formula(spm_unit, period, parameters):
-        person = spm_unit.members
-        heating_expense = person("heating_expense", period)
-        cooling_expense = person("cooling_expense", period)
-        return spm_unit.sum(heating_expense + cooling_expense) > 0
-        # return add(spm_unit, period, ["heating_cooling_expense"]) > 0
+        return (
+            add(spm_unit, period, ["heating_expense", "cooling_expense"]) > 0
+        )
