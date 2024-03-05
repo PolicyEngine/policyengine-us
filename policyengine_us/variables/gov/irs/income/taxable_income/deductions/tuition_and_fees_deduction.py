3872261e6b10e6e6d6fac31360bf1d6b68efb135
Pinyan <129036923+pxu12@users.noreply.github.com>
2024-03-05 02:57:32 +0000

    minor change

--- policyengine_us/variables/gov/irs/income/taxable_income/deductions/tuition_and_fees_deduction.py
+++ policyengine_us/variables/gov/irs/income/taxable_income/deductions/tuition_and_fees_deduction.py
@@ -15,14 +15,9 @@ class tuition_and_fees_deduction(Variable):
         adjusted_gross_income = tax_unit("adjusted_gross_income", period)
         filing_status = tax_unit("filing_status", period)
         p = parameters(period).gov.irs.deductions.tuition_and_fees
-        limit = select(
-            [
-                filing_status == filing_status.possible_values.JOINT,
-                filing_status != filing_status.possible_values.JOINT,
-            ],
-            [
-                p.joint.calc(adjusted_gross_income),
-                p.non_joint.calc(adjusted_gross_income),
-            ],
+        cap = where(
+            filing_status == filing_status.possible_values.JOINT,
+            p.joint.calc(adjusted_gross_income),
+            p.non_joint.calc(adjusted_gross_income),
         )
-        return min_(qualified_tuition_expenses, limit)
+        return min_(qualified_tuition_expenses, cap)
