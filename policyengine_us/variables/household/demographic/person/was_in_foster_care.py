0ff44b6bcb9da4df27a8fad424fed076deed7091
XZ-mabel <131919825+XZ-mabel@users.noreply.github.com>
2024-03-05 00:29:04 -0500

    Update policyengine_us/variables/household/demographic/person/was_in_foster_care.py
    Co-authored-by: Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/household/demographic/person/was_in_foster_care.py
+++ policyengine_us/variables/household/demographic/person/was_in_foster_care.py
@@ -4,6 +4,6 @@ from policyengine_us.model_api import *
 class was_in_foster_care(Variable):
     value_type = bool
     entity = Person
-    label = "Person was in the a qualifying california foster care institution"
+    label = "Person was in the a qualifying foster care institution"
     definition_period = YEAR
     reference = "https://www.ftb.ca.gov/forms/2022/2022-3514.pdf#page=4"
