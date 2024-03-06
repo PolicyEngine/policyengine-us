7c427a114f27db21c81c2cc9a0618b8a2f7ab817
Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>
2024-03-06 18:16:50 -0500

    Limit `WIDOW` filing status to widowed head with child dependents (#4097)
    * Limit `WIDOW` filing status to widowed head with child dependents
    Fixes #4095
    
    * last test
    
    * notebook
    
    * add child dependent
    
    * test
    
    * make format
    
    * eliminate eitc qual. child
    
    * changelog
    
    * changelog

--- policyengine_us/variables/household/demographic/tax_unit/tax_unit_child_dependents.py
+++ policyengine_us/variables/household/demographic/tax_unit/tax_unit_child_dependents.py
@@ -1,11 +1,10 @@
 from policyengine_us.model_api import *
 
 
-class taxsim_dep18(Variable):
-    value_type = float
+class tax_unit_child_dependents(Variable):
+    value_type = int
     entity = TaxUnit
-    label = "Children under 13"
-    unit = USD
+    label = "Number of child dependents in the tax unit"
     definition_period = YEAR
 
     adds = ["is_child_dependent"]
