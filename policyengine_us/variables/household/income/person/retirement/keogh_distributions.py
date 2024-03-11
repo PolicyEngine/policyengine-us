be02425741b1487af93fbefabcae86dffa49f98f
Max Ghenis <mghenis@gmail.com>
2024-03-11 19:53:29 -0400

    Count retirement distributions as unearned income for SSI Fixes #4125

--- policyengine_us/variables/household/income/person/retirement/keogh_distributions.py
+++ policyengine_us/variables/household/income/person/retirement/keogh_distributions.py
@@ -7,3 +7,10 @@ class keogh_distributions(Variable):
     label = "KEOGH distributions"
     unit = USD
     definition_period = YEAR
+
+
+taxable_ira_distributions
+    - taxable_401k_distributions
+    - taxable_sep_distributions
+    - taxable_403b_distributions
+    - keogh_distributions # Keogh plans don't have a Roth option.
