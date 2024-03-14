f86c47f5196c93aefb7491aafb75ed0834813721
hua7450 <113824411+hua7450@users.noreply.github.com>
2024-03-14 03:20:43 +0000

    add eligible variable and test, make format

--- policyengine_us/variables/gov/irs/income/taxable_income/deductions/tuition_and_fees_deduction.py
+++ policyengine_us/variables/gov/irs/income/taxable_income/deductions/tuition_and_fees_deduction.py
@@ -6,6 +6,7 @@ class tuition_and_fees_deduction(Variable):
     entity = TaxUnit
     label = "Tuition and fees deduction"
     unit = USD
+    defined_for = "tuition_and_fees_deduction_eligible"
     definition_period = YEAR
     reference = (
         "https://www.irs.gov/pub/irs-pdf/f8917.pdf#page=2"
@@ -18,27 +19,12 @@ class tuition_and_fees_deduction(Variable):
             tax_unit, period, ["qualified_tuition_expenses"]
         )
         adjusted_gross_income = tax_unit("adjusted_gross_income", period)
-        # married filing separatly are not eligible for this deduction
-        filing_status = tax_unit("filing_status", period)
-        separate = filing_status == filing_status.possible_values.SEPARATE
-        # can't caim this deduction if the household has American oppportunity or lifetime learning credit
-        american_opportunity_credit = tax_unit(
-            "american_opportunity_credit", period
-        )
-        lifetime_learning_credit = tax_unit("lifetime_learning_credit", period)
-        has_take_the_american_opp_or_life_learn_credits = (
-            american_opportunity_credit + lifetime_learning_credit
-        ) > 0
         p = parameters(period).gov.irs.deductions.tuition_and_fees
+        filing_status = tax_unit("filing_status", period)
         joint = filing_status == filing_status.possible_values.JOINT
         cap = where(
             joint,
             p.joint.calc(adjusted_gross_income),
             p.non_joint.calc(adjusted_gross_income),
         )
-        capped_expenses = min_(qualified_tuition_expenses, cap)
-        return (
-            capped_expenses
-            * ~has_take_the_american_opp_or_life_learn_credits
-            * ~separate
-        )
+        return min_(qualified_tuition_expenses, cap)
