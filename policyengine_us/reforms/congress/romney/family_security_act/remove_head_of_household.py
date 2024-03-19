0fda3d9849a9bec2acdc95ed91c31b7ef7a08f3a
JudyZ13 <112495414+JudyZ13@users.noreply.github.com>
2024-03-19 18:11:02 +0000

    Co-authored-by: Pavel Makarchuk <PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/reforms/congress/romney/family_security_act/remove_head_of_household.py
+++ policyengine_us/reforms/congress/romney/family_security_act/remove_head_of_household.py
@@ -2,29 +2,6 @@ from policyengine_us.model_api import *
 
 
 def create_remove_head_of_household() -> Reform:
-    class filing_status(Variable):
-        value_type = Enum
-        entity = TaxUnit
-        possible_values = FilingStatus
-        default_value = FilingStatus.SINGLE
-        definition_period = YEAR
-        label = "Filing status"
-
-        def formula(tax_unit, period, parameters):
-            has_spouse = add(tax_unit, period, ["is_tax_unit_spouse"]) > 0
-            person = tax_unit.members
-            is_separated = tax_unit.any(person("is_separated", period))
-            is_widowed = tax_unit.any(person("is_widowed", period))
-            return select(
-                [has_spouse, is_separated, is_widowed],
-                [
-                    FilingStatus.JOINT,
-                    FilingStatus.SEPARATE,
-                    FilingStatus.SURVIVING_SPOUSE,
-                ],
-                default=FilingStatus.SINGLE,
-            )
-
     class reform(Reform):
         def apply(self):
             self.neutralize_variable("head_of_household_eligible")
