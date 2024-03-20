8f8116a0776dd640f07d3da8bab2011ed9cbd94b
PavelMakarchuk <pavel.ma99@gmail.com>
2024-03-19 23:29:23 -0400

    format

--- policyengine_us/variables/gov/states/id/tax/income/deductions/id_itemized_deductions.py
+++ policyengine_us/variables/gov/states/id/tax/income/deductions/id_itemized_deductions.py
@@ -7,11 +7,15 @@ class id_itemized_deductions(Variable):
     label = "Idaho itemized deductions"
     unit = USD
     definition_period = YEAR
-    reference = "https://tax.idaho.gov/wp-content/uploads/forms/EIN00046/EIN00046_03-01-2023.pdf#page=8"
+    reference = (
+        "https://tax.idaho.gov/wp-content/uploads/forms/EIN00046/EIN00046_03-01-2023.pdf#page=8",
+        "https://tax.idaho.gov/wp-content/uploads/forms/EFO00089/EFO00089_09-23-2021.pdf",
+    )
     defined_for = StateCode.ID
 
     def formula(tax_unit, period, parameters):
-        # Idaho reduces the federal itemized deductions by the amount of the SALT deduction
+        # Idaho reduces the federal itemized deductions
+        # by the amount of the SALT deduction
         id_salt_ded = tax_unit("id_salt_deduction", period)
         itemized_ded = tax_unit("itemized_taxable_income_deductions", period)
         return max_(itemized_ded - id_salt_ded, 0)
