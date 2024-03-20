8a67777fc0e14f25e1efae991115937a46fd38ad
PavelMakarchuk <pavel.ma99@gmail.com>
2024-03-20 16:13:51 -0400

    rename var

--- policyengine_us/variables/gov/states/id/tax/income/deductions/id_salt_deduction.py
+++ policyengine_us/variables/gov/states/id/tax/income/deductions/id_salt_deduction.py
@@ -11,7 +11,7 @@ class id_salt_deduction(Variable):
     defined_for = StateCode.ID
 
     def formula(tax_unit, period, parameters):
-        salt = tax_unit("state_and_local_sales_or_income_tax", period)
+        state_and_local_tax = tax_unit("state_and_local_sales_or_income_tax", period)
         # If the salt amount is above the federal cap, Idaho applies separate limitations
         filing_status = tax_unit("filing_status", period)
         cap = parameters(
@@ -20,7 +20,7 @@ class id_salt_deduction(Variable):
         # Federal Schedule A line 5b - State and local real estate taxes
         # Federal Schedule A line 5c - State and local personal property taxes
         real_estate_taxes = add(tax_unit, period, ["real_estate_taxes"])
-        salt_and_real_estate_tax = salt + real_estate_taxes
+        salt_and_real_estate_tax = state_and_local_tax + real_estate_taxes
         salt_above_cap = salt_and_real_estate_tax > cap
         reduced_cap = max_(cap - real_estate_taxes, 0)
-        return where(salt_above_cap, reduced_cap, salt)
+        return where(salt_above_cap, reduced_cap, state_and_local_tax)
