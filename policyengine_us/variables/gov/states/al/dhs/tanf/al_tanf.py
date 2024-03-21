6534cdd863bb24b9bdd7e0e2f780b90e5aa0f92c
PavelMakarchuk <pavel.ma99@gmail.com>
2024-03-21 19:08:11 -0400

    applicable income and part tanf

--- policyengine_us/variables/gov/states/al/dhs/tanf/al_tanf.py
+++ policyengine_us/variables/gov/states/al/dhs/tanf/al_tanf.py
@@ -1,1 +1,18 @@
+from policyengine_us.model_api import *
 
+class al_tanf(Variable):
+    value_type = float
+    entity = "SPMUnit"
+    label = "Alabama family assistance (TANF)"
+    unit: USD
+    defined_for = "al_tanf_eligible"
+    definition_period = YEAR
+    reference = "https://dhr.alabama.gov/wp-content/uploads/2023/10/DHR-FAD-595-Oct.23.pdf"
+
+    def formula(spm_unit, period, parameters):
+        # First check the family size through spm_unit_size
+
+        # Second check the payment standard based on the family size
+        p = parameters(period).gov.states.al.dhs.tanf
+        # Return payment standard
+        
--- policyengine_us/variables/gov/states/al/dhs/tanf/al_tanf_applicable_income.py
+++ policyengine_us/variables/gov/states/al/dhs/tanf/al_tanf_applicable_income.py
@@ -1,1 +1,13 @@
+from policyengine_us.model_api import *
 
+
+class al_tanf_applicable_income(Variable):
+    value_type = float
+    entity = SPMUnit
+    unit = USD
+    label = "Applicable income for the Alabama family assistance (TANF)"
+    defined_for = StateCode.AL
+    definition_period = YEAR
+    reference = "https://dhr.alabama.gov/wp-content/uploads/2023/10/DHR-FAD-595-Oct.23.pdf"
+
+    adds = "gov.states.al.dhs.tanf.income_sources"
