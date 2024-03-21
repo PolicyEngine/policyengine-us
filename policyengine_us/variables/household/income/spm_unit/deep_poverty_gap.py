894f523055affdf0f3a46c1cae8baef79ec77eba
SirMalamute <neeliyer14@gmail.com>
2024-03-20 22:18:46 -0400

    Update policyengine_us/variables/household/income/spm_unit/deep_poverty_gap.py
    Co-authored-by: Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/household/income/spm_unit/deep_poverty_gap.py
+++ policyengine_us/variables/household/income/spm_unit/deep_poverty_gap.py
@@ -12,6 +12,6 @@ class deep_poverty_gap(Variable):
     definition_period = YEAR
 
     def formula(spm_unit, period, parameters):
-        income = spm_unit("spm_resources", period)
+        resources = spm_unit("spm_resources", period)
         deep_poverty_threshold = spm_unit("deep_poverty_line", period)
         return max_(deep_poverty_threshold - income, 0)
