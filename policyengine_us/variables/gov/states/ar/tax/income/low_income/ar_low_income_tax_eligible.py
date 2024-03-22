d94cbe8a12f12898b4c24b7fb66df631090459ea
xianxinzhu <97819588+xianxinzhu@users.noreply.github.com>
2024-03-22 03:21:59 +0000

    Fixes variables and parameters

--- policyengine_us/variables/gov/states/ar/tax/income/low_income/ar_low_income_tax_eligible.py
+++ policyengine_us/variables/gov/states/ar/tax/income/low_income/ar_low_income_tax_eligible.py
@@ -20,27 +20,34 @@ class ar_low_income_tax_eligible(Variable):
         low_income_tax = select(
             [
                 filing_status == status.SINGLE,
-                (filing_status == status.HEAD_OF_HOUSEHOLD)
-                & (dependents <= 1),
-                (filing_status == status.HEAD_OF_HOUSEHOLD) & (dependents > 1),
+                filing_status == status.HEAD_OF_HOUSEHOLD,
                 filing_status == status.SEPARATE,
-                (filing_status == status.WIDOW) & (dependents <= 1),
-                (filing_status == status.WIDOW) & (dependents > 1),
-                (filing_status == status.JOINT) & (dependents <= 1),
-                (filing_status == status.JOINT) & (dependents > 1),
+                filing_status == status.WIDOW,
+                filing_status == status.JOINT,
             ],
             [
                 p.single.calc(agi, right=True),
-                p.head_of_household.no_or_one_dependent.calc(agi, right=True),
-                p.head_of_household.two_or_more_dependents.calc(
-                    agi, right=True
+                where(
+                    dependents <= 1,
+                    p.head_of_household.no_or_one_dependent.calc(
+                        agi, right=True
+                    ),
+                    p.head_of_household.two_or_more_dependents.calc(
+                        agi, right=True
+                    ),
                 ),
                 # Separate filers are ineligible to use the low income tax tables
                 np.inf,
-                p.widow.no_or_one_dependent.calc(agi, right=True),
-                p.widow.two_or_more_dependents.calc(agi, right=True),
-                p.joint.no_or_one_dependent.calc(agi, right=True),
-                p.joint.two_or_more_dependents.calc(agi, right=True),
+                where(
+                    dependents <= 1,
+                    p.widow.no_or_one_dependent.calc(agi, right=True),
+                    p.widow.two_or_more_dependents.calc(agi, right=True),
+                ),
+                where(
+                    dependents <= 1,
+                    p.joint.no_or_one_dependent.calc(agi, right=True),
+                    p.joint.two_or_more_dependents.calc(agi, right=True),
+                ),
             ],
             default=0,
         )
