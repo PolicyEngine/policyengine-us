2c1ec1f71196c7e267ea03fc85e9ac95ea3c8e0e
Max Ghenis <mghenis@gmail.com>
2024-03-22 07:10:14 -0400

    Add full-time college student from CPS fixes #4264

--- policyengine_us/data/datasets/cps/cps.py
+++ policyengine_us/data/datasets/cps/cps.py
@@ -205,6 +205,8 @@ def add_personal_variables(cps: h5py.File, person: DataFrame) -> None:
 
     cps["is_widowed"] = person.A_MARITL == 4
     cps["is_separated"] = person.A_MARITL == 6
+    # High school or college/university enrollment status.
+    cps["is_full_time_college_student"] = person.A_HSCOL == 2
 
 
 def add_personal_income_variables(
