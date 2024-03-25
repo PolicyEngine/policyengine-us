c18fa99920e43fc4133c46c2137d77c68d6e7f97
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2024-03-25 15:14:27 +0000

    Fix #4293 (#4294)

--- policyengine_us/variables/household/demographic/tax_unit/is_tax_unit_spouse.py
+++ policyengine_us/variables/household/demographic/tax_unit/is_tax_unit_spouse.py
@@ -1,5 +1,4 @@
 from policyengine_us.model_api import *
-from policyengine_core.populations import GroupPopulation, Population
 
 
 class is_tax_unit_spouse(Variable):
@@ -10,9 +9,10 @@ class is_tax_unit_spouse(Variable):
 
     def formula(person, period, parameters):
         # Only non-head adults can be spouses.
+        is_separated = person.tax_unit.any(person("is_separated", period))
         adult = ~person("is_child", period)
         head = person("is_tax_unit_head", period)
-        eligible = adult & ~head
+        eligible = adult & ~head & ~is_separated
         tax_unit = person.tax_unit
         age = person("age", period)
         return person.get_rank(tax_unit, -age, eligible) == 0
