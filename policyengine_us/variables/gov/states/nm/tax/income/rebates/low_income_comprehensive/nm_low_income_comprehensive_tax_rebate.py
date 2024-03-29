9b0fa12f05337851e6565206ee53c5aaddbc08fc
xianxinzhu <97819588+xianxinzhu@users.noreply.github.com>
2024-03-29 05:08:18 +0000

    Fixes variables and parameters

--- policyengine_us/variables/gov/states/nm/tax/income/rebates/low_income_comprehensive/nm_low_income_comprehensive_tax_rebate.py
+++ policyengine_us/variables/gov/states/nm/tax/income/rebates/low_income_comprehensive/nm_low_income_comprehensive_tax_rebate.py
@@ -35,12 +35,12 @@ class nm_low_income_comprehensive_tax_rebate(Variable):
                 exemptions >= 6,
             ],
             [
-                p.amount.one_exemption.calc(agi),
-                p.amount.two_exemptions.calc(agi),
-                p.amount.three_exemptions.calc(agi),
-                p.amount.four_exemptions.calc(agi),
-                p.amount.five_exemptions.calc(agi),
-                p.amount.six_exemptions.calc(agi),
+                p.amount.one_exemption.calc(agi, right=True),
+                p.amount.two_exemptions.calc(agi, right=True),
+                p.amount.three_exemptions.calc(agi, right=True),
+                p.amount.four_exemptions.calc(agi, right=True),
+                p.amount.five_exemptions.calc(agi, right=True),
+                p.amount.six_exemptions.calc(agi, right=True),
             ],
         )
 
