967d9ef74bb561d4fcc47a96e64732031d31b121
liwenxin491 <160153575+liwenxin491@users.noreply.github.com>
2024-04-04 15:32:37 +0000

    Update the earned income calculation

--- policyengine_us/variables/gov/states/az/hhs/tanf/az_hhs_tanf_earned_income.py
+++ policyengine_us/variables/gov/states/az/hhs/tanf/az_hhs_tanf_earned_income.py
@@ -10,18 +10,27 @@ class az_tanf_earned_income(Variable):
     def formula(spm_unit, period, parameters):
         # Earned income of the spm unit
         income = add(spm_unit, period, ["earned_income"])
-        # Childcare expenses
-        childcare = spm_unit("childcare_expenses", period)
-        # Get the age of the child
+        # Get the age of the child(children) and the disabled adult(s)
         person = spm_unit.members
-        child_age = person("age" , period)
-        # Disabled adult expenses
-        disabled_adult_care = spm_unit("care_expenses", period)
+        member_age = person("age" , period)
+        # Get the childcare and disabled adult care expenses
+        special_care_expense = spm_unit("care_expenses", period)
+        # Monthly care expense
+        monthly_care_expense = special_care_expense/MONTHS_IN_YEAR
+        initially_discounted_income = income - p.flat
         # Determine the expense discount
         p = parameters(period).gov.states.az.hhs.tanf.eligibility.income.earned
-        young_child = child_age < p.child_age
-        uncapped_expense_disregard = spm_unit.sum(where(young_child, p.younger, p.older))
-        capped_expense_disregard = min_(uncapped_expense_disregard, childcare)
+        
+        # Whether the kid is under the age threshold with higher income calculation discount
         # Determine the disabled adult care expense after discount
-        disabled_care_expense_discount = min_(disabled_adult_care, p.adult)
-        return (income - p.flat) * p.percentage - capped_expense_disregard - disabled_care_expense_discount
+        adult_discount_threshold=where(member_age > 18, p.adult , 0)
+        capped_adult_expense_disregard = min_(spm_unit.sum(monthly_care_expense), adult_discount_threshold)        
+
+        # Calculate capped disabled adult care expense
+        # use age eligibility variable later
+        eligible_child = person("az_tanf_eligible_child",period)
+        whether_young_child = member_age < p.child_age
+        uncapped_child_expnese_disregard = eligible_child * monthly_care_expense
+        child_discount_threshold = where(whether_young_child, p.younger, p.older)
+        capped_child_expense_disregard = min_(spm_unit.sum(uncapped_child_expnese_disregard), child_discount_threshold)
+        return  initially_discounted_income * p.percentage - capped_child_expense_disregard - capped_adult_expense_disregard
