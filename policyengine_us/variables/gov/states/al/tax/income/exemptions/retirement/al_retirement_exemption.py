44448dba47c968f8f89259df52f78cff30295a01
XZ-mabel <131919825+XZ-mabel@users.noreply.github.com>
2024-04-04 05:01:06 +0000

    Added alabama retirement exemptions

---
+++ policyengine_us/variables/gov/states/al/tax/income/exemptions/retirement/al_retirement_exemption.py
@@ -0,0 +1,16 @@
+from policyengine_us.model_api import *
+
+
+class al_retirement_exemption(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Alabama retirement exemption"
+    unit = USD
+    # The Code of Alabama Section 40-18-19 (a)(13).
+    documentation = "https://casetext.com/statute/code-of-alabama/title-40-revenue-and-taxation/chapter-18-income-taxes/article-1-general-provisions/section-40-18-19-exemptions-generally"
+    definition_period = YEAR
+    defined_for = StateCode.AL
+
+    adds = ["al_retirement_exemption_eligible_person"]
+
+
---
+++ policyengine_us/variables/gov/states/al/tax/income/exemptions/retirement/al_retirement_exemption_eligible_person.py
@@ -0,0 +1,20 @@
+from policyengine_us.model_api import *
+
+
+class al_retirement_exemption_eligible_person(Variable):
+    value_type = float
+    entity = Person
+    label = "Alabama retirement exemption"
+    unit = USD
+    # The Code of Alabama Section 40-18-19 (a)(13).
+    documentation = "https://casetext.com/statute/code-of-alabama/title-40-revenue-and-taxation/chapter-18-income-taxes/article-1-general-provisions/section-40-18-19-exemptions-generally"
+    definition_period = YEAR
+    defined_for = StateCode.AL
+
+    def formula(person, period, parameters):
+        p = parameters(period).gov.states.al.tax.income.exemptions.retirement
+        age = person("age",period)
+        pension_income = person("taxable_pension_income", period)
+        eligible_age = age>=p.age_threshold
+        head_or_spouse = person("is_tax_unit_head_or_spouse", period)
+        return min_(pension_income*eligible_age*head_or_spouse, p.max_amount)
