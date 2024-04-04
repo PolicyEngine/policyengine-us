add vehicles into formula Fixes #4218
Co-authored-by: Pinyan <pxu12@users.noreply.github.com>

policyengine_us/variables/gov/states/sc/tanf/eligibility/sc_tanf_resource_eligible.py
@@ -13,4 +13,16 @@ class sc_tanf_resource_eligible(Variable):
         countable_resources = add(
             spm_unit, period, ["sc_tanf_countable_resources"]
         )
-        return countable_resources <= resource_limit
+        # Each driver's license can exclude one vehicle.
+        adult_count = spm_unit("spm_unit_count_adults", period)
+        vehicle_count = spm_unit.household("household_vehicles_owned", period)
+        vehicle_value = spm_unit.household("household_vehicles_value", period)
+        avg_vehicle_value = np.zeros_like(vehicle_count)
+        mask = vehicle_count != 0
+        avg_vehicle_value[mask] = vehicle_value[mask] / vehicle_count[mask]
+        countable_vehicle_value = (
+            vehicle_count - adult_count
+        ) * avg_vehicle_value
+        return (
+            countable_resources + countable_vehicle_value
+        ) <= resource_limit

policyengine_us/variables/gov/states/sc/tanf/income/sc_tanf_net_income.py
@@ -34,5 +34,4 @@ class sc_tanf_net_income(Variable):
         )
         # G
         unearned_income = add(spm_unit, period, ["sc_tanf_unearned_income"])
-        net_income = unearned_income + net_earned_income
-        return np.round(net_income, 0)
+        return unearned_income + net_earned_income

policyengine_us/variables/gov/states/sc/tanf/sc_tanf.py
@@ -6,7 +6,7 @@ class sc_tanf(Variable):
     entity = SPMUnit
     label = "South Carolina TANF"
     unit = USD
-    definition_period = MONTH
+    definition_period = YEAR
     defined_for = "sc_tanf_eligible"
     reference = (
         "https://dss.sc.gov/media/3926/tanf_policy_manual_vol-60.pdf#page=131"
@@ -16,9 +16,9 @@ class sc_tanf(Variable):
         p = parameters(period).gov.states.sc.tanf
         # Compute need standard based on ferderal poverty guidlines
         fpg = spm_unit("sc_tanf_fpg", period)
-        need_standard = np.floor(fpg * p.income.need_standard.rate)
+        need_standard = fpg * p.income.need_standard.rate
         # G
         net_income = spm_unit("sc_tanf_net_income", period)
         # I
         excess_income = need_standard - net_income
-        return np.floor(excess_income * p.payment.rate)
+        return excess_income * p.payment.rate