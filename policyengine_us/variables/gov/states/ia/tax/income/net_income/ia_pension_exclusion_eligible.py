178d5165c850be327049b96387426bfed23dcead
yy3187 <129118329+yy3187@users.noreply.github.com>
2024-04-12 08:27:00 +0000

    update variable

--- policyengine_us/variables/gov/states/ia/tax/income/net_income/ia_pension_exclusion_eligible.py
+++ policyengine_us/variables/gov/states/ia/tax/income/net_income/ia_pension_exclusion_eligible.py
@@ -5,7 +5,6 @@ class ia_pension_exclusion_eligible(Variable):
     value_type = bool
     entity = Person
     label = "Eligible for the Iowa pension exclusion"
-    unit = USD
     definition_period = YEAR
     reference = (
         "https://tax.iowa.gov/sites/default/files/2023-01/2021%20Expanded%20Instructions_010323.pdf#page=26"
@@ -16,12 +15,12 @@ class ia_pension_exclusion_eligible(Variable):
     def formula(person, period, parameters):
         p = parameters(period).gov.states.ia.tax.income.pension_exclusion
         # determine eligibility for pension exclusion
-        is_head = person("is_tax_unit_head", period)
-        is_spouse = person("is_tax_unit_spouse", period)
+        is_head_or_spouse = person("is_tax_unit_head_or_spouse", period)
         # ... determine age eligibility
-        an_elder = person("age", period) >= p.minimum_age
-        is_elderly = (is_head & an_elder) | (is_spouse & an_elder)
+        age = person("age", period)
+        age_eligible = age >= p.minimum_age
+        head_or_spouse_age_eligible = is_head_or_spouse & age_eligible
         # ... determine disability eligiblity
-        has_disability = person("is_permanently_and_totally_disabled", period)
-        is_disabled = (is_head & has_disability) | (is_spouse & has_disability)
-        return is_elderly | is_disabled
+        is_disabled = person("is_permanently_and_totally_disabled", period)
+        is_head_or_spouse_disabled = is_head_or_spouse & is_disabled
+        return age_eligible | is_head_or_spouse_disabled
