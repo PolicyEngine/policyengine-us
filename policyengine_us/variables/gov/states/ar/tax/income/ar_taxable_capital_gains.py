13e98cd2184766ec302a7169e5558b9affa1165e
PavelMakarchuk <pavel.ma99@gmail.com>
2024-04-15 15:36:36 +0200

    Fix Arkansas capital gains calculations Fixes #4385

--- policyengine_us/variables/gov/states/ar/tax/income/ar_taxable_capital_gains.py
+++ policyengine_us/variables/gov/states/ar/tax/income/ar_taxable_capital_gains.py
@@ -6,15 +6,22 @@ class ar_taxable_capital_gains(Variable):
     entity = Person
     label = "Arkansas taxable capital gains"
     unit = USD
+
     definition_period = YEAR
     defined_for = StateCode.AR
 
     def formula(person, period, parameters):
-        long_term_capital_gains = person("long_term_capital_gains", period)
-        short_term_capital_gains = person("short_term_capital_gains", period)
+        net_lt_capital_gains = person("long_term_capital_gains", period)
+        net_st_capital_gains = person("short_term_capital_gains", period)
+        net_cap_gain = max_(0, net_lt_capital_gains - net_st_capital_gains)
         p = parameters(
             period
         ).gov.states.ar.tax.income.gross_income.capital_gains
+        capped_net_cap_gain = min_(net_cap_gain, p.cap)
+        taxable_capped_net_cap_gain = capped_net_cap_gain * (1 - p.exempt_rate)
+        st_capital_gain = max_(0, net_st_capital_gains)
+        taxable_capital_gain = st_capital_gain + taxable_capped_net_cap_gain
+
         taxable_long_term_capital_gains = long_term_capital_gains * (
             1 - p.exempt_rate
         )
