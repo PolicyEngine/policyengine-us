2c3a34dfa45897892eed8c8225af29bb7cd9329f
PavelMakarchuk <pavel.ma99@gmail.com>
2024-04-16 17:21:05 +0200

    testing

--- policyengine_us/variables/gov/states/ar/tax/income/ar_taxable_capital_gains.py
+++ policyengine_us/variables/gov/states/ar/tax/income/ar_taxable_capital_gains.py
@@ -6,23 +6,24 @@ class ar_taxable_capital_gains(Variable):
     entity = Person
     label = "Arkansas taxable capital gains"
     unit = USD
-
     definition_period = YEAR
     defined_for = StateCode.AR
 
     def formula(person, period, parameters):
         net_lt_capital_gains = person("long_term_capital_gains", period)
         net_st_capital_gains = person("short_term_capital_gains", period)
-        net_cap_gain = max_(0, net_lt_capital_gains - net_st_capital_gains)
+        # Can be capital loss
+        net_cap_gain = net_lt_capital_gains - net_st_capital_gains
         p = parameters(
             period
         ).gov.states.ar.tax.income.gross_income.capital_gains
-        capped_net_cap_gain = min_(net_cap_gain, p.cap)
-        taxable_capped_net_cap_gain = capped_net_cap_gain * (1 - p.exempt_rate)
+        capped_net_cap_gain = min_(net_cap_gain, p.exempt.cap)
+        taxable_capped_net_cap_gain = capped_net_cap_gain * (1 - p.exempt.rate)
         st_capital_gain = max_(0, net_st_capital_gains)
         taxable_capital_gain = st_capital_gain + taxable_capped_net_cap_gain
 
-        taxable_long_term_capital_gains = long_term_capital_gains * (
-            1 - p.exempt_rate
-        )
-        return short_term_capital_gains + taxable_long_term_capital_gains
+        capital_loss = taxable_capital_gain < 0
+        # Taxable capital loss is capped separately
+        filing_status = person.tax_unit("filing_status", period)
+        capped_capital_loss = max_(taxable_capital_gain, p.loss_cap[filing_status])
+        return where(capital_loss, capped_capital_loss, taxable_capital_gain)
