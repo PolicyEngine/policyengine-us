9ed0c9820afe5007d8c6a9411e76da584ad6e76b
Mohammed Ismail <mohammedismail523261@gmail.com>
2024-04-16 23:41:09 +0530

    changes for ne state

--- policyengine_us/variables/gov/states/ne/tax/income/ne_additions.py
+++ policyengine_us/variables/gov/states/ne/tax/income/ne_additions.py
@@ -1,7 +1,7 @@
 from policyengine_us.model_api import *
 
 
-class ne_agi_additions(Variable):
+class ne_additions(Variable):
     value_type = float
     entity = TaxUnit
     label = "NE AGI additions to federal AGI"
