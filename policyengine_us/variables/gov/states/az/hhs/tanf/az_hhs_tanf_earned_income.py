f376799e7094b1721d32d11f8dfcdc350858c38b
liwenxin491 <160153575+liwenxin491@users.noreply.github.com>
2024-04-16 16:07:41 +0000

    Finish the testing of earned income disregard and the earned income.py, working on the testing of earned income

--- policyengine_us/variables/gov/states/az/hhs/tanf/az_hhs_tanf_earned_income.py
+++ policyengine_us/variables/gov/states/az/hhs/tanf/az_hhs_tanf_earned_income.py
@@ -11,63 +11,13 @@ class az_tanf_earned_income(Variable):
     def formula(spm_unit, period, parameters):
         # Earned income of the spm unit
         income = add(spm_unit, period, ["earned_income"])
-        # Get the age of the child(children) and the disabled adult(s)
         person = spm_unit.members
         age = person("age", period)
-        # Get the childcare and disabled adult care expenses
-        care_expenses = spm_unit("childcare_expenses", period)
-
-        # Determine the total eligible disregard
-        # The eligibility reuquirements consider wither children or disabled adults
-        young_eligible_child = age < p.child_age
-        is_child = person("is_child", period)
-        disabled = person("is_disabled", period)
-        old_eligible_dependent = is_child | disabled
-        disregard_amount = select(
-            [
-                young_eligible_child,
-                old_eligible_dependent,
-            ],
-            [
-                p.younger,
-                p.older,
-            ],
-            default=0,
-        )
-        total_disregard = spm_unit.sum(disregard_amount)
-
-        # The disregard is capped at the expenses
-
-        capped_disregard = min_(care_expenses, total_disregard)
-
-        # Monthly care expense
-        monthly_care_expense = special_care_expense / MONTHS_IN_YEAR
-        initially_discounted_income = income - p.flat
         # Determine the expense discount
         p = parameters(period).gov.states.az.hhs.tanf.eligibility.income.earned
-
-        # Whether the kid is under the age threshold with higher income calculation discount
-        # Determine the disabled adult care expense after discount
-        adult_monthy_care_expense = where(age > 18, monthly_care_expense, 0)
-        adult_discount_threshold = p.adult
-        capped_adult_expense_disregard = min_(
-            spm_unit.sum(adult_monthy_care_expense), adult_discount_threshold
-        )
-
-        # Calculate capped disabled adult care expense
-        # use age eligibility variable later
-        eligible_child = person("az_tanf_eligible_child", period)
-        uncapped_child_expense_disregard = (
-            eligible_child * monthly_care_expense
-        )
-        capped_child_expense_disregard = min_(
-            spm_unit.sum(uncapped_child_expense_disregard),
-            child_discount_threshold,
-        )
-        monthly_discounted_earned_income = (
-            initially_discounted_income * p.percentage
-            - capped_child_expense_disregard
-            - capped_adult_expense_disregard
-        )
-        yearly_discounted_earned_income = monthly_discounted_earned_income * 12
-        return yearly_discounted_earned_income
+        # Income after subtracting constant value and certain percentage
+        after_subtracted_income = income - p.flat * MONTHS_IN_YEAR
+        after_discounted_income = after_subtracted_income * (1 - p.percentage)
+        #Calculate countable earned income by further subtracting earned income disregard 
+        countable_earned_income = max(after_discounted_income - 'az_hhs_tanf_earned_income_disregard',0)
+        return countable_earned_income
--- policyengine_us/variables/gov/states/az/hhs/tanf/az_hhs_tanf_earned_income_disregard.py
+++ policyengine_us/variables/gov/states/az/hhs/tanf/az_hhs_tanf_earned_income_disregard.py
@@ -5,11 +5,11 @@ class az_hhs_tanf_earned_income_disregard(Variable):
     value_type = float
     entity = SPMUnit
     label = "Disregard of the earned income for the Arizona Cash Assistance"
-    definition_period = MONTH
+    definition_period = YEAR
     defined_for = StateCode.AZ
 
     def formula(spm_unit, period, parameters):
-        p = parameters(period).gov.states.az.hhs.tanf.eligibility.income.earned
+        p = parameters(period).gov.states.az.hhs.tanf.eligibility.income.earned.care_expenses
         # Get the age of the child(children) and the disabled adult(s)
         person = spm_unit.members
         age = person("age", period)
@@ -19,7 +19,7 @@ class az_hhs_tanf_earned_income_disregard(Variable):
         # Determine the total eligible disregard
         # The eligibility reuquirements consider wither children or disabled adults
         young_eligible_child = age < p.child_age
-        is_child = person("is_child", period)
+        is_child = person("az_tanf_eligible_child", period)
         disabled_adult = person("is_disabled", period)
         disregard_amount = select(
             [
@@ -34,8 +34,7 @@ class az_hhs_tanf_earned_income_disregard(Variable):
             ],
             default=0,
         )
-        total_disregard = spm_unit.sum(disregard_amount)
+        total_disregard = spm_unit.sum(disregard_amount) * MONTHS_IN_YEAR
 
         # The disregard is capped at the expenses
-
         return min_(care_expenses, total_disregard)
