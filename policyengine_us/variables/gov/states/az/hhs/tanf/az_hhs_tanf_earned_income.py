907b3a6daf63713e8db8c8f3006ef171087b41f7
liwenxin491 <160153575+liwenxin491@users.noreply.github.com>
2024-04-16 16:30:59 +0000

    resolve several comments

--- policyengine_us/variables/gov/states/az/hhs/tanf/az_hhs_tanf_earned_income.py
+++ policyengine_us/variables/gov/states/az/hhs/tanf/az_hhs_tanf_earned_income.py
@@ -12,12 +12,11 @@ class az_tanf_earned_income(Variable):
         # Earned income of the spm unit
         income = add(spm_unit, period, ["earned_income"])
         person = spm_unit.members
-        age = person("age", period)
         # Determine the expense discount
         p = parameters(period).gov.states.az.hhs.tanf.eligibility.income.earned
         # Income after subtracting constant value and certain percentage
         after_subtracted_income = income - p.flat * MONTHS_IN_YEAR
         after_discounted_income = after_subtracted_income * (1 - p.percentage)
         #Calculate countable earned income by further subtracting earned income disregard 
-        countable_earned_income = max(after_discounted_income - 'az_hhs_tanf_earned_income_disregard',0)
+        countable_earned_income = max_(after_discounted_income - 'az_hhs_tanf_earned_income_disregard',0)
         return countable_earned_income
