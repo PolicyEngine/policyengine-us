33f8425d5e761026b6ce7e4393380ec34e9adb69
Mohammed Ismail <111173270+masterismail@users.noreply.github.com>
2024-04-17 11:50:15 +0530

    Update policyengine_us/variables/gov/states/or/tax/income/or_additions.py
    Co-authored-by: Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/states/or/tax/income/or_additions.py
+++ policyengine_us/variables/gov/states/or/tax/income/or_additions.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class or_additions(Variable):
     value_type = float
     entity = TaxUnit
-    label = "OR income additions"
+    label = "Oregon income additions"
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.OR
