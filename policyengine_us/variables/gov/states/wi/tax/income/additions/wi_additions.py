89b2827e16f5e60a883a4b089cb487a1bfc2d0d3
Mohammed Ismail <111173270+masterismail@users.noreply.github.com>
2024-04-17 11:50:22 +0530

    Update policyengine_us/variables/gov/states/wi/tax/income/additions/wi_additions.py
    Co-authored-by: Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/states/wi/tax/income/additions/wi_additions.py
+++ policyengine_us/variables/gov/states/wi/tax/income/additions/wi_additions.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class wi_additions(Variable):
     value_type = float
     entity = TaxUnit
-    label = "WI additions to federal adjusted gross income"
+    label = "Wisconsin additions to federal adjusted gross income"
     unit = USD
     definition_period = YEAR
     reference = (
