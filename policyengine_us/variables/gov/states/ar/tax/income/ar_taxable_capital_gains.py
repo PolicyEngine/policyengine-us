343450284c406097014710a1978fd43cb99891d5
PavelMakarchuk <pavel.ma99@gmail.com>
2024-04-18 17:06:47 +0200

    Reduce the Arkansas taxable long term capital gains by the short term capital losses Fixes #4413

--- policyengine_us/variables/gov/states/ar/tax/income/ar_taxable_capital_gains.py
+++ policyengine_us/variables/gov/states/ar/tax/income/ar_taxable_capital_gains.py
@@ -11,16 +11,23 @@ class ar_taxable_capital_gains(Variable):
 
     def formula(person, period, parameters):
         lt_capital_gains = person("long_term_capital_gains", period)
-        net_st_capital_gains = person("short_term_capital_gains", period)
+        st_capital_gains = person("short_term_capital_gains", period)
+        capital_loss = -st_capital_gains
+        has_capital_loss = capital_loss > 0
         # Can be capital loss
-        net_cap_gain = lt_capital_gains - net_st_capital_gains
+        reduced_lt_capital_gain = where(
+            has_capital_loss, lt_capital_gains - capital_loss, lt_capital_gains
+        )
         p = parameters(
             period
         ).gov.states.ar.tax.income.gross_income.capital_gains
-        capped_net_cap_gain = min_(net_cap_gain, p.exempt.cap)
+        capped_net_cap_gain = min_(reduced_lt_capital_gain, p.exempt.cap)
         taxable_capped_net_cap_gain = capped_net_cap_gain * (1 - p.exempt.rate)
-        st_capital_gain = max_(0, net_st_capital_gains)
-        taxable_capital_gain = st_capital_gain + taxable_capped_net_cap_gain
+        taxable_capital_gain = where(
+            has_capital_loss,
+            taxable_capped_net_cap_gain,
+            st_capital_gains + taxable_capped_net_cap_gain,
+        )
 
         taxable_capital_loss = -taxable_capital_gain
         has_taxable_capital_loss = taxable_capital_loss > 0
