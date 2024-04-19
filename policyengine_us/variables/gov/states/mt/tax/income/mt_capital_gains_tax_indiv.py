a48aea7d8b7231c9f2a095d04ff00d56c8ee033a
xianxinzhu <97819588+xianxinzhu@users.noreply.github.com>
2024-04-19 14:14:01 +0000

    Fixes variables and tests

--- policyengine_us/variables/gov/states/mt/tax/income/mt_capital_gains_tax_indiv.py
+++ policyengine_us/variables/gov/states/mt/tax/income/mt_capital_gains_tax_indiv.py
@@ -12,27 +12,36 @@ class mt_capital_gains_tax_indiv(Variable):
     def formula(person, period, parameters):
         capital_gains = person("long_term_capital_gains", period)
         taxable_income = person("mt_taxable_income_indiv", period)
-        nonqualified_income = taxable_income - capital_gains
+        nonqualified_income = max_(taxable_income - capital_gains, 0)
         p = parameters(period).gov.states.mt.tax.income.main.capital_gains
-        rate_p = parameters(
-            period
-        ).gov.states.mt.tax.income.main.capital_gains.rates.reduced_capital_gains
         filing_status = person.tax_unit(
             "state_filing_status_if_married_filing_separately_on_same_return",
             period,
         )
         gap = p.threshold[filing_status] - nonqualified_income
-        lower_base_tax = gap * rate_p.lower[filing_status]
+        lower_base_tax = (
+            gap * p.rates.reduced_capital_gains.lower[filing_status]
+        )
+        capital_gains_under_threshold = capital_gains <= gap
+        lower_capital_gains_tax = (
+            capital_gains * p.rates.reduced_capital_gains.lower[filing_status]
+        )
+        higher_capital_gains_tax = (
+            lower_base_tax
+            + (capital_gains - gap)
+            * p.rates.reduced_capital_gains.higher[filing_status]
+        )
         reduced_capital_gains_tax = where(
-            capital_gains <= gap,
-            capital_gains * rate_p.lower[filing_status],
-            (
-                lower_base_tax
-                + (capital_gains - gap) * rate_p.higher[filing_status]
-            ),
+            capital_gains_under_threshold,
+            lower_capital_gains_tax,
+            higher_capital_gains_tax,
+        )
+        nonqualified_income_over_threshold = gap < 0
+        capital_gains_over_threshold = (
+            capital_gains * p.rates.reduced_capital_gains.higher[filing_status]
         )
         return p.availability * where(
-            gap < 0,
-            capital_gains * rate_p.higher[filing_status],
+            nonqualified_income_over_threshold,
+            capital_gains_over_threshold,
             reduced_capital_gains_tax,
         )
--- policyengine_us/variables/gov/states/mt/tax/income/mt_capital_gains_tax_joint.py
+++ policyengine_us/variables/gov/states/mt/tax/income/mt_capital_gains_tax_joint.py
@@ -13,25 +13,34 @@ class mt_capital_gains_tax_joint(Variable):
         is_head = person("is_tax_unit_head", period)
         capital_gains = person("long_term_capital_gains", period)
         taxable_income = person("mt_taxable_income_joint", period)
-        nonqualified_income = taxable_income - capital_gains
+        nonqualified_income = max_(taxable_income - capital_gains, 0)
         p = parameters(period).gov.states.mt.tax.income.main.capital_gains
-        rate_p = parameters(
-            period
-        ).gov.states.mt.tax.income.main.capital_gains.rates.reduced_capital_gains
         filing_status = person.tax_unit("filing_status", period)
         gap = p.threshold[filing_status] - nonqualified_income
-        lower_base_tax = gap * rate_p.lower[filing_status]
+        lower_base_tax = (
+            gap * p.rates.reduced_capital_gains.lower[filing_status]
+        )
+        capital_gains_under_threshold = capital_gains <= gap
+        lower_capital_gains_tax = (
+            capital_gains * p.rates.reduced_capital_gains.lower[filing_status]
+        )
+        higher_capital_gains_tax = (
+            lower_base_tax
+            + (capital_gains - gap)
+            * p.rates.reduced_capital_gains.higher[filing_status]
+        )
         reduced_capital_gains_tax = where(
-            capital_gains <= gap,
-            capital_gains * rate_p.lower[filing_status],
-            (
-                lower_base_tax
-                + (capital_gains - gap) * rate_p.higher[filing_status]
-            ),
+            capital_gains_under_threshold,
+            lower_capital_gains_tax,
+            higher_capital_gains_tax,
+        )
+        nonqualified_income_over_threshold = gap < 0
+        capital_gains_over_threshold = (
+            capital_gains * p.rates.reduced_capital_gains.higher[filing_status]
         )
         capital_gains_tax = p.availability * where(
-            gap < 0,
-            capital_gains * rate_p.higher[filing_status],
+            nonqualified_income_over_threshold,
+            capital_gains_over_threshold,
             reduced_capital_gains_tax,
         )
         return is_head * person.tax_unit.sum(capital_gains_tax)
