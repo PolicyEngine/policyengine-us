39e4fde82bf75da274570b0ba82fb1a07727687b
PavelMakarchuk <pavel.ma99@gmail.com>
2024-04-22 19:15:53 +0200

    variable and first tests
    Co-authored-by: Yuan Tian <yuantian0204@users.noreply.github.com>

--- policyengine_us/variables/gov/states/ar/tax/income/credits/ar_additional_credit.py
+++ policyengine_us/variables/gov/states/ar/tax/income/credits/ar_additional_credit.py
@@ -1,16 +1,27 @@
 from policyengine_us.model_api import *
 
+
 class ar_additional_credit(Variable):
     value_type = float
-    entity = TaxUnit
+    entity = Person
     label = "Arkansas additional credit"
+    reference = "https://www.dfa.arkansas.gov/images/uploads/incomeTaxOffice/2023_AR1000F_and_AR1000NR_Instructions.pdf#page=26"
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.AR
 
     def formula(person, period, parameter):
-        taxable_income = add(tax_unit, period, ["ar_taxable_income_indiv"])
-        p = parameter(
-            period
-        ).gov.states.ar.tax.income.credits.additional
-        amount = p.amount
+        # Use individual or joint income based on filing status
+        filing_separately = person.tax_unit("ar_files_separately", period)
+        taxable_income_indiv = person("ar_taxable_income_indiv", period)
+        taxable_income_joint = person("ar_taxable_income_joint", period)
+        taxable_income = where(
+            filing_separately, taxable_income_indiv, taxable_income_joint
+        )
+        p = parameter(period).gov.states.ar.tax.income.credits.additional
+        # Only head or spouse can claim this credit
+        head_or_spouse = person("is_tax_unit_head_or_spouse", period)
+        # The credit amount is doubled for married couples filing jointly
+        amount = p.amount.calc(taxable_income)
+        total_amount = where(filing_separately, amount, amount * 2)
+        return total_amount * head_or_spouse
