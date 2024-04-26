e8e0b79fb626545828703185bde1279d6e609d52
xianxinzhu <97819588+xianxinzhu@users.noreply.github.com>
2024-04-26 01:51:49 +0000

    Fixes variables

--- policyengine_us/variables/gov/states/mt/tax/income/mt_income_tax_before_non_refundable_credits_indiv.py
+++ policyengine_us/variables/gov/states/mt/tax/income/mt_income_tax_before_non_refundable_credits_indiv.py
@@ -11,7 +11,7 @@ class mt_income_tax_before_non_refundable_credits_indiv(Variable):
 
     def formula(person, period, parameters):
         p = parameters(period).gov.states.mt.tax.income.main
-        regular_income = person("mt_taxable_income_indiv", period)
+        taxable_income = person("mt_taxable_income_indiv", period)
         filing_status = person.tax_unit(
             "state_filing_status_if_married_filing_separately_on_same_return",
             period,
@@ -19,35 +19,22 @@ class mt_income_tax_before_non_refundable_credits_indiv(Variable):
         status = filing_status.possible_values
         if p.capital_gains.availability:
             capital_gains = person("long_term_capital_gains", period)
-            taxable_income = max_(regular_income - capital_gains, 0)
+            taxable_income = max_(taxable_income - capital_gains, 0)
             capital_gains_tax = person("mt_capital_gains_tax_indiv", period)
-            regular_tax = select(
-                [
-                    filing_status == status.SINGLE,
-                    filing_status == status.HEAD_OF_HOUSEHOLD,
-                    filing_status == status.SEPARATE,
-                    filing_status == status.SURVIVING_SPOUSE,
-                ],
-                [
-                    p.single.calc(taxable_income),
-                    p.head_of_household.calc(taxable_income),
-                    p.separate.calc(taxable_income),
-                    p.widow.calc(taxable_income),
-                ],
-            )
-            return regular_tax + capital_gains_tax
         else:
-            return select(
-                [
-                    filing_status == status.SINGLE,
-                    filing_status == status.HEAD_OF_HOUSEHOLD,
-                    filing_status == status.SEPARATE,
-                    filing_status == status.SURVIVING_SPOUSE,
-                ],
-                [
-                    p.single.calc(regular_income),
-                    p.head_of_household.calc(regular_income),
-                    p.separate.calc(regular_income),
-                    p.widow.calc(regular_income),
-                ],
-            )
+            capital_gains_tax = 0
+        regular_tax = select(
+            [
+                filing_status == status.SINGLE,
+                filing_status == status.HEAD_OF_HOUSEHOLD,
+                filing_status == status.SEPARATE,
+                filing_status == status.SURVIVING_SPOUSE,
+            ],
+            [
+                p.single.calc(taxable_income),
+                p.head_of_household.calc(taxable_income),
+                p.separate.calc(taxable_income),
+                p.widow.calc(taxable_income),
+            ],
+        )
+        return regular_tax + capital_gains_tax
--- policyengine_us/variables/gov/states/mt/tax/income/mt_income_tax_before_non_refundable_credits_joint.py
+++ policyengine_us/variables/gov/states/mt/tax/income/mt_income_tax_before_non_refundable_credits_joint.py
@@ -11,47 +11,33 @@ class mt_income_tax_before_non_refundable_credits_joint(Variable):
 
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states.mt.tax.income.main
-        regular_income = add(tax_unit, period, ["mt_taxable_income_joint"])
+        taxable_income = add(tax_unit, period, ["mt_taxable_income_joint"])
         filing_status = tax_unit("filing_status", period)
         status = filing_status.possible_values
 
         if p.capital_gains.availability:
             capital_gains = add(tax_unit, period, ["long_term_capital_gains"])
-            taxable_income = max_(regular_income - capital_gains, 0)
+            taxable_income = max_(taxable_income - capital_gains, 0)
             capital_gains_tax = add(
                 tax_unit, period, ["mt_capital_gains_tax_joint"]
             )
-            regular_tax = select(
-                [
-                    filing_status == status.SINGLE,
-                    filing_status == status.JOINT,
-                    filing_status == status.HEAD_OF_HOUSEHOLD,
-                    filing_status == status.SEPARATE,
-                    filing_status == status.SURVIVING_SPOUSE,
-                ],
-                [
-                    p.single.calc(taxable_income),
-                    p.joint.calc(taxable_income),
-                    p.head_of_household.calc(taxable_income),
-                    p.separate.calc(taxable_income),
-                    p.widow.calc(taxable_income),
-                ],
-            )
-            return regular_tax + capital_gains_tax
         else:
-            return select(
-                [
-                    filing_status == status.SINGLE,
-                    filing_status == status.JOINT,
-                    filing_status == status.HEAD_OF_HOUSEHOLD,
-                    filing_status == status.SEPARATE,
-                    filing_status == status.SURVIVING_SPOUSE,
-                ],
-                [
-                    p.single.calc(regular_income),
-                    p.joint.calc(regular_income),
-                    p.head_of_household.calc(regular_income),
-                    p.separate.calc(regular_income),
-                    p.widow.calc(regular_income),
-                ],
-            )
+            capital_gains_tax = 0
+
+        regular_tax = select(
+            [
+                filing_status == status.SINGLE,
+                filing_status == status.JOINT,
+                filing_status == status.HEAD_OF_HOUSEHOLD,
+                filing_status == status.SEPARATE,
+                filing_status == status.SURVIVING_SPOUSE,
+            ],
+            [
+                p.single.calc(taxable_income),
+                p.joint.calc(taxable_income),
+                p.head_of_household.calc(taxable_income),
+                p.separate.calc(taxable_income),
+                p.widow.calc(taxable_income),
+            ],
+        )
+        return regular_tax + capital_gains_tax
