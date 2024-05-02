bed71adb29a4922210e1f7436734661cb4f8fab3
liwenxin491 <160153575+liwenxin491@users.noreply.github.com>
2024-05-02 14:33:18 +0000

    add the child_amounts.yaml

--- policyengine_us/variables/gov/states/az/hhs/tanf/az_hhs_tanf_earned_income_care_expense_disregard.py
+++ policyengine_us/variables/gov/states/az/hhs/tanf/az_hhs_tanf_earned_income_care_expense_disregard.py
@@ -20,23 +20,27 @@ class az_hhs_tanf_earned_income_care_expense_disregard(Variable):
         monthly_care_expenses = care_expenses / MONTHS_IN_YEAR
         # Determine the total eligible disregard
         # The eligibility reuquirements consider wither children or disabled adults
-        young_eligible_child = age < p.child_age
-        is_child = person("az_tanf_eligible_child", period)
+        #young_eligible_child = age < p.child_age
+        #is_child = person("az_tanf_eligible_child", period)
         disabled_adult = person("is_disabled", period)
-        disregard_amount = select(
-            [
-                young_eligible_child,
-                is_child,
-                disabled_adult,
-            ],
-            [
-                p.younger,
-                p.older,
-                p.adult,
-            ],
-            default=0,
-        )
-        total_disregard = spm_unit.sum(disregard_amount)
+        child_amount=p.child_amounts.calc(age)
+        print(child_amount)
+        adult_amount = disabled_adult * p.adult  
+        total_amount = child_amount + adult_amount
+        # disregard_amount = select(
+        #     [
+        #         young_eligible_child,
+        #         is_child,
+        #         disabled_adult,
+        #     ],
+        #     [
+        #         p.younger,
+        #         p.older,
+        #         p.adult,
+        #     ],
+        #     default=0,
+        # )
+        #total_disregard = spm_unit.sum(disregard_amount)
 
         # The disregard is capped at the expenses
-        return min_(monthly_care_expenses, total_disregard)
+        return min_(monthly_care_expenses, total_amount)
