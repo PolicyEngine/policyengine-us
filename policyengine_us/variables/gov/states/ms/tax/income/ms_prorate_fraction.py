b8adc5942554c977ed1405d6f40893350fcec4c5
PavelMakarchuk <pavel.ma99@gmail.com>
2024-05-05 11:47:57 -0400

    Create a lives_in_car variable to determine whether a homeless person is using their vehicle as shelter Fixes #4474

--- policyengine_us/variables/gov/states/ms/tax/income/ms_prorate_fraction.py
+++ policyengine_us/variables/gov/states/ms/tax/income/ms_prorate_fraction.py
@@ -10,8 +10,20 @@ class ms_prorate_fraction(Variable):
     defined_for = StateCode.MS
 
     def formula(person, period, parameters):
+        # We will allocate the itemized deduction by first closing the
+        # between the head and spouse income and then allocating 
+        # it evenly 
         agi = person("ms_agi", period)
-        total_agi = person.tax_unit.sum(agi)
+        head = person("is_tax_unit_head", period)
+        spouse = person("is_tax_unit_spouse", period)
+        head_agi = agi * head
+        spouse_agi = agi * spouse
+        agi_difference = np.abs(head_agi - spouse_agi)
+
+        unit_deds = person.tax_unit("ms_itemized_deductions_unit", period)
+        deductions_capped_at_agi_difference = min_(
+            agi_difference, unit_deds
+        )
         # avoid divide-by-zero warnings when using where() function
         fraction = np.zeros_like(total_agi)
         mask = total_agi != 0
@@ -22,3 +34,4 @@ class ms_prorate_fraction(Variable):
             person("is_tax_unit_head", period),
             fraction,
         )
+
