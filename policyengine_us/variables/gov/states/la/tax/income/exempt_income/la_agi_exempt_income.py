53f5924455ed00ebc5f7efa68c797e64508452de
PavelMakarchuk <pavel.ma99@gmail.com>
2024-05-05 16:34:44 -0400

    format and comments

--- policyengine_us/variables/gov/states/la/tax/income/exempt_income/la_agi_exempt_income.py
+++ policyengine_us/variables/gov/states/la/tax/income/exempt_income/la_agi_exempt_income.py
@@ -6,7 +6,11 @@ class la_agi_exempt_income(Variable):
     entity = TaxUnit
     label = "Louisiana income that is exempt from the adjusted gross income"
     defined_for = StateCode.LA
-    reference = ("https://revenue.louisiana.gov/TaxForms/IT540i(2021)%20Instructions.pdf#page=9", "https://revenue.louisiana.gov/TaxForms/IT540WEB(2021)%20F.pdf")
+    unit = USD
+    reference = (
+        "https://revenue.louisiana.gov/TaxForms/IT540i(2021)%20Instructions.pdf#page=9",
+        "https://revenue.louisiana.gov/TaxForms/IT540WEB(2021)%20F.pdf",
+    )
     definition_period = YEAR
 
     # Functions as subtractions.
@@ -22,23 +26,24 @@ class la_agi_exempt_income(Variable):
             exempt_income_rate = np.zeros_like(agi)
             mask = agi != 0
             exempt_income_rate[mask] = total_exempt_income[mask] / agi[mask]
-            # The second option only applies if the tax unit has a federal tax deduction
             # The step by step calculation is as follows:
-            # Line 1, representing federal AGI is devided by Line 4H 
-            # which is the total exempt income. This rate is multiplied by 
+            # Line 1, representing federal AGI is divided by Line 4H
+            # which is the total exempt income. This rate (exempt_income_rate) is multiplied by
             # Line 9 of the Form IT-540, representing the federal tax deduction
             federal_tax_deduction = tax_unit(
                 "la_federal_tax_deduction", period
             )
-            federal_tax_deduction_eligible = federal_tax_deduction > 0
             # Multiply the federal tax deduction by the exempt income rate
             reduced_federal_tax_deduction = (
                 federal_tax_deduction * exempt_income_rate
             )
             # The smaller of the two options is applied if taxable income exceeds zero
+            # Mentioned from the IT-540 instructions, Page 9, Line 4I
             smaller_adjustment = min_(
                 reduced_federal_tax_deduction, reduced_exempt_income
             )
+            # The smaller of the two options is applied if taxable income exceeds zero
+            federal_tax_deduction_eligible = federal_tax_deduction > 0
             final_exempt_income_reduction = where(
                 federal_tax_deduction_eligible,
                 smaller_adjustment,
