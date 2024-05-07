b7b500cb121c2f3a8551ac818b4b15a642282617
liwenxin491 <160153575+liwenxin491@users.noreply.github.com>
2024-05-07 17:40:31 +0000

    update the testing files

--- policyengine_us/variables/gov/states/az/hhs/tanf/eligibility/az_cash_assistance.py
+++ policyengine_us/variables/gov/states/az/hhs/tanf/eligibility/az_cash_assistance.py
@@ -11,16 +11,19 @@ class az_cash_assistance(Variable):
     )
     defined_for = StateCode.AZ
 
-    def formula(spm_unit, period,parameters):
-
+    def formula(spm_unit, period, parameters):
+        household_size = spm_unit("spm_unit_size", period)
         eligibility = spm_unit("az_hhs_tanf_eligibility", period)
-        p = parameters(period).gov.states.az.hhs.tanf.eligibility.payment_standard
-        shelter_cost = spm_unit("housing_cost", period)
-        payment_threshold = where(shelter_cost>0,p.high,p.low)
         monthly_countable_earned_income = spm_unit(
             "az_tanf_hhs_earned_income", period
         )
+        p = parameters(
+            period
+        ).gov.states.az.hhs.tanf.eligibility.payment_standard
+        shelter_cost = spm_unit("housing_cost", period)
+        payment_standard = where(shelter_cost > 0, p.high, p.low)
+        payment_threshold = payment_standard[household_size][period]
         cash_assistance_benifit = max_(
             payment_threshold - monthly_countable_earned_income, 0
         )
-        return cash_assistance_benifit if eligibility else 0
+        return where(eligibility,cash_assistance_benifit,0)
--- policyengine_us/variables/gov/states/az/hhs/tanf/eligibility/az_hhs_tanf_eligibility.py
+++ policyengine_us/variables/gov/states/az/hhs/tanf/eligibility/az_hhs_tanf_eligibility.py
@@ -9,8 +9,9 @@ class az_hhs_tanf_eligibility(Variable):
     reference = "https://des.az.gov/services/child-and-family/cash-assistance/cash-assistance-ca-income-eligibility-guidelines"
     defined_for = StateCode.AZ
 
-    def formula(spm_unit, period,parameters):
+    def formula(spm_unit, period, parameters):
         # Judge whether the countable income exceed the 100% of Ferderal Poverty Guideline
+        household_size = spm_unit("spm_unit_size", period)
         fpg = spm_unit("spm_unit_fpg", period)
         monthly_fpg = fpg / MONTHS_IN_YEAR
         print(monthly_fpg)
@@ -19,11 +20,18 @@ class az_hhs_tanf_eligibility(Variable):
         )
         fpg_eligibility = monthly_countable_earned_income <= monthly_fpg
         # Judge whether the countable income exceed the Cash Assistance Payment Standard
-        p = parameters(period).gov.states.az.hhs.tanf.eligibility.payment_standard
+        p = parameters(
+            period
+        ).gov.states.az.hhs.tanf.eligibility.payment_standard
         shelter_cost = spm_unit("housing_cost", period)
         shelter_cost_standard = p.high
         no_shetler_cost_standard = p.low
-        payment_standard = where(shelter_cost>0,shelter_cost_standard,no_shetler_cost_standard)
-        payment_standard_eligibility = monthly_countable_earned_income <= payment_standard
+        payment_standard = where(
+            shelter_cost > 0, shelter_cost_standard, no_shetler_cost_standard
+        )
+        payment_standard_threshold = payment_standard[household_size]
+        payment_standard_eligibility = (
+            monthly_countable_earned_income <= payment_standard_threshold
+        )
         # The family is eligible for cash assistance only when the two criteria are fitted at the same time
         return fpg_eligibility and payment_standard_eligibility
