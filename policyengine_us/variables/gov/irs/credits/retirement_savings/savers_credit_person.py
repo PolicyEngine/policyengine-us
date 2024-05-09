479026cb85b8251e491e2c54fe1f44a1ca06c3e3
Matty <31402450+Mshuning@users.noreply.github.com>
2024-05-09 03:22:28 +0000

    format

--- policyengine_us/variables/gov/irs/credits/retirement_savings/savers_credit_person.py
+++ policyengine_us/variables/gov/irs/credits/retirement_savings/savers_credit_person.py
@@ -17,7 +17,9 @@ class savers_credit_person(Variable):
     def formula(person, period, parameters):
         p = parameters(period).gov.irs.credits.retirement_saving
 
-        qualified_contributions = person("savers_qualified_contributions", period)
+        qualified_contributions = person(
+            "savers_qualified_contributions", period
+        )
         capped_qualified_contributions = min_(
             qualified_contributions, p.contributions_cap
         )
