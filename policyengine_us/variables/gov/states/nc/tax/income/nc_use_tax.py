897b178425ecf6030b47b5538dcaa8e084a36d09
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2024-05-09 21:13:03 +0000

    Comments for calculation

--- policyengine_us/variables/gov/states/nc/tax/income/nc_use_tax.py
+++ policyengine_us/variables/gov/states/nc/tax/income/nc_use_tax.py
@@ -12,6 +12,10 @@ class nc_use_tax(Variable):
     def formula(tax_unit, period, parameters):
         agi = tax_unit("adjusted_gross_income", period)
         p = parameters(period).gov.states.nc.tax.use_tax
-        use_tax_constant_rate = p.cap.calc(agi)
-        use_tax_fraction_rate = p.rate.calc(agi) * agi
+        use_tax_constant_rate = p.cap.calc(
+            agi
+        )  # when agi less than certain threshold, use tax is a dollar value based on amount
+        use_tax_fraction_rate = (
+            p.rate.calc(agi) * agi
+        )  # when agi is more than the threshold, use tax is a fractional amount of agi
         return use_tax_constant_rate + use_tax_fraction_rate
