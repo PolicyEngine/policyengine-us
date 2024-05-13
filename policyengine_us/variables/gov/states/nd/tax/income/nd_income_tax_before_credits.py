92ca460132acec2b0d1cd5e40a63cbc7f644cf35
XZ-mabel <131919825+XZ-mabel@users.noreply.github.com>
2024-05-13 17:35:36 +0000

    replaced broken links in variables references

--- policyengine_us/variables/gov/states/nd/tax/income/nd_income_tax_before_credits.py
+++ policyengine_us/variables/gov/states/nd/tax/income/nd_income_tax_before_credits.py
@@ -8,10 +8,10 @@ class nd_income_tax_before_credits(Variable):
     unit = USD
     definition_period = YEAR
     reference = (
-        "https://www.tax.nd.gov/sites/www/files/documents/forms/form-nd-1-2021.pdf"
-        "https://www.tax.nd.gov/sites/www/files/documents/forms/2021-individual-income-tax-booklet.pdf"
-        "https://www.tax.nd.gov/sites/www/files/documents/forms/form-nd-1-2022.pdf"
-        "https://www.tax.nd.gov/sites/www/files/documents/forms/2022-individual-income-tax-booklet.pdf"
+        "https://www.tax.nd.gov/sites/www/files/documents/forms/individual/2021-iit/form-nd-1-2021.pdf#page=1"
+        "https://www.tax.nd.gov/sites/www/files/documents/forms/individual/2021-iit/individual-income-tax-booklet-2021.pdf#page=34"
+        "https://www.tax.nd.gov/sites/www/files/documents/forms/individual/2022-iit/form-nd-1-2022.pdf#page=1"
+        "https://www.tax.nd.gov/sites/www/files/documents/forms/individual/2022-iit/2022-individual-income-tax-booklet.pdf#page=34"
     )
     defined_for = StateCode.ND
 
--- policyengine_us/variables/gov/states/nd/tax/income/nd_taxable_income.py
+++ policyengine_us/variables/gov/states/nd/tax/income/nd_taxable_income.py
@@ -8,10 +8,10 @@ class nd_taxable_income(Variable):
     unit = USD
     definition_period = YEAR
     reference = (
-        "https://www.tax.nd.gov/sites/www/files/documents/forms/form-nd-1-2021.pdf"
-        "https://www.tax.nd.gov/sites/www/files/documents/forms/2021-individual-income-tax-booklet.pdf"
-        "https://www.tax.nd.gov/sites/www/files/documents/forms/form-nd-1-2022.pdf"
-        "https://www.tax.nd.gov/sites/www/files/documents/forms/2022-individual-income-tax-booklet.pdf"
+        "https://www.tax.nd.gov/sites/www/files/documents/forms/individual/2021-iit/form-nd-1-2021.pdf"
+        "https://www.tax.nd.gov/sites/www/files/documents/forms/individual/2021-iit/individual-income-tax-booklet-2021.pdf"
+        "https://www.tax.nd.gov/sites/www/files/documents/forms/individual/2022-iit/form-nd-1-2022.pdf"
+        "https://www.tax.nd.gov/sites/www/files/documents/forms/individual/2022-iit/2022-individual-income-tax-booklet.pdf"
     )
     defined_for = StateCode.ND
 
