51571a54e53a26bf39e24da08f08b9bfa67a353d
hua7450 <113824411+hua7450@users.noreply.github.com>
2024-05-13 03:53:42 +0000

    add test cases Fixes #4495

--- policyengine_us/variables/gov/states/mt/tax/income/mt_taxable_social_security.py
+++ policyengine_us/variables/gov/states/mt/tax/income/mt_taxable_social_security.py
@@ -11,28 +11,29 @@ class mt_taxable_social_security(Variable):
 
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states.mt.tax.income.social_security
+        filing_status = tax_unit("filing_status", period)
+        person = tax_unit.members
         # Compute the amount based on the schedule in tax form.
         # line 1&2: 6a in tax form ?
-        social_security = tax_unit("social_security", period)
+        social_security = person("social_security", period)
         social_security_benefits_fraction = social_security * p.rate.lower
         # line 3 total_income - taxable_social_security
         # line 4: Additions Schedule line 15 - line 3  (0?)
         # line 5:
-        tax_emempt_interest_income = tax_unit(
+        tax_emempt_interest_income = person(
             "tax_exempt_interest_income", period
         )
         # line 6: line 2+3+4+5
         new_total_income = (
             social_security_benefits_fraction
-            + tax_unit("total_income", period)
-            - tax_unit("taxable_social_security", period)
+            + person("total_income", period)
+            - person("taxable_social_security", period)
             + tax_emempt_interest_income
         )
+        # line 7&8 
         us_ald_exclue_stduent_loan = tax_unit(
             "above_the_line_deductions", period
-        ) - tax_unit("student_loan_interest", period)
-        # line 8
-        person = tax_unit.members
+        ) - person("student_loan_interest", period)
         increased_subtractions = (
             person("mt_subtractions", period) + us_ald_exclue_stduent_loan
         )
@@ -40,16 +41,16 @@ class mt_taxable_social_security(Variable):
         remaining_income = max(0, new_total_income - increased_subtractions)
         # line 10: get amount based on filing status
         amount_higher = p.social_security.amount.higher[filing_status]
-        # line 11: line 9 - line 10
+        # line 11: line 9 - line 10 (remaining_income - amount_higer)
         # line 12: get amount based on filing status
         amount_lower = p.social_security.amount.lower[filing_status]
         # lien 13: line 11 - line 12
         minimum_tax_threshold = max_(
-            0, amount_higher - remaining_income - amount_lower
+            0, remaining_income - amount_higher - amount_lower
         )
         # line 14 & 15
         minimum_tax_threshold_fraction = (
-            min_(amount_higher - remaining_income, amount_lower) * p.rate.lower
+            min_(remaining_income - amount_higher, amount_lower) * p.rate.lower
         )
         # line 16
         minimum_fraction = min_(
