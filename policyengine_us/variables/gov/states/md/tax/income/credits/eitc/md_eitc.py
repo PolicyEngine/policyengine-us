2e49b4935ea7b3b1abf04b21bf7261ceae3412ee
Ruby Ren <38923159+YichunRen@users.noreply.github.com>
2024-05-13 23:26:53 +0000

    new montgomery_refundable_eitc

--- policyengine_us/variables/gov/states/md/tax/income/credits/eitc/md_eitc.py
+++ policyengine_us/variables/gov/states/md/tax/income/credits/eitc/md_eitc.py
@@ -11,17 +11,4 @@ class md_eitc(Variable):
     reference = "https://casetext.com/statute/code-of-maryland/article-tax-general/title-10-income-tax/subtitle-7-income-tax-credits/section-10-704-effective-until-6302023-for-earned-income"
     defined_for = StateCode.MD
 
-    # adds = ["md_non_refundable_eitc", "md_refundable_eitc"]
-
-    def formula(tax_unit, period, parameters):
-        p = parameters(period).gov.states.md.tax.income.credits.eitc.montgomery
-
-        # check montgomery county match qualification
-        county = tax_unit.household("county", period)
-        montgomery = county == "MONTGOMERY_COUNTY_MD"
-
-        # state EITC
-        refundable_eitc = tax_unit("md_refundable_eitc", period)
-        non_refundable_eitc = tax_unit("md_non_refundable_eitc", period)
-
-        return non_refundable_eitc + refundable_eitc(1 + p.match * montgomery)
+    adds = ["md_non_refundable_eitc", "md_refundable_eitc"]
---
+++ policyengine_us/variables/gov/states/md/tax/income/credits/eitc/refundable/montgomery_refundable_eitc.py
@@ -0,0 +1,25 @@
+from policyengine_us.model_api import *
+
+
+class montgomery_refundable_eitc(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Montgomery EITC"
+    unit = USD
+    documentation = "Refundable Montgomery county EITC"
+    definition_period = YEAR
+    reference = "https://casetext.com/statute/code-of-maryland/article-tax-general/title-10-income-tax/subtitle-7-income-tax-credits/section-10-704-effective-until-6302023-for-earned-income"
+    defined_for = StateCode.MD
+
+    def formula(tax_unit, period, parameters):
+        p = parameters(period).gov.states.md.tax.income.credits.eitc.refundable.montgomery
+
+        # check montgomery county match qualification
+        county = tax_unit.household("county", period)
+        montgomery = county == "MONTGOMERY_COUNTY_MD"
+
+        # state EITC
+        md_refundable_eitc = tax_unit("md_refundable_eitc", period) 
+
+        # apply county match
+        return md_refundable_eitc * p.match * montgomery
