b054d789ceb62fa25a620f2e97f9768462c91599
PavelMakarchuk <pavel.ma99@gmail.com>
2024-05-14 11:38:41 -0400

    2.21.5

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         "microdf_python",
         "pandas",
         "pathlib",
-        "policyengine-core>=2.21.1,<3",
+        "policyengine-core>=2.21.5,<3",
         "pytest",
         "pytest-dependency",
         "pyyaml",
