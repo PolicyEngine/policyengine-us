89e1ff986cbfa452ffbba78751110dc86119cb2f
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2024-05-18 09:51:43 +0100

    Format

--- policyengine_us/variables/household/expense/retirement/able_contributions_person.py
+++ policyengine_us/variables/household/expense/retirement/able_contributions_person.py
@@ -5,8 +5,6 @@ class able_contributions_person(Variable):
     value_type = float
     entity = Person
     label = "Person-level ABLE contributions"
-    documentation = (
-        "Contributions made to an ABLE account by each individual."
-    )
+    documentation = "Contributions made to an ABLE account by each individual."
     unit = USD
     definition_period = YEAR
