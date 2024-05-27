a9209a14f0c986c443b6fbf2d6c8e85f46f1daab
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2024-05-27 18:35:01 +0000

    final adjustments

--- policyengine_us/variables/gov/states/nc/tax/income/nc_use_tax.py
+++ policyengine_us/variables/gov/states/nc/tax/income/nc_use_tax.py
@@ -12,8 +12,8 @@ class nc_use_tax(Variable):
     def formula(tax_unit, period, parameters):
         agi = tax_unit("adjusted_gross_income", period)
         p = parameters(period).gov.states.nc.tax.use_tax
-        # Compute main amount, a dollar amount based on NC AGI.
-        main_amount = p.main.calc(agi)
+        # Compute base amount, a dollar amount based on NC AGI.
+        main_amount = p.base.calc(agi)
         # Switch to a percentage of income above the threshold.
         additional_amount = p.additional.calc(agi) * agi
         return main_amount + additional_amount
