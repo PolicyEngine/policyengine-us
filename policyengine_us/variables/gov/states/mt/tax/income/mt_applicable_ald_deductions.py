b82c009ed081b81064a5430969eab0daf538c2c2
hua7450 <113824411+hua7450@users.noreply.github.com>
2024-05-27 14:35:49 +0000

    minor change Fixes #4495

--- policyengine_us/variables/gov/states/mt/tax/income/mt_applicable_ald_deductions.py
+++ policyengine_us/variables/gov/states/mt/tax/income/mt_applicable_ald_deductions.py
@@ -12,8 +12,8 @@ class mt_applicable_ald_deductions(Variable):
         ald_deductions = person.tax_unit("above_the_line_deductions", period)
         spouse = person("is_tax_unit_spouse", period)
         spouse_present = person.tax_unit.any(spouse)
-        head_or_spouse = person("is_tax_unit_head_or_spouse", period)
         total_deduction = where(
             spouse_present, ald_deductions / 2, ald_deductions
         )
+        head_or_spouse = person("is_tax_unit_head_or_spouse", period)
         return head_or_spouse * total_deduction
--- policyengine_us/variables/gov/states/mt/tax/income/mt_taxable_social_security.py
+++ policyengine_us/variables/gov/states/mt/tax/income/mt_taxable_social_security.py
@@ -10,21 +10,18 @@ class mt_taxable_social_security(Variable):
     reference = "https://mtrevenue.gov/wp-content/uploads/mdocs/form%202%202021.pdf#page=6"
 
     def formula(person, period, parameters):
-        p_amount = parameters(
-            period
-        ).gov.states.mt.tax.income.social_security.amount
-        p_rate = parameters(period).gov.irs.social_security.taxability.rate
-        filing_status = person.tax_unit("filing_status", period)
+        p = parameters(period).gov.states.mt.tax.income.social_security.amount
+        p_irs = parameters(period).gov.irs.social_security.taxability.rate
         # Compute the amount based on the schedule in tax form.
-        # line 1&2: 6a in tax form
+        # line 1 total net SS amount
         social_security = person("social_security", period)
-        social_security_benefits_fraction = social_security * p_rate.lower
+        # Line 2 SS multiplied by lower rate
+        social_security_benefits_fraction = social_security * p_irs.lower
         # line 3: irs_gross_income - taxable_social_security
         taxable_ss = person("taxable_social_security", period)
         gross_income = person("irs_gross_income", period)
         reduced_gross_income = max_(gross_income - taxable_ss, 0)
-
-        # line 4: Additions Schedule line 15 - Additions Schedule line 3  (
+        # line 4: Additions Schedule line 15 - Additions Schedule line 3
         # Iterest and mutual fund dividends from state, county, or municipal bonds
         # from other states - not included
         # line 5: tax exempt intrest income
@@ -37,7 +34,7 @@ class mt_taxable_social_security(Variable):
             + reduced_gross_income
             + tax_emempt_interest_income
         )
-        # line 7: Above the line deductions less stundet loans &8
+        # line 7: Remove the student loans from the above the line deductions
         ald_less_student_loan = person(
             "mt_applicable_ald_deductions", period
         ) - person("student_loan_interest", period)
@@ -49,13 +46,14 @@ class mt_taxable_social_security(Variable):
             0, income_increased_by_ss_and_interest - increased_subtractions
         )
         # line 10: get amount based on filing status
-        amount_higher = p_amount.higher[filing_status]
+        filing_status = person.tax_unit("filing_status", period)
+        amount_higher = p.higher[filing_status]
         # line 11: line 9 - line 10 (income_reduced_by_subtractions - amount_higer)
         income_reduced_by_subtractions_and_threshold = max_(
             0, income_reduced_by_subtractions - amount_higher
         )
         # line 12: get amount based on filing status
-        amount_lower = p_amount.lower[filing_status]
+        amount_lower = p.lower[filing_status]
         # lien 13: line 11 - line 12
         minimum_tax_threshold = max_(
             0, income_reduced_by_subtractions_and_threshold - amount_lower
@@ -64,16 +62,16 @@ class mt_taxable_social_security(Variable):
         capped_reduced_income = min_(
             income_reduced_by_subtractions_and_threshold, amount_lower
         )
-        minimum_tax_threshold_fraction = capped_reduced_income * p_rate.lower
+        minimum_tax_threshold_fraction = capped_reduced_income * p_irs.lower
         # line 16
         smaller_fraction = min_(
             minimum_tax_threshold_fraction, social_security_benefits_fraction
         )
-        # line 17 & 18 ## line_13*p.rate2 + line_16
+        # line 17 & 18 ## line_13*0.85 + line_16
         adjusted_tax_amount = (
-            minimum_tax_threshold * p_rate.upper + smaller_fraction
+            minimum_tax_threshold * p_irs.upper + smaller_fraction
         )
-        # line 19 ## line_1*p.rate2
-        adjusted_taxable_amount = social_security * p_rate.upper
+        # line 19 ## line_1*0.85
+        adjusted_taxable_amount = social_security * p_irs.upper
         # line 20
         return min_(adjusted_taxable_amount, adjusted_tax_amount)
