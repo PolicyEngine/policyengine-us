c36972bd361e86361120245badf652af677ec6c4
hua7450 <113824411+hua7450@users.noreply.github.com>
2024-05-30 14:23:41 +0000

    minor change

--- policyengine_us/variables/gov/states/mt/tax/income/mt_applicable_ald_deductions.py
+++ policyengine_us/variables/gov/states/mt/tax/income/mt_applicable_ald_deductions.py
@@ -10,4 +10,8 @@ class mt_applicable_ald_deductions(Variable):
 
     def formula(person, period, parameters):
         ald_deductions = person.tax_unit("above_the_line_deductions", period)
-        return ald_deductions / person.tax_unit("head_spouse_count", period)
+        total_deductions = ald_deductions / person.tax_unit(
+            "head_spouse_count", period
+        )
+        head_or_spouse = person("is_tax_unit_head_or_spouse", period)
+        return head_or_spouse * total_deductions
