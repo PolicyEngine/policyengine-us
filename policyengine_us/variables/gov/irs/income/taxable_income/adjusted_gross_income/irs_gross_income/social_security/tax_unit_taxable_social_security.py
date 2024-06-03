5c5ee983da8c3100775420c9ac2f59f327d3ef82
hua7450 <113824411+hua7450@users.noreply.github.com>
2024-06-03 13:56:47 +0000

    add changelog_entry and make format

--- policyengine_us/variables/gov/irs/income/taxable_income/adjusted_gross_income/irs_gross_income/social_security/tax_unit_taxable_social_security.py
+++ policyengine_us/variables/gov/irs/income/taxable_income/adjusted_gross_income/irs_gross_income/social_security/tax_unit_taxable_social_security.py
@@ -28,8 +28,12 @@ class tax_unit_taxable_social_security(Variable):
         separate = filing_status == status.SEPARATE
         cohabitating = tax_unit("cohabitating_spouses", period)
         # Married filing separate who cohabitated has base amount and adj base amount of 0.
-        base_amount = p.threshold.lower[filing_status]*(1 - separate * cohabitating)
-        adjusted_base_amount = p.threshold.upper[filing_status]*(1 - separate * cohabitating)
+        base_amount = p.threshold.lower[filing_status] * (
+            1 - separate * cohabitating
+        )
+        adjusted_base_amount = p.threshold.upper[filing_status] * (
+            1 - separate * cohabitating
+        )
 
         under_first_threshold = combined_income < base_amount
         under_second_threshold = combined_income < adjusted_base_amount
