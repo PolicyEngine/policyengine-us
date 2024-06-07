52eb8778a7c7e65e271f298159d06776d764ba15
xianxinzhu <97819588+xianxinzhu@users.noreply.github.com>
2024-06-07 15:34:34 +0000

    Fixes variables, tests and parameters

--- policyengine_us/variables/gov/local/ca/sf/wftc/ca_sf_wftc.py
+++ policyengine_us/variables/gov/local/ca/sf/wftc/ca_sf_wftc.py
@@ -1,16 +1,16 @@
 from policyengine_us.model_api import *
 
 
-class ca_wftc(Variable):
+class ca_sf_wftc(Variable):
     value_type = float
     entity = TaxUnit
-    label = "California Working Family Tax Credit"
+    label = "San Francisco Working Families Tax Credit"
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.CA
 
     def formula(tax_unit, period, parameters):
-        p = parameters(period).gov.states.ca.tax.income.credits.wftc
+        p = parameters(period).gov.local.ca.sf.wftc
         eitc_eligible = tax_unit("eitc_eligible", period)
         caeitc_eligible = tax_unit("ca_eitc_eligible", period)
         eligibility = eitc_eligible | caeitc_eligible
