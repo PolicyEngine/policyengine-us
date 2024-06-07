f3deed714937c640d3b3cbf8a95e6a4c09b90636
Caleb <caleblpena@gmail.com>
2024-06-07 13:45:44 -0600

    add pell grant min fpg limit

--- policyengine_us/variables/gov/ed/pell_grant/sai/eligibility_type/pell_grant_min_fpg_limit.py
+++ policyengine_us/variables/gov/ed/pell_grant/sai/eligibility_type/pell_grant_min_fpg_limit.py
@@ -8,4 +8,25 @@ class pell_grant_min_fpg_percent_limit(Variable):
     label = "The maximum FPG percent to qualify for the minimum Pell Grant"
 
     def formula(person, period, parameters):
-        return 0
+        dependent = person("is_tax_unit_dependent", period)
+        joint = person.tax_unit("tax_unit_is_joint", period)
+        not_parent = person.tax_unit("tax_unit_child_dependents", period) == 0
+
+        p = parameters(period).gov.ed.pell_grant.sai.min_pell_limits
+
+        return select(
+            [
+                dependent & ~joint,
+                dependent & joint,
+                ~dependent & not_parent,
+                ~dependent & ~joint,
+                ~dependent & joint,
+            ],
+            [
+                p.DEPENDENT_SINGLE,
+                p.DEPENDENT_NOT_SINGLE,
+                p.INDEPENDENT_NOT_PARENT,
+                p.INDEPENDENT_SINGLE,
+                p.INDEPENDENT_NOT_SINGLE,
+            ],
+        )
