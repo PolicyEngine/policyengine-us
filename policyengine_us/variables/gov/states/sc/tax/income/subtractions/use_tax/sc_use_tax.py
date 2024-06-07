873fd28fe5da4a535aa250063a7e33ef6c4da4fd
keepjojoyoung <106553218+keepjojoyoung@users.noreply.github.com>
2024-06-07 00:34:34 +0000

    South Carolina Use Tax Fixes #4514

--- policyengine_us/variables/gov/states/sc/tax/income/subtractions/use_tax/sc_use_tax.py
+++ policyengine_us/variables/gov/states/sc/tax/income/subtractions/use_tax/sc_use_tax.py
@@ -7,23 +7,26 @@ class sc_use_tax(Variable):
     label = "SC Use Tax"
     unit = USD
     definition_period = YEAR
-    reference = (
-        "https://dor.sc.gov/resources-site/lawandpolicy/Documents/SC%20Sales%20Tax%20Manual.pdf"
-    )
+    reference = "https://dor.sc.gov/resources-site/lawandpolicy/Documents/SC%20Sales%20Tax%20Manual.pdf"
     defined_for = StateCode.SC
 
     def formula(tax_unit, period, parameters):
         income = tax_unit("sc_agi", period)
         county = spm_unit.household("county_str", period)
-        sc_use_tax_in_group_one_county = tax_unit.household("sc_use_tax_in_group_one_county", period)
-        sc_use_tax_in_group_two_county = tax_unit.household("sc_use_tax_in_group_two_county", period)
-        sc_use_tax_in_group_three_county = tax_unit.household("sc_use_tax_in_group_three_county", period)
+        sc_use_tax_in_group_one_county = tax_unit.household(
+            "sc_use_tax_in_group_one_county", period
+        )
+        sc_use_tax_in_group_two_county = tax_unit.household(
+            "sc_use_tax_in_group_two_county", period
+        )
+        sc_use_tax_in_group_three_county = tax_unit.household(
+            "sc_use_tax_in_group_three_county", period
+        )
 
         p = parameters(period).gov.states.sc.tax.income.use_tax
 
         # Compute main amount, a dollar amount based on SC AGI.
-        additional_rate =
-         select(
+        additional_rate = select(
             [
                 sc_use_tax_in_group_one_county,
                 sc_use_tax_in_group_two_county,
@@ -34,11 +37,10 @@ class sc_use_tax(Variable):
                 p.rate.group_two,
                 p.rate.group_three,
             ],
-            default = 0
+            default=0,
         )
 
         # Compute use tax rate by main rate plus local county additional rate
         total_rate = p.rate.main + additional_rate
 
-        return income * total rate
-        
+        return income * total_rate
