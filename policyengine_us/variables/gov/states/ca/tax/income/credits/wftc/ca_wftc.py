6dd2f35fff25f67736589ddf1815212d76460c13
xianxinzhu <97819588+xianxinzhu@users.noreply.github.com>
2024-06-07 13:02:13 +0000

    San Francisco local EITC - Working Families Tax Credit Fixes #4578

---
+++ policyengine_us/variables/gov/states/ca/tax/income/credits/wftc/ca_wftc.py
@@ -0,0 +1,19 @@
+from policyengine_us.model_api import *
+
+
+class ca_wftc(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "California Working Family Tax Credit"
+    unit = USD
+    definition_period = YEAR
+    defined_for = StateCode.CA
+
+    def formula(tax_unit, period, parameters):
+        p = parameters(period).gov.states.ca.tax.income.credits.wftc
+        eitc_eligible = tax_unit("ca_eitc_eligible", period)
+        caeitc_eligible = tax_unit(
+            "ca_is_qualifying_child_for_caleitc", period
+        )
+        eligibility = eitc_eligible | caeitc_eligible
+        return p.amount * eligibility
