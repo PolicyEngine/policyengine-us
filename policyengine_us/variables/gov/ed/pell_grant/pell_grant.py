d9999658144ed22687327878773f8709480ff9ee
Caleb <caleblpena@gmail.com>
2024-06-11 10:54:31 -0600

    update pell grant to use the sai

--- policyengine_us/variables/gov/ed/pell_grant/pell_grant.py
+++ policyengine_us/variables/gov/ed/pell_grant/pell_grant.py
@@ -1,4 +1,7 @@
 from policyengine_us.model_api import *
+from policyengine_us.variables.gov.ed.pell_grant.sai.eligibility_type.pell_grant_eligibility_type import (
+    PellGrantEligibilityType,
+)
 
 
 class pell_grant(Variable):
@@ -17,3 +20,20 @@ class pell_grant(Variable):
         capped = min_(unbounded, p.amount.max)
         amount = where(capped < p.amount.min, 0, capped)
         return amount * (months_in_school / p.months_in_school_year)
+
+    def formula_2024(person, period, parameters):
+        coa = person("cost_of_attending_college", period)
+        sai = person("pell_grant_sai", period)
+        eligibility = person("pell_grant_eligibility_type", period)
+        p = parameters(period).gov.ed.pell_grant.amount
+        unbound = coa - sai
+        capped = min_(unbound, p.max)
+        amount = where(capped < p.min, 0, capped)
+        return select(
+            [
+                eligibility == PellGrantEligibilityType.INELIGIBLE,
+                eligibility == PellGrantEligibilityType.MAXIMUM,
+                eligibility == PellGrantEligibilityType.MINIMUM,
+            ],
+            [0, p.max, amount],
+        )
