5e19bbb42614b5cec65e39a7b81ac946a1585f61
xianxinzhu <97819588+xianxinzhu@users.noreply.github.com>
2024-06-13 15:21:31 +0000

    Fixes variables and tests

--- policyengine_us/variables/gov/local/ca/sf/wftc/ca_sf_wftc.py
+++ policyengine_us/variables/gov/local/ca/sf/wftc/ca_sf_wftc.py
@@ -11,7 +11,9 @@ class ca_sf_wftc(Variable):
 
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.local.ca.sf.wftc
+        county = tax_unit.household("county_str", period)
+        county_eligible = county == "SAN_FRANCISCO_COUNTY_CA"
         eitc_eligible = tax_unit("eitc_eligible", period)
         ca_eitc_eligible = tax_unit("ca_eitc_eligible", period)
-        eligibility = eitc_eligible | ca_eitc_eligible
+        eligibility = county_eligible & (eitc_eligible | ca_eitc_eligible)
         return p.amount * eligibility
