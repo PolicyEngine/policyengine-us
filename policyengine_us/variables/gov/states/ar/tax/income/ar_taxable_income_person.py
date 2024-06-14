a10012ec9ffd20771f22c51c32da38ba9ccf22bc
Yuan Tian <148718791+yuantian0204@users.noreply.github.com>
2024-06-14 12:27:28 +0000

    Arkansas additional tax credit for qualified individuals Fixes #3521

--- policyengine_us/variables/gov/states/ar/tax/income/ar_taxable_income_person.py
+++ policyengine_us/variables/gov/states/ar/tax/income/ar_taxable_income_person.py
@@ -1,19 +1,18 @@
 from policyengine_us.model_api import *
 
 
-class ar_taxable_income(Variable):
+class ar_taxable_income_person(Variable):
     value_type = float
     entity = Person
-    label = "Arkansas taxable income"
+    label = "Arkansas taxable income for each individual"
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.AR
 
-    def formula(person, period):
+    def formula(person, parameters, period):
         filing_separately = person.tax_unit("ar_files_separately", period)
         taxable_income_indiv = person("ar_taxable_income_indiv", period)
         taxable_income_joint = person("ar_taxable_income_joint", period)
-        taxable_income = where(
+        return where(
             filing_separately, taxable_income_indiv, taxable_income_joint
         )
-        return taxable_income
