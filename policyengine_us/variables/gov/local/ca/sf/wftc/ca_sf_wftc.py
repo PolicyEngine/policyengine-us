4d7f7eefc0d045ba22ff350d6b28bf8598f5ff7b
xianxinzhu <97819588+xianxinzhu@users.noreply.github.com>
2024-06-14 06:04:37 +0000

    Fixes variables and parameters

--- policyengine_us/variables/gov/local/ca/sf/wftc/ca_sf_wftc.py
+++ policyengine_us/variables/gov/local/ca/sf/wftc/ca_sf_wftc.py
@@ -7,13 +7,14 @@ class ca_sf_wftc(Variable):
     label = "San Francisco Working Families Tax Credit"
     unit = USD
     definition_period = YEAR
+    reference = "https://www.sfhsa.org/sites/default/files/media/document/2024-01/form_wfc_english_1.26.24.pdf#page=4"
     defined_for = StateCode.CA
 
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.local.ca.sf.wftc
         county = tax_unit.household("county_str", period)
-        county_eligible = county == "SAN_FRANCISCO_COUNTY_CA"
+        in_sf = county == "SAN_FRANCISCO_COUNTY_CA"
         eitc_eligible = tax_unit("eitc_eligible", period)
         ca_eitc_eligible = tax_unit("ca_eitc_eligible", period)
-        eligibility = county_eligible & (eitc_eligible | ca_eitc_eligible)
+        eligibility = in_sf & (eitc_eligible | ca_eitc_eligible)
         return p.amount * eligibility
