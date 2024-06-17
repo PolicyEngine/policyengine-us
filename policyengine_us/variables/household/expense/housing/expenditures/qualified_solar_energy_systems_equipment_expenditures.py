24cc915fd6d8e283a32e17a5896f37b99c76fe7f
PavelMakarchuk <pavel.ma99@gmail.com>
2024-06-17 15:01:06 -0400

    format

--- policyengine_us/variables/household/expense/housing/expenditures/qualified_solar_energy_systems_equipment_expenditures.py
+++ policyengine_us/variables/household/expense/housing/expenditures/qualified_solar_energy_systems_equipment_expenditures.py
@@ -8,4 +8,4 @@ class qualified_solar_energy_systems_equipment_expenditures(Variable):
     documentation = ""
     unit = USD
     definition_period = YEAR
-    reference = "https://www.nysenate.gov/legislation/laws/TAX/606" # (g), (2), (C), (9), (g-1), (2)
+    reference = "https://www.nysenate.gov/legislation/laws/TAX/606"  # (g), (2), (C), (9), (g-1), (2)
