048cd47d54602abdf45754dea20029c282a89804
yy3187 <129118329+yy3187@users.noreply.github.com>
2024-06-18 15:35:53 +0000

    change variables

--- policyengine_us/variables/gov/states/az/tax/income/rebates/az_one_time_families_tax_rebates.py
+++ policyengine_us/variables/gov/states/az/tax/income/rebates/az_one_time_families_tax_rebates.py
@@ -14,17 +14,20 @@ class az_one_time_families_tax_rebates(Variable):
         person = tax_unit.members
         dependent = person("is_tax_unit_dependent", period)
         age = person("age", period)
-        young_dependent = dependent & (age < p.age_threshold)
+        young_dependent = dependent & (age < p.amount.thresholds[-1])
         young_dependent_count = tax_unit.sum(young_dependent)
         dependent_count = tax_unit.sum(dependent)
         capped_dependent_count = min_(p.dependent_cap, dependent_count)
         rebate = p.amount.calc(age) * dependent
         total_amount = tax_unit.sum(rebate)
         capped_amount = min_(total_amount, p.cap)
+        extra_amount = young_dependent_count * p.amount.calc(
+            p.amount.thresholds[-1] - 1
+        ) + (capped_dependent_count - young_dependent_count) * p.amount.calc(
+            p.amount.thresholds[-1] + 1
+        )
         return where(
             young_dependent_count >= p.dependent_cap,
             capped_amount,
-            young_dependent_count * p.amount.calc(p.age_threshold - 1)
-            + (capped_dependent_count - young_dependent_count)
-            * p.amount.calc(p.age_threshold + 1),
+            extra_amount,
         )
