ecc3b8c51b128c03ae09aa77701d591d89adfb38
PavelMakarchuk <pavel.ma99@gmail.com>
2024-06-19 08:16:00 -0400

    test fixes, parameter path

--- policyengine_us/variables/gov/states/ny/tax/income/credits/ny_solar_energy_systems_equipment_credit.py
+++ policyengine_us/variables/gov/states/ny/tax/income/credits/ny_solar_energy_systems_equipment_credit.py
@@ -10,12 +10,12 @@ class ny_solar_energy_systems_equipment_credit(Variable):
     reference = "https://www.nysenate.gov/legislation/laws/TAX/606"  # (g), (2), (C), (9), (g-1)
     defined_for = StateCode.NY
 
-    def formula(tax_unit, parameters, period):
+    def formula(tax_unit, period, parameters):
         qualified_expenditures = tax_unit(
             "qualified_solar_energy_systems_equipment_expenditures", period
         )
         p = parameters(
             period
-        ).gov.states.ny.tax.income.credit.solar_energy_systems_equipment
+        ).gov.states.ny.tax.income.credits.solar_energy_systems_equipment
         solar_equipment_expenditures_fraction = qualified_expenditures * p.rate
         return min_(solar_equipment_expenditures_fraction, p.cap)
