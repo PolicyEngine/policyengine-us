3199a68cc77b797a3e9bbb76273b34f62dc5c3da
PavelMakarchuk <pavel.ma99@gmail.com>
2024-06-20 18:34:51 +0400

    format and label adj.

--- policyengine_us/variables/gov/states/ny/tax/income/credits/solar_energy_systems/ny_solar_energy_systems_equipment_credit.py
+++ policyengine_us/variables/gov/states/ny/tax/income/credits/solar_energy_systems/ny_solar_energy_systems_equipment_credit.py
@@ -7,15 +7,15 @@ class ny_solar_energy_systems_equipment_credit(Variable):
     label = "New York solar energy systems equipment credit"
     unit = USD
     definition_period = YEAR
-    reference = "https://www.nysenate.gov/legislation/laws/TAX/606"  # (g), (2), (C), (9), (g-1)
+    reference = "https://www.nysenate.gov/legislation/laws/TAX/606"  # (g)(2)(C)(9)(g-1)
     defined_for = StateCode.NY
 
     def formula(tax_unit, period, parameters):
         qualified_expenditures = tax_unit(
-            "qualified_solar_energy_systems_equipment_expenditures", period
+            "ny_qualified_solar_energy_systems_equipment_expenditures", period
         )
         p = parameters(
             period
         ).gov.states.ny.tax.income.credits.solar_energy_systems_equipment
-        solar_equipment_expenditures_fraction = qualified_expenditures * p.rate
-        return min_(solar_equipment_expenditures_fraction, p.cap)
+        uncapped_credit = qualified_expenditures * p.rate
+        return min_(uncapped_credit, p.cap)
