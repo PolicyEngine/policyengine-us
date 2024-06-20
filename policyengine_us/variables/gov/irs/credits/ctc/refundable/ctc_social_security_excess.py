0c2a28259449d88721921b496ca087694b4c9eda
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2024-06-20 15:58:41 +0000

    Using adds/subtracts instead of formula

--- policyengine_us/variables/gov/irs/credits/ctc/refundable/ctc_social_security_excess.py
+++ policyengine_us/variables/gov/irs/credits/ctc/refundable/ctc_social_security_excess.py
@@ -10,13 +10,13 @@ class ctc_social_security_excess(Variable):
         "The excess amount in social security for child tax credit calculation"
     )
     definition_period = YEAR
-    reference = "https://www.law.cornell.edu/uscode/text/26/24#d"
+    reference = "https://www.law.cornell.edu/uscode/text/26/24#d_2"
 
-    def formula(tax_unit, period, parameters):
-        p = parameters(period).gov.irs.credits.ctc.refundable
-        # Compute "Social Security taxes" as defined in the US Code for the ACTC.
-        # This includes OASDI and Medicare payroll taxes, as well as half
-        # of self-employment taxes.
-        ss_add_variables = add(tax_unit, period, p.ss_add)
-        ss_subtract_variables = add(tax_unit, period, p.ss_subtract)
-        return ss_add_variables - ss_subtract_variables
+    adds = [
+        "employee_social_security_tax",
+        "employee_medicare_tax",
+        "unreported_payroll_tax",
+        "self_employment_tax_ald",
+        "additional_medicare_tax",
+    ]
+    subtracts = ["excess_payroll_tax_withheld"]
