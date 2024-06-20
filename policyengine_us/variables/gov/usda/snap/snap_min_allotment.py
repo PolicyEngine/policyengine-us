d3bcc984cd7f7ff5a6ab7298fc88e81e3bb0dbdc
ShiqiLiu <54519061+Kiki-67@users.noreply.github.com>
2024-06-20 00:05:34 +0000

    add test

--- policyengine_us/variables/gov/usda/snap/snap_min_allotment.py
+++ policyengine_us/variables/gov/usda/snap/snap_min_allotment.py
@@ -26,11 +26,15 @@ class snap_min_allotment(Variable):
         # Minimum benefits only apply to households up to a certain size.
         size = spm_unit("spm_unit_size", period)
         eligible = size <= min_allotment.maximum_household_size
-        base_benefit = eligible * min_allotment.rate * relevant_max_allotment
+        base_benefit_amount = eligible * min_allotment.rate * relevant_max_allotment
 
         # Check if the state is in New Jersey then apply specific minimum payment
-        state_code = spm_unit.household("state_code", period)
-        is_nj = state_code == StateCode.NJ
-        nj_supplement = spm_unit("nj_snap_min_allotment", period)
-
-        return where(is_nj, nj_supplement, base_benefit)
+        state_code = spm_unit.household("state_code_str", period)
+        nj_in_effect = parameters(period).gov.usda.snap.temporary_local_benefit.nj.in_effect
+        nj_amount = parameters(period).gov.usda.snap.temporary_local_benefit.nj.amount
+        is_nj_and_effective = state_code == "NJ" and nj_in_effect
+        
+        if is_nj_and_effective:
+            return nj_amount
+        else:
+            return base_benefit_amount
