771038bf365afc7aff3e6f74252af926048b9ace
Minh Nghiem <mngh037@gmail.com>
2024-06-21 09:41:51 -0500

    Made changes per Pavel's comments on PR draft

--- policyengine_us/variables/gov/states/ny/tax/income/credits/ny_geothermal_energy_system_credit.py
+++ policyengine_us/variables/gov/states/ny/tax/income/credits/ny_geothermal_energy_system_credit.py
@@ -8,11 +8,11 @@ class ny_geothermal_energy_system_credit(Variable):
     documentation = "The tax credit for a qualified purchase or lease of geothermal energy system equipment, with a 5-year carryover."
     unit = USD
     definition_period = YEAR
-    reference = "https://www.nysenate.gov/legislation/laws/TAX/606" # (g), (2), (C), (D), (7), (8), (9), (g-4)
+    reference = "https://www.nysenate.gov/legislation/laws/TAX/606" # (g)(2)(C)(9)(g-4)
     defined_for = StateCode.NY
 
     def formula(tax_unit, parameters, period):
-        qualified_geothermal_energy_system_expenditures = tax_unit("qualified_geothermal_energy_system_expenditures", period)
+        qualified_expenditures = tax_unit("ny_qualified_geothermal_energy_system_expenditures", period)
         p = parameters(period).gov.states.ny.tax.income.credits.geothermal_energy_system
-        geothermal_equipment_expenditures_fraction = qualified_geothermal_energy_system_expenditures * p.rate
-        return min_(geothermal_equipment_expenditures_fraction, p.cap)
+        uncapped_credit = qualified_expenditures * p.rate
+        return min_(uncapped_credit, p.cap)
