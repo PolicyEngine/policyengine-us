81be3d422d152de85b6ed4a6fa725e359fd88ab5
Kiran Jivnani <kiranjivnani@Kirans-MacBook-Pro.local>
2024-06-21 12:50:18 -0500

    all files

---
+++ kj_alison_issues/files/residential_ny_sun.py
@@ -0,0 +1,32 @@
+from ... policyengine_us.model_api import *
+from ... policyengine_us.entities import *
+from ... policyengine_us.tools.general import *
+from ... policyengine_us.tools.documentation import Enum 
+
+class residential_solar_ny_sun(Variable):
+    value_type = float
+    entity = TaxUnit
+    definition_period = YEAR
+    label = "Incentive given"
+    documentation = "Eligible for nonrefundable credit for the purchase of a new clean vehicle" #to change 
+    unit = USD
+    reference = "https://www.law.cornell.edu/uscode/text/26/30D" #to change 
+    defined_for = "purchased_qualifying_new_clean_vehicle" #to change 
+
+
+    def formula(tax_unit, home_solar_size, prev_size, period, parameters):
+        region = tax_unit.household("region_incentive", period)
+        max_multiplier = parameters.get('max_pv_system_size_multiplier', 1.1)  
+        max_kwh = tax_unit.household(prev_size) * max_multiplier
+        max_kw = max_kwh / 1200  # Placeholder value
+        max_watt = max_kw * 1000
+        approved_kw = min(max_kw, home_solar_size)
+        incentive_rate = region_incentive[region][str(period)]
+        return approved_kw * incentive_rate
+
+
+
+
+
+
+
---
+++ kj_alison_issues/files/residential_solar_system_use.py
@@ -0,0 +1,24 @@
+from ... policyengine_us.model_api import *
+from ... policyengine_us.entities import *
+from ... policyengine_us.tools.general import *
+from ... kj_alison_issues.files import * # do we need this one??? #wont hurt?
+
+
+
+
+class residential_solar_energy_systems_equipment_incentives(Variable):
+    value_type = float
+    entity = Household
+    label = "Incentives by region: NY"
+    documentation = ""
+    unit = USD
+    definition_period = YEAR 
+    reference = "https://portal.nyserda.ny.gov/servlet/servlet.FileDownload?file=00P8z000001BIuBEAW"
+
+class current_solar_energy_use(Variable):
+    value_type = float
+    entity = Household
+    label = "Current home energy use in annual kilowatt hours"
+    unit = "kWh/year"
+    definition_period = YEAR
+    reference = ""
