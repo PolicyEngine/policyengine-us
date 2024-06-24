724d2c713280c5232e6be37dc0faeaa67806dc89
Griffin Sharps <griffinsharps@gmail.com>
2024-06-24 17:03:16 -0500

    First commit of NYSERDA Drive Clean material

---
+++ policyengine_us/variables/gov/states/ny/nyserda/drive_clean/drive_clean_vehicle_cost.py
@@ -0,0 +1,10 @@
+from policyengine_us.model_api import *
+
+class drive_clean_vehicle_cost(Variable):
+    value_type = float
+    entity = TaxUnit # TODO: Check if this is right. Not a tax credit.
+    label = "Vehicle Price"
+    unit = USD
+    definition_period = YEAR
+    reference = "https://www.nyserda.ny.gov/All-Programs/Drive-Clean-Rebate-For-Electric-Cars-Program"
+    defined_for = StateCode.NY
