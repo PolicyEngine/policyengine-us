b9ddd6e7d3f98cc8d03249ba424868ae4574ce5f
liwenxin491 <160153575+liwenxin491@users.noreply.github.com>
2024-06-25 17:51:59 +0000

    test the eligibility

--- policyengine_us/variables/gov/states/az/hhs/tanf/eligibility/az_hhs_tanf_eligibility.py
+++ policyengine_us/variables/gov/states/az/hhs/tanf/eligibility/az_hhs_tanf_eligibility.py
@@ -4,33 +4,31 @@ from policyengine_us.model_api import *
 class az_hhs_tanf_eligibility(Variable):
     value_type = bool
     entity = SPMUnit
-    label = "Arizona Cash Assistance eligibility"
+    label = "Eligible for the Arizona Cash Assistance"
     definition_period = MONTH
     reference = "https://des.az.gov/services/child-and-family/cash-assistance/cash-assistance-ca-income-eligibility-guidelines"
-    defined_for = "az_hhs_tanf_eligibility"
+    defined_for = StateCode.AZ
 
     def formula(spm_unit, period, parameters):
         # Judge whether the countable income exceed the 100% of Ferderal Poverty Guideline
         household_size = spm_unit("spm_unit_size", period)
         fpg = spm_unit("spm_unit_fpg", period)
+        
         monthly_fpg = fpg / MONTHS_IN_YEAR
         monthly_countable_earned_income = spm_unit(
-            "az_tanf_hhs_earned_income", period
+            "az_tanf_earned_income", period
         )
         fpg_eligibility = monthly_countable_earned_income <= monthly_fpg
         # Judge whether the countable income exceed the Cash Assistance Payment Standard
         p = parameters(
-            period
+           period
         ).gov.states.az.hhs.tanf.eligibility.payment_standard
         shelter_cost = spm_unit("housing_cost", period)
-        shelter_cost_standard = p.high
-        no_shetler_cost_standard = p.low
-        payment_standard = where(
-            shelter_cost > 0, shelter_cost_standard, no_shetler_cost_standard
+        payment_standard_threshold = where(
+            shelter_cost > 0, p.high[household_size], p.low[household_size]
         )
-        payment_standard_threshold = payment_standard[household_size]
         payment_standard_eligibility = (
-            monthly_countable_earned_income <= payment_standard_threshold
+           monthly_countable_earned_income <= payment_standard_threshold
         )
-        # The family is eligible for cash assistance only when the two criteria are fitted at the same time
+        #The family is eligible for cash assistance only when the two criteria are fitted at the same time
         return fpg_eligibility & payment_standard_eligibility
