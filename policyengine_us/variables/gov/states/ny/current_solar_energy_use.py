408b8888c6f83f76d745ffa899cca23fd6e45279
alisonspencer1 <alisonspencer010@gmail.com>
2024-06-26 10:54:47 -0500

    current solar use

--- policyengine_us/variables/gov/states/ny/current_solar_energy_use.py
+++ policyengine_us/variables/gov/states/ny/current_solar_energy_use.py
@@ -1,7 +1,7 @@
 from policyengine_us.model_api import *
 from policyengine_us.entities import *
 from policyengine_us.tools.general import *
-from kj_alison_issues.files import * # do we need this one??? #wont hurt?
+from kj_alison_issues.files import * 
 
 
 
