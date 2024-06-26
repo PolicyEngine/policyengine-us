cf4390ce1b3df767a0e2dae0270c672ba9bedf8b
alisonspencer1 <alisonspencer010@gmail.com>
2024-06-26 15:10:35 -0500

    residential ny sun

--- policyengine_us/variables/gov/states/ny/residential_ny_sun.py
+++ policyengine_us/variables/gov/states/ny/residential_ny_sun.py
@@ -1,7 +1,7 @@
-from policyengine_us.model_api import *
-from policyengine_us.entities import *
-from policyengine_us.tools.general import *
-from policyengine_us.tools.documentation import Enum 
+from ... policyengine_us.model_api import *
+from ... policyengine_us.entities import *
+from ... policyengine_us.tools.general import *
+from ... policyengine_us.tools.documentation import Enum 
 
 class residential_solar_ny_sun(Variable):
     value_type = float
@@ -10,23 +10,25 @@ class residential_solar_ny_sun(Variable):
     label = "Incentive given"
     documentation = "Eligible for nonrefundable credit for the purchase of a new clean vehicle" #to change 
     unit = USD
-    reference = "https://www.law.cornell.edu/uscode/text/26/30D" #to change 
-    defined_for = "purchased_qualifying_new_clean_vehicle" #to change 
+    reference = "https://portal.nyserda.ny.gov/servlet/servlet.FileDownload?file=00P8z000001BIuBEAW" 
+    defined_for = StateCode.NY 
 
 
-    def formula(tax_unit, home_solar_size, prev_size, period, parameters):
-        region = tax_unit.household("region_incentive", period)
-        p = parameters(period).
-        #max_multiplier = parameters.get('max_pv_system_size_multiplier', 1.1)  
-        max_kwh = tax_unit.household(prev_size) * max_multiplier
-        max_kw = max_kwh / 1200  # Placeholder value
-        max_watt = max_kw * 1000
+    def formula(household, period, parameters):
+        # region = household("region_incentive", period)
+        p = parameters(period).parameters.gov.states.ny
+        home_solar_size = household("home_solar_size",period)
+        prev_size = household("prev_size",period)
+        max_multiplier = p.max_pv_system_size.calc(period)  
+        max_kwh = prev_size * max_multiplier
+        #Conversion factor of 1 kW=1 kWh / 3600 (for number of seconds in an hr)
+        max_kw = max_kwh / 3600  
         approved_kw = min(max_kw, home_solar_size)
-        incentive_rate = region_incentive[region][str(period)]
-        return approved_kw * incentive_rate
+        incentive_rate = p.region_incentive.calc(period)
+        return approved_kw * 1000 * incentive_rate
 
 
-
+#home_solar_size, prev_size
 
 
 
