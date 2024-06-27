0db3976694d978ea2877b927ac49230c1ac06c7e
Minh Nghiem <mngh037@gmail.com>
2024-06-27 15:14:46 -0500

    removed pre-program test and 0000 param date values. fixed lint error

--- policyengine_us/variables/gov/states/ny/tax/income/credits/ny_geothermal_energy_system_credit.py
+++ policyengine_us/variables/gov/states/ny/tax/income/credits/ny_geothermal_energy_system_credit.py
@@ -8,11 +8,15 @@ class ny_geothermal_energy_system_credit(Variable):
     documentation = "The tax credit for a qualified purchase or lease of geothermal energy system equipment, with a 5-year carryover."
     unit = USD
     definition_period = YEAR
-    reference = "https://www.nysenate.gov/legislation/laws/TAX/606" # (g)(2)(C)(9)(g-4)
+    reference = "https://www.nysenate.gov/legislation/laws/TAX/606"  # (g)(2)(C)(9)(g-4)
     defined_for = StateCode.NY
 
     def formula(tax_unit, period, parameters):
-        qualified_expenditures = tax_unit("ny_qualified_geothermal_energy_system_expenditures", period)
-        p = parameters(period).gov.states.ny.tax.income.credits.geothermal_energy_system
+        qualified_expenditures = tax_unit(
+            "ny_qualified_geothermal_energy_system_expenditures", period
+        )
+        p = parameters(
+            period
+        ).gov.states.ny.tax.income.credits.geothermal_energy_system
         uncapped_credit = qualified_expenditures * p.rate
         return min_(uncapped_credit, p.cap)
--- policyengine_us/variables/gov/states/ny/tax/income/credits/ny_qualified_geothermal_energy_system_expenditures.py
+++ policyengine_us/variables/gov/states/ny/tax/income/credits/ny_qualified_geothermal_energy_system_expenditures.py
@@ -8,5 +8,5 @@ class ny_qualified_geothermal_energy_system_expenditures(Variable):
     documentation = "Money spent in the current year for the purchase or lease of geothermal energy system equipment."
     unit = USD
     definition_period = YEAR
-    reference = "https://www.nysenate.gov/legislation/laws/TAX/606" # PART 1 (g)(2)(C)(9)(g-4)
+    reference = "https://www.nysenate.gov/legislation/laws/TAX/606"  # PART 1 (g)(2)(C)(9)(g-4)
     defined_for = StateCode.NY
