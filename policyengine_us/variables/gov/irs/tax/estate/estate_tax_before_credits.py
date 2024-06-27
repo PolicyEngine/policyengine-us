5ca4a05b9dfb8eb63b7eca8613c6ac3f1b266281
Matty <31402450+Mshuning@users.noreply.github.com>
2024-06-27 13:25:53 +0000

    format

--- policyengine_us/variables/gov/irs/tax/estate/estate_tax_before_credits.py
+++ policyengine_us/variables/gov/irs/tax/estate/estate_tax_before_credits.py
@@ -10,6 +10,6 @@ class estate_tax_before_credits(Variable):
     reference = "https://www.law.cornell.edu/uscode/text/26/2001#b_1"
 
     def formula(person, period, parameters):
-        p = parameters(period).gov.irs.tax.estate.tentative_tax_rate
-        real_estate_tax = person("taxable_estate_value", period)
-        return p.calc(real_estate_tax)
+        p = parameters(period).gov.irs.tax.estate.rate
+        taxable_estate_value = person("taxable_estate_value", period)
+        return p.calc(taxable_estate_value)
