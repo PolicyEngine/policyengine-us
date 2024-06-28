0315f89984f529ddab420131102261b62fb65505
Minh Nghiem <mngh037@gmail.com>
2024-06-28 15:40:52 -0500

    reformatted structures. computations returning 0 for all tests--potential indexing issues?

--- policyengine_us/variables/gov/states/ny/nysdps/clean_heat_others_incentives.py
+++ policyengine_us/variables/gov/states/ny/nysdps/clean_heat_others_incentives.py
@@ -13,34 +13,38 @@ class clean_heat_others_incentives(Variable):
         uncapped_incentive = 0
         p = parameters(period).gov.states.ny.nysdps.clean_heat
 
-        utility_provider = tax_unit.household("utility_provider", period)
-        heatpump_category = tax_unit.household("heat_pump_category", period)
+        utility_provider = tax_unit.household("utility_provider", period) # additional 25% for NG in selected zip code--implement or not?
+        heat_pump_category = tax_unit.household("heat_pump_category", period)
         qualified_project_cost = tax_unit.household("clean_heat_project_cost", period)
-        annual_energy_savings = tax_unit.household("clean_heat_annual_energy_savings", period) # how to make this visible for selected heatpump category on frontend?
-        equipment_unit = tax_unit.household("clean_heat_equipment_unit", period) # same comment as above
-        heating_capacity = tax_unit.household("clean_heat_heating_capacity", period) # same comment as above
-        tier = tax_unit.household("clean_heat_c4a_tier", period)
+        annual_energy_savings = tax_unit.household("clean_heat_annual_energy_savings", period)
+        equipment_unit = tax_unit.household("clean_heat_equipment_unit", period)
+        heating_capacity = tax_unit.household("clean_heat_heating_capacity", period)
 
         incentive_by_utility = p.incentives_by_utility_others
-        contractor_reward = p.contractor_reward
+        contractor_reward = p.contractor_reward[utility_provider]
 
-        incentive_base = incentive_by_utility[]
-        index_(
-            into=incentive_by_utility,
-            indices=[utility_provider, heatpump_category, tier]) # tier only exists for c4a, to check if indexing like this would work
+        incentive_base = incentive_by_utility[utility_provider][heat_pump_category]
+
+        uncapped_incentive_energy_savings = incentive_base * annual_energy_savings - contractor_reward
+        uncapped_incentive_equipment_unit = incentive_base * equipment_unit - contractor_reward
+        uncapped_incentive_heating_capacity = incentive_base * (heating_capacity/10000) - contractor_reward
         
-        contractor_reward = index_(
-            into=contractor_reward,
-            indices=[utility_provider, heatpump_category])
+        uncapped_incentive = select(
+            [
+                heat_pump_category in p.incentives_structure_annual_energy_savings,
+                heat_pump_category in p.incentives_structure_equipment_unit,
+                heat_pump_category in p.incentives_structure_heating_capacity
+            ],
+            [
+                uncapped_incentive_energy_savings,
+                uncapped_incentive_equipment_unit,
+                uncapped_incentive_heating_capacity,
+            ],
+        )
 
-        if heatpump_category in tax_unit.household("incentives_structure_annual_energy_savings", period): # can we use if statement here?
-            uncapped_incentive = incentive_base * annual_energy_savings - contractor_reward
-        if heatpump_category in tax_unit.household("incentives_structure_equipment_unit", period):
-            uncapped_incentive = incentive_base * equipment_unit - contractor_reward
-        else: 
-            uncapped_incentive = incentive_base * (heating_capacity/10000) - contractor_reward
+        uncapped_incentive = select(
+            heat_pump_category == heat_pump_category.possible_values.C3, # if category C3, apply capped incentive if higher
+            min_(uncapped_incentive, p.gshp_c3_cap)
+            )
 
-        if heatpump_category.status == "GSHP: Full Load Heating": # static var like this ok? how to link and point it to GSHP C3?
-            uncapped_incentive = min_(uncapped_incentive, p.gshp_c3_cap)
-        
         return min_(uncapped_incentive, p.regular_project_cap * qualified_project_cost)
