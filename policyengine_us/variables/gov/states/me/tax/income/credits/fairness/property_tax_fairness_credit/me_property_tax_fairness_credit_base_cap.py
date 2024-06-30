71391d2b1eb720129e7d8bc02276d39ea26f5f7d
hua7450 <113824411+hua7450@users.noreply.github.com>
2024-06-30 04:57:05 +0000

    rewrite senior parameters, adjust formula

--- policyengine_us/variables/gov/states/me/tax/income/credits/fairness/property_tax_fairness_credit/me_property_tax_fairness_credit_base_cap.py
+++ policyengine_us/variables/gov/states/me/tax/income/credits/fairness/property_tax_fairness_credit/me_property_tax_fairness_credit_base_cap.py
@@ -17,20 +17,19 @@ class me_property_tax_fairness_credit_base_cap(Variable):
         filing_status = tax_unit("filing_status", period)
         filing_statuses = filing_status.possible_values
         single = filing_status == filing_statuses.SINGLE
-        joint_no_child = (filing_status == filing_statuses.JOINT) & (
-            dependents == 0
+        joint = filing_status == filing_statuses.JOINT
+        joint_no_child = joint & (dependents == 0)
+        head_of_household = filing_status == filing_statuses.HEAD_OF_HOUSEHOLD
+        head_of_household_one_child = head_of_household & (
+            dependents <= p.dependent_count_threshold
         )
-        head_of_household_one_child = (
-            filing_status == filing_statuses.HEAD_OF_HOUSEHOLD
-        ) & (dependents <= p.dependent_count_threshold)
-        joint_with_child = (filing_status == filing_statuses.JOINT) & (
-            dependents >= p.dependent_count_threshold
+        joint_with_child = joint & (dependents >= p.dependent_count_threshold)
+        head_of_household_multiple_child = head_of_household & (
+            dependents > p.dependent_count_threshold
         )
-        head_of_household_multiple_child = (
-            filing_status == filing_statuses.HEAD_OF_HOUSEHOLD
-        ) & (dependents > p.dependent_count_threshold)
         # legal code does not mention widow, but it does appear in the tax form
         widow = filing_status == filing_statuses.SURVIVING_SPOUSE
+        # Separate filers are ineligible so not included in this select statement.
         benefit_base = select(
             [
                 single,
@@ -45,23 +44,27 @@ class me_property_tax_fairness_credit_base_cap(Variable):
         )
 
         greater_age_head_spouse = tax_unit("greater_age_head_spouse", period)
-        senior = greater_age_head_spouse >= 65
-        consider_senior = max_(p.benefit_base.senior, benefit_base)
-        real_benefit_base = where(senior, consider_senior, benefit_base)
+        senior_benefit_base = p.benefit_base.senior.calc(
+            greater_age_head_spouse
+        )
+        consider_senior = max_(senior_benefit_base, benefit_base)
+        senior_benefit_apply = senior_benefit_base != 0
+        adjusted_benefit_base = where(
+            senior_benefit_apply, consider_senior, benefit_base
+        )
 
         countable_rent_property_tax = tax_unit(
             "me_property_tax_fairness_credit_countable_rent_property_tax",
             period,
         )
         capped_benefit_base = min_(
-            real_benefit_base, countable_rent_property_tax
+            adjusted_benefit_base, countable_rent_property_tax
         )
         income = tax_unit(
             "me_sales_and_property_tax_fairness_credit_income", period
         )
         income_rate = income * p.rate.income
         uncapped_credit = max_(capped_benefit_base - income_rate, 0)
-        greater_age_head_spouse = tax_unit("greater_age_head_spouse", period)
         cap = p.cap.calc(greater_age_head_spouse)
 
         return min_(uncapped_credit, cap)
--- policyengine_us/variables/gov/states/me/tax/income/credits/fairness/property_tax_fairness_credit/me_property_tax_fairness_credit_countable_rent_property_tax.py
+++ policyengine_us/variables/gov/states/me/tax/income/credits/fairness/property_tax_fairness_credit/me_property_tax_fairness_credit_countable_rent_property_tax.py
@@ -11,23 +11,7 @@ class me_property_tax_fairness_credit_countable_rent_property_tax(Variable):
     definition_period = YEAR
     defined_for = StateCode.ME
 
-    def formula(tax_unit, period, parameters):
-        rent = add(tax_unit, period, ["rent"])
-        ptax = add(tax_unit, period, ["real_estate_taxes"])
-        p = parameters(
-            period
-        ).gov.states.me.tax.income.credits.fairness.property_tax
-        utilities_included_in_rent = tax_unit(
-            "utilities_included_in_rent", period
-        )
-        utility_expenses = add(tax_unit, period, ["utility_expense"])
-        # A separate calculation exists for the case where utilities are included in rent
-        # if the filer does not know the portion of rent that is attributable to utilities
-        # This is not implemented
-        applicable_rent_amount = where(
-            utilities_included_in_rent,
-            rent - utility_expenses,
-            rent,
-        )
-        applicable_rent = applicable_rent_amount * p.rate.rent
-        return applicable_rent + ptax
+    adds = [
+        "me_property_tax_fairness_credit_countable_rent",
+        "real_estate_taxes",
+    ]
