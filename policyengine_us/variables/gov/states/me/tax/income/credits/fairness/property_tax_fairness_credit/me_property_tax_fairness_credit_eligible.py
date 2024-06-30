f8900d16ca570ca9097c34325a99b0d02dcf94b1
hua7450 <113824411+hua7450@users.noreply.github.com>
2024-06-30 15:03:58 +0000

    refactor income_limit with benefit_base and rate

--- policyengine_us/variables/gov/states/me/tax/income/credits/fairness/property_tax_fairness_credit/me_property_tax_fairness_credit_eligible.py
+++ policyengine_us/variables/gov/states/me/tax/income/credits/fairness/property_tax_fairness_credit/me_property_tax_fairness_credit_eligible.py
@@ -9,14 +9,20 @@ class me_property_tax_fairness_credit_eligible(Variable):
     defined_for = StateCode.ME
 
     def formula(tax_unit, period, parameters):
-        income = tax_unit(
-            "me_sales_and_property_tax_fairness_credit_income", period
-        )
-        filing_status = tax_unit("filing_status", period)
-        dependents = tax_unit("ctc_qualifying_children", period)
-        capped_dependents = min_(dependents, 2)
         p = parameters(
             period
         ).gov.states.me.tax.income.credits.fairness.property_tax
-        income_threshold = p.income_limit[filing_status][capped_dependents]
-        return income < income_threshold
+        benefit_base = tax_unit(
+            "me_property_tax_fairness_credit_benefit_base", period
+        )
+        income_threshold = benefit_base / p.rate.income
+        income = tax_unit(
+            "me_sales_and_property_tax_fairness_credit_income", period
+        )
+        income_eligible = income < income_threshold
+        # Separate filers are ineligible
+        filing_status = tax_unit("filing_status", period)
+        filing_statuses = filing_status.possible_values
+        separate = filing_status == filing_statuses.SEPARATE
+
+        return income_eligible & ~separate
