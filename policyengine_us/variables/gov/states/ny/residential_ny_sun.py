18bca911a7f3c5900808f5d0a0610bd16a2324f1
alisonspencer1 <142832451+alisonspencer1@users.noreply.github.com>
2024-07-01 18:03:52 -0500

    Update residential_ny_sun.py

--- policyengine_us/variables/gov/states/ny/residential_ny_sun.py
+++ policyengine_us/variables/gov/states/ny/residential_ny_sun.py
@@ -1,12 +1,12 @@
 from policyengine_us.model_api import *
 
 
-class residential_solar_ny_sun(Variable):
+class ny_sun_residential_solar(Variable):
     value_type = float
     entity = Household
     definition_period = YEAR
     label = "Incentive given"
-    documentation = "Eligible for nonrefundable credit for the purchase of a new clean vehicle" #to change 
+    documentation = "Incentives for residential solar panels in NY"
     unit = USD
     reference = "https://portal.nyserda.ny.gov/servlet/servlet.FileDownload?file=00P8z000001BIuBEAW" 
     defined_for = StateCode.NY 
@@ -15,16 +15,18 @@ class residential_solar_ny_sun(Variable):
     def formula(household, period, parameters):
         #region = household("region_incentive", period)
         p = parameters(period).parameters.gov.states.ny
+        #what should home_solar_size be? this is a user input
         home_solar_size = taxunit.household("home_solar_size",period)
-        region = household("region_status", period)
-        incentive_percentage = p.region_incentive[region]
+        region = household("ny_sun_region_status", period)
+        incentive_percentage = p.amount[region]
 
+        #same issue as with household size - prev size is a user input
         prev_size = taxunit.household("prev_size",period)
         multiplier = p.max_pv_system_size
-        max_kwh = prev_size * max_multiplier
+        max_kwh = prev_size * multiplier
         #Conversion factor of 1 kW=1 kWh / 3600 (for number of seconds in an hr)
         max_kw = max_kwh / 3600  
         approved_kw = min(max_kw, home_solar_size)
-        incentive_rate = p.region_incentive.calc(period)
+        incentive_rate = p.amount.calc(period)
         return approved_kw * 1000 * incentive_rate
 
