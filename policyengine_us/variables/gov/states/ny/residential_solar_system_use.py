fd43725ade083e2b7cdb94718117c1dd36718915
alisonspencer1 <142832451+alisonspencer1@users.noreply.github.com>
2024-07-01 17:38:04 -0500

    Update policyengine_us/variables/gov/states/ny/residential_solar_system_use.py
    Co-authored-by: Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/states/ny/residential_solar_system_use.py
+++ policyengine_us/variables/gov/states/ny/residential_solar_system_use.py
@@ -1,7 +1,4 @@
 from policyengine_us.model_api import *
-from policyengine_us.entities import *
-from policyengine_us.tools.general import *
-from kj_alison_issues.files import * # do we need this one??? #wont hurt?
 
 
 
