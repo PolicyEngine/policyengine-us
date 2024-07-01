6d9ba0341de41a7411e61a99aaa5c59d913a2491
alisonspencer1 <142832451+alisonspencer1@users.noreply.github.com>
2024-07-01 17:37:17 -0500

    Update policyengine_us/variables/gov/states/ny/current_solar_energy_use.py
    Co-authored-by: Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/states/ny/current_solar_energy_use.py
+++ policyengine_us/variables/gov/states/ny/current_solar_energy_use.py
@@ -6,7 +6,7 @@ class ny_current_solar_energy_use(Variable):
     value_type = float
     entity = Household
     label = "Past year or projected year home energy use in annual kilowatt hours"
-    unit = "kWh/year"
+    unit = "kWh"
     definition_period = YEAR
     defined_for = StateCode.NY
     reference = ""
