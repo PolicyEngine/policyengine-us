487723836ac2539ccfdcca3a88ed0e63967ef75a
Matty <31402450+Mshuning@users.noreply.github.com>
2024-07-02 13:44:06 +0000

    uprating

--- policyengine_us/variables/household/demographic/person/is_deceased.py
+++ policyengine_us/variables/household/demographic/person/is_deceased.py
@@ -4,5 +4,5 @@ from policyengine_us.model_api import *
 class is_deceased(Variable):
     value_type = bool
     entity = Person
-    label = "Is deceased"
+    label = "Person is deceased"
     definition_period = YEAR
--- policyengine_us/variables/household/demographic/person/year_deceased.py
+++ policyengine_us/variables/household/demographic/person/year_deceased.py
@@ -4,5 +4,5 @@ from policyengine_us.model_api import *
 class year_deceased(Variable):
     value_type = int
     entity = Person
-    label = "Year deceased"
+    label = "Year in which the person deceased"
     definition_period = YEAR
