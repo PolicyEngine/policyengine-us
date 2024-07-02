baaf8c8fefc651ea87d8ff0892eb1e0ba02f2c77
PavelMakarchuk <pavel.ma99@gmail.com>
2024-07-02 04:38:19 +0400

    restructure to dc and remove var

--- policyengine_us/variables/gov/usda/snap/snap_min_allotment.py
+++ policyengine_us/variables/gov/usda/snap/snap_min_allotment.py
@@ -13,20 +13,22 @@ class snap_min_allotment(Variable):
 
     def formula(spm_unit, period, parameters):
         # Parameters for the minimum benefit.
-        snap = parameters(period).gov.usda.snap
-        p_min = snap.min_allotment
+        p = parameters(period).gov.usda.snap
         # Calculate the relevant maximum benefit, defined as the maximum
         # benefit for a household of a certain size in their state.
         snap_region = spm_unit.household("snap_region_str", period)
-        relevant_max_allotment = snap.max_allotment.main[snap_region][
-            str(p_min.relevant_max_allotment_household_size)
+        relevant_max_allotment = p.max_allotment.main[snap_region][
+            str(p.min_allotment.relevant_max_allotment_household_size)
         ]
         # Minimum benefits only apply to households up to a certain size.
         size = spm_unit("spm_unit_size", period)
-        eligible = size <= p_min.maximum_household_size
-        min_allotment = eligible * p_min.rate * relevant_max_allotment
-        if p_min.dc.in_effect:
-            dc_min_allotment = p_min.dc.amount
+        eligible = size <= p.min_allotment.maximum_household_size
+        min_allotment = (
+            eligible * p.min_allotment.rate * relevant_max_allotment
+        )
+        p_dc = parameters(period).gov.states.dc.dhs.snap.min_allotment
+        if p_dc.in_effect:
+            dc_min_allotment = p_dc.amount
 
             state_code = spm_unit.household("state_code_str", period)
             in_dc = state_code == "DC"
