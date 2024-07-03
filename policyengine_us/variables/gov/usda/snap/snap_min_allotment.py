22d3b3a68622285ed6cfe62d314af2888df47726
ShiqiLiu <54519061+Kiki-67@users.noreply.github.com>
2024-07-03 20:08:19 +0000

    merge dc+nj min allotment

--- policyengine_us/variables/gov/usda/snap/snap_min_allotment.py
+++ policyengine_us/variables/gov/usda/snap/snap_min_allotment.py
@@ -15,25 +15,32 @@ class snap_min_allotment(Variable):
         # Parameters for the minimum benefit.
         snap = parameters(period).gov.usda.snap
         min_allotment = snap.min_allotment
-
         # Calculate the relevant maximum benefit, defined as the maximum
         # benefit for a household of a certain size in their state.
         snap_region = spm_unit.household("snap_region_str", period)
         relevant_max_allotment = snap.max_allotment.main[snap_region][
-            str(min_allotment.relevant_max_allotment_household_size)
+            str(snap.min_allotment.relevant_max_allotment_household_size)
         ]
 
         # Minimum benefits only apply to households up to a certain size.
         size = spm_unit("spm_unit_size", period)
-        eligible = size <= min_allotment.maximum_household_size
-        base_benefit_amount = (
+        eligible = size <= snap.min_allotment.maximum_household_size
+        min_allotment = (
             eligible * min_allotment.rate * relevant_max_allotment
         )
 
-        # New Jersey provides a separate minimum allotment amount after 2023
-        if snap.temporary_local_benefit.nj.in_effect:
-             state_code = spm_unit.household("state_code_str", period)
+        # New Jersey and DC provides separate minimum allotment amount after 2023
+        snap_nj = parameters(period).gov.states.nj.snap
+        p_dc = parameters(period).gov.states.dc.dhs.snap.min_allotment
+        state_code = spm_unit.household("state_code_str", period)
+        if p_dc.in_effect:
+            dc_min_allotment = p_dc.amount
+            in_dc = state_code == "DC"
+            min_allotment = where(in_dc, dc_min_allotment, min_allotment)
+ 
+        if snap_nj.in_effect:
+             nj_min_allotment = snap_nj.amount
              in_nj = state_code == "NJ"
-             nj_min_allotment = snap.temporary_local_benefit.nj.amount
-             base_benefit_amount = where(in_nj, nj_min_allotment, base_benefit_amount)
-        return base_benefit_amount
+             min_allotment = where(in_nj, nj_min_allotment, min_allotment)
+        return min_allotment
+
