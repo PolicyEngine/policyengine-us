d98c8401382bd8e810076f773dbd05ea06b2e225
PavelMakarchuk <pavel.ma99@gmail.com>
2024-07-04 00:46:42 +0400

    comment and format

--- policyengine_us/reforms/states/ny/wftc/ny_working_families_tax_credit.py
+++ policyengine_us/reforms/states/ny/wftc/ny_working_families_tax_credit.py
@@ -15,12 +15,15 @@ def create_ny_working_families_tax_credit() -> Reform:
             income = tax_unit("ny_agi", period)
             filing_status = tax_unit("filing_status", period)
             joint = filing_status == filing_status.possible_values.JOINT
-            reduction = where(joint, p.reduction.married.calc(income), p.reduction.single.calc(income))
+            reduction = where(
+                joint,
+                p.reduction.married.calc(income),
+                p.reduction.single.calc(income),
+            )
             children = tax_unit("ny_wftc_eligible_children", period)
             max_amount = p.amount.max * children
             min_amount = p.amount.min * children
             return max_(min_amount, max_amount - reduction)
-
 
     class ny_wftc_eligible_children(Variable):
         value_type = float
@@ -39,15 +42,15 @@ def create_ny_working_families_tax_credit() -> Reform:
             eligible_child = is_dependent & age_eligible
             return tax_unit.sum(eligible_child)
 
-    
-
     class ny_eitc(Variable):
         value_type = float
         entity = TaxUnit
         label = "New York EITC"
         unit = USD
         definition_period = YEAR
-        reference = "https://www.nysenate.gov/legislation/bills/2023/S277/amendment/B"  
+        reference = (
+            "https://www.nysenate.gov/legislation/bills/2023/S277/amendment/B"
+        )
         defined_for = StateCode.NY
 
         def formula(tax_unit, period, parameters):
@@ -57,16 +60,9 @@ def create_ny_working_families_tax_credit() -> Reform:
             household_credit = tax_unit("ny_household_credit", period)
             return max_(0, tentative_nys_eitc - household_credit)
 
-
-# TOINCLUDE: THE  REDUCTION  DESCRIBED  IN  THIS  PARAGRAPH SHALL APPLY ONLY TO THE
-# PORTION OF A TAXPAYER'S EARNED INCOME CREDIT  THAT  IS  ATTRIBUTABLE  TO
-# QUALIFYING  CHILDREN  AS DEFINED IN PARAGRAPH ONE OF SUBSECTION (C-2) OF
-# THIS SECTION. A TAXPAYER SHALL CONTINUE TO BE ALLOWED THE EARNED  INCOME
-# CREDIT  FOR THE PORTIONS OF SUCH CREDIT ATTRIBUTABLE TO ANOTHER QUALIFY-
-# ING CHILD, AS DEFINED IN 26 USC  §152(C),  OR  QUALIFYING  RELATIVE,  AS
-# DEFINED  IN 26 USC §152(D), WHO DO NOT MEET THE DEFINITION OF QUALIFYING
-# CHILD IN PARAGRAPH ONE OF SUBSECTION (C-2) OF THIS SECTION.
-
+    # We currently do not include the limitation describing 
+    # that the NY EITC rate remains unadjusted for income attributed
+    # to student dependents aged between 18 and 24
 
     class ny_exemptions(Variable):
         value_type = float
@@ -80,7 +76,9 @@ def create_ny_working_families_tax_credit() -> Reform:
         def formula(tax_unit, period, parameters):
             person = tax_unit.members
             dependent = person("is_tax_unit_dependent", period)
-            p = parameters(period).gov.contrib.states.ny.wftc.dependent_exemption
+            p = parameters(
+                period
+            ).gov.contrib.states.ny.wftc.dependent_exemption
             age = person("age", period)
             eligible_dependent = dependent & (age >= p.child_age_threshold)
             count_dependents = tax_unit.sum(eligible_dependent)
@@ -88,8 +86,6 @@ def create_ny_working_families_tax_credit() -> Reform:
                 period
             ).gov.states.ny.tax.income.exemptions.dependent
             return dependent_exemption * count_dependents
-
-
 
     class reform(Reform):
         def apply(self):
