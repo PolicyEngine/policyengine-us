7f67e9f0636482ac8f94892d7508a92265ff52bb
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2024-07-04 22:31:37 +0000

    Minor fixes

--- policyengine_us/variables/gov/ssa/ssi/eligibility/income/ssi_blind_or_disabled_working_student.py
+++ policyengine_us/variables/gov/ssa/ssi/eligibility/income/ssi_blind_or_disabled_working_student.py
@@ -1,7 +1,7 @@
 from policyengine_us.model_api import *
 
 
-class ssi_blind_or_disabled_working_student_income(Variable):
+class ssi_blind_or_disabled_working_student(Variable):
     value_type = float
     entity = Person
     label = "SSI blind or disabled student earned income exclusion"
@@ -16,12 +16,14 @@ class ssi_blind_or_disabled_working_student_income(Variable):
         is_blind = person("is_blind", period)
         is_disabled = person("is_disabled", period)
         demographic_eligible = is_blind | is_disabled
-        student_eligible = (person("age", period) <= p.age_limit) & person(
+        under_age_limit = person("age", period) <= p.age_limit
+        eligible_student = under_age_limit & person(
             "is_full_time_student", period
         )
-        program_eligible = student_eligible & demographic_eligible
+        program_eligible = eligible_student & demographic_eligible
         monthly_earned_income = (
             person("ssi_earned_income", period) / MONTHS_IN_YEAR
         )
         max_amount = min_(p.amount, monthly_earned_income) * MONTHS_IN_YEAR
-        return where(program_eligible, min_(max_amount, p.cap), 0)
+        capped_amount = min_(max_amount, p.cap)
+        return where(program_eligible, capped_amount, 0)
