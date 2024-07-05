fd5347c3eda4c3ebb9e92e1d6fc0520401e42875
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2024-07-05 13:50:50 +0000

    Making separate eligibility file and updating parameters

--- policyengine_us/variables/gov/ssa/ssi/eligibility/income/ssi_blind_or_disabled_working_student.py
+++ policyengine_us/variables/gov/ssa/ssi/eligibility/income/ssi_blind_or_disabled_working_student.py
@@ -4,26 +4,18 @@ from policyengine_us.model_api import *
 class ssi_blind_or_disabled_working_student(Variable):
     value_type = float
     entity = Person
-    label = "SSI blind or disabled student earned income exclusion"
+    label = "SSI blind or disabled working student earned income exclusion"
     unit = USD
     definition_period = YEAR
     reference = "https://www.law.cornell.edu/cfr/text/20/416.1112#c_3"
+    defined_for = "is_ssi_blind_or_disabled_working_student_eligible"
 
     def formula(person, period, parameters):
         p = parameters(
             period
-        ).gov.ssa.ssi.income.exclusions.blind_or_disabled_student
-        is_blind = person("is_blind", period)
-        is_disabled = person("is_disabled", period)
-        demographic_eligible = is_blind | is_disabled
-        under_age_limit = person("age", period) <= p.age_limit
-        eligible_student = under_age_limit & person(
-            "is_full_time_student", period
-        )
-        program_eligible = eligible_student & demographic_eligible
+        ).gov.ssa.ssi.income.exclusions.blind_or_disabled_working_student
         monthly_earned_income = (
             person("ssi_earned_income", period) / MONTHS_IN_YEAR
         )
         max_amount = min_(p.amount, monthly_earned_income) * MONTHS_IN_YEAR
-        capped_amount = min_(max_amount, p.cap)
-        return where(program_eligible, capped_amount, 0)
+        return min_(max_amount, p.cap)
