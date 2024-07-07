60aeb778d707f8076436e9a92629525f7149769f
Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>
2024-07-08 03:48:52 +0400

    Halve joint EITC phase out rate reform. (#4727)

--- policyengine_us/reforms/reforms.py
+++ policyengine_us/reforms/reforms.py
@@ -18,6 +18,7 @@ from .biden.budget_2025 import (
     create_medicare_and_investment_tax_increase_reform,
 )
 from .biden.budget_2025 import create_capital_gains_tax_increase_reform
+from .eitc import create_halve_joint_eitc_phase_out_rate_reform
 from policyengine_core.reforms import Reform
 import warnings
 
@@ -54,6 +55,9 @@ def create_structural_reforms_from_parameters(parameters, period):
     capital_gains_tax_increase = create_capital_gains_tax_increase_reform(
         parameters, period
     )
+    halve_joint_eitc_phase_out_rate = (
+        create_halve_joint_eitc_phase_out_rate_reform(parameters, period)
+    )
 
     reforms = [
         afa_reform,
@@ -68,6 +72,7 @@ def create_structural_reforms_from_parameters(parameters, period):
         reported_state_income_tax,
         medicare_and_investment_tax_increase,
         capital_gains_tax_increase,
+        halve_joint_eitc_phase_out_rate,
     ]
     reforms = tuple(filter(lambda x: x is not None, reforms))
 
