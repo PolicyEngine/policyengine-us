de98c70c9035f71fb3fac617173d4fd0acd5dbe4
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2024-07-08 16:35:16 +0000

    Adding documentation

--- policyengine_us/variables/household/demographic/person/is_runaway_child.py
+++ policyengine_us/variables/household/demographic/person/is_runaway_child.py
@@ -5,4 +5,5 @@ class is_runaway_child(Variable):
     value_type = bool
     entity = Person
     label = "Is runaway child"
+    documentation = "Whether an individual under 18 years old leaves home or their legal residence without parental or guardian permission"
     definition_period = YEAR
