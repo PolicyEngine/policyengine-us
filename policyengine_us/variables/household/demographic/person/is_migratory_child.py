c56a59f6e2c4baff0ca582b8ff2e6e4b7b24e4e5
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2024-07-09 01:52:11 +0000

    adding references and more tests

--- policyengine_us/variables/household/demographic/person/is_migratory_child.py
+++ policyengine_us/variables/household/demographic/person/is_migratory_child.py
@@ -7,3 +7,4 @@ class is_migratory_child(Variable):
     label = "Is migratory child"
     documentation = "Whether a child made a qualifying move in the last 36 months as, with, or to join a migratory agricultural worker or fisher"
     definition_period = YEAR
+    reference = "https://www.law.cornell.edu/uscode/text/20/6399#3"
--- policyengine_us/variables/household/demographic/person/is_runaway_child.py
+++ policyengine_us/variables/household/demographic/person/is_runaway_child.py
@@ -7,3 +7,4 @@ class is_runaway_child(Variable):
     label = "Is runaway child"
     documentation = "Whether an individual under 18 years old leaves home or their legal residence without parental or guardian permission"
     definition_period = YEAR
+    reference = "https://www.law.cornell.edu/uscode/text/34/11279#4"
