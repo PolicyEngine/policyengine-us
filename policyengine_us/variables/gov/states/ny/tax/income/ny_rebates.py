4e6d98e00b6c34a7248e3bd1f92f7601094ff3f4
fuyukitn <ftani@uchicago.edu>
2024-07-11 09:38:13 -0500

    Modified fine names(rebates)

--- policyengine_us/variables/gov/states/ny/tax/income/ny_rebates.py
+++ policyengine_us/variables/gov/states/ny/tax/income/ny_rebates.py
@@ -1,12 +1,12 @@
 from policyengine_us.model_api import *
 
 
-class ny_rebate(Variable):
+class ny_rebates(Variable):
     value_type = float
     entity = TaxUnit
-    label = "NY rebate"
+    label = "NY rebates"
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.NY
 
-    adds = "gov.states.ny.tax.income.rebate"
+    adds = "gov.states.ny.tax.income.rebates"
