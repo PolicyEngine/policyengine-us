d40f95c8e6338fe6637d5e1ecc7203ffb4e5d994
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2024-07-16 14:08:05 +0000

    Head Start and Early Head Start

---
+++ policyengine_us/variables/gov/hhs/head_start/head_start.py
@@ -0,0 +1,20 @@
+from policyengine_us.model_api import *
+
+
+class head_start(Variable):
+    value_type = float
+    entity = Person
+    label = "Head Start value"
+    unit = USD
+    definition_period = YEAR
+    reference = (
+        "https://eclkc.ohs.acf.hhs.gov/policy/45-cfr-chap-xiii/1302-12-determining-verifying-documenting-eligibility"
+        "https://www.hhs.gov/answers/programs-for-families-and-children/how-can-i-get-my-child-into-head-start/index.html"
+    )
+    defined_for = "is_head_start_eligible"
+
+    def formula(person, period, parameters):
+        p = parameters(period).gov.hhs.head_start
+        minimum_hours = p.minimum_hours
+        childcare_expenses = person("pre_subsidy_childcare_expenses", period)
+        return childcare_expenses * (minimum_hours / 2600)  # will be fixed
---
+++ policyengine_us/variables/gov/hhs/head_start/is_head_start_eligible.py
@@ -0,0 +1,27 @@
+from policyengine_us.model_api import *
+
+
+class is_head_start_eligible(Variable):
+    value_type = bool
+    entity = Person
+    label = "Eligible for Head Start program"
+    definition_period = YEAR
+    reference = (
+        "https://eclkc.ohs.acf.hhs.gov/policy/45-cfr-chap-xiii/1302-12-determining-verifying-documenting-eligibility"
+        "https://www.hhs.gov/answers/programs-for-families-and-children/how-can-i-get-my-child-into-head-start/index.html"
+    )
+
+    def formula(person, period, parameters):
+        tax_unit = person.tax_unit
+        p = parameters(period).gov.hhs.head_start
+
+        programs = add(tax_unit, period, p.categorical_eligibility)
+        is_program_eligible = np.any(programs)
+
+        federal_agi = tax_unit("adjusted_gross_income", period)
+        is_income_eligible = federal_agi <= tax_unit("tax_unit_fpg", period)
+
+        age = person("age", period)
+        is_age_eligible = p.age_range.calc(age)
+
+        return is_age_eligible & (is_income_eligible | is_program_eligible)
