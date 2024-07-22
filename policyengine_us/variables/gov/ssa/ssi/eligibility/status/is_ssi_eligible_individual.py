dc1c8cbaabe5fcd793ea9b0933c713de839519e8
nicholasburka <nburka@mac.com>
2024-07-22 16:12:52 -0400

    Better SSI citizen testing code
    Co-authored-by: Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/ssa/ssi/eligibility/status/is_ssi_eligible_individual.py
+++ policyengine_us/variables/gov/ssa/ssi/eligibility/status/is_ssi_eligible_individual.py
@@ -11,6 +11,7 @@ class is_ssi_eligible_individual(Variable):
         aged_blind_disabled = person("is_ssi_aged_blind_disabled", period)
         is_ssi_eligible_spouse = person("is_ssi_eligible_spouse", period)
         is_qualified_noncitizen = person("is_ssi_qualified_noncitizen", period)
-        is_citizen = person("immigration_status", period) == ImmigrationStatus.CITIZEN
+        immigration_status = person("immigration_status", period)
+        is_citizen = immigration_status == immigration_status.possible_values.CITIZEN
 
         return (aged_blind_disabled & ~is_ssi_eligible_spouse & (is_qualified_noncitizen | is_citizen))
