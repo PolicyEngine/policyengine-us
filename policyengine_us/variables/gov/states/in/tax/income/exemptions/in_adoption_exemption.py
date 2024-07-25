8cb07e5e01f8558c96e236db8b0f3fc293fb3a76
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2024-07-25 18:10:50 +0000

    fixes

--- policyengine_us/variables/gov/states/in/tax/income/exemptions/in_adoption_exemption.py
+++ policyengine_us/variables/gov/states/in/tax/income/exemptions/in_adoption_exemption.py
@@ -1,10 +1,10 @@
 from policyengine_us.model_api import *
 
 
-class in_adoption_exemptions(Variable):
+class in_adoption_exemption(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Indiana adoption credit"
+    label = "Indiana adoption exemption"
     unit = USD
     definition_period = YEAR
     reference = "https://law.justia.com/codes/indiana/title-6/article-3/chapter-1/section-6-3-1-3-5/"  # (a)(5)(D)
