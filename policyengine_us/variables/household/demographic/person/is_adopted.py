418d1949e7959e0a96df8d0bc0a2489695449fbe
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2024-07-25 15:43:01 +0000

    removing formula

--- policyengine_us/variables/household/demographic/person/is_adopted.py
+++ policyengine_us/variables/household/demographic/person/is_adopted.py
@@ -6,11 +6,3 @@ class is_adopted(Variable):
     entity = Person
     label = "Is adopted"
     definition_period = YEAR
-
-    def formula(person, period, parameters):
-        is_adopted = person("adopted_this_year", period)
-        node = period.last_year
-        while node:
-            is_adopted = is_adopted | person("adopted_this_year", node)
-            node = node.last_year
-        return is_adopted
