fc43d485d2ccb9365155f4c78b51d8ccac426f66
nicholasburka <nburka@mac.com>
2024-07-26 11:55:39 -0400

    simplify qualified noncitizen status checks in is_ssi_qualified_noncitizen.py

--- policyengine_us/variables/gov/ssa/ssi/eligibility/status/is_ssi_qualified_noncitizen.py
+++ policyengine_us/variables/gov/ssa/ssi/eligibility/status/is_ssi_qualified_noncitizen.py
@@ -16,18 +16,13 @@ class is_ssi_qualified_noncitizen(Variable):
         qualified_statuses = parameters(period).gov.ssa.ssi.eligibility.status.qualified_noncitizen
 
         # Create list of tests against immigration status
-        conditions = []
+        qualified_status_checks = []
         for qualified_status in qualified_statuses:
             if qualified_status == "LEGAL_PERMANENT_RESIDENT":
                 # LPR's need 40 Qualifying Quarters of Earnings
-                conditions.append(np.logical_and(status == ImmigrationStatus.LEGAL_PERMANENT_RESIDENT, has_40qq_earnings))
+                qualified_status_checks.append(np.logical_and(status == ImmigrationStatus.LEGAL_PERMANENT_RESIDENT, has_40qq_earnings))
             else:
-                conditions.append(status == getattr(ImmigrationStatus, qualified_status))
+                qualified_status_checks.append(status == getattr(ImmigrationStatus, qualified_status))
 
         # If any of the tests pass, return True
-        qualified = np.select(
-            conditions,
-            [True] * len(conditions),
-            default=False
-        )
-        return qualified
+        return np.any(qualified_status_checks)
