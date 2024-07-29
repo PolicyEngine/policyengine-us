f09044e3b3d304f91d0604b07e4b4ff29948abd7
PavelMakarchuk <pavel.ma99@gmail.com>
2024-07-29 19:08:28 -0400

    Exclude childless filers from the NY WFTC EITC reduction Fixes #4804

--- policyengine_us/reforms/states/ny/wftc/ny_working_families_tax_credit.py
+++ policyengine_us/reforms/states/ny/wftc/ny_working_families_tax_credit.py
@@ -80,9 +80,18 @@ def create_ny_working_families_tax_credit() -> Reform:
         unit = USD
 
         def formula(tax_unit, period, parameters):
-            child_count = tax_unit("eitc_younger_children_count", period)
+            younger_child_count = tax_unit(
+                "eitc_younger_children_count", period
+            )
+            older_child_count = tax_unit("eitc_older_children_count", period)
+            child_count = older_child_count + younger_child_count
             eitc = parameters(period).gov.irs.credits.eitc
-            return eitc.max.calc(child_count)
+            # We will reduce the maximum credit amount by the amount for self
+            # as it is attributed to the younger children EITC
+            # We also need to reduce it by the amount attributed to the younger children
+            base_credit = eitc.max.calc(child_count)
+            amount_for_older = eitc.max.calc(older_child_count)
+            return base_credit - amount_for_older
 
     class eitc_younger_phase_in_rate(Variable):
         value_type = float
@@ -168,21 +177,8 @@ def create_ny_working_families_tax_credit() -> Reform:
         reference = "https://www.law.cornell.edu/uscode/text/26/32#c_1_A"
 
         def formula(tax_unit, period, parameters):
-            person = tax_unit.members
-            has_child = tax_unit("eitc_younger_children_count", period) > 0
-            age = person("age", period)
-            # Relative parameter reference break branching in some states that
-            # modify EITC age limits.
-            min_age_non_student = (
-                parameters.gov.irs.credits.eitc.eligibility.age.min(period)
-            )
-            max_age = parameters.gov.irs.credits.eitc.eligibility.age.max(
-                period
-            )
-            meets_age_requirements = (age >= min_age_non_student) & (
-                age <= max_age
-            )
-            return has_child | tax_unit.any(meets_age_requirements)
+            return tax_unit("eitc_younger_children_count", period) > 0
+
 
     class younger_eitc_eligible(Variable):
         value_type = bool
@@ -264,18 +260,12 @@ def create_ny_working_families_tax_credit() -> Reform:
         unit = USD
 
         def formula(tax_unit, period, parameters):
-            younger_child_count = tax_unit(
-                "eitc_younger_children_count", period
-            )
-            odler_child_count = tax_unit("eitc_older_children_count", period)
-            child_count = odler_child_count + younger_child_count
+            child_count = tax_unit("eitc_older_children_count", period)
             eitc = parameters(period).gov.irs.credits.eitc
             # We will reduce the maximum credit amount by the amount for self
             # as it is attributed to the younger children EITC
             # We also need to reduce it by the amount attributed to the younger children
-            base_credit = eitc.max.calc(child_count)
-            amount_for_younger = eitc.max.calc(younger_child_count)
-            return base_credit - amount_for_younger
+            return eitc.max.calc(child_count)
 
     class eitc_older_phase_in_rate(Variable):
         value_type = float
@@ -371,14 +361,13 @@ def create_ny_working_families_tax_credit() -> Reform:
                     period
                 )
             )
-            student = person("is_full_time_student", period)
             max_age = parameters.gov.irs.credits.eitc.eligibility.age.max(
                 period
             )
             meets_age_requirements = (age >= min_age_student) & (
                 age <= max_age
             )
-            return has_child | tax_unit.any(meets_age_requirements & student)
+            return has_child | tax_unit.any(meets_age_requirements)
 
     class older_eitc_eligible(Variable):
         value_type = bool
