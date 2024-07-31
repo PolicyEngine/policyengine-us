e14bfb9d6fbc581ccc9632dd1b9d9fdb2c53c95c
Shuyu Zhan (Stella) <94806484+shuyu7zhan@users.noreply.github.com>
2024-07-31 01:49:06 +0000

    VITA eligibility Fixes #4720

--- policyengine_us/variables/gov/irs/vita/vita_eligible.py
+++ policyengine_us/variables/gov/irs/vita/vita_eligible.py
@@ -1,7 +1,7 @@
 from policyengine_us.model_api import *
 
 
-class vita_eligibility(Variable):
+class vita_eligible(Variable):
     value_type = bool
     entity = Person
     label = "Eligible for the VITA program"
