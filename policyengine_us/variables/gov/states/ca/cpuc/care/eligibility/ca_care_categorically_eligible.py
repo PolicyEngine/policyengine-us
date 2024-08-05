03622c5a45eda0c08d89c9b341fd2470bbe75bd4
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2024-08-05 19:36:31 +0000

    removing adds

--- policyengine_us/variables/gov/states/ca/cpuc/care/eligibility/ca_care_categorically_eligible.py
+++ policyengine_us/variables/gov/states/ca/cpuc/care/eligibility/ca_care_categorically_eligible.py
@@ -9,7 +9,6 @@ class ca_care_categorically_eligible(Variable):
     documentation = "Eligible for California Alternate Rates for Energy"
     reference = "https://leginfo.legislature.ca.gov/faces/codes_displaySection.xhtml?lawCode=PUC&sectionNum=739.1"
     defined_for = StateCode.CA
-    adds = "gov.states.ca.cpuc.care.eligibility.categorical"
 
     def formula(household, period, parameters):
         p = parameters(period).gov.states.ca.cpuc.care.eligibility
