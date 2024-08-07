e189e9c7dcd2c1cbb8cb561a5f2bfe7d51b904a5
martin.holmer@gmail.com <martin.holmer@gmail.com>
2024-08-07 12:09:57 -0400

    Add debugging logic to test_repeal_head_of_household.py module

--- policyengine_us/tests/microsimulation/reforms/test_repeal_head_of_household.py
+++ policyengine_us/tests/microsimulation/reforms/test_repeal_head_of_household.py
@@ -1,8 +1,8 @@
 def test_repeal_head_of_household():
+    import numpy as np
     from policyengine_us import Microsimulation
     from policyengine_core.reforms import Reform
     from policyengine_core.periods import instant
-    from numpy.testing import assert_allclose
 
     class neutralize_reform(Reform):
         def apply(self):
@@ -11,8 +11,8 @@ def test_repeal_head_of_household():
     baseline = Microsimulation()
     neutralize_reformed = Microsimulation(reform=neutralize_reform)
 
-    baseline_net_income = baseline.calculate("household_net_income")
-    neutralize_reform_net_income = neutralize_reformed.calculate(
+    baseline_netincome = baseline.calculate("household_net_income")
+    neutralize_reform_netincome = neutralize_reformed.calculate(
         "household_net_income"
     )
 
@@ -31,15 +31,22 @@ def test_repeal_head_of_household():
             self.modify_parameters(modify_parameters)
 
     parameter_reformed = Microsimulation(reform=parameter_reform)
-    parameter_reform_net_income = parameter_reformed.calculate(
+    parameter_reform_netincome = parameter_reformed.calculate(
         "household_net_income"
     )
 
+    bni_is_nan = np.isnan(baseline_netincome)
+    print('baseline   NaN count', bni_is_nan.sum())
+    nni_is_nan = np.isnan(neutralize_reform_netincome)
+    print('neutralize NaN count', nni_is_nan.sum())
+    pni_is_nan = np.isnan(parameter_reform_netincome)
+    print('parameter  NaN count', pni_is_nan.sum())
+
     # Check they're the same, element by element.
-    assert_allclose(neutralize_reform_net_income, parameter_reform_net_income)
+    assert np.allclose(neutralize_reform_netincome, parameter_reform_netincome)
 
     # Check they're the right order of magnitude.
-    total_loss = (baseline_net_income - neutralize_reform_net_income).sum()
+    total_loss = (baseline_netincome - neutralize_reform_netincome).sum()
     # CBO estimates $10B to $20B:
     # https://www.cbo.gov/budget-options/54789
     assert total_loss > 5e9
