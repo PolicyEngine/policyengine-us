516ea8ab2cf324c31c146c59e9e3a2c114909825
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2024-08-08 00:11:27 +0000

    variable name and doc and label adjustments

--- policyengine_us/variables/gov/states/ne/tax/income/credits/ctc/refundable/ne_ctc_child_receives_child_care.py
+++ policyengine_us/variables/gov/states/ne/tax/income/credits/ctc/refundable/ne_ctc_child_receives_child_care.py
@@ -1,10 +1,11 @@
 from policyengine_us.model_api import *
 
 
-class ne_refundable_ctc_child_care_receiving_eligible_child(Variable):
+class ne_ctc_child_receives_child_care(Variable):
     value_type = bool
     entity = Person
-    label = "Nebraska refundable Child Tax Credit eligible child via receiving care from an approved license-exempt provider enrolled in the child care subsidy program"
+    label = "Nebraska refundable Child Tax Credit child receiving care from child care subsidy program"
+    documentation = "Nebraska refundable Child Tax Credit eligible child via receiving care from an approved license-exempt provider enrolled in the child care subsidy program"
     definition_period = YEAR
     reference = (
         "https://nebraskalegislature.gov/laws/statutes.php?statute=77-7203"
--- policyengine_us/variables/gov/states/ne/tax/income/credits/ctc/refundable/ne_refundable_ctc_eligible_child.py
+++ policyengine_us/variables/gov/states/ne/tax/income/credits/ctc/refundable/ne_refundable_ctc_eligible_child.py
@@ -15,17 +15,17 @@ class ne_refundable_ctc_eligible_child(Variable):
         p = parameters(period).gov.states.ne.tax.income.credits.ctc.refundable
         age_eligible = person("age", period) <= p.age_threshold
         is_dependent = person("is_tax_unit_dependent", period)
-        qualifying_child = age_eligible & is_dependent
+        age_eligible_dependent = age_eligible & is_dependent
         child_care_enrolled_child = person(
-            "ne_refundable_ctc_child_care_enrolled_eligible_child", period
+            "ne_ctc_child_enrolled_in_eligible_care_program", period
         )
         child_care_receiving_child = person(
-            "ne_refundable_ctc_child_care_receiving_eligible_child", period
+            "ne_ctc_child_receives_child_care", period
         )
         income_eligible = person.tax_unit(
             "ne_refundable_ctc_income_eligible", period
         )
-        return qualifying_child & (
+        return age_eligible_dependent & (
             child_care_enrolled_child
             | child_care_receiving_child
             | income_eligible
