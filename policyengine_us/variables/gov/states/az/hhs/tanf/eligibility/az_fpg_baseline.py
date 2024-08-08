0168b62c03bb595c289ea966b1dc24fedb1e01da
liwenxin491 <160153575+liwenxin491@users.noreply.github.com>
2024-08-08 15:04:47 +0000

    complete part of the fpg data of intervening years

--- policyengine_us/variables/gov/states/az/hhs/tanf/eligibility/az_fpg_baseline.py
+++ policyengine_us/variables/gov/states/az/hhs/tanf/eligibility/az_fpg_baseline.py
@@ -10,11 +10,11 @@ class az_fpg_baseline(Variable):
     defined_for = "az_hhs_tanf_eligible"
 
     def formula(spm_unit, period, parameters):
-        fpg_year = f"1992-01-01"
+        FPG_YEAR = f"1992-01-01"
         household_size = spm_unit("az_countable_household_size", period)
         cap_household_size = max_(household_size, 0)
         state_group = spm_unit.household("state_group_str", period)
-        p_fpg = parameters(fpg_year).gov.hhs.fpg
+        p_fpg = parameters(FPG_YEAR).gov.hhs.fpg
         p1 = p_fpg.first_person[state_group]
         pn = p_fpg.additional_person[state_group]
         return p1 + pn * (cap_household_size - 1)
