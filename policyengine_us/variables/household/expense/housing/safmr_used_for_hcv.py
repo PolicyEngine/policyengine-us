4f74e86148457150a10df2851276ec65d6e9b20c
PavelMakarchuk <pavel.ma99@gmail.com>
2024-08-11 16:28:36 +0200

    descr.

--- policyengine_us/variables/household/expense/housing/safmr_used_for_hcv.py
+++ policyengine_us/variables/household/expense/housing/safmr_used_for_hcv.py
@@ -5,5 +5,4 @@ class safmr_used_for_hcv(Variable):
     value_type = bool
     entity = Household
     label = "Small area fair market rent used for purposes of the Housing Choice Voucher Program"
-    unit = USD
     definition_period = YEAR
