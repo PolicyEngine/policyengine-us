2c5097085dc05b563df3b1e297cb5cc8dc62776e
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2024-08-12 20:04:08 +0000

    refactoring household_benefits

--- policyengine_us/variables/household/income/household/household_benefits.py
+++ policyengine_us/variables/household/income/household/household_benefits.py
@@ -4,23 +4,7 @@ from policyengine_us.model_api import *
 class household_benefits(Variable):
     value_type = float
     entity = Household
-    label = "benefits"
+    label = "Household benefits"
     unit = USD
     definition_period = YEAR
-    adds = [
-        "social_security",
-        "ssi",
-        "snap",
-        "wic",
-        "free_school_meals",
-        "reduced_price_school_meals",
-        "spm_unit_broadband_subsidy",
-        "tanf",
-        "high_efficiency_electric_home_rebate",
-        "residential_efficiency_electrification_rebate",
-        "unemployment_compensation",
-        # Contributed.
-        "basic_income",
-        "spm_unit_capped_housing_subsidy",
-        "household_state_benefits",
-    ]
+    adds = "gov.household.household_benefits"
