d85d18015794514afc9379c506ae0d8f45b9f6fb
PavelMakarchuk <pavel.ma99@gmail.com>
2024-08-13 03:19:56 +0200

    Flat tax on gross income Fixes #4830

--- policyengine_us/variables/contrib/ubi_center/flat_tax.py
+++ policyengine_us/variables/contrib/ubi_center/flat_tax.py
@@ -6,10 +6,12 @@ class flat_tax(Variable):
     entity = TaxUnit
     label = "Flat tax"
     unit = USD
-    documentation = "Flat income tax on federal AGI."
+    documentation = "Flat income tax on federal AGI or gross income."
     definition_period = YEAR
 
     def formula(tax_unit, period, parameters):
-        agi = tax_unit("positive_agi", period)
+        income = tax_unit("positive_agi", period)
         p = parameters(period).gov.contrib.ubi_center.flat_tax
-        return p.rate * agi
+        if p.flat_tax_on_gross_income:
+            income = add(tax_unit, period, ["irs_gross_income"])
+        return p.rate * income
