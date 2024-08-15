887680da5367f340d850adda1ed84876be001aa5
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2024-08-15 19:45:12 +0000

    doc change

--- policyengine_us/variables/gov/states/ne/dhhs/child_care_subsidy/ne_child_care_subsidy_eligible_parent.py
+++ policyengine_us/variables/gov/states/ne/dhhs/child_care_subsidy/ne_child_care_subsidy_eligible_parent.py
@@ -6,7 +6,7 @@ class ne_child_care_subsidy_eligible_parent(Variable):
     entity = Person
     label = "Nebraska Child Care Subsidy program eligible parent"
     definition_period = YEAR
-    documentation = "Nebraska Child Care Subsidy program parent must either be working, involved with Employment First as part of the ADC program, going to school or trainings, going to medical or therapy visits for self or child(ren), or ill or hurt (must be confirmed by a doctor)"
+    documentation = "Nebraska Child Care Subsidy eligible program parent must either be working, involved with Employment First as part of the ADC program, going to school or trainings, going to medical or therapy visits for self or child(ren), or ill or hurt (must be confirmed by a doctor)"
     reference = (
         "https://dhhs.ne.gov/Pages/Child-Care-Subsidy-Information-for-Parents.aspx",
     )
