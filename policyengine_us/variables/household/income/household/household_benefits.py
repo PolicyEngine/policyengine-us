f57467bc22cacfb8514e0a64e586568e2d9a8945
Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>
2024-08-16 13:33:39 +0200

    Update policyengine_us/variables/household/income/household/household_benefits.py

--- policyengine_us/variables/household/income/household/household_benefits.py
+++ policyengine_us/variables/household/income/household/household_benefits.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class household_benefits(Variable):
     value_type = float
     entity = Household
-    label = "Household benefits"
+    label = "benefits"
     unit = USD
     definition_period = YEAR
     adds = "gov.household.household_benefits"
