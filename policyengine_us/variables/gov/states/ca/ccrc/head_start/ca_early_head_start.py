b8ea87c8d1d409ad75d44dc5df928074faea50a7
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2024-08-18 16:07:12 +0000

    changing variable names

--- policyengine_us/variables/gov/states/ca/ccrc/head_start/ca_early_head_start.py
+++ policyengine_us/variables/gov/states/ca/ccrc/head_start/ca_early_head_start.py
@@ -1,10 +1,10 @@
 from policyengine_us.model_api import *
 
 
-class ca_cspp(Variable):
+class ca_early_head_start(Variable):
     value_type = float
     entity = Person
-    label = "Amount of California State Preschool Program benefit"
+    label = "Amount of California General Child Care and Development (Early Head Start) benefit"
     definition_period = YEAR
     reference = (
         "https://www.ccrcca.org/headstart/programs/eligibility-requirements/"
@@ -12,4 +12,4 @@ class ca_cspp(Variable):
     defined_for = StateCode.CA
 
     def formula(person, period, parameters):
-        return person("head_start", period)
+        return person("early_head_start", period)
--- policyengine_us/variables/gov/states/ca/ccrc/head_start/ca_head_start.py
+++ policyengine_us/variables/gov/states/ca/ccrc/head_start/ca_head_start.py
@@ -1,10 +1,10 @@
 from policyengine_us.model_api import *
 
 
-class ca_cctr(Variable):
+class ca_head_start(Variable):
     value_type = float
     entity = Person
-    label = "Amount of California General Child Care and Development"
+    label = "Amount of California State Preschool Program (Head Start) benefit"
     definition_period = YEAR
     reference = (
         "https://www.ccrcca.org/headstart/programs/eligibility-requirements/"
@@ -12,4 +12,4 @@ class ca_cctr(Variable):
     defined_for = StateCode.CA
 
     def formula(person, period, parameters):
-        return person("early_head_start", period)
+        return person("head_start", period)
