64d0449484f2677627ce37fe96d9c0b8b1c4a5c0
hua7450 <113824411+hua7450@users.noreply.github.com>
2024-08-19 04:35:04 +0000

    draft

---
+++ policyengine_us/variables/gov/states/ma/dta/tcap/eaedc/eligibility/ma_eaedc_assets_eligible.py
@@ -0,0 +1,23 @@
+from policyengine_us.model_api import *
+
+
+class ma_eaedc_assets_eligible(Variable):
+    value_type = bool
+    entity = SPMUnit
+    label = "Eligible for Massachusetts EAEDC dependent care deduction"
+    unit = USD
+    definition_period = YEAR
+    defined_for = StateCode.MA
+    reference = (
+        "https://www.law.cornell.edu/regulations/massachusetts/106-CMR-704-110"
+    )
+
+    def formula(spm_unit, period, parameters):
+        p = parameters(period).gov.states.ma.dta.tcap.eaedc.assets
+        countable_assets = add(spm_unit, period, ["ma_eaedc_assets"])
+        vehicle_value = spm_unit.household("household_vehicles_value", period) 
+        countable_vehicle_value = max(vehicle_value - p.vehicle_exemption, 0)
+        total_assets = countable_assets + countable_vehicle_value
+        size = spm_unit("spm_unit_size", period)
+        return total_assets <= p.limit.calc(size)
+    
--- policyengine_us/variables/gov/states/ma/dta/tcap/eaedc/eligibility/ma_eaedc_dependent_care_deduction_eligible.py
+++ policyengine_us/variables/gov/states/ma/dta/tcap/eaedc/eligibility/ma_eaedc_dependent_care_deduction_eligible.py
@@ -2,7 +2,7 @@ from policyengine_us.model_api import *
 
 
 class ma_eaedc_dependent_care_deduction_eligible(Variable):
-    value_type = float
+    value_type = bool
     entity = SPMUnit
     label = "Eligible for Massachusetts EAEDC dependent care deduction"
     unit = USD
@@ -14,3 +14,6 @@ class ma_eaedc_dependent_care_deduction_eligible(Variable):
 
     def formula(spm_unit, period, parameters):
     # If earned income higher than income limit, then no dependent care expense deduction.
+        gross_earned_income = add(spm_unit, period, ["ma_eaedc_earned_income"])
+        income_limit = add(spm_unit, period, ["ma_eaedc_income_limit"])
+        return gross_earned_income <= income_limit
