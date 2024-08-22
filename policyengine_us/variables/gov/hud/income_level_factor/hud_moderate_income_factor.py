98b818506d0aeac9f6efcfd27cbf258baccf3559
hua7450 <113824411+hua7450@users.noreply.github.com>
2024-08-22 14:10:45 +0000

    minor change on hud income factor

--- policyengine_us/variables/gov/hud/income_level_factor/hud_moderate_income_factor.py
+++ policyengine_us/variables/gov/hud/income_level_factor/hud_moderate_income_factor.py
@@ -10,13 +10,12 @@ class hud_moderate_income_factor(Variable):
     definition_period = YEAR
 
     def formula(spm_unit, period, parameters):
-        p_hud = parameters(period).gov.hud.ami_limit
+        p = parameters(period).gov.hud.ami_limit
         size = spm_unit("spm_unit_size", period)
-        size_limit = p_hud.family_size
-        size_limit_excess = p_hud.per_person_exceeding_4
+        p.family_size_excess = p.per_person_exceeding_4
         size_exceeding_4 = max_(size - 4, 0)
         size_capped_at_4 = min_(size, 4)
         return (
-            size_limit.MODERATE[size_capped_at_4]
-            + size_limit_excess.MODERATE * size_exceeding_4
+            p.family_size.MODERATE[size_capped_at_4]
+            + p.family_size_excess.MODERATE * size_exceeding_4
         )
