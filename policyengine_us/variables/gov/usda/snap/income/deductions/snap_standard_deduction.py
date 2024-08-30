f4af94cd1111ed9a0be2b6427bd8038647107b8a
PavelMakarchuk <pavel.ma99@gmail.com>
2024-08-30 19:09:27 +0200

    Exclude students from the SNAP unit with certain exceptions. Fixes #4962

--- policyengine_us/variables/gov/usda/snap/income/deductions/snap_standard_deduction.py
+++ policyengine_us/variables/gov/usda/snap/income/deductions/snap_standard_deduction.py
@@ -15,5 +15,5 @@ class snap_standard_deduction(Variable):
             period
         ).gov.usda.snap.income.deductions.standard
         state_group = spm_unit.household("state_group_str", period)
-        capped_household_size = min_(spm_unit("spm_unit_size", period), 6)
+        capped_household_size = min_(spm_unit("snap_unit", period), 6)
         return standard_deductions[state_group][capped_household_size]
