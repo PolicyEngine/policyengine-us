0b884a64b83bd8dd0a9162d7f698601ef6645048
Max Ghenis <mghenis@gmail.com>
2024-09-02 19:21:31 -0400

    Add formula for health_insurance_premiums

--- policyengine_us/variables/household/expense/health/health_insurance_premiums.py
+++ policyengine_us/variables/household/expense/health/health_insurance_premiums.py
@@ -8,3 +8,7 @@ class health_insurance_premiums(Variable):
     unit = USD
     definition_period = YEAR
     uprating = "gov.bls.cpi.cpi_u"
+    adds = [
+        "health_insurance_premiums_without_medicare_part_b",
+        "medicare_part_b_premiums",
+    ]
