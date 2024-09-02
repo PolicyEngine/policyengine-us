6c4efb57111aad6aff16797c8c28811108e456d5
hua7450 <113824411+hua7450@users.noreply.github.com>
2024-09-02 15:22:36 +0000

    minor change

--- policyengine_us/variables/gov/states/ut/tax/income/ut_income_tax_before_credits.py
+++ policyengine_us/variables/gov/states/ut/tax/income/ut_income_tax_before_credits.py
@@ -9,7 +9,7 @@ class ut_income_tax_before_credits(Variable):
     defined_for = StateCode.UT
     documentation = "Form TC-40, line 10"
     definition_period = YEAR
-    reference: "https://le.utah.gov/xcode/Title59/Chapter10/59-10-S104.html?v=C59-10-S104_2022050420220504"
+    reference = "https://le.utah.gov/xcode/Title59/Chapter10/59-10-S104.html?v=C59-10-S104_2022050420220504"
 
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states.ut.tax.income
