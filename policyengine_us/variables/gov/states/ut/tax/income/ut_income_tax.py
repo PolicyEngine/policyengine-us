40b4bac23393487ddb29f92311135304bdc543e4
hua7450 <113824411+hua7450@users.noreply.github.com>
2024-09-02 03:21:11 +0000

    Refactor ut_income_tax formula draft Fixes #4982

--- policyengine_us/variables/gov/states/ut/tax/income/ut_income_tax.py
+++ policyengine_us/variables/gov/states/ut/tax/income/ut_income_tax.py
@@ -4,8 +4,14 @@ from policyengine_us.model_api import *
 class ut_income_tax(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Utah income tax"
+    label = "Utah income tax "
     unit = USD
     definition_period = YEAR
-    adds = ["ut_income_tax_before_refundable_credits"]
+    documentation = "Form TC-40, line 39"
     defined_for = StateCode.UT
+
+    def formula(tax_unit, period, parameters):
+        income = tax_unit("ut_income_tax_before_refundable_credits", period)
+        withheld_income_tax = tax_unit("ut_withheld_income_tax", period)
+        credits = tax_unit("ut_refundable_credits", period)
+        return max_(income - credits - withheld_income_tax, 0)
