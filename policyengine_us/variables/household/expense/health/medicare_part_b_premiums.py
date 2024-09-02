54991bfcd520e977fbf2b237c4f1e6ad7d9269a2
Max Ghenis <mghenis@gmail.com>
2024-09-02 19:16:38 -0400

    Verify similarity of SPM unit MOOP

--- policyengine_us/variables/household/expense/health/medicare_part_b_premiums.py
+++ policyengine_us/variables/household/expense/health/medicare_part_b_premiums.py
@@ -3,7 +3,7 @@ from policyengine_us.model_api import *
 
 class medicare_part_b_premiums(Variable):
     value_type = float
-    entity = SPMUnit
-    label = "SPM unit Medicare Part B premiums"
+    entity = Person
+    label = "Medicare Part B premiums"
     definition_period = YEAR
     unit = USD
