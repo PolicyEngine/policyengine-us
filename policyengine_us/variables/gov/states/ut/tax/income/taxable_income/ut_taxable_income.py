c3095d100377f060bcc00f0230c1fb3791ef28ef
hua7450 <113824411+hua7450@users.noreply.github.com>
2024-09-04 01:51:18 +0000

    add references

--- policyengine_us/variables/gov/states/ut/tax/income/taxable_income/ut_taxable_income.py
+++ policyengine_us/variables/gov/states/ut/tax/income/taxable_income/ut_taxable_income.py
@@ -9,6 +9,7 @@ class ut_taxable_income(Variable):
     documentation = "Form TC-40, line 9"
     definition_period = YEAR
     defined_for = StateCode.UT
+    reference = "https://tax.utah.gov/forms/2021/tc-40.pdf#page=1"
 
     adds = ["ut_total_income"]
     subtracts = ["ut_subtractions", "ut_state_tax_refund"]
