573662e56ed10b3ba4fda556c3085c82c7f586c1
h0und <79583632+msrezaie@users.noreply.github.com>
2024-09-04 16:39:07 -0400

    NC SNAP utility allowance

--- policyengine_us/variables/gov/usda/snap/income/deductions/shelter/snap_standard_utility_allowance.py
+++ policyengine_us/variables/gov/usda/snap/income/deductions/shelter/snap_standard_utility_allowance.py
@@ -1,4 +1,5 @@
 from policyengine_us.model_api import *
+from policyengine_us.variables.gov.usda.snap.snap_utility_region import SnapUtilityRegion
 
 
 class snap_standard_utility_allowance(Variable):
@@ -10,11 +11,23 @@ class snap_standard_utility_allowance(Variable):
     definition_period = MONTH
 
     def formula(spm_unit, period, parameters):
-        utility = parameters(
-            period).gov.usda.snap.income.deductions.utility.standard.main
+        p = parameters(period).gov.usda.snap.income.deductions.utility.standard
         allowance_type = spm_unit("snap_utility_allowance_type", period)
         allowance_types = allowance_type.possible_values
         region = spm_unit.household("snap_utility_region_str", period)
+        spm_size = spm_unit("spm_unit_size", period)
+        MAX_SPM_SIZE = 9
+        capped_size = min_(MAX_SPM_SIZE, spm_size)
+        sua_household_size_dependent = is_in(region, p.household_size_states)
+
+        sua = where(sua_household_size_dependent, 0, p.main[region])
+
+        # change the state code to NC for the states that do not depend on household size to prevent key error
+        region = where(sua_household_size_dependent, region, 'NC')
+
+        sua = where(sua_household_size_dependent,
+                    p.by_household_size[region][capped_size], sua)
+
         return where(
-            allowance_type == allowance_types.SUA, utility[region], 0
+            allowance_type == allowance_types.SUA, sua, 0
         )
