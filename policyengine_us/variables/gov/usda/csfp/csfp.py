71c5678658f2aaf6e4e2aed841f26f4c95ac41af
Caleb <caleblpena@gmail.com>
2024-09-04 11:25:11 -0600

    format

--- policyengine_us/variables/gov/usda/csfp/csfp.py
+++ policyengine_us/variables/gov/usda/csfp/csfp.py
@@ -12,8 +12,8 @@ class csfp(Variable):
         income = person.spm_unit("wic_fpg", period)
         age = person("age", period)
 
-        age_eligible = (age >= p.min_age)
-        income_eligible = (income <= p.income_limit)
+        age_eligible = age >= p.min_age
+        income_eligible = income <= p.income_limit
 
         eligible = age_eligible & income_eligible
 
