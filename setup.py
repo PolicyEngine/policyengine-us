72993dfe9d3e4cfd03238fa67a1a23432f1bd46d
Anthony Volk <anth.volk@gmail.com>
2024-09-07 01:32:28 +0200

    test: Try using HTTPS over SSL

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     ],
     install_requires=[
         "policyengine-core>=3.6.5",
-        "policyeninge-us-data @ git+ssh://git@github.com/PolicyEngine/policyengine-us-data.git",
+        "policyeninge-us-data @ git+https://git@github.com/PolicyEngine/policyengine-us-data.git",
         "microdf-python",
         "tqdm",
     ],
