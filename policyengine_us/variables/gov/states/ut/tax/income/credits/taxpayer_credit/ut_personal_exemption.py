318268446a32e3897fc3e0bd8b1b500f94ffcb4f
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2024-09-09 15:09:44 +0000

    adding tax form

--- policyengine_us/variables/gov/states/ut/tax/income/credits/taxpayer_credit/ut_personal_exemption.py
+++ policyengine_us/variables/gov/states/ut/tax/income/credits/taxpayer_credit/ut_personal_exemption.py
@@ -11,15 +11,15 @@ class ut_personal_exemption(Variable):
     reference = (
         "https://le.utah.gov/xcode/Title59/Chapter10/59-10-S114.html?v=C59-10-S114_2022032320220323",  # Form TC-40, Line 11
         "https://le.utah.gov/~2023/bills/static/HB0054.html#59-10-1044",  # 59-10-1018 (1)(g)
+        "https://tax.utah.gov/forms/current/tc-40inst.pdf#page=4",  # What's New, Additional dependent for taxpayer tax credit
     )
 
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states.ut.tax.income.credits.taxpayer
-        ut_total_dependents = tax_unit("ut_total_dependents", period)
+        total_dependents = tax_unit("ut_total_dependents", period)
         if p.in_effect:
             additional_dependents = tax_unit(
                 "ut_personal_exemption_additional_dependents", period
             )
-            ut_total_dependents += additional_dependents
-        rate = p.personal_exemption
-        return rate * (ut_total_dependents)
+            total_dependents += additional_dependents
+        return p.personal_exemption * total_dependents
