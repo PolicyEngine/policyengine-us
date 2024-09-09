bc0e425032caadff7a839076f5135533bb7f217a
Pinyan <129036923+pxu12@users.noreply.github.com>
2024-09-09 05:58:14 +0000

    Nebraska Military Retirement Benefit Exclusion 2022 Fixes #5011

---
+++ policyengine_us/variables/gov/states/ne/tax/income/adjusted_gross_income/subtractions/ne_military_retirement_substraction.py
@@ -0,0 +1,20 @@
+from policyengine_us.model_api import *
+
+
+class ne_military_retirement_subtraction(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Nebraska military retirement subtraction"
+    unit = USD
+    definition_period = YEAR
+    defined_for = StateCode.NE
+
+    def formula(tax_unit, period, parameters):
+        fagi = tax_unit("adjusted_gross_income", period)
+        p = parameters(
+            period
+        ).gov.states.ne.tax.income.agi.subtractions.military_retirement
+        military_retirement_benefits = add(
+            tax_unit, period, ["military_retirement_pay"]
+        )
+        return min_(military_retirement_benefits * p.fraction, fagi)
