858ddc26f36a250f46026eeb520e3d2aa21da3ea
Matty <31402450+Mshuning@users.noreply.github.com>
2024-09-12 13:09:09 +0000

    dwks10

---
+++ policyengine_us/variables/gov/irs/tax/federal_income/capital_gains/computed_dividends_gains_whether_has_gains.py
@@ -0,0 +1,40 @@
+from policyengine_us.model_api import *
+
+
+class computed_dividends_gains_whether_has_gains(Variable):
+    value_type = float
+    entity = TaxUnit
+    definition_period = YEAR
+    label = "IRS Form 1040 Schedule D worksheet (part 3 of 6)"  # DWKS10
+    unit = USD
+    reference = (
+        "https://www.irs.gov/pub/irs-pdf/f1040.pdf",
+        "https://www.irs.gov/pub/irs-pdf/i1040gi.pdf",
+        "https://www.irs.gov/pub/irs-pdf/f1040sd.pdf",
+    )
+
+    def formula(tax_unit, period, parameters):
+        if_gains = add(
+            tax_unit,
+            period,
+            [
+                "reduced_qualified_dividends_by_adjusted_investment_interest",
+                "adjusted_capital_gains_reduced_by_investment_income",
+            ],
+        )  # dwks6 & 9
+        if_no_gains = max_(
+            0,
+            min_(
+                add(
+                    tax_unit,
+                    period,
+                    ["long_term_capital_gains", "qualified_dividend_income"],
+                ),
+                tax_unit("net_capital_gains", period),
+            ),
+        ) + add(tax_unit, period, ["non_sch_d_capital_gains"])
+        return where(
+            tax_unit("has_qdiv_or_ltcg", period),
+            if_gains,
+            if_no_gains,
+        )
