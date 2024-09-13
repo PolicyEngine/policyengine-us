98e2b29afbf8ad0afe6e7789ad1b17ce60a55cea
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2024-09-13 20:18:38 +0000

    adding references

--- policyengine_us/variables/gov/states/nc/tax/income/deductions/military_retirement/nc_military_retirement_deduction_eligible.py
+++ policyengine_us/variables/gov/states/nc/tax/income/deductions/military_retirement/nc_military_retirement_deduction_eligible.py
@@ -8,6 +8,10 @@ class nc_military_retirement_deduction_eligible(Variable):
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.NC
+    references = (
+        "https://www.ncdor.gov/2022-d-401-individual-income-tax-instructions/open#page=18",
+        "https://law.justia.com/codes/north-carolina/chapter-105/article-4/section-105-153-5/",
+    )
 
     def formula(person, period, parameters):
         p = parameters(
