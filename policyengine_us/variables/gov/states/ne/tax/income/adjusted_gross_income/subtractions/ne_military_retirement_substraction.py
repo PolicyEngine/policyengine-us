e8b080a5caa19c34c064dab798f50c2d8d000886
Pinyan <129036923+pxu12@users.noreply.github.com>
2024-09-14 01:20:14 +0000

    minor change

--- policyengine_us/variables/gov/states/ne/tax/income/adjusted_gross_income/subtractions/ne_military_retirement_substraction.py
+++ policyengine_us/variables/gov/states/ne/tax/income/adjusted_gross_income/subtractions/ne_military_retirement_substraction.py
@@ -16,4 +16,5 @@ class ne_military_retirement_subtraction(Variable):
         military_retirement_benefits = add(
             tax_unit, period, ["military_retirement_pay"]
         )
+        # From 2015 to 2021, the tax filer may elect to exclude 40% of the military retirement benefit income for 7 consecutive years or elect to receive 15% exclusion for all tax years after age 67.
         return military_retirement_benefits * p.fraction
