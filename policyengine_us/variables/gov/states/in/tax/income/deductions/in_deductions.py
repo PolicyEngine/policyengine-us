c8e7d711135737084b4ad4e39f5f4503d9f851ff
srilareddy <srilareddy09@gmail.com>
2024-09-16 15:25:45 +0000

    Indiana National Guard and Reserve Pay Deduction 2023 Fixes #5031

--- policyengine_us/variables/gov/states/in/tax/income/deductions/in_deductions.py
+++ policyengine_us/variables/gov/states/in/tax/income/deductions/in_deductions.py
@@ -9,15 +9,4 @@ class in_deductions(Variable):
     definition_period = YEAR
     reference = "http://iga.in.gov/legislative/laws/2021/ic/titles/006#6-3"
 
-    adds = [
-        "in_renters_deduction",
-        "in_homeowners_property_tax_deduction",
-        "salt_refund_last_year",
-        "us_govt_interest",
-        "tax_unit_taxable_social_security",  # includes railroad retirement benefits
-        "in_military_service_deduction",
-        "in_nonpublic_school_deduction",
-        "in_nol",
-        "in_unemployment_compensation_deduction",
-        "in_other_deductions",
-    ]
+    adds = "gov.states.in.tax.income.deductions.deductions"
