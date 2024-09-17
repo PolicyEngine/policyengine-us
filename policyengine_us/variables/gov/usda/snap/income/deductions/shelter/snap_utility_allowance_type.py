94b1d1c7747252175c5e0ed7dfbefdc4b92e700d
Caleb <caleblpena@gmail.com>
2024-09-17 11:45:19 -0600

    always use the SUA for co

--- policyengine_us/variables/gov/usda/snap/income/deductions/shelter/snap_utility_allowance_type.py
+++ policyengine_us/variables/gov/usda/snap/income/deductions/shelter/snap_utility_allowance_type.py
@@ -14,23 +14,19 @@ class snap_utility_allowance_type(Variable):
     entity = SPMUnit
     label = "SNAP utility allowance eligibility"
     default_value = SNAPUttilityAllowanceType.NONE
-    documentation = (
-        "The type of utility allowance that is eligible for the SPM unit"
-    )
+    documentation = "The type of utility allowance that is eligible for the SPM unit"
     definition_period = MONTH
 
     def formula(spm_unit, period, parameters):
-        distinct_utility_bills = spm_unit(
-            "count_distinct_utility_expenses", period
-        )
-        lua = parameters(
-            period
-        ).gov.usda.snap.income.deductions.utility.limited
+        distinct_utility_bills = spm_unit("count_distinct_utility_expenses", period)
+        lua = parameters(period).gov.usda.snap.income.deductions.utility.limited
         region = spm_unit.household("snap_utility_region_str", period)
+        always_sua = spm_unit("snap_always_standard_utility_allowance", period)
+        has_heating_cooling = spm_unit("has_heating_cooling_expense", period)
         lua_is_defined = lua.active[region].astype(bool)
         return select(
             [
-                spm_unit("has_heating_cooling_expense", period),
+                has_heating_cooling | always_sua,
                 lua_is_defined & (distinct_utility_bills >= 2),
                 distinct_utility_bills > 0,
             ],
