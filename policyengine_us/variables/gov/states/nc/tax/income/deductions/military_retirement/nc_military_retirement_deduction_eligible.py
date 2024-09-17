aad65a08ebc4e032743f0e4f3820bbdfd7764466
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2024-09-17 18:16:49 +0000

    adjusting parameters

--- policyengine_us/variables/gov/states/nc/tax/income/deductions/military_retirement/nc_military_retirement_deduction_eligible.py
+++ policyengine_us/variables/gov/states/nc/tax/income/deductions/military_retirement/nc_military_retirement_deduction_eligible.py
@@ -5,7 +5,6 @@ class nc_military_retirement_deduction_eligible(Variable):
     value_type = bool
     entity = Person
     label = "North Carolina military retirement deduction eligible"
-    unit = USD
     definition_period = YEAR
     defined_for = StateCode.NC
     references = (
