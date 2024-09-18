1cd5db79fcef1a0cb772340696f818e373479b1e
Anthony Volk <anth.volk@gmail.com>
2024-09-18 18:37:38 +0200

    chore: Update -us-data version

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     ],
     install_requires=[
         "policyengine-core>=3.6.5",
-        "policyengine-us-data>=1.4.0",
+        "policyengine-us-data>=1.4.1",
         "microdf-python",
         "tqdm",
     ],
