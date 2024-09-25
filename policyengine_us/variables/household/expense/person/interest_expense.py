61e26f68ac08175bced588de9ca090a2d3a702a7
PavelMakarchuk <pavel.ma99@gmail.com>
2024-09-25 14:50:42 +0200

    Apply the TCJA mortgage interest deduction limits Fixes #5128

--- policyengine_us/variables/household/expense/person/interest_expense.py
+++ policyengine_us/variables/household/expense/person/interest_expense.py
@@ -7,4 +7,12 @@ class interest_expense(Variable):
     label = "Interest paid on all loans"
     unit = USD
     definition_period = YEAR
-    adds = ["mortgage_interest", "non_mortgage_interest"]
+
+    def formula(person, period, parameters):
+        p = parameters(period).gov.irs.deductions.itemized.interest.mortgage
+        mortgage_interest = person("mortgage_interest", period)
+        filing_status = person.tax_unit("filing_status", period)
+        cap = p.cap[filing_status]
+        capped_mortgage_interest = min_(mortgage_interest, cap)
+        non_mortgage_interest = person("non_mortgage_interest", period)
+        return capped_mortgage_interest + non_mortgage_interest
