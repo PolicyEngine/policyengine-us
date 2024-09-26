24bb6bb729442d8f82cf00ecd38230abb5f7711a
PavelMakarchuk <pavel.ma99@gmail.com>
2024-09-26 21:35:12 +0200

    adjust formula

--- policyengine_us/variables/household/expense/person/interest_expense.py
+++ policyengine_us/variables/household/expense/person/interest_expense.py
@@ -8,11 +8,4 @@ class interest_expense(Variable):
     unit = USD
     definition_period = YEAR
 
-    def formula(person, period, parameters):
-        p = parameters(period).gov.irs.deductions.itemized.interest.mortgage
-        mortgage_interest = person("mortgage_interest", period)
-        filing_status = person.tax_unit("filing_status", period)
-        cap = p.cap[filing_status]
-        capped_mortgage_interest = min_(mortgage_interest, cap)
-        non_mortgage_interest = person("non_mortgage_interest", period)
-        return capped_mortgage_interest + non_mortgage_interest
+    adds = ["mortgage_interest", "non_mortgage_interest"]
