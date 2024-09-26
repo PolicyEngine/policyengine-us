9629097b9e932371a38f819b219043a851135ef5
Max Ghenis <mghenis@gmail.com>
2024-09-26 17:14:41 -0400

    rename variable

--- policyengine_us/variables/gov/states/ak/dor/ak_permanent_fund_dividend.py
+++ policyengine_us/variables/gov/states/ak/dor/ak_permanent_fund_dividend.py
@@ -1,7 +1,7 @@
 from policyengine_us.model_api import *
 
 
-class ak_pfd(Variable):
+class ak_permanent_fund_dividend(Variable):
     value_type = float
     entity = Person
     label = "Alaska Permanent Fund Dividend"
