51a5746d4f4a939070abde29323f96f832a3e988
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2024-10-01 19:43:57 +0000

    adding separate parameter for diff age

--- policyengine_us/variables/gov/hhs/tanf/cash/eligibility/is_person_demographic_tanf_eligible.py
+++ policyengine_us/variables/gov/hhs/tanf/cash/eligibility/is_person_demographic_tanf_eligible.py
@@ -9,15 +9,17 @@ class is_person_demographic_tanf_eligible(Variable):
     reference = "https://www.law.cornell.edu/cfr/text/45/260.30"
 
     def formula(person, period, parameters):
-        age_limit = parameters(period).gov.hhs.tanf.cash.eligibility.age_limit
+        p = parameters(period).gov.hhs.tanf.cash.eligibility
         age = person("age", period)
-        below_age_limit = age < age_limit
-        at_age_limit = age == age_limit
+        below_age_threshold = age < p.age_threshold
+        below_age_limit = age < p.age_limit
         secondary_school_student = person("is_in_secondary_school", period)
-        secondary_school_at_age_limit_student = (
-            secondary_school_student & at_age_limit
+        secondary_school_below_age_limit_student = (
+            secondary_school_student & below_age_limit
         )
         pregnant = person("is_pregnant", period)
         return (
-            below_age_limit | secondary_school_at_age_limit_student | pregnant
+            below_age_threshold
+            | secondary_school_below_age_limit_student
+            | pregnant
         )
