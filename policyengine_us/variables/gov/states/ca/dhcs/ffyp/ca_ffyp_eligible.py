c8f3d8effff8cc44f7a3073bfa69e7bf79decad9
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2024-10-07 15:26:13 +0000

    Adding comments to be more clear

--- policyengine_us/variables/gov/states/ca/dhcs/ffyp/ca_ffyp_eligible.py
+++ policyengine_us/variables/gov/states/ca/dhcs/ffyp/ca_ffyp_eligible.py
@@ -13,13 +13,19 @@ class ca_ffyp_eligible(Variable):
     def formula(person, period, parameters):
         p = parameters(period).gov.states.ca.dhcs.ffyp
         age = person("age", period)
-        age_eligible = p.foster_care_age_minimum <= age < p.age_limit
+        age_eligible = (
+            p.foster_care_age_minimum < age < p.age_limit
+        )  # Person must be below age limit and previously in foster care for a valid age
         if ~age_eligible:
-            return False
-        age_gap_from_minimum = age - p.foster_care_age_minimum
+            return False  # Person automatically ineligible if not within age bounds
+        age_gap_from_minimum = (
+            age - p.foster_care_age_minimum
+        )  # Find the number of possible years the person could have been in foster care
         head = period
         for _ in range(age_gap_from_minimum + 1):
             if person("was_in_foster_care", head):
-                return True
-            head = head.last_year
+                return True  # Person eligible if age eligible and in foster care for a valid year
+            head = (
+                head.last_year
+            )  # If not eligible yet, check the previous year
         return False
