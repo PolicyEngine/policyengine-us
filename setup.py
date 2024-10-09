84d37dd6e4e3a853fb5d07f17e0889fc69947d02
Anthony Volk <anth.volk@gmail.com>
2024-10-09 19:50:58 +0200

    test: Test core changes on -us

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         ),
     ],
     install_requires=[
-        "policyengine-core>=3.8.1",
+        "policyengine-core @ git+https://git@github.com:anth-volk/policyengine-core.git",
         "policyengine-us-data==1.11.0",
         "microdf-python",
         "tqdm",
