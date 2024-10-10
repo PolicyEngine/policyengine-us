70ce44cb847b4ece2ef38275a806f43260eb3109
vrathi101 <72925608+vrathi101@users.noreply.github.com>
2024-10-10 19:30:38 +0000

    incorporating into medicaid eligible variable

--- policyengine_us/variables/gov/states/ca/dhcs/ffyp/ca_ffyp_eligible.py
+++ policyengine_us/variables/gov/states/ca/dhcs/ffyp/ca_ffyp_eligible.py
@@ -16,16 +16,7 @@ class ca_ffyp_eligible(Variable):
         age_eligible = (
             p.foster_care_age_minimum < age < p.age_limit
         )  # Person must be below age limit and previously in foster care for a valid age
-        if ~age_eligible:
-            return False  # Person automatically ineligible if not within age bounds
-        age_gap_from_minimum = (
-            age - p.foster_care_age_minimum
-        )  # Find the number of possible years the person could have been in foster care
-        head = period
-        for _ in range(age_gap_from_minimum + 1):
-            if person("was_in_foster_care", head):
-                return True  # Person eligible if age eligible and in foster care for a valid year
-            head = (
-                head.last_year
-            )  # If not eligible yet, check the previous year
-        return False
+        was_in_foster_care = person(
+            "was_in_foster_care", period
+        )  # Assumes the person was in foster care for a valid age
+        return age_eligible & was_in_foster_care
