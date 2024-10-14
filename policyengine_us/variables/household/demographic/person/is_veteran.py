7ac465711be676dc36b3769bc37516d51d1461fd
hua7450 <113824411+hua7450@users.noreply.github.com>
2024-10-14 22:17:39 +0000

    add formula for is_veteran.py

--- policyengine_us/variables/household/demographic/person/is_veteran.py
+++ policyengine_us/variables/household/demographic/person/is_veteran.py
@@ -8,3 +8,6 @@ class is_veteran(Variable):
     documentation = "A person who served in the active military, naval, air, or space service, and who was discharged or released therefrom under conditions other than dishonorable."
     reference = "https://www.law.cornell.edu/uscode/text/38/101"  # (2)
     definition_period = YEAR
+
+    def formula(person, period, parameters):
+        return person("veterans_benefits", period) > 0
