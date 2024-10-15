33caa80f8edb8750ff1259e80aefb0d4a08f9fec
hua7450 <113824411+hua7450@users.noreply.github.com>
2024-10-14 21:15:01 -0600

    Kansas Additional Exemption for Disabled Veterans 2023 (#5052)
    * Kansas Additional Exemption for Disabled Veterans 2023
    Fixes #4969
    
    * add test cases, adjust formula
    
    * make format
    
    * adjust formula. create is_veteran variable.
    
    * reorganize parameters
    
    * minor change
    
    * format
    
    * minor change
    
    * add formula for is_veteran.py

--- policyengine_us/variables/gov/states/ks/tax/income/exemptions/ks_exemptions.py
+++ policyengine_us/variables/gov/states/ks/tax/income/exemptions/ks_exemptions.py
@@ -7,13 +7,20 @@ class ks_exemptions(Variable):
     label = "Kansas exemptions amount"
     unit = USD
     definition_period = YEAR
-    reference = (
-        "https://www.ksrevenue.gov/pdf/ip21.pdf"
-        "https://www.ksrevenue.gov/pdf/ip22.pdf"
-    )
+    reference = "https://law.justia.com/codes/kansas/chapter-79/article-32/section-79-32-121/"
     defined_for = StateCode.KS
 
     def formula(tax_unit, period, parameters):
-        exemptions_count = tax_unit("ks_count_exemptions", period)
         p = parameters(period).gov.states.ks.tax.income.exemptions
-        return exemptions_count * p.amount
+        exemptions_count = tax_unit("ks_count_exemptions", period)
+        base_exemptions = exemptions_count * p.base
+        veteran_exemptions_count = add(
+            tax_unit,
+            period,
+            ["ks_disabled_veteran_exemptions_eligible_person"],
+        )
+        additional_exemptions = (
+            veteran_exemptions_count * p.disabled_veteran.base
+        )
+
+        return base_exemptions + additional_exemptions
