1087ad40d873e149771c26e172d4910aaa721b57
PavelMakarchuk <pavel.ma99@gmail.com>
2024-10-17 13:52:52 +0700

    remove aggregation

--- policyengine_us/variables/gov/irs/income/taxable_income/adjusted_gross_income/irs_gross_income/irs_overtime_employment_income.py
+++ policyengine_us/variables/gov/irs/income/taxable_income/adjusted_gross_income/irs_gross_income/irs_overtime_employment_income.py
@@ -13,8 +13,8 @@ class irs_overtime_employment_income(Variable):
 
     def formula(person, period, parameters):
         employment_income = person("employment_income", period)
-        hours_worked = person("weekly_hours_worked", period) * 52
-        overtime_hours = person("weekly_overtime_hours", period) * 52
+        hours_worked = person("weekly_hours_worked", period)
+        overtime_hours = person("weekly_overtime_hours", period)
         overtime_percentage = np.zeros_like(hours_worked)
         mask = hours_worked != 0
         overtime_percentage[mask] = overtime_hours[mask] / hours_worked[mask]
