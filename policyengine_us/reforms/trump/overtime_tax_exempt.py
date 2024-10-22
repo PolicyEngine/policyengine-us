c571b78728309be72a25fa4987c45cf54755814e
PavelMakarchuk <pavel.ma99@gmail.com>
2024-10-22 12:07:56 +0700

    adjust var and test

--- policyengine_us/reforms/trump/overtime_tax_exempt.py
+++ policyengine_us/reforms/trump/overtime_tax_exempt.py
@@ -2,26 +2,51 @@ from policyengine_us.model_api import *
 
 
 def create_trump_overtime_tax_exempt() -> Reform:
-    class taxable_income(Variable):
+    class irs_gross_income(Variable):
         value_type = float
-        entity = TaxUnit
-        label = "IRS taxable income"
+        entity = Person
+        label = "Gross income"
+        unit = USD
+        documentation = (
+            "Gross income, as defined in the Internal Revenue Code."
+        )
+        definition_period = YEAR
+        reference = "https://www.law.cornell.edu/uscode/text/26/61"
+
+        def formula(person, period, parameters):
+            sources = parameters(period).gov.irs.gross_income.sources
+            total = 0
+            not_dependent = ~person("is_tax_unit_dependent", period)
+            for source in sources:
+                # Add positive values only - losses are deducted later.
+                total += not_dependent * max_(0, add(person, period, [source]))
+            overtime_income = person("overtime_income", period)
+            return max_(total - overtime_income, 0)
+
+    class payroll_tax_gross_wages(Variable):
+        value_type = float
+        entity = Person
+        label = "Gross wages and salaries for payroll taxes"
+        definition_period = YEAR
+        unit = USD
+
+        def formula(person, period, parameters):
+            income = person("irs_employment_income", period)
+            overtime_income = person("overtime_income", period)
+            return max_(income - overtime_income, 0)
+
+    class overtime_income(Variable):
+        value_type = float
+        entity = Person
+        label = "Income from overtime hours worked"
         unit = USD
         definition_period = YEAR
 
-        def formula(tax_unit, period, parameters):
-            agi = tax_unit("adjusted_gross_income", period)
-            overtime_hours = add(
-                tax_unit, period, ["irs_overtime_employment_income"]
-            )
-            total_agi = max_(0, agi - overtime_hours)
-            exemptions = tax_unit("exemptions", period)
-            deductions = tax_unit("taxable_income_deductions", period)
-            return max_(0, total_agi - exemptions - deductions)
-
     class reform(Reform):
         def apply(self):
-            self.update_variable(taxable_income)
+            self.update_variable(irs_gross_income)
+            self.update_variable(overtime_income)
+            self.update_variable(payroll_tax_gross_wages)
 
     return reform
 
