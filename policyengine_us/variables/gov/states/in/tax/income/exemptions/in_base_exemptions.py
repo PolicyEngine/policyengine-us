b4cfa163442970459cc9bf63c24369155f99829f
samantharudra <98905595+samantharudra@users.noreply.github.com>
2024-10-28 23:21:02 +0000

    Create new structure for Indiana dependent exemptions Fixes #5223

--- policyengine_us/variables/gov/states/in/tax/income/exemptions/in_base_exemptions.py
+++ policyengine_us/variables/gov/states/in/tax/income/exemptions/in_base_exemptions.py
@@ -1,6 +1,5 @@
 from policyengine_us.model_api import *
 
-
 class in_base_exemptions(Variable):
     value_type = float
     entity = TaxUnit
@@ -9,23 +8,16 @@ class in_base_exemptions(Variable):
     definition_period = YEAR
     reference = "http://iga.in.gov/legislative/laws/2021/ic/titles/006#6-3-1-3.5"  # (a)(3)-(4)
     defined_for = StateCode.IN
-
-
+    
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states["in"].tax.income.exemptions
 	    filing_status = tax_unit("filing_status", period)
-	
 	# Personal exemptions
 	    personal_exemptions = p.base.amount[filing_status]
-
-	
 	# Dependent exemptions
         dependents = tax_unit(
             "tax_unit_dependents", period
         )  # Total the number of dependents
-
         dependent_exemptions = dependents * p.base.dependent
-
-
     # total exemptions
         return personal_exemptions + dependent_exemptions
