3e2e64c3184a32be52e8a5de07de09e05f5ac33d
Caleb <caleblpena@gmail.com>
2024-10-28 14:36:07 -0600

    use marital both eligible variable

--- policyengine_us/variables/gov/states/co/ssa/oap/co_oap.py
+++ policyengine_us/variables/gov/states/co/ssa/oap/co_oap.py
@@ -10,10 +10,10 @@ class co_oap(Variable):
 
     def formula(person, period, parameters):
         income = person("ssi_countable_income", period)
-        is_joint = person("ssi_claim_is_joint", period)
+        both_eligible = person("ssi_marital_both_eligible", period)
         ssi = person("ssi", period)
         # The SSI variable assigns SSI to one member of the marital unit if both are eligible
-        marital_ssi = where(is_joint, person.marital_unit.sum(ssi) / 2, ssi)
+        marital_ssi = where(both_eligible, person.marital_unit.sum(ssi) / 2, ssi)
         total_countable_income = marital_ssi + income
         p = parameters(period).gov.states.co.ssa.oap
         grant_standard = p.grant_standard * MONTHS_IN_YEAR
