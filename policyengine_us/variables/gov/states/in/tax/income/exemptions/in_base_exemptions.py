d95836eac8d0453fd5aa271e94ee15610b890ba2
samantharudra <98905595+samantharudra@users.noreply.github.com>
2024-10-29 21:29:59 +0000

    Revised
    Co-authored-by: Pavel Makarchuk <PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/states/in/tax/income/exemptions/in_base_exemptions.py
+++ policyengine_us/variables/gov/states/in/tax/income/exemptions/in_base_exemptions.py
@@ -1,5 +1,6 @@
 from policyengine_us.model_api import *
 
+
 class in_base_exemptions(Variable):
     value_type = float
     entity = TaxUnit
@@ -8,16 +9,17 @@ class in_base_exemptions(Variable):
     definition_period = YEAR
     reference = "http://iga.in.gov/legislative/laws/2021/ic/titles/006#6-3-1-3.5"  # (a)(3)-(4)
     defined_for = StateCode.IN
-    
+
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states["in"].tax.income.exemptions
-	    filing_status = tax_unit("filing_status", period)
-	# Personal exemptions
-	    personal_exemptions = p.base.amount[filing_status]
-	# Dependent exemptions
-        dependents = tax_unit(
-            "tax_unit_dependents", period
-        )  # Total the number of dependents
+        # Personal exemptions
+        non_dependent_exemptions = p.base.amount
+        # Dependent exemptions
+        dependents = tax_unit("tax_unit_dependents", period)
+        unit_size = tax_unit("tax_unit_size", period)
+        unit_without_dependents = max_(unit_size - dependents, 0)
+        total_base = unit_without_dependents * non_dependent_exemptions
+        # Total the number of dependents
         dependent_exemptions = dependents * p.base.dependent
-    # total exemptions
-        return personal_exemptions + dependent_exemptions
+        # total exemptions
+        return total_base + dependent_exemptions
