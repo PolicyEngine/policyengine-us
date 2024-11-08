145e5fe513b9a6a74be7e31e6dc56e4f6b281f31
Anthony Volk <anth.volk@gmail.com>
2024-11-09 00:28:22 +0100

    fix: Correct meets_school_meal_categorical_eligibility for vectorized inputs

--- policyengine_us/variables/gov/usda/school_meals/meets_school_meal_categorical_eligibility.py
+++ policyengine_us/variables/gov/usda/school_meals/meets_school_meal_categorical_eligibility.py
@@ -12,4 +12,4 @@ class meets_school_meal_categorical_eligibility(Variable):
     def formula(spm_unit, period, parameters):
         p = parameters(period).gov.usda.school_meals
         programs = add(spm_unit, period, p.categorical_eligibility)
-        return np.any(programs)
+        return programs > 0
