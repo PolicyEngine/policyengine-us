368f34ccad08617c55609ce5181031213e729530
PavelMakarchuk <pavel.ma99@gmail.com>
2024-11-10 19:05:20 -0500

    format

--- policyengine_us/variables/household/demographic/person/in_foster_care_group_home.py
+++ policyengine_us/variables/household/demographic/person/in_foster_care_group_home.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class is_in_foster_care_group_home(Variable):
     value_type = bool
     entity = Person
-    label = "Person is currently in the a qualifying foster care group home"
+    label = "Person is currently in a qualifying foster care group home"
     definition_period = YEAR
     default_value = False
     defined_for = "is_in_foster_care"
--- policyengine_us/variables/household/demographic/person/months_pregnant.py
+++ policyengine_us/variables/household/demographic/person/months_pregnant.py
@@ -4,5 +4,5 @@ from policyengine_us.model_api import *
 class months_pregnant(Variable):
     value_type = int
     entity = Person
-    label = "Number of months into the pregnancy"
+    label = "Number of pregnancy months completed"
     definition_period = YEAR
