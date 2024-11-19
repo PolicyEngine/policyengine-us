f9b90a24ecc76fffdbebb697fc953b4d1b83024f
Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>
2024-11-19 11:41:24 -0500

    Los Angeles Infant Supplement and Expectant Parent Payment age threshold (#5344)
    * Los Angeles Infant Supplement and Expectant Parent Payment age threshold
    Fixes #5343
    
    * format and new foster care minor dependent var
    
    * naming
    
    * add monthly age variable and testing
    
    * Update policyengine_us/variables/household/demographic/age/monthly_age.py
    
    ---------
    
    Co-authored-by: Max Ghenis <mghenis@gmail.com>

--- policyengine_us/variables/gov/local/ca/la/dss/expectant_parent_payment/ca_la_expectant_parent_payment_eligible.py
+++ policyengine_us/variables/gov/local/ca/la/dss/expectant_parent_payment/ca_la_expectant_parent_payment_eligible.py
@@ -9,10 +9,12 @@ class ca_la_expectant_parent_payment_eligible(Variable):
     defined_for = "in_la"
 
     def formula(person, period, parameters):
-        foster_care = person("is_in_foster_care", period)
         pregnancy_month = person("current_pregnancy_month", period)
         p = parameters(period).gov.local.ca.la.dss.expectant_parent_payment
         eligible_based_on_pregnancy_month = (
             p.pregnancy_month.min <= pregnancy_month <= p.pregnancy_month.max
         )
-        return foster_care & eligible_based_on_pregnancy_month
+        foster_care_minor_dependent = person(
+            "ca_foster_care_minor_dependent", period
+        )
+        return foster_care_minor_dependent & eligible_based_on_pregnancy_month
