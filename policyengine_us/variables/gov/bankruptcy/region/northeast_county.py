3f0973f0a9176e0ca0c4683b73ae6cfe86e4eb1c
hua7450 <113824411+hua7450@users.noreply.github.com>
2024-11-21 05:48:26 +0000

    draft

---
+++ policyengine_us/variables/gov/bankruptcy/region/northeast_county.py
@@ -0,0 +1,48 @@
+from policyengine_us.model_api import *
+
+
+class NorthEastCounty(Enum):
+    BOSTON = "Boston"
+    NEW_YORK = "New York"
+    PHILADELPHIA = "Philadelphia"
+    NORTHEAST = "Northeast"
+
+
+class northeast_county(Variable):
+    value_type = Enum
+    entity = Household
+    possible_values = NorthEastCounty
+    default_value = NorthEastCounty.NORTHEAST
+    definition_period = YEAR
+    label = "South Carolina use tax county group"
+    
+
+    def formula(household, period, parameters):
+
+        county = household("county_str", period)
+
+        boston_counties = parameters(
+            period
+        ).gov.bankruptcy.local_standards.vehicle_operation.region_group.northeast.boston
+
+        new_york_counties = parameters(
+            period
+        ).gov.bankruptcy.local_standards.vehicle_operation.region_group.northeast.new_york
+
+        philadelphia_counties = parameters(
+            period
+        ).gov.bankruptcy.local_standards.vehicle_operation.region_group.northeast.philadelphia
+
+        return select(
+            [
+                np.isin(county, boston_counties),
+                np.isin(county, new_york_counties),
+                np.isin(county, philadelphia_counties),
+            ],
+            [
+                NorthEastCounty.BOSTON,
+                NorthEastCounty.NEW_YORK,
+                NorthEastCounty.PHILADELPHIA,
+            ],
+            default=NorthEastCounty.NORTHEAST,
+        )
