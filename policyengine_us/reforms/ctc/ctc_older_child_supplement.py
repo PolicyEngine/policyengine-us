7abc589100b4103f119967b29bc62c2d141b9113
PavelMakarchuk <pavel.ma99@gmail.com>
2024-11-20 20:55:40 -0500

    format

--- policyengine_us/reforms/ctc/ctc_older_child_supplement.py
+++ policyengine_us/reforms/ctc/ctc_older_child_supplement.py
@@ -16,16 +16,16 @@ def create_ctc_older_child_supplement() -> Reform:
             "https://www.law.cornell.edu/uscode/text/26/24#h",
             "https://www.law.cornell.edu/uscode/text/26/24#i",
         )
+        defined_for = "is_tax_unit_dependent"
 
         def formula(person, period, parameters):
             age = person("age", period)
             base_amount = parameters(period).gov.irs.credits.ctc.amount.base
-            is_dependent = person("is_tax_unit_dependent", period)
             base_amount = base_amount.calc(age)
-            child_index = person("child_index", period)
+            oldest_child = person("child_index", period) == 1
             p_ref = parameters(period).gov.contrib.ctc.oldest_child_supplement
-            supplement = where(child_index == 1, p_ref.amount, 0)
-            return (base_amount + supplement) * is_dependent
+            oldest_child_supplement = oldest_child * p_ref.amount
+            return base_amount + oldest_child_supplement
 
     class reform(Reform):
         def apply(self):
