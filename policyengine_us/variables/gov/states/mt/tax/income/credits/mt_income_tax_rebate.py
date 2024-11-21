00b42fa6119bf855a27e60ddfcafa9cda971a32e
PavelMakarchuk <pavel.ma99@gmail.com>
2024-11-20 23:28:05 -0500

    Montana 2023 income tax rebate Fixes #5338

---
+++ policyengine_us/variables/gov/states/mt/tax/income/credits/mt_income_tax_rebate.py
@@ -0,0 +1,16 @@
+from policyengine_us.model_api import *
+
+
+class mt_income_tax_rebate(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Montana 2023 income tax rebate"
+    unit = USD
+    definition_period = YEAR
+    reference = "https://archive.legmt.gov/bills/mca/title_0150/chapter_0300/part_0210/section_0910/0150-0300-0210-0910.html"
+    defined_for = StateCode.MT
+
+    def formula(tax_unit, period, parameters):
+        p = parameters(period).gov.states.mt.tax.income.credits.rebate
+        filing_status = tax_unit("filing_status", period)
+        return p.amount[filing_status]
