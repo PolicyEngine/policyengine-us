66b5525302ec14d466c26adfb4686e4e30d93579
PavelMakarchuk <pavel.ma99@gmail.com>
2024-11-27 09:54:44 -0500

    Louisiana replaces personal and dependent exemption with higher standard deduction in 2025 Fixes #5371

--- policyengine_us/variables/gov/states/la/tax/income/la_taxable_income.py
+++ policyengine_us/variables/gov/states/la/tax/income/la_taxable_income.py
@@ -10,14 +10,24 @@ class la_taxable_income(Variable):
     definition_period = YEAR
 
     def formula(tax_unit, period, parameters):
+        agi = tax_unit("la_agi", period)
+
         itemizes = tax_unit("tax_unit_itemizes", period)
         # Louisana does not provide a standard deduction
         itemized_deductions = tax_unit("la_itemized_deductions", period)
         claimed_itemized_deductions = itemizes * itemized_deductions
         fed_tax_deduction = tax_unit("la_federal_tax_deduction", period)
+        p = parameters(period).gov.states.la.tax.income.deductions.standard
+        if p.applies:
+            standard_deduction = tax_unit("la_standard_deductions", period)
+            return max_(
+                agi
+                - standard_deduction
+                - claimed_itemized_deductions
+                - fed_tax_deduction,
+                0,
+            )
         return max_(
-            tax_unit("la_agi", period)
-            - claimed_itemized_deductions
-            - fed_tax_deduction,
+            agi - claimed_itemized_deductions - fed_tax_deduction,
             0,
         )
