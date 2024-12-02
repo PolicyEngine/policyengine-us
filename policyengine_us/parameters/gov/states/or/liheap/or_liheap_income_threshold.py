e1cf00bc0f3284f236bb1c97fb69f60a9a6df7e6
srilareddy <srilareddy09@gmail.com>
2024-12-02 13:41:42 +0000

    seperated files and changed loaction

---
+++ policyengine_us/parameters/gov/states/or/liheap/or_liheap_income_threshold.py
@@ -0,0 +1,15 @@
+from policyengine_us.model_api import *
+
+class or_liheap_income_threshold(Variable):
+        value_type = float
+        entity = TaxUnit
+        label = "Income threshold for Oregon LIHEAP eligibility"
+        unit = USD
+        definition_period = YEAR
+        reference = "https://www.oregon.gov/ohcs/hcs-liheap.aspx"
+        defined_for = StateCode.OR
+        
+        def formula(tax_unit, period, parameters):
+            state_median_income = tax_unit("hhs_smi", period)  
+            p = parameters(period).gov.states.["or"].liheap.eligibility
+            return state_median_income * p.eligibility
