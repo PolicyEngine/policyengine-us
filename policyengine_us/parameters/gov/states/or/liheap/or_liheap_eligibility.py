1a65c5efe96f210611eb902c616ec4faad892318
srilareddy <srilareddy09@gmail.com>
2024-12-02 14:31:56 +0000

    switched tax unit income to adjusted gross income

--- policyengine_us/parameters/gov/states/or/liheap/or_liheap_eligibility.py
+++ policyengine_us/parameters/gov/states/or/liheap/or_liheap_eligibility.py
@@ -1,6 +1,6 @@
 from policyengine_us.model_api import *
 
- class or_liheap_eligibility(Variable):
+class or_liheap_eligibility(Variable):
         value_type = bool
         entity = TaxUnit
         label = "Oregon LIHEAP eligibility"
@@ -9,6 +9,6 @@ from policyengine_us.model_api import *
         defined_for = StateCode.OR
 
         def formula(tax_unit, period, parameters):
-            income = tax_unit("tax_unit_income", period)  
+            income = tax_unit("adjusted_gross_income", period)  
             threshold = tax_unit("or_liheap_income_threshold", period)
             return income <= threshold
