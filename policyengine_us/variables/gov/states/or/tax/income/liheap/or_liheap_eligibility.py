627789fbbaf8c162cd617a3088621f6ce0e4e067
srilareddy <srilareddy09@gmail.com>
2024-12-16 09:52:10 +0000

    finished tests

--- policyengine_us/variables/gov/states/or/tax/income/liheap/or_liheap_eligibility.py
+++ policyengine_us/variables/gov/states/or/tax/income/liheap/or_liheap_eligibility.py
@@ -1,14 +1,15 @@
 from policyengine_us.model_api import *
 
-class or_liheap_eligibility(Variable):
-        value_type = bool
-        entity = SPMUnit
-        label = "Oregon LIHEAP eligibility"
-        definition_period = YEAR
-        reference = "https://liheapch.acf.hhs.gov/profiles/Oregon.htm"
-        defined_for = StateCode.OR
 
-        def formula(spm_unit, period, parameters):
-            income = spm_unit.tax_unit("adjusted_gross_income", period)  
-            threshold = spm_unit("or_liheap_income_threshold", period)
-            return income <= threshold
+class or_liheap_eligibility(Variable):
+    value_type = bool
+    entity = SPMUnit
+    label = "Oregon LIHEAP eligibility"
+    definition_period = YEAR
+    reference = "https://liheapch.acf.hhs.gov/profiles/Oregon.htm"
+    defined_for = StateCode.OR
+
+    def formula(spm_unit, period, parameters):
+        income = add(spm_unit, period, ["adjusted_gross_income"])
+        threshold = spm_unit("or_liheap_income_threshold", period)
+        return income <= threshold
