acb5345293612a11802f3e8a05a155aed1fab065
PavelMakarchuk <pavel.ma99@gmail.com>
2024-12-24 14:00:17 -0500

    format

--- policyengine_us/variables/gov/ssa/ssi/eligibility/status/is_ssi_qualified_noncitizen.py
+++ policyengine_us/variables/gov/ssa/ssi/eligibility/status/is_ssi_qualified_noncitizen.py
@@ -12,30 +12,25 @@ class is_ssi_qualified_noncitizen(Variable):
         qualifying_quarters_earnings = person(
             "ssi_qualifying_quarters_earnings", period
         )
-        qualifying_quarters_threshold = parameters(
-            period
-        ).gov.ssa.ssi.income.sources.qualifying_quarters_threshold
-        qualified_statuses = parameters(
-            period
-        ).gov.ssa.ssi.eligibility.status.qualified_noncitizen_status
-
+        p = parameters(period).gov.ssa.ssi
         immigration_status = person("immigration_status", period)
         legal_permanent_resident = (
             immigration_status
             == immigration_status.possible_values.LEGAL_PERMANENT_RESIDENT
         )
+        # Need qualifying quarters if person is a legal permanent resident.
+        has_qualifying_quarters = (
+            qualifying_quarters_earnings
+            >= p.income.sources.qualifying_quarters_threshold
+        )
         earnings_quarters_eligible = where(
             legal_permanent_resident,
-            qualifying_quarters_earnings >= qualifying_quarters_threshold,
+            has_qualifying_quarters,
             True,
         )
         immigration_status_str = immigration_status.decode_to_str()
-
         qualifies_based_on_status = np.isin(
-            immigration_status_str, qualified_statuses
+            immigration_status_str,
+            p.eligibility.status.qualified_noncitizen_status,
         )
-        return where(
-            qualifies_based_on_status,
-            earnings_quarters_eligible,
-            False,
-        )
+        return qualifies_based_on_status & earnings_quarters_eligible
