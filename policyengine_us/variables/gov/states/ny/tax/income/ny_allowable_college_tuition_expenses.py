284c699c2c6ea03938fc922318d523657646372d
Max Ghenis <mghenis@gmail.com>
2025-01-04 13:15:01 -0500

    fix variable

--- policyengine_us/variables/gov/states/ny/tax/income/ny_allowable_college_tuition_expenses.py
+++ policyengine_us/variables/gov/states/ny/tax/income/ny_allowable_college_tuition_expenses.py
@@ -7,13 +7,11 @@ class ny_allowable_college_tuition_expenses(Variable):
     label = "New York allowable college tuition expenses for the credit and deduction"
     unit = USD
     definition_period = YEAR
-    reference = (
-        "https://www.nysenate.gov/legislation/laws/TAX/606"  # (t)(2)(A)
-    )
+    reference = "https://www.nysenate.gov/legislation/laws/TAX/606"  # (t)(2)(A)
     defined_for = StateCode.NY
 
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states.ny.tax.income.college_tuition
         person = tax_unit.members
-        capped = min_(person("qualified_tuition_expenses"), p.cap)
+        capped = min_(person("qualified_tuition_expenses", period), p.cap)
         return tax_unit.sum(capped)
