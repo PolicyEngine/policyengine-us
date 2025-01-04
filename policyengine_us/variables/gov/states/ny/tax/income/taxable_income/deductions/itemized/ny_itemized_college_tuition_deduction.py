96ee8cbc7bd5a1dc67f8b9680fa088aefaf51900
Max Ghenis <mghenis@gmail.com>
2025-01-03 19:02:31 -0500

    Do not cap real estate taxes for New York itemized deductions Fixes #5430

---
+++ policyengine_us/variables/gov/states/ny/tax/income/taxable_income/deductions/itemized/ny_itemized_college_tuition_deduction.py
@@ -0,0 +1,21 @@
+from policyengine_us.model_api import *
+
+
+class ny_itemized_college_tuition_deduction(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "New York itemized deduction for college tuition expenses"
+    unit = USD
+    definition_period = YEAR
+    reference = (
+        "https://www.nysenate.gov/legislation/laws/TAX/615",  # (d)(4)
+        "https://www.nysenate.gov/legislation/laws/TAX/606",  # (t)
+    )
+    defined_for = StateCode.NY
+
+    def formula(tax_unit, period, parameters):
+        p = parameters(period).gov.states.ny.tax.income
+        return min_(
+            p.deductions.itemized.college_tuition_max,
+            add(tax_unit, period, ["qualified_tuition_expenses"]),
+        )
