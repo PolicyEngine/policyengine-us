ffc853778a5ddcaf1b286fa3578204365ef656c8
Max Ghenis <mghenis@gmail.com>
2025-01-05 23:35:07 -0500

    type fix

--- policyengine_us/variables/gov/states/ny/tax/income/credits/college_tuition/ny_college_tuition_credit_eligible.py
+++ policyengine_us/variables/gov/states/ny/tax/income/credits/college_tuition/ny_college_tuition_credit_eligible.py
@@ -2,10 +2,9 @@ from policyengine_us.model_api import *
 
 
 class ny_college_tuition_credit_eligible(Variable):
-    value_type = float
+    value_type = bool
     entity = TaxUnit
-    label = "NY college tuition credit eligible"
-    unit = USD
+    label = "New York college tuition credit eligible"
     definition_period = YEAR
     reference = "https://www.nysenate.gov/legislation/laws/TAX/606"  # (t)
     defined_for = StateCode.NY
