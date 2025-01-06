8fdd7fe71e9c0ac16b82accede628eb65a7996d0
hua7450 <113824411+hua7450@users.noreply.github.com>
2025-01-06 21:40:43 +0000

    minor

--- policyengine_us/variables/gov/bankruptcy/deductions/chapter_7_bankruptcy_vehicle_operation_expense_deduction.py
+++ policyengine_us/variables/gov/bankruptcy/deductions/chapter_7_bankruptcy_vehicle_operation_expense_deduction.py
@@ -13,10 +13,12 @@ class chapter_7_bankruptcy_vehicle_operation_expense_deduction(Variable):
         p = parameters(period).gov.bankruptcy.local_standards.vehicle_operation
 
         qualify_vehicles_owned = spm_unit("vehicles_loan_count", period)
-        #qualify_vehicles_owned_capped = min_(
+        # qualify_vehicles_owned_capped = min_(
         #    qualify_vehicles_owned, p.vehicles_owned_cap
-        #)
-        qualify_vehicles_owned_capped = clip(qualify_vehicles_owned, 1, p.vehicles_owned_cap)
+        # )
+        qualify_vehicles_owned_capped = clip(
+            qualify_vehicles_owned, 1, p.vehicles_owned_cap
+        )
         northeast = spm_unit.household("northeastern_county", period)
         midwest = spm_unit.household("midwestern_county", period)
         south = spm_unit.household("southern_county", period)
