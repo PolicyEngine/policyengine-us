d00c8c616903584b342c7781b7b447bcc9e8088e
Max Ghenis <mghenis@gmail.com>
2025-01-06 22:15:58 -0500

    fix autogenerated formula

--- policyengine_us/variables/gov/states/tax/income/_generate_state_mfs_variables.py
+++ policyengine_us/variables/gov/states/tax/income/_generate_state_mfs_variables.py
@@ -132,9 +132,9 @@ for state_code, state_name in [
                 unit=USD,
                 definition_period=YEAR,
                 defined_for=getattr(StateCode, state_code.upper()),
-                formula=lambda self, tax_unit, period, parameters, indiv=indiv_var, joint=joint_var: where(
+                formula=lambda tax_unit, period, parameters, indiv=indiv_var, joint=joint_var: where(
                     tax_unit(
-                        f"{self.__class__.__name__.split('_')[0]}_files_separately",
+                        f"{indiv.split('_')[0]}_files_separately",  # Get state code from the variable name
                         period,
                     ),
                     add(tax_unit, period, [indiv]),
