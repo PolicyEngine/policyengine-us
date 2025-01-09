a5ec8a0d543e204f6728e463caef8585e51b75a3
PavelMakarchuk <pavel.ma99@gmail.com>
2025-01-09 13:01:15 -0500

    changelog entry

--- policyengine_us/variables/gov/territories/pr/tax/income/credits/pr_refundable_credits.py
+++ policyengine_us/variables/gov/territories/pr/tax/income/credits/pr_refundable_credits.py
@@ -1,10 +1,10 @@
 from policyengine_us.model_api import *
 
 
-class pr_refundable_credit(Variable):
+class pr_refundable_credits(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Puerto Rico refundable credit"
+    label = "Puerto Rico refundable credits"
     unit = USD 
     definition_period = YEAR
 
