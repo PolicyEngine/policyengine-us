c71fee533355710f2add9af92e9b1a9e0c3c64f4
srilareddy <srilareddy09@gmail.com>
2025-01-13 17:34:54 +0000

    corrected income range function

--- policyengine_us/variables/gov/states/or/tax/income/liheap/or_liheap_income_range.py
+++ policyengine_us/variables/gov/states/or/tax/income/liheap/or_liheap_income_range.py
@@ -1,30 +1,38 @@
 from policyengine_us.model_api import *
 
+class OregonLIHEAPIncomeRange(Enum):
+    RANGE_ONE = "Range one"
+    RANGE_TWO = "Range two"
+    RANGE_THREE = "Range three"
+    RANGE_FOUR = "Range four"
 
 class or_liheap_income_range(Variable):
-    value_type = int
+    value_type = Enum
     entity = SPMUnit
+    possible_values = OregonLIHEAPIncomeRange
+    default_value = OregonLIHEAPIncomeRange.RANGE_FOUR
     label = "Income range for Oregon LIHEAP eligibility"
     definition_period = YEAR
     reference = "https://www.oregon.gov/ohcs/energy-weatherization/Documents/2021-Energy-Assistance-Manual.pdf#page=55"
-
+    
     defined_for = StateCode.OR
 
     def formula(spm_unit, period, parameters):
         income = spm_unit("adjusted_gross_income", period)
         threshold = spm_unit("or_liheap_income_threshold", period)
         p = parameters(period).gov.states["or"].liheap.income_range
-
+        
         range1_upper = threshold * p.range1_upper
         range2_upper = threshold * p.range2_upper
         range3_upper = threshold * p.range3_upper
-
         return select(
             [
                 income <= range1_upper,
                 income <= range2_upper,
                 income <= range3_upper,
             ],
-            [1, 2, 3],
-            default=4,
+            [OregonLIHEAPIncomeRange.RANGE_ONE,
+             OregonLIHEAPIncomeRange.RANGE_TWO,
+             OregonLIHEAPIncomeRange.RANGE_THREE],
+            default=OregonLIHEAPIncomeRange.RANGE_FOUR,
         )
