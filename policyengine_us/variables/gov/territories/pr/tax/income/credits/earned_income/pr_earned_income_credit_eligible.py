eb9f6befe361213f339819979d754520d5cda079
Lucy Lennemann <78785519+llennemann@users.noreply.github.com>
2025-01-16 08:08:11 +0000

    Puerto Rico Earned Income Credit Fixes #5467

--- policyengine_us/variables/gov/territories/pr/tax/income/credits/earned_income/pr_earned_income_credit_eligible.py
+++ policyengine_us/variables/gov/territories/pr/tax/income/credits/earned_income/pr_earned_income_credit_eligible.py
@@ -1,9 +1,7 @@
 from policyengine_us.model_api import *
 
-
 class pr_earned_income_credit_eligible(Variable):
     value_type = bool
-    unit = USD
     entity = Person
     label = ""
     definition_period = YEAR
@@ -14,6 +12,7 @@ class pr_earned_income_credit_eligible(Variable):
             period
         ).gov.territories.pr.tax.income.credits.earned_income
         head_or_spouse = person("is_tax_unit_head_or_spouse", period)
-        net_income = person("") # if net_income (interest, child care payments etc.)
-
-        return head_or_spouse & net_income < p.net_income_limit
+        adds = "gov.territories.pr.tax.income.credits.earned_income.ineligible_income_categories"
+        investment_income_amount_allowed = adds < p.max_investment_income 
+        # check that earned income is above 0?
+        return head_or_spouse & investment_income_amount_allowed
