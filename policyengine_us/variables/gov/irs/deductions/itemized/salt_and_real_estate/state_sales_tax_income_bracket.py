9f509de8b53031835e06b762c5c95b0932e5575d
liwenxin491 <160153575+liwenxin491@users.noreply.github.com>
2025-01-21 02:08:39 +0000

    The formula cannot be called in state_sales_tax_income_bracket.py

--- policyengine_us/variables/gov/irs/deductions/itemized/salt_and_real_estate/state_sales_tax_income_bracket.py
+++ policyengine_us/variables/gov/irs/deductions/itemized/salt_and_real_estate/state_sales_tax_income_bracket.py
@@ -1,36 +1,39 @@
 from policyengine_us.model_api import *
 
 class StateSalesTaxIncomeBracket(Enum):
-    One = "ONE"
-    Two = "TWO"
-    Three = "THREE"
-    Four = "FOUR"
-    Five = "FIVE"
-    Six = "SIX"
-    Seven = "SEVEN"
-    Eight = "EIGHT"
-    Nine = "NINE"
-    Ten = "TEN"
-    Eleven = "ELEVEN"
-    Twelve = "TWELVE"
-    Thirteen = "THIRTEEN"
-    Fourteen = "FOURTEEN"
-    Fifteen = "FIFTEEN"
-    Sixteen = "SIXTEEN"
-    Seventeen = "SEVENTEEN"
-    Eighteen = "EIGHTEEN"
-    Nineteen = "NINETEEN"
-
+    ONE = "ONE"
+    TWO = "TWO"
+    THREE = "THREE"
+    FOUR = "FOUR"
+    FIVE = "FIVE"
+    SIX = "SIX"
+    SEVEN = "SEVEN"
+    EIGHT = "EIGHT"
+    NINE = "NINE"
+    TEN = "TEN"
+    ELEVEN = "ELEVEN"
+    TWELVE = "TWELVE"
+    THIRTEEN = "THIRTEEN"
+    FOURTEEN = "FOURTEEN"
+    FIFTEEN = "FIFTEEN"
+    SIXTEEN = "SIXTEEN"
+    SEVENTEEN = "SEVENTEEN"
+    EIGHTEEN = "EIGHTEEN"
+    NINETEEN = "NINETEEN"
 
 class state_sales_tax_income_bracket(Variable):
     value_type = Enum
     entity = TaxUnit
     possible_values = StateSalesTaxIncomeBracket
+    default_value = StateSalesTaxIncomeBracket.ONE
     definition_period = YEAR
     label = "State Sales Tax Income Bracket"
 
     def formula(tax_unit, period, parameters):
+        print("Formula method called")  # The Formula is not called
         income = tax_unit("total_income_tax", period)
+        print(f"Income: {income}")  # No output
+
         return select(
             [
                 (income > 0) & (income < 20_000),
@@ -40,40 +43,39 @@ class state_sales_tax_income_bracket(Variable):
                 (income >= 50_000) & (income < 60_000),
                 (income >= 60_000) & (income < 70_000),
                 (income >= 70_000) & (income < 80_000),
-                (income >= 80_000 & income < 90_000),
-                (income >= 90_000 & income < 100_000),
-                (income >= 100_000 & income < 120_000),
-                (income >= 120_000 & income < 140_000),
-                (income >= 140_000 & income < 160_000),
-                (income >= 160_000 & income < 180_000),
-                (income >= 180_000 & income < 200_000),
-                (income >= 200_000 & income < 225_000),
-                (income >= 225_000 & income < 250_000),
-                (income >= 250_000 & income < 275_000),
-                (income >= 275_000 & income < 300_000),
+                (income >= 80_000) & (income < 90_000),
+                (income >= 90_000) & (income < 100_000),
+                (income >= 100_000) & (income < 120_000),
+                (income >= 120_000) & (income < 140_000),
+                (income >= 140_000) & (income < 160_000),
+                (income >= 160_000) & (income < 180_000),
+                (income >= 180_000) & (income < 200_000),
+                (income >= 200_000) & (income < 225_000),
+                (income >= 225_000) & (income < 250_000),
+                (income >= 250_000) & (income < 275_000),
+                (income >= 275_000) & (income < 300_000),
                 (income >= 300_000),
             ],
             [
-                StateSalesTaxIncomeBracket.One,
-                StateSalesTaxIncomeBracket.Two,
-                StateSalesTaxIncomeBracket.Three,
-                StateSalesTaxIncomeBracket.Four,
-                StateSalesTaxIncomeBracket.Five,
-                StateSalesTaxIncomeBracket.Six,
-                StateSalesTaxIncomeBracket.Seven,
-                StateSalesTaxIncomeBracket.Eight,
-                StateSalesTaxIncomeBracket.Nine,
-                StateSalesTaxIncomeBracket.Ten,
-                StateSalesTaxIncomeBracket.Eleven,
-                StateSalesTaxIncomeBracket.Twelve,
-                StateSalesTaxIncomeBracket.Thirteen,
-                StateSalesTaxIncomeBracket.Fourteen,
-                StateSalesTaxIncomeBracket.Fifteen,
-                StateSalesTaxIncomeBracket.Sixteen,
-                StateSalesTaxIncomeBracket.Seventeen,
-                StateSalesTaxIncomeBracket.Eighteen,
-                StateSalesTaxIncomeBracket.Nineteen,
+                StateSalesTaxIncomeBracket.ONE,
+                StateSalesTaxIncomeBracket.TWO,
+                StateSalesTaxIncomeBracket.THREE,
+                StateSalesTaxIncomeBracket.FOUR,
+                StateSalesTaxIncomeBracket.FIVE,
+                StateSalesTaxIncomeBracket.SIX,
+                StateSalesTaxIncomeBracket.SEVEN,
+                StateSalesTaxIncomeBracket.EIGHT,
+                StateSalesTaxIncomeBracket.NINE,
+                StateSalesTaxIncomeBracket.TEN,
+                StateSalesTaxIncomeBracket.ELEVEN,
+                StateSalesTaxIncomeBracket.TWELVE,
+                StateSalesTaxIncomeBracket.THIRTEEN,
+                StateSalesTaxIncomeBracket.FOURTEEN,
+                StateSalesTaxIncomeBracket.FIFTEEN,
+                StateSalesTaxIncomeBracket.SIXTEEN,
+                StateSalesTaxIncomeBracket.SEVENTEEN,
+                StateSalesTaxIncomeBracket.EIGHTEEN,
+                StateSalesTaxIncomeBracket.NINETEEN,
 
-            ],
-            default=StateSalesTaxIncomeBracket.One,
+            ]
         )
