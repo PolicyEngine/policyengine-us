04abea089bc67e991b6337f4d5d9d0cb2c71bfd2
daphnehanse11 <128793799+daphnehanse11@users.noreply.github.com>
2025-01-23 14:45:36 -0500

    tax unit calc

--- policyengine_us/variables/gov/aca/slspc/slspc_age_adjusted_cost.py
+++ policyengine_us/variables/gov/aca/slspc/slspc_age_adjusted_cost.py
@@ -1,49 +1,10 @@
 from policyengine_us.model_api import *
 
-
-class slspc_age_adjusted_cost_person(Variable):
+class slspc_age_adjusted_cost(Variable):
     value_type = float
-    entity = Person
+    entity = TaxUnit
     label = "Second-lowest ACA silver-plan cost adjusted for age"
     unit = USD
     definition_period = MONTH
 
-    def formula(person, period, parameters):
-        state = person.household("state_code_str", period)
-        age = person("monthly_age", period)
-        base_cost = person.household("slspc_baseline_cost", period)
-
-        # Get age curve based on state
-        special_states = [
-            "district_of_columbia",
-            "alabama",
-            "massachusetts",
-            "minnesota",
-            "mississippi",
-            "oregon",
-            "utah",
-        ]
-
-        p = parameters(period).gov.aca.age_curved
-        applicable_rate = select(
-            [
-                state_code == "AL",
-                state_code == "DC",
-                state_code == "MA",
-                state_code == "MN",
-                state_code == "MS",
-                state_code == "OR",
-                state == "UT",
-            ],
-            [
-                p.alabama.calc(age),
-                p.district_of_columbia.calc(age),
-                p.massachusetts.calc(age),
-                p.minnesota.calc(age),
-                p.mississippi.calc(age),
-                p.oregon.calc(age),
-                p.utah.calc(age),
-            ],
-            default=p.default.calc(age),
-        )
-        return base_cost * applicable_rate
+    adds = ["slspc_age_adjusted_cost_person"]
