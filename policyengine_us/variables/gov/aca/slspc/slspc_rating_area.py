c83e777e234026c1f55626fe194ec80ec135d873
daphnehanse11 <128793799+daphnehanse11@users.noreply.github.com>
2025-01-24 15:39:41 -0500

    handle la county zip 3

--- policyengine_us/variables/gov/aca/slspc/slspc_rating_area.py
+++ policyengine_us/variables/gov/aca/slspc/slspc_rating_area.py
@@ -15,6 +15,16 @@ class slspc_rating_area(Variable):
         # Get county data
         county = household("county_str", period)
 
+        is_la = county == "Los Angeles County, CA"
+
+        if is_la:
+            zip3 = household("three_digit_zip_code", period)
+            la_data = parameters.gov.aca.la_county_rating_area[period]
+        for rating_area, zip_codes in la_data.items():
+            if zip3 in zip_codes:
+                return rating_area
+
+
         # Create DataFrame with county information
         df = pd.DataFrame({"county": county})
 
