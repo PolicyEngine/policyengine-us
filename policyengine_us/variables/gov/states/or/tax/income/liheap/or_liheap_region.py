25905aa1d94fd2902b7b3d79af16ffe8dd5151dc
srilareddy <nsrila@gmail.com>
2025-01-24 11:52:42 -0500

    Liheap region variable

--- policyengine_us/variables/gov/states/or/tax/income/liheap/or_liheap_region.py
+++ policyengine_us/variables/gov/states/or/tax/income/liheap/or_liheap_region.py
@@ -1,6 +1,6 @@
 from policyengine_us.model_api import *
 
-class liheap_region(Variable):
+class or_liheap_region(Variable):
     value_type = str
     entity = SPMUnit
     definition_period = YEAR
@@ -8,26 +8,12 @@ class liheap_region(Variable):
     label = "LIHEAP Region"
     reference = "https://www.oregon.gov/ohcs/energy-weatherization/Documents/2021-Energy-Assistance-Manual.pdf#page=55" 
 
-    def formula(SPMUnit, period, parameters):
-        county = SPMUnit("county_str", period)
-
+    def formula(spm_unit, period, parameters):
+        county = spm_unit.household("county_str", period)
         region1_counties = parameters(period).gov.states["or"].liheap.region1_counties
-
-        region = select(
+        return select(
             [county in region1_counties],  
             ["Region 1"],                 
             default="Region 2"            
         )
 
-        return region
-
-class liheap_payout(Variable):
-    value_type = float
-    entity = SPMUnit
-    definition_period = YEAR
-    defined_for = StateCode.OR
-    label = "LIHEAP Payout"
-
-    def formula(SPMUnit, period, parameters):
-        payout = SPMUnit("utility_type", period)
-        
