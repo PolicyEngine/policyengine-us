517e347caceae710873057dcceb9bd87932e35a0
PavelMakarchuk <pavel.ma99@gmail.com>
2025-02-10 12:32:27 +0100

    use applied yaml and real CBO projections

--- policyengine_us/variables/gov/irs/income/taxable_income/deductions/itemizing/itemized_taxable_income_deductions_reduction.py
+++ policyengine_us/variables/gov/irs/income/taxable_income/deductions/itemizing/itemized_taxable_income_deductions_reduction.py
@@ -9,14 +9,17 @@ class itemized_taxable_income_deductions_reduction(Variable):
     definition_period = YEAR
 
     def formula(tax_unit, period, parameters):
-        agi = tax_unit("adjusted_gross_income", period)
         p = parameters(period).gov.irs.deductions.itemized.reduction
-        filing_status = tax_unit("filing_status", period)
-        agi_threshold = p.agi_threshold[filing_status]
-        agi_excess = max_(0, agi - agi_threshold)
-        agi_excess_reduction = agi_excess * p.rate.excess_agi
-        maximum_deductions = tax_unit(
-            "total_itemized_taxable_income_deductions", period
-        )
-        maximum_deductions_reduction = maximum_deductions * p.rate.base
-        return min_(agi_excess_reduction, maximum_deductions_reduction)
+        if p.applies:
+            agi = tax_unit("adjusted_gross_income", period)
+
+            filing_status = tax_unit("filing_status", period)
+            agi_threshold = p.agi_threshold[filing_status]
+            agi_excess = max_(0, agi - agi_threshold)
+            agi_excess_reduction = agi_excess * p.rate.excess_agi
+            maximum_deductions = tax_unit(
+                "total_itemized_taxable_income_deductions", period
+            )
+            maximum_deductions_reduction = maximum_deductions * p.rate.base
+            return min_(agi_excess_reduction, maximum_deductions_reduction)
+        return 0
