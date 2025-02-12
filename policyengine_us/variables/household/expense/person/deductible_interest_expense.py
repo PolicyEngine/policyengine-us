ba5992b6cea32da1d85dcfd39a2d49fc20de706c
Sonali Bedge <sonali@codethedream.org>
2025-02-11 18:35:00 -0800

    Rename interest_expense to deductible_interest_expense

--- policyengine_us/variables/household/expense/person/deductible_interest_expense.py
+++ policyengine_us/variables/household/expense/person/deductible_interest_expense.py
@@ -1,7 +1,7 @@
 from policyengine_us.model_api import *
 
 
-class interest_expense(Variable):
+class deductible_interest_expense(Variable):
     value_type = float
     entity = Person
     label = "Interest paid on all loans"
