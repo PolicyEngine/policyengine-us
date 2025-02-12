6cb1a4b3649cf5de025f3f4c2aab015cd2f98170
Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>
2025-02-12 21:02:37 +0100

    Remove the Montana CTC for the enacted policies (#5578)
    * Remove the Montana CTC for the enacted policies
    Fixes #5577
    
    * minor
    
    * remove from state ctcs

--- policyengine_us/reforms/reforms.py
+++ policyengine_us/reforms/reforms.py
@@ -86,6 +86,10 @@ from .local.nyc.stc.phase_out import (
     create_nyc_school_tax_credit_with_phase_out_reform,
 )
 
+from .states.mt.ctc import (
+    create_mt_ctc_reform,
+)
+
 from policyengine_core.reforms import Reform
 import warnings
 
@@ -188,6 +192,7 @@ def create_structural_reforms_from_parameters(parameters, period):
     nyc_school_tax_credit_with_phase_out = (
         create_nyc_school_tax_credit_with_phase_out_reform(parameters, period)
     )
+    mt_ctc = create_mt_ctc_reform(parameters, period)
 
     reforms = [
         afa_reform,
@@ -226,6 +231,7 @@ def create_structural_reforms_from_parameters(parameters, period):
         ny_2025_inflation_rebates,
         limit_salt_deduction_to_property_taxes,
         nyc_school_tax_credit_with_phase_out,
+        mt_ctc,
     ]
     reforms = tuple(filter(lambda x: x is not None, reforms))
 
