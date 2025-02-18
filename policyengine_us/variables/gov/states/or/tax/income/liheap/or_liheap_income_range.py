4f9ae01a9564226975c31a917e6fe827be7d1df4
srilareddy <nsrila@gmail.com>
2025-02-18 11:04:06 -0500

    Updated income range multiplier logic

--- policyengine_us/variables/gov/states/or/tax/income/liheap/or_liheap_income_range.py
+++ policyengine_us/variables/gov/states/or/tax/income/liheap/or_liheap_income_range.py
@@ -1,18 +1,9 @@
 from policyengine_us.model_api import *
 
 
-class OregonLIHEAPIncomeRange(Enum):
-    RANGE_ONE = "Range one"
-    RANGE_TWO = "Range two"
-    RANGE_THREE = "Range three"
-    RANGE_FOUR = "Range four"
-
-
 class or_liheap_income_range(Variable):
-    value_type = Enum
+    value_type = int
     entity = SPMUnit
-    possible_values = OregonLIHEAPIncomeRange
-    default_value = OregonLIHEAPIncomeRange.RANGE_FOUR
     label = "Income range for Oregon LIHEAP eligibility"
     definition_period = YEAR
     reference = "https://www.oregon.gov/ohcs/energy-weatherization/Documents/2021-Energy-Assistance-Manual.pdf#page=55"
@@ -22,22 +13,22 @@ class or_liheap_income_range(Variable):
     def formula(spm_unit, period, parameters):
         income = add(spm_unit, period, ["adjusted_gross_income"])
         threshold = spm_unit("or_liheap_income_threshold", period)
-        p = parameters(period).gov.states["or"].liheap.income_range
+        p = parameters(period).gov.states["or"].liheap
 
-        range1 = threshold * p.range_one
-        range2 = threshold * p.range_two
-        range3 = threshold * p.range_three
+        range_one = threshold * p.income_range_multiplier
+        range_two = threshold * p.income_range_multiplier * 2
+        range_three = threshold * p.income_range_multiplier * 3
 
         return select(
             [
-                income <= range1,
-                income <= range2,
-                income <= range3,
+                income <= range_one,
+                income <= range_two,
+                income <= range_three,
             ],
             [
-                OregonLIHEAPIncomeRange.RANGE_ONE,
-                OregonLIHEAPIncomeRange.RANGE_TWO,
-                OregonLIHEAPIncomeRange.RANGE_THREE,
+                1,
+                2,
+                3,
             ],
-            default=OregonLIHEAPIncomeRange.RANGE_FOUR,
+            default=4,
         )
