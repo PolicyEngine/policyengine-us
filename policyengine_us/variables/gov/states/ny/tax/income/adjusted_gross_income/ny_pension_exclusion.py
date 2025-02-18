8afe0de512a8ccb6da6ab7e50c32de453c90971c
Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>
2025-02-18 23:11:06 +0100

    Update policyengine_us/variables/gov/states/ny/tax/income/adjusted_gross_income/ny_pension_exclusion.py

--- policyengine_us/variables/gov/states/ny/tax/income/adjusted_gross_income/ny_pension_exclusion.py
+++ policyengine_us/variables/gov/states/ny/tax/income/adjusted_gross_income/ny_pension_exclusion.py
@@ -8,10 +8,7 @@ class ny_pension_exclusion(Variable):
     unit = USD
     documentation = "Exclusion for pension income for eligible individuals."
     definition_period = YEAR
-    dict(
-        title="N.Y. Comp. Codes R. & Regs. tit. 20 § 112.3",
-        href="https://casetext.com/regulation/new-york-codes-rules-and-regulations/title-20-department-of-taxation-and-finance/chapter-ii-income-taxes-and-estate-taxes/subchapter-a-new-york-state-personal-income-tax-under-article-22-of-the-tax-law/article-2-residents/part-112-new-york-adjusted-gross-income-of-a-resident-individual/section-1123-modifications-reducing-federal-adjusted-gross-income",
-    )
+    reference = "https://casetext.com/regulation/new-york-codes-rules-and-regulations/title-20-department-of-taxation-and-finance/chapter-ii-income-taxes-and-estate-taxes/subchapter-a-new-york-state-personal-income-tax-under-article-22-of-the-tax-law/article-2-residents/part-112-new-york-adjusted-gross-income-of-a-resident-individual/section-1123-modifications-reducing-federal-adjusted-gross-income"
 
     def formula(person, period, parameters):
         pension_income = person("pension_income", period)
