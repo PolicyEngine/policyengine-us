75ac6694161dd2034dba8739509d25aa3e4e87f3
PavelMakarchuk <pavel.ma99@gmail.com>
2025-02-19 10:04:18 +0100

    use an applies param

--- policyengine_us/variables/gov/irs/income/taxable_income/deductions/itemizing/misc_deduction.py
+++ policyengine_us/variables/gov/irs/income/taxable_income/deductions/itemizing/misc_deduction.py
@@ -7,12 +7,12 @@ class misc_deduction(Variable):
     label = "Miscellaneous deduction"
     unit = USD
     definition_period = YEAR
+    reference = "https://www.law.cornell.edu/uscode/text/26/67#b"
 
     def formula(tax_unit, period, parameters):
-        # We currently only model unreimbursed business employee expenses as miscellaneous expenses
-        expense = add(
-            tax_unit, period, ["unreimbursed_business_employee_expenses"]
-        )
         p = parameters(period).gov.irs.deductions.itemized.misc
-        misc_floor = p.floor * tax_unit("positive_agi", period)
-        return max_(0, expense - misc_floor)
+        if p.applies:
+            expenses = add(tax_unit, period, p.sources)
+            misc_floor = p.floor * tax_unit("positive_agi", period)
+            return max_(0, expenses - misc_floor)
+        return 0
