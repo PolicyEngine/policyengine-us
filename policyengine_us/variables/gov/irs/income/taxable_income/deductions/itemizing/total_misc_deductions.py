6e2eadbd712210db5975b2b24803ce7473bd4d86
PavelMakarchuk <pavel.ma99@gmail.com>
2025-02-19 11:58:47 +0100

    minor

--- policyengine_us/variables/gov/irs/income/taxable_income/deductions/itemizing/total_misc_deductions.py
+++ policyengine_us/variables/gov/irs/income/taxable_income/deductions/itemizing/total_misc_deductions.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class total_misc_deductions(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Total miscellaneous deductions subject to the floor"
+    label = "Total miscellaneous deductions subject to the AGI floor"
     unit = USD
     definition_period = YEAR
     reference = "https://www.law.cornell.edu/uscode/text/26/67#b"
