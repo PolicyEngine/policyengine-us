c8d726be1570a9d77d428b4c366e4a2d0bc824d5
PavelMakarchuk <pavel.ma99@gmail.com>
2025-02-19 20:30:05 +0100

    format adn remove other misc ded.

--- policyengine_us/variables/household/expense/person/other_miscellaneous_expenses.py
+++
@@ -1,9 +0,0 @@
-from policyengine_us.model_api import *
-
-
-class other_miscellaneous_expenses(Variable):
-    value_type = float
-    entity = Person
-    label = "Other miscellaneous expenses"
-    unit = USD
-    definition_period = YEAR
