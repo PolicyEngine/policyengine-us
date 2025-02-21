c08d8c7875480109686b61dbd1233c61e4976c9c
daphnehanse11 <128793799+daphnehanse11@users.noreply.github.com>
2025-02-21 11:01:13 -0800

    Delete old CA slspc.yaml, add new variables for LA county Fixes #5627

--- policyengine_us/variables/gov/aca/slspc/slcsp_rating_area.py
+++ policyengine_us/variables/gov/aca/slspc/slcsp_rating_area.py
@@ -1,7 +1,4 @@
 from policyengine_us.model_api import *
-from policyengine_us.parameters.gov.hhs.medicaid.geography import (
-    aca_rating_areas,
-)
 
 
 class slcsp_rating_area(Variable):
@@ -11,23 +8,8 @@ class slcsp_rating_area(Variable):
     definition_period = YEAR
 
     def formula(household, period, parameters):
-        county = household("county_str", period)
-        is_la = county == "LOS_ANGELES_COUNTY_CA"
-
-        if is_la:
-            zip3 = household("three_digit_zip_code", period)
-            p = parameters(period).gov.aca
-            return p.la_county_rating_area[zip3]
-        # If not LA, merge with aca_rating_areas.
-        df = pd.DataFrame({"county": county})
-        aca_rating_areas["rating_area"] = aca_rating_areas[
-            "rating_area"
-        ].astype(str)
-        df_matched = pd.merge(
-            df,
-            aca_rating_areas,
-            how="left",
-            left_on="county",
-            right_on="county",
+        return where(
+            household("in_la", period),
+            household("slcsp_rating_area_la_county", period),
+            household("slcsp_rating_area_default", period),
         )
-        return df_matched["rating_area"].fillna(1)
