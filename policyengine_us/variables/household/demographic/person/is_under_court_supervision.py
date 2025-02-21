cab6589bd4e6f70ff5200e2bcebea11c842bbd3f
Ming K <myang1010@gmail.com>
2025-02-21 11:12:01 -0800

    remove an unused parameter

--- policyengine_us/variables/household/demographic/person/is_under_court_supervision.py
+++
@@ -1,9 +0,0 @@
-from policyengine_us.model_api import *
-
-
-class is_under_court_supervision(Variable):
-    value_type = bool
-    entity = Person
-    label = "Is under court supervision"
-    documentation = "Whether this person is under court supervision."
-    definition_period = YEAR
