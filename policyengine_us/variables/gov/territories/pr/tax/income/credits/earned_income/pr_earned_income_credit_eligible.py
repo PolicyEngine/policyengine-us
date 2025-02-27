8831fb656b927f7730f5d34a9c373c3f7302ef9d
Lucy Lennemann <78785519+llennemann@users.noreply.github.com>
2025-02-27 03:25:43 +0000

    Small fix for tests

--- policyengine_us/variables/gov/territories/pr/tax/income/credits/earned_income/pr_earned_income_credit_eligible.py
+++ policyengine_us/variables/gov/territories/pr/tax/income/credits/earned_income/pr_earned_income_credit_eligible.py
@@ -7,7 +7,6 @@ class pr_earned_income_credit_eligible(Variable):
     label = "Puerto Rico earned income credit eligible unit"
     definition_period = YEAR
     reference = "https://hacienda.pr.gov/sites/default/files/schedule_ct_rev._jul_5_23_informative_-_instructions.pdf#page=1"
-    defined_for = "pr_earned_income_credit_eligible_people"
 
     def formula(tax_unit, period, parameters):
         # list of eligible people in the tax unit from the adds function
