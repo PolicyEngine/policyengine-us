d10d01c0c5347e427d67f2b2c5699b95db4a1eb4
PavelMakarchuk <pavel.ma99@gmail.com>
2025-03-06 14:23:12 +0100

    Mississippi retirement income exemption Fixes #5666

---
+++ policyengine_us/variables/gov/states/ms/tax/income/adjustments/ms_retirement_income_exemption.py
@@ -0,0 +1,23 @@
+from policyengine_us.model_api import *
+
+
+class ms_retirement_income_exemption(Variable):
+    value_type = float
+    entity = Person
+    label = "Mississippi retirement income exemption"
+    unit = USD
+    definition_period = YEAR
+    reference = "https://law.justia.com/codes/mississippi/title-27/chapter-7/article-1/section-27-7-15/"  # (4)(k)
+    defined_for = StateCode.MS
+
+    def formula(person, period, parameters):
+        excluded_income = add(
+            person,
+            period,
+            [
+                "social_security_retirement",
+                "taxable_pension_income",
+            ],
+        )
+        head_or_spouse = person("is_tax_unit_head_or_spouse", period)
+        return head_or_spouse * excluded_income
