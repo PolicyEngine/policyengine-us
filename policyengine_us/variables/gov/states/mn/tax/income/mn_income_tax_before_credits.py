e65ef0674756a33d8bf96a92aab026ca0ca2ad6c
juaristi22 <juaristi@uni.minerva.edu>
2025-03-07 12:59:21 -0300

    added niit in_effect.yaml and updated mn_income_tax_before_credits.py

--- policyengine_us/variables/gov/states/mn/tax/income/mn_income_tax_before_credits.py
+++ policyengine_us/variables/gov/states/mn/tax/income/mn_income_tax_before_credits.py
@@ -12,4 +12,12 @@ class mn_income_tax_before_credits(Variable):
         "https://www.revenue.state.mn.us/sites/default/files/2023-03/m1_inst_22.pdf"
     )
     defined_for = StateCode.MN
-    adds = ["mn_basic_tax", "mn_amt", "mn_niit"]
+
+    def formula(tax_unit, period, parameters):
+        basic_tax = tax_unit("mn_basic_tax", period)
+        amt = tax_unit("mn_amt", period)
+        adds = [basic_tax, amt]
+        # Only add NIIT if it's in effect
+        if parameters(period).gov.states.mn.tax.income.niit.in_effect:
+            niit = tax_unit("mn_niit", period)
+            adds.append(niit)
