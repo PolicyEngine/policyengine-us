02c15e99b730a40b2c74ee12a912ef1f5239fddf
PavelMakarchuk <pavel.ma99@gmail.com>
2025-03-10 13:02:05 +0100

    tax form

--- policyengine_us/variables/gov/states/ms/tax/income/adjustments/ms_retirement_income_exemption.py
+++ policyengine_us/variables/gov/states/ms/tax/income/adjustments/ms_retirement_income_exemption.py
@@ -10,6 +10,7 @@ class ms_retirement_income_exemption(Variable):
     reference = [
         "https://law.justia.com/codes/mississippi/title-27/chapter-7/article-1/section-27-7-15/", # (4)(k)
         "https://www.dor.ms.gov/sites/default/files/Forms/Individual/80100221.pdf#page=11", # Line 46
+        "https://www.dor.ms.gov/sites/default/files/Forms/Individual/2024/80105248.pdf", 
     ]
     defined_for = StateCode.MS
 
