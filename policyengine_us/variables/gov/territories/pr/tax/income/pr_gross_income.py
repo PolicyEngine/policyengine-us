af3246e9610962be72465850e8f9faab25ad831a
Lucy Lennemann <78785519+llennemann@users.noreply.github.com>
2025-03-13 16:05:12 +0000

    PR gross income

--- policyengine_us/variables/gov/territories/pr/tax/income/pr_gross_income.py
+++ policyengine_us/variables/gov/territories/pr/tax/income/pr_gross_income.py
@@ -8,3 +8,12 @@ class pr_gross_income(Variable):
     unit = USD
     definition_period = YEAR
     reference = "https://casetext.com/statute/laws-of-puerto-rico/title-thirteen-taxation-and-finance/subtitle-17-internal-revenue-code-of-2011/part-ii-income-taxes/chapter-1005-computation-of-taxable-income/subchapter-a-determination-of-net-income-general-concepts/30101-gross-income"
+    defined_for = StateCode.PR
+
+    def formula(tax_unit, period, parameters):
+        p = parameters(period).gov.territories.pr.tax.income # gross_income/sources
+        income = 0
+        for source in p.gross_income.sources:
+            # if the source has over 0 income, add it to total
+            income += max_0(0, add(tax_unit, period, [source])) 
+        return income
