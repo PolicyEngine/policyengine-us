02af34b6e2eb4ed460578e5a138c5ef3b2f373c5
David Trimmer <david@policyengine.org>
2025-03-20 10:53:30 -0400

    Refactor NM itemized deductions (#5693)
    * Refactor NM itemized deductions
    
    * add changelog
    
    * fix lint
    
    * fix brackets
    
    * rename variable in state_itemized_deductions.yaml
    
    * refactor and add int test
    
    * minor
    
    * fix state itm ded.
    
    * label
    
    * reference
    
    * minor
    
    * int test fix
    
    ---------
    
    Co-authored-by: PavelMakarchuk <pavel.ma99@gmail.com>

--- policyengine_us/variables/gov/states/nm/tax/income/deductions/nm_deductions.py
+++ policyengine_us/variables/gov/states/nm/tax/income/deductions/nm_deductions.py
@@ -11,12 +11,14 @@ class nm_deductions(Variable):
 
     def formula(tax_unit, period, parameters):
         itemized_on_federal_return = tax_unit("tax_unit_itemizes", period)
-        itm_ded = tax_unit("nm_itemized_deductions", period)
+        itm_ded = tax_unit("total_itemized_taxable_income_deductions", period)
         standard_ded = tax_unit("standard_deduction", period)
         # Tax filer is required to itemize deductions if they itemize on their federal return as per:
         # https://klvg4oyd4j.execute-api.us-west-2.amazonaws.com/prod/PublicFiles/34821a9573ca43e7b06dfad20f5183fd/1afc56af-ea90-4d48-82e5-1f9aeb43255a/PITbook2022.pdf
         itemized_or_standard = where(
-            itemized_on_federal_return, itm_ded, standard_ded
+            itemized_on_federal_return,
+            itm_ded,
+            standard_ded,
         )
         OTHER_DEDUCTIONS = [
             "nm_medical_care_expense_deduction",
