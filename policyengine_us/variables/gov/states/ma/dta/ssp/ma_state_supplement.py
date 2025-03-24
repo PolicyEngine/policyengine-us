a855ae91743a4e16c774e3f645ce6a371584bfdf
Ziming <ziminghua331@gmail.com>
2025-03-24 16:01:13 -0400

    Refactor the Massachusetts state supplement to reflect state specific logic Fixes #5769

--- policyengine_us/variables/gov/states/ma/dta/ssp/ma_state_supplement.py
+++ policyengine_us/variables/gov/states/ma/dta/ssp/ma_state_supplement.py
@@ -1,17 +1,18 @@
 from policyengine_us.model_api import *
 
 
-class state_supplement(Variable):
+class ma_state_supplement(Variable):
     value_type = float
     entity = Person
-    label = "State Supplement"
+    label = "Massachusetts State Supplement payment amount"
     definition_period = YEAR
-    exhaustive_parameter_dependencies = "gov.ssa.state_supplement"
+    exhaustive_parameter_dependencies = "gov.states.ma.dta.ssp"
+    reference = "https://www.law.cornell.edu/regulations/massachusetts/106-CMR-327-330"
 
     def formula(person, period, parameters):
         uncapped_ssi = person("uncapped_ssi", period)
         reduction_after_ssi = max_(0, -uncapped_ssi)
-        maximum_ss = person("maximum_state_supplement", period)
+        maximum_ss = person("ma_maximum_state_supplement", period)
         state_supplement = max_(0, maximum_ss - reduction_after_ssi)
         abd = person("is_ssi_aged_blind_disabled", period)
         meets_resource_test = person("meets_ssi_resource_test", period)
