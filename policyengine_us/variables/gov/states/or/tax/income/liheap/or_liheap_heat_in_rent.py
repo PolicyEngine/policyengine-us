604764691dca3282303dded8e289248a2d509309
srilareddy <nsrila@gmail.com>
2025-03-27 18:32:50 +0530

    format

--- policyengine_us/variables/gov/states/or/tax/income/liheap/or_liheap_heat_in_rent.py
+++ policyengine_us/variables/gov/states/or/tax/income/liheap/or_liheap_heat_in_rent.py
@@ -1,9 +1,10 @@
 from policyengine_us.model_api import *
 
 
-class heat_in_rent(Variable):
+class heating_costs_included_in_rent(Variable):
     value_type = bool
     entity = SPMUnit
-    label = "Household's heat is included in rent"
+    label = "Household's heating costs are included in the rent amount"
     definition_period = YEAR
+    # Excludes other types of utilities
     reference = "https://liheapch.acf.hhs.gov/tables/FY2016/subsidize.htm#OR"
