99edc1891d0af9e04d18cc6c1a31d889f918a7ab
Ziming <ziminghua331@gmail.com>
2025-03-27 14:44:16 -0400

    adjustment on formula

--- policyengine_us/variables/gov/states/ny/tax/income/taxable_income/deductions/itemized/ny_itemized_deductions_reduction.py
+++ policyengine_us/variables/gov/states/ny/tax/income/taxable_income/deductions/itemized/ny_itemized_deductions_reduction.py
@@ -8,7 +8,7 @@ class ny_itemized_deductions_reduction(Variable):
     unit = USD
     definition_period = YEAR
     reference = "https://www.nysenate.gov/legislation/laws/TAX/615"  # (f)&(g)
-    defined_for = StateCode.NY
+    defined_for = "ny_itemized_deductions_reduction_applies"
 
     def formula(tax_unit, period, parameters):
         p = parameters(
@@ -18,23 +18,17 @@ class ny_itemized_deductions_reduction(Variable):
         filing_status = tax_unit("filing_status", period)
         itemized_deduction = tax_unit("ny_itemized_deductions_max", period)
         charitable_deduction = tax_unit("charitable_deduction", period)
-        itemized_deduction_less_charity = (
-            itemized_deduction - charitable_deduction
-        )
-        # Income threshold for non-charitable deductions reduction
-        first_reduction_threshold = p.amount.start[filing_status]  # 100_000
-        second_reduction_threshold = p.amount.end  # 475_000
+        # Income threshold itemized deductions reduction
+        first_reduction_threshold = p.amount.first_reduction[
+            filing_status
+        ]  # 100_000
+        second_reduction_threshold = p.amount.second_reduction  # 475_000
         # Income threshold where only charitable deduction applies
         high_income_threshold = p.amount.high_income
         higher_income_threshold = p.amount.higher_income
 
-        no_adjustment_condition = agi <= first_reduction_threshold
-        first_reduction_condition = (
-            first_reduction_threshold < agi <= second_reduction_threshold
-        )
-        first_reduction_excess_amount = max_(
-            agi - first_reduction_threshold, 0
-        )
+        first_reduction_condition = agi <= second_reduction_threshold
+        first_reduction_excess_amount = agi - first_reduction_threshold
         first_reduction_base_amount = min_(
             p.amount.numerator, first_reduction_excess_amount
         )
@@ -43,7 +37,7 @@ class ny_itemized_deductions_reduction(Variable):
         )
         first_reduction = (
             p.rate.first_reduction
-            * itemized_deduction_less_charity
+            * itemized_deduction
             * first_reduction_multiplier
         )
         second_reduction_condition = (
@@ -60,26 +54,28 @@ class ny_itemized_deductions_reduction(Variable):
         )
         second_reduction = (
             p.rate.second_reduction
-            * itemized_deduction_less_charity
+            * itemized_deduction
             * second_reduction_multiplier
         )
         high_income_condition = (
             high_income_threshold < agi <= higher_income_threshold
         )
-        high_income_reduction = p.rate.high_income * charitable_deduction
+        high_income_reduction = (
+            itemized_deduction - p.rate.high_income * charitable_deduction
+        )
         higher_income_condition = agi > higher_income_threshold
-        higher_income_reduction = p.rate.higher_income * charitable_deduction
+        higher_income_reduction = (
+            itemized_deduction - p.rate.higher_income * charitable_deduction
+        )
 
         return select(
             [
-                no_adjustment_condition,
                 first_reduction_condition,
                 second_reduction_condition,
                 high_income_condition,
                 higher_income_condition,
             ],
             [
-                0,
                 first_reduction,
                 first_reduction + second_reduction,
                 high_income_reduction,
