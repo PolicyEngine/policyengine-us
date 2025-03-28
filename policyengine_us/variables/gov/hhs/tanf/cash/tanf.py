da6e90f132b7b79a577df2c9870a71ca417f2020
PavelMakarchuk <pavel.ma99@gmail.com>
2025-03-28 16:04:28 +0100

    payout yearly

--- policyengine_us/variables/gov/hhs/tanf/cash/tanf.py
+++ policyengine_us/variables/gov/hhs/tanf/cash/tanf.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class tanf(Variable):
     value_type = float
     entity = SPMUnit
-    definition_period = MONTH
+    definition_period = YEAR
     label = "TANF"
     documentation = (
         "Value of Temporary Assistance for Needy Families benefit received."
