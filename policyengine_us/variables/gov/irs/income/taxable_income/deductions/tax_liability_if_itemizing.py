ee442b39ec29fbc899350a9bc0f3cd5425097421
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2025-04-01 15:42:12 +0100

    Add bin collection

--- policyengine_us/variables/gov/irs/income/taxable_income/deductions/tax_liability_if_itemizing.py
+++ policyengine_us/variables/gov/irs/income/taxable_income/deductions/tax_liability_if_itemizing.py
@@ -1,4 +1,5 @@
 from policyengine_us.model_api import *
+from policyengine_core.simulations import Simulation
 
 
 class tax_liability_if_itemizing(Variable):
@@ -9,9 +10,11 @@ class tax_liability_if_itemizing(Variable):
     definition_period = YEAR
 
     def formula(tax_unit, period, parameters):
-        simulation = tax_unit.simulation
+        simulation: Simulation = tax_unit.simulation
         itemized_branch = simulation.get_branch("itemizing")
         itemized_branch.set_input(
             "tax_unit_itemizes", period, np.ones((tax_unit.count,), dtype=bool)
         )
-        return itemized_branch.calculate("federal_state_income_tax", period)
+        values = itemized_branch.calculate("federal_state_income_tax", period)
+        del simulation.branches["itemizing"]
+        return values
