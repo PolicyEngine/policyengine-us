7fc826894039ab91a030f6ba69f4d912ff3b1a7c
Ziming <ziminghua331@gmail.com>
2025-04-02 14:21:15 -0400

    format

--- policyengine_us/variables/gov/states/ny/tax/income/taxable_income/deductions/itemized/ny_itemized_deductions_reduction.py
+++ policyengine_us/variables/gov/states/ny/tax/income/taxable_income/deductions/itemized/ny_itemized_deductions_reduction.py
@@ -19,45 +19,42 @@ class ny_itemized_deductions_reduction(Variable):
         itemized_deduction = tax_unit("ny_itemized_deductions_max", period)
         charitable_deduction = tax_unit("charitable_deduction", period)
         # Income thresholds for itemized deductions reduction
-        first_reduction_threshold = p.income_threshold.lower[filing_status]
-        second_reduction_threshold = p.income_threshold.higher
-        # High Income thresholds that only charitable deduction applies
+        lower_income_threshold = p.income_threshold.lower[filing_status]
+        addition_reduction_income_threshold = p.income_threshold.higher
         high_income_threshold = p.high_income_brackets.thresholds[1]
         higher_income_threshold = p.high_income_brackets.thresholds[2]
 
-        first_reduction_condition = agi <= second_reduction_threshold
-        first_reduction_excess_amount = agi - first_reduction_threshold
-        first_reduction_base_amount = min_(
-            p.amount.numerator, first_reduction_excess_amount
+        lower_income_condition = agi <= addition_reduction_income_threshold
+        lower_income_excess_amount = agi - lower_income_threshold
+        lower_income_base_amount = min_(
+            p.amount.numerator, lower_income_excess_amount
         )
-        first_reduction_multiplier = (
-            first_reduction_base_amount / p.amount.denominator
+        lower_income_multiplier = (
+            lower_income_base_amount / p.amount.denominator
         )
-        first_reduction = (
-            p.rate.lower
-            * itemized_deduction
-            * first_reduction_multiplier
+        lower_income_reduction_amount = (
+            p.rate.lower * itemized_deduction * lower_income_multiplier
         )
-        second_reduction_condition = (agi > second_reduction_threshold) & (
-            agi <= high_income_threshold
+        addition_reduction_condition = (
+            agi > addition_reduction_income_threshold
+        ) & (agi <= high_income_threshold)
+        addition_reduction_excess_amount = max_(
+            agi - addition_reduction_condition, 0
         )
-        second_reduction_excess_amount = max_(
-            agi - second_reduction_threshold, 0
+        addition_reduction_base_amount = min_(
+            p.amount.numerator, addition_reduction_excess_amount
         )
-        second_reduction_base_amount = min_(
-            p.amount.numerator, second_reduction_excess_amount
+        addition_reduction_multiplier = (
+            addition_reduction_base_amount / p.amount.denominator
         )
-        second_reduction_multiplier = (
-            second_reduction_base_amount / p.amount.denominator
-        )
-        second_reduction = (
-            p.rate.higher
-            * itemized_deduction
-            * second_reduction_multiplier
+        addition_reduction_amount = (
+            p.rate.higher * itemized_deduction * addition_reduction_multiplier
         )
         high_income_condition = (agi > high_income_threshold) & (
             agi <= higher_income_threshold
         )
+        # For filers with NY AGI higher than $1,000,000, only fraction of their
+        # charitable deduction can be applied to itemized deduction
         high_income_rate = p.high_income_brackets.rates[1]
         high_income_reduction = (
             itemized_deduction - high_income_rate * charitable_deduction
@@ -70,14 +67,14 @@ class ny_itemized_deductions_reduction(Variable):
 
         return select(
             [
-                first_reduction_condition,
-                second_reduction_condition,
+                lower_income_condition,
+                addition_reduction_condition,
                 high_income_condition,
                 higher_income_condition,
             ],
             [
-                first_reduction,
-                first_reduction + second_reduction,
+                lower_income_reduction_amount,
+                lower_income_reduction_amount + addition_reduction_amount,
                 high_income_reduction,
                 higher_income_reduction,
             ],
