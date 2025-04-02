4396d36a4b4417470b1638c5f0c760a4c1015138
Ziming <ziminghua331@gmail.com>
2025-04-01 23:09:56 -0400

    add 2022 and 2023 forms to references

--- policyengine_us/variables/gov/states/ny/tax/income/taxable_income/deductions/itemized/ny_itemized_deductions_reduction_applies.py
+++ policyengine_us/variables/gov/states/ny/tax/income/taxable_income/deductions/itemized/ny_itemized_deductions_reduction_applies.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class ny_itemized_deductions_reduction_applies(Variable):
     value_type = bool
     entity = TaxUnit
-    label = "Whether NY itemized deductions reduction applies"
+    label = "Whether the reduction to the New York itemized deductions applies"
     definition_period = YEAR
     reference = "https://www.nysenate.gov/legislation/laws/TAX/615"  # (f)&(g)
     defined_for = StateCode.NY
@@ -15,6 +15,6 @@ class ny_itemized_deductions_reduction_applies(Variable):
         ).gov.states.ny.tax.income.deductions.itemized.reduction
         agi = tax_unit("ny_agi", period)
         filing_status = tax_unit("filing_status", period)
-        first_reduction_threshold = p.amount.first_reduction[filing_status]
+        first_reduction_threshold = p.income_threshold[filing_status]
 
         return agi > first_reduction_threshold
