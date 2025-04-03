94c28b74539b643faea076eb43ee18eff10a3b4e
Ziming <ziminghua331@gmail.com>
2025-04-03 19:15:48 -0400

    with eaedc

--- policyengine_us/variables/gov/states/ma/dta/tcap/eaedc/income/ma_eaedc_standard_assistance.py
+++ policyengine_us/variables/gov/states/ma/dta/tcap/eaedc/income/ma_eaedc_standard_assistance.py
@@ -12,10 +12,11 @@ class ma_eaedc_standard_assistance(Variable):
 
     def formula(spm_unit, period, parameters):
         n = spm_unit("spm_unit_size", period)
+        capped_size = min_(n, 8).astype(int)
         living_arrangement = spm_unit("ma_eaedc_living_arrangement", period)
         p = parameters(
             period
         ).gov.states.ma.dta.tcap.eaedc.standard_assistance.amount
         p1 = p.base[living_arrangement]
         pn = p.additional[living_arrangement]
-        return p1 + pn * (n - 1)
+        return p1 + pn * (capped_size - 1)
