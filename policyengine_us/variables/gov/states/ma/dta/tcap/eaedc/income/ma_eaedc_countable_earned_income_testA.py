8e53b9a763042ddea255d595fd722980872b895a
Ziming <ziminghua331@gmail.com>
2025-04-03 22:10:17 -0400

    check eaedc_countable_earned_income_testA

---
+++ policyengine_us/variables/gov/states/ma/dta/tcap/eaedc/income/ma_eaedc_countable_earned_income_testA.py
@@ -0,0 +1,13 @@
+from policyengine_us.model_api import *
+
+
+class ma_eaedc_countable_earned_income_testA(Variable):
+    value_type = float
+    entity = SPMUnit
+    label = "Massachusetts EAEDC countable earned income"
+    unit = USD
+    definition_period = MONTH
+    defined_for = StateCode.MA
+    reference = "https://www.law.cornell.edu/regulations/massachusetts/106-CMR-704-500"  # (B) step 2
+
+    adds = ["ma_eaedc_countable_earned_income"]
