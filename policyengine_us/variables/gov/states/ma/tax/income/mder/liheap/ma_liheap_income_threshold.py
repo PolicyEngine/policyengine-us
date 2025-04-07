d4890312f7c4cee52d2835bcdbcb7dfe98b77034
srilareddy <nsrila@gmail.com>
2025-04-07 14:10:10 +0530

    income eligibility variable and tests

---
+++ policyengine_us/variables/gov/states/ma/tax/income/mder/liheap/ma_liheap_income_threshold.py
@@ -0,0 +1,19 @@
+from policyengine_us.model_api import *
+
+
+class or_liheap_income_threshold(Variable):
+    value_type = float
+    entity = SPMUnit
+    label = "Income threshold for Oregon LIHEAP eligibility"
+    unit = USD
+    definition_period = YEAR
+    reference = (
+        "https://www.mass.gov/info-details/learn-about-home-energy-assistance-heap"
+    )
+
+    defined_for = StateCode.MA
+
+    def formula(spm_unit, period, parameters):
+        state_median_income = spm_unit("hhs_smi", period)
+        p = parameters(period).gov.hhs.liheap
+        return state_median_income * p.income_rate
