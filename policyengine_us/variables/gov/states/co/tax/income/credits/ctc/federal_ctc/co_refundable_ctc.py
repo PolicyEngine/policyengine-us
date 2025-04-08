c3763e257322512811a01f070ef34cfb62076ecc
PavelMakarchuk <pavel.ma99@gmail.com>
2025-04-08 11:04:24 +0200

    lint

--- policyengine_us/variables/gov/states/co/tax/income/credits/ctc/federal_ctc/co_refundable_ctc.py
+++ policyengine_us/variables/gov/states/co/tax/income/credits/ctc/federal_ctc/co_refundable_ctc.py
@@ -23,7 +23,7 @@ class co_refundable_ctc(Variable):
     def formula(tax_unit, period, parameters):
         # follow 2022 DR 0104CN form and its instructions (in Book cited above):
         adjusted_fed_ctc = tax_unit("co_non_refundable_ctc", period)  # Line 7
-        max_child_amount = tax_unit("co_federal_ctc_maximum", period) # Line 3
+        max_child_amount = tax_unit("co_federal_ctc_maximum", period)  # Line 3
         credit_excess_over_tax = max_(
             0, max_child_amount - adjusted_fed_ctc
         )  # Line 8
