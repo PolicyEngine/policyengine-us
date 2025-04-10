5ea71014a754b5dc095328539f00c483f21b691a
Ziming <ziminghua331@gmail.com>
2025-04-10 19:20:29 -0400

    minor

--- policyengine_us/variables/gov/states/il/dhs/aabd/income/il_aabd_earned_income_after_exemption_person.py
+++ policyengine_us/variables/gov/states/il/dhs/aabd/income/il_aabd_earned_income_after_exemption_person.py
@@ -15,8 +15,7 @@ class il_aabd_earned_income_after_exemption_person(Variable):
         p = parameters(period).gov.states.il.dhs.aabd
         gross_earned_income = person("il_aabd_gross_earned_income", period)
         # Determine demographic status
-        age = person("monthly_age", period)
-        elderly = age >= p.elderly_age_threshold
+        elderly = person("is_ssi_aged", period)
         blind = person("is_blind", period)
         disabled = person("is_ssi_disabled", period)
         elderly_or_disabled = elderly | disabled
