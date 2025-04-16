4fe99883c8378df8d720a3aa918d2781f8429721
Max Ghenis <mghenis@gmail.com>
2025-04-16 17:14:43 -0400

    format

--- policyengine_us/variables/gov/aca/eligibility/is_aca_ptc_eligible.py
+++ policyengine_us/variables/gov/aca/eligibility/is_aca_ptc_eligible.py
@@ -4,7 +4,9 @@ from policyengine_us.model_api import *
 class is_aca_ptc_eligible(Variable):
     value_type = bool
     entity = Person
-    label = "Person is eligible for ACA premium tax credit and pays ACA premium"
+    label = (
+        "Person is eligible for ACA premium tax credit and pays ACA premium"
+    )
     definition_period = YEAR
 
     def formula(person, period, parameters):
