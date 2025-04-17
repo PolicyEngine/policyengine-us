c02c4fc03e9201645aaf544493dfebe93ed124b5
Ziming <ziminghua331@gmail.com>
2025-04-17 01:00:41 -0400

    Illinois Temporary Assistance for Needy Families (TANF) Fixes #5870

---
+++ policyengine_us/variables/gov/states/il/dhs/tanf/eligibility/il_tanf_eligible.py
@@ -0,0 +1,29 @@
+from policyengine_us.model_api import *
+
+
+class il_tanf_eligible(Variable):
+    value_type = bool
+    entity = SPMUnit
+    label = "Eligible for Illinois Temporary Assistance for Needy Families (TANF)"
+    definition_period = MONTH
+    reference = "https://www.dhs.state.il.us/page.aspx?item=30358"
+    defined_for = StateCode.IL
+
+    def formula(spm_unit, period, parameters):
+        demographic_eligible = (
+            add(spm_unit, period, ["il_tanf_demographic_eligible_person"]) > 0
+        )
+        income_eligible = spm_unit("dc_tanf_income_eligible", period)
+        immigration_status_eligible = (
+            add(
+                spm_unit,
+                period,
+                ["il_tanf_immigration_status_eligible_person"],
+            )
+            > 0
+        )
+        return (
+            demographic_eligible
+            & income_eligible
+            & immigration_status_eligible
+        )
---
+++ policyengine_us/variables/gov/states/il/dhs/tanf/income/in_tanf_countable_income_at_application.py
@@ -0,0 +1,27 @@
+from policyengine_us.model_api import *
+
+
+class il_tanf_countable_income_at_application(Variable):
+    value_type = float
+    entity = SPMUnit
+    label = (
+        "Illinois Temporary Assistance for Needy Families (TANF) countable income at application"
+    )
+    unit = USD
+    definition_period = MONTH
+    reference = "https://www.law.cornell.edu/regulations/illinois/Ill-Admin-Code-tit-89-SS-112.155"
+    defined_for = StateCode.IL
+    
+    def formula(spm_unit, period, parameters):
+        p = parameters(period).gov.states.il.dhs.tanf.income.initial_employment_deduction
+        
+        countable_gross_earned_income = spm_unit("il_tanf_countable_gross_earned_income", period)
+        exempted_expense = spm_unit("spm_unit_capped_work_childcare_expenses", period)
+        adjust_earned_income = max_(countable_gross_earned_income - exempted_expense, 0)
+        
+        fpg = spm_unit("spm_unit_fpg", period)
+        initial_employment_deduction = p.rate * fpg - spm_unit("il_tanf_payment_level", period)
+        earned_income_deduction = min_(adjust_earned_income, initial_employment_deduction)
+        countable_unearned_income = spm_unit("il_tanf_countable_unearned_income", period)                                            
+        
+        return adjust_earned_income - earned_income_deduction + countable_unearned_income 
