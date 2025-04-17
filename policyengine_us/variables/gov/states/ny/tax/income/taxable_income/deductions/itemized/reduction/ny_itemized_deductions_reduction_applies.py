b1c403472e8f9599ab74aca096a0efe7ee001134
PavelMakarchuk <pavel.ma99@gmail.com>
2025-04-17 16:29:46 -0400

    adjust structure
    Co-authored-by: Ziming Hua  <hua7450@users.noreply.github.com>

--- policyengine_us/variables/gov/states/ny/tax/income/taxable_income/deductions/itemized/reduction/ny_itemized_deductions_reduction_applies.py
+++ policyengine_us/variables/gov/states/ny/tax/income/taxable_income/deductions/itemized/reduction/ny_itemized_deductions_reduction_applies.py
@@ -6,15 +6,15 @@ class ny_itemized_deductions_reduction_applies(Variable):
     entity = TaxUnit
     label = "Whether the reduction to the New York itemized deductions applies"
     definition_period = YEAR
-    reference = "https://www.nysenate.gov/legislation/laws/TAX/615"  # (f)&(g)
+    reference = "https://www.nysenate.gov/legislation/laws/TAX/615"  # (f)
     defined_for = StateCode.NY
 
     def formula(tax_unit, period, parameters):
         p = parameters(
             period
-        ).gov.states.ny.tax.income.deductions.itemized.reduction.income_threshold
+        ).gov.states.ny.tax.income.deductions.itemized.reduction.incremental.lower
         agi = tax_unit("ny_agi", period)
         filing_status = tax_unit("filing_status", period)
-        first_reduction_threshold = p.lower[filing_status]
+        lower_income_threshold = p.income_threshold[filing_status]
         # No reduction applies if the AGI is less than the first reduction threshold
-        return agi > first_reduction_threshold
+        return agi > lower_income_threshold
