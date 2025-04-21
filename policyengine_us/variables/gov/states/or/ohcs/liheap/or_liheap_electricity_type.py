a140f909f839253a916e4b4e8e4ed40dd47360ff
srilareddy <nsrila@gmail.com>
2025-04-21 17:51:59 +0530

    modified electricity type variable

--- policyengine_us/variables/gov/states/or/ohcs/liheap/or_liheap_electricity_type.py
+++ policyengine_us/variables/gov/states/or/ohcs/liheap/or_liheap_electricity_type.py
@@ -4,13 +4,13 @@ from policyengine_us.model_api import *
 class OregonLIHEAPElectricityType(Enum):
     HEATING = "Heating"
     COOLING = "Cooling"
-    BOTH = "Both"
+    HEATING_AND_COOLING = "Heating and Cooling"
 
 
 class or_liheap_electricity_type(Variable):
     value_type = Enum
     entity = SPMUnit
     possible_values = OregonLIHEAPElectricityType
-    default_value = OregonLIHEAPElectricityType.BOTH
+    default_value = OregonLIHEAPElectricityType.HEATING_AND_COOLING
     definition_period = YEAR
     label = "Electricity usage type for LIHEAP payout"
