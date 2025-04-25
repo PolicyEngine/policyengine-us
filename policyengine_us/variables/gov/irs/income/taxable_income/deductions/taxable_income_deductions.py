196530ec6ad7d2f8fdc2ee0d9f0f876759cbbb26
PavelMakarchuk <pavel.ma99@gmail.com>
2025-04-25 15:05:43 -0400

    Limit reported SALT to the amount that would zero out regular tax liability Fixes #5899

--- policyengine_us/variables/gov/irs/income/taxable_income/deductions/taxable_income_deductions.py
+++ policyengine_us/variables/gov/irs/income/taxable_income/deductions/taxable_income_deductions.py
@@ -16,21 +16,6 @@ class taxable_income_deductions(Variable):
         deductions_if_not_itemizing = tax_unit(
             "taxable_income_deductions_if_not_itemizing", period
         )
-        # Limiting itemized deductions to taxable income, if itemizing
-        p = parameters(period).gov.simulation
-        if p.limit_itemized_deductions_to_taxable_income:
-            agi = tax_unit("adjusted_gross_income", period)
-            exemptions = tax_unit("exemptions", period)
-            itemizes = tax_unit("tax_unit_itemizes", period)
-            agi_less_exemptions = max_(0, agi - exemptions)
-            capped_deductions_if_itemizing = min_(
-                deductions_if_itemizing, agi_less_exemptions
-            )
-            return where(
-                itemizes,
-                capped_deductions_if_itemizing,
-                deductions_if_not_itemizing,
-            )
         return where(
             itemizes, deductions_if_itemizing, deductions_if_not_itemizing
         )
