9484f7a42777ee3dfa43a2cd501a47b285a01aa8
Ziming <ziminghua331@gmail.com>
2025-04-25 14:25:15 -0400

    add income limit and age threshold for free ride

--- policyengine_us/variables/gov/states/il/rta/cta/free_ride/il_cta_free_ride_eligible.py
+++ policyengine_us/variables/gov/states/il/rta/cta/free_ride/il_cta_free_ride_eligible.py
@@ -12,14 +12,22 @@ class il_cta_free_ride_eligible(Variable):
     reference = "https://www.transitchicago.com/reduced-fare-programs/#free"
 
     def formula(person, period, parameters):
-        p = parameters(period).gov.states.il.rta.age_threshold
+        p = parameters(period).gov.states.il.rta
         age = person("age", period)
-        young_child = age <= p.young_child
-        senior = age >= p.senior
-        disabled = person("is_disabled", period)
-        is_in_dabap = person("il_dabap_eligible", period)
-        eligible_senior = senior & is_in_dabap
-        eligible_disabled = disabled & is_in_dabap
+        young_child = age <= p.age_threshold.young_child
+
+        senior = age >= p.age_threshold.senior
+        income = person("irs_gross_income", period)
+        household_income = person.spm_unit.sum(income)
+        size = person.spm_unit("spm_unit_size", period)
+        capped_size = min_(size, 3)
+        income_eligible = household_income <= p.income_limit[capped_size]
+        eligible_senior = senior & income_eligible
+
+        disabled = person("is_permanently_and_totally_disabled", period)
+        disabled_age = age >= p.age_threshold.disabled
+        eligible_disabled = disabled & disabled_age & income_eligible
+
         eligible_military_status = person(
             "il_cta_military_service_pass_eligible", period
         )
