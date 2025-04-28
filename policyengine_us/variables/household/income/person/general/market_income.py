293b8964022cf86989c5c19fcbf07e3aee37df82
PavelMakarchuk <pavel.ma99@gmail.com>
2025-04-28 11:28:49 -0400

    generalization

--- policyengine_us/variables/household/income/person/general/market_income.py
+++ policyengine_us/variables/household/income/person/general/market_income.py
@@ -22,6 +22,6 @@ class market_income(Variable):
             "illicit_income",
             "farm_income",
             "miscellaneous_income",
-            "taxable_ira_distributions",
+            "retirement_distributions",
         ]
         return add(person, period, COMPONENTS)
