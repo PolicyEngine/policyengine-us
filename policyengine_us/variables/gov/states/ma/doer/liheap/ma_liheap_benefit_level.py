79dcc5bf7223291910a451e487e4715af38ade04
srilareddy <nsrila@gmail.com>
2025-04-28 16:41:25 +0530

    benefit level variable

---
+++ policyengine_us/variables/gov/states/ma/doer/liheap/ma_liheap_benefit_level.py
@@ -0,0 +1,44 @@
+from policyengine_us.model_api import *
+
+
+class ma_liheap_benefit_level(Variable):
+    value_type = int
+    entity = SPMUnit
+    label = "Benefit Levels for Massachusetts LIHEAP payout"
+    definition_period = YEAR
+    reference = "https://www.mass.gov/doc/fy-2025-heap-income-eligibility-and-benefit-chart-january-2025/download"
+
+    defined_for = StateCode.MA
+
+    def formula(spm_unit, period, parameters):
+        income = add(spm_unit, period, ["irs_gross_income"])
+        threshold = spm_unit("ma_liheap_income_threshold", period)
+        fpg = spm_unit("spm_unit_fpg", period)
+        p = parameters(period).gov.states.ma.doer.liheap
+
+        level_one = fpg 
+        level_two = fpg * p.benefit_level_multiplier * 5
+        level_three = fpg * p.benefit_level_multiplier * 6
+        level_four = fpg * p.benefit_level_multiplier * 7
+        level_five = fpg * p.benefit_level_multiplier * 8
+        level_six = threshold
+        return select(
+            [
+                income <= level_one,
+                income <= level_two,
+                income <= level_three,
+                income <= level_four,
+                income <= level_five,
+                income <= level_six,
+            ],
+            [
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+            ],
+            default=6,
+        )
+    
---
+++ policyengine_us/variables/gov/states/ma/doer/liheap/ma_liheap_payout.py
@@ -0,0 +1,38 @@
+from policyengine_us.model_api import *
+
+
+class or_liheap_payout(Variable):
+    value_type = float
+    entity = SPMUnit
+    definition_period = YEAR
+    defined_for = "ma_liheap_eligible"
+    label = "Massachusetts LIHEAP Payout"
+
+    def formula(spm_unit, period, parameters):
+        utility_type = spm_unit("utility_type", period)
+        unit_size = clip(spm_unit("spm_unit_size", period), 1, 6)
+        benefit_level = spm_unit("or_liheap_benefit_level", period)
+        is_region1 = spm_unit("or_liheap_in_region_one", period)
+        electricity_type = spm_unit("or_liheap_electricity_type", period)
+
+        p = parameters(period).gov.states["or"].ohcs.liheap
+
+        # Applies multiplier to the payout when electricity is used for both heating and cooling (not an official policy parameter).
+        electricity_multiplier = where(
+            (utility_type == utility_type.possible_values.ELECTRICITY)
+            & (electricity_type == electricity_type.possible_values.BOTH),
+            2,
+            1,
+        )
+
+        payout = where(
+            is_region1,
+            p.payout.or_liheap_region_one[unit_size][benefit_level][
+                utility_type
+            ],
+            p.payout.or_liheap_region_two[unit_size][benefit_level][
+                utility_type
+            ],
+        )
+
+        return payout * electricity_multiplier
