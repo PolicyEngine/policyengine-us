165c83c85afe41d0b13db19e79b1b35b9dd05734
Lucy Lennemann <78785519+llennemann@users.noreply.github.com>
2025-04-28 23:47:30 +0000

    Nonrefundable CTC Fixes #5921

---
+++ policyengine_us/variables/gov/territories/pr/tax/income/credits/ctc/pr_ctc.py
@@ -0,0 +1,15 @@
+from policyengine_us.model_api import *
+
+
+class pr_ctc(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Puerto Rico child tax credit"
+    unit = USD
+    definition_period = YEAR
+    reference = "https://www.irs.gov/pub/irs-pdf/f1040s8.pdf#page=1"
+
+    def formula(tax_unit, period, parameters):
+        maximum_amount = tax_unit("ctc_maximum_with_arpa_addition", period)
+        reduction = tax_unit("pr_ctc_phase_out", period)
+        return max_(0, maximum_amount - reduction)
