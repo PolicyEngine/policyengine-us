f336cd075ce5b35ece3b374281ea7b871bea0f5e
Ziming <ziminghua331@gmail.com>
2025-04-29 14:13:26 -0400

    adjust formula and test
    Co-authored-by: Pavel Makarchuk <PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/states/il/rta/cta/free_ride/il_cta_free_ride_eligible.py
+++ policyengine_us/variables/gov/states/il/rta/cta/free_ride/il_cta_free_ride_eligible.py
@@ -15,25 +15,8 @@ class il_cta_free_ride_eligible(Variable):
         p = parameters(period).gov.states.il.rta
         age = person("age", period)
         young_child = age <= p.age_threshold.young_child
-
-        senior = age >= p.age_threshold.senior
-        income = person("irs_gross_income", period)
-        household_income = person.spm_unit.sum(income)
-        size = person.spm_unit("spm_unit_size", period)
-        capped_size = min_(size, 3)
-        income_eligible = household_income <= p.income_limit[capped_size]
-        eligible_senior = senior & income_eligible
-
-        disabled = person("is_permanently_and_totally_disabled", period)
-        disabled_age = age >= p.age_threshold.disabled
-        eligible_disabled = disabled & disabled_age & income_eligible
-
+        bap_eligible = person("il_bap_eligible", period)
         eligible_military_status = person(
             "il_cta_military_service_pass_eligible", period
         )
-        return (
-            young_child
-            | eligible_senior
-            | eligible_disabled
-            | eligible_military_status
-        )
+        return young_child | bap_eligible | eligible_military_status
