e18eea03e49917506a3913125d23bdd882c0580b
PavelMakarchuk <pavel.ma99@gmail.com>
2025-05-06 16:26:49 -0400

    minor

--- policyengine_us/variables/gov/local/ca/la/dss/infant_supplement/ca_la_infant_supplement_eligible_infant.py
+++ policyengine_us/variables/gov/local/ca/la/dss/infant_supplement/ca_la_infant_supplement_eligible_infant.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class ca_la_infant_supplement_eligible_infant(Variable):
     value_type = bool
     entity = Person
-    definition_period = MONTH
+    definition_period = YEAR
     label = "Eligible Infant for the Los Angeles County infant supplement"
     defined_for = "in_la"
     reference = "https://cdss.ca.gov/Portals/9/Additional-Resources/Letters-and-Notices/ACLs/2021/21-123.pdf?ver=2021-10-08-140950-570"
