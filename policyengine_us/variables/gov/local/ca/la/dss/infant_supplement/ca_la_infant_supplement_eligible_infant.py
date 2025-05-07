2cea2903815f02123fb62320bc48f0c0fe2e0833
PavelMakarchuk <pavel.ma99@gmail.com>
2025-05-07 11:45:54 -0400

    remove formula

--- policyengine_us/variables/gov/local/ca/la/dss/infant_supplement/ca_la_infant_supplement_eligible_infant.py
+++ policyengine_us/variables/gov/local/ca/la/dss/infant_supplement/ca_la_infant_supplement_eligible_infant.py
@@ -8,8 +8,3 @@ class ca_la_infant_supplement_eligible_infant(Variable):
     label = "Eligible Infant for the Los Angeles County infant supplement"
     defined_for = "in_la"
     reference = "https://cdss.ca.gov/Portals/9/Additional-Resources/Letters-and-Notices/ACLs/2021/21-123.pdf?ver=2021-10-08-140950-570"
-
-    def formula(person, period, parameters):
-        head_or_spouse = person("is_tax_unit_head_or_spouse", period)
-        dependent = person("is_tax_unit_dependent", period)
-        return ~head_or_spouse & ~dependent
