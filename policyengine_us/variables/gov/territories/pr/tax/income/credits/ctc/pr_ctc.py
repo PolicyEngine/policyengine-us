e3f549f2a1a37c2c46deb7d2e413304818a477a6
Lucy Lennemann <78785519+llennemann@users.noreply.github.com>
2025-05-12 17:28:31 +0000

    formatting

--- policyengine_us/variables/gov/territories/pr/tax/income/credits/ctc/pr_ctc.py
+++ policyengine_us/variables/gov/territories/pr/tax/income/credits/ctc/pr_ctc.py
@@ -9,8 +9,8 @@ class pr_ctc(Variable):
     definition_period = YEAR
     reference = "https://www.irs.gov/pub/irs-pdf/f1040s8.pdf#page=1"
 
-    # This provision is part of the federal CTC legal code 
-    # will will merge the logic with the federal CTC once the puerto income tax structure is completed 
+    # This provision is part of the federal CTC legal code
+    # will will merge the logic with the federal CTC once the puerto income tax structure is completed
     def formula(tax_unit, period, parameters):
         maximum_amount = tax_unit("ctc_maximum_with_arpa_addition", period)
         reduction = tax_unit("pr_ctc_phase_out", period)
