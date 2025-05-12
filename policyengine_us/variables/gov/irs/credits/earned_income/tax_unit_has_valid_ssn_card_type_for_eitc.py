091f4cf21a6de520efd9c38ba1216e9d3f9830be
Ziming <ziminghua331@gmail.com>
2025-05-12 15:36:19 -0400

    minor

--- policyengine_us/variables/gov/irs/credits/earned_income/tax_unit_has_valid_ssn_card_type_for_eitc.py
+++ policyengine_us/variables/gov/irs/credits/earned_income/tax_unit_has_valid_ssn_card_type_for_eitc.py
@@ -5,7 +5,8 @@ class tax_unit_has_valid_ssn_card_type_for_eitc(Variable):
     value_type = bool
     entity = TaxUnit
     definition_period = YEAR
-    label = "All members in the tax unit have valid ssn card type to be eligible for the EITC"
+    label = "All members in the tax unit have valid SSN card type to be eligible for the EITC"
+    reference = "https://www.law.cornell.edu/uscode/text/26/32#c_1_E"
 
     def formula(tax_unit, period, parameters):
         ssn_card_type = tax_unit.members("ssn_card_type", period)
