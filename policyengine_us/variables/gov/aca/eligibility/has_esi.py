b283ad786c36dfde2e3818a0b29990f91be54ccd
daphnehanse11 <128793799+daphnehanse11@users.noreply.github.com>
2025-05-12 17:47:21 -0400

    Update PolicyEngine US

---
+++ policyengine_us/variables/gov/aca/eligibility/has_esi.py
@@ -0,0 +1,10 @@
+from policyengine_us.model_api import *
+
+
+class has_esi(Variable):
+    value_type = bool
+    entity = Person
+    label = "Person currently has ESI"
+    definition_period = YEAR
+    default_value = False
+
