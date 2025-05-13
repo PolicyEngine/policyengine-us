a533b67bb2cd88578f7531c7ba55a01e5fc4d0de
Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>
2025-05-13 13:47:22 -0400

    Apply SALT phase-out to separate filers separately (#5988)
    * Apply SALT phase-out to separate filers separately
    Fixes  #5987
    
    * structure
    
    * test fix
    
    * minor
    
    * format

--- policyengine_us/reforms/salt_phase_out/salt_phase_out_reform.py
+++ policyengine_us/reforms/salt_phase_out/salt_phase_out_reform.py
@@ -21,11 +21,23 @@ def create_salt_phase_out() -> Reform:
             cap = p.cap[filing_status]
             p_ref = parameters(period).gov.contrib.salt_phase_out
             income = tax_unit("adjusted_gross_income", period)
-            joint = filing_status == filing_status.possible_values.JOINT
-            phase_out = where(
-                joint,
-                p_ref.rate.joint.calc(income),
-                p_ref.rate.other.calc(income),
+            phase_out = select(
+                [
+                    filing_status == filing_status.possible_values.SEPARATE,
+                    filing_status == filing_status.possible_values.JOINT,
+                    filing_status
+                    == filing_status.possible_values.HEAD_OF_HOUSEHOLD,
+                    filing_status
+                    == filing_status.possible_values.SURVIVING_SPOUSE,
+                    filing_status == filing_status.possible_values.SINGLE,
+                ],
+                [
+                    p_ref.rate.separate.calc(income),
+                    p_ref.rate.joint.calc(income),
+                    p_ref.rate.head_of_household.calc(income),
+                    p_ref.rate.surviving_spouse.calc(income),
+                    p_ref.rate.single.calc(income),
+                ],
             )
             capped_salt = min_(cap, salt_amount)
             if p_ref.floor.applies:
