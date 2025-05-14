4340685991333d29bd5155b24e4460d81c186bc7
Ziming Hua <113824411+hua7450@users.noreply.github.com>
2025-05-14 13:02:16 -0600

    Add new immigration rules to ACA (#5992)
    * Add new immigration rules to ACA
    Fixes #5983
    
    * make the whole PR to reform
    
    * add to reforms.py
    
    * adjust
    
    * minor and reference
    
    * minor
    
    * fix error
    
    * fix changelog format
    
    ---------
    
    Co-authored-by: Ziming <ziminghua331@gmail.com>

--- policyengine_us/reforms/reconciliation/__init__.py
+++ policyengine_us/reforms/reconciliation/__init__.py
@@ -19,3 +19,6 @@ from .reconciled_additional_senior_standard_deduction import (
 from .reconciled_ssn_for_llc_and_aoc import (
     create_reconciled_ssn_for_llc_and_aoc_reform,
 )
+from .aca_ptc_immigration_status import (
+    create_aca_ptc_immigration_status_reform,
+)
