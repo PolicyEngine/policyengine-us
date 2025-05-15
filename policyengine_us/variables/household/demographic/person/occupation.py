9754bd1ae688af5af331e4bd01bab25ad087eebc
juaristi22 <juaristi@uni.minerva.edu>
2025-05-15 13:15:35 +0200

    occupation as Enum variable

--- policyengine_us/variables/household/demographic/person/occupation.py
+++ policyengine_us/variables/household/demographic/person/occupation.py
@@ -1,9 +1,25 @@
 from policyengine_us.model_api import *
 
 
+class Occupation(Enum):
+    OTHER = 0
+    MANAGEMENT_BUSINESS_FINANCIAL = 1
+    PROFESSIONAL_RELATED = 2
+    SERVICE = 3
+    SALES = 4
+    OFFICE_ADMINISTRATIVE = 5
+    FARMING_FISHING_FORESTRY = 6
+    CONSTRUCTION_EXTRACTION = 7
+    INSTALLATION_MAINTENANCE_REPAIR = 8
+    PRODUCTION = 9
+    TRANSPORTATION_MATERIAL_MOVING = 10
+    MILITARY_SPECIFIC = 11
+
+
 class occupation(Variable):
-    # will add more specific categorical logic after loading variable into CPS
-    value_type = int
+    value_type = Enum
+    possible_values = Occupation
+    default_value = Occupation.OTHER
     entity = Person
     label = "Occupation"
     definition_period = YEAR
