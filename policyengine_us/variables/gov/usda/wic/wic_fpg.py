f2daaf640e4b3ecbfaaa6cba11e580020624837f
Ziming <ziminghua331@gmail.com>
2025-05-22 15:25:01 -0400

    minor

--- policyengine_us/variables/gov/usda/wic/wic_fpg.py
+++ policyengine_us/variables/gov/usda/wic/wic_fpg.py
@@ -12,12 +12,14 @@ class wic_fpg(Variable):
 
     def formula(spm_unit, period, parameters):
         pregnant = spm_unit.any(spm_unit.members("is_pregnant", period))
+        # The system divides annual variables by 12 automatically when bringing them down to a month.
+        # The normal FPG is an annual variable, so the system divides it by 12 by default.
         normal_fpg = spm_unit("spm_unit_fpg", period)
         state_group = spm_unit.household("state_group_str", period)
         additional = parameters(period).gov.hhs.fpg.additional_person[
             state_group
         ]
-        # The system divides annual variables by 12 automatically when bringing them down to a month.
-        # The normal FPG is an annual variable, so the system divides it by 12 by default. 
-        # But the additional amount is based on a yearly parameter, so we need to manually divide it by 12.
-        return normal_fpg + additional * pregnant / MONTHS_IN_YEAR
+        annual_pregnant_addition = additional * pregnant
+        # The additional amount is based on a yearly parameter, so we need to manually divide it by 12.
+        monthly_pregnant_addition = annual_pregnant_addition / MONTHS_IN_YEAR 
+        return normal_fpg + monthly_pregnant_addition
