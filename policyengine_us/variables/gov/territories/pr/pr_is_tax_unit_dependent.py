8263cb74ed4c1005ea16f5e0a2f0d60ad1964505
Lucy Lennemann <78785519+llennemann@users.noreply.github.com>
2025-05-27 16:31:09 +0000

    added pr is tax unit variable

---
+++ policyengine_us/variables/gov/territories/pr/pr_is_tax_unit_dependent.py
@@ -0,0 +1,36 @@
+from policyengine_us.model_api import *
+
+
+class pr_is_tax_unit_dependent(Variable):
+    value_type = bool
+    entity = Person
+    label = "Puerto Rico tax unit dependent"
+    definition_period = YEAR
+    reference = "https://hacienda.pr.gov/sites/default/files/inst_individuals_2023.pdf#page=28"
+
+    def formula(person, period, parameters):
+        # dependent definition in Puerto Rico:
+        p = parameters(period).gov.territories.pr.tax_unit_dependent
+        age = person("age", period)
+
+        # taxpayer is <21 years old OR >= 65 years old
+        age_eligible = (age < p.lower_age_limit) | (age >= p.upper_age_limit)
+
+        # OR the taxpayer's mother or father
+        parent_eligible = person("is_parent_of_filer_or_spouse", period)
+
+        # OR a person over 21 that is incapable of self-support because of a physical or mental condition
+        incapable_of_self_support_eligible = person(
+            "is_incapable_of_self_care", period
+        )
+
+        # OR a university student <26 years old that attended at least 1 years of an accredited uni
+        student_eligible = (person("is_full_time_student", period)) and (
+            age < p.student_age_limit
+        )
+        return (
+            student_eligible
+            or parent_eligible
+            or incapable_of_self_support_eligible
+            or age_eligible
+        )
