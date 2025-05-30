fb2e58cfac33185b8b329d6f62ad2ba91f45a72c
Max Ghenis <mghenis@gmail.com>
2025-05-30 09:46:24 +0100

    Use `defined_for = "ca_eitc_eligible"` in `ca_eitc` Fixes #1906

--- policyengine_us/variables/gov/states/ca/tax/income/credits/earned_income/ca_eitc.py
+++ policyengine_us/variables/gov/states/ca/tax/income/credits/earned_income/ca_eitc.py
@@ -7,12 +7,10 @@ class ca_eitc(Variable):
     label = "CalEITC"
     unit = USD
     definition_period = YEAR
-    defined_for = StateCode.CA
+    defined_for = "ca_eitc_eligible"
 
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states.ca.tax.income.credits.earned_income
-
-        eligible = tax_unit("ca_eitc_eligible", period)
 
         earned_income = tax_unit("filer_adjusted_earnings", period)
         child_count = tax_unit("eitc_child_count", period)
@@ -24,9 +22,7 @@ class ca_eitc(Variable):
         phased_in_amount = phase_in_income * phase_in_rate
 
         phase_out_min_income = p.phase_out.start.calc(child_count)
-        phase_out_rate = (
-            p.phase_out.rate.calc(child_count) * p.adjustment.factor
-        )
+        phase_out_rate = p.phase_out.rate.calc(child_count) * p.adjustment.factor
 
         phase_out_income = max_(0, earned_income - phase_out_min_income)
 
@@ -36,9 +32,7 @@ class ca_eitc(Variable):
         # not the income level.
 
         maximum_eitc = phase_in_max_income * phase_in_rate
-        eitc_fall_by_first_phase_out = (
-            maximum_eitc - second_phase_out_start_eitc
-        )
+        eitc_fall_by_first_phase_out = maximum_eitc - second_phase_out_start_eitc
         earnings_range_of_first_phase_out = (
             eitc_fall_by_first_phase_out / phase_out_rate
         )
@@ -47,9 +41,7 @@ class ca_eitc(Variable):
         )
         second_phase_out_end = p.phase_out.final.end
 
-        phase_out_income = min_(
-            phase_out_income, earnings_range_of_first_phase_out
-        )
+        phase_out_income = min_(phase_out_income, earnings_range_of_first_phase_out)
         amount_after_first_phase_out = (
             phased_in_amount - phase_out_income * phase_out_rate
         )
@@ -58,9 +50,8 @@ class ca_eitc(Variable):
             / (second_phase_out_end - second_phase_out_start),
             1,
         )
-        return eligible * where(
+        return where(
             earned_income > second_phase_out_start,
-            amount_after_first_phase_out
-            * (1 - percentage_along_second_phase_out),
+            amount_after_first_phase_out * (1 - percentage_along_second_phase_out),
             amount_after_first_phase_out,
         )
