0f3de058f52dc4eb8a3c10533791074280e6fe18
Ziming <ziminghua331@gmail.com>
2025-05-30 14:16:07 -0400

    foramt

--- policyengine_us/variables/gov/states/il/dhs/aabd/payment/utility/il_utility_allowance_person.py
+++ policyengine_us/variables/gov/states/il/dhs/aabd/payment/utility/il_utility_allowance_person.py
@@ -4,9 +4,7 @@ from policyengine_us.model_api import *
 class il_aabd_utility_allowance_person(Variable):
     value_type = float
     entity = Person
-    label = (
-        "Illinois Aid to the Aged, Blind or Disabled (AABD) utility allowance per person"
-    )
+    label = "Illinois Aid to the Aged, Blind or Disabled (AABD) utility allowance per person"
     unit = USD
     definition_period = MONTH
     defined_for = StateCode.IL
@@ -15,7 +13,9 @@ class il_aabd_utility_allowance_person(Variable):
     )
 
     def formula(person, period, parameters):
-        utility_allowance = person.spm_unit("il_aabd_utility_allowance", period)
+        utility_allowance = person.spm_unit(
+            "il_aabd_utility_allowance", period
+        )
         size = person.spm_unit("spm_unit_size", period)
         # Prorate the total utility allowance across all household members
         return utility_allowance / size
