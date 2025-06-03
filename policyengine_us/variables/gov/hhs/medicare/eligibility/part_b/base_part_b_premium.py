297b2ce21f3ca3b4103e704266215da5926bc080
daphnehanse11 <128793799+daphnehanse11@users.noreply.github.com>
2025-06-03 16:56:11 -0400

    medicare

--- policyengine_us/variables/gov/hhs/medicare/eligibility/part_b/base_part_b_premium.py
+++ policyengine_us/variables/gov/hhs/medicare/eligibility/part_b/base_part_b_premium.py
@@ -34,13 +34,9 @@ class base_part_b_premium(Variable):
             [
                 brackets.single.calc(0),
                 brackets.joint.calc(0),
-                brackets.single.calc(0),  # HOH uses single brackets
-                brackets.single.calc(
-                    0
-                ),  # Surviving spouse uses single brackets
-                brackets.single.calc(
-                    0
-                ),  # Separate uses single brackets (no separate file exists)
+                brackets.head_of_household.calc(0),  # HOH uses single brackets
+                brackets.surviving_spouce.calc(0),  # Surviving spouse uses single brackets
+                brackets.separate.calc(0),  
             ],
         )
 
