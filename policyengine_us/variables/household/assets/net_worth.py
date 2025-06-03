9463de39afe45b106acf2eb200f301bc6fc64dc3
María Juaristi <127882282+juaristi22@users.noreply.github.com>
2025-06-03 15:33:21 +0200

    Adding wealth variables (#6064)
    * Adding wealth variables (to be imputed)
    
    * update variable names
    
    * keep single net worth variable
    
    * rename net_worth
    
    * fix typo

--- policyengine_us/variables/household/assets/net_worth.py
+++ policyengine_us/variables/household/assets/net_worth.py
@@ -1,10 +1,10 @@
 from policyengine_us.model_api import *
 
 
-class auto_loan_interest(Variable):
+class net_worth(Variable):
     value_type = float
     entity = Household
-    label = "auto loan interest expense"
+    label = "net worth"
     unit = USD
     definition_period = YEAR
     uprating = "gov.bls.cpi.cpi_u"
