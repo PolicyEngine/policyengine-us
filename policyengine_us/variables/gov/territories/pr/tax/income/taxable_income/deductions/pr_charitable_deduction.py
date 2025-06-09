8944be8294b95879d4291182e16e3def742774d6
Lucy Lennemann <78785519+llennemann@users.noreply.github.com>
2025-06-09 22:47:27 +0000

    small wording adjustments

--- policyengine_us/variables/gov/territories/pr/tax/income/taxable_income/deductions/pr_charitable_deduction.py
+++ policyengine_us/variables/gov/territories/pr/tax/income/taxable_income/deductions/pr_charitable_deduction.py
@@ -7,21 +7,17 @@ class pr_charitable_deduction(Variable):
     label = "Puerto Rico charitable deduction"
     unit = USD
     definition_period = YEAR
-    reference = "https://hacienda.pr.gov/sites/default/files/individuals_2024_rev._jul_12_24_9-30-24_informative.pdf#page=5"
+    reference = "https://law.justia.com/codes/puerto-rico/title-thirteen/subtitle-17/part-ii/chapter-1005/subchapter-c/30135/"  # (3)(B)(i)(IV)
     defined_for = StateCode.PR
 
     def formula(tax_unit, period, parameters):
         p = parameters(
             period
         ).gov.territories.pr.tax.income.taxable_income.deductions.charity
-        # sum up charitable contributions - line 1, column B
-        cash_donations = add(tax_unit, period, ["charitable_cash_donations"])
-        non_cash_donations = add(
-            tax_unit, period, ["charitable_non_cash_donations"]
+        charitable_contributions = add(
+            tax_unit,
+            period,
+            ["charitable_cash_donations", "charitable_non_cash_donations"],
         )
-        charitable_sum = cash_donations + non_cash_donations
-
-        # 50% of AGI is the maximum deduction for charitable contributions, line 4
         charity_floor = p.floor * tax_unit("pr_agi", period)
-        # line 5
-        return min_(charitable_sum, charity_floor)
+        return min_(charitable_contributions, charity_floor)
--- policyengine_us/variables/gov/territories/pr/tax/income/taxable_income/deductions/pr_medical_expense_deduction.py
+++ policyengine_us/variables/gov/territories/pr/tax/income/taxable_income/deductions/pr_medical_expense_deduction.py
@@ -7,16 +7,13 @@ class pr_medical_expense_deduction(Variable):
     label = "Puerto Rico medical expense deduction"
     unit = USD
     definition_period = YEAR
-    reference = "https://hacienda.pr.gov/sites/default/files/individuals_2024_rev._jul_12_24_9-30-24_informative.pdf#page=5"
+    reference = "https://law.justia.com/codes/puerto-rico/title-thirteen/subtitle-17/part-ii/chapter-1005/subchapter-c/30135/"  # (4)
     defined_for = StateCode.PR
 
     def formula(tax_unit, period, parameters):
-        # line 1 - column A
         expense = add(tax_unit, period, ["medical_out_of_pocket_expenses"])
         p = parameters(
             period
         ).gov.territories.pr.tax.income.taxable_income.deductions.medical
-        # line 2
         medical_floor = p.floor * tax_unit("pr_agi", period)
-        # line 3
         return max_(0, expense - medical_floor)
