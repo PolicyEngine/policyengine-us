fb8ec2fd9e92c7237015bc1cf6dd5c028db5c535
PavelMakarchuk <pavel.ma99@gmail.com>
2025-06-11 15:01:32 -0400

    typo

--- policyengine_us/variables/gov/aca/aca_take_up_seed.py
+++ policyengine_us/variables/gov/aca/aca_take_up_seed.py
@@ -4,5 +4,5 @@ from policyengine_us.model_api import *
 class aca_take_up_seed(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Randomly assiged seed for ACA take-up"
+    label = "Randomly assigned seed for ACA take-up"
     definition_period = YEAR
