7bbe5cfc23dc78437964b431aed43a0c07ce4d92
PavelMakarchuk <pavel.ma99@gmail.com>
2025-06-11 09:21:37 -0400

    add to comput tree

--- policyengine_us/variables/gov/aca/takes_up_aca_if_eligible.py
+++ policyengine_us/variables/gov/aca/takes_up_aca_if_eligible.py
@@ -3,7 +3,7 @@ from policyengine_us.model_api import *
 
 class takes_up_aca_if_eligible(Variable):
     value_type = bool
-    entity = Person
+    entity = TaxUnit
     label = "Whether a random eligible SPM unit does not claim ACA Premium Tax Credit"
     definition_period = YEAR
     default_value = True
