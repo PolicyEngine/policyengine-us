c9656136767b10a3925a9666eb4929f214c89378
PavelMakarchuk <pavel.ma99@gmail.com>
2025-06-11 14:59:30 -0400

    minor

--- policyengine_us/variables/gov/usda/snap/snap_take_up_seed.py
+++ policyengine_us/variables/gov/usda/snap/snap_take_up_seed.py
@@ -4,5 +4,5 @@ from policyengine_us.model_api import *
 class snap_take_up_seed(Variable):
     value_type = float
     entity = SPMUnit
-    label = "Randomly assiged seed for SNAP take-up"
+    label = "Randomly assigned seed for SNAP take-up"
     definition_period = YEAR
