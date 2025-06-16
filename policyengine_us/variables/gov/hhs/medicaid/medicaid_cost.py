2edf6896b4da229538786b1f1c1dc271d75301c6
daphnehanse11 <128793799+daphnehanse11@users.noreply.github.com>
2025-06-16 11:29:47 -0400

    medicaid

--- policyengine_us/variables/gov/hhs/medicaid/medicaid_cost.py
+++ policyengine_us/variables/gov/hhs/medicaid/medicaid_cost.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class medicaid_cost(Variable):
     value_type = float
     entity = Person
-    label = "Medicaid"
+    label = "Medicaid_cost"
     unit = USD
     definition_period = YEAR
     reference = "https://www.law.cornell.edu/uscode/text/42/1396a"
