286eb2d0c0d8d4862389ca4bafcb907b2aa5ea6f
daphnehanse11 <128793799+daphnehanse11@users.noreply.github.com>
2025-06-18 09:26:20 -0400

    Apply suggestions from code review
    Co-authored-by: Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>

--- policyengine_us/variables/gov/hhs/medicaid/medicaid_enrolled.py
+++ policyengine_us/variables/gov/hhs/medicaid/medicaid_enrolled.py
@@ -2,10 +2,9 @@ from policyengine_us.model_api import *
 
 
 class medicaid_enrolled(Variable):
-    value_type = float
+    value_type = bool
     entity = Person
     label = "Medicaid enrolled"
-    unit = USD
     definition_period = YEAR
     reference = "https://www.law.cornell.edu/uscode/text/42/1396a"
     defined_for = "is_medicaid_eligible"
