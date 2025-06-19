187c547c9912aaec14136631bdad60662d4ac6f5
Pavel Makarchuk <110687043+PavelMakarchuk@users.noreply.github.com>
2025-06-19 19:53:20 -0400

    Senate Finance QBID (#6161)
    Fixes #6155

--- policyengine_us/reforms/reforms.py
+++ policyengine_us/reforms/reforms.py
@@ -108,6 +108,7 @@ from .reconciliation import (
     create_reconciled_additional_senior_standard_deduction_reform,
     create_reconciled_ssn_for_llc_and_aoc_reform,
     create_aca_ptc_immigration_status_reform,
+    create_reconciliation_qbid_with_floor_reform,
 )
 
 
@@ -248,6 +249,9 @@ def create_structural_reforms_from_parameters(parameters, period):
     aca_ptc_immigration_status = create_aca_ptc_immigration_status_reform(
         parameters, period
     )
+    reconciliation_qbid_with_floor = (
+        create_reconciliation_qbid_with_floor_reform(parameters, period)
+    )
     ctc_additional_bracket = create_ctc_additional_bracket_reform(
         parameters, period
     )
@@ -302,6 +306,7 @@ def create_structural_reforms_from_parameters(parameters, period):
         reconciled_additional_senior_standard_deduction,
         reconciled_ssn_for_llc_and_aoc,
         aca_ptc_immigration_status,
+        reconciliation_qbid_with_floor,
         ctc_additional_bracket,
     ]
     reforms = tuple(filter(lambda x: x is not None, reforms))
