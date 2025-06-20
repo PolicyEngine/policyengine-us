ed6bb5a89f51e44bdb9e2a17eeb92c3e21e388e0
baogorek <baogorek@gmail.com>
2025-06-20 10:18:07 -0400

    TCJA qbid params back in and test updates

--- policyengine_us/variables/gov/irs/income/taxable_income/deductions/qualified_business_income_deduction/qualified_business_income_deduction.py
+++ policyengine_us/variables/gov/irs/income/taxable_income/deductions/qualified_business_income_deduction/qualified_business_income_deduction.py
@@ -16,5 +16,12 @@ class qualified_business_income_deduction(Variable):
         # compute sum of QBID amounts for each person in TaxUnit following
         # logic in 2018 IRS Publication 535, Worksheet 12-A, line 16
         person = tax_unit.members
-        qbid_amt = person("qbid_amount", period)
-        return tax_unit.sum(qbid_amt)
+        qbid_amt = person("qbi_deduction_component", period)
+        uncapped_qbid = tax_unit.sum(qbid_amt)
+        # apply taxinc cap at the TaxUnit level following logic
+        # in 2018 IRS Publication 535, Worksheet 12-A, lines 32-37
+        taxinc_less_qbid = tax_unit("taxable_income_less_qbid", period)
+        netcg_qdiv = tax_unit("adjusted_net_capital_gain", period)
+        p = parameters(period).gov.irs.deductions.qbi.max
+        taxinc_cap = p.rate * max_(0, taxinc_less_qbid - netcg_qdiv)
+        return min_(uncapped_qbid, taxinc_cap)
