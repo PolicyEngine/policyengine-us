d9dcf8ef7b5f29fa4bbca2703949464cfe300dd9
Lucy Lennemann <78785519+llennemann@users.noreply.github.com>
2025-07-22 01:58:10 +0000

    Retirement deduction

--- policyengine_us/variables/gov/territories/pr/tax/income/taxable_income/deductions/retirement/pr_retirement_deduction.py
+++ policyengine_us/variables/gov/territories/pr/tax/income/taxable_income/deductions/retirement/pr_retirement_deduction.py
@@ -3,20 +3,21 @@ from policyengine_us.model_api import *
 
 class pr_retirement_deduction(Variable):
     value_type = float
-    entity = TaxUnit
+    entity = Person
     label = "Puerto Rico retirement contribution deduction"
     unit = USD
     definition_period = YEAR
     reference = "https://law.justia.com/codes/puerto-rico/title-thirteen/subtitle-17/part-ii/chapter-1005/subchapter-c/30135/" # (7)   
     defined_for = "pr_retirement_deduction_eligibility"
 
-    def formula(tax_unit, period, parameters):
+    def formula(person, period, parameters):
         p = parameters(
             period
         ).gov.territories.pr.tax.income.taxable_income.deductions.retirement
 
-        max_deduction = min_(p.max, tax_unit("pr_agi", period))
+        agi = person("pr_agi_person", period)
+        max_deduction = min_(p.max, agi)
         contributions = add(
-            tax_unit, period, ["traditional_ira_contributions", "roth_ira_contributions"]
+            person, period, ["traditional_ira_contributions", "roth_ira_contributions"]
         )
         return min_(max_deduction, contributions)
