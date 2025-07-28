92c510a0f83c42d032d0c3581feaebc558e4e3ab
PavelMakarchuk <pavel.ma99@gmail.com>
2025-07-28 14:07:05 +0200

    tip ded. rework

--- policyengine_us/variables/gov/irs/income/taxable_income/deductions/tip_income/tip_income_deduction.py
+++ policyengine_us/variables/gov/irs/income/taxable_income/deductions/tip_income/tip_income_deduction.py
@@ -10,23 +10,6 @@ class tip_income_deduction(Variable):
 
     def formula(tax_unit, period, parameters):
         person = tax_unit.members
-        ssn_card_type = person("ssn_card_type", period)
-        ssn_card_types = ssn_card_type.possible_values
-        citizen = ssn_card_type == ssn_card_types.CITIZEN
-        non_citizen_valid_ead = (
-            ssn_card_type == ssn_card_types.NON_CITIZEN_VALID_EAD
-        )
-        eligible_ssn_card_type = citizen | non_citizen_valid_ead
-        head = person("is_tax_unit_head", period)
-        spouse = person("is_tax_unit_spouse", period)
-        ineligible_head_exists = tax_unit.any(head & ~eligible_ssn_card_type)
-        ineligible_spouse_exists = tax_unit.any(
-            spouse & ~eligible_ssn_card_type
-        )
-        ineligible_cardholder_present = (
-            ineligible_head_exists | ineligible_spouse_exists
-        )
-
         tip_income = person("tip_income", period)
         agi = tax_unit("adjusted_gross_income", period)
         filing_status = tax_unit("filing_status", period)
@@ -37,4 +20,7 @@ class tip_income_deduction(Variable):
         total_tip_income = tax_unit.sum(tip_income)
         capped_tip_income = min_(p.cap, total_tip_income)
         phased_out_tip_income = max_(0, capped_tip_income - phase_out_amount)
+        ineligible_cardholder_present = tax_unit(
+            "tip_income_deduction_ineligible_cardholder_present", period
+        )
         return phased_out_tip_income * ~ineligible_cardholder_present
---
+++ policyengine_us/variables/gov/irs/income/taxable_income/deductions/tip_income/tip_income_deduction_ineligible_cardholder_present.py
@@ -0,0 +1,25 @@
+from policyengine_us.model_api import *
+
+
+class tip_income_deduction_ineligible_cardholder_present(Variable):
+    value_type = bool
+    entity = TaxUnit
+    label = "Ineligible cardholder present for the tip income deduction"
+    definition_period = YEAR
+
+    def formula(tax_unit, period, parameters):
+        person = tax_unit.members
+        ssn_card_type = person("ssn_card_type", period)
+        ssn_card_types = ssn_card_type.possible_values
+        citizen = ssn_card_type == ssn_card_types.CITIZEN
+        non_citizen_valid_ead = (
+            ssn_card_type == ssn_card_types.NON_CITIZEN_VALID_EAD
+        )
+        eligible_ssn_card_type = citizen | non_citizen_valid_ead
+        head = person("is_tax_unit_head", period)
+        spouse = person("is_tax_unit_spouse", period)
+        ineligible_head_exists = tax_unit.any(head & ~eligible_ssn_card_type)
+        ineligible_spouse_exists = tax_unit.any(
+            spouse & ~eligible_ssn_card_type
+        )
+        return ineligible_head_exists | ineligible_spouse_exists
