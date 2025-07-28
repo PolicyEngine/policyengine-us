1ddad72d5ef2b519843eafca691c195504ad574f
PavelMakarchuk <pavel.ma99@gmail.com>
2025-07-28 21:12:54 +0200

    senior ded.

--- policyengine_us/variables/gov/irs/income/taxable_income/deductions/senior_deduction/additional_senior_deduction.py
+++ policyengine_us/variables/gov/irs/income/taxable_income/deductions/senior_deduction/additional_senior_deduction.py
@@ -7,17 +7,17 @@ class additional_senior_deduction(Variable):
     label = "Senior deduction"
     unit = USD
     definition_period = YEAR
-    defined_for = (
-        "filer_meets_additional_senior_deduction_identification_requirements"
-    )
     reference = "https://www.finance.senate.gov/imo/media/doc/finance_committee_legislative_text_title_vii.pdf#page=3"
 
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.irs.deductions.senior_deduction
-        aged_head = tax_unit("aged_head", period).astype(int)
-        aged_spouse = tax_unit("aged_spouse", period).astype(int)
-        aged_count = aged_spouse + aged_head
-        base_deduction = p.amount * aged_count
+        eligible_seniors = add(
+            tax_unit,
+            period,
+            ["meets_additional_senior_deduction_identification_requirements"],
+        )
+
+        base_deduction = p.amount * eligible_seniors
         agi = tax_unit("adjusted_gross_income", period)
         filing_status = tax_unit("filing_status", period)
         joint = filing_status == filing_status.possible_values.JOINT
--- policyengine_us/variables/gov/irs/income/taxable_income/deductions/senior_deduction/meets_additional_senior_deduction_identification_requirements.py
+++ policyengine_us/variables/gov/irs/income/taxable_income/deductions/senior_deduction/meets_additional_senior_deduction_identification_requirements.py
@@ -1,13 +1,11 @@
 from policyengine_us.model_api import *
 
 
-class meets_additional_senior_deduction_identification_requirements(Variable):
+class additional_senior_deduction_eligible_person(Variable):
     value_type = bool
     entity = Person
     definition_period = YEAR
-    label = (
-        "Person meets additional senior deduction identification requirements"
-    )
+    label = "Person is eligible for the additional senior deduction"
     reference = "https://www.finance.senate.gov/imo/media/doc/finance_committee_legislative_text_title_vii.pdf#page=3"
 
     def formula(person, period, parameters):
@@ -17,4 +15,8 @@ class meets_additional_senior_deduction_identification_requirements(Variable):
         non_citizen_valid_ead = (
             ssn_card_type == ssn_card_types.NON_CITIZEN_VALID_EAD
         )
-        return citizen | non_citizen_valid_ead
+        head_or_spouse = person("is_tax_unit_head_or_spouse", period)
+        age = person("age", period)
+        p_irs = parameters(period).gov.irs.deductions.standard.aged_or_blind
+        aged = age >= p_irs.age_threshold
+        return (citizen | non_citizen_valid_ead) & head_or_spouse & aged
