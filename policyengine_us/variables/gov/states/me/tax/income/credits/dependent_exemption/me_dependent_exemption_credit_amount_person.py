c14e62918d88ed0f860aba80a80086b4f7162046
PavelMakarchuk <pavel.ma99@gmail.com>
2025-08-06 18:58:06 +0200

    var and tests

---
+++ policyengine_us/variables/gov/states/me/tax/income/credits/dependent_exemption/me_dependent_exemption_credit_amount_person.py
@@ -0,0 +1,21 @@
+from policyengine_us.model_api import *
+from numpy import ceil
+
+
+class me_dependent_exemption_credit_amount_person(Variable):
+    value_type = float
+    entity = Person
+    label = "Maine dependent exemption credit amount for each person"
+    reference = "https://www.mainelegislature.org/legis/statutes/36/title36sec5219-SS.html"
+    unit = USD
+    definition_period = YEAR
+    defined_for = "ctc_qualifying_child"
+
+    def formula(person, period, parameters):
+        dependent = person("is_tax_unit_dependent", period)
+        p = parameters(
+            period
+        ).gov.states.me.tax.income.credits.dependent_exemption
+        age = person("age", period)
+        multiplier = p.multiplier.calc(age)
+        return dependent * multiplier * p.amount
