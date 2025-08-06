d3a4537e6ce367f5ee43515d1c0a07fbe4876e91
Lucy Lennemann <78785519+llennemann@users.noreply.github.com>
2025-08-06 01:27:17 +0000

    tax unit dependent test fix

--- policyengine_us/variables/gov/territories/pr/pr_is_tax_unit_dependent.py
+++ policyengine_us/variables/gov/territories/pr/pr_is_tax_unit_dependent.py
@@ -11,11 +11,13 @@ class pr_is_tax_unit_dependent(Variable):
 
     def formula(person, period, parameters):
         # dependent definition in Puerto Rico:
-        p = parameters(period).gov.territories.pr.tax_unit_dependent
+        p = parameters(
+            period
+        ).gov.territories.pr.tax.income.taxable_income.exemptions.dependent.age_threshold
         age = person("age", period)
 
         # taxpayer is <21 years old OR >= 65 years old
-        age_eligible = (age < p.lower_age_limit) | (age >= p.upper_age_limit)
+        age_eligible = (age < p.younger) | (age >= p.upper)
 
         # OR the taxpayer's mother or father
         parent_eligible = person("is_parent_of_filer_or_spouse", period)
@@ -29,7 +31,7 @@ class pr_is_tax_unit_dependent(Variable):
 
         # OR a university student <26 years old that attended at least 1 years of an accredited uni
         student_eligible = (person("is_full_time_college_student", period)) & (
-            age < p.student_age_limit
+            age < p.student
         )
         return (
             student_eligible
