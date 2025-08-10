938f29493c9ad10828e50860606720a184762efd
Lucy Lennemann <78785519+llennemann@users.noreply.github.com>
2025-08-10 22:02:21 +0000

    Format and small change

--- policyengine_us/variables/gov/territories/pr/tax/income/pr_gross_income.py
+++ policyengine_us/variables/gov/territories/pr/tax/income/pr_gross_income.py
@@ -3,7 +3,7 @@ from policyengine_us.model_api import *
 
 class pr_gross_income(Variable):
     value_type = float
-    entity = Person
+    entity = TaxUnit
     label = "Puerto Rico gross income"
     unit = USD
     definition_period = YEAR
