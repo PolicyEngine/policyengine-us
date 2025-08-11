d602f3cf81d2993fedbef0fdcc6d6e8e049aeaed
Ziming Hua <113824411+hua7450@users.noreply.github.com>
2025-08-10 21:53:10 -0400

    Head Start and Early Head Start programs payout (#6343)
    * Head Start and Early Head Start programs payout
    Fixes #4774
    
    * add FY2023 values
    
    * descriptions and labels
    
    * rename parameters
    
    * add uprating
    
    * add hhs uprating
    
    * lint
    
    * uprating factor
    
    ---------
    
    Co-authored-by: Ziming <ziminghua331@gmail.com>
    Co-authored-by: PavelMakarchuk <pavel.ma99@gmail.com>

--- policyengine_us/variables/gov/hhs/head_start/head_start.py
+++ policyengine_us/variables/gov/hhs/head_start/head_start.py
@@ -7,3 +7,14 @@ class head_start(Variable):
     label = "Amount of Head Start benefit"
     definition_period = YEAR
     defined_for = "is_head_start_eligible"
+    reference = "https://headstart.gov/program-data/article/head-start-program-facts-fiscal-year-2022"
+
+    def formula(person, period, parameters):
+        p = parameters(period).gov.hhs.head_start
+        state = person.household("state_code_str", period)
+        spending = p.spending[state]
+        enrollment = p.enrollment[state]
+        mask = enrollment > 0
+        result = np.zeros_like(p.spending[state])
+        result[mask] = spending[mask] / enrollment[mask]
+        return result
