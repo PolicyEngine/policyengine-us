23c14e7f41923f1bba260dda5bc2c8ec23f85cc5
Lucy Lennemann <78785519+llennemann@users.noreply.github.com>
2025-08-11 01:38:04 +0000

    Reference

--- policyengine_us/variables/gov/territories/pr/tax/income/taxable_income/pr_net_taxable_income.py
+++ policyengine_us/variables/gov/territories/pr/tax/income/taxable_income/pr_net_taxable_income.py
@@ -5,6 +5,7 @@ class pr_net_taxable_income(Variable):
     value_type = float
     entity = TaxUnit
     label = "Puerto Rico net taxable income"
+    reference = "https://hacienda.pr.gov/sites/default/files/individuals_2024_rev._jul_12_24_9-30-24_informative.pdf#page=2"
     unit = USD
     definition_period = YEAR
 
