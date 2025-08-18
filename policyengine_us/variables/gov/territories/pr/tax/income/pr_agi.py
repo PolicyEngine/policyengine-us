3dae1406414a09415c38df287dbdbb1aeffa210f
Lucy Lennemann <78785519+llennemann@users.noreply.github.com>
2025-08-18 03:10:00 +0000

    Changed calculations

--- policyengine_us/variables/gov/territories/pr/tax/income/pr_agi.py
+++ policyengine_us/variables/gov/territories/pr/tax/income/pr_agi.py
@@ -8,6 +8,14 @@ class pr_agi(Variable):
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.PR
-    reference = "https://hacienda.pr.gov/sites/default/files/individuals_2024_rev._jul_12_24_9-30-24_informative.pdf#page=2"
+    reference = (
+        "https://hacienda.pr.gov/sites/default/files/individuals_2024_rev._jul_12_24_9-30-24_informative.pdf#page=2",
+        "https://law.justia.com/codes/puerto-rico/title-thirteen/subtitle-17/part-ii/chapter-1005/subchapter-a/30103/",
+    )
 
-    adds = ["pr_agi_person"]
+    # Discrepancy: legal code defines Puerto Rico AGI as gross income minus deductions and exemptions, among other items
+    def formula(tax_unit, period, parameters):
+        person = tax_unit.members
+        total_income = tax_unit("pr_gross_income", period)
+        alimony_paid = person("alimony_expense", period)
+        return max_(0, total_income - alimony_paid)
