39dacff157b74889fd84ff79022e41bc4742ca15
Lucy Lennemann <78785519+llennemann@users.noreply.github.com>
2025-08-19 00:41:00 -0700

    Puerto Rico Refundable CTC - form 1040 computation (#5925)
    Implements Puerto Rico refundable CTC calculation following IRS Schedule 8812 Part II-B for bona fide PR residents.

--- policyengine_us/variables/gov/territories/pr/tax/income/pr_gross_income_person.py
+++ policyengine_us/variables/gov/territories/pr/tax/income/pr_gross_income_person.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class pr_gross_income_person(Variable):
     value_type = float
     entity = Person
-    label = "Puerto Rico gross income person"
+    label = "Puerto Rico gross income"
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.PR
