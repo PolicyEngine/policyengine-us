c568fe11170bf9772199b35a3bdfe0babf619c2d
Isabelle Bennett <64175615+isabellejoy00@users.noreply.github.com>
2025-08-22 15:49:37 +0000

    Add child care deduction rules

--- policyengine_us/variables/gov/states/mt/dhs/tanf/income/earned/mt_tanf_earned_income_after_disregard_person.py
+++ policyengine_us/variables/gov/states/mt/dhs/tanf/income/earned/mt_tanf_earned_income_after_disregard_person.py
@@ -16,9 +16,6 @@ class mt_tanf_earned_income_after_disregard_person(Variable):
         gross_earnings = person("mt_tanf_gross_earned_income", period)
         p = parameters(period).gov.states.mt.dhs.tanf.income.deductions
 
-        # MT doesn't have rules for new applicants like DC?
-        #enrolled = person.spm_unit("is_tanf_enrolled", period)
-
         earnings_after_flat_exclusion = max_(
             gross_earnings - p.work_related_expense.amount, 0
         )
@@ -26,8 +23,6 @@ class mt_tanf_earned_income_after_disregard_person(Variable):
             earnings_after_flat_exclusion
             * p.earned_income_disregard.percentage
         )
-
-        #encode tanf cash dependent care disregard here too?
 
         return max_(earnings_after_flat_exclusion - percentage_disregard, 0)
     
