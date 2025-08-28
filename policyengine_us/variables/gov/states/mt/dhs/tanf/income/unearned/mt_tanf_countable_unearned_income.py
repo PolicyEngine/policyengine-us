27076dbb5379050d729cd99268d8fafed2d0653e
Isabelle Bennett <64175615+isabellejoy00@users.noreply.github.com>
2025-08-28 17:09:38 +0000

    Update Montana TANF income parameters and deduction rules

--- policyengine_us/variables/gov/states/mt/dhs/tanf/income/unearned/mt_tanf_countable_unearned_income.py
+++ policyengine_us/variables/gov/states/mt/dhs/tanf/income/unearned/mt_tanf_countable_unearned_income.py
@@ -3,19 +3,10 @@ from policyengine_us.model_api import *
 
 class mt_tanf_countable_unearned_income(Variable):
     value_type = float
-    entity = SPMUnit
+    entity = Person
     label = "Montana Temporary Assistance for Needy Families (TANF) countable unearned income"
     unit = USD
     definition_period = MONTH
-    reference = (
-        "https://dphhs.mt.gov/assets/hcsd/tanfmanual/TANF501-1Jan012018.pdf"
-    )
     defined_for = StateCode.MT
 
-    def formula(spm_unit, period, parameters):
-        p = parameters(period).gov.states.mt.dhs.tanf.income
-        total_unearned_income = add(
-            spm_unit, period, ["mt_tanf_gross_unearned_income"]
-        )
-
-        return max_(0, total_unearned_income)
+    adds = "gov.states.mt.dhs.tanf.income.sources.unearned"
