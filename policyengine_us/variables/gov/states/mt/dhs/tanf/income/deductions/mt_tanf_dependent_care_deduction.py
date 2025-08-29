d424b2f4b87725826a4868c9641507f8ad267515
isabellejoy00 <isabelle-99@live.com>
2025-08-29 12:54:20 -0600

    Refactor Montana TANF income variables

--- policyengine_us/variables/gov/states/mt/dhs/tanf/income/deductions/mt_tanf_dependent_care_deduction.py
+++ policyengine_us/variables/gov/states/mt/dhs/tanf/income/deductions/mt_tanf_dependent_care_deduction.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class mt_tanf_dependent_care_deduction(Variable):
     value_type = float
     entity = SPMUnit
-    label = "Montana Temporary Assistance for Needy Families (TANF) dependent care deduction "
+    label = "Montana Temporary Assistance for Needy Families (TANF) dependent care deduction"
     unit = USD
     definition_period = MONTH
     reference = (
@@ -17,14 +17,16 @@ class mt_tanf_dependent_care_deduction(Variable):
             period
         ).gov.states.mt.dhs.tanf.income.deductions.dependent_care
         person = spm_unit.members
-        dependent = person("is_tax_unit_dependent", period)
+        
+        is_dependent = person("is_tax_unit_dependent", period)
         is_child = person("is_child", period)
         is_incapable_of_self_care = person("is_incapable_of_self_care", period)
-        eligible_person = dependent & (is_child | is_incapable_of_self_care)
+        is_eligible_person = is_dependent & (is_child | is_incapable_of_self_care)
+        
         dependent_expenses = spm_unit(
             "childcare_expenses", period
         )
-        dependent_deduction_person = p.amount.value * eligible_person
+        dependent_deduction_person = p.amount.value * is_eligible_person
         total_dependent_deduction = spm_unit.sum(dependent_deduction_person)
 
         return min_(dependent_expenses, total_dependent_deduction)
