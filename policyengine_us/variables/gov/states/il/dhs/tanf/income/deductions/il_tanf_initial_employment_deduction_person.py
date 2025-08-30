5d050ca1c772957d0f9b9a54c15e03c314d49c96
PavelMakarchuk <pavel.ma99@gmail.com>
2025-08-29 20:54:51 -0400

    lint

--- policyengine_us/variables/gov/states/il/dhs/tanf/income/deductions/il_tanf_initial_employment_deduction_person.py
+++ policyengine_us/variables/gov/states/il/dhs/tanf/income/deductions/il_tanf_initial_employment_deduction_person.py
@@ -16,7 +16,9 @@ class il_tanf_initial_employment_deduction_person(Variable):
         ).gov.states.il.dhs.tanf.income.initial_employment_deduction
 
         gross_earned_income = person("il_tanf_gross_earned_income", period)
-        fpg = person.spm_unit("il_tanf_initial_employment_deduction_fpg", period)
+        fpg = person.spm_unit(
+            "il_tanf_initial_employment_deduction_fpg", period
+        )
         payment_level = person.spm_unit(
             "il_tanf_payment_level_for_initial_eligibility", period
         )
