4160581a3173c075428633f81b3919ab4ba75d36
Ziming <ziming@policyengine.org>
2025-09-03 16:46:01 -0400

    adjustment

--- policyengine_us/variables/gov/states/nc/ncdhhs/scca/nc_scca.py
+++ policyengine_us/variables/gov/states/nc/ncdhhs/scca/nc_scca.py
@@ -10,10 +10,10 @@ class nc_scca(Variable):
     defined_for = "nc_scca_entry_eligible"
 
     def formula(spm_unit, period, parameters):
-        total_market_rate = add(spm_unit, period, ["nc_scca_market_rate"])
         parent_fee = spm_unit("nc_scca_parent_fee", period)
         childcare_expenses = spm_unit(
             "spm_unit_pre_subsidy_childcare_expenses", period
         )
         uncapped_amount = max_(childcare_expenses - parent_fee, 0)
-        return min_(uncapped_amount, total_market_rate)
+        maximum_payment = spm_unit("nc_scca_maximum_payment", period)
+        return min_(uncapped_amount, maximum_payment)
--- policyengine_us/variables/gov/states/nc/ncdhhs/scca/nc_scca_maximum_payment.py
+++ policyengine_us/variables/gov/states/nc/ncdhhs/scca/nc_scca_maximum_payment.py
@@ -1,12 +1,13 @@
 from policyengine_us.model_api import *
 
 
-class nc_scca_maximum_net_payment(Variable):
+class nc_scca_maximum_payment(Variable):
     value_type = float
     entity = SPMUnit
-    label = "North Carolina Subsidized Child Care Assistance Program maximum net payment"
+    label = "North Carolina Subsidized Child Care Assistance Program maximum payment"
     unit = USD
     definition_period = MONTH
+    reference = "https://www.publichealthlawcenter.org/sites/default/files/NC%20Chapter%2010%20Subsidized%20Child%20Care%20Rules.pdf#page=2"
     defined_for = "nc_scca_entry_eligible"
 
     def formula(spm_unit, period, parameters):
