368843f5c77f4059c8c4c0ff1fd982109133857b
isabellejoy00 <isabelle-99@live.com>
2025-09-15 10:33:30 -0600

    Update Montana TANF income standards test inputs

--- policyengine_us/variables/gov/states/mt/dhs/tanf/assistance_unit/mt_tanf_assistance_unit_fpg.py
+++ policyengine_us/variables/gov/states/mt/dhs/tanf/assistance_unit/mt_tanf_assistance_unit_fpg.py
@@ -10,15 +10,16 @@ class mt_tanf_assistance_unit_fpg(Variable):
     definition_period = MONTH
 
     def formula(spm_unit, period, parameters):
-        n = spm_unit("mt_tanf_assistance_unit_size", period.this_year)
-        state_group = spm_unit.household("state_group_str", period.this_year)
+        n = spm_unit("mt_tanf_assistance_unit_size", period)
+        capped_size = min_(n, 20)
+        state_group = spm_unit.household("state_group_str", period)
         year = period.start.year
         month = period.start.month
         if month >= 7:
-            instant_str = f"{year}-7-01"
+            instant_str = f"{year}-07-01"
         else:
-            instant_str = f"{year - 1}-7-01"
+            instant_str = f"{year - 1}-07-01"
         p_fpg = parameters(instant_str).gov.hhs.fpg
         p1 = p_fpg.first_person[state_group] / MONTHS_IN_YEAR
         pn = p_fpg.additional_person[state_group] / MONTHS_IN_YEAR
-        return p1 + pn * (n - 1)
+        return p1 + pn * (capped_size - 1)
