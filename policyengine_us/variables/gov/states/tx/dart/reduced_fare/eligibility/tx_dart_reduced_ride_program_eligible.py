8afbe090907b1afaf7e25a834ad7ea02d71514b8
Ziming <ziming@policyengine.org>
2025-09-23 17:23:00 -0400

    format

--- policyengine_us/variables/gov/states/tx/dart/reduced_fare/eligibility/tx_dart_reduced_ride_program_eligible.py
+++ policyengine_us/variables/gov/states/tx/dart/reduced_fare/eligibility/tx_dart_reduced_ride_program_eligible.py
@@ -4,16 +4,14 @@ from policyengine_us.model_api import *
 class tx_dart_reduced_ride_program_eligible(Variable):
     value_type = bool
     entity = Person
-    label = (
-        "Eligible for Dallas Area Rapid Transit (DART) Reduce Fare program due to qualifying program enrollment"
-    )
+    label = "Eligible for Dallas Area Rapid Transit (DART) Reduce Fare program due to qualifying program enrollment"
     definition_period = YEAR
     defined_for = StateCode.TX
     reference = "https://www.dart.org/fare/general-fares-and-overview/discount-gopass-tap-card"
 
     # The Discount GoPass program provides 50% fare reduction for riders enrolled
-    # in qualifying assistance programs. It is the same as Reduced Fare program. 
-    # This uses the 'adds' parameter to check if the person receives benefits from any 
+    # in qualifying assistance programs. It is the same as Reduced Fare program.
+    # This uses the 'adds' parameter to check if the person receives benefits from any
     # program listed in qualifying_programs.yaml
     # (SNAP, Medicaid, Medicare, CHIP, TANF, WIC, etc.)
     # Note: Some programs like CEAP and DHA Housing are listed but not yet modeled
