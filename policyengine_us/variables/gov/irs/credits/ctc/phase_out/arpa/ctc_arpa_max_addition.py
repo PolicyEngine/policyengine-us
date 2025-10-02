30a71fe94d7a680093c4b2fd74348e69e6f4c2fc
PavelMakarchuk <pavel.ma99@gmail.com>
2025-10-02 15:21:49 -0400

    fixes to the logic and tests

--- policyengine_us/variables/gov/irs/credits/ctc/phase_out/arpa/ctc_arpa_max_addition.py
+++ policyengine_us/variables/gov/irs/credits/ctc/phase_out/arpa/ctc_arpa_max_addition.py
@@ -27,3 +27,4 @@ class ctc_arpa_max_addition(Variable):
             tax_unit, period, ["ctc_child_individual_maximum_arpa"]
         )
         return ctc_child_maximum_arpa - ctc_child_maximum_base
+ 
