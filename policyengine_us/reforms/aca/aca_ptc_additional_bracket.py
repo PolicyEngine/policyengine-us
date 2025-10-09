45ec193ffe0d802e633ea3daa97b20c139d3879a
daphnehanse11 <128793799+daphnehanse11@users.noreply.github.com>
2025-10-09 11:55:02 -0400

    includes income threshold changes

--- policyengine_us/reforms/aca/aca_ptc_additional_bracket.py
+++ policyengine_us/reforms/aca/aca_ptc_additional_bracket.py
@@ -35,9 +35,63 @@ def create_aca_ptc_additional_bracket() -> Reform:
             ).gov.contrib.aca.ptc_additional_bracket.brackets
             return np.interp(magi_frac, p.thresholds, p.amounts)
 
+    class is_aca_ptc_eligible(Variable):
+        """
+        Extended ACA PTC eligibility beyond 400% FPL.
+        """
+
+        value_type = bool
+        entity = Person
+        label = "Person is eligible for ACA premium tax credit and pays ACA premium"
+        definition_period = YEAR
+
+        def formula(person, period, parameters):
+            # determine status eligibility for ACA PTC
+            fstatus = person.tax_unit("filing_status", period)
+            separate = fstatus == fstatus.possible_values.SEPARATE
+            immigration_eligible = person(
+                "is_aca_ptc_immigration_status_eligible", period
+            )
+            taxpayer_has_itin = person.tax_unit("taxpayer_has_itin", period)
+            is_status_eligible = (
+                taxpayer_has_itin & ~separate & immigration_eligible
+            )
+
+            # determine coverage eligibility for ACA plan
+            INELIGIBLE_COVERAGE = [
+                "is_medicaid_eligible",
+                "is_chip_eligible",
+                "is_aca_eshi_eligible",
+                "is_medicare_eligible",
+            ]
+            is_coverage_eligible = (
+                add(person, period, INELIGIBLE_COVERAGE) == 0
+            )
+
+            # determine income eligibility for ACA PTC (using reform parameter)
+            p = parameters(period).gov.contrib.aca.ptc_additional_bracket
+            magi_frac = person.tax_unit("aca_magi_fraction", period)
+            is_income_eligible = p.income_eligibility.calc(magi_frac)
+
+            # determine which people pay an age-based ACA plan premium
+            p_aca = parameters(period).gov.aca
+            is_aca_adult = person("age", period) > p_aca.slcsp.max_child_age
+            child_pays = (
+                person("aca_child_index", period) <= p_aca.max_child_count
+            )
+            pays_aca_premium = is_aca_adult | child_pays
+
+            return (
+                is_status_eligible
+                & is_coverage_eligible
+                & is_income_eligible
+                & pays_aca_premium
+            )
+
     class reform(Reform):
         def apply(self):
             self.update_variable(aca_ptc_phase_out_rate)
+            self.update_variable(is_aca_ptc_eligible)
 
     return reform
 
