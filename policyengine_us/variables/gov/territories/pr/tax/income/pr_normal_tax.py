a36a85d6ee6a5323a2f5ca51279c58cf221d0b27
Lucy Lennemann <78785519+llennemann@users.noreply.github.com>
2025-10-13 19:27:09 +0000

    Rename variable

--- policyengine_us/variables/gov/territories/pr/tax/income/pr_normal_tax.py
+++ policyengine_us/variables/gov/territories/pr/tax/income/pr_normal_tax.py
@@ -1,10 +1,10 @@
 from policyengine_us.model_api import *
 
 
-class pr_tax(Variable):
+class pr_normal_tax(Variable):
     value_type = float
     entity = TaxUnit
-    label = "Puerto Rico tax"
+    label = "Puerto Rico normal tax"
     unit = USD
     definition_period = YEAR
     defined_for = StateCode.PR
