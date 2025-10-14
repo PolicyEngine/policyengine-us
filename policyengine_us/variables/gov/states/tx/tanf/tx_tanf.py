42a22c44945efae5d7733e21cf477acca39b61d1
Ziming <ziming@policyengine.org>
2025-10-13 22:19:53 -0400

    rename variables

--- policyengine_us/variables/gov/states/tx/tanf/tx_tanf.py
+++ policyengine_us/variables/gov/states/tx/tanf/tx_tanf.py
@@ -1,25 +1,25 @@
 from policyengine_us.model_api import *
 
 
-class tx_tanf(Variable):
+class tx_tanf_optimal(Variable):
     value_type = float
     entity = SPMUnit
-    label = "Texas Temporary Assistance for Needy Families (TANF)"
+    label = "Texas TANF optimal benefit (TANF or OTTANF, whichever is higher)"
     unit = USD
     definition_period = MONTH
     reference = (
-        "https://www.hhs.texas.gov/handbooks/texas-works-handbook/a-1340-income-limits",
-        "https://www.hhs.texas.gov/handbooks/texas-works-handbook/c-110-tanf",
+        "https://www.hhs.texas.gov/handbooks/texas-works-handbook/a-2410-general-policy",
     )
-    defined_for = "tx_tanf_eligible"
+    defined_for = StateCode.TX
 
     def formula(spm_unit, period, parameters):
-        payment_standard = spm_unit("tx_tanf_payment_standard", period)
-        countable_income = spm_unit("tx_tanf_countable_income", period)
-        p = parameters(period).gov.states.tx.tanf
+        # Compare monthly TANF benefit with monthly equivalent of OTTANF
+        # Return whichever is higher for the household
 
-        # Calculate benefit as payment standard minus countable income
-        calculated_benefit = max_(payment_standard - countable_income, 0)
+        # Regular TANF: monthly benefit
+        tanf = spm_unit("tx_tanf", period)
 
-        # Apply minimum grant rule
-        return max_(calculated_benefit, p.minimum_grant)
+        # OTTANF: one-time payment divided by 12 for monthly equivalent
+        ottanf = spm_unit("tx_ottanf", period)
+
+        return max_(tanf, ottanf)
