2e3ce2a87931e5eef9f372508b4f40f13bc447a4
Lucy Lennemann <78785519+llennemann@users.noreply.github.com>
2025-10-22 16:17:39 +0000

    Added state code

--- policyengine_us/variables/gov/territories/pr/tax/income/taxable_income/pr_net_taxable_income.py
+++ policyengine_us/variables/gov/territories/pr/tax/income/taxable_income/pr_net_taxable_income.py
@@ -8,6 +8,7 @@ class pr_net_taxable_income(Variable):
     reference = "https://hacienda.pr.gov/sites/default/files/individuals_2024_rev._jul_12_24_9-30-24_informative.pdf#page=2"
     unit = USD
     definition_period = YEAR
+    defined_for = StateCode.PR
 
     def formula(tax_unit, period, parameters):
         agi = tax_unit("pr_agi", period)
