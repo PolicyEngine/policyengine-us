3ec13ac14cb31d4c630f8da04eb2dabddc631315
David Trimmer <david@policyengine.org>
2025-11-04 15:43:27 -0500

    fix

--- policyengine_us/variables/gov/states/ga/tax/income/credits/ga_itemizer_credit.py
+++ policyengine_us/variables/gov/states/ga/tax/income/credits/ga_itemizer_credit.py
@@ -8,11 +8,12 @@ class ga_itemizer_credit(Variable):
     unit = USD
     definition_period = YEAR
     reference = "https://law.justia.com/codes/georgia/2022/title-48/chapter-7/article-2/section-48-7-29-23/"
-    defined_for = "tax_unit_itemizes"
+    defined_for = StateCode.GA
 
     def formula(tax_unit, period, parameters):
         # Full-year and part-year residents who itemize their deductions
         # are entitled to a credit up to $300 per taxpayer
+        itemizes = tax_unit("tax_unit_itemizes", period)
         taxpayer_count = tax_unit("head_spouse_count", period)
         p = parameters(period).gov.states.ga.tax.income.credits.itemizer
-        return p.amount * taxpayer_count
+        return where(itemizes, p.amount * taxpayer_count, 0)
