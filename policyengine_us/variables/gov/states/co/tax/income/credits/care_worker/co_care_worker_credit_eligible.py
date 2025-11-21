0c650556dd69c4a8b19f4a3c140f1b24cc26cefb
PavelMakarchuk <pavel.ma99@gmail.com>
2025-11-21 23:59:18 +0800

    format and head/spouse condition

--- policyengine_us/variables/gov/states/co/tax/income/credits/care_worker/co_care_worker_credit_eligible.py
+++ policyengine_us/variables/gov/states/co/tax/income/credits/care_worker/co_care_worker_credit_eligible.py
@@ -18,12 +18,5 @@ class co_care_worker_credit_eligible(Variable):
             joint, p.income_limit.joint, p.income_limit.single
         )
         income_eligible = agi <= income_limit
-        eligible_care_worker_present = (
-            add(
-                tax_unit,
-                period,
-                ["co_care_worker_credit_eligible_care_worker"],
-            )
-            > 0
-        )
+        eligible_care_worker_present = tax_unit("co_care_worker_credit_eligible_care_worker_count", period) > 0
         return income_eligible & eligible_care_worker_present
