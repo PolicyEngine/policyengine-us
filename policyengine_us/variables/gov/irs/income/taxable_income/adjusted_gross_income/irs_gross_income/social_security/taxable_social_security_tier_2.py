73126cfe3b7cb09fd96c3c78b8f48741b0a9d5d8
PavelMakarchuk <pavel.ma99@gmail.com>
2025-12-14 16:50:18 -0500

    format

--- policyengine_us/variables/gov/irs/income/taxable_income/adjusted_gross_income/irs_gross_income/social_security/taxable_social_security_tier_2.py
+++ policyengine_us/variables/gov/irs/income/taxable_income/adjusted_gross_income/irs_gross_income/social_security/taxable_social_security_tier_2.py
@@ -9,7 +9,7 @@ class taxable_social_security_tier_2(Variable):
     documentation = "Taxable Social Security from 50-85% taxation tier, credited to Medicare HI trust fund"
     unit = USD
     reference = "https://www.law.cornell.edu/uscode/text/26/86#a_2"
-    
+
     # tier_2 = total_taxable - tier_1 capped at 0
     def formula(tax_unit, period, parameters):
         tier_1 = tax_unit("taxable_social_security_tier_1", period)
