75696f21b46e886fca57eaf08dcc73bb28a1550d
Ziming <ziming@policyengine.org>
2025-12-16 11:57:51 -0500

    rename variable.

--- policyengine_us/variables/household/demographic/person/has_prior_formal_early_learning.py
+++
@@ -1,9 +0,0 @@
-from policyengine_us.model_api import *
-
-
-class has_prior_formal_early_learning(Variable):
-    value_type = bool
-    entity = Person
-    label = "Has previously participated in formal early learning"
-    definition_period = YEAR
-    documentation = "Whether the child has previously participated in a formal early learning program (e.g., Head Start, preschool, licensed childcare)"
