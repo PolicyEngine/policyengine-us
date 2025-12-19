b0cbb045d525aa99835b67ed4657abf778feac82
Ziming <ziming@policyengine.org>
2025-12-19 12:15:10 -0500

    delete unused filles

--- policyengine_us/variables/household/demographic/person/has_iep.py
+++
@@ -1,8 +0,0 @@
-from policyengine_us.model_api import *
-
-
-class has_iep(Variable):
-    value_type = bool
-    entity = Person
-    label = "Has an Individualized Education Program (IEP)"
-    definition_period = YEAR
