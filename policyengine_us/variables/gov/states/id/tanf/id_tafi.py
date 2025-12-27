2a675d9218f5b72b1ffdaaa6251b23ab5fa05d86
Ziming <ziming@policyengine.org>
2025-12-27 14:09:38 -0500

    test

--- policyengine_us/variables/gov/states/id/tanf/id_tafi.py
+++ policyengine_us/variables/gov/states/id/tanf/id_tafi.py
@@ -1,5 +1,4 @@
 from policyengine_us.model_api import *
-import numpy as np
 
 
 class id_tafi(Variable):
@@ -8,18 +7,11 @@ class id_tafi(Variable):
     label = "Idaho Temporary Assistance for Families in Idaho (TAFI)"
     unit = USD
     definition_period = MONTH
-    reference = (
-        "https://www.law.cornell.edu/regulations/idaho/IDAPA-16.03.08.248",
-        "https://www.law.cornell.edu/regulations/idaho/IDAPA-16.03.08.252",
-    )
+    reference = "https://www.law.cornell.edu/regulations/idaho/IDAPA-16.03.08.248"
     defined_for = "id_tafi_eligible"
 
     def formula(spm_unit, period, parameters):
         p = parameters(period).gov.states.id.tanf
         grant_standard = spm_unit("id_tafi_grant_standard", period)
-        # Per IDAPA 16.03.08.252: Grant is capped at maximum grant
-        # and rounded down to the next lowest dollar
-        capped_grant = min_(grant_standard, p.maximum_grant)
-        floored_grant = np.floor(max_(capped_grant, 0))
-        # Per IDAPA 16.03.08.254: No payment when grant is less than $10
-        return where(floored_grant >= 10, floored_grant, 0)
+        # Cap at maximum grant
+        return min_(grant_standard, p.maximum_grant)
