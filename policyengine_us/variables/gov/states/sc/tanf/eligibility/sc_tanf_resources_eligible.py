444d0915f60496ba7cd4bcb49a350d33acddf02f
Ziming <ziming@policyengine.org>
2026-01-06 18:54:13 -0500

    add tests

--- policyengine_us/variables/gov/states/sc/tanf/eligibility/sc_tanf_resources_eligible.py
+++ policyengine_us/variables/gov/states/sc/tanf/eligibility/sc_tanf_resources_eligible.py
@@ -9,22 +9,6 @@ class sc_tanf_resources_eligible(Variable):
     defined_for = StateCode.SC
 
     def formula(spm_unit, period, parameters):
-        resource_limit = parameters(period).gov.states.sc.tanf.resources.limit
-        countable_resources = spm_unit("sc_tanf_countable_resources", period)
-        # Each driver's license can exclude one vehicle.
-        adult_count = spm_unit("spm_unit_count_adults", period.this_year)
-        vehicle_count = spm_unit.household(
-            "household_vehicles_owned", period.this_year
-        )
-        vehicle_value = spm_unit.household(
-            "household_vehicles_value", period.this_year
-        )
-        avg_vehicle_value = np.zeros_like(vehicle_count)
-        mask = vehicle_count != 0
-        avg_vehicle_value[mask] = vehicle_value[mask] / vehicle_count[mask]
-        countable_vehicle_value = (
-            vehicle_count - adult_count
-        ) * avg_vehicle_value
-        return (
-            countable_resources + countable_vehicle_value
-        ) <= resource_limit
+        p = parameters(period).gov.states.sc.tanf.resources
+        assets = spm_unit("spm_unit_assets", period.this_year)
+        return assets <= p.limit
