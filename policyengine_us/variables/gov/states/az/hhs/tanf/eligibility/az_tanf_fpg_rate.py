401a8676e44506ddb1dbe1295687c6152387a4e7
Ziming <ziming@policyengine.org>
2026-01-12 21:13:47 -0500

    minor

--- policyengine_us/variables/gov/states/az/hhs/tanf/eligibility/az_tanf_fpg_rate.py
+++ policyengine_us/variables/gov/states/az/hhs/tanf/eligibility/az_tanf_fpg_rate.py
@@ -5,7 +5,7 @@ class az_tanf_fpg_rate(Variable):
     value_type = float
     entity = SPMUnit
     label = "Arizona TANF needy family FPG rate"
-    definition_period = YEAR
+    definition_period = MONTH
     reference = "https://www.azleg.gov/ars/46/00292.htm"
     defined_for = StateCode.AZ
 
@@ -16,10 +16,11 @@ class az_tanf_fpg_rate(Variable):
         p = parameters(period).gov.states.az.hhs.tanf.income.fpg_limit
 
         person = spm_unit.members
-        is_head = person("is_tax_unit_head", period)
+        # Use period.this_year for YEAR-defined demographic variables
+        is_head = person("is_tax_unit_head", period.this_year)
         # Use is_parent which checks own_children_in_household > 0
-        is_parent = person("is_parent", period)
-        is_child = person("is_child", period)
+        is_parent = person("is_parent", period.this_year)
+        is_child = person("is_child", period.this_year)
 
         # Check if head is a parent (has own children in household)
         head_is_parent = spm_unit.any(is_head & is_parent)
