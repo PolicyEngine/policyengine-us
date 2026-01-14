c1aa1d1e456ede88f4088ac33cc9c35e08253263
Ziming <ziming@policyengine.org>
2026-01-13 20:46:07 -0500

    Add historical TANF parameter data (pre-2018) for DE, MO, PA, RI, SD Fixes #7169

--- policyengine_us/variables/gov/states/pa/dhs/tanf/pa_tanf.py
+++ policyengine_us/variables/gov/states/pa/dhs/tanf/pa_tanf.py
@@ -9,11 +9,28 @@ class pa_tanf(Variable):
     definition_period = MONTH
     defined_for = "pa_tanf_eligible"
     reference = (
-        "https://www.pa.gov/agencies/dhs/resources/cash-assistance/tanf"
+        "https://www.pa.gov/agencies/dhs/resources/cash-assistance/tanf",
+        "http://services.dpw.state.pa.us/oimpolicymanuals/cash/137_Cash_Initiatives/137.4_Work_Expense_Reimbursement.htm",
     )
 
     def formula(spm_unit, period, parameters):
+        p = parameters(period).gov.states.pa.dhs.tanf.income.work_expense
+
         maximum_benefit = spm_unit("pa_tanf_maximum_benefit", period)
         countable_income = spm_unit("pa_tanf_countable_income", period)
+        base_grant = max_(maximum_benefit - countable_income, 0)
 
-        return max_(maximum_benefit - countable_income, 0)
+        # Work Expense Reimbursement (WER): $50/month bonus for families with earned income
+        # Active 2009-2020, before Work Expense Deduction (WED) replaced it
+        gross_earned_income = add(
+            spm_unit, period, ["tanf_gross_earned_income"]
+        )
+        has_earned_income = gross_earned_income > 0
+        uses_wer = p.uses_deduction == False
+        wer_amount = where(
+            uses_wer & has_earned_income,
+            p.reimbursement,
+            0,
+        )
+
+        return base_grant + wer_amount
