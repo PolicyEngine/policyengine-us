907c5e918d09adf2cdeb6d76a9db34420f1b75f7
Ziming <ziming@policyengine.org>
2026-01-15 18:24:10 -0500

    minor

--- policyengine_us/variables/gov/states/il/tollway/ipass_assist/il_ipass_assist_categorical_eligible.py
+++ policyengine_us/variables/gov/states/il/tollway/ipass_assist/il_ipass_assist_categorical_eligible.py
@@ -6,10 +6,10 @@ class il_ipass_assist_categorical_eligible(Variable):
     entity = SPMUnit
     definition_period = YEAR
     label = "Illinois I-PASS Assist categorically eligible"
-    reference = (
+    reference = [
         "https://agency.illinoistollway.com/assist",
         "https://www.dhs.state.il.us/page.aspx?item=150431",
-    )
+    ]
     defined_for = StateCode.IL
 
     def formula(spm_unit, period, parameters):
