a3404dbadd5179199e0a145a4a6cebd5b7b194cf
Ziming <ziming@policyengine.org>
2026-01-22 00:33:20 -0500

    tests

--- policyengine_us/variables/gov/states/ca/cdss/child_care/common/ca_child_care_countable_income.py
+++ policyengine_us/variables/gov/states/ca/cdss/child_care/common/ca_child_care_countable_income.py
@@ -8,6 +8,8 @@ class ca_child_care_countable_income(Variable):
     unit = USD
     definition_period = MONTH
     defined_for = StateCode.CA
-    reference = "https://www.law.cornell.edu/regulations/california/5-CCR-18078"
+    reference = (
+        "https://www.law.cornell.edu/regulations/california/5-CCR-18078"
+    )
 
     adds = "gov.states.ca.cdss.child_care.income.sources"
