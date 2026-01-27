c8c686b5e1118e249dfe06fd3436be3ee4fa15de
Ziming <ziming@policyengine.org>
2026-01-26 23:55:41 -0500

    minor

--- policyengine_us/variables/gov/states/il/tollway/ipass_assist/il_ipass_assist_categorical_eligible.py
+++ policyengine_us/variables/gov/states/il/tollway/ipass_assist/il_ipass_assist_categorical_eligible.py
@@ -6,12 +6,14 @@ class il_ipass_assist_categorical_eligible(Variable):
     entity = SPMUnit
     definition_period = YEAR
     label = "Illinois I-PASS Assist categorically eligible"
-    reference = [
-        "https://agency.illinoistollway.com/assist",
+    reference = (
         "https://www.dhs.state.il.us/page.aspx?item=150431",
-    ]
+        "https://agency.illinoistollway.com/documents/20184/1344218/2023-06_IPASS%20Assist%20One%20Pager.pdf/11051bbf-1809-6d1b-de08-90ba3d931b23#page=1",
+    )
     defined_for = StateCode.IL
 
     def formula(spm_unit, period, parameters):
-        # Per IL DHS: "Customers getting SNAP may be eligible for I-PASS Assist"
-        return spm_unit("snap", period) > 0
+        # IL Tollway: eligible if "currently participating in SNAP or TANF"
+        snap = spm_unit("snap", period) > 0
+        tanf = spm_unit("il_tanf", period) > 0
+        return snap | tanf
