82edb409aa88317e71ecaf84558eb643af63c3d9
baogorek@gmail.com <baogorek@gmail.com>
2026-02-03 08:35:32 -0500

    Make medicaid takeup seed deterministic

--- policyengine_us/variables/gov/hhs/medicaid/takes_up_medicaid_if_eligible.py
+++ policyengine_us/variables/gov/hhs/medicaid/takes_up_medicaid_if_eligible.py
@@ -4,7 +4,7 @@ from policyengine_us.model_api import *
 class takes_up_medicaid_if_eligible(Variable):
     value_type = bool
     entity = Person
-    label = "Whether a random eligible person takes up Medicaid"
+    label = "Whether an eligible person takes up Medicaid"
     definition_period = YEAR
 
     def formula(person, period, parameters):
