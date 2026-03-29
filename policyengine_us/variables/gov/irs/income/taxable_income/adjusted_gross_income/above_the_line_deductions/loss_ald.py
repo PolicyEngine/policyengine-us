867ca3b8bac7bf0b20fae0a37e5ec206a628c6eb
Max Ghenis <mghenis@gmail.com>
2026-03-29 17:30:15 -0400

    Fix loss_ald netting under section 461(l)

--- policyengine_us/variables/gov/irs/income/taxable_income/adjusted_gross_income/above_the_line_deductions/loss_ald.py
+++ policyengine_us/variables/gov/irs/income/taxable_income/adjusted_gross_income/above_the_line_deductions/loss_ald.py
@@ -6,9 +6,7 @@ class loss_ald(Variable):
     entity = TaxUnit
     label = "Business loss ALD"
     unit = USD
-    documentation = (
-        "Above-the-line deduction from gross income for business losses."
-    )
+    documentation = "Above-the-line deduction from gross income for business losses."
     definition_period = YEAR
     reference = (
         "https://www.law.cornell.edu/uscode/text/26/461#l",
@@ -21,12 +19,16 @@ class loss_ald(Variable):
         person = tax_unit.members
         indiv_se_loss = max_(0, -person("self_employment_income", period))
         self_employment_loss = tax_unit.sum(indiv_se_loss)
+        indiv_se_gain = max_(0, person("self_employment_income", period))
+        self_employment_gain = tax_unit.sum(indiv_se_gain)
         indiv_ps_loss = max_(0, -person("partnership_s_corp_income", period))
         partnership_s_corp_loss = tax_unit.sum(indiv_ps_loss)
+        indiv_ps_gain = max_(0, person("partnership_s_corp_income", period))
+        partnership_s_corp_gain = tax_unit.sum(indiv_ps_gain)
+        business_loss = self_employment_loss + partnership_s_corp_loss
+        business_gain = self_employment_gain + partnership_s_corp_gain
+        allowed_business_loss = min_(business_loss, business_gain + max_loss)
         limited_capital_loss = tax_unit("limited_capital_loss", period)
-        return min_(
-            max_loss,
-            self_employment_loss
-            + partnership_s_corp_loss
-            + limited_capital_loss,
-        )
+        # Capital losses are deductible in AGI, but excluded from the
+        # section 461(l) excess business loss limitation.
+        return allowed_business_loss + limited_capital_loss
