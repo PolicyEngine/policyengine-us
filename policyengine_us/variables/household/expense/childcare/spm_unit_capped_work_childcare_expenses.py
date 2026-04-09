d2ad9586de5dc97e3495e44402bd3695850d3fb6
Max Ghenis <mghenis@gmail.com>
2026-04-09 06:59:23 -0400

    Add Census SPM work expense formulas

--- policyengine_us/variables/household/expense/childcare/spm_unit_capped_work_childcare_expenses.py
+++ policyengine_us/variables/household/expense/childcare/spm_unit_capped_work_childcare_expenses.py
@@ -7,4 +7,12 @@ class spm_unit_capped_work_childcare_expenses(Variable):
     label = "SPM unit work and childcare expenses"
     definition_period = YEAR
     unit = USD
-    uprating = "gov.bls.cpi.cpi_u"
+
+    def formula_2024(spm_unit, period, parameters):
+        work_expenses = spm_unit("spm_unit_work_expenses", period)
+        childcare_expenses = spm_unit(
+            "spm_unit_pre_subsidy_childcare_expenses", period
+        )
+        earned_cap = spm_unit("spm_unit_head_spouse_earned_cap", period)
+        combined_expenses = np.maximum(work_expenses + childcare_expenses, 0)
+        return min_(combined_expenses, earned_cap)
