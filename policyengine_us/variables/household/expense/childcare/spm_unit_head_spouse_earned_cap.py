6a2f523179596c52888e92f1e0356374991d65db
Max Ghenis <mghenis@gmail.com>
2026-04-09 07:40:25 -0400

    Catch SPM childcare cap corner cases

--- policyengine_us/variables/household/expense/childcare/spm_unit_head_spouse_earned_cap.py
+++ policyengine_us/variables/household/expense/childcare/spm_unit_head_spouse_earned_cap.py
@@ -4,17 +4,38 @@ from policyengine_us.model_api import *
 class spm_unit_head_spouse_earned_cap(Variable):
     value_type = float
     entity = SPMUnit
-    label = "SPM unit lower-earner cap for work and childcare expenses"
+    label = "SPM unit lower-earner cap for the reference person and spouse/partner"
     definition_period = YEAR
     unit = USD
 
     def formula(spm_unit, period, parameters):
         person = spm_unit.members
+        is_reference_person = person("is_household_head", period)
+        has_reference_person = spm_unit.any(is_reference_person)
         is_head_or_spouse = person("is_tax_unit_head_or_spouse", period)
-        earned_income = person("earned_income", period)
-        eligible_earnings = is_head_or_spouse * np.maximum(earned_income, 0)
+        is_reference_person_spouse = (
+            person.tax_unit.any(is_reference_person)
+            & is_head_or_spouse
+            & ~is_reference_person
+        )
+        is_reference_person_partner = person(
+            "is_unmarried_partner_of_household_head", period
+        )
+        has_spouse = spm_unit.any(is_reference_person_spouse)
+        eligible_reference_person_or_partner = (
+            is_reference_person
+            | is_reference_person_spouse
+            | (~has_spouse & is_reference_person_partner)
+        )
+        eligible_people = where(
+            has_reference_person,
+            eligible_reference_person_or_partner,
+            is_head_or_spouse,
+        )
+        earned_income = person("spm_work_childcare_earnings", period)
+        eligible_earnings = eligible_people * np.maximum(earned_income, 0)
 
-        count_head_or_spouse = spm_unit.sum(is_head_or_spouse)
+        count_head_or_spouse = spm_unit.sum(eligible_people)
         total_earned = spm_unit.sum(eligible_earnings)
         max_earned = spm_unit.max(eligible_earnings)
 
