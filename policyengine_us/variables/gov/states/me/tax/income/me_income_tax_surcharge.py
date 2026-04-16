9d83644a05622073bb0b045804185365de1c57c3
David Trimmer <david@policyengine.org>
2026-04-16 10:09:09 -0400

    Add Maine income tax surcharge

---
+++ policyengine_us/variables/gov/states/me/tax/income/me_income_tax_surcharge.py
@@ -0,0 +1,24 @@
+from policyengine_us.model_api import *
+
+
+class me_income_tax_surcharge(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Maine income tax surcharge"
+    unit = USD
+    definition_period = YEAR
+    defined_for = StateCode.ME
+    reference = "https://legislature.maine.gov/legis/bills/getPDF.asp?paper=HP1491&item=2&snum=132#page=220"
+
+    def formula(tax_unit, period, parameters):
+        p = parameters(period).gov.states.me.tax.income.surcharge
+        if not p.in_effect:
+            return 0
+
+        taxable_income = tax_unit("me_taxable_income", period)
+        filing_status = tax_unit("filing_status", period)
+
+        threshold = p.threshold[filing_status]
+        income_above_threshold = max_(taxable_income - threshold, 0)
+
+        return income_above_threshold * p.rate
