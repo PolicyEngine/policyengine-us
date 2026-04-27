8725150ca614ee8ba014bc35883d008bd3d058e2
Max Ghenis <mghenis@gmail.com>
2026-04-26 22:29:37 -0400

    Make input variables pure leaves

--- policyengine_us/variables/gov/hhs/medicare/costs/medicare_enrolled.py
+++ policyengine_us/variables/gov/hhs/medicare/costs/medicare_enrolled.py
@@ -8,5 +8,4 @@ class medicare_enrolled(Variable):
     documentation = "Whether the person is enrolled in Medicare (Part A and/or Part B)"
     definition_period = YEAR
     reference = "https://www.cms.gov/medicare"
-    defined_for = "is_medicare_eligible"
-    default_value = True  # Most Medicare-eligible people are enrolled
+    default_value = False
