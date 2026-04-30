7b49cbc3fa288cece6d2321d0704f6794ac29643
Daphne Hansell <128793799+daphnehanse11@users.noreply.github.com>
2026-04-30 15:53:40 -0400

    Use actual bronze premium for selected marketplace plans

--- policyengine_us/variables/gov/aca/ptc/selected_marketplace_plan_premium_proxy.py
+++ policyengine_us/variables/gov/aca/ptc/selected_marketplace_plan_premium_proxy.py
@@ -1,4 +1,7 @@
 from policyengine_us.model_api import *
+from policyengine_us.variables.gov.aca.ptc.selected_marketplace_plan_category import (
+    MarketplacePlanCategory,
+)
 
 
 class selected_marketplace_plan_premium_proxy(Variable):
@@ -12,9 +15,19 @@ class selected_marketplace_plan_premium_proxy(Variable):
         takes_up_aca_if_eligible = tax_unit("takes_up_aca_if_eligible", period)
         person = tax_unit.members
         pays_marketplace_premium = tax_unit.sum(person("pays_aca_premium", period)) > 0
+        selected_plan_category = tax_unit(
+            "selected_marketplace_plan_category", period
+        )
+        silver_premium = tax_unit("slcsp", period) * tax_unit(
+            "selected_marketplace_plan_benchmark_ratio", period
+        )
+        selected_plan_premium = where(
+            selected_plan_category == MarketplacePlanCategory.BRONZE,
+            tax_unit("lcbp", period),
+            silver_premium,
+        )
         return where(
             takes_up_aca_if_eligible & pays_marketplace_premium,
-            tax_unit("slcsp", period)
-            * tax_unit("selected_marketplace_plan_benchmark_ratio", period),
+            selected_plan_premium,
             0,
         )
