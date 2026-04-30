4bb97ea2d3e140c1871064cfad56b93b139dfd28
Daphne Hansell <128793799+daphnehanse11@users.noreply.github.com>
2026-04-30 10:16:00 -0400

    Polish Medicaid non-custodial exception metadata

--- policyengine_us/variables/gov/hhs/medicaid/income/medicaid_tax_dependent_exception_non_custodial_parent.py
+++ policyengine_us/variables/gov/hhs/medicaid/income/medicaid_tax_dependent_exception_non_custodial_parent.py
@@ -5,10 +5,5 @@ class medicaid_tax_dependent_exception_non_custodial_parent(Variable):
     value_type = bool
     entity = Person
     label = "Medicaid MAGI tax-dependent exception for a child claimed by a non-custodial parent"
-    documentation = (
-        "Direct input for the Medicaid MAGI exception that applies when a "
-        "child is claimed by a non-custodial parent. PolicyEngine does not "
-        "currently link a child to a claiming parent outside the tax unit."
-    )
     definition_period = YEAR
     reference = "https://www.law.cornell.edu/cfr/text/42/435.603#f_2_iii"
