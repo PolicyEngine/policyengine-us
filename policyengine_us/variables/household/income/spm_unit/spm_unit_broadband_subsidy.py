326c23ee0aeb5dc05e1aed939c237664b6d751c8
Max Ghenis <mghenis@gmail.com>
2026-05-12 10:46:59 -0400

    Remove reported SPM input paths

--- policyengine_us/variables/household/income/spm_unit/spm_unit_broadband_subsidy.py
+++ policyengine_us/variables/household/income/spm_unit/spm_unit_broadband_subsidy.py
@@ -9,6 +9,4 @@ class spm_unit_broadband_subsidy(Variable):
     unit = USD
 
     def formula(spm_unit, period, parameters):
-        if parameters(period).gov.simulation.reported_broadband_subsidy:
-            return spm_unit("spm_unit_broadband_subsidy_reported", period)
         return add(spm_unit, period, ["ebb", "acp"])
