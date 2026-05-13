7bf3439367b4200e3e02da6e16c419cac828401c
Daphne Hansell <128793799+daphnehanse11@users.noreply.github.com>
2026-05-13 15:02:57 -0400

    Gate selected marketplace plan actuarial value

--- policyengine_us/variables/gov/aca/ptc/selected_marketplace_plan_actuarial_value.py
+++ policyengine_us/variables/gov/aca/ptc/selected_marketplace_plan_actuarial_value.py
@@ -10,6 +10,18 @@ class selected_marketplace_plan_actuarial_value(Variable):
     reference = "https://www.law.cornell.edu/cfr/text/45/156.140#b"
 
     def formula(tax_unit, period, parameters):
+        takes_up_aca_if_eligible = tax_unit("takes_up_aca_if_eligible", period)
+        person = tax_unit.members
+        pays_marketplace_premium = tax_unit.sum(person("pays_aca_premium", period)) > 0
         category = tax_unit("selected_marketplace_plan_category", period)
         p = parameters(period).gov.aca.metal_actuarial_value
-        return where(category == category.possible_values.BRONZE, p.bronze, p.silver)
+        selected_plan_av = where(
+            category == category.possible_values.BRONZE,
+            p.bronze,
+            p.silver,
+        )
+        return where(
+            takes_up_aca_if_eligible & pays_marketplace_premium,
+            selected_plan_av,
+            0,
+        )
