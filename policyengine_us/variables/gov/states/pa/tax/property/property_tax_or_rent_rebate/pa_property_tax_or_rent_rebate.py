cf67d90d125972d46d332e0bbde693df03e4de11
Daphne Hansell <128793799+daphnehanse11@users.noreply.github.com>
2026-05-14 16:00:48 -0400

    Add Pennsylvania property tax rent rebate

---
+++ policyengine_us/variables/gov/states/pa/tax/property/property_tax_or_rent_rebate/pa_property_tax_or_rent_rebate.py
@@ -0,0 +1,17 @@
+from policyengine_us.model_api import *
+
+
+class pa_property_tax_or_rent_rebate(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "Pennsylvania Property Tax/Rent Rebate"
+    unit = USD
+    definition_period = YEAR
+    reference = "https://www.pa.gov/agencies/revenue/ptrr"
+    defined_for = "pa_property_tax_or_rent_rebate_eligible"
+
+    def formula(tax_unit, period, parameters):
+        p = parameters(period).gov.states.pa.tax.property.property_tax_or_rent_rebate
+        income = tax_unit("adjusted_gross_income", period)
+        rent_and_property_tax = add(tax_unit, period, ["rent", "real_estate_taxes"])
+        return min_(p.amount.calc(income), rent_and_property_tax)
