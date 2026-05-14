0cf70b74e37159ee0fbfb3f28500b5ae85a53981
Daphne Hansell <128793799+daphnehanse11@users.noreply.github.com>
2026-05-14 15:54:19 -0400

    Add North Dakota renters refund

---
+++ policyengine_us/variables/gov/states/nd/tax/property/renters_refund/nd_renters_refund.py
@@ -0,0 +1,22 @@
+from policyengine_us.model_api import *
+
+
+class nd_renters_refund(Variable):
+    value_type = float
+    entity = TaxUnit
+    label = "North Dakota Renter's Refund"
+    unit = USD
+    definition_period = YEAR
+    reference = "https://www.tax.nd.gov/renters-refund"
+    defined_for = "nd_renters_refund_eligible"
+
+    def formula(tax_unit, period, parameters):
+        p = parameters(period).gov.states.nd.tax.property.renters_refund
+        return min_(
+            p.cap,
+            max_(
+                add(tax_unit, period, ["rent"]) * p.rent_rate
+                - tax_unit("adjusted_gross_income", period) * p.income_rate,
+                0,
+            ),
+        )
---
+++ policyengine_us/variables/gov/states/nd/tax/property/renters_refund/nd_renters_refund_eligible.py
@@ -0,0 +1,29 @@
+from policyengine_us.model_api import *
+
+
+class nd_renters_refund_eligible(Variable):
+    value_type = bool
+    entity = TaxUnit
+    label = "Eligible for the North Dakota Renter's Refund"
+    definition_period = YEAR
+    reference = "https://www.tax.nd.gov/renters-refund"
+    defined_for = StateCode.ND
+
+    def formula(tax_unit, period, parameters):
+        p = parameters(period).gov.states.nd.tax.property.renters_refund
+        person = tax_unit.members
+        head_or_spouse = person("is_tax_unit_head_or_spouse", period)
+        age = person("age", period.this_year)
+        is_disabled = person("is_disabled", period)
+        age_or_disability_eligible = tax_unit.any(
+            ((age >= p.age_threshold) | is_disabled) & head_or_spouse,
+        )
+        income = tax_unit("adjusted_gross_income", period)
+        rent = add(tax_unit, period, ["rent"])
+
+        return (
+            age_or_disability_eligible
+            & (income <= p.income_limit)
+            & (rent > 0)
+            & ((rent * p.rent_rate - income * p.income_rate) > 0)
+        )
