c098569378143ab20efa0dfa4f3b802280c7d389
Daphne Hansell <128793799+daphnehanse11@users.noreply.github.com>
2026-05-15 13:29:38 -0400

    Convert South Carolina homestead exemption to assessed value

--- policyengine_us/variables/gov/states/sc/tax/property/homestead_exemption/sc_homestead_exemption.py
+++ policyengine_us/variables/gov/states/sc/tax/property/homestead_exemption/sc_homestead_exemption.py
@@ -11,7 +11,9 @@ class sc_homestead_exemption(Variable):
     defined_for = "sc_homestead_exemption_eligible"
 
     def formula(tax_unit, period, parameters):
+        p = parameters(period).gov.states.sc.tax.property.homestead_exemption
+        assessed_exemption_amount = p.amount * p.assessment_rate
         return min_(
             add(tax_unit, period, ["assessed_property_value"]),
-            parameters(period).gov.states.sc.tax.property.homestead_exemption.amount,
+            assessed_exemption_amount,
         )
