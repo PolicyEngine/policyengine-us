b3a566ffbff91658df9ffced45ae123dbe003ec8
Max Ghenis <mghenis@gmail.com>
2026-05-17 09:00:26 -0400

    Compute capped SPM housing subsidy

--- policyengine_us/variables/household/income/spm_unit/spm_unit_capped_housing_subsidy.py
+++ policyengine_us/variables/household/income/spm_unit/spm_unit_capped_housing_subsidy.py
@@ -7,4 +7,14 @@ class spm_unit_capped_housing_subsidy(Variable):
     label = "Housing subsidies"
     definition_period = YEAR
     unit = USD
-    uprating = "gov.bls.cpi.cpi_u"
+    reference = "https://www2.census.gov/programs-surveys/supplemental-poverty-measure/datasets/spm/spm_techdoc.pdf"
+
+    def formula(spm_unit, period, parameters):
+        housing_assistance = spm_unit("housing_assistance", period)
+        housing_portion = spm_unit(
+            "spm_unit_spm_threshold_housing_portion",
+            period,
+        )
+        tenant_payment = spm_unit("hud_ttp", period)
+        cap = max_(housing_portion - tenant_payment, 0)
+        return min_(housing_assistance, cap)
