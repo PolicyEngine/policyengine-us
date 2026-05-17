88172d52c1ffed68c16e22527d1087d48d0276ec
Max Ghenis <mghenis@gmail.com>
2026-05-16 23:40:27 -0400

    Include missing SPM resources and canonicalize subsidies

--- policyengine_us/variables/household/income/spm_unit/spm_unit_capped_housing_subsidy.py
+++ policyengine_us/variables/household/income/spm_unit/spm_unit_capped_housing_subsidy.py
@@ -7,11 +7,4 @@ class spm_unit_capped_housing_subsidy(Variable):
     label = "Housing subsidies"
     definition_period = YEAR
     unit = USD
-
-    def formula(spm_unit, period, parameters):
-        if parameters(period).gov.hud.abolition:
-            return 0
-        disabled_programs = parameters(period).simulation.disabled_programs
-        if "spm_unit_capped_housing_subsidy" in disabled_programs:
-            return spm_unit("spm_unit_capped_housing_subsidy_data", period)
-        return 0
+    uprating = "gov.bls.cpi.cpi_u"
