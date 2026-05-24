a2fae42a9faefbbc2ac77f9ea060315ef9998ab8
Max Ghenis <mghenis@gmail.com>
2026-05-23 22:46:46 -0400

    Fix dependent Social Security taxability

--- policyengine_us/variables/gov/irs/income/taxable_income/adjusted_gross_income/irs_gross_income/social_security/tax_unit_social_security.py
+++ policyengine_us/variables/gov/irs/income/taxable_income/adjusted_gross_income/irs_gross_income/social_security/tax_unit_social_security.py
@@ -7,10 +7,5 @@ class tax_unit_social_security(Variable):
     label = "Tax unit Social Security"
     unit = USD
     definition_period = YEAR
-    reference = "https://www.irs.gov/faqs/social-security-income"
 
-    def formula(tax_unit, period, parameters):
-        person = tax_unit.members
-        not_dependent = ~person("is_tax_unit_dependent", period)
-        social_security = person("social_security", period)
-        return tax_unit.sum(not_dependent * social_security)
+    adds = ["social_security"]
