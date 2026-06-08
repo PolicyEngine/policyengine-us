0c68c40dd79ef7b608042399dfc4421c7e75a0ba
Ziming Hua <113824411+hua7450@users.noreply.github.com>
2026-06-08 00:15:33 -0400

    Fix Indiana TANF maximum benefit

--- policyengine_us/variables/gov/states/in/fssa/tanf/eligibility/in_tanf_payment_eligible.py
+++ policyengine_us/variables/gov/states/in/fssa/tanf/eligibility/in_tanf_payment_eligible.py
@@ -7,12 +7,12 @@ class in_tanf_payment_eligible(Variable):
     label = "Indiana TANF payment eligible"
     definition_period = MONTH
     reference = (
-        "https://iar.iga.in.gov/code/2026/470/10.3#470-10.3-4-3",
         "https://iga.in.gov/laws/2025/ic/titles/12/#12-14-2-5",
+        "https://www.in.gov/fssa/dfr/files/3000.pdf#page=7",
     )
     defined_for = StateCode.IN
 
     def formula(spm_unit, period, parameters):
         countable = spm_unit("in_tanf_countable_income_for_payment", period)
-        payment_standard = spm_unit("in_tanf_payment_standard", period)
-        return countable < payment_standard
+        maximum_benefit = spm_unit("in_tanf_maximum_benefit", period)
+        return countable < maximum_benefit
