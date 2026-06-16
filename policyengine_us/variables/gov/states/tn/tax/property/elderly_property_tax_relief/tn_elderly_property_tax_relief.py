6650d99c15193dd29e5c4e7988cf69933f962272
Daphne Hansell <128793799+daphnehanse11@users.noreply.github.com>
2026-06-16 15:43:41 -0400

    Address Tennessee property tax relief review

--- policyengine_us/variables/gov/states/tn/tax/property/elderly_property_tax_relief/tn_elderly_property_tax_relief.py
+++ policyengine_us/variables/gov/states/tn/tax/property/elderly_property_tax_relief/tn_elderly_property_tax_relief.py
@@ -9,20 +9,31 @@ class tn_elderly_property_tax_relief(Variable):
     definition_period = YEAR
     reference = (
         "https://comptroller.tn.gov/office-functions/pa/property-taxes/property-tax-programs/tax-relief.html",
-        "https://comptroller.tn.gov/content/dam/cot/pa/documents/tax-relief/TaxReliefBrochure.pdf",
-        "https://comptroller.tn.gov/content/dam/cot/pa/documents/tax-relief/TCA%2067-5-701%20through%2067-5-704.pdf",
+        "https://comptroller.tn.gov/content/dam/cot/pa/documents/tax-relief/TaxReliefBrochure.pdf#page=2",
+        "https://comptroller.tn.gov/content/dam/cot/pa/documents/tax-relief/TCA%2067-5-701%20through%2067-5-704.pdf#page=3",
     )
     defined_for = "tn_elderly_property_tax_relief_eligible"
 
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states.tn.tax.property.elderly_property_tax_relief
-        assessed_property_value = add(tax_unit, period, ["assessed_property_value"])
+        person = tax_unit.members
+        head = person("is_tax_unit_head", period)
+        joint_spouse = person("is_tax_unit_spouse", period) & person.tax_unit(
+            "tax_unit_is_joint", period
+        )
+        head_or_joint_spouse = head | joint_spouse
+        assessed_property_value = tax_unit.sum(
+            person("assessed_property_value", period) * head_or_joint_spouse
+        )
+        real_estate_taxes = tax_unit.sum(
+            person("real_estate_taxes", period) * head_or_joint_spouse
+        )
         capped_assessed_property_value = min_(
             assessed_property_value,
             p.property_value_cap * p.assessment_rate,
         )
         return (
-            add(tax_unit, period, ["real_estate_taxes"])
+            real_estate_taxes
             * capped_assessed_property_value
-            / assessed_property_value
+            / max_(assessed_property_value, 1)
         )
--- policyengine_us/variables/gov/states/tn/tax/property/elderly_property_tax_relief/tn_elderly_property_tax_relief_eligible.py
+++ policyengine_us/variables/gov/states/tn/tax/property/elderly_property_tax_relief/tn_elderly_property_tax_relief_eligible.py
@@ -8,20 +8,33 @@ class tn_elderly_property_tax_relief_eligible(Variable):
     definition_period = YEAR
     reference = (
         "https://comptroller.tn.gov/office-functions/pa/property-taxes/property-tax-programs/tax-relief.html",
-        "https://comptroller.tn.gov/content/dam/cot/pa/documents/tax-relief/TaxReliefBrochure.pdf",
-        "https://comptroller.tn.gov/content/dam/cot/pa/documents/tax-relief/TCA%2067-5-701%20through%2067-5-704.pdf",
+        "https://comptroller.tn.gov/content/dam/cot/pa/documents/tax-relief/TaxReliefBrochure.pdf#page=2",
+        "https://comptroller.tn.gov/content/dam/cot/pa/documents/tax-relief/TCA%2067-5-701%20through%2067-5-704.pdf#page=3",
     )
     defined_for = StateCode.TN
 
     def formula(tax_unit, period, parameters):
         p = parameters(period).gov.states.tn.tax.property.elderly_property_tax_relief
+        person = tax_unit.members
+        head = person("is_tax_unit_head", period)
+        joint_spouse = person("is_tax_unit_spouse", period) & person.tax_unit(
+            "tax_unit_is_joint", period
+        )
+        head_or_joint_spouse = head | joint_spouse
+        age = person("age", period.this_year)
+        assessed_property_value = tax_unit.sum(
+            person("assessed_property_value", period) * head_or_joint_spouse
+        )
+        real_estate_taxes = tax_unit.sum(
+            person("real_estate_taxes", period) * head_or_joint_spouse
+        )
         return (
-            (tax_unit("greater_age_head_spouse", period) >= p.age_threshold)
+            tax_unit.any((age >= p.age_threshold) & head_or_joint_spouse)
             & (
                 tax_unit("tn_elderly_property_tax_relief_income", period)
                 <= p.income_limit
             )
-            & (add(tax_unit, period, ["real_estate_taxes"]) > 0)
-            & (add(tax_unit, period, ["assessed_property_value"]) > 0)
+            & (real_estate_taxes > 0)
+            & (assessed_property_value > 0)
             & ~tax_unit("rents", period)
         )
