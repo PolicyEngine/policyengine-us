7f79f6d6ed33a95e8a9e83c32588d744bc17b4cc
Daphne Hansell <128793799+daphnehanse11@users.noreply.github.com>
2026-06-22 13:37:50 -0400

    Fix formatting

--- policyengine_us/variables/gov/hhs/medicaid/eligibility/is_in_medicaid_community_engagement_treatment_program.py
+++ policyengine_us/variables/gov/hhs/medicaid/eligibility/is_in_medicaid_community_engagement_treatment_program.py
@@ -5,8 +5,7 @@ class is_in_medicaid_community_engagement_treatment_program(Variable):
     value_type = bool
     entity = Person
     label = (
-        "Participates in qualifying treatment program for Medicaid community "
-        "engagement"
+        "Participates in qualifying treatment program for Medicaid community engagement"
     )
     definition_period = YEAR
     default_value = False
@@ -17,4 +16,3 @@ class is_in_medicaid_community_engagement_treatment_program(Variable):
         "commitments are not modeled."
     )
     reference = "https://www.govinfo.gov/content/pkg/FR-2026-06-03/pdf/2026-11094.pdf"
-
